38ca0892b4e65b85d33bf591645a208b0c65ecb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:28 +0000

    Add package eveimageserver/0.1.0/eveimageserver-0.1.0.tar.gz

---
+++ packages/eveimageserver/eveimageserver-0.1.0.tar.gz/eveimageserver-0.1.0/eveimageserver/__init__.py
@@ -0,0 +1,12 @@
+
+
+from .main import (
+    get_character_links,
+    get_corporation_links,
+    get_alliance_links,
+    get_faction_links,
+    get_type_links,
+    get_image_server_link,
+)
+
+__version__ = '0.1.0'
