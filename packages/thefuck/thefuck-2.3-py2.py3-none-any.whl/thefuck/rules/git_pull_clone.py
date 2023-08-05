4147956c21abab32aa784c5b79b45037965954ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:32 +0000

    Add package thefuck/2.3/thefuck-2.3-py2.py3-none-any.whl

---
+++ packages/thefuck/thefuck-2.3-py2.py3-none-any.whl/thefuck/rules/git_pull_clone.py
@@ -0,0 +1,12 @@
+from thefuck import utils
+
+
+@utils.git_support
+def match(command, settings):
+    return ('fatal: Not a git repository' in command.stderr
+            and "Stopping at filesystem boundary (GIT_DISCOVERY_ACROSS_FILESYSTEM not set)." in command.stderr)
+
+
+@utils.git_support
+def get_new_command(command, settings):
+    return command.script.replace(' pull ', ' clone ')
