ba1101399ad23d135d93657af49aefeba5ffa3eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:33 +0000

    Add package toolspy/0.1.2/toolspy-0.1.2.tar.gz

---
+++ packages/toolspy/toolspy-0.1.2.tar.gz/toolspy-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='toolspy',
+    version='0.1.2',
+    long_description='A python toolset',
+    packages=['toolspy'],
+    include_package_data=True,
+    license='MIT',
+    author='SuryaSankar',
+    author_email='suryashankar.m@gmail.com')
