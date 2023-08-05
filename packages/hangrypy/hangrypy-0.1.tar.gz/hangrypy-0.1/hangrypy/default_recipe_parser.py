ffb287de13657904c3b5378063c31833aa1abbc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:05 +0000

    Add package hangrypy/0.1/hangrypy-0.1.tar.gz

---
+++ packages/hangrypy/hangrypy-0.1.tar.gz/hangrypy-0.1/hangrypy/default_recipe_parser.py
@@ -0,0 +1,20 @@
+from isodate import parse_duration
+
+
+class recipe_parser(object):
+    def __init__(self, soup):
+        self.soup = soup
+
+    # TODO: create a default recipe parser
+
+    def parse_prep(self):
+        pass
+
+    def parse_isoduration(self, datetime):
+        try:
+            return int(parse_duration(datetime).total_seconds())
+        except:
+            try:
+                return int(datetime)
+            except:
+                return None
