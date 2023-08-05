a041751c1390fb9da5fa04094f5c32e31562f656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:11 +0000

    Add package cron-sentry/0.5.0/cron-sentry-0.5.0.tar.gz

---
+++ packages/cron-sentry/cron-sentry-0.5.0.tar.gz/cron-sentry-0.5.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from cron_sentry.version import VERSION
+
+setup(
+    name='cron-sentry',
+    version=VERSION,
+    author='Yipit Coders',
+    author_email='coders@yipit.com',
+    description='Cron-Sentry is a command-line wrapper that reports unsuccessful runs to Sentry (https://www.getsentry.com)',
+    long_description=open('README.md').read(),
+    license='MIT',
+    classifiers=[
+        'Topic :: Utilities',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+    ],
+    url='http://github.com/yipit/cron-sentry',
+    packages=find_packages(),
+    install_requires=['raven'],
+    data_files=[],
+    entry_points={
+        'console_scripts': [
+            # `raven-cron` entry point is for backwards compatibility purposes.
+            # it should get removed in future releases
+            'raven-cron = cron_sentry.runner:run',
+            'cron-sentry = cron_sentry.runner:run',
+        ]
+    }
+)
