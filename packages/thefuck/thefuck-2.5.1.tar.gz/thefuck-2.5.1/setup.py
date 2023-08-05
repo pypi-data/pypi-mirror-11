0a60b0cb4f75ecd31cd9e4dd9f490d6281f2ae57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:33 +0000

    Add package thefuck/2.5.1/thefuck-2.5.1.tar.gz

---
+++ packages/thefuck/thefuck-2.5.1.tar.gz/thefuck-2.5.1/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import sys
+
+try:
+    import pypandoc
+
+    long_description = pypandoc.convert('README.md', 'rst', 'markdown_github')
+except:
+    long_description = open('README.md').read()
+
+version = sys.version_info[:2]
+if version < (2, 7):
+    print('thefuck requires Python version 2.7 or later' +
+          ' ({}.{} detected).'.format(*version))
+    sys.exit(-1)
+elif (3, 0) < version < (3, 3):
+    print('thefuck requires Python version 3.3 or later' +
+          ' ({}.{} detected).'.format(*version))
+    sys.exit(-1)
+
+VERSION = '2.5.1'
+
+install_requires = ['psutil', 'colorama', 'six']
+extras_require = {':python_version<"3.4"': ['pathlib']}
+
+setup(name='thefuck',
+      version=VERSION,
+      description="Magnificent app which corrects your previous console command",
+      long_description=long_description,
+      author='Vladimir Iakovlev',
+      author_email='nvbn.rm@gmail.com',
+      url='https://github.com/nvbn/thefuck',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples',
+                                      'tests', 'release']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      extras_require=extras_require,
+      entry_points={'console_scripts': [
+          'thefuck = thefuck.main:main',
+          'thefuck-alias = thefuck.main:print_alias']})
