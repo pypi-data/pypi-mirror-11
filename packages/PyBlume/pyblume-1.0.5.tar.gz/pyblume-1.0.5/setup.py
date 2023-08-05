2de2016647d62a010366809ad539046100f07361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:15 +0000

    Add package PyBlume/1.0.5/pyblume-1.0.5.tar.gz

---
+++ packages/PyBlume/pyblume-1.0.5.tar.gz/pyblume-1.0.5/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup, Extension
+setup(name="pyblume",
+	version="1.0.5",
+	url='http://terbiumlabs.com/pyblume',
+	license='BSD',
+	author="Terbium Labs",
+	author_email="floss_dev@terbiumlabs.com",
+	description='A fast, scalable Bloom Filter for Python',
+	zip_safe=False,
+    include_package_data=True,
+    platforms='Linux',
+    ext_modules=[Extension("pyblume", ["pyblume.c", "murmur.c", "blume.c"])],
+	classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: C']
+    )
