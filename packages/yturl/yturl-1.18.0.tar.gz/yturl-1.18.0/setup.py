47006c8ec53f8e6d1e5373ef0a5617a43424417b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:02 +0000

    Add package yturl/1.18.0/yturl-1.18.0.tar.gz

---
+++ packages/yturl/yturl-1.18.0.tar.gz/yturl-1.18.0/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="yturl",
+    version="1.18.0",
+    description="Gets direct media URLs to YouTube media",
+    long_description="""
+    Gets direct media URLs to YouTube media, freeing you having to view them in
+    your browser.
+    """,
+    url="https://github.com/cdown/yturl",
+    license='ISC',
+
+    author="Chris Down",
+    author_email="chris@chrisdown.name",
+
+    py_modules=["yturl"],
+
+    entry_points={
+        'console_scripts': ['yturl=yturl:_main'],
+    },
+
+    keywords='youtube media video',
+
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Multimedia",
+        "Topic :: Internet",
+        "Topic :: Utilities",
+    ],
+)
