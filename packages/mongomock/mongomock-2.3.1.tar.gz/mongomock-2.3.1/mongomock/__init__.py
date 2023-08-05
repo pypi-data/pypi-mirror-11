89a00326e1a76c9a479e1deb9c5dd214db718b2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:30 +0000

    Add package mongomock/2.3.1/mongomock-2.3.1.tar.gz

---
+++ packages/mongomock/mongomock-2.3.1.tar.gz/mongomock-2.3.1/mongomock/__init__.py
@@ -0,0 +1,37 @@
+import sys
+
+from .helpers import ObjectId
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+try:
+    from pymongo.errors import DuplicateKeyError
+except:
+    class DuplicateKeyError(Exception):
+        pass
+
+try:
+    from pymongo.errors import OperationFailure
+except:
+    class OperationFailure(Exception):
+        pass
+
+try:
+    from pymongo.errors import CollectionInvalid
+except:
+    class CollectionInvalid(Exception):
+        pass
+
+from mongomock.__version__ import __version__
+
+
+__all__ = ['Connection', 'Database', 'Collection', 'ObjectId', 'WriteConcern']
+
+
+from .write_concern import WriteConcern
+from .connection import Connection, MongoClient
+from .database import Database
+from .collection import Collection
+
