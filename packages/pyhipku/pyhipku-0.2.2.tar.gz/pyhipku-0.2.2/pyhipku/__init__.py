cb34273b7b3e475d3324def7bee1ce1cd1f53a2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:01 +0000

    Add package pyhipku/0.2.2/pyhipku-0.2.2.tar.gz

---
+++ packages/pyhipku/pyhipku-0.2.2.tar.gz/pyhipku-0.2.2/pyhipku/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    hipku
+    ~~~~~
+
+    Encode any IP address as a haiku.
+
+    :copyright: (c) 2015 by lord63.
+    :license: MIT, see LICENSE for more details.
+"""
+
+from __future__ import absolute_import
+
+from .encode import encode
+from .decode import decode
+
+
+__all__ = ["encode", "decode"]
+__title__ = "hipku"
+__version__ = "0.2.2"
+__author__ = "lord63"
+__license__ = "MIT"
+__copyright__ = "Copyright 2015 lord63"
