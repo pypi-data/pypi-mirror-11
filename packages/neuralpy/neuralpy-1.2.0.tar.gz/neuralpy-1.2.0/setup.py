a9e3f0a7ba69b8c16209fb7a31f413a5cc9b9eb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:15 +0000

    Add package neuralpy/1.2.0/neuralpy-1.2.0.tar.gz

---
+++ packages/neuralpy/neuralpy-1.2.0.tar.gz/neuralpy-1.2.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+setup(
+	name='neuralpy',
+	version='1.2.0',
+	description='The most intuitive Neural Network Model',
+	author='Jonathan N. Lee',
+	author_email='jonathan_lee@berkeley.edu',
+	url='https://github.com/jon--lee/neuralpy',
+	license='MIT',
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+		'Intended Audience :: Developers',
+		'Intended Audience :: Science/Research',
+		'Topic :: Software Development',
+		'Topic :: Scientific/Engineering',
+		'Topic :: Scientific/Engineering :: Artificial Intelligence',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 2.7',
+
+		],
+	install_requires=['numpy==1.9.2', 'matplotlib==1.4.3'],
+	packages=find_packages()
+	)
+
+
