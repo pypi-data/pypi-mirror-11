afda5f899070f6f0671fa8c0891d0dbe5a25ed88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:17 +0000

    Add package mnamr/0.1.1/mnamr-0.1.1.tar.gz

---
+++ packages/mnamr/mnamr-0.1.1.tar.gz/mnamr-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mnamr",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=["imdbpie>=4.0.2"],
+    entry_points={
+        'console_scripts': [
+            'mnamr = mnamr.mnamr:main',
+        ],
+    },
+    author="Ali Kaafarani",
+    author_email="ali@kvikshaug.no",
+    description="Movie folder renamer",
+    license="MIT",
+    url="https://pypi.python.org/pypi/mnamr/",
+)
