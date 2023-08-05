1c701377860383713fc99dbf44cdd1b6ead5dd57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:26 +0000

    Add package dojson/0.1.1/dojson-0.1.1-py2-none-any.whl

---
+++ packages/dojson/dojson-0.1.1-py2-none-any.whl/dojson/overdo.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of DoJSON
+# Copyright (C) 2015 CERN.
+#
+# DoJSON is free software; you can redistribute it and/or
+# modify it under the terms of the Revised BSD License; see LICENSE
+# file for more details.
+
+"""Do JSON translation."""
+
+import re
+
+import esmre
+
+from six import iteritems
+
+
+class Overdo(object):
+
+    """Translation index."""
+
+    def __init__(self):
+        """Constructor."""
+        self.rules = []
+        self.index = None
+
+    def build(self):
+        """Build."""
+        self.index = esmre.Index()
+        for rule in self.rules:
+            self.index.enter(*rule)
+
+    def over(self, name, *source_tags):
+        """Register creator rule."""
+        def decorator(creator):
+            self.index = None
+            for field in source_tags:
+                self.rules.append((field, (name, creator, re.compile(field))))
+            return creator
+        return decorator
+
+    def _query(self, key):
+        """Query."""
+        for name, creator, field in self.index.query(key):
+            if field.match(key):
+                yield name, creator, field
+
+    def do(self, blob):
+        """Translate blob values and instantiate new model instance."""
+        output = {}
+
+        if self.index is None:
+            self.build()
+
+        for key, value in iteritems(blob):
+            for name, creator, field in self._query(key):
+                output[name] = creator(output, key, value)
+
+        return output
+
+    def missing(self, blob):
+        """Return keys with missing rules."""
+        if self.index is None:
+            self.build()
+        return [key for key in blob.keys() if not len(list(self._query(key)))]
