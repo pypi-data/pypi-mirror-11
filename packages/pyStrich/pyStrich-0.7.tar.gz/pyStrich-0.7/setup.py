6eec39069bf25767a8a87e337fa61d30f6dd5851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:05 +0000

    Add package pyStrich/0.7/pyStrich-0.7.tar.gz

---
+++ packages/pyStrich/pyStrich-0.7.tar.gz/pyStrich-0.7/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyStrich',
+    version='0.7',
+    packages=['pystrich',
+              'pystrich.ean13',
+              'pystrich.qrcode',
+              'pystrich.code128',
+              'pystrich.datamatrix',
+              'pystrich.fonts'],
+    package_data={
+        'pystrich.fonts': ['*.pil', '*.pbm'],
+        'pystrich.qrcode': ['qrcode_data/*.dat']
+    },
+    install_requires = ['Pillow'],
+    url='http://method-b.uk/pystrich/',
+    license='Apache 2.0',
+    author='Michael Mulqueen',
+    author_email='michael@mulqueen.me.uk',
+    description='PyStrich is a Python module to generate 1D and 2D barcodes (Code 128, DataMatrix, QRCode and EAN13).'
+                'Forked from huBarcode.',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Office/Business"
+    ]
+)
