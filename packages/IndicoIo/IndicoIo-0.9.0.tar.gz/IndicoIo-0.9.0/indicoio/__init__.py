b772d4276edcc014a95626621336cc585fa1bad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:25 +0000

    Add package IndicoIo/0.9.0/IndicoIo-0.9.0.tar.gz

---
+++ packages/IndicoIo/IndicoIo-0.9.0.tar.gz/IndicoIo-0.9.0/indicoio/__init__.py
@@ -0,0 +1,49 @@
+from functools import wraps, partial
+import warnings
+
+Version, version, __version__, VERSION = ('0.9.0',) * 4
+
+JSON_HEADERS = {
+    'Content-type': 'application/json',
+    'Accept': 'application/json',
+    'client-lib': 'python',
+    'version-number': VERSION
+}
+
+from indicoio.text.sentiment import political, posneg, sentiment_hq
+from indicoio.text.sentiment import posneg as sentiment
+from indicoio.text.lang import language
+from indicoio.text.tagging import text_tags
+from indicoio.text.keywords import keywords
+from indicoio.text.ner import named_entities
+from indicoio.images.fer import fer
+from indicoio.images.features import facial_features
+from indicoio.images.features import image_features
+from indicoio.images.filtering import content_filtering
+from indicoio.utils.multi import predict_image, predict_text
+
+from indicoio.config import API_NAMES
+
+def deprecation_decorator(f, api):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        warnings.warn(
+            "'batch_" + api + "' will be deprecated in the next major update.  Please call '" + api + "' instead with the same arguments.",
+            DeprecationWarning
+        )
+        return f(*args, **kwargs)
+    return wrapper
+
+def detect_batch_decorator(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if isinstance(args[0], list):
+            kwargs['batch'] = True
+        return f(*args, **kwargs)
+    return wrapper
+    
+apis = dict((api, globals().get(api)) for api in API_NAMES)
+
+for api in apis:
+    globals()[api] = partial(detect_batch_decorator(apis[api]))
+    globals()['batch_' + api] = partial(deprecation_decorator(apis[api], api), batch=True)
---
+++ packages/IndicoIo/IndicoIo-0.9.0.tar.gz/IndicoIo-0.9.0/indicoio/config.py
@@ -0,0 +1,75 @@
+import os
+from StringIO import StringIO
+
+import ConfigParser
+
+class Settings(ConfigParser.ConfigParser):
+
+    def __init__(self, *args, **kwargs):
+        """
+        files: filepaths or open file objects
+        """
+        self.files = kwargs.pop('files')
+
+        ConfigParser.ConfigParser.__init__(self, *args, **kwargs)
+
+        for fd in self.files:
+            try:
+                self.readfp(fd)
+            except AttributeError:
+                self.read(fd)
+
+        self.auth_settings = self.get_section('auth')
+        self.private_cloud_settings = self.get_section('private_cloud')
+
+    def get_section(self, section):
+        """
+        Retrieve a ConfigParser section as a dictionary, default to {}
+        """
+        try:
+            return dict(self.items(section))
+        except ConfigParser.NoSectionError:
+            return {}
+
+    def cloud(self):
+        return (
+            os.getenv("INDICO_CLOUD") or
+            self.private_cloud_settings.get('cloud') or
+            None
+        )
+
+    def api_key(self):
+        return (
+            os.getenv("INDICO_API_KEY") or
+            self.auth_settings.get('api_key') or
+            None
+        )
+
+TEXT_APIS = [
+    'text_tags',
+    'political',
+    'sentiment',
+    'language',
+    'sentiment_hq',
+    'keywords',
+    'named_entities'
+]
+
+IMAGE_APIS = [
+    'fer',
+    'facial_features',
+    'image_features',
+    'content_filtering'
+]
+
+API_NAMES = IMAGE_APIS + TEXT_APIS + ["predict_text", "predict_image"]
+
+SETTINGS = Settings(files=[
+    os.path.expanduser("~/.indicorc"),
+    os.path.join(os.getcwd(), '.indicorc')
+])
+
+api_key = SETTINGS.api_key()
+cloud = SETTINGS.cloud()
+PUBLIC_API_HOST = 'apiv2.indico.io'
+url_protocol = "https:"
