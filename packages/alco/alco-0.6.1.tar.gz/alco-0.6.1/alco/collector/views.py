5db6dab4924ddf226e9b532f0ffa41246038df7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:29 +0000

    Add package alco/0.6.1/alco-0.6.1.tar.gz

---
+++ packages/alco/alco-0.6.1.tar.gz/alco-0.6.1/alco/collector/views.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+# $Id: $
+
+
+from django.views.generic import ListView
+from django.conf import settings
+
+from alco.collector.models import LoggerIndex
+
+
+class LoggerIndexView(ListView):
+    model = LoggerIndex
+    content_type = 'text/plain'
+    template_name = 'collector/sphinx.conf'
+
+    def get_context_data(self, **kwargs):
+        cd = super(LoggerIndexView, self).get_context_data(**kwargs)
+        cd['settings'] = settings
+        return cd
