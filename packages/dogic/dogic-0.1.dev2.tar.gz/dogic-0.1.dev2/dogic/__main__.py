55fa78112f5464365e2ddb7adf10057701744315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:34 +0000

    Add package dogic/0.1.dev2/dogic-0.1.dev2.tar.gz

---
+++ packages/dogic/dogic-0.1.dev2.tar.gz/dogic-0.1.dev2/dogic/__main__.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+if __package__ == '':
+    # __file__ is pip-*.whl/pip/__main__.py
+    # first dirname call strips of '/__main__.py', second strips off '/pip'
+    # Resulting path is the name of the wheel itself
+    # Add that to sys.path so we can import pip
+    path = os.path.dirname(os.path.dirname(__file__))
+    sys.path.insert(0, path)
+
+import dogic  # noqa
+
+if __name__ == '__main__':
+  dogic.main()
