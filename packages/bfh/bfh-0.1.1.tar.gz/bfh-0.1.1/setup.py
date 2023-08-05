4e80121479ba38a0595346b904dadbec078fbe2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:43 +0000

    Add package bfh/0.1.1/bfh-0.1.1.tar.gz

---
+++ packages/bfh/bfh-0.1.1.tar.gz/bfh-0.1.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='bfh',
+    version="0.1.1",
+    description="Smacks schemas into other schemas",
+    author="Evan Bender",
+    install_requires=[
+        "python-dateutil",
+    ],
+    author_email="evan.bender@percolate.com",
+    url="https://github.com/percolate/bfh",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Programming Language :: Python :: 2.7",
+    ],
+    packages=["bfh"],
+)
