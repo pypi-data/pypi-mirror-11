5815ce7aa5d221f7cc76ff392bf6a771e5406039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:47 +0000

    Add package pygnip/1.0.0.a1/pygnip-1.0.0.a1.tar.gz

---
+++ packages/pygnip/pygnip-1.0.0.a1.tar.gz/pygnip-1.0.0.a1/pygnip/__init__.py
@@ -0,0 +1,1 @@
+from pygnip import GnipPowerTrack
---
+++ packages/pygnip/pygnip-1.0.0.a1.tar.gz/pygnip-1.0.0.a1/setup.py
@@ -0,0 +1,31 @@
+"""
+Python-Gnip
+"""
+from setuptools import setup
+
+
+setup(
+    name='pygnip',
+    version='1.0.0.a1',
+    keywords=['gnip api powertrack'],
+    url='https://github.com/benjiao/python-gnip',
+    license='BSD',
+    author='Benjie Jiao',
+    author_email='benjiao12@gmail.com',
+    description='A wrapper for the Gnip API',
+    long_description=__doc__,
+    py_modules=['pygnip.pygnip'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
