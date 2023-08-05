30caa10a0a924c25dfac02f6b3b1d1be1b85ac33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:44 +0000

    Add package django-handlers/0.1.1/django-handlers-0.1.1.tar.gz

---
+++ packages/django-handlers/django-handlers-0.1.1.tar.gz/django-handlers-0.1.1/tests/conftest.py
@@ -0,0 +1,31 @@
+def pytest_configure():
+    import django
+    from django.conf import settings
+
+    settings.configure(
+        DEBUG_PROPAGATE_EXCEPTIONS=True,
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:'
+            }
+        },
+        SITE_ID=1,
+        SECRET_KEY='secret',
+        ROOT_URLCONF='tests.urls',
+        MIDDLEWARE_CLASSES=(
+            'django.middleware.common.CommonMiddleware',
+            'django.contrib.sessions.middleware.SessionMiddleware',
+            'django.contrib.auth.middleware.AuthenticationMiddleware',
+            'django.contrib.messages.middleware.MessageMiddleware',
+        ),
+        INSTALLED_APPS=(
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.sessions',
+            'django.contrib.sites',
+            'django.contrib.staticfiles',
+        ),
+    )
+
+    django.setup()
