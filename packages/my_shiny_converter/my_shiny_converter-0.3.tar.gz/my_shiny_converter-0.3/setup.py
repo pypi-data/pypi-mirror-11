ea25643189c8bc1df3217bb7f3322b3aa69b3a73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:28 +0000

    Add package my_shiny_converter/0.3/my_shiny_converter-0.3.tar.gz

---
+++ packages/my_shiny_converter/my_shiny_converter-0.3.tar.gz/my_shiny_converter-0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="my_shiny_converter",
+    version="0.3",
+    install_requires=[
+        'pandas',
+    ],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'id_concat = converter.converter:main',
+        ]
+    }
+)
