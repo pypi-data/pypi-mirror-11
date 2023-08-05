2b90df2d90580138d30c317ead0b86af6e2216ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:52 +0000

    Add package openamazon/0.1/openamazon-0.1.zip

---
+++ packages/openamazon/openamazon-0.1.zip/openamazon-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'openamazon',
+  packages = ['openamazon'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A basic crawler for amazon.in website ',
+  author = 'Sanchit Sokhey',
+  author_email = 'sanchit.sokhey@gmail.com',
+  url = 'https://github.com/djsan15/openamazon', # use the URL to the github repo
+  download_url = 'https://github.com/djsan15/openamazon/tarball/0.1', # I'll explain this in a second
+  keywords = ['crawling', 'amazon', 'scrap','api'], # arbitrary keywords
+  classifiers = [],
+)
