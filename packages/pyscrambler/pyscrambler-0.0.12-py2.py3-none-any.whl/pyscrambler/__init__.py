35ae0e27263c99c22b9c59044b0859ce3abd2f36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:31 +0000

    Add package pyscrambler/0.0.12/pyscrambler-0.0.12-py2.py3-none-any.whl

---
+++ packages/pyscrambler/pyscrambler-0.0.12-py2.py3-none-any.whl/pyscrambler/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from . import binary
+from . import permutation
+from . import rearrange
+from . import scramblers
+
+
+__all__ = ['permutation', 'rearrange', 'binary', 'scramblers']
---
+++ packages/pyscrambler/pyscrambler-0.0.12-py2.py3-none-any.whl/pyscrambler/scramblers/__init__.py
@@ -0,0 +1,5 @@
+from .basescrambler import BaseScrambler
+from .multiscrambler import MultiScrambler
+
+
+__all__ = ['BaseScrambler', 'MultiScrambler']
