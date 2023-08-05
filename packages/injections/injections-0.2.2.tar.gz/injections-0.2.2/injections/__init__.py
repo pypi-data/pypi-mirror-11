fbcf41d1813080940375856289f4ae51ca9bf501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:40 +0000

    Add package injections/0.2.2/injections-0.2.2.tar.gz

---
+++ packages/injections/injections-0.2.2.tar.gz/injections-0.2.2/injections/__init__.py
@@ -0,0 +1,21 @@
+from .core import (
+    Container,
+    Dependency,
+    MissingDependencyError,
+    has,
+    depends,
+    propagate,
+    get_container,
+    dependencies,
+    )
+
+__all__ = [
+    'Container',
+    'Dependency',
+    'MissingDependencyError',
+    'has',
+    'depends',
+    'propagate',
+    'get_container',
+    'dependencies',
+    ]
