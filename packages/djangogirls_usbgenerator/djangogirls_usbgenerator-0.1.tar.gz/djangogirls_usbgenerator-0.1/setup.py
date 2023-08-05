da94cccb824513a728a5b7ca8bdc0077bc608001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:16 +0000

    Add package djangogirls_usbgenerator/0.1/djangogirls_usbgenerator-0.1.tar.gz

---
+++ packages/djangogirls_usbgenerator/djangogirls_usbgenerator-0.1.tar.gz/djangogirls_usbgenerator-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'djangogirls_usbgenerator',
+  packages = ['djangogirls_usbgenerator'], # this must be the same as the name above
+  version = '0.1',
+  description = 'USB generator is a script to download everything you need for Django Girls workshop in case there is no Internet.',
+  author = 'Lucie Daeye',
+  author_email = 'lucie.daeye@gmail.com',
+  url = 'https://github.com/patjouk/djangogirls_usbgenerator', # use the URL to the github repo
+  download_url = 'https://github.com/patjouk/djangogirls_usbgenerator/tarball/0.1', # I'll explain this in a second
+  keywords = ['Django Girls'], # arbitrary keywords
+  classifiers = [],
+)
