4a307fbf1629d162dc1def39180c39d77cc7449a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:17 +0000

    Add package parallelprogress/0.1.2/parallelprogress-0.1.2.tar.gz

---
+++ packages/parallelprogress/parallelprogress-0.1.2.tar.gz/parallelprogress-0.1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+import os
+
+
+def read(fname):
+    try:
+        content = open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except IOError:
+        content = ''
+    return content
+
+setup(name='parallelprogress',
+      version='0.1.2',
+      description='Share a progress bar between parallel processes',
+      long_description=read('README.md'),
+      author="Aaron O'Leary",
+      author_email='dev@aaren.me',
+      url='http://github.com/aaren/parallelprogress',
+      py_modules=['parallelprogress'],
+      )
