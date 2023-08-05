8282ef2230516e0cf74fbe4efbe7e848b467bfb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:13 +0000

    Add package django-social-publisher/0.2.5/django-social-publisher-0.2.5.tar.gz

---
+++ packages/django-social-publisher/django-social-publisher-0.2.5.tar.gz/django-social-publisher-0.2.5/social_publisher/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from social_publisher import conf
+
+if any('facebook' in x for x in conf.PUBLISHER_BACKENDS):
+    import facebook  #NOQA
+
+if any('twitter' in x for x in conf.PUBLISHER_BACKENDS):
+    import twython  #NOQA
+
+if any('.vk.' in x for x in conf.PUBLISHER_BACKENDS):
+    import vk  #NOQA
+
+if any('.tumblr.' in x for x in conf.PUBLISHER_BACKENDS):
+    import pytumblr  #NOQA
