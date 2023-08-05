1927a9dbe1bb845c4f0899373d5d9bcfc6caea6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:14 +0000

    Add package tchannel/0.11.2/tchannel-0.11.2.tar.gz

---
+++ packages/tchannel/tchannel-0.11.2.tar.gz/tchannel-0.11.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='tchannel',
+    version='0.11.2',
+    author='Abhinav Gupta, Aiden Scandella, Bryce Lampe, Grayson Koonce, Junchao Wu',
+    author_email='dev@uber.com',
+    description='Network multiplexing and framing protocol for RPC',
+    license='MIT',
+    url='https://github.com/uber/tchannel',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    install_requires=[
+        'contextlib2',
+        'crcmod',
+        'tornado>=4.0,<5.0',
+        'toro>=0.8,<0.9',
+        'threadloop>=0.3,<0.4',
+        'futures',
+    ],
+    entry_points={
+        'console_scripts': [
+            'tcurl.py = tchannel.tcurl:start_ioloop'
+        ]
+    },
+)
