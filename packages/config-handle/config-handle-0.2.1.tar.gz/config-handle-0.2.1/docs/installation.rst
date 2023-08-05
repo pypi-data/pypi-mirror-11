7ac22096d8c0ee206e9f50e348cf2657702f7f3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:54 +0000

    Add package config-handle/0.2.1/config-handle-0.2.1.tar.gz

---
+++ packages/config-handle/config-handle-0.2.1.tar.gz/config-handle-0.2.1/docs/installation.rst
@@ -0,0 +1,12 @@
+============
+Installation
+============
+
+At the command line::
+
+    $ easy_install config-handle
+
+Or, if you have virtualenvwrapper installed::
+
+    $ mkvirtualenv config-handle
+    $ pip install config-handle
---
+++ packages/config-handle/config-handle-0.2.1.tar.gz/config-handle-0.2.1/tests/test_config-handle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_config-handle
+----------------------------------
+
+Tests for `config-handle` module.
+"""
+
+import unittest
+
+from config-handle import config-handle
+
+
+class TestConfig-handle(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
