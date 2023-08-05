a0a43d1c016323bf987ba2de27d46c9fb7501a17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:36 +0000

    Add package yowsup2/2.3.167/yowsup2-2.3.167.tar.gz

---
+++ packages/yowsup2/yowsup2-2.3.167.tar.gz/yowsup2-2.3.167/yowsup/__init__.py
@@ -0,0 +1,2 @@
+__version__ = "2.3.167"
+__author__ = "Tarek Galal"
---
+++ packages/yowsup2/yowsup2-2.3.167.tar.gz/yowsup2-2.3.167/yowsup/common/constants.py
@@ -0,0 +1,10 @@
+class YowConstants:
+    DOMAIN       = "s.whatsapp.net"
+    ENDPOINTS     = (
+        ("e16.whatsapp.net", 443),
+        )
+
+    PATH_STORAGE = "~/.yowsup"
+
+    PREVIEW_WIDTH = 64
+    PREVIEW_HEIGHT = 64
