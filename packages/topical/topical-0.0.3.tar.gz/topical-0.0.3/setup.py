73eb33ae62182260bc7cbaa6729c8ead45ec22b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:52 +0000

    Add package topical/0.0.3/topical-0.0.3.tar.gz

---
+++ packages/topical/topical-0.0.3.tar.gz/topical-0.0.3/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="topical",
+    version="0.0.3",
+    author="Tom Leach",
+    author_email="tom@gc.com",
+    description="Topic-oriented API for producing and consuming Kafka streams with support for pluggable codecs",
+    license="MIT",
+    keywords="kafka topic encoding decoding streams python",
+    url="http://github.com/gamechanger/topical",
+    packages=["topical"],
+    install_requires=['avro_codec==1.0.0'],
+    tests_require=['nose', 'mock']
+    )
