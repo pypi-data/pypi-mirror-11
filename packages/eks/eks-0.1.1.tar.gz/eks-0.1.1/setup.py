6a7c204550ecd9507d1dce23db138a249a4f0126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:07 +0000

    Add package eks/0.1.1/eks-0.1.1.tar.gz

---
+++ packages/eks/eks-0.1.1.tar.gz/eks-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='eks',
+    version='0.1.1',
+    packages=find_packages(".", exclude=["*.tests", "*.tests.*", "tests.*", "tests", "demo.*"]),
+    url='https://github.com/olisim/eks',
+    license='MIT',
+    author='Oliver Simon',
+    author_email='simon@dajool.com',
+    maintainer='Jochen Breuer',
+    maintainer_email='breuer@dajool.com',
+    install_requires=[],
+    description='A Python library to communicate with an Euchner Electronic-Key-System (EKS).',
+    keywords='eks Euchner Electronic Key System keysystem',
+    platforms='any',
+)
