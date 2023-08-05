3d54916241a7363ab5ab384043d0077fa3f7e959
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:25 +0000

    Add package myZip/1.0.1/myZip-1.0.1.zip

---
+++ packages/myZip/myZip-1.0.1.zip/myZip-1.0.1/myZip.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*
+# Filename: myZip.py
+
+__author__ = 'Piratf'
+
+import os, sys
+import zipfile
+
+def zipFiles2TargetPath(sourcePath, targetPath, exclude = []):
+    ''' zip files in sourcePath, recursively
+
+    the targetPath means the path of the target zip file, including the name of the zip file.
+    if you want to exclude some folders in the sourcePath, list them in the third parameter.
+    using zipfile in the standard library'''
+    print ("start zip")
+    filesCount = 0;
+    f = zipfile.ZipFile(targetPath, 'w', zipfile.ZIP_DEFLATED)
+    for dirpath, dirnames, filenames in os.walk( sourcePath ):
+        for excludePath in exclude:
+            if excludePath in dirnames:
+                dirnames.remove(excludePath)
+        for filename in filenames:
+            filesCount += 1;
+            # at here we will report the process
+            print ('dealing %s\r' % filesCount, end = '')
+            sys.stdout.flush()
+            f.write(os.path.join(dirpath, filename))
+    f.close()
+    print ("zip completed")
+    return filesCount
---
+++ packages/myZip/myZip-1.0.1.zip/myZip-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'myZip',
+    version = '1.0.1',
+    py_modules = ['myZip'],
+    author = 'piratf',
+    author_email = 'piratf@aliyun.com',
+    url = 'piratf.ml',
+    description = 'A repackage zip module, base on zipFile \
+    in standard library',
+)
