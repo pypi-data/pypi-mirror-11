8d89bd688a4d7c522750e1773b66c56753599771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:55 +0000

    Add package kmsclient/0.3/kmsclient-0.3.tar.gz

---
+++ packages/kmsclient/kmsclient-0.3.tar.gz/kmsclient-0.3/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='kmsclient',
+    packages=['kmsclient'],
+    version='0.3',
+    description='For encrypting and decrypting using a specific key from amazons kms',
+    author='Mark Kelly',
+    author_email='mkelly28@tcd.ie',
+    url='https://github.com/zalando/kmsclient',
+    download_url='https://github.com/zalando/kmsclient/tarball/0.3',
+    keywords=['kms', 'aws', 'key', 'management', 'system', 'client', 'encryption'],
+    classifiers=[],
+    entry_points={'console_scripts': 'kmsclient = kmsclient.cli:main'},
+    install_requires=['clickclick', 'pyperclip', 'boto3'],
+    license='Apache License 2.0'
+)
