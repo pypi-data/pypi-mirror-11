c352ebb218c26be2dfcd91e056d64ce59aad7a85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:53 +0000

    Add package flipper/0.9.0/flipper-0.9.0.tar.gz

---
+++ packages/flipper/flipper-0.9.0.tar.gz/flipper-0.9.0/source/example/distribution.py
@@ -0,0 +1,19 @@
+
+from __future__ import print_function
+
+import flipper
+
+def main(surface, lower, upper, step=1, sample=100):
+	S = flipper.load(surface)
+	for length in range(lower, upper, step):
+		count = 0
+		for i in range(sample):
+			mapping_class = S.mapping_class(length)
+			if mapping_class.is_pseudo_anosov(): count += 1
+			print('\rLength: %d, Computed %d/%d - %0.1f%% pA' % (length, (i+1), sample, float(count) * 100 / (i+1)), end='')
+		print('')
+
+if __name__ == '__main__':
+	#main('S_1_2', 27, 30, sample=500)
+	main('S_2_1', 10, 20)
+
