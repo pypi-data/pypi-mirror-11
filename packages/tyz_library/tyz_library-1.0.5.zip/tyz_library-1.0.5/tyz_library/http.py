2b08d75bed74fe84a53449280563363ac0c6d02a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:27 +0000

    Add package tyz_library/1.0.5/tyz_library-1.0.5.zip

---
+++ packages/tyz_library/tyz_library-1.0.5.zip/tyz_library-1.0.5/tyz_library/http.py
@@ -0,0 +1,30 @@
+#!C:\Python27\\python.exe
+#coding=utf-8
+'''
+http相关请求函数
+'''
+import urllib2
+import socket
+import chardet
+from tyz_library.myException import MyException 
+socket.setdefaulttimeout(10)
+def getRemote(url):
+	'''获取远程html内容'''
+	try:
+		req = urllib2.urlopen(url)
+	except urllib2.URLError:
+		return ''
+	html = req.read()
+	return html
+
+def convertUnicode(html):
+	'''将未知编码的html内容转换成unicde编码'''
+	encodeDict = chardet.detect(html)
+	if encodeDict['encoding'] == 'gbk' or encodeDict['encoding'] == 'GBK':
+		return html.decode('gbk', 'ignore')
+	elif encodeDict['encoding'] == 'gb2312' or encodeDict['encoding'] == 'GB2312':
+		return html.decode('gb2312', 'ignore')
+	else:
+		raise MyException, 'uncatch coding!'
+		
+	
