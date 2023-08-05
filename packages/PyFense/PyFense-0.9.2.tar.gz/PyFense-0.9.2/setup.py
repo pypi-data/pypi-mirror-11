a6d0d88f7496f3f81e2995b966267a2bab641561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:39 +0000

    Add package PyFense/0.9.2/PyFense-0.9.2.tar.gz

---
+++ packages/PyFense/PyFense-0.9.2.tar.gz/PyFense-0.9.2/setup.py
@@ -0,0 +1,31 @@
+#from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name = 'PyFense',
+    version = '0.9.2',
+    license = 'GPLv3',
+    packages = ['pyfense'],
+    package_data = {
+        'pyfense': [
+            'assets/*.*',
+            'data/*.*']
+    },
+
+    install_requires = ['pyglet>=1.2.3', 'cocos2d>=0.6.0', 'numpy>=1.9.2'],
+
+    maintainer = 'n',
+    maintainer_email = 'l.nimar.b@gmail.com',
+
+    description = 'PyFense is a tower defense game built in Python \
+        using the Cocos2d framework.',
+
+    entry_points = {
+        'console_scripts': ['pyfense = pyfense.pyfense:main']
+    },
+    #scripts = ['pyfense/pyfense'],
+    keywords = "pyfense tower defense td",
+    classifiers = [
+        'Programming Language :: Python',
+        'Topic :: Games/Entertainment']
+)
