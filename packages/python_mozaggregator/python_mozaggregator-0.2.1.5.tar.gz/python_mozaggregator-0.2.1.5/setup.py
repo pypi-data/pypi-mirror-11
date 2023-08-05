6566b069c4dfbbd33f65d366c2dc1da73e238ff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:23 +0000

    Add package python_mozaggregator/0.2.1.5/python_mozaggregator-0.2.1.5.tar.gz

---
+++ packages/python_mozaggregator/python_mozaggregator-0.2.1.5.tar.gz/python_mozaggregator-0.2.1.5/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from distutils.core import setup
+
+import urllib
+import setuptools.command.install
+
+
+setup(name='python_mozaggregator',
+      version='0.2.1.5',
+      author='Roberto Agostino Vitillo',
+      author_email='rvitillo@mozilla.com',
+      description='Telemetry aggregation job',
+      url='https://github.com/vitillo/python_mozaggregator',
+      packages=['mozaggregator'],
+      package_dir={'mozaggregator': 'mozaggregator'},
+      install_requires=['python_moztelemetry', 'Flask', 'flask-cors', 'joblib', 'boto', 'ujson', 'psycopg2', 'pandas>=0.15.2'])
+
