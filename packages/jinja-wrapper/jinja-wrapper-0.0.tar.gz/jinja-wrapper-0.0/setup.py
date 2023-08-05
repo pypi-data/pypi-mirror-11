b5792c2b87efc04f41f20e89562c17cdaa35be31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:07 +0000

    Add package jinja-wrapper/0.0/jinja-wrapper-0.0.tar.gz

---
+++ packages/jinja-wrapper/jinja-wrapper-0.0.tar.gz/jinja-wrapper-0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='jinja-wrapper',
+      version='0.0',
+      description='Interface for jinja',
+      url='',
+      author='Kevin S Lin',
+      author_email='kevinslin8@gmail.com',
+      license='MIT',
+      packages=['jinja-wrapper'],
+      zip_safe=False)
