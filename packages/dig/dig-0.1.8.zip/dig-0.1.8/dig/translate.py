c4173f874897b92c7dd6c969b4c3f85c7c705e02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:33 +0000

    Add package dig/0.1.8/dig-0.1.8.zip

---
+++ packages/dig/dig-0.1.8.zip/dig-0.1.8/dig/translate.py
@@ -0,0 +1,48 @@
+from __future__ import unicode_literals
+from __future__ import print_function
+
+from translate_api import TranslateService,TTSService
+from dig.share import ensure_decode
+
+
+class Translator:
+    """
+    Description:
+        Translator Class, like top package
+    """
+    @ensure_decode
+    def __init__(self, from_lang, to_lang, data, debug=False):        
+        self._debug = debug
+        self._from_lang = from_lang
+        self._to_lang = to_lang
+        self._data = data
+
+    def translate(self):
+        
+        translator = TranslateService()
+        result = translator.trans_details(
+            self._from_lang,
+            self._to_lang,
+            self._data,
+        )
+        return result
+
+    @staticmethod
+    def display_result(from_lang,to_lang,result):
+        
+        display_option = 'source lang: {0} ----> target lang: {1}'.format(from_lang,to_lang)
+        print (display_option)
+        print (result)
+
+
+class Speaker:
+
+    @ensure_decode
+    def __init__(self, from_lang, text):
+        self._from_lang = from_lang
+        self._text = text
+
+    def speak(self):
+    
+        tts = TTSService()
+        tts.speak_details(self._from_lang,self._text)
