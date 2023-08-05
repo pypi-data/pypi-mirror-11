25a1ea094ec1077d0b78b0701d4ce92af8477da3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:02 +0000

    Add package printLoop/1.4.0/printLoop-1.4.0.zip

---
+++ packages/printLoop/printLoop-1.4.0.zip/printLoop-1.4.0/printLoop.py
@@ -0,0 +1,15 @@
+"""This is my first pythonCode function.
+This function will print all items in all the lists."""
+
+import sys
+
+def print_lol (the_list, indent = False, level = 0, fh = sys.stdout):
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, indent, level+1, fh)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end = '', file = fh)
+            print(each_item, file = fh)
+
---
+++ packages/printLoop/printLoop-1.4.0.zip/printLoop-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'printLoop',
+    version = '1.4.0',
+    py_modules = ['printLoop'],
+    author = 'jonnylee719',
+    author_email = 'jonnylee719@gmail.com',
+    url = 'http://www.google.com',
+    description = 'Additional function of modifying output destination other than sys.stdout',
+    )
