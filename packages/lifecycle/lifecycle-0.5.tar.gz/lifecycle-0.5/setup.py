0d3d09bb75d78ace160f7b50d0cda5e32e32b761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:56 +0000

    Add package lifecycle/0.5/lifecycle-0.5.tar.gz

---
+++ packages/lifecycle/lifecycle-0.5.tar.gz/lifecycle-0.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='lifecycle',
+      version='0.5',
+      description='Easily identify and track users with minimal code.',
+      url='https://github.com/TeamLifecycle/lifecycle-python',
+      author='Jake Mooney',
+      author_email='jrmooney@gmail.com',
+      license='MIT',
+      packages=['lifecycle'],
+      install_requires=[
+          'unirest',
+      ],
+      zip_safe=False)
