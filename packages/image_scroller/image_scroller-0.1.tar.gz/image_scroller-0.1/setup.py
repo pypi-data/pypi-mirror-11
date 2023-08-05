1522f48e6b79f078e1a4103744eccb2cac7abc05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:53 +0000

    Add package image_scroller/0.1/image_scroller-0.1.tar.gz

---
+++ packages/image_scroller/image_scroller-0.1.tar.gz/image_scroller-0.1/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+
+setup(name='image_scroller',
+      version=0.1,
+      packages=['image_viewer'],
+      include_package_data=True,
+      description="A simple image viewer with vertical scroller.",
+      author="Mayur Swami"
+)
