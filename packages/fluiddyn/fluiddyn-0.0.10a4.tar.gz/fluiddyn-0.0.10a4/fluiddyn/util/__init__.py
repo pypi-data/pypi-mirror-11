a46503542e6e3e98fd93caa2adcd4fb5f4da1499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:46 +0000

    Add package fluiddyn/0.0.10a4/fluiddyn-0.0.10a4.tar.gz

---
+++ packages/fluiddyn/fluiddyn-0.0.10a4.tar.gz/fluiddyn-0.0.10a4/fluiddyn/util/__init__.py
@@ -0,0 +1,22 @@
+"""
+Utilities
+=========
+
+.. currentmodule:: fluiddyn.util
+
+.. autosummary::
+   :toctree:
+
+   util
+   paramcontainer
+   constants
+   query
+   timer
+   deamons
+   signal
+   logger
+
+"""
+
+from fluiddyn.util.util import (
+    Params, time_as_str)
