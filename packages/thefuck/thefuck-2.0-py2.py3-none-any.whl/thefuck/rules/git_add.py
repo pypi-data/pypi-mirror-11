b99e5ec3cddba1b4ff21cb09ed7432aefd84fbae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:25 +0000

    Add package thefuck/2.0/thefuck-2.0-py2.py3-none-any.whl

---
+++ packages/thefuck/thefuck-2.0-py2.py3-none-any.whl/thefuck/rules/git_add.py
@@ -0,0 +1,19 @@
+import re
+from thefuck import utils, shells
+
+
+@utils.git_support
+def match(command, settings):
+    return ('git' in command.script
+            and 'did not match any file(s) known to git.' in command.stderr
+            and "Did you forget to 'git add'?" in command.stderr)
+
+
+@utils.git_support
+def get_new_command(command, settings):
+    missing_file = re.findall(
+            r"error: pathspec '([^']*)' "
+            "did not match any file\(s\) known to git.", command.stderr)[0]
+
+    formatme = shells.and_('git add -- {}', '{}')
+    return formatme.format(missing_file, command.script)
