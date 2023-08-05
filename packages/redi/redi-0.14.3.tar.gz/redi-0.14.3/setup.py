0b603b1a28def2a2382dad98c43d2b1c67f4938a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:15 +0000

    Add package redi/0.14.3/redi-0.14.3.tar.gz

---
+++ packages/redi/redi-0.14.3.tar.gz/redi-0.14.3/setup.py
@@ -0,0 +1,58 @@
+"""
+RED-I Setup
+
+Please read the documentation in README.md!
+
+Quick install:
+
+    $ make egg
+    $ sudo easy_install dist/REDI*.egg
+
+Prerequisites on Debian Wheezy:
+
+  apt-get install python-setuptools python-dev libxml2 libxslt1-dev
+
+"""
+
+from setuptools import setup, find_packages
+# from distutils.core import setup
+
+setup(
+    name='redi',
+    version='0.14.3',
+    author='https://www.ctsi.ufl.edu/research/study-development/informatics-consulting/',
+    author_email='ctsit@ctsi.ufl.edu',
+    packages=find_packages(exclude=['test']),
+    include_package_data=True,
+    package_data={
+        'redi': ['utils/*.xsl', 'utils/*.xsd']
+    },
+    url='https://github.com/ctsit/redi',
+    download_url = 'https://github.com/ctsit/redi/releases/tag/0.14.1',
+    keywords = ['EMR', 'EHR', 'REDCap', 'Clinical Data'],
+    license='BSD 3-Clause',
+    description='REDCap Electronic Data Importer',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "requests >= 2.5.1",
+        "lxml >= 3.3.5",
+        "PyCap >= 1.0",
+        "pysftp >= 0.2.8",
+        "docopt >= 0.6.2",
+        "pycrypto >= 2.6.1",
+    ],
+    entry_points={
+        'console_scripts': [
+            'redi = redi.redi:main',
+        ],
+    },
+    test_suite='test.TestSuite',
+    tests_require=[
+        "mock >= 1.0.1",
+        "sftpserver >= 0.2",
+    ],
+    setup_requires=[
+        "nose >= 1.0",
+        "nosexcover >= 1.0.10",
+    ],
+)
