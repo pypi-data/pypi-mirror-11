9626041ea581d4c596ffcaa40a46c4bc422e8730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:12 +0000

    Add package mview/0.1.0/mview-0.1.0.zip

---
+++ packages/mview/mview-0.1.0.zip/mview-0.1.0/mview/__init__.py
@@ -0,0 +1,47 @@
+__author__ = 'Zhichao HAN'
+
+
+import os
+import collections
+import ConfigParser
+
+
+from tool import show_by_tool as show
+
+
+config = collections.defaultdict(str)
+
+_path_config = os.path.expanduser('~/.mview.cfg')
+
+
+def init_pacakge(path_config=_path_config):
+    cfg = ConfigParser.RawConfigParser()
+    cfg.read(path_config)
+    for k, v in cfg.items('MView'):
+        config[k] = v
+
+
+def update_executor(path, path_config=_path_config):
+    cfg = ConfigParser.RawConfigParser()
+    cfg.read(path_config)
+    cfg.set('MView', 'executor', path)
+    with open(path_config, 'w+', encoding='utf-8') as fp:
+        cfg.write(fp)
+    _auto_init_package()
+
+
+def dump_package(config=collections.defaultdict(str), path=_path_config):
+    raw = ConfigParser.RawConfigParser()
+    raw.add_section('MView')
+    for k, v in config.items():
+        raw.set('MView', k, v)
+    with open(_path_config, 'w+', encoding='utf-8') as fp:
+        raw.write(fp)
+
+
+def _auto_init_package():
+    if not os.path.exists(_path_config):
+        dump_package()
+    init_pacakge(_path_config)
+
+_auto_init_package()
---
+++ packages/mview/mview-0.1.0.zip/mview-0.1.0/setup.py
@@ -0,0 +1,29 @@
+__author__ = 'Zhichao HAN'
+
+import os
+from setuptools import setup, find_packages
+
+
+version = '0.1.0'
+
+README = os.path.join(os.path.dirname(__file__), 'README.md')
+long_description = open(README).read()
+
+
+setup(name='mview',
+      version=version,
+      description=('Package to show graphic view of pandas.DataFrame.'),
+      long_description=long_description,
+      classifiers=[
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Visualization',
+        ],
+      keywords='matrix view data frame',
+      author='Zhichao HAN',
+      author_email='hanzhichao2000@foxmail.com',
+      license='LGPL',
+      packages=['mview'],
+      package_dir={'mview': 'mview'},
+      url='https://github.com/hanzhichao2000/mview',
+      install_requires=['pandas']
+      )
