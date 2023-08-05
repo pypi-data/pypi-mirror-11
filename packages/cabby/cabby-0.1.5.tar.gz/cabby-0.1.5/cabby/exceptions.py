ef2e4c8a428f5c0bc98e14faaa1a7d7d393b6918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:26 +0000

    Add package cabby/0.1.5/cabby-0.1.5.tar.gz

---
+++ packages/cabby/cabby-0.1.5.tar.gz/cabby-0.1.5/cabby/exceptions.py
@@ -0,0 +1,66 @@
+
+
+class ClientException(Exception):
+    pass
+
+
+class HTTPError(ClientException):
+    pass
+
+
+class InvalidResponseError(ClientException):
+    pass
+
+
+class UnsuccessfulStatusError(ClientException):
+
+    def __init__(self, taxii_status, *args, **kwargs):
+        super(UnsuccessfulStatusError, self).__init__(
+            _status_to_message(taxii_status), *args, **kwargs)
+
+        self.status = taxii_status.status_type
+        self.text = taxii_status.to_text()
+
+        self.raw = taxii_status
+
+
+class AmbiguousServicesError(ClientException):
+    pass
+
+
+class ServiceNotFoundError(ClientException):
+    pass
+
+
+class NoURIProvidedError(ValueError):
+    pass
+
+
+class NotSupportedError(ClientException):
+
+    def __init__(self, version, *args, **kwargs):
+        super(NotSupportedError, self).__init__(
+            "Not supported in version {}".format(version),
+            *args, **kwargs)
+
+
+def _status_to_message(status):
+    l = [status.status_type]
+
+    if status.status_detail:
+        l.append(_dict_to_pairs(status.status_detail))
+
+    if status.extended_headers:
+        l.append(_dict_to_pairs(status.extended_headers))
+
+    if status.message:
+        l.append(status.message)
+
+    return "; ".join(l)
+
+
+def _dict_to_pairs(d):
+    pairs = []
+    for k, v in list(d.items()):
+        pairs.append('%s=%s' % (k, v))
+    return ", ".join(pairs)
