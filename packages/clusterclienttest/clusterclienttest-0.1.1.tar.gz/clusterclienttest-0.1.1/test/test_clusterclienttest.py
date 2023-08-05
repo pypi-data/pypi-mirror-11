c3652ed0f3f678edc8aead25aa2ce1b39a34bb0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:34 +0000

    Add package clusterclienttest/0.1.1/clusterclienttest-0.1.1.tar.gz

---
+++ packages/clusterclienttest/clusterclienttest-0.1.1.tar.gz/clusterclienttest-0.1.1/test/test_clusterclienttest.py
@@ -0,0 +1,20 @@
+"""
+Tests for `clusterclienttest` module.
+"""
+#import pytest
+
+#from clusterclienttest import clusterclienttest
+
+
+class TestClusterclienttest(object):
+
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    def test_something(self):
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        pass
