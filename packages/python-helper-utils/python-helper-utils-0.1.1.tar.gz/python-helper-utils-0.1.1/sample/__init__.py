992b9b3f4650f1113b10d24fccf6eb346b85350f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:34 +0000

    Add package python-helper-utils/0.1.1/python-helper-utils-0.1.1.tar.gz

---
+++ packages/python-helper-utils/python-helper-utils-0.1.1.tar.gz/python-helper-utils-0.1.1/sample/__init__.py
@@ -0,0 +1,12 @@
+
+
+class UtilOne(object):
+
+    def get_one(self):
+
+        return 'util_one'
+
+
+def main():
+    """Entry point for the application script"""
+    print("Call your main application code here")
