408c4cc7cabcd4646f1aed9f80bc43f1cea1419a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:52 +0000

    Add package basicevents/0.1.2/basicevents-0.1.2.tar.gz

---
+++ packages/basicevents/basicevents-0.1.2.tar.gz/basicevents-0.1.2/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+readme = open('README.md').read()
+
+setup(
+    name='basicevents',
+    version='0.1.2',
+    description='python events non-blocking',
+    long_description=readme,
+    author='Alberto Galera Jimenez',
+    author_email='galerajimenez@gmail.com',
+    url='https://github.com/kianxineki/basicevents',
+    py_modules=['basicevents'],
+    include_package_data=True,
+    install_requires=[
+    ],
+    license="GPL",
+    zip_safe=False,
+    keywords='basicevents',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4'
+    ],
+    entry_points={}
+)
