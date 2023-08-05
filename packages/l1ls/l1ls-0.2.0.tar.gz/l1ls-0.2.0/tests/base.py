f635bf79a71b12b4bfa30d1ebf7daea8c1215322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:16 +0000

    Add package l1ls/0.2.0/l1ls-0.2.0.tar.gz

---
+++ packages/l1ls/l1ls-0.2.0.tar.gz/l1ls-0.2.0/tests/base.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of l1ls.
+# https://github.com/musically-ut/l1-ls.py
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/MIT-license
+# Copyright (c) 2015, Utkarsh Upadhyay <musically.ut@gmail.com>
+from unittest import TestCase as PythonTestCase
+
+
+class TestCase(PythonTestCase):
+    pass
