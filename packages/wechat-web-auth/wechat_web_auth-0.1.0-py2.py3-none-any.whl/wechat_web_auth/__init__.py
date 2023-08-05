66cbac64d136739787ea63573bcac21e750b1f14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:57 +0000

    Add package wechat-web-auth/0.1.0/wechat_web_auth-0.1.0-py2.py3-none-any.whl

---
+++ packages/wechat-web-auth/wechat_web_auth-0.1.0-py2.py3-none-any.whl/wechat_web_auth/__init__.py
@@ -0,0 +1,11 @@
+#-*- coding:utf-8 -*-
+# created:     Tue Jul 28 21:03:58 2015
+# filename:    __init__.py
+# author:      juntao liu
+# email:       jinuljt@gmail.com
+# descritpion: 
+
+
+__all__ = ['Wechat']
+
+from .wechat import Wechat
