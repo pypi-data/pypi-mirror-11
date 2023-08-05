3af44f91acd538b00ad3b28dc758d15d17a4ac98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:18 +0000

    Add package django-q/0.4.0/django-q-0.4.0.tar.gz

---
+++ packages/django-q/django-q-0.4.0.tar.gz/django-q-0.4.0/django_q/__init__.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath)
+
+from .tasks import async, schedule, result, result_group, fetch, fetch_group, count_group, delete_group
+from .models import Task, Schedule, Success, Failure
+from .cluster import Cluster
+
+VERSION = (0, 4, 0)
+
+default_app_config = 'django_q.apps.DjangoQConfig'
