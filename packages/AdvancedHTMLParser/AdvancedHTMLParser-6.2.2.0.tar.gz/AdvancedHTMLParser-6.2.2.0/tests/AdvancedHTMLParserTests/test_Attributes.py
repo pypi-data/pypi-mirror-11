ce1640c3e05bf999c7c8a946a20e3b26c12476a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:53 +0000

    Add package AdvancedHTMLParser/6.2.2.0/AdvancedHTMLParser-6.2.2.0.tar.gz

---
+++ packages/AdvancedHTMLParser/AdvancedHTMLParser-6.2.2.0.tar.gz/AdvancedHTMLParser-6.2.2.0/tests/AdvancedHTMLParserTests/test_Attributes.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env GoodTests.py
+
+import sys
+import subprocess
+
+from AdvancedHTMLParser.Tags import AdvancedTag
+
+class TestAttributes(object):
+    '''
+        Tests some attribute behaviour
+    '''
+
+    def test_setAttribute(self):
+        tag = AdvancedTag('div')
+
+        tag.setAttribute('id', 'abc')
+
+        assert tag.getAttribute('id') == 'abc' , 'Expected id to be abc'
+
+        assert tag.getAttribute('blah') == None , 'Expected unset attribute to return None, actually returned %s' %(tag.getAttribute('blah'),)
+
+    def test_setAttributes(self):
+        tag = AdvancedTag('div')
+        tag.setAttributes( {
+            'id' : 'abc',
+            'name'  :  'cheese',
+            'x-attr' : 'bazing'
+        })
+
+        assert tag.getAttribute('id') == 'abc'
+        assert tag.getAttribute('name') == 'cheese'
+        assert tag.getAttribute('x-attr') == 'bazing'
+
+    def test_specialAttributes(self):
+        tag = AdvancedTag('div')
+        tag.setAttribute('style', 'position: absolute')
+        styleValue = str(tag.getAttribute('style'))
+        styleValue = styleValue.strip()
+        assert styleValue == 'position: absolute' , 'Expected position: absolute, got %s' %(str(tag.getAttribute('style')),)
+
+        tag.setAttribute('className', 'one two')
+        assert str(tag.className).strip() == 'one two' , 'Expected classname to be "one two", got %s' %(repr(str(tag.className).strip()),)
+
+
+if __name__ == '__main__':
+    pipe  = subprocess.Popen('GoodTests.py "%s"' %(sys.argv[0],), shell=True).wait()
