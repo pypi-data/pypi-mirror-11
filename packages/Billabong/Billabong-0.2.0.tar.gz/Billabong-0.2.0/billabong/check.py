44aee57388bf14f081ee765e4f409da7f3db807a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:45 +0000

    Add package Billabong/0.2.0/Billabong-0.2.0.tar.gz

---
+++ packages/Billabong/Billabong-0.2.0.tar.gz/Billabong-0.2.0/billabong/check.py
@@ -0,0 +1,85 @@
+# Copyright (c) 2015 "Hugo Herter http://hugoherter.com"
+#
+# This file is part of Billabong.
+#
+# Intercom is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+"""
+Check the integrity of the data.
+"""
+
+import logging
+from base64 import b64decode
+
+from .encryption import hashing, decrypt_blob
+from .utils import read_in_chunks
+from .exceptions import CheckError
+from .settings import inventory, storage
+
+
+def compute_hash(file_object, chunk_size=1024):
+    """Compute the hash of the content of a file object using
+    the given hashing function.
+    """
+    file_hash = hashing()
+    for chunk in read_in_chunks(file_object, chunk_size):
+        file_hash.update(chunk)
+    return file_hash
+
+
+def check_data(id=None, meta=None, raises=False):
+
+    if id and not meta:
+        meta = inventory.get_record(id)
+    elif meta and not id:
+        id = meta['id']
+    else:
+        raise ValueError("Missing value for 'id' or 'meta'.")
+
+    blob_id = meta['blob']
+    check_enc_data(blob_id)
+
+    key = b64decode(meta['key'])
+    hash = meta['hash']
+    check_clear_data(blob_id, key, hash)
+
+
+def check_enc_data(blob_id, raises=False):
+    "Check the validity of an encrypted blob"
+
+    enc_path = storage._blob_path(blob_id)
+    with open(enc_path, 'rb') as enc_file:
+        enc_hash = compute_hash(enc_file)
+
+    if id != enc_hash.hexdigest():
+        if raises:
+            raise CheckError(
+                "Data does not match the hash for id '{}'".format(id))
+        else:
+            logging.error(
+                "Data does not match the hash for id '{}'".format(id))
+
+
+def check_clear_data(id, key, hash):
+    "Check the validity of the clear data inside a blob"
+
+    clear_data = decrypt_blob(storage, id, key)
+
+    clear_hash = hashing()
+    for chunk in clear_data:
+        clear_hash.update(chunk)
+
+    if hash != "sha256-" + clear_hash.hexdigest():
+        raise CheckError()
---
+++ packages/Billabong/Billabong-0.2.0.tar.gz/Billabong-0.2.0/billabong/encryption.py
@@ -0,0 +1,104 @@
+# Copyright (c) 2015 "Hugo Herter http://hugoherter.com"
+#
+# This file is part of Billabong.
+#
+# Intercom is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+import os
+import uuid
+import hashlib
+from Crypto import Random
+from Crypto.Cipher import AES
+from Crypto.Util import Counter
+
+from .settings import TMPSTORAGE_PATH
+from .utils import read_in_chunks
+
+hashing = hashlib.sha256
+
+
+def random_key():
+    "Return a randomly generated AES key"
+    random = Random.new()
+    key = random.read(AES.key_size[2])  # 256 bits
+    return key
+
+
+def copy_and_encrypt(storage, filepath, key):
+    "Encrypt the file into the data store, and returns its id."
+
+    tmp_name = 'billabong-{}.part'.format(uuid.uuid4())
+    tmp_destination = os.path.join(TMPSTORAGE_PATH, tmp_name)
+
+    source_hash = hashing()
+    enc_hash = hashing()
+    ctr = Counter.new(128)
+    crypto = AES.new(key, AES.MODE_CTR, counter=ctr)
+
+    with open(filepath, 'rb') as source_file, \
+            open(tmp_destination, 'wb') as dest_file:
+
+        for chunk in read_in_chunks(source_file):
+            source_hash.update(chunk)
+            enc_chunk = crypto.encrypt(chunk)
+            enc_hash.update(enc_chunk)
+            dest_file.write(enc_chunk)
+
+    # Now that we have the hash of the encrypted file, move the
+    # encrypted file to the storage and return the hash.
+    id_ = enc_hash.hexdigest()
+
+    with open(tmp_destination, 'rb') as tmp_file:
+        storage.import_blob(id_, tmp_file)
+    os.remove(tmp_destination)
+
+    return enc_hash.hexdigest()
+
+
+def counter_for_offset(offset):
+    "Return a Counter for the given offset."
+    initial_value = 1 + (offset // 16)
+    return Counter.new(128, initial_value=initial_value)
+
+
+def decrypt_blob(storage, blob_id, key, offset=0, length=None):
+    "Decrypt the content of a blob through a generator."
+
+    modulo = offset % 16
+    # Compute a file offset, might be lower than the real offset
+    file_offset = offset - modulo if offset else 0
+
+    end = offset + length if length else None
+    ctr = counter_for_offset(offset)
+
+    crypto = AES.new(key, AES.MODE_CTR, counter=ctr)
+
+    chunks_generator = storage.read_in_chunks(blob_id, offset=file_offset,
+                                              chunk_size=10)
+
+    for i, enc_chunk in chunks_generator:
+        chunk = crypto.decrypt(enc_chunk)
+
+        if end is not None:
+            here = offset - modulo + i*10
+            if end <= here + 10:
+                chunk = chunk[:end - here]
+                yield chunk
+                break
+
+        if i == 0:
+            yield chunk[modulo:]
+        else:
+            yield chunk
