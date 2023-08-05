f8ae3ac9af01e61708219982d1302ba471e3420f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:16 +0000

    Add package ocflib/2015.8.2.15.8/ocflib-2015.8.2.15.8.tar.gz

---
+++ packages/ocflib/ocflib-2015.8.2.15.8.tar.gz/ocflib-2015.8.2.15.8/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open('.version') as f:
+    VERSION = f.readline().strip()
+
+setup(
+    name='ocflib',
+    version=VERSION,
+    author='Open Computing Facility',
+    author_email='help@ocf.berkeley.edu',
+    description='libraries for account and server management',
+    url='https://www.ocf.berkeley.edu',
+    packages=find_packages(),
+    install_requires=(
+        'colorama',
+        'cracklib',
+        'dnspython3',
+        'ldap3',
+        'paramiko',
+        'pexpect',
+        'pycrypto',
+        'pysnmp',
+        'requests',
+    )
+)
