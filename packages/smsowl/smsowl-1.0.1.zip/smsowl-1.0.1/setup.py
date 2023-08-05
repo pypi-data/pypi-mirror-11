bb0b3c2aa2e7e9e0f8547c4afe564b8750b92993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:38 +0000

    Add package smsowl/1.0.1/smsowl-1.0.1.zip

---
+++ packages/smsowl/smsowl-1.0.1.zip/smsowl-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'smsowl',
+  packages = ['smsowl'], # this must be the same as the name above
+  install_requires = ['requests'],
+  version = '1.0.1',
+  description = 'This is Python wrapper for SmsOwl REST API',
+  author = 'M/s Mahoujas',
+  author_email = 'mahoujas@mahoujas.com',
+  url = 'https://smsowl.in', # use the URL to the github repo
+  download_url = 'https://github.com/mahoujas/smsowl-python/tarball/1.0.1', # I'll explain this in a second
+  keywords = ['SmsOwl', 'smsowl', 'transactional sms','promotional sms','sms india','sms gateway','dnd sms'], # arbitrary keywords
+  classifiers = [],
+)
