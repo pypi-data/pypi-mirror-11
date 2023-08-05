bc7da0e22b5672a5ff3a8484eb498d802e14f61d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:56 +0000

    Add package graphistry/0.0.14/graphistry-0.0.14.tar.gz

---
+++ packages/graphistry/graphistry-0.0.14.tar.gz/graphistry-0.0.14/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+from setuptools import setup, find_packages
+#from codecs import open
+#from os import path
+
+#here = path.abspath(path.dirname(__file__))
+
+#with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+ #   long_description = f.read()
+
+setup(
+    name='graphistry',
+
+    version='0.0.14',
+    py_modules = ['graphistry.etl'],
+    description = 'Visualize node-link graphs using Graphistry\'s cloud',
+    #long_description=long_description,
+
+    url='https://github.com/graphistry/pygraphistry',
+
+    author='Graphistry',
+    author_email='pygraphistry@graphistry.com',
+
+    install_requires=['requests'],
+
+    license='BSD',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+
+        'Topic :: Scientific/Engineering :: Visualization',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    keywords='graph plot visualization pandas',
+
+    packages=find_packages(exclude=['contrib', 'tests*'])
+)
