0681f2c7fe61ffbc5537933fa670c7a546985c5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:32 +0000

    Add package pytff/0.4.5/pytff-0.4.5.tar.gz

---
+++ packages/pytff/pytff-0.4.5.tar.gz/pytff-0.4.5/pytff/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# License: 3 Clause BSD
+# Part of Carpyncho - http://carpyncho.jbcabral.org
+
+
+# =============================================================================
+# DOC
+# =============================================================================
+
+"""Wrapper arround G. Kovacs & G. Kupi Template Fourier Fitting
+
+For more info please check: http://www.konkoly.hu/staff/kovacs/tff.html
+
+"""
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+
+from .core import TFFCommand, loadtarget  # noqa
