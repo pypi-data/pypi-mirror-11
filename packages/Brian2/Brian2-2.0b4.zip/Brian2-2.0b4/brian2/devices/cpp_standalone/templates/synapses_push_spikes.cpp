1faf243a0a09bf847719b606f738457c950b502a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:46 +0000

    Add package Brian2/2.0b4/Brian2-2.0b4.zip

---
+++ packages/Brian2/Brian2-2.0b4.zip/Brian2-2.0b4/brian2/devices/cpp_standalone/templates/synapses_push_spikes.cpp
@@ -0,0 +1,42 @@
+{# IS_OPENMP_COMPATIBLE #}
+////////////////////////////////////////////////////////////////////////////
+//// MAIN CODE /////////////////////////////////////////////////////////////
+
+{% macro cpp_file() %}
+#include "code_objects/{{codeobj_name}}.h"
+#include "brianlib/common_math.h"
+#include "brianlib/stdint_compat.h"
+#include<cmath>
+
+void _run_{{codeobj_name}}()
+{
+	using namespace brian;
+    ///// CONSTANTS ///////////
+	%CONSTANTS%
+	///// POINTERS ////////////
+    {{pointers_lines|autoindent}}
+
+    //// MAIN CODE ////////////
+	// we do advance at the beginning rather than at the end because it saves us making
+	// a copy of the current spiking synapses
+    {#  Get the name of the array that stores these events (e.g. the spikespace array) #}
+    {% set _eventspace = get_array_name(eventspace_variable) %}
+	{{owner.name}}.advance();
+	{{owner.name}}.push({{_eventspace}}, {{_eventspace}}[_num{{eventspace_variable.name}}-1]);
+	//{{owner.name}}.queue->peek();
+}
+{% endmacro %}
+
+////////////////////////////////////////////////////////////////////////////
+//// HEADER FILE ///////////////////////////////////////////////////////////
+
+{% macro h_file() %}
+#ifndef _INCLUDED_{{codeobj_name}}
+#define _INCLUDED_{{codeobj_name}}
+
+#include "objects.h"
+
+void _run_{{codeobj_name}}();
+
+#endif
+{% endmacro %}
