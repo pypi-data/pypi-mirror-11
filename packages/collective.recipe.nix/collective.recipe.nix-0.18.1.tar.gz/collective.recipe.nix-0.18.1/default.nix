4c6dedfe8f8d7fceca14f73bbed2d0b470ffcbcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:37 +0000

    Add package collective.recipe.nix/0.18.1/collective.recipe.nix-0.18.1.tar.gz

---
+++ packages/collective.recipe.nix/collective.recipe.nix-0.18.1.tar.gz/collective.recipe.nix-0.18.1/default.nix
@@ -0,0 +1,16 @@
+with import <nixpkgs> {}; {
+  myEnv = stdenv.mkDerivation {
+    name = "myEnv";
+    buildInputs = [
+      libxml2
+      libxslt
+      pythonPackages.ldap
+      pythonPackages.pillow
+      pythonPackages.readline
+      pythonPackages.zc_buildout_nix
+    ];
+    shellHook = ''
+      export SSL_CERT_FILE=~/.nix-profile/etc/ca-bundle.crt
+    '';
+  };
+}
