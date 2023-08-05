e405050ec655523d65edd7f7a9d5e940a23469ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:38 +0000

    Add package atomictempfile/0.2.post3/atomictempfile-0.2.post3.tar.gz

---
+++ packages/atomictempfile/atomictempfile-0.2.post3.tar.gz/atomictempfile-0.2.post3/setup.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+
+from setuptools import setup, find_packages
+import versioneer
+
+setup (
+    name = "atomictempfile",
+    version = versioneer.get_version (),
+    description = "An atomic file write context manager",
+    long_description = file ("README.rst").read (),
+    cmdclass = versioneer.get_cmdclass (),
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: "
+        + "GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Topic :: Utilities",
+    ],
+    keywords = "Atomic file write context manager",
+    author = "J C Lawrence",
+    author_email = "claw@kanga.nu",
+    url = "https://github.com/clearclaw/atomictempfile",
+    license = "LGPL v3.0",
+    test_suite = "tests", 
+    packages = find_packages (exclude = ["tests",]),
+    package_data = {
+    },
+    zip_safe = True,
+    install_requires = [line.strip ()
+        for line in file ("requirements.txt").readlines ()],
+    entry_points = {
+        "console_scripts": [
+        ],
+    },
+)
