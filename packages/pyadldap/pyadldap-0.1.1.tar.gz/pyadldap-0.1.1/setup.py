e17ac8456800942da7c01d361baf0990399371b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:56 +0000

    Add package pyadldap/0.1.1/pyadldap-0.1.1.tar.gz

---
+++ packages/pyadldap/pyadldap-0.1.1.tar.gz/pyadldap-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+	name="pyadldap",
+	version="0.1.1",
+	description="Library for managing Microsoft Active Directory from LDAP",
+	author="Jonas Delgado Mesa",
+	author_email="jdelgado@yohnah.net",
+	url="https://github.com/Wengex/PyADLdap",
+	license="GPLv2",
+	packages=["pyadldap"],
+	long_description=open('README.md').read(),
+)
