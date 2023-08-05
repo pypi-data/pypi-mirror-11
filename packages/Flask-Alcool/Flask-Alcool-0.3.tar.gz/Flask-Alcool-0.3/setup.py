25e177f7f8904eee5d0c40f01fa7bc109e2c84e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:40 +0000

    Add package Flask-Alcool/0.3/Flask-Alcool-0.3.tar.gz

---
+++ packages/Flask-Alcool/Flask-Alcool-0.3.tar.gz/Flask-Alcool-0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='Flask-Alcool',
+    version='0.3',
+    url='http://github.com/Kozea/flask-alcool',
+    license='Beer License',
+    author='Yohann Rebattu',
+    author_email='yohann.rebattu@kozea.fr',
+    description='Implement access control lists as decorators for flask.',
+    long_description=__doc__,
+    packages=['flask_alcool'],
+    zip_safe=False,
+    install_requires=[
+        'Flask',
+        'Jinja2>=2.5'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
