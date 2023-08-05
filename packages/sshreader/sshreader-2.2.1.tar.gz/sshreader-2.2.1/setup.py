0efa8ad3bd6ef86cd8d8ae762874a8c92f42db72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:16 +0000

    Add package sshreader/2.2.1/sshreader-2.2.1.tar.gz

---
+++ packages/sshreader/sshreader-2.2.1.tar.gz/sshreader-2.2.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2
+# coding=utf-8
+"""Setup file for sshreader module"""
+
+from setuptools import setup
+
+setup(name='sshreader',
+      version='2.2.1',
+      description='Multi-threading/processing for Paramiko',
+      author='Jesse Almanrode',
+      author_email='jesse@almanrode.com',
+      url='https://bitbucket.org/isaiah1112/sshreader',
+      py_modules=['sshreader'],
+      license='GNU Lesser General Public License v3 or later (LGPLv3+)',
+      install_requires=['paramiko>=1.15',
+                        ],
+      platforms='any',
+      classifiers=[
+          'Programming Language :: Python',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+          'Development Status :: 5 - Production/Stable',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          ],
+      )
