4f2e712d11e849e1bc5889cdc822a8fef51425b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:36 +0000

    Add package pybbdb/0.3/pybbdb-0.3.tar.gz

---
+++ packages/pybbdb/pybbdb-0.3.tar.gz/pybbdb-0.3/conf/unittest.py
@@ -0,0 +1,27 @@
+"""
+Unit test support.
+"""
+
+import os
+import sys
+
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        #os.chdir('tests')
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
