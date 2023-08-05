927036a07707f04a2062b5f55ec19fa9c7a108d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:09 +0000

    Add package pytter/1.0.0/pytter-1.0.0.tar.gz

---
+++ packages/pytter/pytter-1.0.0.tar.gz/pytter-1.0.0/setup.py
@@ -0,0 +1,20 @@
+# vim: fileencoding=utf-8 tw=100 expandtab ts=4 sw=4 :
+#
+# pytter
+# copyright (c) 2015 Vincent Loy
+# Vincent Loy <vincent.loy1@gmail.com>
+
+from distutils.core import setup
+
+setup(
+    name='pytter',
+    packages=['pytter'],
+    version='1.0.0',
+    description='A simple python module to parse tweets',
+    author='Vincent Loy',
+    author_email='vincent.loy1@gmail.com',
+    url='https://github.com/VincentLoy/pytter',
+    download_url='https://github.com/VincentLoy/pytter/tarball/v1.0.0',
+    keywords=['twitter', 'parse', 'parser', 'tweet'],
+    classifiers=[],
+)
