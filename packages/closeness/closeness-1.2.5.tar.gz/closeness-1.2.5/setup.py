374eb041e6d263bd9774157eb1e7c27eab99cb50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:21 +0000

    Add package closeness/1.2.5/closeness-1.2.5.tar.gz

---
+++ packages/closeness/closeness-1.2.5.tar.gz/closeness-1.2.5/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='closeness',
+
+    version='1.2.5',
+
+    description='Mongodb document similarity',
+    long_description=long_description,
+
+    url='https://github.com/mthahakp/mongodb-doc-closeness',
+
+    author='Thaha',
+    author_email='tkannippoyil@suyati.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    keywords='mongodb setuptools development relationship query',
+
+    packages=['closeness'],
+
+)
