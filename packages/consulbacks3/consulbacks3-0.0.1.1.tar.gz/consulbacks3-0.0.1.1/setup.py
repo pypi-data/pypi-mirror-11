37d6fdd36b26130ef57c53ae7dad36527a81eccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:51 +0000

    Add package consulbacks3/0.0.1.1/consulbacks3-0.0.1.1.tar.gz

---
+++ packages/consulbacks3/consulbacks3-0.0.1.1.tar.gz/consulbacks3-0.0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='consulbacks3',
+      version='0.0.1.1',
+      description='Backup the consul key value pair directory wise in  yml files and uploads to an s3 bucket ',
+      packages=['consulbacks3'],
+      install_requires = [ 'tinys3' ],
+      url='https://github.com/Ankit-Kulkarni/consulbacks3',
+      scripts = ["consulbacks3/consulbacks3", "consulbacks3/consulbacks3-configure", "consulbacks3/s3upload.py"],
+      author='Ankit Kulkarni',
+      author_email='ankit.kul1890@gmail.com',
+      license='ISC',
+      keywords = ["consul", "backup", "yml", "key-value", "boto", "s3", "s3bucket", "bucket", "cli", "command"],
+      classifiers = [
+        "Natural Language :: English",
+        "Environment :: Console",
+        "Topic :: Utilities"
+        ],
+        zip_safe=False)
