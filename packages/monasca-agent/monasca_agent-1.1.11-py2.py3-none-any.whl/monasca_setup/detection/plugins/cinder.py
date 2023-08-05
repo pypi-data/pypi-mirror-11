20811530ebdaae40c902e865ba09715007ef5467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:36 +0000

    Add package monasca-agent/1.1.11/monasca_agent-1.1.11-py2.py3-none-any.whl

---
+++ packages/monasca-agent/monasca_agent-1.1.11-py2.py3-none-any.whl/monasca_setup/detection/plugins/cinder.py
@@ -0,0 +1,20 @@
+import monasca_setup.detection
+
+
+class Cinder(monasca_setup.detection.ServicePlugin):
+
+    """Detect Cinder daemons and setup configuration to monitor them."""
+
+    def __init__(self, template_dir, overwrite=True, args=None):
+        service_params = {
+            'args': args,
+            'template_dir': template_dir,
+            'overwrite': overwrite,
+            'service_name': 'block-storage',
+            'process_names': ['cinder-volume', 'cinder-scheduler',
+                              'cinder-api','cinder-backup'],
+            'service_api_url': 'http://localhost:8776/v2.0',
+            'search_pattern': '.*version=1.*'
+        }
+
+        super(Cinder, self).__init__(service_params)
