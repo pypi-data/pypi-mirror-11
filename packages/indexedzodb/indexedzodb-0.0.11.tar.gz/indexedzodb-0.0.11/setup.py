ed0c6d06eb735df9ba67f6b4bb0e1a134264940c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:56 +0000

    Add package indexedzodb/0.0.11/indexedzodb-0.0.11.tar.gz

---
+++ packages/indexedzodb/indexedzodb-0.0.11.tar.gz/indexedzodb-0.0.11/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(name='indexedzodb',
+      version='0.0.11',
+      license='MIT',
+      description='A simple, indexed model layer over ZODB',
+      author='Mark Skelton',
+      author_email='mark@software13.co.uk',
+      url='https://github.com/mtskelton/indexedzodb/',
+      py_modules=['indexedzodb.models'],
+      install_requires=['ZODB', 'zodbpickle', 'repoze.catalog'],
+      requires=['ZODB', 'zodbpickle'],
+      data_files=['LICENSE', 'README.md'],
+      provides=['indexedzodb'],
+      classifiers=['Development Status :: 2 - Pre-Alpha',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7',
+                   'License :: OSI Approved :: MIT License',
+                   'Topic :: Database :: Front-Ends',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   ]
+      )
