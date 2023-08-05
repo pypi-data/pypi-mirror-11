53321962c303ca0c9f441608d36a0c174c077a70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:00 +0000

    Add package slybot/0.12.0/slybot-0.12.0.tar.gz

---
+++ packages/slybot/slybot-0.12.0.tar.gz/slybot-0.12.0/setup.py
@@ -0,0 +1,29 @@
+from slybot import __version__
+from setuptools import setup, find_packages
+
+install_requires = ['Scrapy', 'scrapely', 'loginform', 'lxml', 'jsonschema',
+                    'dateparser', 'rfc3987', 'scrapyjs']
+extras = {
+    'tests': ['nose']
+}
+
+setup(name='slybot',
+      version=__version__,
+      license='BSD',
+      description='Slybot crawler',
+      author='Scrapy project',
+      author_email='info@scrapy.org',
+      url='http://github.com/scrapy/slybot',
+      packages=find_packages(exclude=('tests', 'tests.*')),
+      platforms=['Any'],
+      scripts=['bin/slybot', 'bin/portiacrawl'],
+      install_requires=install_requires,
+      extras_require=extras,
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7'
+      ])
