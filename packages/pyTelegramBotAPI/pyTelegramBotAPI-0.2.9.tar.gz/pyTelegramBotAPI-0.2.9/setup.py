af9824201e957f9596219d58875ce588f4f11e1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:25 +0000

    Add package pyTelegramBotAPI/0.2.9/pyTelegramBotAPI-0.2.9.tar.gz

---
+++ packages/pyTelegramBotAPI/pyTelegramBotAPI-0.2.9.tar.gz/pyTelegramBotAPI-0.2.9/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='pyTelegramBotAPI',
+      version='0.2.9',
+      description='Python Telegram bot api. ',
+      author='eternnoir',
+      author_email='eternnoir@gmail.com',
+      url='https://github.com/eternnoir/pyTelegramBotAPI',
+      packages=['telebot'],
+      license='GPL2',
+      keywords='tools',
+      install_requires=['pytest', 'requests', 'six'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Environment :: Console',
+          'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+      ]
+      )
