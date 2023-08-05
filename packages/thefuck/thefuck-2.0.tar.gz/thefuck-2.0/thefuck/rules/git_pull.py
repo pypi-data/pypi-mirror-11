b559d8f943f2c6225286aa9500b53e8b0332f392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:25 +0000

    Add package thefuck/2.0/thefuck-2.0.tar.gz

---
+++ packages/thefuck/thefuck-2.0.tar.gz/thefuck-2.0/thefuck/rules/git_pull.py
@@ -0,0 +1,17 @@
+from thefuck import shells, utils
+
+
+@utils.git_support
+def match(command, settings):
+    return ('git' in command.script
+            and 'pull' in command.script
+            and 'set-upstream' in command.stderr)
+
+
+@utils.git_support
+def get_new_command(command, settings):
+    line = command.stderr.split('\n')[-3].strip()
+    branch = line.split(' ')[-1]
+    set_upstream = line.replace('<remote>', 'origin')\
+                       .replace('<branch>', branch)
+    return shells.and_(set_upstream, command.script)
