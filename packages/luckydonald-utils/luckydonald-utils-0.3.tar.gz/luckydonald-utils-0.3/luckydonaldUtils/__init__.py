22c4c0534effb762a492dcb784864c41c06a3b4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:10 +0000

    Add package luckydonald-utils/0.3/luckydonald-utils-0.3.tar.gz

---
+++ packages/luckydonald-utils/luckydonald-utils-0.3.tar.gz/luckydonald-utils-0.3/luckydonaldUtils/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+__author__ = 'luckydonald'
+
+# DO NOT REMOVE the following, to allow 'from luckydonaldUtils import logging'
+from .Logging import logging as Logging
+
+
+VERSION = "0.3"
+
