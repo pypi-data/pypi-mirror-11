c7826876da7971891d7f6e02fccf8b4a78382e5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:20 +0000

    Add package jf15_athletes/1.0.0/jf15_athletes-1.0.0.zip

---
+++ packages/jf15_athletes/jf15_athletes-1.0.0.zip/jf15_athletes-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name		 = 'jf15_athletes',
+		version		 = '1.0.0',
+		py_modules	 = ['athletelist'],
+		author		 = 'hfpython',
+		author_email = 'hfpython@headfirstlabs.com',
+		url			 = 'http://www.headfirstlabs.com',
+		description  = 'A class of athlete',
+		)
