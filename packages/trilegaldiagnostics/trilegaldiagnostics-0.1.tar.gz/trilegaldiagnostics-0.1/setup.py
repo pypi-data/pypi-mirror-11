6e3af7ea51d908ae38a01b4ce5b0ad48b12c403c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:03 +0000

    Add package trilegaldiagnostics/0.1/trilegaldiagnostics-0.1.tar.gz

---
+++ packages/trilegaldiagnostics/trilegaldiagnostics-0.1.tar.gz/trilegaldiagnostics-0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='trilegaldiagnostics',
+    author='Phil Rosenfield',
+    author_email='philip.rosenfield@unipd.it',
+    version='0.1',
+    py_modules=['color_by_arg'],
+    scripts=['color_by_arg'],
+    install_requires=['numpy', 'matplotlib'],
+    url='https://github.com/philrosenfield/trilegaldiagnostics'
+)
