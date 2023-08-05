703a99f7ffff2772a7adec8f0b40cacec70fea51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:56 +0000

    Add package coolfig/0.3.0/coolfig-0.3.0.tar.gz

---
+++ packages/coolfig/coolfig-0.3.0.tar.gz/coolfig-0.3.0/coolfig/__init__.py
@@ -0,0 +1,21 @@
+"""
+Support for working with different sources of configuration values.
+
+    class DefaultSettings(schema.Settings):
+        SECRET_KEY = schema.Value(str)
+        DEBUG = schema.Value(types.boolean, default=False)
+        DB_URL = schema.Value(types.sqlalchemy_url)
+        LOCALES = schema.Value(types.list(str))
+
+    settings = DefaultSettings(
+        providers.DictConfig(os.environ, prefix='MYAPP_'))
+"""
+from .schema import Value, Settings
+from .providers import EnvConfig, DictConfig
+from .django import load_django_settings
+
+
+__version__ = '0.3.0'
+__url__ = 'https://github.com/GaretJax/coolfig'
+__all__ = ['Value', 'Settings', 'EnvConfig', 'DictConfig',
+           'load_django_settings']
