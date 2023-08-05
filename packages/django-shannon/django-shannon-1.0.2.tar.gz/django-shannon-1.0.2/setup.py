4fc8acba39c1caf8125dcf017f58e63b99f4c267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:04 +0000

    Add package django-shannon/1.0.2/django-shannon-1.0.2.tar.gz

---
+++ packages/django-shannon/django-shannon-1.0.2.tar.gz/django-shannon-1.0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name = 'django-shannon',
+    packages = ['shannon'],
+    version = '1.0.2',
+    description='S3 backed django media object.',
+    maintainer='Eric Neuman',
+    maintainer_email='eric@soloptimus.net',
+    url="https://github.com/neuman/django-shannon",
+    download_url = 'https://github.com/neuman/django-shannon/tarball/v1.0.2',
+    keywords=['django', 'media','s3','storage']
+)
