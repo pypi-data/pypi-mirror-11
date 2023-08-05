a94023e376542b4439ea37c7bf0def08e1ace8b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:32 +0000

    Add package ansible-windows-compat/0.1.0/ansible-windows-compat-0.1.0.tar.gz

---
+++ packages/ansible-windows-compat/ansible-windows-compat-0.1.0.tar.gz/ansible-windows-compat-0.1.0/README.rst
@@ -0,0 +1,14 @@
+|PyPI Version| |Python Versions|
+
+Ansible Windows Compat
+======================
+
+Initial work on a compatibility library to allow Ansible modules written in
+Python to run on a Windows target.  Only planned to support Ansible v2, and so
+far only ping works.
+
+
+.. |PyPI Version| image:: https://img.shields.io/pypi/v/ansible-windows-compat.svg
+   :target: https://pypi.python.org/pypi/ansible-windows-compat
+.. |Python Versions| image:: https://img.shields.io/pypi/pyversions/ansible-windows-compat.svg
+   :target: https://pypi.python.org/pypi/ansible-windows-compat
---
+++ packages/ansible-windows-compat/ansible-windows-compat-0.1.0.tar.gz/ansible-windows-compat-0.1.0/setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+# Python
+import os
+import sys
+from distutils import sysconfig
+
+# Setuptools
+from setuptools import setup
+
+extra = {}
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
+
+relative_site_packages = os.path.relpath(sysconfig.get_python_lib(), sys.prefix)
+
+setup(
+    name='ansible-windows-compat',
+    version='0.1.0',
+    author='Chris Church',
+    author_email='chris@ninemoreminutes.com',
+    description='Compatibility library to run Ansible Python modules on a '
+                'Windows target.',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst'),
+                          'rb').read().decode('utf-8'),
+    license='BSD',
+    keywords='python ansible windows',
+    url='https://github.com/cchurch/ansible-windows-compat',
+    packages=['ansible_windows_compat'],
+    data_files=[(relative_site_packages, ['ansible_windows_compat.pth'])],
+    include_package_data=False,
+    zip_safe=False,
+    install_requires=[],
+    setup_requires=[],
+    #tests_require=['six'],
+    #test_suite='test_ansible_windows_compat.suite',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Win32 (MS Windows)',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        #'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        #'Programming Language :: Python :: 3.2',
+        #'Programming Language :: Python :: 3.3',
+        #'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries',
+    ],
+    options={
+        'egg_info': {
+            'tag_build': '.dev',
+        },
+        'aliases': {
+            'dev_build': 'egg_info sdist',
+            'release_build': 'egg_info -b "" -R sdist',
+        },
+    },
+    **extra
+)
