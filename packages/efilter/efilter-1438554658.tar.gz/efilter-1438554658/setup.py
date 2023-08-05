a8b864d3bf721bbd79c0479ddc881b0f8b805085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:18 +0000

    Add package efilter/1438554658/efilter-1438554658.tar.gz

---
+++ packages/efilter/efilter-1438554658.tar.gz/efilter-1438554658/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from efilter import version
+from setuptools import find_packages, setup
+
+setup(name="efilter",
+      version=version.get_version(),
+      description="EFILTER query language",
+      license="Apache 2.0",
+      author="Adam Sindelar",
+      author_email="adam.sindelar@gmail.com",
+      url="https://github.com/google/dotty/",
+      packages=find_packages(exclude=["tests*"]),
+      package_dir={"efilter": "efilter"})
