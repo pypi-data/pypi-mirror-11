dabe9511dc0740381a168708a5bc5169488368e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:51 +0000

    Add package dj-spam/0.1.0/dj-spam-0.1.0.tar.gz

---
+++ packages/dj-spam/dj-spam-0.1.0.tar.gz/dj-spam-0.1.0/spam/utils.py
@@ -0,0 +1,23 @@
+from django.apps import apps
+from django.core.exceptions import FieldDoesNotExist
+
+
+def spammables():
+    # Lists all models that are marked flaggable
+    flaggables = []
+    for model in apps.get_models():
+        try:
+            model._meta.get_field_by_name('spam_flag')
+        except FieldDoesNotExist:
+            continue
+        flaggables.append(model)
+    return flaggables
+
+
+def is_spammable(app, model):
+    model_class = apps.get_model("{}.{}".format(app, model))
+    return model_class in spammables()
+
+
+def get_app_name(model_class_or_instance):
+    return model_class_or_instance._meta.app_config.name.split('.')[-1]
