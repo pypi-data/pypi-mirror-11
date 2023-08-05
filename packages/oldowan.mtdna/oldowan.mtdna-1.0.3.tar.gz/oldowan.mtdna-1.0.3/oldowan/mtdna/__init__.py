a20430c2381585a88218337333b02667c67dfa3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:12 +0000

    Add package oldowan.mtdna/1.0.3/oldowan.mtdna-1.0.3.tar.gz

---
+++ packages/oldowan.mtdna/oldowan.mtdna-1.0.3.tar.gz/oldowan.mtdna-1.0.3/oldowan/mtdna/__init__.py
@@ -0,0 +1,33 @@
+"""This is the oldowan.mtdna package."""
+
+import os
+
+VERSION = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'VERSION')).read().strip()
+
+__all__ = ['rCRS', 
+           'rCRSplus',
+           'rCRSplus_positions',
+           'rCRSlist']
+
+try:
+    from oldowan.mtdna.rCRS import rCRS
+    from oldowan.mtdna.rCRS import rCRSlist
+    from oldowan.mtdna.rCRS import rCRSplus
+    from oldowan.mtdna.rCRS import rCRSplus_positions
+    from oldowan.mtdna.rCRS import HVR1_indices
+    from oldowan.mtdna.rCRS import HVR2_indices
+    from oldowan.mtdna.rCRS import HVR1and2_indices
+    from oldowan.mtdna.rCRS import HVR1to2_indices
+    from oldowan.mtdna.rCRS import coding_indices
+    from oldowan.mtdna.rCRS import all_indices
+except:
+    from rCRS import rCRS
+    from rCRS import rCRSlist
+    from rCRS import rCRSplus
+    from rCRS import rCRSplus_positions
+    from rCRS import HVR1_indices
+    from rCRS import HVR2_indices
+    from rCRS import HVR1and2_indices
+    from rCRS import HVR1to2_indices
+    from rCRS import coding_indices
+    from rCRS import all_indices
