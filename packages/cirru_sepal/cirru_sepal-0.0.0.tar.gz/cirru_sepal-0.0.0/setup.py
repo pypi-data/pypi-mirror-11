e51ff486385bd898e4a3c601ae5d87e3d76c7c30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:51 +0000

    Add package cirru_sepal/0.0.0/cirru_sepal-0.0.0.tar.gz

---
+++ packages/cirru_sepal/cirru_sepal-0.0.0.tar.gz/cirru_sepal-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'cirru_sepal',
+  packages = ['cirru_sepal'],
+  version = '0.0.0',
+  description = 'Cirru Sepal experiment in Python',
+  author = 'jiyinyiyong',
+  author_email = 'jiyinyiyong@gmail.com',
+  url = 'https://github.com/Cirru/sepal.py',
+  download_url = 'https://github.com/Cirru/sepal.py/tarball/0.0.01',
+  keywords = ['cirru', 'sepal', 'ast'],
+  classifiers = [],
+)
