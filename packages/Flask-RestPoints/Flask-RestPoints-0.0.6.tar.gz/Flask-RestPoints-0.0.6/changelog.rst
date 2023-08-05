8d6ed8c8b07fc822c7bcfa7c39afefe4e0ba5e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:53 +0000

    Add package Flask-RestPoints/0.0.6/Flask-RestPoints-0.0.6.tar.gz

---
+++ packages/Flask-RestPoints/Flask-RestPoints-0.0.6.tar.gz/Flask-RestPoints-0.0.6/changelog.rst
@@ -0,0 +1,19 @@
+Changelog
+=========
+
+.. currentmodule:: Flask-RestPoints
+
+Release 0.0a6
+-------------
+
+ping, epoch, status routes are now canonical.
+
+Release 0.0a5
+-------------
+
+Removed setting the `app` on the RestPoints class during `init_app`. Added comments.
+
+Release 0.0a4
+-------------
+
+Initial release
---
+++ packages/Flask-RestPoints/Flask-RestPoints-0.0.6.tar.gz/Flask-RestPoints-0.0.6/flask_restpoints/handlers.py
@@ -0,0 +1,71 @@
+import concurrent.futures
+from time import time
+from timeit import timeit
+
+from flask import jsonify
+
+
+def status(jobs):
+    """Handler that calls each status job in a worker pool, attempting to timeout.
+    The resulting durations/errors are written to the response
+    as JSON.
+
+    eg.
+
+    `{
+        "endpoints": [
+            { "endpoint": "Jenny's Database", "duration": 1.002556324005127 },
+            { "endpoint": "Hotmail", "duration": -1, "error": "Host is down" },
+        ]
+     }`
+    """
+    def status_handler():
+        endpoints = []
+        stats = {"endpoints": None}
+
+        executor = concurrent.futures.ThreadPoolExecutor(max_workers=5)
+        # This is basically calling the below within the executor:
+        #
+        #     >>> timeit(job[2], number=1)
+        #
+        # job is a tuple of (name, timeout, func) so the above is really:
+        #     >>> timeit(func, number=1)
+        #
+        #gen = ((job, executor.submit(timeit, job[2], number=1)) for job in jobs)
+        #for job, future in gen:
+        for job, future in [(job, executor.submit(timeit, job[2], number=1)) for job in jobs]:
+            name, timeout, _ = job
+            endpoint = {"endpoint": name}
+            try:
+                data = future.result(timeout=timeout)
+                endpoint["duration"] = data
+            except concurrent.futures.TimeoutError:
+                endpoint["error"] = "timeout exceeded"
+            except Exception as ex:
+                endpoint["error"] = str(ex)
+            endpoints.append(endpoint)
+
+        if len(endpoints) > 0:
+            stats["endpoints"] = endpoints
+
+        executor.shutdown(wait=False)
+        return jsonify(**stats)
+        # TODO: Look into potentially cleaning up jobs that have timed-out.
+        #
+        #       This could be done by changing jobs from a func to an object
+        #       that implements `def interrupt(self):` which would be used
+        #       to interrupt/stop/close/cleanup any resources.
+    return status_handler
+
+
+def ping():
+    """Handler that simply returns `pong` from a GET.
+    """
+    return "pong"
+
+
+def epoch():
+    """Handler that returns the systems current Epoch time.
+    """
+    seconds = int(time())
+    return str(seconds)
