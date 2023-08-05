8de1d6a71dacd7f8fe36bfa383813d3679a39819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:47 +0000

    Add package pandashells/0.1.4/pandashells-0.1.4.tar.gz

---
+++ packages/pandashells/pandashells-0.1.4.tar.gz/pandashells-0.1.4/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+fileDir = os.path.dirname(__file__)
+
+LONG_DESCRIPTION = """
+Pandashells is a set of unix command-line tools that provide accessibility
+to the python data stack directly from the bash prompt.  Users can utilize
+pandas-based data-munging, perform linear regression, and even take
+the spectral decomposition of non-uniformly sampled time series.
+"""
+
+setup(
+    name="pandashells",
+    version="0.1.4",
+    author="Rob deCarvalho",
+    author_email="unlisted",
+    description=("Command line data tools"),
+    license="BSD",
+    keywords="pandas plot plotting data dataframe command line cli statistics stats",
+    url="https://github.com/robdmc/pandashells",
+    packages=find_packages(),
+    package_data={'pandashells': ['example_data/*.csv']},
+    # include_package_data=True,
+    long_description=LONG_DESCRIPTION,
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Scientific/Engineering',
+    ],
+    entry_points={
+        'console_scripts': [
+            'p.cdf = pandashells.bin.p_cdf:main',
+            'p.config = pandashells.bin.p_config:main',
+            'p.crypt = pandashells.bin.p_crypt:main',
+            'p.df = pandashells.bin.p_df:main',
+            'p.facet_grid = pandashells.bin.p_facet_grid:main',
+            'p.format = pandashells.bin.p_format:main',
+            'p.hist = pandashells.bin.p_hist:main',
+            'p.linspace = pandashells.bin.p_linspace:main',
+            'p.lomb_scargle = pandashells.bin.p_lomb_scargle:main',
+            'p.merge = pandashells.bin.p_merge:main',
+            'p.parallel = pandashells.bin.p_parallel:main',
+            'p.plot = pandashells.bin.p_plot:main',
+            'p.rand = pandashells.bin.p_rand:main',
+            'p.regplot = pandashells.bin.p_regplot:main',
+            'p.regress = pandashells.bin.p_regress:main',
+            'p.example_data = pandashells.bin.p_example_data:main',
+            'p.sig_edit = pandashells.bin.p_sig_edit:main',
+        ],
+    }
+)
