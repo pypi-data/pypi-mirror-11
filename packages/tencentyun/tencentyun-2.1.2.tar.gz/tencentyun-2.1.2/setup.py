1d86a09cb72092a1009b0330c73158f2973fefe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:14 +0000

    Add package tencentyun/2.1.2/tencentyun-2.1.2.tar.gz

---
+++ packages/tencentyun/tencentyun-2.1.2.tar.gz/tencentyun-2.1.2/setup.py
@@ -0,0 +1,25 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+import sys
+
+pkgdir = {'': 'python%s' % sys.version_info[0]}
+VERSION = '2.1.2'
+
+setup(
+    name='tencentyun',
+    version=VERSION,
+    keywords = ('tencentyun', 'qcloud'),
+    author='Jay Li, Jamis Hoo',
+    author_email='jayli@tencent.com, hoojamis@gmail.com',
+    url='https://github.com/tencentyun/python-sdk',
+    download_url='https://codeload.github.com/tencentyun/python-sdk/tar.gz/%s' % VERSION,
+    description='Python 2/3 SDK for app.qcloud.com',
+    license='MIT',
+    # long_description="",
+    package_dir=pkgdir,
+    packages=['tencentyun'],
+    classifiers=[ ],
+)
+
