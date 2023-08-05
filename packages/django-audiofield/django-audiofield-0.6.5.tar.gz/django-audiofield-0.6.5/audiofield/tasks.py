e2af464c696c3e84b0cba112c4a2167d86c5da1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:55 +0000

    Add package django-audiofield/0.6.5/django-audiofield-0.6.5.tar.gz

---
+++ packages/django-audiofield/django-audiofield-0.6.5.tar.gz/django-audiofield-0.6.5/audiofield/tasks.py
@@ -0,0 +1,38 @@
+#
+# django-audiofield License
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (C) 2011-2014 Star2Billing S.L.
+#
+# The Initial Developer of the Original Code is
+# Arezqui Belaid <info@star2billing.com>
+#
+
+from celery.utils.log import get_task_logger
+from celery.decorators import task
+import subprocess
+# import shlex
+
+logger = get_task_logger(__name__)
+
+
+@task()
+def audio_convert_task(conv):
+    """Convert audio files"""
+
+    logger.info('Received a request to convert audio file :> ' + str(conv))
+
+    # Option 1 : Popen
+    response = subprocess.Popen(conv.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (filetype, error) = response.communicate()
+    if error:
+        logger.error('Error conversion : %s ' % error)
+
+    # Option 2 : Popen & Shlex
+    # args = shlex.split(conv)
+    # p = subprocess.Popen(args)
+
+    return response
