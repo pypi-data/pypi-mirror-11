5230066bc3b769829deb2dbfcd5bc09902abba93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:27 +0000

    Add package sloth-ci/1.3.0/sloth-ci-1.3.0.zip

---
+++ packages/sloth-ci/sloth-ci-1.3.0.zip/sloth-ci-1.3.0/sloth_ci/ext/db_build_history.py
@@ -0,0 +1,22 @@
+def extend(cls, extension):
+    from ..util import SqliteHandler
+
+
+    class Sloth(cls):
+        def __init__(self, config):
+            super().__init__(config)
+
+            db_build_log_config = extension['config']
+
+            db_build_log_handler = SqliteHandler(db_build_log_config['db'], db_build_log_config['table'])
+
+            self.build_logger.addHandler(db_build_log_handler)
+
+            self.log_handlers[extension['name']] = db_build_log_handler
+
+        def stop(self):
+            super().stop()
+            self.build_logger.removeHandler(self.log_handlers.pop(extension['name']))
+
+
+    return Sloth
