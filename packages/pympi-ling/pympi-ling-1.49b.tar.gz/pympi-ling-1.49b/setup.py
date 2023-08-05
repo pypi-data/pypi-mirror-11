5ce5245915a181b7291e82de36a54dab6330b7a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:58 +0000

    Add package pympi-ling/1.49b/pympi-ling-1.49b.tar.gz

---
+++ packages/pympi-ling/pympi-ling-1.49b.tar.gz/pympi-ling-1.49b/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+licence = '"THE BEER-WARE LICENSE" (Revision 42)'
+version = '1.49b'
+
+setup(name='pympi-ling',
+      version=version,
+      description=
+        'A python module for processing ELAN and Praat annotation files',
+      author='Mart Lubbers',
+      long_description=open('README.rst').read(),
+      author_email='mart@martlubbers.net',
+      url='https://github.com/dopefishh/pympi',
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'Environment :: Console',
+                   'Natural Language :: English',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.4',
+                   'Topic :: Text Processing :: Linguistic'],
+      packages=['pympi'])
