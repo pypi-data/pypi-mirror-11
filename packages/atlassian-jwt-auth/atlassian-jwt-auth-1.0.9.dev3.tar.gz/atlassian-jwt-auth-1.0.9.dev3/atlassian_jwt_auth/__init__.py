06a2420ebafa71392b96c4e6f01f5ae5d03da400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:49 +0000

    Add package atlassian-jwt-auth/1.0.9.dev3/atlassian-jwt-auth-1.0.9.dev3.tar.gz

---
+++ packages/atlassian-jwt-auth/atlassian-jwt-auth-1.0.9.dev3.tar.gz/atlassian-jwt-auth-1.0.9.dev3/atlassian_jwt_auth/__init__.py
@@ -0,0 +1,16 @@
+__version__ = '1.0.6'
+
+
+def get_permitted_algorithm_names():
+    """ returns permitted algorithm names. """
+    return [
+        'RS256',
+        'RS384',
+        'RS512',
+        'ES256',
+        'ES384',
+        'ES512',
+        'PS256',
+        'PS384',
+        'PS512'
+    ]
