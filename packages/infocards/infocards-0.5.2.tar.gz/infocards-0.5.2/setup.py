6c8073168052ae3e0eb7ba04d4d99f86e6a78f8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:28 +0000

    Add package infocards/0.5.2/infocards-0.5.2.tar.gz

---
+++ packages/infocards/infocards-0.5.2.tar.gz/infocards-0.5.2/setup.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+#
+# Simple information card archive library
+# https://github.com/rmed/infocards
+#
+# Copyright (C) 2015  Rafael Medina García <rafamedgar@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'DESCRIPTION.rst')) as f:
+    long_description = f.read()
+
+setup(
+    name='infocards',
+    version='0.5.2',
+
+    description='Simple information card archive library',
+    long_description=long_description,
+
+    url='https://github.com/rmed/infocards',
+
+    author='Rafael Medina García',
+    author_email='rafamedgar@gmail.com',
+
+    license='GPLv2+',
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Database',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    keywords='information info card archive database',
+
+    packages=['infocards'],
+
+    install_requires=[
+        'python-Levenshtein == 0.12.0',
+        'fuzzywuzzy == 0.5.0',
+        'peewee == 2.6.3'
+    ],
+)
