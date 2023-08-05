119a6bb7abab19cac0aa58fd840cc54f6602dbd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:05 +0000

    Add package bu-cascade/1.983/bu_cascade-1.983.tar.gz

---
+++ packages/bu-cascade/bu_cascade-1.983.tar.gz/bu_cascade-1.983/bu_cascade/dynamic_data_def_builder/urls.py
@@ -0,0 +1,22 @@
+__author__ = 'phg49389'
+
+from django.conf.urls import patterns, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+# fetch_array requires a username as the first part of the url, and an optional filepath as the second.
+# fetch/<username> or fetch/<username>/<path: called> in Flask terms
+from interfacer import fetch_array
+
+# Because Django does first url match returning, I'm putting the longer match first.
+# The first url listens for subsequent fetches, which go to subfolders.
+# The second url listens for the first fetch, which gets the top-level structure by default.
+# Django should call fetch_array($, $) for the first url, and fetch_array($) for the second.
+
+
+def print_calls(args):
+    print args
+
+urlpatterns = patterns('sample1',
+                       url(r'^fetch/(.*)/(.*)$', fetch_array),
+                       url(r'^fetch/(.*)$', print_calls),
+                       )
+urlpatterns += staticfiles_urlpatterns()
