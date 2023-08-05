7035a73a9353c6447645e6087bdb2aa681a71671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:05 +0000

    Add package eagle_automation/0.1.6/eagle_automation-0.1.6-py3.4.egg

---
+++ packages/eagle_automation/eagle_automation-0.1.6-py3.4.egg/eagle_automation/config.py
@@ -0,0 +1,22 @@
+import os
+
+CONFIG_PATHS = [
+	os.path.join(os.path.dirname(__file__), 'default.py'),
+	'/etc/eagle_automation.conf',
+	os.path.join(os.environ.get('HOME', '/'), '.config/eagle_automation.conf'),
+	'eagle_automation.conf',
+]
+
+class Config: pass
+
+def _get_config():
+
+	config = Config()
+
+	for path in CONFIG_PATHS:
+		if os.path.exists(path):
+			exec(compile(open(path).read(), path, 'exec'), config.__dict__)
+
+	return config
+
+config = _get_config()
