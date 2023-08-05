d839ff461109ab14b84b05789ea7c212a39ca7a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:24 +0000

    Add package stringcase/1.0.1/stringcase-1.0.1.tar.gz

---
+++ packages/stringcase/stringcase-1.0.1.tar.gz/stringcase-1.0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='stringcase',
+    version='1.0.1',
+    py_modules=[
+        'stringcase'
+    ],
+    url='https://github.com/okunishinishi/python-stringcase',
+    license='MIT',
+    author='Taka Okunishi',
+    author_email='',
+    description='String case converter.',
+    long_description=open('README.rst', 'rt').read()
+)
