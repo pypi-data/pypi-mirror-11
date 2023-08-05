7e1badee26d3526b3c07a6f06d6dfffcd264aad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:34 +0000

    Add package mxp/0.1/mxp-0.1.tar.gz

---
+++ packages/mxp/mxp-0.1.tar.gz/mxp-0.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+    name = "mxp",
+    packages = ["mxp"],
+    version = "0.1",
+    description = "qPCR fileformat reader",
+    author = "Florian FInkernagel",
+    author_email = "finkernagel@imt.uni-marburg.de",
+    url = "http://www.imt.uni-marburg.de",
+    keywords = ["qPCR", "MXP"],
+    classifiers = [
+        "Programming Language :: Python",
+        #"Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+    long_description = """\
+            Reads raw qPCR data from .mxp files and turns it into pandas dataframes.
+"""
+)
