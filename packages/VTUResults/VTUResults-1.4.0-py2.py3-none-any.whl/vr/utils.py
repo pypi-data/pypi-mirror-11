f1171e1b539e7eb003ddd838f36f9a7648903626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:58 +0000

    Add package VTUResults/1.4.0/VTUResults-1.4.0-py2.py3-none-any.whl

---
+++ packages/VTUResults/VTUResults-1.4.0-py2.py3-none-any.whl/vr/utils.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python   
+
+import requests
+from bs4 import BeautifulSoup
+
+from constants import BASE_URL
+
+def get_result(usn):
+    """
+    Returns a html object for the requested usn
+    """
+    payload = {'rid':usn, 'submit':'SUBMIT'} 
+    print "Hey, there!"
+    print "Hold on!, till I get your result.."
+    try:
+    	response = requests.get(BASE_URL, params=payload).text
+    	return BeautifulSoup(response, 'html.parser')
+    except requests.exceptions.Timeout as e:
+    	print "Sorry, Time Out : ", e 
+    except requests.exceptions.TooManyRedirects as e:
+    	print "Sorry, Too Many Redirects : ", e
+    except requests.exceptions.RequestException as e:
+    	print "Sorry, Catastrophic Error : ",e	
