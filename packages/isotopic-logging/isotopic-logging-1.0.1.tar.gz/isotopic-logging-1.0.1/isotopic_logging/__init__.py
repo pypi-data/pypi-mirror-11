371c769e19ae3ff4b9a7b0664a76f80b85aa12f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:06 +0000

    Add package isotopic-logging/1.0.1/isotopic-logging-1.0.1.tar.gz

---
+++ packages/isotopic-logging/isotopic-logging-1.0.1.tar.gz/isotopic-logging-1.0.1/isotopic_logging/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from .context import injection_context
+from .generators import default_oid_generator
+from .injectors import PrefixInjector
+from .prefix import make_prefix, join_prefix
+
+
+def direct_injector(prefix, container=None):
+    injector = PrefixInjector(prefix)
+    return injection_context(injector, container)
+
+
+def prefix_injector(prefix, delimiter=None, container=None):
+    prefix = make_prefix(prefix, delimiter)
+    injector = PrefixInjector(prefix)
+    return injection_context(injector, container)
+
+
+def autoprefix_injector(oid_generator=None, delimiter=None, container=None):
+    autopart = next(oid_generator or default_oid_generator)
+    return prefix_injector(autopart, delimiter, container)
+
+
+def hybrid_injector(prefix, oid_generator=None, delimiter=None,
+                    container=None):
+    autopart = next(oid_generator or default_oid_generator)
+    prefix = join_prefix([autopart, prefix, ], delimiter)
+    injector = PrefixInjector(prefix)
+    return injection_context(injector, container)
---
+++ packages/isotopic-logging/isotopic-logging-1.0.1.tar.gz/isotopic-logging-1.0.1/isotopic_logging/defaults.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+OID_LENGTH = 6
+OID_MAX_LENGTH = 32
+
+DELIMITER = " | "
