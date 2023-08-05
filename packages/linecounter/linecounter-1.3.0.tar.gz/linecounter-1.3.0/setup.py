8f67f59959cfb4e85d72a4fd01229f31fc12ec72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:25 +0000

    Add package linecounter/1.3.0/linecounter-1.3.0.tar.gz

---
+++ packages/linecounter/linecounter-1.3.0.tar.gz/linecounter-1.3.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='linecounter',
+      version='1.3.0',
+      description='linecounter for files',
+      long_description='linecounter is a tool you can count how many lines you have in your files with it.',
+      url='https://github.com/halitkarakis/line-counter/',
+      py_modules=['linecounter'],
+      author='M.Halit Karakis',
+      author_email='halit@halitkarakis.com.tr',
+      entry_points={
+        'console_scripts': [
+            'linecounter=linecounter:main',
+        ],
+      },
+      license='Apache Software License',
+      keywords='line counting tool command line'
+      )
