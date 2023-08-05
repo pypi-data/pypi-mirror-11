2bd51623af083cc1246f34f68f8a4f68b68bde32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:33 +0000

    Add package funniests/3.0/funniests-3.0.zip

---
+++ packages/funniests/funniests-3.0.zip/funniests-3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='funniests',
+    py_module=['funniests'],
+    version='3.0',
+    description='A simple parser through the list',
+    author='karthik',
+    author_email='chicool.viper@gmail.com',
+    url='http://github.com/mkarthik',
+    )
