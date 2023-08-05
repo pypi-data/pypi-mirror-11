fe76da26ffd4495b39bf09053dbb4625517c9fc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:26 +0000

    Add package dj-utils/0.1.81/dj-utils-0.1.81.zip

---
+++ packages/dj-utils/dj-utils-0.1.81.zip/dj-utils-0.1.81/dj_utils/validators.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+from __future__ import absolute_import
+import re
+from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
+from django.utils.translation import ugettext_lazy, ugettext as _
+from dj_utils import settings as u_settings
+
+
+def validate_email_domain(email):
+    """ Валідація email по чорному списку доменів. """
+    try:
+        domain = email.split('@', 1)[1]
+    except IndexError:
+        pass
+    else:
+        if domain.lower() in u_settings.EMAIL_DOMAIN_BLACK_LIST:
+            raise ValidationError(_(u'Email with domain "%s" is disallowed.') % domain)
+
+
+validate_phone_number12 = RegexValidator(  # Валідація номеру телефону в форматі +XX XXX XXXXXXX.
+    r'^\+[\d]{12}$',
+    ugettext_lazy('Phone number must be in +XX XXX XXXXXXX format.'),
+    'invalid'
+)
