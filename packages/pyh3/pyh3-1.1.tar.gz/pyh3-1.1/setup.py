0687ef2039f45e748f393b5d3cfd2ef7bfd7c4c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:19 +0000

    Add package pyh3/1.1/pyh3-1.1.tar.gz

---
+++ packages/pyh3/pyh3-1.1.tar.gz/pyh3-1.1/setup.py
@@ -0,0 +1,23 @@
+try:
+  from setuptools import setup, find_packages
+except ImportError:
+  import distribute_setup
+  distribute_setup.use_setuptools()
+  from setuptools import setup, find_packages
+
+setup(
+    name='pyh3',
+    version='1.1',
+    author='Songxiao Zhang, Adam Kelleher',
+    author_email='ee08b397@gmail.com, adam.kelleher@buzzfeed.com',
+    description='Pure Python implementation of h3 for visualizing LARGE graphs',
+    long_description='Implements O(|V|) graph layout in hyperbolic space for visualizing very large ( > 1M nodes ) graphs.',
+    keywords='walrus h3 graph visualization graphviz dot graphml network rendering render layout hyperbolic',
+    packages=['h3'],
+    install_requires=[
+        'python-igraph==0.7',
+        'numpy==1.9.2',
+        'matplotlib==1.4.3',
+    ],
+    url='http://www.github.com/buzzfeed/pyh3',
+)
