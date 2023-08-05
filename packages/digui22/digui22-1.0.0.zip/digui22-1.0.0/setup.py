3952b3b6a6be9a1e93b873b5b4111a31e2f66b15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:02 +0000

    Add package digui22/1.0.0/digui22-1.0.0.zip

---
+++ packages/digui22/digui22-1.0.0.zip/digui22-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name       = 'digui22',
+        
+version    = '1.0.0',
+        py_modules =  ['digui22'],
+        author     = 'xihuyu',
+        author_email = 'fenggaopan@hotmail.com',
+        url='http://www.xihuyu.org',
+        description = 'A simple printer of lists',
+      )
