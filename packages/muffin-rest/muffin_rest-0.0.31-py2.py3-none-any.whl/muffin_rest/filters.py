52a6b230032bf569891be3156be2652f1efa6fed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:04 +0000

    Add package muffin-rest/0.0.31/muffin_rest-0.0.31-py2.py3-none-any.whl

---
+++ packages/muffin-rest/muffin_rest-0.0.31-py2.py3-none-any.whl/muffin_rest/filters.py
@@ -0,0 +1,109 @@
+""" Support admin filters. """
+
+import operator
+import wtforms as wtf
+
+
+FILTER_PREFIX = 'mr-'
+
+
+class FilterDefault:
+
+    """ Default filters value. """
+
+    def __str__(self):
+        """ Nothing here. """
+        return ""
+
+
+class FilterForm(wtf.Form):
+
+    """ Store filters for resource. """
+
+    def process(self, collection, formdata=None, obj=None, data=None, **kwargs):
+        """ Filter collection. """
+        super(FilterForm, self).process(formdata, obj, data, **kwargs)
+        self.filters = {}
+        for field in self._fields.values():
+            collection, active = field.flt.filter(collection, self.data)
+            if active:
+                self.filters[field.flt.column_name] = field.data
+        return collection
+
+
+class Filter:
+
+    """ Implement filters. """
+
+    form_field = wtf.StringField
+    default = FilterDefault()
+    options = {}
+    operations = {
+        '==': operator.eq,
+        '!=': operator.ne,
+        '>=': operator.ge,
+        '<=': operator.le,
+        '>': operator.gt,
+        '<': operator.lt,
+    }
+
+    def __init__(self, column_name, filter_name=None, op='==', **options):
+        """ Store name and mode. """
+        self.column_name = column_name
+        self.filter_name = filter_name or column_name
+        self.options = options or self.options
+
+        try:
+            self.op = self.operations[op]
+        except KeyError:
+            raise ValueError('Unknown filters operation: %s' % op)
+
+    def bind(self, form):
+        """ Bind to filter's form. """
+        form_field = self.form_field(**self.options)
+        form_field = form._fields[self.filter_name] = form_field.bind(
+            form, self.filter_name, prefix=form._prefix)
+        form_field.flt = self
+
+    def filter(self, collection, data):
+        """ Load value and filter collection. """
+        value = self.value(data)
+        if value is self.default:
+            return collection, False
+        return self.apply(collection, value), True
+
+    def value(self, data):
+        """ Get value from data. """
+        value = data.get(self.filter_name, self.default)
+        return value or self.default
+
+    def apply(self, collection, value):
+        """ Filter collection. """
+        return [o for o in collection if self.op(getattr(o, self.column_name, None), value)]
+
+
+class DummyFilter(Filter):
+
+    """ Do nothing. """
+
+    def filter(self, collection, data):
+        """Do nothing."""
+        value = self.value(data)
+        return collection, value is not self.default
+
+
+# Filters for base primitives
+BoolFilter = type('BoolFilter', (Filter,), {'form_field': wtf.BooleanField})
+IntegerFilter = type('IntegerFilter', (Filter,), {'form_field': wtf.IntegerField})
+ChoiceFilter = type('ChoiceFilter', (Filter,), {'form_field': wtf.SelectField})
+
+
+def default_converter(handler, flt, fcls=Filter):
+    """ Convert column name to filter. """
+    if isinstance(flt, Filter):
+        return flt
+
+    if isinstance(flt, str):
+        flt = (flt,)
+
+    return fcls(*flt)
