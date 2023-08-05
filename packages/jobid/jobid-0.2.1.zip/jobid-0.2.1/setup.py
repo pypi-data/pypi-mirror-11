f031d4b69bcb8506a072d7697a30f9121f4e31b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:52 +0000

    Add package jobid/0.2.1/jobid-0.2.1.zip

---
+++ packages/jobid/jobid-0.2.1.zip/jobid-0.2.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='jobid',
+      version='0.2.1',
+      description='A python JOBID number generator',
+      url='https://github.com/deparkes/jobid',
+      author='deparkes',
+      author_email='deparkes@gmail.com',
+      license='GPL',
+      packages=['jobid'],
+      zip_safe=False)
