9351056406b0dd42360bf37d4bd5d7f6554bbfcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:19 +0000

    Add package carpentry-ci/0.2.40/carpentry-ci-0.2.40.tar.gz

---
+++ packages/carpentry-ci/carpentry-ci-0.2.40.tar.gz/carpentry-ci-0.2.40/carpentry/workers/pipelines.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+from __future__ import unicode_literals
+from lineup import Pipeline
+from carpentry.workers.steps import (
+    PrepareSSHKey,
+    PushKeyToGithub,
+    LocalRetrieve,
+    RunBuild,
+    CheckAndLoadBuildFile,
+    PrepareShellScript,
+    DockerDependencyStopper,
+    DockerDependencyRunner,
+)
+
+
+class RunBuilder(Pipeline):
+
+    """A very simple builder that just runs subprocesses in the
+    machine where carpentry is installed.
+    """
+    name = 'local-builder'
+    steps = [
+        PrepareSSHKey,
+        PushKeyToGithub,
+        LocalRetrieve,
+        CheckAndLoadBuildFile,
+        PrepareShellScript,
+        DockerDependencyRunner,
+        RunBuild,
+        DockerDependencyStopper,
+    ]
