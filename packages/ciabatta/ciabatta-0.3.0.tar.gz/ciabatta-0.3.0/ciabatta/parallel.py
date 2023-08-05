680a886a0a9cb47ea3ba5c0e898762db02977d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:47 +0000

    Add package ciabatta/0.3.0/ciabatta-0.3.0.tar.gz

---
+++ packages/ciabatta/ciabatta-0.3.0.tar.gz/ciabatta-0.3.0/ciabatta/parallel.py
@@ -0,0 +1,25 @@
+"""
+Functions to help with task-based parallel execution.
+"""
+from __future__ import print_function, division
+import multiprocessing as mp
+
+
+def run_func(func, args, parallel=False):
+    """
+    Run a function with a list of argument sets, either in serial or parallel.
+
+    Parameters
+    ----------
+    func: function
+        The function to run
+    args:
+        A list of objects, where each entry `a` can be used like `func(a)`
+    parallel: bool
+        Whether to run the tasks in parallel.
+    """
+    if parallel:
+        mp.Pool(mp.cpu_count() - 1).map(func, args)
+    else:
+        for arg in args:
+            func(arg)
