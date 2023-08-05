a8e251392a4291bb93fea5dfd8622c4e7be8c974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:41 +0000

    Add package djangocms-blog/0.5.0/djangocms_blog-0.5.0-py2.py3-none-any.whl

---
+++ packages/djangocms-blog/djangocms_blog-0.5.0-py2.py3-none-any.whl/djangocms_blog/cms_app.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from cms.app_base import CMSApp
+from cms.apphook_pool import apphook_pool
+from django.utils.translation import ugettext_lazy as _
+
+from .menu import BlogCategoryMenu
+
+
+class BlogApp(CMSApp):
+    name = _('Blog')
+    urls = ['djangocms_blog.urls']
+    app_name = 'djangocms_blog'
+    menus = [BlogCategoryMenu]
+
+apphook_pool.register(BlogApp)
