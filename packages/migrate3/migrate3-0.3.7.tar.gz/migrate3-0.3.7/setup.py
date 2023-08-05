d550d45873d0dc74c812329a0fe446cf5b945b1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:36 +0000

    Add package migrate3/0.3.7/migrate3-0.3.7.tar.gz

---
+++ packages/migrate3/migrate3-0.3.7.tar.gz/migrate3-0.3.7/setup.py
@@ -0,0 +1,57 @@
+'''
+Language agnostic database migration tool
+
+migrate
+=======
+
+A simple language agnostic database migration tool
+
+install
+-------
+::
+
+    $ pip install migrate
+
+usage
+-----
+::
+
+    usage: migrate [options] <command>
+
+See Readme_
+
+.. _Readme: https://github.com/migrate/migrate/blob/master/README.md
+'''
+
+import migrate3
+from setuptools import setup
+
+setup(
+    name='migrate3',
+    version=migrate3.__version__,
+    license='MIT',
+    author='Francis Asante',
+    author_email='kofrasa@gmail.com',
+    url='https://github.com/mylokin/migrate',
+    description='A simple language agnostic database migration tool',
+    long_description=__doc__,
+    py_modules=['migrate3'],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Environment :: Console',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Database',
+        'Topic :: Utilities'
+    ],
+    entry_points={
+        'console_scripts': ['migrate=migrate3:main']
+    },
+    test_suite = 'test_migrate',
+)
