c8560edcfb6451c82eff234a81cdf4b354675bc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:54 +0000

    Add package ipysh/0.2/ipysh-0.2.tar.gz

---
+++ packages/ipysh/ipysh-0.2.tar.gz/ipysh-0.2/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ipysh',
+      version='0.2',
+      description='UNIX commandline for Python',
+      author="Jason Kingsbury",
+      author_email="jason@relva.co.uk",
+      url="https://bitbucket.org/jrelva/pysh",
+      download_url="https://bitbucket.org/jrelva/pysh/get/0.2.tar.gz",
+      keywords=['shell', 'commandline', 'pipe', 'chain', 'tool', 'sh'],
+      packages=["ipysh"]
+      )
+
