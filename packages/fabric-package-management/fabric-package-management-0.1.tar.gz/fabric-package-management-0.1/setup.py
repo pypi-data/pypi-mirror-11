bbcb3f4abf657786d7150bc7ed3626bf915d8d94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:54 +0000

    Add package fabric-package-management/0.1/fabric-package-management-0.1.tar.gz

---
+++ packages/fabric-package-management/fabric-package-management-0.1.tar.gz/fabric-package-management-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='fabric-package-management',
+    version='0.1',
+    description='A collection of fabric tasks for package management',
+    long_description=long_description,
+    url='https://github.com/andrewsomething/fabric-package-management',
+    download_url='https://github.com/andrewsomething/fabric-package-management/releases',
+    license='MIT License (Expat)',
+
+    author='Andrew Starr-Bochicchio',
+    author_email='a.starr.b@gmail.com',
+
+    packages=find_packages(),
+    install_requires=['fabric']
+)
