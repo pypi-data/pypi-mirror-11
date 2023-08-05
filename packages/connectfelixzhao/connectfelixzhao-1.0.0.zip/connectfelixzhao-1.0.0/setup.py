89d3875e8207267ca73c52b363e3e92130e89073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:00 +0000

    Add package connectfelixzhao/1.0.0/connectfelixzhao-1.0.0.zip

---
+++ packages/connectfelixzhao/connectfelixzhao-1.0.0.zip/connectfelixzhao-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='connectfelixzhao',
+      version='1.0.0',
+      author='zhaofei',
+      author_email='felix.zhao@nokia.com',
+      url='',
+      description='connect to NE use telnet',
+      )
