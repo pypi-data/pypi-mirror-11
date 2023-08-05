f4bca414e3451f1643da8ddfdf5d8e295e3218bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:53 +0000

    Add package shardcache_client/1.0/shardcache_client-1.0.tar.gz

---
+++ packages/shardcache_client/shardcache_client-1.0.tar.gz/shardcache_client-1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name             = 'shardcache_client',
+    version          = '1.0',
+    description      = 'Pure python library for shardcache client',
+    url              = 'https://github.com/Shardcached/libshardcache',
+    author           = 'Andrea Guzzo',
+    author_email     = 'xant@xant.net',
+    maintainer       = 'Antonio Malara',
+    maintainer_email = 'antonio.malara@booking.com',
+    packages         = ('shardcache_client',)
+)
+
