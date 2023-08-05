ac7f28719d19f835b2267bb4670c0b3868eb3474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:11 +0000

    Add package ebzl/0.8/ebzl-0.8-py2-none-any.whl

---
+++ packages/ebzl/ebzl-0.8-py2-none-any.whl/ebzl/modules/help.py
@@ -0,0 +1,58 @@
+import argparse
+
+from .. modules import (
+    bundle,
+    create,
+    deploy,
+    ecs,
+    env,
+    instances,
+    list as list_,
+    version
+)
+
+
+def get_module_list():
+    return [
+        "bundle",
+        "create",
+        "deploy",
+        "ecs",
+        "env",
+        "instances",
+        "list",
+        "version"
+    ]
+
+
+def get_module(name):
+    d = {
+        "bundle": bundle,
+        "create": create,
+        "deploy": deploy,
+        "ecs": ecs,
+        "env": env,
+        "instances": instances,
+        "list": list_,
+        "version": version
+    }
+
+    return d.get(name)
+
+
+def get_argument_parser():
+    parser = argparse.ArgumentParser("ebzl help")
+
+    parser.add_argument("action",
+                        help="Ebzl module.")
+
+    return parser
+
+
+def run(argv):
+    args = get_argument_parser().parse_args(argv)
+    try:
+        get_module(args.action).get_argument_parser().print_help()
+    except AttributeError:
+        print("Module not found: %s" % args.action)
+        exit()
