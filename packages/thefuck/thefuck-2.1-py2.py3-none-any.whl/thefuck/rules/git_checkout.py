4c9c81340243a3b09a36ad6cd5b1ec30d256ffb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:04 +0000

    Add package thefuck/2.1/thefuck-2.1-py2.py3-none-any.whl

---
+++ packages/thefuck/thefuck-2.1-py2.py3-none-any.whl/thefuck/rules/git_checkout.py
@@ -0,0 +1,37 @@
+import re
+import subprocess
+from thefuck import shells, utils
+
+
+@utils.git_support
+def match(command, settings):
+    return ('git' in command.script
+            and 'did not match any file(s) known to git.' in command.stderr
+            and "Did you forget to 'git add'?" not in command.stderr)
+
+
+def get_branches():
+    proc = subprocess.Popen(
+        ['git', 'branch', '-a', '--no-color', '--no-column'],
+        stdout=subprocess.PIPE)
+    for line in proc.stdout.readlines():
+        line = line.decode('utf-8')
+        if line.startswith('*'):
+            line = line.split(' ')[1]
+        if '/' in line:
+            line = line.split('/')[-1]
+        yield line.strip()
+
+
+@utils.git_support
+def get_new_command(command, settings):
+    missing_file = re.findall(
+        r"error: pathspec '([^']*)' "
+        "did not match any file\(s\) known to git.", command.stderr)[0]
+    closest_branch = utils.get_closest(missing_file, get_branches(),
+                                       fallback_to_first=False)
+    if closest_branch:
+        return command.script.replace(missing_file, closest_branch, 1)
+    else:
+        return shells.and_('git branch {}', '{}').format(
+            missing_file, command.script)
