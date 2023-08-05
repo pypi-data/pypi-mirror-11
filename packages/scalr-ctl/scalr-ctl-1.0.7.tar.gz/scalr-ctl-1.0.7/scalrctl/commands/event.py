c7879a9412000178db0abe2c32f24787501f9e6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:37 +0000

    Add package scalr-ctl/1.0.7/scalr-ctl-1.0.7.tar.gz

---
+++ packages/scalr-ctl/scalr-ctl-1.0.7.tar.gz/scalr-ctl-1.0.7/scalrctl/commands/event.py
@@ -0,0 +1,57 @@
+__author__ = 'Dmitriy Korsakov'
+__doc__ = 'Manage events available in the Scalr Account'
+
+
+from scalrctl import commands
+
+
+name = "account-level-event"
+enabled = True
+
+
+def callback(*args, **kwargs):
+    """
+    print('in event module')
+    print(args)
+    print(kwargs)
+    """
+    pass
+
+
+class Event(commands.SubCommand):
+    pass
+
+
+class ListEvents(Event):
+    name = "list"
+    route = "/events/"
+    method = "get"
+    enabled = True
+
+
+class RetrieveEvent(Event):
+    name = "retrieve"
+    route = "/events/{eventId}/"
+    method = "get"
+    enabled = True
+
+
+class CreateEvent(Event):
+    name = "create"
+    route = "/events/"
+    method = "post"
+    enabled = True
+
+
+class ChangeEventAttrs(Event):
+    name = "change-attributes"
+    route = "/events/{eventId}/"
+    method = "patch"
+    enabled = True
+
+
+class DeleteEvent(Event):
+    name = "delete"
+    route = "/events/{eventId}/"
+    method = "delete"
+    enabled = True
