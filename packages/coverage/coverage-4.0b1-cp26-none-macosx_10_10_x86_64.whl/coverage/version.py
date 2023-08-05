23f105f14a683252e2c7675c59fccc187abf2ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:19 +0000

    Add package coverage/4.0b1/coverage-4.0b1-cp26-none-macosx_10_10_x86_64.whl

---
+++ packages/coverage/coverage-4.0b1-cp26-none-macosx_10_10_x86_64.whl/coverage/version.py
@@ -0,0 +1,12 @@
+# Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
+# For details: https://bitbucket.org/ned/coveragepy/src/default/NOTICE.txt
+
+"""The version and URL for coverage.py"""
+# This file is exec'ed in setup.py, don't import anything!
+
+__version__ = "4.0b1"                   # see detailed history in CHANGES.txt
+
+__url__ = "https://coverage.readthedocs.org"
+if max(__version__).isalpha():
+    # For pre-releases, use a version-specific URL.
+    __url__ += "/en/coverage-" + __version__
