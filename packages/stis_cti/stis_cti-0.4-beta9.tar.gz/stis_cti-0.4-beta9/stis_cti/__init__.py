ac6871ea4848bdb4eb2b98b39fccdca7331a9c94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:34 +0000

    Add package stis_cti/0.4-beta9/stis_cti-0.4-beta9.tar.gz

---
+++ packages/stis_cti/stis_cti-0.4-beta9.tar.gz/stis_cti-0.4-beta9/stis_cti/__init__.py
@@ -0,0 +1,11 @@
+'''
+Usage:
+import stis_cti
+stis_cti.stis_cti(science_dir, dark_dir, ref_dir, num_processes, pctetab=None, 
+                  all_weeks_flag=False, allow=False, clean=False, clean_all=False, verbose=1)
+'''
+
+from stis_cti import stis_cti, __version__, __author__
+from stis_cti import viable_ccd_file
+import StisPixCteCorr
+from archive_dark_query import archive_dark_query
