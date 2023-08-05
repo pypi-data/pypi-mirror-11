3edcf6e998086e49f4156fa93c1a79a0eaaa9d6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:21 +0000

    Add package pygeoprocessing/0.3.0a5/pygeoprocessing-0.3.0a5-cp27-none-win32.whl

---
+++ packages/pygeoprocessing/pygeoprocessing-0.3.0a5-cp27-none-win32.whl/pygeoprocessing/__init__.py
@@ -0,0 +1,28 @@
+"""__init__ module for pygeprocessing, imports all the geoprocessing functions
+    into the pygeoprocessing namespace"""
+
+import natcap.versioner
+__version__ = natcap.versioner.get_version('pygeoprocessing')
+
+import os
+
+import unittest
+import logging
+import types
+
+import pygeoprocessing.geoprocessing as geoprocessing
+from geoprocessing import *
+
+__all__ = []
+for attrname in dir(geoprocessing):
+    if type(getattr(geoprocessing, attrname)) is types.FunctionType:
+        __all__.append(attrname)
+
+LOGGER = logging.getLogger('pygeoprocessing')
+LOGGER.setLevel(logging.DEBUG)
+
+def test():
+    """run modulewide tests"""
+    LOGGER.info('running tests on %s', os.path.dirname(__file__))
+    suite = unittest.TestLoader().discover(os.path.dirname(__file__))
+    unittest.TextTestRunner(verbosity=2).run(suite)
