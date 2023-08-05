7a630eae46ba464ab975b934a56bcd7c1e7981c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:37 +0000

    Add package nose-printlog/0.1.1/nose-printlog-0.1.1.tar.gz

---
+++ packages/nose-printlog/nose-printlog-0.1.1.tar.gz/nose-printlog-0.1.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+try:
+    desc = open('README.rst').read()
+except:
+    desc = 'see README.rst'
+
+setup(
+    name='nose-printlog',
+    version='0.1.1',
+    description='Print log to console in nose tests',
+    long_description=desc,
+    author='Fumihiro Bessho',
+    author_email='fumihiro.bessho@gmail.com',
+    url='http://github.com/fbessho/nose-printlog/',
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['nose'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    entry_points={
+        'nose.plugins': ['printlog = noseprintlog.noseprintlog:PrintLog']
+    },
+)
