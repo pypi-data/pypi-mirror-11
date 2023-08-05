6775a7d4080b7c7bb0c89ca366871e7a07b2df08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:50 +0000

    Add package TAGsieve/1.1/TAGsieve-1.1-py2-none-any.whl

---
+++ packages/TAGsieve/TAGsieve-1.1-py2-none-any.whl/TAGsieve-1.1.dist-info/DESCRIPTION.rst
@@ -0,0 +1,37 @@
+========
+TAGsieve
+========
+
+TAGsieve is a small HTML/XML stripper GUI application, written in Python and based on the HTML sanitizer bleach_. It strips or batch strips tags from .html, .htm, or .xml files, ignoring specified tags and attributes through whitelisting.
+
+Basic use
+=========
+TAGsieve is a simple GUI application that strips tags of a single file or a directory of files.
+
+It follows bleach_ and works with tag, attribute, and style whitelists: these tags, attributes, or styles will not be stripped. Click the "Clean" button to clean a file or directory of files.
+
+.. image:: tagsievegui.png
+	:height: 200
+	:width: 200
+	:scale: 50%
+
+Installation
+============
+Install Python on your machine, using the Anaconda_ distribution. This includes the PyQt package needed for the GUI.
+
+After installing the distribution, check if PyQt is there::
+
+	$ conda install pyqt
+
+Then run::
+
+	$ pip install TAGsieve
+
+This should install the requirements used for TAGsieve. Start the program in terminal by running::
+
+	$ python -m TAGsieve
+
+.. _bleach: https://github.com/jsocol/bleach
+.. _Anaconda: http://continuum.io/downloads
+
+
---
+++ packages/TAGsieve/TAGsieve-1.1-py2-none-any.whl/TAGsieve/CleanDirtySoup.py
@@ -0,0 +1,55 @@
+from bs4 import BeautifulSoup
+import bleach
+import sys, os
+
+class CleanDirtySoup(object):
+	'''
+	Class that removes HTML/XML tags based on the
+	Python modules BeautifulSoup and bleach.
+	The class ignores a set of allowed tags and attributes,
+	and writing the results into a new '_cleaned' file.
+	'''
+
+	unclean_file = ''
+	new_file = ''
+
+	allowed_tags = []
+	allowed_attrs = {}
+	allowed_styles = []
+
+	def __init__(self, unclean_file, allowed_tags, allowed_attrs, allowed_styles):
+		self.unclean_file = unclean_file
+		self.allowed_tags = allowed_tags
+		self.allowed_attrs = allowed_attrs
+		self.allowed_styles = allowed_styles
+
+		uncleanFile, uncleanExtension = os.path.splitext(unclean_file)
+
+		self.new_file = uncleanFile+'_cleaned'+uncleanExtension
+
+		self.make_cleansoup()
+		
+	def clean_dirtysoup(self):
+		'''
+		Sends the unclean file to BeautifulSoup parser.
+			The parsing unescapes HTML ascii characters.
+
+		The parsed HTML is then sent to bleach for cleaning. Unescaped HTML is correctly ignored.
+			The result is then re-encoded,
+			replacing unescaped ascii characters with their raw HTML string variants.
+			e.g. &ldquo; --> &#8220;
+		'''
+		dirtysoup = BeautifulSoup(open(self.unclean_file).read())
+
+		clean_soup =  bleach.clean(dirtysoup, self.allowed_tags, self.allowed_attrs, self.allowed_styles, strip=True)
+		clean_soup = clean_soup.encode('ascii', 'xmlcharrefreplace')
+
+		return clean_soup
+
+	def make_cleansoup(self):
+		'''Write the clean, non-ascii tree in the new file.'''
+		f = open(self.new_file, 'w')
+		f.write(self.clean_dirtysoup())
+
+		sys.stdout.write('Finished cleaning: ' + f.name + '\n')
+		f.close()
