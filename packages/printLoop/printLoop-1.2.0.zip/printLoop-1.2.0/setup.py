a674b40fbfe7ee122b638c1632b572dd0d7fa2aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:13 +0000

    Add package printLoop/1.2.0/printLoop-1.2.0.zip

---
+++ packages/printLoop/printLoop-1.2.0.zip/printLoop-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'printLoop',
+    version = '1.2.0',
+    py_modules = ['printLoop'],
+    author = 'jonnylee719',
+    author_email = 'jonnylee719@gmail.com',
+    url = 'http://www.google.com',
+    description = 'This def function prints all items in all lists.',
+    )
