c360a32b0ac561889cf28540c64b52b1ecff40be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:09 +0000

    Add package datascience/0.2.0/datascience-0.2.0.tar.gz

---
+++ packages/datascience/datascience-0.2.0.tar.gz/datascience-0.2.0/setup.py
@@ -0,0 +1,48 @@
+import sys
+from distutils.core import setup
+from setuptools.command.test import test as TestCommand
+
+install_requires = [
+  'numpy',
+  'matplotlib',
+  'pandas'
+]
+
+test_requires = [
+  'pytest'
+]
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+setup(
+  name = 'datascience',
+  py_modules = ['datascience'],
+  version = '0.2.0',
+  install_requires = install_requires,
+  tests_require = test_requires,
+  cmdclass = {'test': PyTest},
+  description = 'A Python library for introductory data science',
+  author = 'John DeNero, David Culler, Alvin Wan',
+  author_email = 'ds-instr@berkeley.edu',
+  url = 'https://github.com/dsten/datascience',
+  download_url = 'https://github.com/dsten/datascience/archive/0.2.0.zip',
+  keywords = ['data', 'tools', 'berkeley'],
+  classifiers = [],
+)
