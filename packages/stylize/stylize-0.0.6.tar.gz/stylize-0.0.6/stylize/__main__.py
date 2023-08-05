04e170d4e08e5492d850764f64a21ebeb68174e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:38 +0000

    Add package stylize/0.0.6/stylize-0.0.6.tar.gz

---
+++ packages/stylize/stylize-0.0.6.tar.gz/stylize-0.0.6/stylize/__main__.py
@@ -0,0 +1,116 @@
+from stylize import util
+from stylize.clang_formatter import ClangFormatter
+from stylize.yapf_formatter import YapfFormatter
+
+import argparse
+import fcntl
+import struct
+import subprocess
+import sys
+import termios
+import os
+
+
+def enumerate_all_files(exclude=[]):
+    for root, dirs, files in os.walk('.', topdown=True):
+        dirs[:] = [d for d in dirs
+                   if os.path.abspath(root + '/' + d) not in exclude]
+        for f in files:
+            yield root + '/' + f
+
+
+def enumerate_changed_files(exclude=[], diffbase="robojackets/master"):
+    p = subprocess.Popen(["git", "diff", "--name-only", diffbase],
+                         stdout=subprocess.PIPE)
+    for line in p.stdout:
+        filepath = line.rstrip().decode("utf-8")
+        if os.path.exists(filepath):
+            for excluded_dir in exclude:
+                if filepath.startswith(excluded_dir):
+                    continue
+            yield filepath
+
+
+def main():
+    file_scan_count = file_change_count = 0
+
+    # Command line options
+    parser = argparse.ArgumentParser(
+        description="Format and checkstyle C++ and Python code")
+    parser.add_argument(
+        "--check",
+        action='store_true',
+        help=
+        "Determine if all code is in accordance with the style configs, but don't fix them if they're not. An nonzero exit code indicates that some files don't meet the style requirements.")
+    parser.add_argument(
+        "--exclude_dirs",
+        type=str,
+        default=[],
+        nargs="+",
+        help="A list of directories to exclude")
+    parser.add_argument(
+        "--diffbase",
+        help="The git branch/tag/SHA1 to compare against.  If provided, only files that have changed since the diffbase will be scanned.")
+
+    formatters = [ClangFormatter(), YapfFormatter()]
+
+    # register any formatter-specific arguments
+    for formatter in formatters: formatter.add_args(parser)
+
+    # map file extension to formatter
+    formatter_map = {}
+    for f in formatters:
+        for ext in f.file_extensions:
+            formatter_map[ext] = f
+
+    ARGS = parser.parse_args()
+
+    ARGS.exclude_dirs = [os.path.abspath(p) for p in ARGS.exclude_dirs]
+
+    # Print initial status info
+    verb = "Checkstyling" if ARGS.check else "Formatting"
+    if ARGS.diffbase:
+        print("%s files that differ from %s..." % (verb, ARGS.diffbase))
+        files_to_format = enumerate_changed_files(ARGS.exclude_dirs,
+                                                  ARGS.diffbase)
+    else:
+        print("%s all c++ and python files in the project..." % verb)
+        files_to_format = enumerate_all_files(ARGS.exclude_dirs)
+
+
+    def process_file(filepath):
+        nonlocal file_scan_count
+        nonlocal file_change_count
+        nonlocal ARGS
+
+        _, ext = os.path.splitext(filepath)
+        if ext not in formatter_map:
+            return
+        formatter = formatter_map[ext]
+
+        needed_formatting = formatter.run(ARGS, filepath, ARGS.check)
+
+        file_scan_count += 1
+        if needed_formatting:
+            file_change_count += 1
+
+            suffix = "✗" if ARGS.check else "✔"
+            util.print_justified(filepath, suffix)
+        else:
+            util.print_justified("> %s: %s" % (ext[1:], filepath),
+                                 "[%d]" % file_scan_count, end="\r")
+
+    # Use all the cores!
+    from multiprocessing.pool import ThreadPool
+    workers = ThreadPool()
+    workers.map(process_file, files_to_format)
+
+    # Print final stats
+    if ARGS.check:
+        util.print_justified(
+            "[%d / %d] files need formatting" % (file_change_count, file_scan_count), "")
+        sys.exit(file_change_count)
+    else:
+        util.print_justified(
+            "[%d / %d] files formatted" % (file_change_count, file_scan_count), "")
+        sys.exit(0)
---
+++ packages/stylize/stylize-0.0.6.tar.gz/stylize-0.0.6/stylize/clang_formatter.py
@@ -0,0 +1,37 @@
+from stylize.formatter import Formatter
+from stylize.util import file_md5
+
+import os
+import subprocess
+
+
+class ClangFormatter(Formatter):
+    def __init__(self):
+        self.file_extensions = [".c", ".h", ".cpp", ".hpp"]
+
+    def add_args(self, argparser):
+        argparser.add_argument(
+        "--clang_style",
+        type=str,
+        default=None,
+        help="The style to pass to clang-format.  See `clang-format --help` for more info.")
+
+    def run(self, args, filepath, check=False):
+        logfile = open("/dev/null", "w")
+        if check:
+            style_arg = "-style=%s" % args.clang_style if args.clang_style != None else ""
+            return os.system(
+                "clang-format %s -output-replacements-xml %s | grep '<replacement ' > /dev/null 2>&1"
+                % (style_arg, filepath)) == 0
+        else:
+            md5_before = file_md5(filepath)
+            popen_args = ["clang-format", "-i"]
+            if args.clang_style:
+                popen_args.append("-style=%s" % args.clang_style)
+            args.append(filepath)
+            proc = subprocess.Popen(popen_args,
+                                    stdout=logfile,
+                                    stderr=logfile)
+            proc.communicate()
+            md5_after = file_md5(filepath)
+            return md5_before != md5_after
