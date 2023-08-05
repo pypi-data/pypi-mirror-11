f8724560b92e9ad3983ea45b27dca1fc2df694fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:32 +0000

    Add package pyfifo/0.8.0/pyfifo-0.8.0.tar.gz

---
+++ packages/pyfifo/pyfifo-0.8.0.tar.gz/pyfifo-0.8.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyfifo',
+    version='0.8.0',
+    description='A simple and reasonably efficient FIFO',
+    author='Byron Platt',
+    author_email='byron.platt@gmail.com',
+    license='GPL2',
+    url='https://github.com/greenbender/python-fifo',
+    packages=['fifo'],
+)
