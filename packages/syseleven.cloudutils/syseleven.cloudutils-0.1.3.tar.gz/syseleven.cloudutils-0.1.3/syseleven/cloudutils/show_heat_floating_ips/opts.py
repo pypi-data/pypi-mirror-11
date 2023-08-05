d695d34a46f9b7176cc4fcb7bb62ad7a2f565a33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:18 +0000

    Add package syseleven.cloudutils/0.1.3/syseleven.cloudutils-0.1.3.tar.gz

---
+++ packages/syseleven.cloudutils/syseleven.cloudutils-0.1.3.tar.gz/syseleven.cloudutils-0.1.3/syseleven/cloudutils/show_heat_floating_ips/opts.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import ConfigParser
+
+try:
+    from oslo.config import cfg
+except ImportError:
+    from oslo_config import cfg
+
+def init_params():
+    """ this functions load all parameter either from
+        command-line OR config-file/config-dir """
+
+    params = [
+        cfg.StrOpt('stack',
+                    positional=True,
+                    help='Name of the stack'
+                    ),
+        cfg.BoolOpt('debug',
+                    short='d',
+                    help='Enable debug'
+                    ),
+    ]
+
+    cfg.CONF.register_cli_opts(params)
+    cfg.CONF.register_opts(params)
+
+    cfg.CONF(sys.argv[1:],
+            project='show_heat_floating_ips',
+            prog='show_heat_floating_ips',
+            )
