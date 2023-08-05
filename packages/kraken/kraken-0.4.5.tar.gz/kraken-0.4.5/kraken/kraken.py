4c0c110c45a9efeb906469d4ea2dc783e5c0c8dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:42 +0000

    Add package kraken/0.4.5/kraken-0.4.5.tar.gz

---
+++ packages/kraken/kraken-0.4.5.tar.gz/kraken-0.4.5/kraken/kraken.py
@@ -0,0 +1,172 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+from future import standard_library
+standard_library.install_aliases()
+from builtins import str
+
+import click
+import csv
+import os
+import urllib.request, urllib.error, urllib.parse
+
+from PIL import Image
+from urllib.parse import urljoin
+
+from kraken import binarization
+from kraken import pageseg
+from kraken import rpred
+from kraken import html
+from kraken.lib import models
+
+APP_NAME = 'kraken'
+MODEL_URL = 'http://l.unchti.me/'
+DEFAULT_MODEL = 'en-default.pyrnn.hdf5'
+LEGACY_MODEL_DIR = '/usr/local/share/ocropus'
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command('binarize')
+@click.option('--threshold', default=0.5, type=click.FLOAT)
+@click.option('--zoom', default=0.5, type=click.FLOAT)
+@click.option('--escale', default=1.0, type=click.FLOAT)
+@click.option('--border', default=0.1, type=click.FLOAT)
+@click.option('--perc', default=80, type=click.IntRange(1, 100))
+@click.option('--range', default=20, type=click.INT)
+@click.option('--low', default=5, type=click.IntRange(1, 100))
+@click.option('--high', default=90, type=click.IntRange(1, 100))
+@click.argument('input', type=click.File(mode='rb'))
+@click.argument('output', type=click.File(mode='wb'))
+@click.pass_context
+def binarize(ctx, threshold, zoom, escale, border, perc, range, low, high,
+             input, output):
+    im = Image.open(input)
+    res = binarization.nlbin(im, threshold, zoom, escale, border, perc, range,
+                             low, high)
+    res.save(output)
+
+
+@click.command('segment')
+@click.option('--scale', default=None, type=click.FLOAT)
+@click.option('-b/-w', '--black_colseps/--white_colseps', default=False)
+@click.argument('input', type=click.File(mode='rb'))
+@click.argument('output', type=click.File(mode='w', encoding='utf-8'), required=False)
+@click.pass_context
+def segment(ctx, scale, black_colseps, input, output):
+    try:
+        im = Image.open(input)
+    except IOError as e:
+        click.BadParameter(e.message)
+    res = pageseg.segment(im, scale, black_colseps)
+    for box in res:
+        click.echo(u','.join([str(c) for c in box]) + u'\n', file=output,
+                   nl=False)
+
+
+@click.command('ocr')
+@click.pass_context
+@click.option('-m', '--model', default=DEFAULT_MODEL, help='Path to an '
+              'recognition model')
+@click.option('-p', '--pad', type=click.INT, default=16, help='Left and right '
+              'padding around lines')
+@click.option('-h/-t', '--hocr/--text', default=False, help='Switch between '
+              'hOCR and plain text output')
+@click.option('-l', '--lines', type=click.File(mode='r', encoding='utf-8'),
+              required=True, help='JSON file containing line coordinates')
+@click.option('--enable-autoconversion/--disable-autoconversion', 'conv',
+              default=True, help='Automatically convert pyrnn models zu HDF5')
+@click.argument('input', type=click.File(mode='rb'))
+@click.argument('output', type=click.File(mode='w', encoding='utf-8'),
+                required=False)
+def ocr(ctx, model, pad, hocr, lines, conv, input, output):
+    # first we try to find the model in the absolue path, then ~/.kraken, then
+    # LEGACY_MODEL_DIR
+    search = [model,
+              os.path.join(click.get_app_dir(APP_NAME, force_posix=True), model),
+              os.path.join(LEGACY_MODEL_DIR, model)]
+    # if automatic conversion is enabled we look for an converted model in
+    # ~/.kraken
+    if conv:
+        search.insert(0, os.path.join(click.get_app_dir(APP_NAME,
+                       force_posix=True),
+                       os.path.basename(os.path.splitext(model)[0]) +
+                       '.hdf5'))
+    location = None
+    for loc in search:
+        if os.path.isfile(loc):
+            location = loc
+            break
+    if not location:
+        raise click.BadParameter('No model found')
+    click.echo('Loading RNN\t', nl=False)
+    rnn = models.load_any(location)
+    if rnn:
+        click.secho(u'\u2713', fg='green')
+    else:
+        click.secho(u'\u2717', fg='red')
+        ctx.exit(1)
+    # convert input model to HDF5
+    if conv and rnn.kind == 'pyrnn':
+        name, _ = os.path.splitext(os.path.basename(model))
+        op = os.path.join(click.get_app_dir(APP_NAME, force_posix=True), name + '.hdf5')
+        try:
+            os.makedirs(click.get_app_dir(APP_NAME, force_posix=True))
+        except OSError:
+            pass
+        models.pyrnn_to_hdf5(rnn, op)
+    try:
+        im = Image.open(input)
+    except IOError as e:
+        click.BadParameter(e.message)
+    lc = len(lines.readlines())
+    lines.seek(0)
+    with click.progressbar(csv.reader(lines), lc, label='Reading line bounds',
+                           fill_char=click.style('#', fg='green'),) as b:
+        bounds = [(int(x1), int(y1), int(x2), int(y2)) for x1, y1, x2, y2 in b]
+
+    it = rpred.rpred(rnn, im, bounds, pad)
+    with click.progressbar(it, len(bounds),
+                           label='Recognizing lines',
+                           fill_char=click.style('#', fg='green')) as pred:
+        records = []
+        for rec in pred:
+            records.append(rec)
+        if hocr:
+            click.echo(html.hocr(records, input.name, im.size), file=output,
+                       nl=False)
+        else:
+            click.echo(u'\n')
+            click.echo(u'\n'.join([s.prediction for s in records]),
+                       file=output, nl=False)
+
+
+@click.command('download')
+def download():
+    default_model = urllib.request.urlopen(urljoin(MODEL_URL, DEFAULT_MODEL))
+    try:
+        os.makedirs(click.get_app_dir(APP_NAME, force_posix=True))
+    except OSError:
+        pass
+    # overwrite next function for iterator to return 8192 octets instead of
+    # line
+    default_model.next = lambda: default_model.read(8192)
+    fs = int(default_model.info()["Content-Length"])
+    with open(os.path.join(click.get_app_dir(APP_NAME, force_posix=True),
+                           DEFAULT_MODEL), 'wb') as fp:
+        with click.progressbar(default_model, fs // 256,
+                               label='Downloading default model',
+                               fill_char=click.style('#', fg='green')) as dl:
+            for buf in dl:
+                if not buf:
+                    raise StopIteration()
+                fp.write(buf)
+
+
+cli.add_command(binarize)
+cli.add_command(segment)
+cli.add_command(ocr)
+cli.add_command(download)
---
+++ packages/kraken/kraken-0.4.5.tar.gz/kraken-0.4.5/kraken/lib/models.py
@@ -0,0 +1,208 @@
+"""
+kraken.lib.models
+~~~~~~~~~~~~~~~~~
+
+Wraps around legacy pyrnn and HDF5 models to provide a single interface. In the
+future it will also include support for clstm models.
+"""
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+from future import standard_library
+standard_library.install_aliases()
+from future.utils import PY3
+from builtins import next
+from builtins import chr
+
+import h5py
+import numpy
+import gzip
+import bz2
+import sys
+
+import kraken.lib.lstm
+import kraken.lib.lineest
+
+from kraken.lib.lineest import CenterNormalizer
+from kraken.lib.exceptions import KrakenInvalidModelException
+
+def load_any(fname):
+    """
+    Loads anything that was, is, and will be a valid ocropus model and
+    instantiates a shiny new kraken.lib.lstm.SeqRecognizer from the RNN
+    configuration in the file.
+    
+    Currently it recognizes the following kinds of models:
+        * pyrnn models containing BIDILSTMs
+        * HDF5 models containing converted python BIDILSTMs
+
+    Additionally an attribute 'kind' will be added to the SeqRecognizer
+    containing a string representation of the source kind. Current known values
+    are:
+        * pyrnn for pickled BIDILSTMs
+        * hdf-pyrnn for HDF models converted from pickled objects
+
+    Args:
+        fname (unicode): Path to the model
+
+    Returns:
+        A kraken.lib.lstm.SeqRecognizer object.
+    """
+    seq = None
+    try:
+        seq = load_hdf5(fname)
+        seq.kind = 'hdf-pyrnn'
+        return seq
+    except:
+        try:
+            seq = load_pyrnn(fname)
+            seq.kind = 'pyrnn'
+            return seq
+        except:
+            pass
+
+
+def load_hdf5(fname, line_height=0):
+    """
+    Loads a model in HDF5 format and instantiates a
+    kraken.lib.lstm.SeqRecognizer object.
+
+    Args:
+        fname (unicode): Path to the HDF5 file
+        line_height (int): Target height of input. Will be extracted from model
+        if set to 0.
+
+    Returns:
+        A kraken.lib.lstm.SeqRecognizer object
+    """
+    known_hdf5 = [b'pyrnn-bidi']
+    with h5py.File(fname, 'r') as rnn:
+        try: 
+            if rnn.attrs['kind'] not in known_hdf5:
+                raise KrakenInvalidModelException(b'Unknown model kind ' +
+                                                  rnn.attrs['kind'])
+            # first extract the codec character set
+            charset = [chr(x) for x in rnn.get('codec')]
+            # code 0 is handled separately by the model
+            charset[0] = ''
+            codec = kraken.lib.lstm.Codec().init(charset)
+
+            # get number of states from shape of first array
+            hiddensize = rnn['.bidilstm.0.parallel.0.lstm.WGI'].shape[0]
+            if not line_height:
+                line_height = rnn['.bidilstm.0.parallel.0.lstm.WGI'].shape[1] - hiddensize - 1
+
+            # next build a line estimator
+            lnorm = kraken.lib.lineest.CenterNormalizer(line_height)
+            network = kraken.lib.lstm.SeqRecognizer(lnorm.target_height, 
+                                         hiddensize, 
+                                         codec = codec, 
+                                         normalize = kraken.lib.lstm.normalize_nfkc)
+            parallel, softmax = network.lstm.nets
+            nornet, rev = parallel.nets
+            revnet = rev.net
+            for w in ('WGI', 'WGF', 'WGO', 'WCI', 'WIP', 'WFP', 'WOP'):
+                setattr(nornet, w, rnn['.bidilstm.0.parallel.0.lstm.' +
+                        w][:].reshape(getattr(nornet, w).shape))
+                setattr(revnet, w, rnn['.bidilstm.0.parallel.1.reversed.0.lstm.' +
+                        w][:].reshape(getattr(nornet, w).shape))
+            softmax.W2 = numpy.hstack((rnn['.bidilstm.1.softmax.w'][:],
+                                       rnn['.bidilstm.1.softmax.W'][:]))
+        except (KeyError, TypeError):
+            raise KrakenInvalidModelException('Model incomplete')
+        network.lnorm = lnorm
+        return network
+
+def load_pyrnn(fname):
+    """
+    Loads a legacy RNN from a pickle file.
+
+    Args:
+        fname (unicode): Path to the pickle object
+
+    Returns:
+        Unpickled object
+
+    """
+    
+    if PY3:
+        raise KrakenInvalidModelException('Loading pickle models is not '
+                                          'supported on python 3')
+    import cPickle
+    def find_global(mname, cname):
+        aliases = {
+            'lstm.lstm': kraken.lib.lstm,
+            'ocrolib.lstm': kraken.lib.lstm,
+            'ocrolib.lineest': kraken.lib.lineest,
+        }
+        if mname in aliases:
+            return getattr(aliases[mname], cname)
+        return getattr(sys.modules[mname], cname)
+
+    of = open
+    if fname.endswith(u'.gz'):
+        of = gzip.open
+    elif fname.endswith(u'.bz2'):
+        of = bz2.BZ2File
+    with of(fname, 'rb') as fp:
+        unpickler = cPickle.Unpickler(fp)
+        unpickler.find_global = find_global
+        try:
+            rnn = unpickler.load()
+        except cPickle.UnpicklingError as e:
+            raise KrakenInvalidModelException(str(e))
+        if not isinstance(rnn, kraken.lib.lstm.SeqRecognizer):
+            raise KrakenInvalidModelException('Pickle is %s instead of '
+                                              'SeqRecognizer' %
+                                              type(rnn).__name__)
+        return rnn
+
+def pyrnn_to_hdf5(pyrnn=None, output='en-default.hdf5'):
+    """
+    Converts a legacy python RNN to the new HDF5 format. Benefits of the new
+    format include independence from particular python versions and no
+    arbitrary code execution issues inherent in pickle.
+
+    Args:
+        pyrnn (kraken.lib.lstm.SegRecognizer): pyrnn model
+        output (unicode): path of the converted HDF5 model
+    """
+
+    parallel, softmax = pyrnn.lstm.nets
+    fwdnet, revnet = parallel.nets
+    
+    with h5py.File(output, 'w') as nf:
+        # write metadata first
+        nf.attrs['kind'] = 'pyrnn-bidi'
+
+        for w in ('WGI', 'WGF', 'WGO', 'WCI'):
+            dset = nf.create_dataset(".bidilstm.0.parallel.0.lstm." + w,
+                                     getattr(fwdnet, w).shape, dtype='f')
+            dset[...] = getattr(fwdnet, w)
+            dset = nf.create_dataset(".bidilstm.0.parallel.1.reversed.0.lstm." + w,
+                                     getattr(revnet.net, w).shape, dtype='f')
+            dset[...] = getattr(revnet.net, w)
+
+        for w in ('WIP', 'WFP', 'WOP'):
+            data = getattr(fwdnet, w).reshape((-1, 1))
+            dset = nf.create_dataset(".bidilstm.0.parallel.0.lstm." + w,
+                                     data.shape, dtype='f')
+            dset[...] = data
+            
+            data = getattr(revnet.net, w).reshape((-1, 1))
+            dset = nf.create_dataset(".bidilstm.0.parallel.1.reversed.0.lstm." + w,
+                                     data.shape, dtype='f')
+            dset[...] = data
+
+        dset = nf.create_dataset(".bidilstm.1.softmax.w",
+                                 (softmax.W2[:,0].shape[0], 1), dtype='f')
+        dset[:] = softmax.W2[:,0].reshape((-1, 1))
+
+        dset = nf.create_dataset(".bidilstm.1.softmax.W", softmax.W2[:,1:].shape,
+                                 dtype='f')
+        dset[:] = softmax.W2[:,1:]
+        cvals = iter(pyrnn.codec.code2char.values())
+        next(cvals)
+        codec = numpy.array([0]+[ord(x) for x in cvals], dtype='f').reshape((-1, 1))
+        dset = nf.create_dataset("codec", codec.shape, dtype='f')
+        dset[:] = codec
