c3bf73b9b8fa1c37ae94dd3a4b093bcfc53027e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:18 +0000

    Add package pybomb/0.1.1/pybomb-0.1.1.tar.gz

---
+++ packages/pybomb/pybomb-0.1.1.tar.gz/pybomb-0.1.1/setup.py
@@ -0,0 +1,22 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'name': 'pybomb',
+    'description': 'Client for the Giant Bomb API',
+    'version': '0.1.1',
+    'author': 'Steve Hutchins',
+    'author_email': 'hutchinsteve@gmail.com',
+    'url': 'https://github.com/steveYeah/PyBomb',
+    'download_url': 'https://github.com/steveYeah/PyBomb/archive/v0.1.1.tar.gz',
+    'keywords': ['giant', 'bomb', 'game', 'api', 'client'],
+    'packages': ['pybomb', 'pybomb.clients'],
+    'install_requires': [
+        'nose',
+        'requests',
+    ]
+}
+
+setup(**config)
