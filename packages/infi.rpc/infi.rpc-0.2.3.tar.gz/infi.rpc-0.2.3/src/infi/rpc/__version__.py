f38b9fa94c943cba0a5aec23bb813361269af1d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:07 +0000

    Add package infi.rpc/0.2.3/infi.rpc-0.2.3.tar.gz

---
+++ packages/infi.rpc/infi.rpc-0.2.3.tar.gz/infi.rpc-0.2.3/src/infi/rpc/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.3"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = 'develop'
+__git_remote_tracking_branch__ = 'origin/develop'
+__git_remote_url__ = 'git@git.infinidat.com:host-opensource/infi.rpc.git'
+__git_head_hash__ = 'b91f5fd3dc0ca1e45e7d96bcdbaa51ce85a1c044'
+__git_head_subject__ = 'TRIVIAL adding support for zerorpc>=0.5.1'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
