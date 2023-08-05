9ca0ed9626ae3218473e4f2fefbac79a4093d833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:53 +0000

    Add package django-classy-settings/1.1.3/django-classy-settings-1.1.3.tar.gz

---
+++ packages/django-classy-settings/django-classy-settings-1.1.3.tar.gz/django-classy-settings-1.1.3/cbs/base/__init__.py
@@ -0,0 +1,29 @@
+
+import inspect
+import os
+
+
+class CoreSettings(object):
+    '''
+    Settings not likely to change between versions
+    '''
+
+    @property
+    def BASE_DIR(self):
+        return os.path.dirname(
+            os.path.dirname(
+                os.path.abspath(inspect.getfile(self.__class__))
+            )
+        )
+
+    DEBUG = True
+
+    ALLOWED_HOSTS = []
+
+    @property
+    def ROOT_URLCONF(self):
+        return '{}.urls'.format(self.PROJECT_NAME)
+
+    @property
+    def WSGI_APPLICATION(self):
+        return '{}.wsgi.application'.format(self.PROJECT_NAME)
