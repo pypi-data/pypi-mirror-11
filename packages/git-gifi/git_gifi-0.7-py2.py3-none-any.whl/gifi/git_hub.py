4b1b4478b93ae99881bbe7659187fd59a0f0d2d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:52 +0000

    Add package git-gifi/0.7/git_gifi-0.7-py2.py3-none-any.whl

---
+++ packages/git-gifi/git_gifi-0.7-py2.py3-none-any.whl/gifi/git_hub.py
@@ -0,0 +1,112 @@
+import getpass
+import logging
+
+from github import Github, GithubException
+from command import AggregatedCommand, Command, CommandException
+from github.MainClass import DEFAULT_BASE_URL
+from utils.configuration import Configuration, NOT_SET, configuration_command, REPOSITORY_CONFIG_LEVEL
+from utils.git_utils import get_repo, remote_origin_url, current_branch, get_from_last_commit_message
+import slack
+
+PULL_REQUEST_COMMIT_TAG = 'Pull request'
+
+
+def _authorize(config_level=REPOSITORY_CONFIG_LEVEL):
+    config = _configuration()
+    if config.login is NOT_SET:
+        config.configure(config_level, keys=['login'])
+    pw = getpass.getpass('Enter your Github password (will not be stored anywhere): ')
+    gh = Github(config.login, pw, base_url=_get_github_url())
+    _create_authorization(config, config_level, gh)
+
+
+def _create_authorization(config, config_level, gh):
+    try:
+        authorization = gh.get_user().create_authorization(scopes=['repo'], note='git-gifi')
+        config.set('access-token', authorization.token, config_level)
+    except GithubException as e:
+        _handle_github_exception(e, 'create an authorization')
+
+
+def _handle_github_exception(e, event):
+    logging.warn('%s raised an exception: %s' % (event, e))
+    if 'errors' in e.data:
+        error = ', '.join(map(_map_github_error, e.data['errors']))
+    else:
+        error = e.data['message']
+    raise CommandException('Unable to %s: %s' % (event, error))
+
+
+def _map_github_error(error):
+    if error['code'] == 'already_exists':
+        return 'Authorization already exists, copy paste existing token and use it in configure command.'
+    return error
+
+
+def get_github(repo):
+    config = _configuration(repo)
+    if config.login is NOT_SET:
+        raise missingConfigurationException('login')
+    if config.access_token is NOT_SET:
+        raise missingConfigurationException('access token')
+    return Github(config.access_token, base_url=(_get_github_url(repo)))
+
+
+def _get_github_url(repo=None):
+    origin_url = remote_origin_url(repo)
+    if 'github.com' not in origin_url:
+        return 'https://%s/api/v3' % origin_url.split('@')[1].split(':')[0]
+    else:
+        return DEFAULT_BASE_URL
+
+
+def request(base_branch=None, repo=None):
+    if base_branch is None:
+        raise CommandException('No base branch argument is given')
+
+    repo = get_repo(repo)
+
+    try:
+        pull = _create_pull_request(repo, base_branch)
+    except GithubException as e:
+        _handle_github_exception(e, 'create a pull request')
+
+    repo.git.commit('--amend', '-m', '%s\n\n%s: %s' % (repo.head.commit.message, PULL_REQUEST_COMMIT_TAG, pull.html_url))
+    print 'Pull request URL: %s' % pull.html_url
+
+    reviewers = get_from_last_commit_message(repo, 'Reviewers')
+    message = '%s Please review: %s' % (', '.join(map(lambda r: '<@%s>' % r, reviewers)), pull.html_url)
+    slack.notify(message)
+
+
+def _create_pull_request(repo, base_branch):
+    origin_url = remote_origin_url(repo)
+    full_repo_name = origin_url.split(':')[1].split('.')[0]
+    cur_branch = current_branch(repo)
+    if cur_branch is base_branch:
+        raise CommandException("Unable to create a pull request from '%s' which is basing on the same branch." % base_branch)
+    return get_github(repo).get_repo(full_repo_name).create_pull(
+        title=repo.head.commit.summary,
+        body=repo.head.commit.message,
+        head=cur_branch,
+        base=base_branch
+    )
+
+
+def missingConfigurationException(item):
+    return CommandException('No github %s is set, please do configure or authorize github first.' % item)
+
+
+def _configuration(repo=None):
+    repo = get_repo(repo)
+    return Configuration(repo, 'github', {
+        'login': (NOT_SET, 'Github login'),
+        'access-token': (NOT_SET, 'Github access token')
+    })
+
+
+command = AggregatedCommand('github', 'Integration with github.', [
+    Command('authorize', 'Create authorization and retrieve github access token.', _authorize),
+    Command('request', 'Creates a pull request from current branch.', request, '<base branch>'),
+    configuration_command(_configuration, 'Configure github settings.')
+])
---
+++ packages/git-gifi/git_gifi-0.7-py2.py3-none-any.whl/gifi/main.py
@@ -0,0 +1,81 @@
+import sys
+import logging
+
+from utils import git_utils
+from command import Command, AggregatedCommand, UnknownCommandException, CommandException
+import feature
+import pkg_resources
+import queue
+import git_hub
+import slack
+
+logging.basicConfig(filename='/tmp/gifi.log', level=logging.DEBUG)
+
+command = AggregatedCommand('gifi', 'Git and github enhancements to git.', [
+    feature.command,
+    queue.command,
+    git_hub.command,
+    slack.command,
+    Command('version', 'Show version number.', lambda: pkg_resources.require("git-gifi")[0].version)
+])
+
+
+class HelpGenerator(object):
+    def __init__(self, main):
+        self.main = main
+
+    def __call__(self):
+        help = str(self.main)
+        help += '\nUsage:\n\t%s command [command arguments]\n\nCommands:\n' % self.main.name
+
+        # it does not have to be recursive as there are only two levels
+        for command in self.main.nested_commands():
+            help += str(command)
+            if len(command.nested_commands()) != 0:
+                help += ' See below subcommands:\n'
+                for subcommand in command.nested_commands():
+                    help += '\t%s\n' % str(subcommand)
+            help += '\n'
+
+        return help
+
+
+command.add_command(Command('help', 'Display this window.', HelpGenerator(command)))
+
+
+class AliasesInstaller(object):
+    def __init__(self, main):
+        self.main = main
+
+    def __call__(self, config_level='global'):
+        repo = git_utils.get_repo()
+        config_writer = repo.config_writer(config_level)
+        # it does not have to be recursive as there are only two levels
+        for command in self.main.nested_commands():
+            if len(command.nested_commands()) != 0:
+                for subcommand in command.nested_commands():
+                    alias = '%s-%s' % (command.name, subcommand.name)
+                    config_writer.set_value('alias', alias, '"!%s %s %s"' % (sys.argv[0], command.name, subcommand.name))
+        config_writer.release()
+
+
+command.add_command(Command('install', 'Install gifi as git bunch of aliases.', AliasesInstaller(command)))
+
+
+def main():
+    args = list(sys.argv)
+    args.pop(0)
+    _main(args)
+
+
+def _main(args):
+    if len(args) == 0:
+        args.append('help')
+    try:
+        result = command(*args)
+        if result is not None:
+            print(result)
+    except UnknownCommandException:
+        print "Wrong command, try 'help'."
+    except CommandException as e:
+        print "ERROR: %s" % e
