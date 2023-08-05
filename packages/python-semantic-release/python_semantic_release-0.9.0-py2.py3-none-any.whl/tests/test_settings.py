1a28c76df2297078f4bfc884d1900955d549fe26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:01 +0000

    Add package python-semantic-release/0.9.0/python_semantic_release-0.9.0-py2.py3-none-any.whl

---
+++ packages/python-semantic-release/python_semantic_release-0.9.0-py2.py3-none-any.whl/tests/test_settings.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+
+from semantic_release.settings import _config
+
+from . import mock
+
+
+class ConfigTests(TestCase):
+
+    def test_config(self):
+        config = _config()
+        self.assertEqual(
+            config.get('semantic_release', 'version_variable'),
+            'semantic_release/__init__.py:__version__'
+        )
+
+    @mock.patch('semantic_release.settings.getcwd', return_value='/tmp/')
+    def test_defaults(self, mock_getcwd):
+        config = _config()
+        mock_getcwd.assert_called_once_with()
+        self.assertEqual(config.get('semantic_release', 'major_tag'), ':boom:')
+        self.assertEqual(config.get('semantic_release', 'minor_tag'), ':sparkles:')
+        self.assertEqual(config.get('semantic_release', 'patch_tag'), ':bug:')
+        self.assertFalse(config.getboolean('semantic_release', 'patch_without_tag'))
+        self.assertFalse(config.getboolean('semantic_release', 'check_build_status'))
+        self.assertEqual(config.get('semantic_release', 'hvcs'), 'github')
