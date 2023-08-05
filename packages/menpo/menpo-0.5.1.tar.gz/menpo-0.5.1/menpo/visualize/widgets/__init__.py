257cb4445d33dfc5e54de9b4e60964679558b705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:27 +0000

    Add package menpo/0.5.1/menpo-0.5.1.tar.gz

---
+++ packages/menpo/menpo-0.5.1.tar.gz/menpo-0.5.1/menpo/visualize/widgets/__init__.py
@@ -0,0 +1,7 @@
+from .base import (visualize_pointclouds, visualize_landmarkgroups,
+                   visualize_landmarks, visualize_images, plot_graph,
+                   save_matplotlib_figure, features_selection)
+from .options import (AnimationOptionsWidget, ChannelOptionsWidget,
+                      LandmarkOptionsWidget, TextPrintWidget,
+                      SaveFigureOptionsWidget, RendererOptionsWidget,
+                      FeatureOptionsWidget, GraphOptionsWidget)
