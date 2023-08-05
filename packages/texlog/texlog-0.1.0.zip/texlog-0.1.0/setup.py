a4cee7e2284e04fae0d0f723f61cb48e0393b275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:03 +0000

    Add package texlog/0.1.0/texlog-0.1.0.zip

---
+++ packages/texlog/texlog-0.1.0.zip/texlog-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='texlog',
+      version='0.1.0',
+      description='Log the wordcount of tex files',
+      url='https://github.com/deparkes/texlog',
+      author='deparkes',
+      author_email='deparkes@gmail.com',
+      license='GPL',
+      packages=['texlog'],
+      zip_safe=False)
