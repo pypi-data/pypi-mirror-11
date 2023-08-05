1ec818b713cd142960f82a5859284fce0adb769b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:28 +0000

    Add package pyrecommend/0.1.dev5/pyrecommend-0.1.dev5-py2-none-any.whl

---
+++ packages/pyrecommend/pyrecommend-0.1.dev5-py2-none-any.whl/pyrecommend/__init__.py
@@ -0,0 +1,7 @@
+# coding: utf8
+"""The pyrecommendation package."""
+# pylint: disable=unused-import
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+from .rec import calculate_similarity
