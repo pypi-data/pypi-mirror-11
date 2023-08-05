055fe270a6b83f529a4b3bb0a426ba24794b9945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:46 +0000

    Add package ehc-sdk/0.0.1/ehc-sdk-0.0.1.tar.gz

---
+++ packages/ehc-sdk/ehc-sdk-0.0.1.tar.gz/ehc-sdk-0.0.1/setup.py
@@ -0,0 +1,16 @@
+__author__ = 'yue'
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'ehc-sdk',
+    version = '0.0.1',
+    keywords = ('ehc', 'sdk'),
+    description = 'ehc python sdk',
+    license = 'ehc License',
+    install_requires = [],
+    author = 'jingyue',
+    author_email = 'jingyue@zetyun.com',
+    packages = find_packages(),
+    platforms = 'any',
+)
