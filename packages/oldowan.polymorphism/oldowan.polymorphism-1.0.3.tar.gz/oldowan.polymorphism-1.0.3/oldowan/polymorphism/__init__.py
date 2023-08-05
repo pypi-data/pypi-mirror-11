01fd95a51e3f0faa2430c84e6b62a7dc7a712935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:12 +0000

    Add package oldowan.polymorphism/1.0.3/oldowan.polymorphism-1.0.3.tar.gz

---
+++ packages/oldowan.polymorphism/oldowan.polymorphism-1.0.3.tar.gz/oldowan.polymorphism-1.0.3/oldowan/polymorphism/__init__.py
@@ -0,0 +1,19 @@
+"""This is the oldowan.polymorphism package."""
+
+import os
+
+VERSION = open(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+               'VERSION')).read().strip()
+
+__all__ = ['Polymorphism',
+           'PolymorphismRange',
+           'InfiniteRange']
+
+try:
+    from oldowan.polymorphism.polymorphism import Polymorphism
+    from oldowan.polymorphism.range import PolymorphismRange
+    from oldowan.polymorphism.range import InfiniteRange
+except:
+    from polymorphism import Polymorphism
+    from range import PolymorphismRange
+    from range import InfiniteRange
---
+++ packages/oldowan.polymorphism/oldowan.polymorphism-1.0.3.tar.gz/oldowan.polymorphism-1.0.3/oldowan/polymorphism/range.py
@@ -0,0 +1,28 @@
+from types import IntType
+
+
+class PolymorphismRange(object):
+
+    def __init__(self, *range_tuples):
+        self.range_tuples = range_tuples
+
+    def __iter__(self):
+        yield self
+
+    def __eq__(self, other):
+        position = other
+        if type(other) != IntType:
+            if hasattr(other, "position"):
+                position = other.position
+        for start, end in self.range_tuples:
+            if position >= start and position <= end:
+                return True
+        return False
+
+
+class InfiniteRange(PolymorphismRange):
+
+    def __eq__(self, other):
+        if type(other) != IntType and not hasattr(other, "position"):
+            raise TypeError
+        return True
