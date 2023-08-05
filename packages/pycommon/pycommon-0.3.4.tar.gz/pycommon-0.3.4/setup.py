5673fc26a68be883f010b8c9e4804e4e466ddfff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:16 +0000

    Add package pycommon/0.3.4/pycommon-0.3.4.tar.gz

---
+++ packages/pycommon/pycommon-0.3.4.tar.gz/pycommon-0.3.4/setup.py
@@ -0,0 +1,18 @@
+"""
+Setup script
+"""
+from distutils.core import setup
+
+VERSION = '0.3.4'
+setup(
+    name='pycommon',
+    packages=['pycommon'],
+    version=VERSION,
+    description='Useful functions missing from the standard libraries',
+    author='David Grant',
+    author_email='davidgrant@gmail.com',
+    url='https://github.com/dgrant/pycommon',
+    download_url='https://github.com/dgrant/pycommon/tarball/' + VERSION,
+    keywords=['utility', 'miscellaneous', 'library'],
+    classifiers=[],
+)
