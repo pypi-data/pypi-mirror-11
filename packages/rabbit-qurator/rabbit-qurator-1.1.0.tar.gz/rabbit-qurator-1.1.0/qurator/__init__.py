c57c1accd0ee8779426b8582c711a5fb4301f26d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:54 +0000

    Add package rabbit-qurator/1.1.0/rabbit-qurator-1.1.0.tar.gz

---
+++ packages/rabbit-qurator/rabbit-qurator-1.1.0.tar.gz/rabbit-qurator-1.1.0/qurator/__init__.py
@@ -0,0 +1,24 @@
+# main qurator module
+import os
+import logging
+import logging.config
+from logging import LoggerAdapter
+
+from .settings import LOGGING_CONFIG, DEPLOY_ENVIRONMENT
+
+
+def get_logger(name=None, config=None):
+    """Top level logger initialiser thingy.
+
+    :name: str identifying logger
+    :returns: logging logger object
+
+    """
+    if config is not None and name is not None:
+        LOGGING_CONFIG['loggers'][name] = config
+    logging.config.dictConfig(LOGGING_CONFIG)
+    logger = logging.getLogger(name)
+    adapter = LoggerAdapter(logger, {"deploy_environment": DEPLOY_ENVIRONMENT})
+    adapter.debug(
+        "Just created logger with config {!r}".format(LOGGING_CONFIG))
+    return adapter
