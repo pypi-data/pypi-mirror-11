ce9b5f2815560d5afa81fc4b242927bc7a256908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:39 +0000

    Add package jira-bulk-loader/0.2.6/jira-bulk-loader-0.2.6.tar.gz

---
+++ packages/jira-bulk-loader/jira-bulk-loader-0.2.6.tar.gz/jira-bulk-loader-0.2.6/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='jira-bulk-loader',
+    version='0.2.6',
+    packages=['jirabulkloader', 'jirabulkloader.test'],
+    author='Alexander Dudko',
+    author_email='alex.dudko@gmail.com',
+    license='GPLv3',
+    url='http://bitbucket.org/oktopuz/jira-bulk-loader',
+    scripts=['bin/jira-bulk-loader.py'],
+    description='Create tasks in Jira via RESTful API',
+    install_requires=[
+        "simplejson >= 3.3.0",
+        "requests >= 0.13.1",
+        "argparse >= 1.2.1",
+        "pytest >= 2.3.5",
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Information Technology',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+)
