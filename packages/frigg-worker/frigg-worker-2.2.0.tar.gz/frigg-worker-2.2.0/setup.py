888a023ffc9aa750e351ad126ee05348053ccbb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:46 +0000

    Add package frigg-worker/2.2.0/frigg-worker-2.2.0.tar.gz

---
+++ packages/frigg-worker/frigg-worker-2.2.0.tar.gz/frigg-worker-2.2.0/setup.py
@@ -0,0 +1,38 @@
+# -*- encoding: utf8 -*-
+import codecs
+from os import path
+from setuptools import setup, find_packages
+
+
+def read(*parts):
+    file_path = path.join(path.dirname(__file__), *parts)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+setup(
+    name='frigg-worker',
+    version='2.2.0',
+    description='A worker application that listens to the frigg broker '
+                'an pick up builds and build them.',
+    long_description=read('README.rst'),
+    author='The frigg team',
+    author_email='hi@frigg.io',
+    license='MIT',
+    url='https://github.com/frigg/frigg-worker',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    install_requires=[
+        'click==4.1',
+        'frigg-coverage==1.1.0',
+        'frigg-test-discovery>0.0,<1.1',
+        'docker-wrapper==0.7.0',
+        'pyyaml==3.11',
+        'raven==5.5.0'
+    ],
+    entry_points={
+        'console_scripts': ['frigg-worker = frigg_worker.cli:start']
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3',
+    ]
+)
