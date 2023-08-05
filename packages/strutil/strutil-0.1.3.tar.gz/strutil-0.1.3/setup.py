8828d1f25947259118a7a45f615bd343435a672e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:56 +0000

    Add package strutil/0.1.3/strutil-0.1.3.tar.gz

---
+++ packages/strutil/strutil-0.1.3.tar.gz/strutil-0.1.3/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import os, sys
+from setuptools import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit(0)
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+# Dynamically calculate the version based on swingtime.VERSION.
+version = __import__('strutil').__version__
+
+setup(
+    name='strutil',
+    url='https://github.com/dakrauth/strutil',
+    author='David A Krauth',
+    author_email='dakrauth@gmail.com',
+    description='Simple tools for downloading, cleaning, extracting and parsing content',
+    version=version,
+    long_description=long_description,
+    platforms=['any'],
+    license='MIT License',
+    py_modules=['strutil'],
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing'
+    ),
+)
