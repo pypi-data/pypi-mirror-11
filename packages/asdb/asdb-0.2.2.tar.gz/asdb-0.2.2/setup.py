c084a4ca14131e71b147d6b83234514140a0afc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:24 +0000

    Add package asdb/0.2.2/asdb-0.2.2.tar.gz

---
+++ packages/asdb/asdb-0.2.2.tar.gz/asdb-0.2.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='asdb',
+      version='0.2.2',
+      description='Instant convenient debugger',
+      url='https://github.com/alexmojaki/asdb',
+      author='Alex Hall',
+      author_email='alex.mojaki@gmail.com',
+      license='MIT',
+      packages=['asdb'],
+      install_requires=['rpdb'],
+      scripts=['shell'],
+      zip_safe=False)
