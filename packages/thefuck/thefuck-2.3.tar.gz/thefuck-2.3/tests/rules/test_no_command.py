19ad00cb2d527bbc8251a8d0047e456b3a08057c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:32 +0000

    Add package thefuck/2.3/thefuck-2.3.tar.gz

---
+++ packages/thefuck/thefuck-2.3.tar.gz/thefuck-2.3/tests/rules/test_no_command.py
@@ -0,0 +1,29 @@
+import pytest
+from thefuck.rules.no_command import match, get_new_command
+from tests.utils import Command
+
+
+@pytest.fixture(autouse=True)
+def get_all_executables(mocker):
+    mocker.patch('thefuck.rules.no_command.get_all_executables',
+                 return_value=['vim', 'apt-get', 'fsck'])
+
+
+@pytest.mark.usefixtures('no_memoize')
+def test_match():
+    assert match(Command(stderr='vom: not found', script='vom file.py'), None)
+    assert match(Command(stderr='fucck: not found', script='fucck'), None)
+    assert not match(Command(stderr='qweqwe: not found', script='qweqwe'), None)
+    assert not match(Command(stderr='some text', script='vom file.py'), None)
+
+
+@pytest.mark.usefixtures('no_memoize')
+def test_get_new_command():
+    assert get_new_command(
+        Command(stderr='vom: not found',
+                script='vom file.py'),
+        None) == 'vim file.py'
+    assert get_new_command(
+        Command(stderr='fucck: not found',
+                script='fucck'),
+        Command) == 'fsck'
---
+++ packages/thefuck/thefuck-2.3.tar.gz/thefuck-2.3/thefuck/rules/git_push_force.py
@@ -0,0 +1,17 @@
+from thefuck import utils
+
+
+@utils.git_support
+def match(command, settings):
+    return ('push' in command.script
+            and '! [rejected]' in command.stderr
+            and 'failed to push some refs to' in command.stderr
+            and 'Updates were rejected because the tip of your current branch is behind' in command.stderr)
+
+
+@utils.git_support
+def get_new_command(command, settings):
+    return command.script.replace('push', 'push --force')
+
+
+enabled_by_default = False
