eca55028e3a16f298705fe5924f5d9efcab8d9cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:31 +0000

    Add package glb-slave/0.0.9/glb-slave-0.0.9.tar.gz

---
+++ packages/glb-slave/glb-slave-0.0.9.tar.gz/glb-slave-0.0.9/glb_slave/command.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import click
+from .process import Process
+
+
+@click.command()
+@click.option('-o', '--option',
+              type=click.Choice(['start', 'stop', 'restart']), required=True)
+@click.option('-h', '--host', required=True, help='the host of the server')
+@click.option('-d', '--dist', required=True,
+              default='/etc/haproxy/haproxy.cfg',
+              help='native location of haproxy cfg.')
+@click.option('-s', '--s_dist', required=True,
+              default='/etc/init.d/haproxy',
+              help='native location of haproxy service.')
+@click.option('-p', '--p_dist', required=True,
+              default='/etc/haproxy/ssl/',
+              help='native location of ssl pems.')
+def process(option, host, dist, s_dist, p_dist):
+    if option:
+        websocket = "ws://%s/websocket" % host
+        p = Process(websocket, dist, s_dist, p_dist)
+        if option == 'start':
+            print 'glb_slave service start'
+            p.start()
+        if option == 'stop':
+            p.stop()
+        if option == 'restart':
+            p.restart()
