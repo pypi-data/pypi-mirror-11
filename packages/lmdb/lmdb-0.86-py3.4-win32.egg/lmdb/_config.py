eb4eb7c9ab0145aa644adde8f31fa720fccb2143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:37 +0000

    Add package lmdb/0.86/lmdb-0.86-py3.4-win32.egg

---
+++ packages/lmdb/lmdb-0.86-py3.4-win32.egg/lmdb/_config.py
@@ -0,0 +1,2 @@
+CONFIG = {'extra_include_dirs': ['lib/py-lmdb', 'lib', 'lib\\win32'], 'extra_sources': ['lib/mdb.c', 'lib/midl.c'], 'extra_library_dirs': [], 'extra_compile_args': ['-UNDEBUG', '-w', '/FIPython.h'], 'libraries': ['Advapi32']}
+
