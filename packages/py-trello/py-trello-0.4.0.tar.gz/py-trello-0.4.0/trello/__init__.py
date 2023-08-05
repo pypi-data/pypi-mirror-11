ff5dd8f124f034493c2366e133a77acef9c90eae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:57 +0000

    Add package py-trello/0.4.0/py-trello-0.4.0.tar.gz

---
+++ packages/py-trello/py-trello-0.4.0.tar.gz/py-trello-0.4.0/trello/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+from board import *
+from card import *
+from checklist import *
+from exceptions import *
+from label import *
+from member import *
+from organization import *
+from trelloclient import *
+from trellolist import *
+from webhook import *
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
