0ce69f16c389e4b6e011ff15f5f9b4eede6bd02d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:11 +0000

    Add package pyrecommend/0.1.dev4/pyrecommend-0.1.dev4.tar.gz

---
+++ packages/pyrecommend/pyrecommend-0.1.dev4.tar.gz/pyrecommend-0.1.dev4/setup.py
@@ -0,0 +1,24 @@
+"""setuptools config for pyrecommend."""
+import os
+from setuptools import setup
+
+
+def read(fname):
+    """Get the contents of the named file as a string."""
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except IOError:
+        return u''
+
+
+setup(
+    name='pyrecommend',
+    version='0.1.dev4',
+    author='Dan Passaro',
+    author_email='danpassaro@gmail.com',
+    url='https://github.com/dan-passaro/pyrecommend/',
+    description='A simple collaborative filtering algorithm for Python.',
+    license='BSD',
+    packages=['pyrecommend'],
+    long_description=read('README.md'),
+)
