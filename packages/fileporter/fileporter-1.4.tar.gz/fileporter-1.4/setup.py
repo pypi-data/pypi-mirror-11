19edbdd7c85406bc51ec5f150f3dd8aef8876690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:45 +0000

    Add package fileporter/1.4/fileporter-1.4.tar.gz

---
+++ packages/fileporter/fileporter-1.4.tar.gz/fileporter-1.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup,find_packages
+
+install_requires = [
+    'tornado>=3.1.0',
+]
+
+setup(
+    name='fileporter',
+    version='1.4',
+    description='support download/upload/browser,use tornado',
+    author='fengyun',
+    author_email='rfyiamcool@163.com',
+    url='https://github.com/rfyiamcool/fileporter',
+    keywords = 'better SimpleHTTPServer fengyun',
+    license='BSD',
+    #py_modules=['fileporter'],
+    packages = ['fileporter'],
+    package_data={'fileporter': ['static/*',
+        'static/index.html'
+    ]},
+    zip_safe=False,
+    install_requires=install_requires,
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Utilities',
+    ]
+)
