98b640b4724295be3e5f411fde72cb7b8b87166c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:21 +0000

    Add package htmlPy/2.0.0/htmlPy-2.0.0.tar.gz

---
+++ packages/htmlPy/htmlPy-2.0.0.tar.gz/htmlPy-2.0.0/htmlPy/__init__.py
@@ -0,0 +1,10 @@
+from PySide.QtCore import QObject as Object
+from PySide.QtCore import Slot
+from .base_gui import BaseGUI
+from .web_app_gui import WebAppGUI
+from .app_gui import AppGUI
+del app_gui
+del base_gui
+del descriptors
+del gui_helper
+del web_app_gui
