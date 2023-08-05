3adbabc912790ba5dacdaeb985de879156869407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:16 +0000

    Add package flask_checkargs/0.1.1/flask_checkargs-0.1.1.tar.gz

---
+++ packages/flask_checkargs/flask_checkargs-0.1.1.tar.gz/flask_checkargs-0.1.1/setup.py
@@ -0,0 +1,27 @@
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+setup(
+    name='flask_checkargs',
+    version='0.1.1',
+    requires=['flask'],
+    url='http://www.github.com/jsevilleja/flask_checkargs',
+    include_package_data=True,
+    license='MIT',
+    author='Joel Sevilleja',
+    author_email='joel@jsevilleja.org',
+    description='A module to simplify checking arguments in Flask apps',
+    packages = ["flask_checkargs"],
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Security",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
