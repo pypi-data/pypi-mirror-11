4e3796a6370321c9841574259bc8287360e1e164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:40 +0000

    Add package Dory/0.0.2/Dory-0.0.2.tar.gz

---
+++ packages/Dory/Dory-0.0.2.tar.gz/Dory-0.0.2/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import sys
+
+if sys.version_info[0] < 3:
+    print("python < 3.0 is not supported :(")
+    sys.exit(0)
+
+setup(name='Dory',
+      version='0.0.2',
+      description='Simple Backup',
+      author='inosms',
+      url='https://github.com/inosms/dory',
+      packages=["dory"],
+      entry_points={
+      "console_scripts":["dory=dory.dory:main"]
+      },
+      install_requires = ["termcolor"]
+     )
