7bac81908b76dc12946234f2911af6a3f7dd37dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:39 +0000

    Add package polygonmeshtools/0.1/polygonmeshtools-0.1.tar.gz

---
+++ packages/polygonmeshtools/polygonmeshtools-0.1.tar.gz/polygonmeshtools-0.1/setup.py
@@ -0,0 +1,14 @@
+import os
+from distutils.core import setup
+
+setup(
+    name = "polygonmeshtools",
+    packages=["polygonmeshtools"],
+    version = "0.1",
+    description = "A package for creating polygon-shaped geometries from cuboidal finite difference elements",
+    author = "Xander Marjoram",
+    author_email = "am31g12@soton.ac.uk",
+    keywords = ["polygon", "mesh", "geometry", "finite", "difference", "fidimag"],
+    url = "https://bitbucket.org/fangohr/polygon-finite-difference-mesh-tools/get/tip.tar.gz",
+    #classifiers = [],
+)
