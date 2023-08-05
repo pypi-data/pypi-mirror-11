67fd695c5d0c3e9847e16d07f3569268ededde5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:10 +0000

    Add package atomic-reactor/1.4.0/atomic-reactor-1.4.0.tar.gz

---
+++ packages/atomic-reactor/atomic-reactor-1.4.0.tar.gz/atomic-reactor-1.4.0/atomic_reactor/plugins/post_store_metadata_in_osv3.py
@@ -0,0 +1,109 @@
+"""
+Copyright (c) 2015 Red Hat, Inc
+All rights reserved.
+
+This software may be modified and distributed under the terms
+of the BSD license. See the LICENSE file for details.
+"""
+from __future__ import unicode_literals
+
+import json
+import os
+
+from osbs.api import OSBS
+from osbs.conf import Configuration
+
+from atomic_reactor.plugin import PostBuildPlugin
+from atomic_reactor.plugins.pre_return_dockerfile import CpDockerfilePlugin
+from atomic_reactor.plugins.pre_pyrpkg_fetch_artefacts import DistgitFetchArtefactsPlugin
+from atomic_reactor.plugins.post_rpmqa import PostBuildRPMqaPlugin
+
+
+class StoreMetadataInOSv3Plugin(PostBuildPlugin):
+    key = "store_metadata_in_osv3"
+
+    def __init__(self, tasker, workflow, url, verify_ssl=True, use_auth=True):
+        """
+        constructor
+
+        :param tasker: DockerTasker instance
+        :param workflow: DockerBuildWorkflow instance
+        :param url: str, URL to OSv3 instance
+        :param use_auth: bool, initiate authentication with openshift?
+        """
+        # call parent constructor
+        super(StoreMetadataInOSv3Plugin, self).__init__(tasker, workflow)
+        self.url = url
+        self.verify_ssl = verify_ssl
+        self.use_auth = use_auth
+
+    def run(self):
+        try:
+            build_json = json.loads(os.environ["BUILD"])
+        except KeyError:
+            self.log.error("No $BUILD env variable. Probably not running in build container.")
+            return
+        try:
+            build_id = build_json["metadata"]["name"]
+        except KeyError:
+            self.log.error("malformed build json")
+            return
+        self.log.info("build id = %s", build_id)
+
+        # initial setup will use host based auth: apache will be set to accept everything
+        # from specific IP and will set specific X-Remote-User for such requests
+        osbs_conf = Configuration(conf_file=None, openshift_uri=self.url,
+                                  use_auth=self.use_auth, verify_ssl=self.verify_ssl)
+        osbs = OSBS(osbs_conf, osbs_conf)
+
+        # usually repositories formed from NVR labels
+        # these should be used for pulling and layering
+        primary_repositories = []
+        for registry in self.workflow.push_conf.all_registries:
+            for image in self.workflow.tag_conf.primary_images:
+                registry_image = image.copy()
+                registry_image.registry = registry.uri
+                primary_repositories.append(registry_image.to_str())
+
+        # unique unpredictable repositories
+        unique_repositories = []
+        for registry in self.workflow.push_conf.all_registries:
+            for image in self.workflow.tag_conf.unique_images:
+                registry_image = image.copy()
+                registry_image.registry = registry.uri
+                unique_repositories.append(registry_image.to_str())
+
+        repositories = {
+            "primary": primary_repositories,
+            "unique": unique_repositories,
+        }
+
+        try:
+            commit_id = self.workflow.source.lg.commit_id
+        except AttributeError:
+            commit_id = ""
+
+        labels = {
+            "dockerfile": self.workflow.prebuild_results.get(CpDockerfilePlugin.key, ""),
+            "artefacts": self.workflow.prebuild_results.get(DistgitFetchArtefactsPlugin.key, ""),
+            "logs": "\n".join(self.workflow.build_logs),
+            "rpm-packages": "\n".join(self.workflow.postbuild_results.get(PostBuildRPMqaPlugin.key, "")),
+            "repositories": json.dumps(repositories),
+            "commit_id": commit_id,
+        }
+
+        tar_path = self.workflow.exported_squashed_image.get("path")
+        tar_size = self.workflow.exported_squashed_image.get("size")
+        tar_md5sum = self.workflow.exported_squashed_image.get("md5sum")
+        tar_sha256sum = self.workflow.exported_squashed_image.get("sha256sum")
+        # looks like that openshift can't handle value being None (null in json)
+        if tar_size is not None and tar_md5sum is not None and tar_sha256sum is not None and \
+                tar_path is not None:
+            labels["tar_metadata"] = json.dumps({
+                "size": tar_size,
+                "md5sum": tar_md5sum,
+                "sha256sum": tar_sha256sum,
+                "filename": os.path.basename(tar_path),
+            })
+        osbs.set_annotations_on_build(build_id, labels)
+        return labels
---
+++ packages/atomic-reactor/atomic-reactor-1.4.0.tar.gz/atomic-reactor-1.4.0/docs/plugins.md
@@ -0,0 +1,59 @@
+Plugins
+=======
+
+One of the biggest strength of Atomic Reactor is its plugin system.
+
+## Plugin types
+
+There are 4 types of plugins:
+
+1. **Input** — when running Atomic Reactor inside a build container, input plugin fetches build input (it can live in [path](https://github.com/projectatomic/atomic-reactor/blob/master/atomic_reactor/plugins/input_path.py), [environment variable](https://github.com/projectatomic/atomic-reactor/blob/master/atomic_reactor/plugins/input_env.py), ...)
+2. **Pre-build** — this plugin is executed after cloning a git repo (so you can edit sources, Dockerfile, ...), prior to build
+3. **Pre-publish** — once build finishes, the built image is pushed to registry, but prior to that, pre-publish plugins are executed (time for some simple tests)
+4. **Post-build** — these are run as a last thing of build process (when build is finished and image was pushed to registries)
+
+## Plugin configuration
+
+Build plugins are requested and configured via input json: key `prebuild_plugins`, `postbuild_plugins` or `prepublish_plugins`. Each field is an array of following items:
+
+```
+{
+    "name": "plugin_name",
+    "can_fail": false,
+    "args": {
+        "args1": "value"
+    }
+}
+```
+
+Order is important, because plugins are executed in the order as they are specified (one plugin can use input from another plugin). `args` are directly passed to a plugin in constructor. If `can_fail` is set to `false`, once the plugin raises an exception, build process is halted.
+
+
+## Input plugins
+
+Input plugin is requested via command line: command `inside-build`, option `--input`. Some input plugins require configuration, e.g. path plugins requires a file path to the file with build json. This is done via argument `--input-arg`. It has special syntax: `--input-arg key=value`.
+
+### path input plugin
+
+This input plugin reads specified file from filesystem and uses it as build json. Sample usage:
+```
+--input path --input-arg path=path/to/the/build.json
+```
+
+### env input plugin
+
+Loads specified environment variable as build json. Sample usage:
+```
+--input env --input-arg env_name=MY_BUILD_JSON
+```
+
+### Configuration substitution
+
+You may substitute configuration in your provided build json. This is really handy if you have a template with build json and don't want to change it yourself.
+
+All you need to do to accomplish this is to pass argument `--substitute` with value:
+ * `argument_name=argument_value`
+ * `plugin_type.plugin_name.argument_name=argument_value`
+
+E.g. `--substitute image=my-nice-image --substitute prebuild_plugins.koji.target=f22`.
+
