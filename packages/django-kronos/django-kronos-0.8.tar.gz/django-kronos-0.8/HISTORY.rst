e3697061d4af4e74ef593e96cc6299926fdc4691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:14 +0000

    Add package django-kronos/0.8/django-kronos-0.8.tar.gz

---
+++ packages/django-kronos/django-kronos-0.8.tar.gz/django-kronos-0.8/HISTORY.rst
@@ -0,0 +1,56 @@
+0.8.0
++++++
+
+* Kronos is now even more compatible Python 3.
+* Kronos is no longer compatible with Python 2.6.
+* You may now prefix commands with ``KRONOS_PREFIX``.
+* Fix an issue where Kronos would not pick up on AppConfig apps.
+
+0.7.0
++++++
+
+* You may now pass arguments to Django management commands registered with Kronos.
+* Kronos is now compatible with Python 3.
+* Kronos will now log errors when it fails to load tasks.
+
+0.6.0
++++++
+
+* You may now register Django management commands.
+
+0.5.0
++++++
+
+* You may now list commands with ``python manage.py showtasks``.
+
+0.4.0
++++++
+
+* You may now postfix commands with ``KRONOS_POSTFIX``.
+
+0.3.0
++++++
+
+* You may now customize the interpreter, management path and python path for tasks with the ``KRONOS_PYTHON``,
+  ``KRONOS_MANAGE`` and ``KRONOS_PYTHONPATH`` settings, respectively.
+
+0.2.3
++++++
+
+* Kronos now supports Django 1.4-style projects.
+* Fixed a bug that caused installation to fail for users that didn't already have a crontab.
+
+0.2.2
++++++
+
+* Fixed a bug that caused unclosed single quotes in the crontab to raise a ValueError
+
+0.2.1
++++++
+
+* Fixed a bug that caused 'cron'-modules in the project root to be ignored.
+
+0.2.0
++++++
+
+* Kronos will now collect tasks from a 'cron' module in the project root.
---
+++ packages/django-kronos/django-kronos-0.8.tar.gz/django-kronos-0.8/kronos/__init__.py
@@ -0,0 +1,162 @@
+from functools import wraps
+
+import django
+from django.core.management import get_commands, load_command_class
+
+try:
+    from importlib import import_module
+except ImportError:
+    from django.utils.importlib import import_module
+
+from kronos.settings import PROJECT_MODULE, KRONOS_PYTHON, KRONOS_MANAGE, \
+    KRONOS_PYTHONPATH, KRONOS_POSTFIX, KRONOS_PREFIX
+from django.conf import settings
+from kronos.utils import read_crontab, write_crontab, delete_crontab
+from kronos.version import __version__
+import six
+from django.utils.module_loading import autodiscover_modules
+
+tasks = []
+
+
+def load():
+    """
+    Load ``cron`` modules for applications listed in ``INSTALLED_APPS``.
+    """
+    autodiscover_modules('cron')
+
+    if '.' in PROJECT_MODULE.__name__:
+        try:
+            import_module('%s.cron' % '.'.join(
+	            PROJECT_MODULE.__name__.split('.')[0:-1]))
+        except ImportError as e:
+            if 'No module named' not in str(e):
+                print(e)
+
+    # load django tasks
+    for cmd, app in get_commands().items():
+        try:
+            load_command_class(app, cmd)
+        except django.core.exceptions.ImproperlyConfigured:
+            pass
+
+
+def register(schedule, *args, **kwargs):
+    def decorator(function):
+        global tasks
+        passed_args = []
+
+        if "args" in kwargs:
+            for key, value in six.iteritems(kwargs["args"]):
+                if isinstance(value, dict):
+                    raise TypeError('Parse for dict arguments not yet implemented.')
+
+                if isinstance(value, list):
+                    temp_args = ",".join(map(str, value))
+                    passed_args.append("{}={}".format(key, temp_args))
+                else:
+                    if value is None:
+                        arg_text = "{}"
+                    elif isinstance(value, str):
+                        arg_text = '{} "{}"'
+                    else:
+                        arg_text = '{} {}'
+
+                    passed_args.append(arg_text.format(key, value))
+
+        if hasattr(function, 'handle'):
+            # django command
+            function.cron_expression = '%(schedule)s %(prefix)s %(python)s %(manage)s ' \
+                '%(task)s %(passed_args)s --settings=%(settings_module)s %(postfix)s' \
+                '$KRONOS_BREAD_CRUMB' % {
+                    'schedule': schedule,
+                    'prefix': KRONOS_PREFIX,
+                    'python': KRONOS_PYTHON,
+                    'manage': KRONOS_MANAGE,
+                    'task': function.__module__.split('.')[-1],
+                    'passed_args': " ".join(passed_args),
+                    'settings_module': settings.SETTINGS_MODULE,
+                    'postfix': KRONOS_POSTFIX
+                }
+            task = dict(name=function.__module__.split('.')[-1],
+                django_command=True,
+                fn=function)
+        else:
+            function.cron_expression = '%(schedule)s %(prefix)s %(python)s %(manage)s ' \
+                'runtask %(task)s %(passed_args)s --settings=%(settings_module)s ' \
+                '%(postfix)s $KRONOS_BREAD_CRUMB' % {
+                    'schedule': schedule,
+                    'prefix': KRONOS_PREFIX,
+                    'python': KRONOS_PYTHON,
+                    'manage': KRONOS_MANAGE,
+                    'task': function.__name__,
+                    'passed_args': " ".join(passed_args),
+                    'settings_module': settings.SETTINGS_MODULE,
+                    'postfix': KRONOS_POSTFIX
+                }
+            task = dict(name=function.__name__,
+                django_command=False,
+                fn=function)
+
+        if KRONOS_PYTHONPATH is not None:
+            function.cron_expression += ' --pythonpath=%s' % KRONOS_PYTHONPATH
+
+        tasks.append(task)
+
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            return function(*args, **kwargs)
+        return wrapper
+
+    return decorator
+
+
+def install():
+    """
+    Register tasks with cron.
+    """
+    load()
+    current_crontab = six.u(read_crontab())
+
+    new_crontab = ''
+    for task in tasks:
+        new_crontab += '%s\n' % task['fn'].cron_expression
+
+    write_crontab(current_crontab + new_crontab)
+
+
+def printtasks():
+    """
+    Print the tasks that would be installed in the
+    crontab, for debugging purposes.
+    """
+    load()
+
+    for task in tasks:
+        print(task['fn'].cron_expression)
+
+
+def uninstall():
+    """
+    Uninstall tasks from cron.
+    """
+    current_crontab = read_crontab()
+
+    new_crontab = ''
+    for line in six.u(current_crontab).split('\n')[:-1]:
+        exp = '%(python)s %(manage)s runtask' % {
+            'python': KRONOS_PYTHON,
+            'manage': KRONOS_MANAGE,
+            }
+        if '$KRONOS_BREAD_CRUMB' not in line and exp not in line:
+            new_crontab += '%s\n' % line
+
+    if new_crontab:
+        write_crontab(new_crontab)
+    else:
+        delete_crontab()
+
+
+def reinstall():
+    uninstall()
+    install()
