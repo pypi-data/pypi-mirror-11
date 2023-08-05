969938b9e2c72e552a1e30e16d58253517f7f9cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:49 +0000

    Add package PikaChewie/1.0.1/PikaChewie-1.0.1.tar.gz

---
+++ packages/PikaChewie/PikaChewie-1.0.1.tar.gz/PikaChewie-1.0.1/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import sys
+
+from setuptools import setup, find_packages
+
+if sys.version_info < (2, 6):
+    raise Exception("This package requires Python 2.6 or higher.")
+
+
+def read_release_version():
+    """Read the version from the file ``RELEASE-VERSION``."""
+    with open("RELEASE-VERSION", "r") as f:
+        return f.readline().strip()
+
+url = 'https://github.com/bdeeney/PikaChewie'
+version = read_release_version()
+
+setup(
+    name='PikaChewie',
+    version=version,
+    description='A pika-based RabbitMQ publisher-consumer framework',
+    long_description='''PikaChewie is your pika co-pilot, providing RabbitMQ
+        messaging tools with bandoliers included.''',
+    author='Bryan Deeney',
+    author_email='rennybot@pobox.com',
+    packages=find_packages(exclude=['tests.*', 'tests']),
+    url=url,
+    download_url='{url}/tarball/{version}'.format(url=url, version=version),
+    license='BSD',
+    test_suite='nose.collector',
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'pika>=0.9.14,<0.10dev',
+        'simplejson',
+        'tornado',
+    ],
+    classifiers=[],
+)
