370f05f5abd23d48f2763fa041b5142b1eab160e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:14 +0000

    Add package handler/1.3.0/handler-1.3.0.zip

---
+++ packages/handler/handler-1.3.0.zip/handler-1.3.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+
+setup(
+        name='handler',
+        version='1.3.0',
+        py_modules=['handler'],
+        author='lyson',
+        author_email='kd0604@126.com',
+        url='http://hi.baidu.com/tianchoua',
+        description='A simple printer of nested list',
+
+
+    )
