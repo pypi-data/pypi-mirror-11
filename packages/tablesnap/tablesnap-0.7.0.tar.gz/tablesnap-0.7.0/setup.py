1d260f8e9000bb2abd37219743d28fb007bd7cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:48 +0000

    Add package tablesnap/0.7.0/tablesnap-0.7.0.tar.gz

---
+++ packages/tablesnap/tablesnap-0.7.0.tar.gz/tablesnap-0.7.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='tablesnap',
+    version='0.7.0',
+    author='Jeremy Grosser',
+    author_email='jeremy@synack.me',
+    url='https://github.com/JeremyGrosser/tablesnap',
+    scripts=[
+        'tablesnap',
+        'tableslurp',
+        'tablechop'
+    ],
+    install_requires=[
+        x.strip()
+        for x in open('requirements.txt').readlines()
+        if x and not x.startswith('#')
+    ],
+)
