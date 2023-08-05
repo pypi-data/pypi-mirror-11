822620a5fe163e96d2481440a94a5af443203ae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:52 +0000

    Add package gtabview/0.1/gtabview-0.1.tar.gz

---
+++ packages/gtabview/gtabview-0.1.tar.gz/gtabview-0.1/README.rst
@@ -0,0 +1,91 @@
+gtabview: a simple graphical tabular data viewer
+================================================
+
+A graphical counterpart to `tabview <https://github.com/firecat53/tabview/>`_,
+a simple tabular data viewer that can be used both stand-alone and as a Python
+module for various files and Python/Pandas/NumPy data structures.
+
+
+Stand-alone usage
+-----------------
+
+`gtabview` reads most tabular data formats automatically::
+
+  gtabview data.csv
+
+
+Usage as a module
+-----------------
+
+.. code:: python
+
+    from gtabview import view
+
+    # view a file
+    view("/path/to/file")
+
+    # view a list
+    view([1, 2, 3])
+
+    # view a dict (by columns)
+    view({'a': [1, 2, 3], 'b': [4, 5, 6], 'c': [7, 8, 9]})
+
+    # view a simple list of lists
+    view([['a', 'b', 'c'], [1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+    # view a DataFrame/Series/Panel
+    import pandas as pd
+    df = pd.DataFrame([[1, 2, 3], [4, 5, 6]],
+		      columns=['a', 'b', 'c'], index=['x', 'y'])
+    view(df)
+
+    # numpy is supported as well
+    import numpy as np
+    view(np.array([[1, 2, 3], [4, 5, 6]]))
+
+If you're using gtabview with matplotlib either directly or indirectly (for
+example, using the Pandas visualization API or Seaborn), be sure to include
+matplotlib first to correctly initialize gtabview.
+
+gtabview will also use matplotlib's ``interactive`` setting to determine the
+default behavior of the data window: when interactive, calls to ``view()`` will
+not block, and will keep recycling the same window.
+
+
+Requirements and installation
+-----------------------------
+
+- Python 2 or Python 3
+- PyQt4 or PySide
+- setuptools and setuptools-git (install-only).
+
+Under Debian/Ubuntu, install the required dependencies with::
+
+  sudo apt-get install python python-qt4
+  sudo apt-get install python-setuptools python-setuptools-git
+
+Then download and install regularly with::
+
+  git clone https://github.com/wavexx/gtabview
+  cd gtabview
+  ./setup.py install
+
+You can also install directly via `pip`::
+
+  pip install git+git://github.com/wavexx/gtabview
+
+Excel files are supported if the ``xlrd`` module is also installed::
+
+  sudo apt-get install python-xlrd
+
+or::
+
+  pip install xlrd
+
+
+License
+-------
+
+| gtabview is distributed under the MIT license (see ``LICENSE.txt``)
+| Copyright(c) 2014-2015: wave++ "Yuri D'Elia" <wavexx@thregr.org>
+| Copyright(c) 2014-2015: Scott Hansen <firecat4153@gmail.com>
---
+++ packages/gtabview/gtabview-0.1.tar.gz/gtabview-0.1/gtabview/models.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, unicode_literals, absolute_import, generators
+
+import collections
+from .compat import *
+
+
+def getitem(lst, idx, default=None):
+    return lst[idx] if idx < len(lst) else default
+
+def getitem2(lst, y, x, default=None):
+    return getitem(getitem(lst, y, []), x, default)
+
+
+class ExtDataModel(object):
+    def shape(self):
+        pass
+
+    def header_shape(self):
+        pass
+
+    def data(self, y, x):
+        pass
+
+    def header(self, axis, x, level):
+        pass
+
+
+class ExtListModel(ExtDataModel):
+    def __init__(self, data, hdr_rows=None, idx_cols=None):
+        super(ExtListModel, self).__init__()
+        if hdr_rows is None:
+            hdr_rows = 1 if len(data) > 1 else 0
+        if idx_cols is None:
+            idx_cols = 0
+        self._header_shape = (hdr_rows, idx_cols)
+        self._shape = (len(data) - hdr_rows, len(data[0]) - idx_cols)
+        self._data = data
+
+    def shape(self):
+        return self._shape
+
+    def header_shape(self):
+        return self._header_shape
+
+    def header(self, axis, x, level):
+        if axis == 0:
+            return getitem2(self._data, level, x + self._header_shape[1], '')
+        else:
+            return getitem2(self._data, x + self._header_shape[0], level, '')
+
+    def data(self, y, x):
+        return getitem2(self._data, y + self._header_shape[0],
+                        x + self._header_shape[1], '')
+
+
+class ExtVectorModel(ExtDataModel):
+    def __init__(self, data):
+        super(ExtVectorModel, self).__init__()
+        self._data = data
+
+    def shape(self):
+        return (len(self._data), 1)
+
+    def header_shape(self):
+        return (0, 0)
+
+    def data(self, y, x):
+        return self._data[y]
+
+
+class ExtMatrixModel(ExtDataModel):
+    def __init__(self, data):
+        super(ExtMatrixModel, self).__init__()
+        self._data = data
+
+    def shape(self):
+        return self._data.shape
+
+    def header_shape(self):
+        return (0, 0)
+
+    def data(self, y, x):
+        return self._data[y, x]
+
+
+class ExtFrameModel(ExtDataModel):
+    def __init__(self, data):
+        super(ExtFrameModel, self).__init__()
+        self._data = data
+
+    def _axis(self, axis):
+        return self._data.columns if axis == 0 else self._data.index
+
+    def _axis_levels(self, axis):
+        ax = self._axis(axis)
+        return 1 if not hasattr(ax, 'levels') \
+            else len(ax.levels)
+
+    def shape(self):
+        return self._data.shape
+
+    def header_shape(self):
+        return (self._axis_levels(0), self._axis_levels(1))
+
+    def data(self, y, x):
+        return str(self._data.iat[y, x])
+
+    def header(self, axis, x, level=0):
+        ax = self._axis(axis)
+        return str(ax.values[x]) if not hasattr(ax, 'levels') \
+            else str(ax.values[x][level])
+
+
+def as_model(data, hdr_rows=None, idx_cols=None):
+    if isinstance(data, ExtDataModel):
+        return data
+
+    # TODO: add specific data models to reduce overhead
+    if data.__class__.__name__ in ['Series', 'Panel']:
+        data = data.to_frame()
+    elif isinstance(data, dict):
+        data = [data.keys()] + list(map(list, zip(*[data[i] for i in data.keys()])))
+
+    if data.__class__.__name__ == 'DataFrame':
+        return ExtFrameModel(data)
+    elif data.__class__.__name__ == 'ndarray':
+        return ExtMatrixModel(data)
+    elif isinstance(data, collections.Sequence) and \
+         isinstance(data[0], collections.Sequence):
+        return ExtListModel(data, hdr_rows=hdr_rows, idx_cols=idx_cols)
+    elif isinstance(data, collections.Sequence):
+        return ExtVectorModel(data)
+
+    return None
