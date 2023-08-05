fa17417294889801151789400c0b0f198a86f6e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:17 +0000

    Add package slacksocket/0.5.1/slacksocket-0.5.1.tar.gz

---
+++ packages/slacksocket/slacksocket-0.5.1.tar.gz/slacksocket-0.5.1/slacksocket/errors.py
@@ -0,0 +1,8 @@
+class SlackSocketEventNameError(NameError):
+    """ Invalid name """
+
+class SlackSocketAPIError(RuntimeError):
+    """ Error response from Slack API """
+
+class SlackSocketConnectionError(IOError):
+    """ Unrecoverable error maintaining a websocket connection """
