da3e7e96dfa60ba14678065caceb946148deef8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:52 +0000

    Add package django-webservice/0.0.2/django-webservice-0.0.2.tar.gz

---
+++ packages/django-webservice/django-webservice-0.0.2.tar.gz/django-webservice-0.0.2/README.md
@@ -0,0 +1,11 @@
+# Thinc Webservice
+Adds i18n and delta support for models to [django-restframework](http://www.django-rest-framework.org/)
+
+[![Build Status](https://api.travis-ci.org/webair/thinc.django-webservice.svg)](https://travis-ci.org/webair/thinc.django-webservice)
+[![Coverage Status](https://coveralls.io/repos/webair/thinc.django-webservice/badge.svg?branch=develop)](https://coveralls.io/r/webair/thinc.django-webservice?branch=develop)
+
+## Requirements
+- Django (1.8 or higher)
+- djangorestframework (3.1 or higher)
+- pytz (2014.10 or higher)
+- django-grappelli (2.6.0 or higher)
---
+++ packages/django-webservice/django-webservice-0.0.2.tar.gz/django-webservice-0.0.2/setup.py
@@ -0,0 +1,39 @@
+from distutils.core import setup
+import os
+import re
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+version = get_version('webservice')
+
+setup(
+    name='django-webservice',
+    packages=['webservice'],
+    version=version,
+    description='Extends the django restframework library for i18n and delta support',
+    author='Chris Weber',
+    author_email='chrisr.weber@gmail.com',
+    url='https://github.com/webair/thinc.django-webservice',
+    license='MIT License',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
