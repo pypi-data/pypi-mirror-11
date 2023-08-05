0fdef404ba259cb6c21159c26fa33c90257cc268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:26 +0000

    Add package GeobricksRasterCorrelation/0.1.12/GeobricksRasterCorrelation-0.1.12.tar.gz

---
+++ packages/GeobricksRasterCorrelation/GeobricksRasterCorrelation-0.1.12.tar.gz/GeobricksRasterCorrelation-0.1.12/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='GeobricksRasterCorrelation',
+    version='0.1.12',
+    author='Simone Murzilli; Guido Barbaglia',
+    author_email='geobrickspy@gmail.com',
+    packages=find_packages(),
+    license='LICENSE.txt',
+    long_description=open('README.md').read(),
+    description='Geobricks library to correlate two raster and create statistics and scatter charts.',
+    install_requires=[
+        # 'flask',
+        # 'brewer2mpl',
+        # 'PySal',
+        # 'numpy',
+        # 'scipy',
+        # 'GeobricksCommon',
+    ],
+    entry_points={
+        'console_scripts': [
+            'raster_correlation = geobricks_raster_correlation.cli.cli:main',
+            # 'raster_correlation = geobricks_raster_correlation.cli.cli_argh:main',
+        ]
+    },
+    url='http://pypi.python.org/pypi/GeobricksRasterCorrelation/',
+    keywords=['geobricks', 'processing', 'raster', 'gis', 'gdal', 'correlation', 'raster correlation', 'highcharts']
+)
