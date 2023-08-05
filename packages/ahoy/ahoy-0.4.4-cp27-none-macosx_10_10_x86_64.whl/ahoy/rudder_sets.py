93e82e455021f65f279f921cfb69eb84e11de42c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:36 +0000

    Add package ahoy/0.4.4/ahoy-0.4.4-cp27-none-macosx_10_10_x86_64.whl

---
+++ packages/ahoy/ahoy-0.4.4-cp27-none-macosx_10_10_x86_64.whl/ahoy/rudder_sets.py
@@ -0,0 +1,97 @@
+from __future__ import print_function, division
+from ahoy.rudders import TumbleRudders, RotationRudders, rudders_factory
+from ciabatta.meta import make_repr_str
+
+
+class RudderSets(object):
+
+    def __init__(self):
+        self.sets = []
+
+    @property
+    def chi(self):
+        for rudders in self.sets:
+            if rudders.is_chemotactic:
+                return rudders.chi
+
+    @property
+    def does_tumbling(self):
+        for rs in self.sets:
+            if isinstance(rs, TumbleRudders):
+                return True
+        return False
+
+    @property
+    def does_rotation(self):
+        for rs in self.sets:
+            if isinstance(rs, RotationRudders):
+                return True
+        return False
+
+    @property
+    def chemo_rudders(self):
+        for rs in self.sets:
+            if rs.is_chemotactic:
+                return rs
+        raise Exception
+
+    @property
+    def Dr_0(self):
+        for rs in self.sets:
+            if isinstance(rs, RotationRudders):
+                return rs.noise_measurer.noise_0
+        return None
+
+    @property
+    def p_0(self):
+        for ruds in self.sets:
+            if isinstance(ruds, TumbleRudders):
+                return ruds.noise_measurer.noise_0
+        return None
+
+    def rotate(self, directions, dt, rng):
+        for rudders in self.sets:
+            directions = rudders.rotate(directions, dt, rng)
+        return directions
+
+    def __repr__(self):
+        fs = [('sets', self.sets)]
+        return make_repr_str(self, fs)
+
+
+def rudder_set_factory(temporal_chemo_flag,
+                       ds,
+                       ps, v_0, dt_mem, t_mem, time,
+                       c_field_flag, c_field,
+                       onesided_flag, chi,
+                       tumble_flag, p_0, tumble_chemo_flag,
+                       rotation_flag, Dr_0, dim, rotation_chemo_flag):
+    D_rot_0 = 0.0
+    if tumble_flag:
+        D_rot_0 += p_0
+    if rotation_flag:
+        D_rot_0 += Dr_0
+    t_rot_0 = 1.0 / D_rot_0
+
+    sets = RudderSets()
+    if tumble_flag:
+        rudders = rudders_factory(True, dim,
+                                  tumble_chemo_flag,
+                                  p_0,
+                                  onesided_flag, chi,
+                                  temporal_chemo_flag,
+                                  ds,
+                                  ps, v_0, dt_mem, t_mem, t_rot_0, time,
+                                  c_field_flag, c_field)
+        sets.sets.append(rudders)
+    if rotation_flag:
+        rudders = rudders_factory(False, dim,
+                                  rotation_chemo_flag,
+                                  Dr_0,
+                                  onesided_flag, chi,
+                                  temporal_chemo_flag,
+                                  ds,
+                                  ps, v_0, dt_mem, t_mem, t_rot_0, time,
+                                  c_field_flag, c_field)
+        sets.sets.append(rudders)
+    return sets
