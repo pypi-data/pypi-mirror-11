5034c200c09285a61c76e2c1f1994acaca431f72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:25 +0000

    Add package mongoop/0.0.5/mongoop-0.0.5.tar.gz

---
+++ packages/mongoop/mongoop-0.0.5.tar.gz/mongoop-0.0.5/mongoop/default_settings.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+    mongoop.default_settings
+    ~~~~~~~~~~~~~~~~~~~~~~~~
+
+    :copyright: (c) 2015 by Lujeni.
+    :license: BSD, see LICENSE for more details.
+"""
+
+mongodb_host = 'localhost'
+
+mongodb_port = 27017
+
+mongodb_credentials = None
+
+mongodb_options = None
+
+frequency = 10
+
+triggers = {}
