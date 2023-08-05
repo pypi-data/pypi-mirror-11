51e80464a6e6ff7f36ef2e4c47f70b43c08eeb4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:53 +0000

    Add package rb/0.9/rb-0.9.tar.gz

---
+++ packages/rb/rb-0.9.tar.gz/rb-0.9/rb/__init__.py
@@ -0,0 +1,30 @@
+"""
+    rb
+    ~~
+
+    The redis blaster.
+
+    :copyright: (c) 2015 Functional Software Inc.
+    :license: Apache License 2.0, see LICENSE for more details.
+"""
+from rb.cluster import Cluster
+from rb.clients import RoutingClient, MappingClient, FanoutClient
+from rb.router import BaseRouter, ConsistentHashingRouter, PartitionRouter
+from rb.promise import Promise
+
+
+__version__ = '0.9'
+
+__all__ = [
+    # cluster
+    'Cluster',
+
+    # client
+    'RoutingClient', 'MappingClient', 'FanoutClient',
+
+    # router
+    'BaseRouter', 'ConsistentHashingRouter', 'PartitionRouter',
+
+    # promise
+    'Promise',
+]
