15d69c85d05967af8a5a4d290f1c17a2be50cc89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:39 +0000

    Add package bormeparser/0.1.3/bormeparser-0.1.3-py2.py3-none-any.whl

---
+++ packages/bormeparser/bormeparser-0.1.3-py2.py3-none-any.whl/bormeparser/backends/pypdf2/functions.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+from PyPDF2 import PdfFileReader
+from collections import OrderedDict
+
+from bormeparser.regex import regex_cargos, regex_empresa, REGEX_TEXT, REGEX_BORME_NUM, REGEX_BORME_CVE
+from bormeparser.acto import ACTO
+
+logger = logging.getLogger(__name__)
+#logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.WARN)
+
+DATA = OrderedDict()
+for key in ('borme_fecha', 'borme_num', 'borme_seccion', 'borme_subseccion', 'borme_provincia', 'borme_cve'):
+    DATA[key] = None
+
+
+def clean_data(data):
+    return data.replace('\(', '(').replace('\)', ')').replace('  ', ' ').strip()
+
+
+def parse_content(content):
+    cabecera = False
+    texto = False
+    data = ""
+    actos = {}
+    nombreacto = None
+    acto_id = None
+    empresa = None
+    fecha = False
+    numero = False
+    seccion = False
+    subseccion = False
+    provincia = False
+    cve = False
+
+    # Python 3
+    if isinstance(content, bytes):
+        content = content.decode('unicode_escape')
+    logger.debug(content)
+
+    for line in content.split('\n'):
+        if line.startswith('/Cabecera_acto'):
+            cabecera = True
+            data = ""
+            actos = {}
+            continue
+
+        if line.startswith('/Texto_acto'):
+            texto = True
+            data = ""
+            continue
+
+        if line.startswith('/Fecha'):
+            if not DATA['borme_fecha']:
+                fecha = True
+            continue
+
+        if line.startswith('/Numero_BORME'):
+            if not DATA['borme_num']:
+                numero = True
+            continue
+
+        if line.startswith('/Seccion'):
+            if not DATA['borme_seccion']:
+                seccion = True
+            continue
+
+        if line.startswith('/Subseccion'):
+            if not DATA['borme_subseccion']:
+                subseccion = True
+            continue
+
+        if line.startswith('/Provincia'):
+            if not DATA['borme_provincia']:
+                provincia = True
+            continue
+
+        if line.startswith('/Codigo_verificacion'):
+            if not DATA['borme_cve']:
+                cve = True
+            continue
+
+        if line == 'BT':
+            # Begin text object
+            continue
+
+        if line == 'ET':
+            # End text object
+            if cabecera:
+                cabecera = False
+                data = clean_data(data)
+                acto_id, empresa = regex_empresa(data)
+            if texto:
+                texto = False
+                data = clean_data(data)
+                actos[nombreacto] = data
+                DATA[acto_id] = {'Empresa': empresa, 'Actos': actos}
+            continue
+
+        if not any([texto, cabecera, fecha, numero, seccion, subseccion, provincia, cve]):
+            continue
+
+        if line == '/F1 8 Tf':
+            # Font 1: bold
+            if nombreacto:
+                data = clean_data(data)
+                if nombreacto in ACTO.CARGOS_KEYWORDS:
+                    data = regex_cargos(data)
+                actos[nombreacto] = data
+            data = ""
+            continue
+
+        if line == '/F2 8 Tf':
+            # Font 2: normal
+
+            # FIXME: Declaración de unipersonalidad. Socio único: GARCIA FUENTES JUAN CARLOS. Nombramientos
+            """
+            if nombreacto and 'Declaración de unipersonalidad' in nombreacto:
+                nombreacto = clean_data(data)[:-1]
+                f = nombreacto.rfind('.')
+                data = nombreacto[f+1:]
+                nombreacto = nombreacto[:f+1]
+                actos[nombreacto] = 'X'
+            """
+            nombreacto = clean_data(data)[:-1]
+            data = ""
+            continue
+
+        m = REGEX_TEXT.match(line)
+        if m:
+            if fecha:
+                DATA['borme_fecha'] = m.group(1)
+                fecha = False
+            if numero:
+                text = m.group(1)
+                DATA['borme_num'] = int(REGEX_BORME_NUM.match(text).group(1))
+                numero = False
+            if seccion:
+                DATA['borme_seccion'] = m.group(1)
+                seccion = False
+            if subseccion:
+                DATA['borme_subseccion'] = m.group(1)
+                subseccion = False
+            if provincia:
+                DATA['borme_provincia'] = m.group(1)
+                provincia = False
+            if cve:
+                text = m.group(1)
+                DATA['borme_cve'] = REGEX_BORME_CVE.match(text).group(1)
+                cve = False
+            logger.debug(m.group(1))
+            data += ' ' + m.group(1)
+
+
+def parse_file(filename):
+    reader = PdfFileReader(open(filename, 'rb'))
+    for n in range(0, reader.getNumPages()):
+        content = reader.getPage(n).getContents().getData()
+        parse_content(content)
+    return DATA
---
+++ packages/bormeparser/bormeparser-0.1.3-py2.py3-none-any.whl/bormeparser/tests/test_bormeregex.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# test_bormeregexp.py -
+# Copyright (C) 2015 Pablo Castellano <pablo@anche.no>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+from bormeparser.regex import regex_cargos, regex_empresa, is_company
+
+DATA = {'fake1': {'Adm. Solid.': {'RAMA SANCHEZ JAVIER JORGE', 'RAMA SANCHEZ JOSE PEDRO'}},
+        'fake2': {'Auditor': {'ACME AUDITORES SL'}, 'Aud.Supl.': {u'MACIAS MUÑOZ FELIPE JOSE'}},
+        'fake3': {'Auditor': {'A.T.A AUDITORES SL'}, 'Aud.Supl.': {u'CUEVAS MUÑOZ SILVIA MARIA'}}}
+
+
+class BormeparserIsCompanyTestCase(unittest.TestCase):
+    empresa1 = 'PATATAS SL'
+    empresa2 = 'HAMBURGUESAS AIE'
+    empresa3 = 'ZANAHORIAS SA'
+    empresa4 = 'COA-COA BARBACOA SRL'
+    persona1 = 'JOHN DOE'
+
+    def test_is_company(self):
+        self.assertTrue(is_company(self.empresa1))
+        self.assertTrue(is_company(self.empresa2))
+        self.assertTrue(is_company(self.empresa3))
+        self.assertTrue(is_company(self.empresa4))
+        self.assertFalse(is_company(self.persona1))
+
+
+class BormeparserRegexEmpresaTestCase(unittest.TestCase):
+    acto1 = '57344 - ALDARA CATERING SL.'
+
+    def test_regex_empresa(self):
+        acto_id, empresa = regex_empresa(self.acto1)
+        self.assertEqual(acto_id, 57344)
+        self.assertEqual(empresa, 'ALDARA CATERING SL')
+
+
+class BormeparserRegexCargosTestCase(unittest.TestCase):
+    nombramientos1 = 'Adm. Solid.: RAMA SANCHEZ JOSE PEDRO;RAMA SANCHEZ JAVIER JORGE.'
+    nombramientos2 = u'Auditor: ACME AUDITORES SL. Aud.Supl.: MACIAS MUÑOZ FELIPE JOSE.'
+    nombramientos3 = u'Auditor: A.T.A AUDITORES SL. Aud.Supl.: CUEVAS MUÑOZ SILVIA MARIA.'
+
+    def test_regex_nombramientos(self):
+        cargos1 = regex_cargos(self.nombramientos1)
+        self.assertEqual(cargos1, DATA['fake1'])
+
+        cargos2 = regex_cargos(self.nombramientos2)
+        self.assertEqual(cargos2, DATA['fake2'])
+
+        cargos3 = regex_cargos(self.nombramientos3)
+        self.assertEqual(cargos3, DATA['fake3'])
+
+if __name__ == '__main__':
+    unittest.main()
