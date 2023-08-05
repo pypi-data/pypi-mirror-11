484e5cb81e068935e07533829caa46e310caf3c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:55 +0000

    Add package gherkin3/3.0.0/gherkin3-3.0.0.tar.gz

---
+++ packages/gherkin3/gherkin3-3.0.0.tar.gz/gherkin3-3.0.0/setup.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+from distutils.core import setup
+setup(
+  name = 'gherkin3',
+  packages = ['gherkin3'],
+  version = '3.0.0',
+  description = 'Gherkin parser',
+  author = 'Björn Rasmusson',
+  author_email = 'cukes@googlegroups.com',
+  url = 'https://github.com/cucumber/gherkin-python',
+  license = 'MIT',
+  download_url = 'https://github.com/cucumber/gherkin-ruby/archive/v3.0.0.tar.gz',
+  keywords = ['gherkin', 'cucumber', 'bdd'],
+  classifiers = [],
+)
