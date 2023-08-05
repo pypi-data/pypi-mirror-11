45717cb3c923b3a36ea3f6d9d1aad8c000150a34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:55 +0000

    Add package logtool/0.2.23/logtool-0.2.23.tar.gz

---
+++ packages/logtool/logtool-0.2.23.tar.gz/logtool-0.2.23/setup.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+
+try:
+  import pyver
+except ImportError:
+  import pip
+  pip.main (['install', 'pyver'])
+  import pyver # pylint: disable=W0611
+
+from setuptools import setup, find_packages
+
+__version__, __version_info__ = pyver.get_version (pkg = "logtool",
+                                                   public = True)
+
+setup (
+    name = "logtool",
+    version = __version__,
+    description = "Methods and tools that assist logging.",
+    long_description = file ("README.rst").read (),
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: "
+        + "GNU General Public License v3 or later (GPLv3+)",
+        "Topic :: Utilities",
+    ],
+    keywords = "logging, exceptions, callpoints, call tracing",
+    author = "J C Lawrence",
+    author_email = "claw@kanga.nu",
+    url = "https://github.com/clearclaw/logtool",
+    download_url = ("https://github.com/clearclaw/logtool/tarball/%s.%s"
+                    % (__version_info__[0], __version_info__[1])),
+    license = "GPL v3.0",
+    packages = find_packages (exclude = ["tests",]),
+    package_data = {},
+    data_files = [],
+    zip_safe = True,
+    install_requires = [line.strip ()
+        for line in file ("requirements.txt").readlines ()],
+    entry_points = {
+        "console_scripts": [
+        ],
+    },
+)
