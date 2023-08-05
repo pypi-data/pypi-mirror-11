f5a97a85e7c7baf94b3d8b525cdc58203c2a2697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:22 +0000

    Add package zschema/0.0.10/zschema-0.0.10.tar.gz

---
+++ packages/zschema/zschema-0.0.10.tar.gz/zschema-0.0.10/zschema/__init__.py
@@ -0,0 +1,27 @@
+from zschema.keys import Port, DataValidationException
+from zschema.leaves import (
+    DateTime,
+    AnalyzedString,
+    String,
+    Binary,
+    IndexedBinary,
+    Boolean,
+    Double,
+    Float,
+    Long,
+    Short,
+    Byte,
+    Integer,
+    IPv4Address
+)
+from zschema.compounds import ListOf, SubRecord, Record
+
+__version__ = "0.0.10"
+
+__schemas = {}
+
+def register_schema(name, schema):
+    __schemas[name] = schema
+
+def get_schema(name):
+    return __schemas[name]
