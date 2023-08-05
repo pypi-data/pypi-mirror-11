df889b6f9f7b400dd0bcab424930f59c8dc037d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:30 +0000

    Add package wechatpy/1.0.0/wechatpy-1.0.0.tar.gz

---
+++ packages/wechatpy/wechatpy-1.0.0.tar.gz/wechatpy-1.0.0/wechatpy/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import, unicode_literals
+
+from wechatpy.parser import parse_message  # NOQA
+from wechatpy.replies import create_reply  # NOQA
+from wechatpy.client import WeChatClient  # NOQA
+from wechatpy.exceptions import WeChatException  # NOQA
+from wechatpy.oauth import WeChatOAuth  # NOQA
+from wechatpy.pay import WeChatPay  # NOQA
+
+
+__version__ = '1.0.0'
+__author__ = 'messense'
