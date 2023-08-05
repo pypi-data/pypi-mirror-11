f405eb32833e81e21d334c5ee554cbad5ee13cc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:58 +0000

    Add package pandoc-eqnos/0.3/pandoc-eqnos-0.3.tar.gz

---
+++ packages/pandoc-eqnos/pandoc-eqnos-0.3.tar.gz/pandoc-eqnos-0.3/setup.py
@@ -0,0 +1,37 @@
+"""setup.py - install script for pandoc-eqnos."""
+
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup
+
+LONG_DESCRIPTION = """\
+pandoc-eqnos is a pandoc filter for numbering equations and equation references.
+"""
+
+setup(
+    name='pandoc-eqnos',
+    version='0.3',
+
+    author='Thomas J. Duck',
+    author_email='tomduck@tomduck.ca',
+    description='Equation number filter for pandoc',
+    long_description=LONG_DESCRIPTION,
+    license='GPL',
+    keywords='pandoc equation numbers filter',
+    url='https://github.com/tomduck/pandoc-eqnos',
+    download_url = 'https://github.com/tomduck/pandoc-eqnos/tarball/0.3',
+
+    install_requires=['pandocfilters', 'pandoc-attributes'],
+
+    py_modules=['pandoc_eqnos'],
+    entry_points={'console_scripts':['pandoc-eqnos = pandoc_eqnos:main']},
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python'
+        ]
+)
