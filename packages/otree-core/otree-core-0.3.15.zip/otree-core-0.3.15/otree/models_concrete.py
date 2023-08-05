1e4de2d9e70c563e4e9777e4f4dafd9e82e4b345
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:41 +0000

    Add package otree-core/0.3.15/otree-core-0.3.15.zip

---
+++ packages/otree-core/otree-core-0.3.15.zip/otree-core-0.3.15/otree/models_concrete.py
@@ -0,0 +1,69 @@
+from otree.db import models
+
+
+class PageCompletion(models.Model):
+    app_name = models.CharField(max_length=300)
+    player_pk = models.PositiveIntegerField()
+    page_index = models.PositiveIntegerField()
+    page_name = models.CharField(max_length=300)
+    time_stamp = models.PositiveIntegerField()
+    seconds_on_page = models.PositiveIntegerField()
+    subsession_pk = models.PositiveIntegerField()
+    participant_pk = models.PositiveIntegerField()
+    session_pk = models.PositiveIntegerField()
+
+
+class WaitPageVisit(models.Model):
+    '''difference between this and PageVisit model is that this is run
+    when the player first loads the page, rather than when they leave
+
+    '''
+    session_pk = models.PositiveIntegerField()
+    page_index = models.PositiveIntegerField()
+
+    id_in_session = models.PositiveIntegerField()
+
+
+class PageTimeout(models.Model):
+    participant_pk = models.PositiveIntegerField()
+    page_index = models.PositiveIntegerField()
+    expiration_time = models.PositiveIntegerField()
+
+
+class CompletedGroupWaitPage(models.Model):
+    page_index = models.PositiveIntegerField()
+    session_pk = models.PositiveIntegerField()
+    group_pk = models.PositiveIntegerField()
+
+
+class CompletedSubsessionWaitPage(models.Model):
+    page_index = models.PositiveIntegerField()
+    session_pk = models.PositiveIntegerField()
+
+
+class SessionuserToUserLookup(models.Model):
+    session_user_pk = models.PositiveIntegerField()
+    page_index = models.PositiveIntegerField()
+    app_name = models.CharField(max_length=300)
+    user_pk = models.PositiveIntegerField()
+
+
+class GroupSize(models.Model):
+    app_label = models.CharField(max_length=300)
+    subsession_pk = models.PositiveIntegerField()
+    group_index = models.PositiveIntegerField()
+    group_size = models.PositiveIntegerField()
+
+
+class ParticipantLockModel(models.Model):
+    participant_code = models.CharField(max_length=10)
+
+
+class StubModel(models.Model):
+    """To be used as the model for an empty form, so that form_class can be
+    omitted. Consider using SingletonModel for this. Right now, I'm not
+    sure we need it.
+
+    """
+
+    pass
