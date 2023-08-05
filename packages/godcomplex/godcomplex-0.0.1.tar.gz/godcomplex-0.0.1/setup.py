a4f3a53c82dfca957c50f3599b6f21b6545ad417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:57 +0000

    Add package godcomplex/0.0.1/godcomplex-0.0.1.tar.gz

---
+++ packages/godcomplex/godcomplex-0.0.1.tar.gz/godcomplex-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name="godcomplex",
+	version="0.0.1",
+	description="this will say something if you say god",
+	author="Isaac Robinson",
+	author_email="isaacrob@me.com",
+	license="GNU",
+	packages=["godcomplex"],
+	keywords=["god","irony","speech recognition"],
+	install_requires=['SpeechRecognition','espeak'],
+	zip_safe=False)
