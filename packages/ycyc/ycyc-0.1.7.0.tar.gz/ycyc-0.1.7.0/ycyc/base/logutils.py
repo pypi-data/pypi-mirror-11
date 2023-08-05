b074dcf2c38a1a3f20779450d448e8df57cb8c84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:54 +0000

    Add package ycyc/0.1.7.0/ycyc-0.1.7.0.tar.gz

---
+++ packages/ycyc/ycyc-0.1.7.0.tar.gz/ycyc-0.1.7.0/ycyc/base/logutils.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import contextlib
+import logging.config
+import logging
+
+
+def quick_config(log_file="application.log"):
+    """
+    a quick config for logging
+
+    :param log_file: the file for file handler
+    """
+    config = {
+        'version': 1,
+        'disable_existing_loggers': True,
+        'formatters': {
+            'verbose': {
+                'format': '%(asctime)s %(levelname)s %(name)s %(message)s',
+                'datefmt': '[%d/%b/%Y %H:%M:%S]',
+            },
+            'simple': {
+                'format': '%(asctime)s %(levelname)s %(name)s:%(lineno)s %(message)s',
+                'datefmt': '[%d/%b/%Y %H:%M:%S]',
+            },
+        },
+        'handlers': {
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'formatter': 'simple'
+            },
+            'file': {
+                'level': 'INFO',
+                'class': 'logging.FileHandler',
+                'filename': log_file,
+                'formatter': 'verbose'
+            },
+        },
+        'loggers': {
+            '': {
+                'handlers': ['console', 'file'],
+                'level': 'DEBUG',
+            },
+        },
+    }
+    logging.config.dictConfig(config)
+
+
+@contextlib.contextmanager
+def log_disable(level=logging.CRITICAL):
+    """
+    A context manager to disable all logging calls of
+    severity 'level' and below in block
+    """
+    level, logging.root.manager.disable = logging.root.manager.disable, level
+    try:
+        yield
+    finally:
+        level, logging.root.manager.disable = logging.root.manager.disable, level
+
+
+def log_with_label(log_method, label):
+    """
+    >>> logger_waring = log_with_label(logger.waring, "waring: %s")
+    >>> try:
+    ...     may_raise_exception()
+    ... except Exception as err:
+    ...     logger_waring(err)
+
+    :param log_method: method of a logger to pack
+    :param label: label pass to log_method as the first argument
+    :return: logger method packed method
+    """
+    import functools
+
+    @functools.wraps(log_method)
+    def logger_method(*args, **kwargs):
+        return log_method(label, *args, **kwargs)
+
+    return logger_method
+
+
+class LogFunctionCall(object):
+    def __init__(self, func=None, args=None, kwargs=None, returned=None):
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+        self.returned = returned
+
+    def __str__(self):
+        s_returned = (
+            "" if self.returned is None
+            else "-> %s" % str(self.returned)
+        )
+
+        args = map(str, self.args or ())
+        args.extend(
+            "{k}={v}".format(k=str(k), v=str(v))
+            for k, v in self.kwargs.items() or ()
+        )
+
+        return "{f}({args}){returned}".format(
+            f=self.func.__name__ if self.func else "",
+            args=", ".join(args),
+            returned="" if self.returned is None
+            else "-> %s" % str(self.returned)
+        )
