454bea61d9beb2727db041735f46f32d6ec7892a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:17 +0000

    Add package js.expect/0.3.1/js.expect-0.3.1.tar.gz

---
+++ packages/js.expect/js.expect-0.3.1.tar.gz/js.expect-0.3.1/js/expect/__init__.py
@@ -0,0 +1,7 @@
+from fanstatic import Library, Resource
+
+library = Library('expect.js', 'resources')
+
+# Define the resources in the library like this.
+# For options and examples, see the fanstatic documentation.
+expect = Resource(library, 'index.js')
