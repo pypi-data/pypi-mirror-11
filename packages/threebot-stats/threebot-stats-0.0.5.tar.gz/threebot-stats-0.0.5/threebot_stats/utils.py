e3d69930dd4488e64b71a77958814c769a8b835d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:04 +0000

    Add package threebot-stats/0.0.5/threebot-stats-0.0.5.tar.gz

---
+++ packages/threebot-stats/threebot-stats-0.0.5.tar.gz/threebot-stats-0.0.5/threebot_stats/utils.py
@@ -0,0 +1,37 @@
+''' Utility functions that gather statistical information on a Workflow
+'''
+
+''' Number of logs for Workflow '''
+def count_logs(workflow):
+    return workflow.workflowlog_set.count()
+
+
+''' Log execution time as a timedelta object '''
+def response_time(workflow_log):
+    diff = workflow_log.date_finished - workflow_log.date_started
+    return diff.total_seconds()
+
+
+''' Filter logs with start and end time '''
+def _executed_logs(workflow):
+    return workflow.workflowlog_set.exclude(date_finished__isnull=True, date_started__isnull=True)
+
+
+def average_response_time(workflow):
+    times = [response_time(log) for log in _executed_logs(workflow)]
+    if times:
+        return sum(times) / float(len(times))
+    return 0
+
+
+''' Response times of last n logs in seconds, as a list '''
+def response_time_series(workflow, n):
+    recent_logs = list(_executed_logs(workflow).order_by('-date_finished'))[-n:]
+    return [response_time(log) for log in recent_logs]
+
+
+''' Total of seconds the Workflow has run '''
+def impact(workflow):
+    logs = _executed_logs(workflow)
+    seconds = sum([response_time(log) for log in logs])
+    return seconds
---
+++ packages/threebot-stats/threebot-stats-0.0.5.tar.gz/threebot-stats-0.0.5/threebot_stats/views.py
@@ -0,0 +1,24 @@
+from django.http import Http404, HttpResponse
+from django.shortcuts import render
+
+from threebot.models import Workflow
+from threebot_stats.utils import *
+
+
+def index(request):
+    return HttpResponse("Hello, world.")
+
+
+def detail(request, workflow_slug):
+    try:
+        workflow = Workflow.objects.get(slug=workflow_slug)
+    except Workflow.DoesNotExist:
+        raise Http404("Workflow does not exist")
+
+    data = {
+        'num_logs': count_logs(workflow),
+        'impact': impact(workflow),
+        'response_time_series': response_time_series(workflow, 10),
+    }
+
+    return render(request, 'threebot_stats/workflow_stats.html', {'data': data, 'workflow': workflow})
