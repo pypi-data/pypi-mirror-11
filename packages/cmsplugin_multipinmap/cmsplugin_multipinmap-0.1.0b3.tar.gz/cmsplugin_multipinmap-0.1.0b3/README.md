1f3855d6c5609c4edfa190511c9953e68b3ad277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:24 +0000

    Add package cmsplugin_multipinmap/0.1.0b3/cmsplugin_multipinmap-0.1.0b3.tar.gz

---
+++ packages/cmsplugin_multipinmap/cmsplugin_multipinmap-0.1.0b3.tar.gz/cmsplugin_multipinmap-0.1.0b3/README.md
@@ -0,0 +1,27 @@
+[![Build Status](https://travis-ci.org/creimers/cmsplugin_multipinmap.svg?branch=master)](https://travis-ci.org/creimers/cmsplugin_multipinmap)
+[![Coverage Status](https://coveralls.io/repos/creimers/cmsplugin_multipinmap/badge.svg?branch=master)](https://coveralls.io/r/creimers/cmsplugin_multipinmap?branch=master)
+[![Latest Version](https://img.shields.io/pypi/v/cmsplugin_multipinmap.svg)](https://pypi.python.org/pypi/cmsplugin_multipinmap)
+[![Supported Python versions](https://img.shields.io/pypi/pyversions/cmsplugin_multipinmap.svg)](https://pypi.python.org/pypi/cmsplugin_multipinmap)
+[![Status](https://img.shields.io/pypi/status/cmsplugin_multipinmap.svg)](https://pypi.python.org/pypi/cmsplugin_multipinmap)
+
+# djangocms multi pin map plugin
+
+A djangocms map plugin that can display multiple pins. Avaliable as google maps or [leaflet](http://leafletjs.com/) style.
+
+![preview](preview.png)
+
+## Installation
+
+* ``pip install cmsplugin_multipinmap``
+
+* Add
+
+  ```
+  'cmsplugin_multipinmap',
+  ```
+
+  to ``INSTALLED_APPS``.
+
+* Sync the database
+
+* For leaflet, [mapbox](https://www.mapbox.com/) is used as map provider. You can add ``MAPBOX_ACCESS_TOKEN`` and ``MAPBOX_MAP_ID`` default values to ``settings.py``.
---
+++ packages/cmsplugin_multipinmap/cmsplugin_multipinmap-0.1.0b3.tar.gz/cmsplugin_multipinmap-0.1.0b3/cmsplugin_multipinmap/tests.py
@@ -0,0 +1,82 @@
+from django.test import TestCase
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+from cms import api
+from cms.models import CMSPlugin
+from cms.test_utils.testcases import BaseCMSTestCase
+from cms.utils import get_cms_setting
+
+
+from . import models
+from . import cms_plugins
+
+
+class MultipinmapTestCase(TestCase, BaseCMSTestCase):
+    su_username = 'user'
+    su_password = 'pass'
+
+    def setUp(self):
+        self.template = get_cms_setting('TEMPLATES')[0][0]
+        self.language = settings.LANGUAGES[0][0]
+        self.page = api.create_page('page', self.template, self.language, published=True)
+        self.placeholder = self.page.placeholders.all()[0]
+        self.superuser = self.create_superuser()
+
+    def tearDown(self):
+        self.client.logout()
+
+    def create_superuser(self):
+        return User.objects.create_superuser(self.su_username, 'email@example.com', self.su_password)
+
+    def test_add_multipinmap_plugin_google(self):
+        street = 'Hongkongstrasse 10'
+        postal_code = '20457'
+        city = 'Hamburg'
+        multipinmap_plugin = api.add_plugin(
+           self.placeholder,
+           cms_plugins.MapPlugin,
+           self.language,
+           name='test',
+           style='google',
+           street=street,
+           postal_code=postal_code,
+           city=city
+        )
+        pin1 = models.Pin(
+            name="greenpeace",
+            street=street,
+            postal_code=postal_code,
+            city=city,
+            map_plugin=multipinmap_plugin
+        )
+        pin1.save()
+
+        self.assertTrue(pin1.__str__() == 'greenpeace')
+        self.assertTrue(
+            models.Map.objects.filter(pk=multipinmap_plugin.pk).exists()
+        )
+
+    def test_add_multipinmap_plugin_leaflet_error(self):
+        street = 'Hongkongstrasse 10'
+        postal_code = '20457'
+        city = 'Hamburg'
+        multipinmap_plugin = api.add_plugin(
+           self.placeholder,
+           cms_plugins.MapPlugin,
+           self.language,
+           name='test',
+           style='leaflet',
+           street=street,
+           postal_code=postal_code,
+           city=city
+        )
+        self.assertRaises(ValidationError, multipinmap_plugin.full_clean)
+
+    def test_render_page(self):
+        self.test_add_multipinmap_plugin_google()
+        api.publish_page(self.page, self.superuser, self.language)
+        response = self.client.get(self.page.get_absolute_url())
+
+        self.assertTrue("map" in response.rendered_content)
