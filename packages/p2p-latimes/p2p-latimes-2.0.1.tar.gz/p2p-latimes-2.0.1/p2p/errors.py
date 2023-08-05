7733ea592e5f2d07799572dfdc6addfb518e429d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:03 +0000

    Add package p2p-latimes/2.0.1/p2p-latimes-2.0.1.tar.gz

---
+++ packages/p2p-latimes/p2p-latimes-2.0.1.tar.gz/p2p-latimes-2.0.1/p2p/errors.py
@@ -0,0 +1,18 @@
+class P2PException(Exception):
+    pass
+
+
+class P2PSlugTaken(P2PException):
+    pass
+
+
+class P2PNotFound(P2PException):
+    pass
+
+
+class P2PUniqueConstraintViolated(P2PException):
+    pass
+
+
+class P2PForbidden(P2PException):
+    pass
