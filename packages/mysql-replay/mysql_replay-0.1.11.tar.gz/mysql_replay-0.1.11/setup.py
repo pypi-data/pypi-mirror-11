250c41eb8c6610020f44e83d1a2c87acc475fb93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:15 +0000

    Add package mysql-replay/0.1.11/mysql_replay-0.1.11.tar.gz

---
+++ packages/mysql-replay/mysql_replay-0.1.11.tar.gz/mysql_replay-0.1.11/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name="mysql_replay",
+    version="0.1.11",
+    author="Hugo Chinchilla",
+    author_email="hchinchilla@habitissimo.com",
+    url='https://pypi.python.org/pypi/mysql-replay',
+    license='GPLv3',
+    keywords='mysql bechmarking log slow',
+    packages=[
+        "mysql_replay",
+        "mysql_replay.mysql",
+        "mysql_replay.mysql.utilities",
+        "mysql_replay.mysql.utilities.common",
+    ],
+    include_package_data=True,
+    description="Replay queries from a mysql slow log",
+    install_requires=[
+        "mysqlclient",
+    ],
+    entry_points={
+        'console_scripts': [
+            'mysql-replay = mysql_replay.execute:main',
+            'mysql-replay-prepare = mysql_replay.prepare:main',
+        ],
+    }
+)
