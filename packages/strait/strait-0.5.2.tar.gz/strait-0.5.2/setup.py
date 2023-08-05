92276cc50c0db30b0c65e60695463411fe3429c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:41 +0000

    Add package strait/0.5.2/strait-0.5.2.tar.gz

---
+++ packages/strait/strait-0.5.2.tar.gz/strait-0.5.2/setup.py
@@ -0,0 +1,32 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+try:
+    docfile = open('/tmp/docs.rst')
+except IOError:  # file not found, ignore
+    doc = ''
+else:
+    doc = docfile.read()
+    docfile.close()
+
+VERSION = '0.5.2'
+setup(name='strait',
+      version=VERSION,
+      description='Simple Traits for Python',
+      long_description=doc,
+      author='Michele Simionato',
+      author_email='michele.simionato@gmail.com',
+      url='http://pypi.python.org/pypi/strait',
+      license="BSD License",
+      py_modules=['strait'],
+      keywords='',
+      platforms=['any'],
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Natural Language :: English',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Software Development :: Libraries'],
+      zip_safe=False)
