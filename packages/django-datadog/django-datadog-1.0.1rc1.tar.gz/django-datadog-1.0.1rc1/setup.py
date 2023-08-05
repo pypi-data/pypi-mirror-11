ea016d1423b3ceadfd5e502d9736b3eafa977138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:45 +0000

    Add package django-datadog/1.0.1rc1/django-datadog-1.0.1rc1.tar.gz

---
+++ packages/django-datadog/django-datadog-1.0.1rc1.tar.gz/django-datadog-1.0.1rc1/setup.py
@@ -0,0 +1,27 @@
+import os
+import sys
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+reqs = []
+if [sys.version_info[0], sys.version_info[1]] < [2, 7]:
+    reqs.append("simplejson>=2.0.9")
+
+setup(
+    name='django-datadog',
+    version='1.0.1rc1',
+    packages=[
+        'djdatadog',
+        'djdatadog.middleware'
+    ],
+    include_package_data=True,
+    license='BSD',
+    description='simple Django middleware for submitting timings and exceptions to Datadog.',
+    long_description=README,
+    author='Chris Heisel',
+    author_email='cheisel@kabbage.com',
+    install_requires=reqs.extend([
+        'datadog==0.4.0'
+    ])
+)
