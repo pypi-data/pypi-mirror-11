3c401ba41ad430b1ac07ab4e5a8a97cc6f2a4beb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:17 +0000

    Add package pyspriter/0.4/pyspriter-0.4.tar.gz

---
+++ packages/pyspriter/pyspriter-0.4.tar.gz/pyspriter-0.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='pyspriter',
+	  version='0.4',
+	  description='Command line sprite generator module for Python',
+	  url='https://github.com/halilkaya/pyspriter',
+	  author='Halil Kaya',
+	  author_email='halil@halilkaya.net',
+	  license='MPLv2',
+	  packages=['pyspriter'],
+	  scripts=['pyspriter/pyspriter.py'],
+	  install_requires=['setuptools'],
+	  py_modules=['pyspriter'],
+	  zip_safe=False)
