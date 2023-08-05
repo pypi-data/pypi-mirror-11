fcf7a69f58827b5f6fd1c674490a37568408eb31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:17 +0000

    Add package first_hh/1.3.0/first_hh-1.3.0.zip

---
+++ packages/first_hh/first_hh-1.3.0.zip/first_hh-1.3.0/first_hh.py
@@ -0,0 +1,13 @@
+"""This is "first_hh"block,it provided a print_lol() function to print list"""
+def print_lol(the_list,indent=False,level=0):
+    """this function have a position parameter,which name is the_list,
+        every data in the list will print in the screen ,the second parameter
+        is used to insert tab"""
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,indent,level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t",end='')
+            print(each_item)
---
+++ packages/first_hh/first_hh-1.3.0.zip/first_hh-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name         = 'first_hh',
+    version      = '1.3.0',
+    py_modules   = ['first_hh'],
+    author       = 'mia',
+    author_email = 'mia@pratice.com',
+    url          = 'http://www.practice.com',
+    description  = 'A simple example',
+    )
