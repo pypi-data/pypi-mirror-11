75a9e82734af329649d897f7e41de4de536430c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:28 +0000

    Add package WeRoBot/0.6.1/WeRoBot-0.6.1.tar.gz

---
+++ packages/WeRoBot/WeRoBot-0.6.1.tar.gz/WeRoBot-0.6.1/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+import werobot
+
+from setuptools import setup, find_packages
+
+setup(
+    name='WeRoBot',
+    version=werobot.__version__,
+    author=werobot.__author__,
+    author_email='whtsky@me.com',
+    url='https://github.com/whtsky/WeRoBot',
+    packages=find_packages(),
+    keywords="wechat weixin werobot",
+    description='WeRoBot: writing WeChat Offical Account Robots with fun',
+    long_description=open("README.rst").read().replace("latest", werobot.__version__).replace("develop", "master"),
+    install_requires=open("requirements.txt").readlines(),
+    include_package_data=True,
+    license='MIT License',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+    tests_require=['nose'],
+    test_suite='nose.collector',
+)
