8ab69793b26f1d2125b73324758ff94018415880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:01 +0000

    Add package clusterlogger/0.1.0/clusterlogger-0.1.0-py2.py3-none-any.whl

---
+++ packages/clusterlogger/clusterlogger-0.1.0-py2.py3-none-any.whl/clusterlogger/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+from .logfilter import *
+
+__all__ = [logfilter.__all__]
+
+__author__ = 'David Zuber'
+__email__ = 'zuber.david@gmx.de'
+__version__ = '0.1.0'
