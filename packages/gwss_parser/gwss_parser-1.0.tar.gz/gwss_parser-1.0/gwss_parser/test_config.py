363a2e92afa553f415dd1c3b52ff3fec933524c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:38 +0000

    Add package gwss_parser/1.0/gwss_parser-1.0.tar.gz

---
+++ packages/gwss_parser/gwss_parser-1.0.tar.gz/gwss_parser-1.0/gwss_parser/test_config.py
@@ -0,0 +1,6 @@
+ALL_URL = ['www.test1.com', 'www.test2.com', 'www.test2.com/hello/']
+#ALL_URL = ['www.test2.com/hello/']
+#ALL_URL = ['www.test2.com/notfound/']
+#ALL_URL = ['www.test2.com']
+N = 10000
+TEST_SLEEP = 0.02
