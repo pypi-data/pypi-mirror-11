c9636ab55a4438c1c8f9105aec22d78ef8a643f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:17 +0000

    Add package Flask-Alembic/1.2/Flask-Alembic-1.2.tar.gz

---
+++ packages/Flask-Alembic/Flask-Alembic-1.2.tar.gz/Flask-Alembic-1.2/flask_alembic/__init__.py
@@ -0,0 +1,13 @@
+"""
+flask_alembic
+-------------
+
+.. autoclass:: flask_alembic.Alembic
+    :members:
+    :member-order: bysource
+
+.. automodule:: flask_alembic.cli
+    :members:
+"""
+
+from flask_alembic.extension import Alembic
