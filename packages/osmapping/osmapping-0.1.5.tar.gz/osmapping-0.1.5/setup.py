321348d8ab4c636445ebad9270136c84ce07ac5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:31 +0000

    Add package osmapping/0.1.5/osmapping-0.1.5.tar.gz

---
+++ packages/osmapping/osmapping-0.1.5.tar.gz/osmapping-0.1.5/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='osmapping',
+    version='0.1.5',
+    packages=[''],
+    url='https://github.com/astyler/osmapping',
+    download_url='https://github.com/astyler/osmapping/tarball/0.1.5',
+    license='MIT',
+    author='Alex Styler',
+    author_email='astyler@gmail.com',
+    description='Mapping Library for Open Street Maps'
+)
