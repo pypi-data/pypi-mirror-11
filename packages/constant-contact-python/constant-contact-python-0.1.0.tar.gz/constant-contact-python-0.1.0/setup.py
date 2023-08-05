55d7ec61ba20bc44330ae032e73c9584c67f26a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:55 +0000

    Add package constant-contact-python/0.1.0/constant-contact-python-0.1.0.tar.gz

---
+++ packages/constant-contact-python/constant-contact-python-0.1.0.tar.gz/constant-contact-python-0.1.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+try:
+  from setuptools import setup
+except ImportError:
+  from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+  os.system('python setup.py sdist upload')
+  sys.exit()
+
+requires = [
+  'requests==2.6.0'
+]
+
+setup(
+  name='constant-contact-python',
+  version='0.1.0',
+  description='Python 3 api sdk for constantcontact.com',
+  author='Jordan Clark',
+  author_email='jordan@blitzen.com',
+  packages=['constantcontact'],
+  install_requires=requires,
+  license='MIT',
+  zip_safe=False,
+  classifiers=(
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Natural Language :: English',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+  )
+)
