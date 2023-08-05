d78ab05f75529ccb88f554c9081b6b2f4dd5e441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:15 +0000

    Add package gorgon/0.3/gorgon-0.3.tar.gz

---
+++ packages/gorgon/gorgon-0.3.tar.gz/gorgon-0.3/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    # metadata
+    name='gorgon',
+    description='A simple multiplier task analysis tool in Python',
+    license='MIT',
+    version='0.3',
+    author='Jaime Buelta',
+    author_email='jaime.buelta@gmail.com',
+    url='https://github.com/jaimebuelta/gorgon',
+    download_url='https://github.com/jaimebuelta/gorgon/tarball/0.3',
+    platforms='Cross Platform',
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords=['multiprocess', 'multithread', 'analysis'],
+    scripts=['scripts/gorgon_cluster'],
+    install_requires=['paramiko'],
+    packages=['gorgon'],
+)
