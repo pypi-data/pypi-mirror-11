ca18a28256228db0c098951743647a3098b0771e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:26 +0000

    Add package coverage-tools/0.0.4/coverage-tools-0.0.4.tar.gz

---
+++ packages/coverage-tools/coverage-tools-0.0.4.tar.gz/coverage-tools-0.0.4/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import unittest
+from setuptools import setup, find_packages, Command
+
+class TestRunner(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        tests = unittest.defaultTestLoader.discover('.')
+        runner = unittest.runner.TextTestRunner()
+        result = runner.run(tests)
+        sys.exit(not result.wasSuccessful())
+
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+config = {
+    'name' : 'coverage-tools',
+    'version' : '0.0.4',
+    'packages' : find_packages(),
+    'scripts' : [
+        'coverage-annotate',
+        'coverage-combine',
+        'coverage-diff',
+    ],
+    'author' : 'Alexander Todorov',
+    'author_email' : 'atodorov@nospam.otb.bg',
+    'license' : 'MIT',
+    'description' : 'Additional tools for Python coverage',
+    'long_description' : long_description,
+    'url' : 'https://github.com/atodorov/coverage-tools',
+    'keywords' : ['code', 'coverage', 'testing'],
+    'classifiers' : [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Testing ',
+    ],
+    'zip_safe' : False,
+    'install_requires' : ['coverage'],
+    'cmdclass': {'test': TestRunner},
+}
+
+setup(**config)
