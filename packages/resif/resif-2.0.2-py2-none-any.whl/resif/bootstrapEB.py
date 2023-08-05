ae692936fb3ca153edd5c925016d369b7fc8cc99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:50 +0000

    Add package resif/2.0.2/resif-2.0.2-py2-none-any.whl

---
+++ packages/resif/resif-2.0.2-py2-none-any.whl/resif/bootstrapEB.py
@@ -0,0 +1,232 @@
+#######################################################################################################################
+# Author: Maxime Schmitt
+# Mail: maxime.schmitt@telecom-bretagne.eu
+# Overview: Module that take care of bootstraping EasyBuild.
+#######################################################################################################################
+
+import os
+import re
+import sys
+import shutil
+import subprocess
+
+sys.path.append('.')
+from configManager import getEasyBuildVersion
+
+#######################################################################################################################
+# Function that do the bootstrap using the other functions of this module and return the associated modulePath.
+def bootstrap(hashTable):
+
+	# We install all the necessary files of EasyBuild.
+	easybuildFilesInstaller(hashTable)
+
+	# We create the modulefile of EasyBuild.
+	eb_version = getEasyBuildVersion(hashTable['rootinstall'])
+	modulefileCreator(hashTable, "install-" + eb_version)
+
+	# We create some files to source to rapidly start using the bootstrap module and the associated softwares.
+	modulePath = sourcefileCreator(hashTable)
+
+	return modulePath
+
+#######################################################################################################################
+
+
+#######################################################################################################################
+# Functions that do the different important tasks of the bootstrap.
+
+# Get and put at the right place all the EasyBuild files.
+def easybuildFilesInstaller(hashTable):
+	# We create the directory that we need to install the EasyBuild sources
+	easyBuildDir = os.path.join(hashTable['rootinstall'], '.installRef')
+	if not os.path.exists(easyBuildDir):
+		os.makedirs(easyBuildDir)
+
+	ebSources = {}
+	# If we were provided alternative sources for the EasyBuild files, we create the useful variables
+	if any(True for x in ['gh_ebuser', 'git_ebframework', 'git_ebblocks', 'git_ebconfigs'] if x in hashTable):
+		if 'gh_ebuser' in hashTable:
+			ebSources['easybuild-framework'] = ('https://github.com/' + hashTable['gh_ebuser'] + '/easybuild-framework.git', \
+				hashTable['branch_ebframework'] if 'branch_ebframework' in hashTable else None)
+
+			ebSources['easybuild-easyblocks'] = ('https://github.com/' + hashTable['gh_ebuser'] + '/easybuild-easyblocks.git', \
+				hashTable['branch_ebblocks'] if 'branch_ebblocks' in hashTable else None)
+
+			ebSources['easybuild-easyconfigs'] = ('https://github.com/' + hashTable['gh_ebuser'] + '/easybuild-easyconfigs.git', \
+				hashTable['branch_ebconfigs'] if 'branch_ebconfigs' in hashTable else None)
+
+		if 'git_ebframework' in hashTable:
+			ebSources['easybuild-framework'] = (hashTable['git_ebframework'], hashTable['branch_ebframework'] if 'branch_ebframework' in hashTable else None)
+	
+		for v in ['blocks', 'configs']:
+			if 'git_eb'+v in hashTable:
+				ebSources['easybuild-easy'+v] = (hashTable['git_eb'+v], hashTable['branch_eb'+v] if 'branch_eb'+v in hashTable else None)
+
+	# Import any alternative EasyBuild files from given sources
+	for k,v in ebSources.iteritems():
+		if v[1] != None:
+			subprocess.check_call(['git', 'clone', v[0], '-b', v[1], os.path.join(os.path.join(hashTable['rootinstall'], '.installRef'), k)])
+		else:
+			subprocess.check_call(['git', 'clone', v[0], os.path.join(os.path.join(hashTable['rootinstall'], '.installRef'), k)])
+
+	# If the backward compability is activated, we complete the sources with the ones from the subtrees.
+	if 'subtrees' in hashTable and hashTable['subtrees']:
+		# Complete the EasyBuild files with the ones from the subtree if necessary
+		if any(True for x in ['easybuild-framework', 'easybuild-easyblocks', 'easybuild-easyconfigs'] if not x in ebSources):
+			pwd = os.getcwd()
+			os.chdir(hashTable['srcpath'])
+	
+			for k in ['easybuild-framework', 'easybuild-easyblocks', 'easybuild-easyconfigs']:
+				if not k in ebSources:
+					originalBranch = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD'])[:-1]
+					if hashTable['release'] != 'HEAD':
+						subprocess.check_call(['git', 'branch', k, hashTable['release']])
+						subprocess.check_call(['git', 'checkout', k])
+					elif 'branch' in hashTable:
+						subprocess.check_call(['git', 'branch', k, hashTable['branch']])
+						subprocess.check_call(['git', 'checkout', k])
+					else:
+						subprocess.check_call(['git', 'branch', k])
+					subprocess.check_call(['git', 'filter-branch', '-f', '--subdirectory-filter', 'easybuild/'+k, k])
+					subprocess.check_call(['git', 'clone', hashTable['srcpath'], '-b', k, os.path.join(os.path.join(hashTable['rootinstall'], '.installRef'), k)])
+					subprocess.check_call(['git', 'checkout', originalBranch])
+					subprocess.check_call(['git', 'branch', '-D', k])
+			
+			os.chdir(pwd)
+	# If not, we output the missing components and exit.
+	else:
+		if any(True for x in ['easybuild-framework', 'easybuild-easyblocks', 'easybuild-easyconfigs'] if not x in ebSources):
+			for k in ['easybuild-framework', 'easybuild-easyblocks', 'easybuild-easyconfigs']:
+				if not k in ebSources:
+					sys.stdout.write('Missing sources for ' + k + '\n')
+			sys.stdout.write('Please provide complete EasyBuild sources to allow complete installation.\n')
+			exit(60)
+	# Adding vsc-base from the hpcugent git repository
+	subprocess.check_call(['git', 'clone', 'https://github.com/hpcugent/vsc-base.git', os.path.join(os.path.join(hashTable['rootinstall'], '.installRef'), 'vsc-base')])
+
+
+# Create the EasyBuild module file and the associated symlink and put them at the right places.
+# If the ThematicMNS module naming scheme is used, it is also installed.
+def modulefileCreator(hashTable, moduleName):
+	modulesDirPath = os.path.join(os.path.join(hashTable['rootinstall'], 'core'), 'modules')
+	# Adapt the location of the modulfile to the chosen MNS
+	if hashTable['mns'] == "ThematicMNS":
+		# We create the directories we need to install EasyBuild
+		EBmoduleDir = os.path.join('base', 'EasyBuild')
+		easybuildPath = os.path.join(os.path.join(modulesDirPath, 'all'), EBmoduleDir)
+		if not os.path.exists(easybuildPath):
+			os.makedirs(easybuildPath)
+		easybuildPath = os.path.join(os.path.join(modulesDirPath, 'base'), EBmoduleDir)
+		if not os.path.exists(easybuildPath):
+			os.makedirs(easybuildPath)
+		# We install the ThematicMNS
+		setThematicMNS(hashTable)
+
+	else:
+		# We create the directories we need to install EasyBuild
+		EBmoduleDir = 'EasyBuild'
+		easybuildPath = os.path.join(os.path.join(modulesDirPath, 'all'), EBmoduleDir)
+		if not os.path.exists(easybuildPath):
+			os.makedirs(easybuildPath)
+		easybuildPath = os.path.join(os.path.join(modulesDirPath, 'base'), EBmoduleDir)
+		if not os.path.exists(easybuildPath):
+			os.makedirs(easybuildPath)
+
+	# Path to the actual module file
+	moduleFilePath = os.path.join(os.path.join(os.path.join(modulesDirPath, 'all'), EBmoduleDir), moduleName)
+
+	with open(moduleFilePath, "w") as f:
+		f.write("\
+#%Module\n\
+\n\
+proc ModulesHelp { } {\n\
+	puts stderr {   EasyBuild is a software build and installation framework\n\
+written in Python that allows you to install software in a structured,\n\
+repeatable and robust way. - Homepage: http://hpcugent.github.com/easybuild/\n\
+This module provides the development version of EasyBuild.\n\
+}\n\
+}\n\
+module-whatis {EasyBuild is a software build and installation framework\n\
+written in Python that allows you to install software in a structured,\n\
+repeatable and robust way. - Homepage: http://hpcugent.github.com/easybuild/\n\
+This module provides the development version of EasyBuild.\n\
+}\n\
+set root    " + os.path.join(hashTable['rootinstall'], '.installRef') + "\n\
+conflict    EasyBuild\n\
+prepend-path    PATH            \"$root/easybuild-framework\"\n" \
++ ("\nprepend-path    PYTHONPATH      \"$root/MNS\"\n" if hashTable['mns'] == 'ThematicMNS' else "\n") + \
+"prepend-path    PYTHONPATH      \"$root/easybuild-framework\"\n\
+prepend-path    PYTHONPATH      \"$root/easybuild-easyblocks\"\n\
+prepend-path    PYTHONPATH      \"$root/easybuild-easyconfigs\"\n\
+prepend-path    PYTHONPATH      \"$root/vsc-base/lib\"\n\
+")
+	
+	# Path to the symlink to the module file.
+	symlinkPath = os.path.join(os.path.join(os.path.join(modulesDirPath, 'base'), EBmoduleDir), moduleName)
+	os.symlink(moduleFilePath, symlinkPath)
+
+
+# Install the ThematicMNS module naming scheme.
+def setThematicMNS(hashTable):
+	mnsroot = os.path.join(os.path.join('.installRef', 'MNS'), 'easybuild')
+	
+	# Extends the Python path in such a way that we included this architecture in the easybuild namespace.
+	mnsPath = os.path.join(hashTable['rootinstall'], os.path.join(os.path.join(mnsroot, 'tools'), 'module_naming_scheme'))
+	if not os.path.exists(mnsPath):
+		os.makedirs(mnsPath)
+	with open(os.path.join(mnsPath, '__init__.py'), "w") as f:
+		f.write("\
+from pkgutil import extend_path\n\
+__path__ = extend_path(__path__, __name__)\n\
+")
+	shutil.copyfile(os.path.join(mnsPath, '__init__.py'), \
+		os.path.join(os.path.join(os.path.join(hashTable['rootinstall'] ,mnsroot), 'tools'), '__init__.py'))
+	shutil.copyfile(os.path.join(mnsPath, '__init__.py'), \
+		os.path.join(os.path.join(hashTable['rootinstall'], mnsroot), '__init__.py'))
+
+	# Import the module naming scheme
+	thematicStr = hashTable['git_tree']['bin/ThematicMNS.py'].data_stream.read()
+	with open(os.path.join(mnsPath, 'ThematicMNS.py'), 'w') as f:
+		f.write(thematicStr)
+
+
+# Create some files to source to easily start using the EasyBuild module and the associated softwares.
+# These files are put in the <rootinstall> directory.
+def sourcefileCreator(hashTable):
+	trueVersion = os.path.basename(hashTable['rootinstall'])
+	# Variable containing all the required path for the MODULEPATH
+	modulePath = ""
+	moduleClasses = ['bio', 'cae', 'chem', 'compiler', 'data', 'debugger', 'devel', 'geo', 'lang', 'lib', 'math', \
+	'mpi', 'numlib', 'perf', 'phys', 'system', 'toolchain', 'tools', 'vis', 'base']
+	for moduleclass in moduleClasses:
+		modulePath += os.path.join(os.path.join(os.path.join('$RESIF_ROOTINSTALL', 'core'), 'modules'), moduleclass) + ":"
+
+	prependModulepath = (hashTable['prepend_modulepath'] + ":") if 'prepend_modulepath' in hashTable else ""
+	appendModulepath = hashTable['append_modulepath'] if 'append_modulepath' in hashTable else ""
+	modulePath = prependModulepath + modulePath + appendModulepath
+
+	# We create the files to source to use the infrastructure.
+	# The admin file is there to easily add software in the ulhpc swset without any manual changes to the config.
+	with open(os.path.join(hashTable['rootinstall'], "LOADME-" + trueVersion + ".sh"), "w") as f:
+		modulevar = "export EASYBUILD_MODULES_TOOL=Lmod" if hashTable["module_cmd"] == "lmod" else "#export EASYBUILD_MODULES_TOOL=Lmod"
+		f.write("\
+export RESIF_ROOTINSTALL=" + hashTable['rootinstall'] + "\n\
+export MODULEPATH=" + modulePath + "\n\
+" + modulevar + "\n\
+export EASYBUILD_MODULE_NAMING_SCHEME=" + hashTable['mns'] + "\n\
+export RESIF_OUT_PLACE=False\n\
+")
+	# The user file is there to easily add software locally without any manual change to the config.
+	with open(os.path.join(hashTable['rootinstall'], "LOADME-" + trueVersion + "-out-place.sh"), "w") as f:
+		modulevar = "export EASYBUILD_MODULES_TOOL=Lmod" if hashTable["module_cmd"] == "lmod" else "#export EASYBUILD_MODULES_TOOL=Lmod"
+		f.write("\
+export RESIF_ROOTINSTALL=" + hashTable['rootinstall'] + "\n\
+export MODULEPATH=" + modulePath + "\n\
+" + modulevar + "\n\
+export EASYBUILD_MODULE_NAMING_SCHEME=" + hashTable['mns'] + "\n\
+export RESIF_OUT_PLACE=True\n\
+")
+	
+	return modulePath.replace('$RESIF_ROOTINSTALL', hashTable['rootinstall'])
+
+#######################################################################################################################
---
+++ packages/resif/resif-2.0.2-py2-none-any.whl/resif/buildSwSets.py
@@ -0,0 +1,232 @@
+#######################################################################################################################
+# Author: Maxime Schmitt
+# Mail: maxime.schmitt@telecom-bretagne.eu
+# Overview: Module that take care of installing a given software set using a given EasyBuild module.
+#######################################################################################################################
+
+import os
+import sys
+import subprocess
+import yaml
+import time
+import re
+import glob
+
+#######################################################################################################################
+
+# Installs the software sets.
+def build(hashTable):
+	# Assume that the MODULEPATH is already set to a correct value
+    easybuild = hashTable['easybuild_module']
+    
+    if "swsets_config" in hashTable:
+        stream = file(hashTable['swsets_config'], 'r')
+    else:
+        stream = hashTable["git_tree"]['config/swsets.yaml'].data_stream.read()
+
+    swsets = yaml.load(stream)
+
+    # We define the options that are going to be passed to EasyBuild
+    sharedOptions = defineSharedOptions(hashTable)
+
+    # We use the additional options if any
+    if 'eb_options' in hashTable:
+        additionalOptions = " " + hashTable["eb_options"]
+    else:
+        additionalOptions = ""
+
+    for swset in hashTable['swsets']:
+        # We set the installpath. If there is no installpath given, we stop the execution.
+        installpath = setInstallpath(hashTable, swset)
+
+        # We add the place where the software will be installed to the MODULEPATH for the duration of the installation
+        # so that EasyBuild will not instantly forget that it has installed them after it is done (problematic for dependency resolution)
+        # Part for environment-modules (come later for Lmod)
+        # We also add the core software set to ensure that EasyBuild will be available
+        corePath = os.path.join(hashTable['rootinstall'], 'core', 'modules', 'all')
+        if hashTable["module_cmd"] == "modulecmd":
+            try:
+                os.environ['MODULEPATH'] = ':'.join([os.environ['MODULEPATH'], os.path.join(os.path.join(installpath[15:], 'modules'), 'all'), corePath])
+            except KeyError:
+                os.environ['MODULEPATH'] = ':'.join([os.path.join(os.path.join(installpath, 'modules'), 'all'), corePath])
+    	
+    	process = subprocess.Popen('/bin/bash', stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        # If we're doing a cleaninstall, we don't want the EASYBUILD_INSTALLPATH environment variable to interfere.
+        if 'iscleaninstall' in hashTable and hashTable['iscleaninstall']:
+            process.stdin.write('unset EASYBUILD_INSTALLPATH\n')
+
+        # Lmod part for MODULEPATH management
+        if hashTable["module_cmd"] == "lmod":
+            process.stdin.write("module use " + corePath + "\n")
+            process.stdin.write("module use " + os.path.join(os.path.join(installpath[15:], 'modules'), 'all') + "\n")
+
+    	process.stdin.write('module load ' + easybuild + '\n')
+    	
+        alreadyInstalled = False
+    	# If it actually exist in the yaml file, we install the listed software.
+    	if swset in swsets:
+            swsetStart= time.time()
+    	    for software in swsets[swset]:
+                sys.stdout.write("Now starting to install " + software[:-3] + "\n")
+    	        process.stdin.write('eb ' + software + installpath + sharedOptions + additionalOptions + ' --robot\n')
+    	        # Command to have at the end of the output the execution code of the last command
+    	        process.stdin.write('echo $?\n')
+    	        out = ""
+    	        while True:
+    	            out = process.stdout.readline()
+                    if re.search("\(module found\)", out) != None:
+                        alreadyInstalled = True
+    	            try:
+    	                i = int(out)
+    	            except ValueError:
+    	                i = -1
+    	            if i < 0:
+    	                sys.stdout.write(out)
+    	            else:
+    	                if i == 0:
+                            if alreadyInstalled:
+                                sys.stdout.write(software[:-3] + " was already installed. Nothing to be done.\n")
+                                alreadyInstalled = False
+                            else:
+                                sys.stdout.write('Successfully installed ' + software[:-3] + '.\n')
+    	                else:
+    	                    sys.stdout.write('Failed to install ' + software[:-3] + '\n' + 'Operation failed with return code ' + out + '\n')
+    	                    exit(out)
+    	                break
+            if swset != 'core':
+                if not 'out_place' in hashTable or not hashTable['out_place']:
+                    swsetModulefileCreator(hashTable, installpath[15:], swset)
+            swsetEnd = time.time()
+            swsetDuration = swsetEnd - swsetStart
+            m, s = divmod(swsetDuration, 60)
+            h, m = divmod(m, 60)
+            swsetDurationStr = "%d:%d:%d" % (h, m, s)
+    	    sys.stdout.write("Software set " + swset + " Successfully installed. Build duration: " + swsetDurationStr + ".\n")
+    	# If it doesn't, we print an error message as well as an help to use the script.
+    	else:
+    	    sys.stdout.write("Error: Invalid set of software.\nYou asked for the software set named " + swset  + \
+    	    	" to be installed but it wasn't found in your software set configuration file.\n")
+    	    sys.stdout.write("The configuration file location is: " + os.path.abspath(hashTable['swsets_config']) + \
+    	    	" and the software sets found in it are:\n")
+    	    for k in swsets.iteritems():
+    	        sys.stdout.write("- " + k[0] + "\n")
+    	    exit(20)
+
+
+# Defines options to pass to EasyBuild and return them in a string.
+def defineSharedOptions(hashTable):
+    options = ""
+
+    if 'eb_sourcepath' in hashTable:
+        options += ' --sourcepath=' + os.path.abspath(os.path.expandvars(hashTable['eb_sourcepath']))
+    if 'eb_buildpath' in hashTable:
+        options += ' --buildpath=' + os.path.abspath(os.path.expandvars(hashTable['eb_buildpath']))
+    if 'eb_repository' in hashTable:
+        options += ' --repository=' + hashTable['eb_repository']
+    if 'eb_repositorypath' in hashTable:
+        options += ' --repositorypath=' + os.path.abspath(os.path.expandvars(hashTable['eb_repositorypath']))
+    if 'mns' in hashTable:
+        options += ' --module-naming-scheme=' + hashTable['mns']
+
+    # If this file doesn't exist, it wont crash, EasyBuild will just ignore it and use other sources for the 
+    # options (default config file and so on).
+    if 'out_place' in hashTable and hashTable['out_place']:
+        easybuild_config = os.path.join(os.path.join(hashTable['srcpath'], 'config'), 'easybuild-out-place.cfg')
+    else:
+        easybuild_config = os.path.join(os.path.join(hashTable['srcpath'], 'config'), 'easybuild.cfg')
+    options += ' --configfile=' + easybuild_config
+
+    return options
+
+
+# Return the installpath for the given swset.
+def setInstallpath(hashTable, swset):
+    installpath = ""
+    # An installpath has to be provided, if not explicitely specified we use the rootinstall.
+    if 'installdir' in hashTable:
+        installpath = ' --installpath=' + os.path.join(hashTable['installdir'], swset)
+    elif 'out_place' in hashTable and hashTable['out_place']:
+        return installpath
+    elif 'rootinstall' in hashTable:
+            installpath = ' --installpath=' + os.path.join(hashTable['rootinstall'], swset)
+    else:
+        sys.stdout.write("\
+No information on where to install the softwares has been found. Please provide this information.\n\
+To do so, either:\n\
+- use the --rootinstall option to define the root directory of the EasyBuild install.\n\
+- set the RESIF_ROOTINSTALL environement variable to define the same information.\n\
+- set the rootinstall field in your configuration file if you are providing one. (using the --configfile option)\n\
+- use the --installdir option to define an alternative place to install (modules will be installed in <eb-installpath>/<swset>/modules)\n\
+- set the RESIF_INSTALLDIR environement variable to define the same information.\n\
+- set the installdir field in your configuration file if you are providing one. (using the --configfile option)\n\
+- set --out-place to True to install use the EasyBuild default installpath\n\
+- set RESIF_OUT_PLACE to True to define the same information\n\
+")
+        exit(10)
+
+    return installpath
+
+# For a given logfile we get the name of the software, the start and end dates of the build and convert them to timestamp and then determine the duration of the build.
+def getSoftwareBuildTimes(logfile):
+    logfileReduced = re.search("[^/]*$", logfile).group(0)
+    software = re.findall("-.*-", logfileReduced)[0][1:-1]
+    with open(logfile, "r") as log:
+        raw = log.readlines()
+        stime = "%s %s"%(raw[0].split()[1],raw[0].split()[2])
+        stime = time.mktime(time.strptime(stime[:-4], "%Y-%m-%d %H:%M:%S"))
+        etime = "%s %s"%(raw[-1].split()[1],raw[-1].split()[2])
+        etime = time.mktime(time.strptime(etime[:-4], "%Y-%m-%d %H:%M:%S"))
+        softwareDuration = etime - stime
+        
+    return (software, softwareDuration)
+
+# Create a module file and the associated symlink (if not already existing) to load the software set and put them at the right places.
+def swsetModulefileCreator(hashTable, installpath, moduleName):
+    if 'installdir' in hashTable:
+        modulesDirPath = os.path.join(os.path.join(hashTable['installdir'], 'core'), 'modules')
+    else:
+        modulesDirPath = os.path.join(os.path.join(hashTable['rootinstall'], 'core'), 'modules')
+    # Adapt the location of the modulfile to the chosen MNS
+    if hashTable['mns'] == "ThematicMNS":
+        # We create the directories we need to install EasyBuild
+        EBmoduleDir = os.path.join('base', 'swsets')
+        easybuildPath = os.path.join(os.path.join(modulesDirPath, 'all'), EBmoduleDir)
+        if not os.path.exists(easybuildPath):
+            os.makedirs(easybuildPath)
+        easybuildPath = os.path.join(os.path.join(modulesDirPath, 'base'), EBmoduleDir)
+        if not os.path.exists(easybuildPath):
+            os.makedirs(easybuildPath)
+
+    else:
+        # We create the directories we need to install EasyBuild
+        EBmoduleDir = 'swsets'
+        easybuildPath = os.path.join(os.path.join(modulesDirPath, 'all'), EBmoduleDir)
+        if not os.path.exists(easybuildPath):
+            os.makedirs(easybuildPath)
+        easybuildPath = os.path.join(os.path.join(modulesDirPath, 'base'), EBmoduleDir)
+        if not os.path.exists(easybuildPath):
+            os.makedirs(easybuildPath)
+
+    # Path to the actual module file
+    moduleFilePath = os.path.join(os.path.join(os.path.join(modulesDirPath, 'all'), EBmoduleDir), moduleName)
+
+    with open(moduleFilePath, "w") as f:
+        f.write("\
+#%Module\n\
+\n\
+proc ModulesHelp { } {\n\
+    puts stderr {   " + moduleName + " software set.\n\
+}\n\
+}\n\
+module-whatis {" + moduleName + " software set.\n\
+}\n\
+set root        " + installpath + "\n\
+prepend-path    MODULEPATH      \"$root/modules/all\"\n\
+")
+    
+    # Path to the symlink to the module file.
+    symlinkPath = os.path.join(os.path.join(os.path.join(modulesDirPath, 'base'), EBmoduleDir), moduleName)
+    if not os.path.exists(symlinkPath):
+        os.symlink(moduleFilePath, symlinkPath)
+
+#######################################################################################################################
