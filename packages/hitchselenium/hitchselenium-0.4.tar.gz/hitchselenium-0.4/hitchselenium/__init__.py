03d3a20f6fb67c091a8af113b4e2e0120551eaee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:41 +0000

    Add package hitchselenium/0.4/hitchselenium-0.4.tar.gz

---
+++ packages/hitchselenium/hitchselenium-0.4.tar.gz/hitchselenium-0.4/hitchselenium/__init__.py
@@ -0,0 +1,3 @@
+from hitchselenium.selenium_service import SeleniumService
+from hitchselenium.profile import Profile
+from hitchselenium.server import run
---
+++ packages/hitchselenium/hitchselenium-0.4.tar.gz/hitchselenium-0.4/hitchselenium/server.py
@@ -0,0 +1,30 @@
+from hitchselenium.profile import Profile
+from selenium import webdriver
+from sys import stdout
+import json
+import time
+
+
+def stop(driver):
+    driver.quit()
+    stdout.write("Firefox closed\n")
+    stdout.flush()
+
+def run():
+    driver = webdriver.Firefox(firefox_profile=Profile())
+    stdout.write("READY\n")
+    stdout.flush()
+    config = {"uri": "http://127.0.0.1:{}/hub".format(driver.profile.port), }
+    stdout.write("{}\n".format(json.dumps(config)))
+    stdout.flush()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        stop(driver)
+    except SystemExit:
+        stop(driver)
+
+if __name__=='__main__':
+    run()
