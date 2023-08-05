cc7043415a5679aee4d18ccb5659d513d90a666a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:05 +0000

    Add package auxlib/0.0.11/auxlib-0.0.11.tar.gz

---
+++ packages/auxlib/auxlib-0.0.11.tar.gz/auxlib-0.0.11/auxlib/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""auxiliary library to the python standard library"""
+__version__ = '0.0.11'
+__author__ = 'Kale Franz'
+__contact__ = 'kale@franz.io'
+__homepage__ = 'https://github.com/kalefranz/auxlib'
+__license__ = "ISC"
+__copyright__ = "(c) 2015 Kale Franz. All rights reserved."
+
