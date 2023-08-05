4ced2571aeeb90ff18e3e3f20942b708f1f6229f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:51 +0000

    Add package tld/0.7.3/tld-0.7.3.tar.gz

---
+++ packages/tld/tld-0.7.3.tar.gz/tld-0.7.3/src/tld/__init__.py
@@ -0,0 +1,9 @@
+__title__ = 'tld'
+__version__ = '0.7.3'
+__build__ = 0x00000d
+__author__ = 'Artur Barseghyan'
+__copyright__ = '2013-2015 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = ('get_tld', 'update_tld_names', 'Result',)
+
+from tld.utils import get_tld, update_tld_names, Result
