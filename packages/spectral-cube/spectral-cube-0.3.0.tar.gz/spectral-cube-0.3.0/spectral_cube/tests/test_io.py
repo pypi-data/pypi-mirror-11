bdf53b140a1e0ada5739a420181f904e3db100d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:28 +0000

    Add package spectral-cube/0.3.0/spectral-cube-0.3.0.tar.gz

---
+++ packages/spectral-cube/spectral-cube-0.3.0.tar.gz/spectral-cube-0.3.0/spectral_cube/tests/test_io.py
@@ -0,0 +1,29 @@
+from astropy.io import fits as pyfits
+from ..io import class_lmv, fits
+from .. import SpectralCube
+from . import path
+import pytest
+
+def test_lmv_fits():
+    c1 = SpectralCube.read(path('example_cube.fits'))
+    c2 = SpectralCube.read(path('example_cube.lmv'))
+
+    assert c1.shape == c2.shape
+
+    # should be able to do this, but it is not true:
+    #assert c1.header == c2.header
+
+    # should also be able to do this, but it is again false:
+    #assert c1.wcs==c2.wcs
+
+def test_3d_4d_stokes():
+    f3 = pyfits.open(path('adv.fits'))
+    f4 = pyfits.open(path('advs.fits'))
+    f3b = pyfits.PrimaryHDU(data=f3[0].data, header=f4[0].header)
+
+    c1 = SpectralCube.read(f3)
+    c2 = SpectralCube.read(f4)
+    c3 = SpectralCube.read(f3b)
+
+    assert c1.shape == c3.shape
+    # c2 has a different shape on disk...
