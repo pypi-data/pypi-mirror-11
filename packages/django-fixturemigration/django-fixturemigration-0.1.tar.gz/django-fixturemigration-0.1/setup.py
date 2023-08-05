7d8e3da1e148a909c5c6979dd93b1c8379510df7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:24 +0000

    Add package django-fixturemigration/0.1/django-fixturemigration-0.1.tar.gz

---
+++ packages/django-fixturemigration/django-fixturemigration-0.1.tar.gz/django-fixturemigration-0.1/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name = 'django-fixturemigration',
+    packages = ['fixturemigration'],
+    version = '0.1',
+    description = 'An app to load fixtures inside a migration data step',
+    author = 'Emilio A. Sánchez',
+    author_email = 'emilio@commite.co',
+    url = 'https://github.com/emiliosanchez/django-fixturemigration',
+    keywords = ['django', 'migrations', 'fixtures'],
+    classifiers = [
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Environment :: Web Environment",
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Framework :: Django",
+        "Framework :: Django :: 1.7",
+        "Framework :: Django :: 1.8"
+    ],
+)
