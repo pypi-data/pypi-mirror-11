52df4c3ee9e0e6a4cf016c6c7b98da6863fc1659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:25 +0000

    Add package exlogging/0.1.4/exlogging-0.1.4.tar.gz

---
+++ packages/exlogging/exlogging-0.1.4.tar.gz/exlogging-0.1.4/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='exlogging',
+    version='0.1.4',
+    description="Supports to setup python standard logging package.",
+    long_descriptiondescription=readme(),
+    classifiers=[
+        'Topic :: Utilities',
+        'Topic :: Software Development',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    license='MIT',
+    author='Motoki Naruse',
+    author_email='motoki@naru.se',
+    url='https://github.com/narusemotoki/exlogging',
+    keywords=' '.join(['log', 'logging', 'logger']),
+    zip_safe=False,
+    install_requires=[],
+    extras_require={
+        'test': [
+            'nose',
+            'rednose',
+            'coverage',
+            'flake8',
+        ],
+    }
+)
