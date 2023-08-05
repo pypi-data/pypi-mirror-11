35b6895ccc3601d57a24d61a7bcfc4ca9888b2f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:05 +0000

    Add package arpa/0.1.0a5/arpa-0.1.0a5.tar.gz

---
+++ packages/arpa/arpa-0.1.0a5.tar.gz/arpa-0.1.0a5/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import os.path
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(name):
+    path = os.path.join(os.path.dirname(__file__), name)
+    with open(path) as f:
+        return f.read()
+
+
+setup(
+    name="arpa",
+    version="0.1.0a5",
+    author="Stefan Fischer",
+    author_email="sfischer13@ymail.com",
+    url="https://github.com/sfischer13/python-arpa/",
+    license="MIT",
+    description="This is a library for reading ARPA n-gram models.",
+    long_description=read("README.rst"),
+    keywords="ARPA n-gram ngram language model LM language technology LT "
+    "computational linguistics CL natural language processing NLP",
+    install_requires=["enum34"],
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing",
+        "Topic :: Text Processing :: Linguistic"
+    ],
+    test_suite="nose.collector",
+    zip_safe=True)
---
+++ packages/arpa/arpa-0.1.0a5.tar.gz/arpa-0.1.0a5/src/arpa/models/base.py
@@ -0,0 +1,88 @@
+from abc import ABCMeta, abstractmethod
+
+
+class ARPAModel(metaclass=ABCMeta):
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    def __contains__(self, word):
+        return word in self.vocabulary()
+
+    @abstractmethod
+    def add_count(self, order, count):
+        pass
+
+    @abstractmethod
+    def add_entry(self, ngram, p, bo=None, order=None):
+        pass
+
+    def log10_p(self, ngram):
+        ngram = self._check_input(ngram)
+        try:
+            return self._log10_p(ngram)
+        except KeyError:
+            try:
+                log10_bo = self._log10_bo(ngram[:-1])
+            except KeyError:
+                log10_bo = 0
+            return log10_bo + self.log10_p(ngram[1:])
+
+    def log10_s(self, sentence):
+        words = self._check_input(sentence)
+        return sum(self.log10_p(words[:i]) for i in range(1, len(words) + 1))
+
+    def p(self, ngram):
+        return 10 ** self.log10_p(ngram)
+
+    def s(self, sentence):
+        return 10 ** self.log10_s(sentence)
+
+    @abstractmethod
+    def counts(self):
+        pass
+
+    @abstractmethod
+    def vocabulary(self):
+        pass
+
+    # TODO: write comment in header
+    def write(self, fp):
+        fp.write("\\data\\\n")
+        for order, count in self.counts():
+            fp.write("ngram {}={}\n".format(order, count))
+        fp.write("\n")
+        for order, _ in self.counts():
+            fp.write("\\{}-grams:\n".format(order))
+            for e in self._entries(order):
+                fp.write("{}\t{}".format(e[0], " ".join(e[1])))
+                if len(e) == 3:
+                    fp.write("\t{}".format(e[2]))
+                fp.write("\n")
+            fp.write("\n")
+        fp.write("\\end\\\n")
+
+    @abstractmethod
+    def _entries(self, order):
+        pass
+
+    @abstractmethod
+    def _log10_bo(self, ngram):
+        pass
+
+    @abstractmethod
+    def _log10_p(self, ngram):
+        pass
+
+    @staticmethod
+    def _check_input(input):
+        if not input:
+            raise ValueError
+        elif isinstance(input, tuple):
+            return input
+        elif isinstance(input, list):
+            return tuple(input)
+        elif isinstance(input, str):
+            return tuple(input.strip().split(" "))
+        else:
+            raise ValueError
