640124bdb6b63012c81cddab988d1bb1c9aad7a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:05 +0000

    Add package libssdb/0.0.1/libssdb-0.0.1.tar.gz

---
+++ packages/libssdb/libssdb-0.0.1.tar.gz/libssdb-0.0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(name='libssdb',
+      version='0.0.1',
+      description='SSDB API. It is for personal use only, not generic API.',
+      author='Tyler Long',
+      author_email='tyler4long@gmail.com',
+      packages=['libssdb'],
+      url='https://github.com/tylingsoft',
+      license='GPL',
+)
