49126a5eed1f0d6f4812f7c02b5f5d493bae42d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:46 +0000

    Add package txt-wrangler/0.0.1dev/txt-wrangler-0.0.1dev.zip

---
+++ packages/txt-wrangler/txt-wrangler-0.0.1dev.zip/txt-wrangler-0.0.1dev/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='txt-wrangler',
+    version='0.0.1dev',
+    author="David Martinez",
+    author_email="david.martinez.anim@gmail.com",
+    packages=['txt_wrangler',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
