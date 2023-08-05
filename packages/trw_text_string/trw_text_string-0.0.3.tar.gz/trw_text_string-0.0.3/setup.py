6105b9993591762e97f9a8490800e3fb0c76c76d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:46 +0000

    Add package trw_text_string/0.0.3/trw_text_string-0.0.3.tar.gz

---
+++ packages/trw_text_string/trw_text_string-0.0.3.tar.gz/trw_text_string-0.0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+setup(name='trw_text_string',
+      version='0.0.3',
+      author='T Williams',
+      description='Package for a challenge.',
+      long_description=readme,
+      packages=find_packages())
