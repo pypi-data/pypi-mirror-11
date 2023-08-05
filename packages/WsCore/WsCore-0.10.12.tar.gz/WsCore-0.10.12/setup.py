61fbe7713ebf49f117295d3d264bf02fea0e233d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:05 +0000

    Add package WsCore/0.10.12/WsCore-0.10.12.tar.gz

---
+++ packages/WsCore/WsCore-0.10.12.tar.gz/WsCore-0.10.12/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='WsCore',
+    version='0.10.12',
+    packages=['WsCore', 'WsCore.db', 'WsCore.kenel'],
+    url='http://www.wanersoft.com',
+    license='MIL',
+    author='ShouCai Zhang',
+    author_email='wanernet@qq.com',
+    description='WanerSoft Core'
+)
