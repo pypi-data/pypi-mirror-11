3bc8ea87e96634a5a4a97a207daf1c1364dd3243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:05 +0000

    Add package dataserv-client/1.0.5/dataserv-client-1.0.5.tar.gz

---
+++ packages/dataserv-client/dataserv-client-1.0.5.tar.gz/dataserv-client-1.0.5/README.rst
@@ -0,0 +1,91 @@
+###############
+dataserv-client
+###############
+
+|BuildLink|_ |CoverageLink|_ |LicenseLink|_ |IssuesLink|_
+
+
+.. |BuildLink| image:: https://travis-ci.org/Storj/dataserv-client.svg?branch=master
+.. _BuildLink: https://travis-ci.org/Storj/dataserv-client
+
+.. |CoverageLink| image:: https://coveralls.io/repos/Storj/dataserv-client/badge.svg
+.. _CoverageLink: https://coveralls.io/r/Storj/dataserv-client
+
+.. |LicenseLink| image:: https://img.shields.io/badge/license-MIT-blue.svg
+.. _LicenseLink: https://raw.githubusercontent.com/Storj/dataserv-client
+
+.. |IssuesLink| image:: https://img.shields.io/github/issues/Storj/dataserv-client.svg
+.. _IssuesLink: https://github.com/Storj/dataserv-client/issues
+
+
+Linux Setup
+###########
+
+::
+
+    $ pip install dataserv-client
+    $ dataserv-client version
+
+
+Windows Setup
+#############
+
+Download and install `Python 3.4 <https://www.python.org/downloads/release/python-343/>`_
+TODO add pycrypto instructions
+
+::
+
+    $ pip install dataserv-client
+    $ dataserv-client.py version
+
+
+Usage
+#####
+
+Show programm help and optional arguments:
+
+::
+
+    $ dataserv-client.py --help
+
+
+Show command help and optional arguments:
+
+::
+
+    $ dataserv-client.py <COMMAND> --help
+
+
+Show version number
+
+::
+
+    $ dataserv-client.py verison
+
+
+Register address:
+
+::
+
+    $ dataserv-client.py --address=<YOUR_BITCOIN_ADDRESS> register
+
+
+Ping address:
+
+::
+
+    $ dataserv-client.py --address=<YOUR_BITCOIN_ADDRESS> ping
+
+
+Poll address:
+
+::
+
+    $ dataserv-client.py --address=<YOUR_BITCOIN_ADDRESS> poll
+
+
+Build:
+
+::
+
+    $ dataserv-client.py --address=<YOUR_BITCOIN_ADDRESS> build
---
+++ packages/dataserv-client/dataserv-client-1.0.5.tar.gz/dataserv-client-1.0.5/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import os
+from setuptools import setup, find_packages
+
+
+THISDIR = os.path.dirname(os.path.abspath(__file__))
+os.chdir(THISDIR)
+
+
+VERSION = open(os.path.join("dataserv_client", "version.txt")).readline().strip()
+DOWNLOAD_BASEURL = "https://pypi.python.org/packages/source/a/dataserv-client/"
+DOWNLOAD_URL = DOWNLOAD_BASEURL + "dataserv-client-%s.tar.gz" % VERSION
+
+
+setup(
+    name='dataserv-client',
+    version=VERSION,
+    description='',
+    long_description=open("README.rst").read(),
+    keywords=(""),
+    url='http://storj.io',
+    author='Shawn Wilkinson',
+    author_email='shawn+dataserv-client@storj.io',
+    license='MIT',
+    packages=find_packages(exclude=['dataserv_client.bin']),
+    scripts=['dataserv_client/bin/dataserv-client.py'],
+    download_url = DOWNLOAD_URL,
+    test_suite="tests",
+    package_data={'': ['version.txt']},
+    install_requires=[
+        'RandomIO == 0.2.1',
+        'dataserv == 1.0.1',  # FIXME why do test only work when its here?
+    ],
+    tests_require=[
+        'coverage',
+        'coveralls'
+    ],
+    zip_safe=False,
+    classifiers=[
+        # "Development Status :: 1 - Planning",
+        # "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 3 - Alpha",
+        # "Development Status :: 4 - Beta",
+        # "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        #"Programming Language :: Python :: 2",
+        #"Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
