949cad8bb38669d2129c99110115ab0e95e72e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:58 +0000

    Add package ocpy/0.4/ocpy-0.4.tar.gz

---
+++ packages/ocpy/ocpy-0.4.tar.gz/ocpy-0.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ocpy',
+  packages = ['ocpy', 'ocpy.access', 'ocpy.convert', 'ocpy.ramon'],
+  version = '0.4',
+  description = 'A Python library for neuroscience data access and manipulation.',
+  author = 'Jordan Matelsky',
+  author_email = 'j6k4m8@gmail.com',
+  url = 'https://github.com/openconnectome/ocpy',
+  download_url = 'https://github.com/openconnectome/ocpy/tarball/0.1',
+  keywords = ['brain', 'neuro', 'neuroscience', 'ocpy', 'ocp', 'ocp.me', 'connectome', 'connectomics'],
+  classifiers = [],
+)
