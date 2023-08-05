e466577e4f2da0ac8c9ebf5081caa5e762c3ffb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:40 +0000

    Add package PuzzleSolver/1.0.1/PuzzleSolver-1.0.1.zip

---
+++ packages/PuzzleSolver/PuzzleSolver-1.0.1.zip/PuzzleSolver-1.0.1/PuzzleSolver.py
@@ -0,0 +1,32 @@
+__author__ = 'Fam. Cowe'
+
+import Sudoku
+
+"""This cutie engine will help solving puzzles as they are published in different magazines and other media.
+The only thing you will need to do is take a picture of it, and let this mighty little engine do his job.
+It might even send out your calculated solution to its publishers so that you can attend gaming competitions.
+Automated texting or mailing or whatever is needed to get you at the winning side, we will achieve it !
+And, who knows, you might even end up with a bunch of iPads, bikes, holidays, ...
+(I really know such a lady ! We'll do the same but without the thinking part)
+
+Somehow... Someday... but lets first start learning some of this Python language"""
+
+def run() :
+
+    """ Everybody got to run sometimes... as one would sing.
+    Yep, it does what it exactly says: it runs the engine and take you by the hand.
+    Enjoy !
+    """
+    print("Here we go... vroem vroem.")
+    print(" - Yup, you are right. The author is a noob and this is a stub   :-/ ")
+    print(" - And yes you will think, 'OMG, even the run command is a stub'")
+
+    print("Let us start with some demo Sudoku Solving")
+    Sudoku.sudokuRunner()
+
+"Main Program"
+"------------"
+run()
+
+
+
