e8acb4d5299b2675bf182a2e4f39060cf72eea12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:59 +0000

    Add package cache_tier/1.0/cache_tier-1.0.tar.gz

---
+++ packages/cache_tier/cache_tier-1.0.tar.gz/cache_tier-1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='cache_tier',
+    packages=['cache_tier'],
+    version='1.0',
+    description='TODO', #TODO Do this
+    author='Michael Kennedy - @mkennedy',
+    author_email='mikeckennedy+pypi@gmail.com',
+    url='https://github.com/mikeckennedy/cache-tier',
+    download_url='https://github.com/mikeckennedy/cache-tier/tarball/1.0.0',
+    license='MIT License',
+    keywords=['caching', 'cache-tier', 'cache', 'tier'],
+    classifiers=[],
+    install_requires=['requests']
+)
