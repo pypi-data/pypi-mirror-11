7d431da8639220a224840508c3eb2407af1de618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:02 +0000

    Add package rpweibo/0.2.2/rpweibo-0.2.2.tar.gz

---
+++ packages/rpweibo/rpweibo-0.2.2.tar.gz/rpweibo-0.2.2/setup.py
@@ -0,0 +1,47 @@
+import sys
+
+if sys.version_info[0] < 3:
+    sys.stderr.write("Python (<= 2) is not supported!\n")
+    sys.exit(1)
+
+import rpweibo
+
+try:
+    from setuptools import setup
+    use_setuptools = True
+except ImportError:
+    from distutils.core import setup
+    use_setuptools = False
+
+kw = {
+    "name": 'rpweibo',
+    "version": rpweibo.__version__,
+    "description": 'cURL + Python Weibo Wrapper',
+    "long_description": open('README', 'r').read(),
+    "author": 'Tom Li',
+    "author_email": 'biergaizi@member.fsf.org',
+    "url": 'https://github.com/WeCase/rpweibo',
+    "download_url": 'https://github.com/WeCase/rpweibo',
+    "py_modules": ['rpweibo'],
+    "license": 'LGPLv3+',
+    "requires": ["pycurl (>= 7.19.3)", "rsa (>=3.1)"],
+    "classifiers": [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+}
+
+if use_setuptools:
+    requires = kw.pop("requires")
+    for idx, val in enumerate(requires):
+        requires[idx] = val.replace("(", "").replace(")", "")
+    kw["install_requires"] = requires
+
+
+setup(**kw)
