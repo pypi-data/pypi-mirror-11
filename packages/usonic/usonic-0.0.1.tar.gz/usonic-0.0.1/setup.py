b34b60d7870c5d6fc59ce8751a16bafe1656f2c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:45 +0000

    Add package usonic/0.0.1/usonic-0.0.1.tar.gz

---
+++ packages/usonic/usonic-0.0.1.tar.gz/usonic-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+	name = 'usonic',
+	packages = ['usonic'],
+	version = '0.0.1',
+	description = 'An ultrasonic distance measurement module using HC-SR04',
+	author = 'Macpaul Lin',
+	author_email = 'macpaul@gmail.com',
+	url = 'https://github.com/macpaul/usonic',
+	download_url = 'https://github.com/macpaul/usonic/tarball/0.0.1',
+	keywords = ['ultrasonic', 'raspberry', 'distance'],
+	classifiers = [],
+)
