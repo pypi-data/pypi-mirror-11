47511f1eba95756d0f3b6fbfcefdf31a24c3319a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:48 +0000

    Add package Dory/0.0.3/Dory-0.0.3.tar.gz

---
+++ packages/Dory/Dory-0.0.3.tar.gz/Dory-0.0.3/dory/dory.py
@@ -0,0 +1,29 @@
+import sys
+import shutil
+import argparse
+import backup
+from termcolor import colored
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="simple file backup tool using rsync")
+    parser.add_argument("source",type=str, help="source path for backup, will be backed up in destination")
+    parser.add_argument("destination",type=str, help="destination path for backup")
+    args = parser.parse_args()
+
+    SRC = args.source
+    DEST = args.destination
+
+    # make sure that the destination directory ends with a "/"
+    if not DEST.endswith("/"):
+        DEST = DEST + "/"
+
+    backup.start(SRC,DEST)
+
+def check_rsync():
+    if shutil.which("rsync") is None:
+        print(colored("rsync not available on system","red"))
+        sys.exit(0)
+
+def main():
+    check_rsync()
+    parse_arguments()
