9f12173b13bf6d7fc2d1bfcd111c200e76bebc82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:12 +0000

    Add package django-gsschema/0.1b1/django-gsschema-0.1b1.tar.gz

---
+++ packages/django-gsschema/django-gsschema-0.1b1.tar.gz/django-gsschema-0.1b1/gsschema/urls.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('gsschema.views',
+    url(r'^$', 'index', name='index'),
+    url(r'(?P<layer>\w+)/download', 'download', name='download'),
+    url(r'(?P<layer>\w+)/describe', 'describe', name='describe'),
+    url(r'(?P<layer>\w+)/upload', 'upload', name='upload'),
+    url(r'(?P<layer>\w+)/remove', 'remove', name='remove'),
+)
