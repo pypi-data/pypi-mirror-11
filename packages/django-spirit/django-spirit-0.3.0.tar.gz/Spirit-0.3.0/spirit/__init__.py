e27b8087de846c3315c7eb12dc1a5a7d430eb734
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:02 +0000

    Add package django-spirit/0.3.0/django-spirit-0.3.0.tar.gz

---
+++ packages/django-spirit/django-spirit-0.3.0.tar.gz/Spirit-0.3.0/spirit/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+# TODO: check django User.count >= spirit custom.User.count
+# in the migration to remove that table/model (Spirit v0.4)
+__version__ = '0.3.0'
+
+default_app_config = 'spirit.apps.SpiritConfig'
