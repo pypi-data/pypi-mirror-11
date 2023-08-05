9ca50751fc3ba07beb43a2958555f1de69f2ff25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:15 +0000

    Add package compose-addons/0.2.0/compose-addons-0.2.0.tar.gz

---
+++ packages/compose-addons/compose-addons-0.2.0.tar.gz/compose-addons-0.2.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+from compose_addons import version
+
+setup(
+    name="compose-addons",
+    version=version,
+    provides=["compose_addons"],
+    author="Daniel Nephin",
+    author_email="dnephin@gmail.com",
+    url="http://github.com/dnephin/compose-addons",
+    description='Tools to supplement',
+    classifiers=[
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+    ],
+    license="Apache License 2.0",
+    packages=['compose_addons'],
+    install_requires=[
+        'requests',
+        'six',
+        'pyyaml',
+    ],
+    extras_require={
+        's3': ['boto'],
+    },
+    entry_points={
+        'console_scripts': [
+            'dcao-include = compose_addons.includes:main',
+            'dcao-namespace = compose_addons.namespace:main',
+            'dcao-merge = compose_addons.merge:main',
+        ],
+    },
+)
