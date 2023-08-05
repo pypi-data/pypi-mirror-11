8fed9d5c55fb804d6fc00bb0490599bd7f0cb5a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:37 +0000

    Add package GatherGit/0.0.1/GatherGit-0.0.1.tar.gz

---
+++ packages/GatherGit/GatherGit-0.0.1.tar.gz/GatherGit-0.0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: ts=4 sw=4 et
+
+from distutils.core import setup
+
+setup(name='GatherGit',
+      version='0.0.1',
+      description='Gather Git Repositories together to one large file tree',
+      author='Arnold Bechtoldt',
+      author_email='mail@arnoldbechtoldt.com',
+      url='https://github.com/bechtoldt/GatherGit',
+      #packages=['gathergit'],
+     )
