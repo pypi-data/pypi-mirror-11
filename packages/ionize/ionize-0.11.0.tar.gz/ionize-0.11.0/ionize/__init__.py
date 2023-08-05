e82cd8428fee96c5645f51c451bd35a855c5d3e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:56 +0000

    Add package ionize/0.11.0/ionize-0.11.0.tar.gz

---
+++ packages/ionize/ionize-0.11.0.tar.gz/ionize-0.11.0/ionize/__init__.py
@@ -0,0 +1,21 @@
+"""Represent ions in aqueous solution.
+
+Class Ion represents ion species.
+
+Class Solution represents an aqueous solution containing one or more ions.
+
+Function load_ion loads these ions from a database housed in ions_shelve.db.
+
+Function search_ion searches the database.
+
+Function get_db returns the database as a dictionary.
+"""
+
+from .Aqueous import Aqueous
+from .Ion import Ion
+from .Solution import Solution
+from .get_db import get_db
+from .load_ion import load_ion
+from .search_ion import search_ion
+from .nucleic_acid import nucleic_acid
+from .deserialize import deserialize
