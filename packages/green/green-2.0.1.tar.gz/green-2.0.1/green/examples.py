688b1cbd0d7e69f342ec3f97c9b9ac3028e911ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:28 +0000

    Add package green/2.0.1/green-2.0.1.tar.gz

---
+++ packages/green/green-2.0.1.tar.gz/green-2.0.1/green/examples.py
@@ -0,0 +1,51 @@
+from __future__ import unicode_literals
+import unittest
+
+
+
+class TestStates(unittest.TestCase):
+
+
+    def test0Pass(self):
+        """
+        This test will print output to stdout, and then pass.
+        """
+        print("Sunshine and daisies")
+
+
+    def test1Fail(self):
+        """
+        This test will print output to stdout, and then fail an assertion
+        """
+        print("Doom and gloom.")
+        self.assertTrue(False)
+
+
+    def test2Error(self):
+        """
+        An Exception will be raised (and not caught) while running this test.
+        """
+        raise Exception
+
+
+    @unittest.skip("This is the 'reason' portion of the skipped test.")
+    def test3Skip(self):
+        """
+        This test will be skipped.
+        """
+        pass
+
+    @unittest.expectedFailure
+    def test4ExpectedFailure(self):
+        """
+        This test will fail, but we expect it to.
+        """
+        self.assertEqual(True, False)
+
+
+    @unittest.expectedFailure
+    def test5UnexpectedPass(self):
+        """
+        This test will pass, but we expected it to fail!
+        """
+        pass
