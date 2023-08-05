2d1663ed2f03a9cd6c1fe3f89de2728290e17132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:05 +0000

    Add package uranium/0.2.10/uranium-0.2.10-py2.py3-none-any.whl

---
+++ packages/uranium/uranium-0.2.10-py2.py3-none-any.whl/uranium/exceptions.py
@@ -0,0 +1,16 @@
+class UraniumException(Exception):
+    pass
+
+
+class CacheException(UraniumException):
+    """ exception with the cache object """
+    pass
+
+
+class PackageException(UraniumException):
+    """ exceptions with the package object """
+    pass
+
+
+class ScriptException(UraniumException):
+    pass
