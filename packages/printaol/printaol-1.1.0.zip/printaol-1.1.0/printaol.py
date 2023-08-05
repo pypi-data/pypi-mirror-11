1deb842b46d103370943ed2241442bfd162f85a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:12 +0000

    Add package printaol/1.1.0/printaol-1.1.0.zip

---
+++ packages/printaol/printaol-1.1.0.zip/printaol-1.1.0/printaol.py
@@ -0,0 +1,10 @@
+def print_AOL(the_list,indent = False,num = 0):
+	for item in the_list:
+		if isinstance(item,list):
+			print_AOL(item,indent,num+1)
+		else:
+                        if indent:
+                                for tab_stop in range(num):
+                                        print("\t",end='')
+                        print(item)
+
