8385cec109b4a59f4bc0d8a8ffe67579a11ae0ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:28 +0000

    Add package Feni/0.5/Feni-0.5.tar.gz

---
+++ packages/Feni/Feni-0.5.tar.gz/Feni-0.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Feni",
+    description='A simple static site generator.',
+    url='http://bitbucket.org/sras/feni',
+    author='Sandeep.C.R',
+    author_email='sandeepcr2@gmail.com',
+    license='MIT',
+    version = "0.5",
+    packages = ['feni'],
+    entry_points = {
+        "console_scripts":['feni=feni.feni:main']
+    },
+    install_requires=[
+      'lesscpy',
+      'markdown'
+    ]
+)
