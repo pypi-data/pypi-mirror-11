f01e1ee7b8872e5e2e721f8cb607da766e432cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:23 +0000

    Add package click-threading/0.1.0/click_threading-0.1.0-py2.py3-none-any.whl

---
+++ packages/click-threading/click_threading-0.1.0-py2.py3-none-any.whl/click_threading/monkey.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import contextlib
+
+
+_ui_functions = (
+    'echo',
+    'echo_via_pager',
+    'prompt',
+    'confirm',
+    'clear',
+    'edit',
+    'launch',
+    'getchar',
+    'pause',
+)
+
+
+@contextlib.contextmanager
+def patch_ui_functions(wrapper):
+    '''Wrap all termui functions with a custom decorator.'''
+    NONE = object()
+    saved = {}
+    import click
+
+    for name in _ui_functions:
+        orig = getattr(click, name, NONE)
+        if orig is not NONE:
+            saved[name] = orig
+            setattr(click, name, wrapper(orig))
+
+    try:
+        yield
+    finally:
+        for name, orig in saved.items():
+            setattr(click, name, orig)
