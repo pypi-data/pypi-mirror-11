e12bb03eb3df2f8cb711eff47a95110798f5edef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:14 +0000

    Add package jsh/0.3.6/jsh-0.3.6.tar.gz

---
+++ packages/jsh/jsh-0.3.6.tar.gz/jsh-0.3.6/setup.py
@@ -0,0 +1,30 @@
+"""Setup for JSH"""
+
+from setuptools import setup
+from platform import system
+from jsh.version import __VERSION__
+
+setup(
+    name='jsh',
+    version=__VERSION__,
+    description='Junos-like shell library for Python',
+    author='Ocado Technology',
+    author_email='code@ocado.com',
+    long_description=open('README.rst').read(),
+    url='https://github.com/ocadotechnology/jsh/',
+    packages=['jsh'],
+    install_requires=['readline'] if system() == 'Darwin' else [],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
