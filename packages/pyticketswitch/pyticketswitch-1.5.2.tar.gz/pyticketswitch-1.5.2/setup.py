6a2d7a9d09939af59a58ca6858f3cf30b5803859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:51 +0000

    Add package pyticketswitch/1.5.2/pyticketswitch-1.5.2.tar.gz

---
+++ packages/pyticketswitch/pyticketswitch-1.5.2.tar.gz/pyticketswitch-1.5.2/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='pyticketswitch',
+    version='1.5.2',
+    author='Matt Jared',
+    author_email='mattjared@ingresso.co.uk',
+    packages=[
+        'pyticketswitch',
+        'pyticketswitch.test',
+        'pyticketswitch.interface_objects'
+    ],
+    license='LICENSE.txt',
+    description='A Python interface for the Ingresso XML Core API',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
