becbef3d2d34e2f3a51786760224d25a8004ca5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:39 +0000

    Add package netster/1.1.0/netster-1.1.0.zip

---
+++ packages/netster/netster-1.1.0.zip/netster-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name='netster',
+	version='1.1.0',
+	py_modules=['netster'],
+	author='lenovo',
+	author_email='1209938046@qq.com',
+	url='http://www.baidu.com',
+	description='a simple prints of netsted list',
+)
