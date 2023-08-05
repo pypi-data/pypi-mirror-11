1f104fb4ca1574dea513e53ec1c5854266695e99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:01 +0000

    Add package geocoder/1.5.1/geocoder-1.5.1.tar.gz

---
+++ packages/geocoder/geocoder-1.5.1.tar.gz/geocoder-1.5.1/geocoder/keys.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# coding: utf8
+
+bing_key = 'Aj1bcHN7s_wQomMB6-P_n2WM6V5DFJ2aR6HO2DY47Gty5j7tUwx1e7EY9hoTsaqO'
+tomtom_key = '95kjrqtpzv39ujcxfyr57wz3'
+app_id = 'UUB690Wqo8oyEZySnxJb'
+app_code = 'IBDbqNqTi7l_h5CwhSYbvg'
+geonames_username = 'addxy'
+canadapost_key = ''
+opencage_key = '519418b47cfac1f8d435a7f3fda6a7e0'
+mapquest_key = 'Kmjtd|luua2qu7n9,7a=o5-lzbgq'
+baidu_key = 'E4805d16520de693a3fe707cdc962045'
+w3w_key = 'AHWMVNDY'
+mapbox_key = 'sk.eyJ1Ijoicm9ja3NvY2tlciIsImEiOiJ1QXFMbVpNIn0.Qfp_eHbPJUkflGLfInkRlg'
