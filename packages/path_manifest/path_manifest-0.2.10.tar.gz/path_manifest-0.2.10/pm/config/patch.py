10f1d8c3c9d8a7f1c8b1b50daa8b62b9d17fbf80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:07 +0000

    Add package path_manifest/0.2.10/path_manifest-0.2.10.tar.gz

---
+++ packages/path_manifest/path_manifest-0.2.10.tar.gz/path_manifest-0.2.10/pm/config/patch.py
@@ -0,0 +1,6 @@
+TIMESTAMP_FORMAT = '%Y-%m-%d %H:%M:%S'
+PATCH_FILENAME_TEMPLATE = 'pm-patch-%(name)s.tar.bz2'
+PATCH_INFO_FILENAME_TEMPLATE = '.patch_info.%(name)s'
+
+DEFAULT_MANIFEST_FILENAME = '.manifest.csv.bz2'
+NAME_RX = r"^[a-zA-Z0-9_\-]+$"
