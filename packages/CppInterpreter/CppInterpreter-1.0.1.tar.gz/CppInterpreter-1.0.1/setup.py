5e07a72bd777a85b47aab840aba5763cca90f549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:53 +0000

    Add package CppInterpreter/1.0.1/CppInterpreter-1.0.1.tar.gz

---
+++ packages/CppInterpreter/CppInterpreter-1.0.1.tar.gz/CppInterpreter-1.0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(name="CppInterpreter",
+version="1.0.1",
+py_modules=["cppInterpreter","cppResources"],
+author="Noah Rossignol",
+author_email="noahrossignol@yahoo.com",
+description="An interpreter for C++ for easy prototyping and testing",
+license="GNU",
+classifiers=["Programming Language :: Python :: 2.7",
+"Programming Language :: C++",
+"Operating System :: POSIX :: Linux",
+"Topic :: Software Development :: Interpreters",
+"Natural Language :: English",
+"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",])
