abbfe836bc8348aabe5bb9f924ae535d1c2d6b51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:14 +0000

    Add package matrix-client/0.0.2/matrix_client-0.0.2.tar.gz

---
+++ packages/matrix-client/matrix_client-0.0.2.tar.gz/matrix_client-0.0.2/matrix_client/api.py
@@ -0,0 +1,322 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015 OpenMarket Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import re
+import requests
+import urllib
+import urlparse
+
+class MatrixError(Exception):
+    """A generic Matrix error. Specific errors will subclass this."""
+    pass
+
+
+class MatrixRequestError(MatrixError):
+    """ The home server returned an error response. """
+
+    def __init__(self, code=0, content=""):
+        super(MatrixRequestError, self).__init__("%d: %s" % (code, content))
+        self.code = code
+        self.content = content
+
+
+class MatrixHttpApi(object):
+    """Contains all raw Matrix HTTP Client-Server API calls.
+
+    Usage:
+        matrix = MatrixHttpApi("https://matrix.org", token="foobar")
+        response = matrix.initial_sync()
+        response = matrix.send_message("!roomid:matrix.org", "Hello!")
+
+    For room and sync handling, consider using MatrixClient.
+    """
+
+    def __init__(self, base_url, token=None):
+        """Construct and configure the HTTP API.
+
+        Args:
+            base_url(str): The home server URL e.g. 'http://localhost:8008'
+            token(str): Optional. The client's access token.
+        """
+        if not base_url.endswith("/_matrix/client/api/v1"):
+            self.url = urlparse.urljoin(base_url, "/_matrix/client/api/v1")
+        else:
+            self.url = base_url
+        self.token = token
+        self.txn_id = 0
+        self.validate_cert = True
+
+    def initial_sync(self, limit=1):
+        """Perform /initialSync.
+
+        Args:
+            limit(int): The limit= param to provide.
+        """
+        return self._send("GET", "/initialSync", query_params={"limit": limit})
+
+    def validate_certificate(self, valid):
+        self.validate_cert = valid
+        return
+
+    def register(self, login_type, **kwargs):
+        """Performs /register.
+
+        Args:
+            login_type(str): The value for the 'type' key.
+            **kwargs: Additional key/values to add to the JSON submitted.
+        """
+        content = {
+            "type": login_type
+        }
+        for key in kwargs:
+            content[key] = kwargs[key]
+
+        return self._send("POST", "/register", content)
+
+    def login(self, login_type, **kwargs):
+        """Perform /login.
+
+        Args:
+            login_type(str): The value for the 'type' key.
+            **kwargs: Additional key/values to add to the JSON submitted.
+        """
+        content = {
+            "type": login_type
+        }
+        for key in kwargs:
+            content[key] = kwargs[key]
+
+        return self._send("POST", "/login", content)
+
+    def create_room(self, alias=None, is_public=False, invitees=()):
+        """Perform /createRoom.
+
+        Args:
+            alias(str): Optional. The room alias name to set for this room.
+            is_public(bool): Optional. The public/private visibility.
+            invitees(list<str>): Optional. The list of user IDs to invite.
+        """
+        content = {
+            "visibility": "public" if is_public else "private"
+        }
+        if alias:
+            content["room_alias_name"] = alias
+        if invitees:
+            content["invite"] = invitees
+        return self._send("POST", "/createRoom", content)
+
+    def join_room(self, room_id_or_alias):
+        """Performs /join/$room_id
+
+        Args:
+            room_id_or_alias(str): The room ID or room alias to join.
+        """
+        if not room_id_or_alias:
+            raise MatrixError("No alias or room ID to join.")
+
+        path = "/join/%s" % urllib.quote(room_id_or_alias)
+
+        return self._send("POST", path)
+
+    def event_stream(self, from_token, timeout=30000):
+        """Performs /events
+
+        Args:
+            from_token(str): The 'from' query parameter.
+            timeout(int): Optional. The 'timeout' query parameter.
+        """
+        path = "/events"
+        return self._send(
+            "GET", path, query_params={
+                "timeout": timeout,
+                "from": from_token
+            }
+        )
+
+    def send_state_event(self, room_id, event_type, content, state_key=""):
+        """Perform /rooms/$room_id/state/$event_type
+
+        Args:
+            room_id(str): The room ID to send the state event in.
+            event_type(str): The state event type to send.
+            content(dict): The JSON content to send.
+            state_key(str): Optional. The state key for the event.
+        """
+        path = ("/rooms/%s/state/%s" %
+            (urllib.quote(room_id), urllib.quote(event_type))
+        )
+        if state_key:
+            path += "/%s" % (urllib.quote(state_key))
+        return self._send("PUT", path, content)
+
+    def send_message_event(self, room_id, event_type, content, txn_id=None):
+        """Perform /rooms/$room_id/send/$event_type
+
+        Args:
+            room_id(str): The room ID to send the message event in.
+            event_type(str): The event type to send.
+            content(dict): The JSON content to send.
+            txn_id(int): Optional. The transaction ID to use.
+        """
+        if not txn_id:
+            txn_id = self.txn_id
+
+        self.txn_id = self.txn_id + 1
+
+        path = ("/rooms/%s/send/%s/%s" %
+            (urllib.quote(room_id), urllib.quote(event_type),
+             urllib.quote(unicode(txn_id)))
+        )
+        return self._send("PUT", path, content)
+
+    def send_message(self, room_id, text_content, msgtype="m.text"):
+        """Perform /rooms/$room_id/send/m.room.message
+
+        Args:
+            room_id(str): The room ID to send the event in.
+            text_content(str): The m.text body to send.
+        """
+        return self.send_message_event(
+            room_id, "m.room.message",
+            self.get_text_body(text_content, msgtype)
+        )
+
+    def send_emote(self, room_id, text_content):
+        """Perform /rooms/$room_id/send/m.room.message with m.emote msgtype
+
+        Args:
+            room_id(str): The room ID to send the event in.
+            text_content(str): The m.emote body to send.
+        """
+        return self.send_message_event(
+            room_id, "m.room.message",
+            self.get_emote_body(text_content)
+        )
+
+    def get_room_name(self, room_id):
+        """Perform GET /rooms/$room_id/state/m.room.name
+        Args:
+            room_id(str): The room ID
+        """
+        return self._send("GET", "/rooms/" + room_id + "/state/m.room.name")
+
+    def get_room_topic(self, room_id):
+        """Perform GET /rooms/$room_id/state/m.room.topic
+        Args:
+            room_id(str): The room ID
+        """
+        return self._send("GET", "/rooms/" + room_id + "/state/m.room.topic")
+
+    def leave_room(self, room_id):
+        """Perform POST /rooms/$room_id/leave
+        Args:
+            room_id(str): The room ID
+        """
+        return self._send("POST", "/rooms/" + room_id + "/leave", {})
+
+    def invite_user(self, room_id, user_id):
+        """Perform POST /rooms/$room_id/invite
+        Args:
+            room_id(str): The room ID
+            user_id(str): The user ID of the invitee
+        """
+        body = {
+            "user_id": user_id
+        }
+        return self._send("POST", "/rooms/" + room_id + "/invite", body)
+
+    def kick_user(self, room_id, user_id, reason=""):
+        """Calls set_membership with membership="leave" for the user_id provided
+        """
+        self.set_membership(room_id, user_id, "leave", reason)
+
+    def set_membership(self, room_id, user_id, membership, reason=""):
+        """Perform PUT /rooms/$room_id/state/m.room.member/$user_id
+        Args:
+            room_id(str): The room ID
+            user_id(str): The user ID
+            membership(str): New membership value
+            reason(str): The reason
+        """
+        body = {
+            "membership": membership,
+            "reason": reason
+        }
+        return self._send("PUT", "/rooms/" + room_id + "/state/m.room.member/" + user_id, body)
+
+
+
+    def ban_user(self, room_id, user_id, reason=""):
+        """Perform POST /rooms/$room_id/ban
+        Args:
+            room_id(str): The room ID
+            user_id(str): The user ID of the banee(sic)
+            reason(str): The reason for this ban
+        """
+        body = {
+            "user_id": user_id,
+            "reason": reason
+        }
+        return self._send("POST", "/rooms/" + room_id + "/ban", body)
+
+    def get_room_state(self, room_id):
+        """Perform GET /rooms/$room_id/state
+        Args:
+            room_id(str): The room ID
+        """
+        return self._send("GET", "/rooms/" + room_id + "/state")
+
+    def get_text_body(self, text, msgtype="m.text"):
+        return {
+            "msgtype": msgtype,
+            "body": text
+        }
+
+    def get_html_body(self, html, msgtype="m.text"):
+        return {
+            "body": re.sub('<[^<]+?>', '', html),
+            "msgtype": msgtype,
+            "format": "org.matrix.custom.html",
+            "formatted_body": html
+        }
+
+    def get_emote_body(self, text):
+        return {
+            "msgtype": "m.emote",
+            "body": text
+        }
+
+    def _send(self, method, path, content=None, query_params={}, headers={}):
+        method = method.upper()
+        if method not in ["GET", "PUT", "DELETE", "POST"]:
+            raise MatrixError("Unsupported HTTP method: %s" % method)
+
+        headers["Content-Type"] = "application/json"
+        query_params["access_token"] = self.token
+        endpoint = self.url + path
+
+        response = requests.request(
+            method, endpoint, params=query_params,
+            data=json.dumps(content), headers=headers
+            , verify=self.validate_cert  #if you want to use SSL without verifying the Cert
+        )
+
+        if response.status_code < 200 or response.status_code >= 300:
+            raise MatrixRequestError(
+                code=response.status_code, content=response.text
+            )
+
+        return response.json()
---
+++ packages/matrix-client/matrix_client-0.0.2.tar.gz/matrix_client-0.0.2/matrix_client/client.py
@@ -0,0 +1,259 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015 OpenMarket Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from .api import MatrixHttpApi, MatrixRequestError
+from threading import Thread
+import sys
+# TODO: Finish implementing this.
+
+
+class MatrixClient(object):
+    """ WORK IN PROGRESS
+    The client API for Matrix. For the raw HTTP calls, see MatrixHttpApi.
+
+    Usage (new user):
+        client = MatrixClient("https://matrix.org")
+        token = client.register_with_password(username="foobar",
+            password="monkey")
+        room = client.create_room("myroom")
+        room.send_image(file_like_object)
+
+    Usage (logged in):
+        client = MatrixClient("https://matrix.org", token="foobar")
+        rooms = client.get_rooms()  # NB: From initial sync
+        client.add_listener(func)  # NB: event stream callback
+        rooms[0].add_listener(func)  # NB: callbacks just for this room.
+        room = client.join_room("#matrix:matrix.org")
+        response = room.send_text("Hello!")
+        response = room.kick("@bob:matrix.org")
+
+    Incoming event callbacks (scopes):
+
+        def user_callback(user, incoming_event):
+            pass
+
+        def room_callback(room, incoming_event):
+            pass
+
+        def global_callback(incoming_event):
+            pass
+
+    """
+
+    def __init__(self, base_url, token=None, valid_cert_check=True):
+        self.api = MatrixHttpApi(base_url, token)
+        self.api.validate_certificate(valid_cert_check)
+        self.listeners = []
+        self.rooms = {
+            # room_id: Room
+        }
+        if token:
+            self._sync()
+
+    def register_with_password(self, username, password, limit=1):
+        response = self.api.register(
+            "m.login.password", user=username, password=password
+        )
+        self.user_id = response["user_id"]
+        self.token = response["access_token"]
+        self.hs = response["home_server"]
+        self.api.token = self.token
+        self._sync(limit)
+        return self.token
+
+    def login_with_password(self, username, password, limit=1):
+        response = self.api.login(
+            "m.login.password", user=username, password=password
+        )
+        self.user_id = response["user_id"]
+        self.token = response["access_token"]
+        self.hs = response["home_server"]
+        self.api.token = self.token
+        self._sync(limit)
+        return self.token
+
+    def create_room(self, alias=None, is_public=False, invitees=()):
+        response = self.api.create_room(alias, is_public, invitees)
+        return self._mkroom(response["room_id"])
+
+    def join_room(self, room_id_or_alias):
+        response = self.api.join_room(room_id_or_alias)
+        room_id = (
+            response["room_id"] if "room_id" in response else room_id_or_alias
+        )
+        return self._mkroom(room_id)
+
+    def get_rooms(self):
+        return self.rooms
+
+    def add_listener(self, callback):
+        self.listeners.append(callback)
+
+    def listen_for_events(self, timeout=30000):
+        response = self.api.event_stream(self.end, timeout)
+        self.end = response["end"]
+
+        for chunk in response["chunk"]:
+            for listener in self.listeners:
+                listener(chunk)
+            if "room_id" in chunk:
+                if chunk["room_id"] not in self.rooms:
+                    self._mkroom(chunk["room_id"])
+                self.rooms[chunk["room_id"]].events.append(chunk)
+                for listener in self.rooms[chunk["room_id"]].listeners:
+                    listener(chunk)
+
+    def listen_forever(self, timeout=30000):
+        while(True):
+            self.listen_for_events(timeout)
+
+    def start_listener_thread(self, timeout=30000):
+        try:
+            thread = Thread(target=self.listen_forever, args=(timeout, ))
+            thread.daemon = True
+            thread.start()
+        except:
+            e = sys.exc_info()[0]
+            print("Error: unable to start thread. " + str(e))
+
+    def _mkroom(self, room_id):
+        self.rooms[room_id] = Room(self, room_id)
+        return self.rooms[room_id]
+
+    def _sync(self, limit=1):
+        response = self.api.initial_sync(limit)
+        try:
+            self.end = response["end"]
+            for room in response["rooms"]:
+                self._mkroom(room["room_id"])
+
+                current_room = self.get_rooms()[room["room_id"]]
+                for chunk in room["messages"]["chunk"]:
+                    current_room.events.append(chunk)
+
+                for state_event in room["state"]:
+                    if "type" in state_event and state_event["type"] == "m.room.name":
+                        current_room.name = state_event["content"]["name"]
+                    if "type" in state_event and state_event["type"] == "m.room.topic":
+                        current_room.topic = state_event["content"]["topic"]
+                    if "type" in state_event and state_event["type"] == "m.room.aliases":
+                        current_room.aliases = state_event["content"]["aliases"]
+
+        except KeyError:
+            pass
+
+
+class Room(object):
+
+    def __init__(self, client, room_id):
+        self.room_id = room_id
+        self.client = client
+        self.listeners = []
+        self.events = []
+        self.name = None
+        self.aliases = []
+        self.topic = None
+
+    def send_text(self, text):
+        return self.client.api.send_message(self.room_id, text)
+
+    def send_emote(self, text):
+        return self.client.api.send_emote(self.room_id, text)
+
+    def add_listener(self, callback):
+        self.listeners.append(callback)
+
+    def get_events(self):
+        return self.events
+
+    def invite_user(self, user_id):
+        """Invite user to this room
+
+        Return True if the invitation was sent
+        """
+        try:
+            response = self.client.api.invite_user(self.room_id, user_id)
+            return True
+        except MatrixRequestError:
+            return False
+
+    def kick_user(self, user_id, reason=""):
+        try:
+            response = self.client.api.kick_user(self.room_id, user_id)
+            return True
+        except MatrixRequestError:
+            return False
+
+    def ban_user(self, user_id, reason):
+        try:
+            response = self.client.api.ban_user(self.room_id, user_id, reason)
+            return True
+        except MatrixRequestError:
+            return False
+
+    def leave(self, user_id):
+        try:
+            response = self.client.api.leave_room(self.room_id)
+            self.client.rooms.remove(self.room_id)
+            return True
+        except MatrixRequestError:
+            return False
+
+    def update_room_name(self):
+        """Get room name
+
+        Return True if the room name changed, False if not
+        """
+        try:
+            response = self.client.api.get_room_name(self.room_id)
+            if "name" in response and response["name"] != self.name:
+                self.name = response["name"]
+                return True
+            else:
+                return False
+        except MatrixRequestError:
+            return False
+
+    def update_room_topic(self):
+        """Get room topic
+
+        Return True if the room topic changed, False if not
+        """
+        try:
+            response = self.client.api.get_room_topic(self.room_id)
+            if "topic" in response and response["topic"] != self.topic:
+                self.topic = response["topic"]
+                return True
+            else:
+                return False
+        except MatrixRequestError:
+            return False
+
+    def update_aliases(self):
+        """Get aliases information from room state
+
+        Return True if the aliases changed, False if not
+        """
+        try:
+            response = self.client.api.get_room_state(self.room_id)
+            for chunk in response:
+                if "content" in chunk and "aliases" in chunk["content"]:
+                    if chunk["content"]["aliases"] != self.aliases:
+                        self.aliases = chunk["content"]["aliases"]
+                        return True
+                    else:
+                        return False
+        except MatrixRequestError:
+            return False
+
