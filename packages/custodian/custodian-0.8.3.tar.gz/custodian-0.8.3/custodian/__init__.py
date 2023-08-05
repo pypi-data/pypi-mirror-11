f8c9350f558dd9375b2180ab05028a3f505acf43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:07 +0000

    Add package custodian/0.8.3/custodian-0.8.3.tar.gz

---
+++ packages/custodian/custodian-0.8.3.tar.gz/custodian-0.8.3/custodian/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+
+__author__ = "Shyue Ping Ong, William Davidson Richards, Stephen Dacek, " \
+             "Xiaohui Qu"
+__date__ = "Aug 2 2015"
+__version__ = "0.8.3"
+
+from .custodian import Custodian
