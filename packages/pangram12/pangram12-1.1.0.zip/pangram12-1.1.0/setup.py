5ab00650686b24611efd1e25f8e6c59fe18e20e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:35 +0000

    Add package pangram12/1.1.0/pangram12-1.1.0.zip

---
+++ packages/pangram12/pangram12-1.1.0.zip/pangram12-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup (
+       name = 'pangram12',
+       version = '1.1.0',
+       py_modules = ['pangram12'],
+       author = 'rohit',
+       author_email = 'hitrojo11@gmail.com',
+       url = ' ',
+       description = 'A simple program to check whether given string is pangram or not.',
+      )
