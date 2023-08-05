9146353fe0dab4ce54bd6d699e17e716196e22d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:26 +0000

    Add package hug/0.0.6/hug-0.0.6.tar.gz

---
+++ packages/hug/hug-0.0.6.tar.gz/hug-0.0.6/hug/output_format.py
@@ -0,0 +1,41 @@
+import json as json_converter
+from datetime import date, datetime
+
+
+def _json_converter(item):
+    if isinstance(item, (date, datetime)):
+        return item.isoformat()
+    elif isinstance(item, bytes):
+        return item.decode('utf8')
+    raise TypeError("Type not serializable")
+
+
+def json(content, **kwargs):
+    """JSON (Javascript Serialized Object Notation)"""
+    return json_converter.dumps(content, default=_json_converter, **kwargs).encode('utf8')
+
+
+def text(content):
+    """Free form UTF8 text"""
+    return content.encode('utf8')
+
+
+def _camelcase(dictionary):
+    if not isinstance(dictionary, dict):
+        return dictionary
+
+    new_dictionary = {}
+    for key, value in dictionary.items():
+        if isinstance(key, str):
+            key = key[0] + "".join(key.title().split('_'))[1:]
+        new_dictionary[key] = _camelcase(dictionary)
+
+
+def json_camelcase(content):
+    """JSON (Javascript Serialized Object Notation) with all keys camelCased"""
+    return _camelcase(json(body))
+
+
+def pretty_json(content):
+    """JSON (Javascript Serialized Object Notion) pretty printed and indented"""
+    return json(content, indent=4, separators=(',', ': '))
