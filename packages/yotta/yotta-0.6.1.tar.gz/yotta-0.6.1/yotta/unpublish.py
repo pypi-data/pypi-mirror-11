40a5f8af604a33655242f41f228a32ccd6691c63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:23 +0000

    Add package yotta/0.6.1/yotta-0.6.1.tar.gz

---
+++ packages/yotta/yotta-0.6.1.tar.gz/yotta-0.6.1/yotta/unpublish.py
@@ -0,0 +1,28 @@
+# Copyright 2015 ARM Limited
+#
+# Licensed under the Apache License, Version 2.0
+# See LICENSE file for details.
+
+# standard library modules, , ,
+import argparse
+import logging
+import os
+
+# validate, , validate things, internal
+from .lib import validate
+
+def addOptions(parser):
+    pass
+
+def execCommand(args, following_args):
+    p = validate.currentDirectoryModuleOrTarget()
+    if not p:
+        return 1
+
+    error = p.unpublish(args.registry)
+    if error:
+        logging.error(error)
+        return 1
+
+    logging.info('unpublished version: %s', p.getVersion())
+    return 0
