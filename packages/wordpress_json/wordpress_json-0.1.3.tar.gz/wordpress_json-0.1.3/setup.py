283df112e5f3ba30d0d61de3fcde66db711f407d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:13 +0000

    Add package wordpress_json/0.1.3/wordpress_json-0.1.3.tar.gz

---
+++ packages/wordpress_json/wordpress_json-0.1.3.tar.gz/wordpress_json-0.1.3/setup.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
+import os
+import sys
+
+from setuptools import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+try:
+    long_description = open('README.rst', 'r').read()
+except IOError:
+    long_description = "A thin wrapper around the Wordpress JSON API"
+
+with open('README.rst', 'r') as f:
+    readme = f.read()
+with open('HISTORY.rst', 'r') as f:
+    history = f.read()
+
+setup(
+    name='wordpress_json',
+    version='0.1.3',
+    description='A thin wrapper for the Wordpress JSON API',
+    author='Raul Taranu, Dimitar Roustchev',
+    author_email='raul.taranu@stylight.com, dimitar.roustchev@stylight.com',
+    url='http://github.com/stylight/python-wordpress-json',
+    license='MIT License',
+    packages=['wordpress_json'],
+    install_requires=[
+        'requests',
+    ],
+    setup_requires=["nose>=1.0"],
+    test_suite='nose.collector',
+    keywords='wordpress json api'
+)
