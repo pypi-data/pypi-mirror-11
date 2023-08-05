75a0e5bf61129d6db6c4b892cc1b22f17914ce94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:53 +0000

    Add package Annalist/0.1.16/Annalist-0.1.16.tar.gz

---
+++ packages/Annalist/Annalist-0.1.16.tar.gz/Annalist-0.1.16/annalist_root/annalist/__init__.py
@@ -0,0 +1,15 @@
+"""
+Annalist application
+"""
+
+__author__        = "Graham Klyne"
+__author_email__  = "GK@ACM.ORG"
+__copyright__     = "Copyright 2014, G. Klyne"
+__license__       = "MIT (http://opensource.org/licenses/MIT)"
+
+# https://docs.djangoproject.com/en/dev/ref/applications/
+default_app_config = 'annalist.apps.AnnalistConfig'
+
+__version__        = "0.1.16"  # Software version number (odd = unstable)
+
+# End.
