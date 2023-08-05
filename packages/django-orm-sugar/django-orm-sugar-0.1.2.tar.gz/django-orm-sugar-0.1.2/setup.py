0d0ed749ef5decbd0e89e34f6c2572637445bfe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:38 +0000

    Add package django-orm-sugar/0.1.2/django-orm-sugar-0.1.2.tar.gz

---
+++ packages/django-orm-sugar/django-orm-sugar-0.1.2.tar.gz/django-orm-sugar-0.1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+__author__ = 'Alexey Zankevich'
+
+
+setup(
+    name="django-orm-sugar",
+    version="0.1.2",
+    py_modules=['django_orm_sugar'],
+    author="Alexey Zankevich",
+    author_email="alex.zankevich@gmail.com",
+    description="Django ORM sugar library to simplify querying",
+    keywords=['Django', 'ORM', 'util', 'sugar'],
+    license="MIT",
+)
+
+
+
