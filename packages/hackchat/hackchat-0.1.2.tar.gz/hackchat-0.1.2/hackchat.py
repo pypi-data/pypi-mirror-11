80b750a4b9b2b1d54ef91e6bbe2a6fc1a1b58610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:20 +0000

    Add package hackchat/0.1.2/hackchat-0.1.2.tar.gz

---
+++ packages/hackchat/hackchat-0.1.2.tar.gz/hackchat-0.1.2/hackchat.py
@@ -0,0 +1,41 @@
+import json
+import websocket
+
+class HackChat:
+    def __init__(self, nick, channel="programming"):
+        self.nick = nick
+        self.channel = channel
+
+        self.online_users = set()
+
+        self.on_message = []
+        self.on_join = []
+        self.on_leave = []
+
+        self.ws = websocket.create_connection("wss://hack.chat/chat-ws")
+        self.ws.send(json.dumps({"cmd": "join", "channel": channel, "nick": nick}))
+
+    def run(self):
+        result = json.loads(self.ws.recv())
+        if result["cmd"] == "chat" and not result["nick"] == self.nick:
+            for handler in list(self.on_message):
+                handler(self, result["text"], result["nick"])
+        elif result["cmd"] == "onlineAdd":
+            self.online_users.append(nick)
+            for handler in list(self.on_join):
+                handler(self, result["nick"])
+        elif result["cmd"] == "onlineRemove":
+            self.online_users.remove(nick)
+            for handler in list(self.on_leave):
+                handler(self, result["nick"])
+        elif result["cmd"] == "onlineSet":
+            for nick in result["nicks"]:
+                self.online_users.append(nick)
+        return result
+
+    def run_loop(self):
+        while True:
+            self.run()
+
+    def send_message(self, msg):
+        self.ws.send(json.dumps({"cmd": "chat", "text": msg}))
