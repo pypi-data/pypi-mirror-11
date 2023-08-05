f26eecb85da762f984877176760d33fb6fa4cf6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:51 +0000

    Add package django-shell-plus/1.1.6/django-shell-plus-1.1.6.tar.gz

---
+++ packages/django-shell-plus/django-shell-plus-1.1.6.tar.gz/django-shell-plus-1.1.6/shell_plus/management/commands/update_permissions.py
@@ -0,0 +1,12 @@
+from django.core.management.base import NoArgsCommand
+
+class Command(NoArgsCommand):
+    help = "Update permissions for all installed apps."
+
+    requires_model_validation = True
+
+    def handle_noargs(self, **options):
+        from django.contrib.auth.management import create_permissions
+        from django.db.models import get_apps
+        for app in get_apps():
+           create_permissions(app, None, 2)
