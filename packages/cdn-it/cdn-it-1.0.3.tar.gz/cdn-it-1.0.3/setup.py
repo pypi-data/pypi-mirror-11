ba1231bc937498331bb88b603348cd1d9995d04d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:18 +0000

    Add package cdn-it/1.0.3/cdn-it-1.0.3.tar.gz

---
+++ packages/cdn-it/cdn-it-1.0.3.tar.gz/cdn-it-1.0.3/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+  name = 'cdn-it',
+  packages = ['cdn_it'],
+  version = '1.0.3',
+  description = 'Quickly create an auto updating jsDelivr project',
+  author = 'Graeme Yeates',
+  author_email = 'megawac@gmail.com',
+  url = 'https://github.com/megawac/cdn-it',
+  download_url = 'https://github.com/megawac/cdn-it/tarball/v1.0.3',
+  keywords = ['cdn', 'jsdelivr'], # arbitrary keywords
+  classifiers = [],
+  entry_points={
+    'console_scripts': [
+        'cdn-it=cdn_it:main'
+    ]
+  }
+)
