ff44a4a75caddba8c78216f63e0d6c5c58bb13f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:14 +0000

    Add package graphistry/0.0.23/graphistry-0.0.23.tar.gz

---
+++ packages/graphistry/graphistry-0.0.23.tar.gz/graphistry-0.0.23/graphistry/pygraphistry.py
@@ -0,0 +1,91 @@
+from __future__ import absolute_import
+from future import standard_library
+standard_library.install_aliases()
+from builtins import object
+import sys
+import gzip
+import io
+import json
+import requests
+import numpy
+
+from . import util
+
+
+class PyGraphistry(object):
+    api_key = None
+    _tag = util.fingerprint()
+    _dataset_prefix = 'PyGraphistry/'
+    _hostname = 'localhost:3000'
+
+    @staticmethod
+    def register(key, server='proxy-labs.graphistry.com'):
+        PyGraphistry.api_key = key
+        shortcuts = {'localhost': 'localhost:3000',
+                     'staging': 'proxy-staging.graphistry.com',
+                     'labs': 'proxy-labs.graphistry.com'}
+        if server in shortcuts:
+            PyGraphistry._hostname = shortcuts[server]
+        else:
+            PyGraphistry._hostname = server
+
+    @staticmethod
+    def bind(node=None, source=None, destination=None,
+             edge_title=None, edge_label=None, edge_color=None, edge_weight=None,
+             point_title=None, point_label=None, point_color=None, point_size=None):
+
+        from . import plotter
+        return plotter.Plotter().bind(source, destination, node, \
+                              edge_title, edge_label, edge_color, edge_weight, \
+                              point_title, point_label, point_color, point_size)
+
+    @staticmethod
+    def _etl_url():
+        return 'http://%s/etl' % PyGraphistry._hostname
+
+    @staticmethod
+    def _viz_url(dataset_name, url_params):
+        extra = '&'.join([ k + '=' + str(v) for k,v in list(url_params.items())])
+        pattern = 'http://%s/graph/graph.html?dataset=%s&usertag=%s&%s'
+        return pattern % (PyGraphistry._hostname, dataset_name, PyGraphistry._tag, extra)
+
+    @staticmethod
+    def _etl(dataset):
+        if PyGraphistry.api_key is None:
+            raise ValueError('API key required')
+
+        json_dataset = json.dumps(dataset, ensure_ascii=False, cls=NumpyJSONEncoder)
+        headers = {'Content-Encoding': 'gzip', 'Content-Type': 'application/json'}
+        params = {'usertag': PyGraphistry._tag, 'agent': 'pygraphistry', 'apiversion' : '1',
+                  'agentversion': sys.modules['graphistry'].__version__,
+                  'key': PyGraphistry.api_key}
+
+        out_file = io.BytesIO()
+        with gzip.GzipFile(fileobj=out_file, mode='w', compresslevel=9) as f:
+            f.write(json_dataset.encode('utf8'))
+        try:
+            response = requests.post(PyGraphistry._etl_url(), out_file.getvalue(),
+                                     headers=headers, params=params)
+        except requests.exceptions.ConnectionError as e:
+            raise ValueError('Connection Error:', e.message)
+        except requests.exceptions.HTTPError as e:
+            raise ValueError('HTTP Error:', e.message)
+
+        jres = response.json()
+        if (jres['success'] is not True):
+            raise ValueError('Server reported error:', jres['msg'])
+        else:
+            return jres['dataset']
+
+
+register = PyGraphistry.register
+bind = PyGraphistry.bind
+
+
+class NumpyJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, numpy.ndarray) and obj.ndim == 1:
+                return obj.tolist()
+        elif isinstance(obj, numpy.generic):
+            return obj.item()
+        return json.JSONEncoder.default(self, obj)
---
+++ packages/graphistry/graphistry-0.0.23.tar.gz/graphistry-0.0.23/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+#from codecs import open
+#from os import path
+
+#here = path.abspath(path.dirname(__file__))
+
+#with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+ #   long_description = f.read()
+
+setup(
+    name='graphistry',
+    version='0.0.23',
+    packages = ['graphistry'],
+    platforms='any',
+    description = 'Visualize node-link graphs using Graphistry\'s cloud',
+    #long_description=long_description,
+    url='https://github.com/graphistry/pygraphistry',
+    download_url= 'https://pypi.python.org/pypi/graphistry/',
+    author='The Graphistry Team',
+    author_email='pygraphistry@graphistry.com',
+    setup_requires=['numpy'],
+    install_requires=['numpy', 'pandas', 'requests', 'future'],
+    extras_require={
+        'igraph': ['python-igraph'],
+        'networkx': ['networkx'],
+        'pandas-extra': ['numexpr', 'Bottleneck'],
+        'all': ['python-igraph', 'networkx', 'numexpr', 'Bottleneck']
+    },
+    license='BSD',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+    ],
+    keywords=['Graph', 'Network', 'Plot', 'Visualization', 'Pandas', 'Igraph']
+)
