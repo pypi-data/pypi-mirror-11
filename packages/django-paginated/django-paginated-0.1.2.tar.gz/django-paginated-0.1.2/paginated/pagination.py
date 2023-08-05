4429087baab99c80c02b59d4c9cc6e97c5594f12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:39 +0000

    Add package django-paginated/0.1.2/django-paginated-0.1.2.tar.gz

---
+++ packages/django-paginated/django-paginated-0.1.2.tar.gz/django-paginated-0.1.2/paginated/pagination.py
@@ -0,0 +1,30 @@
+from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage
+from django.core.paginator import PageNotAnInteger
+from django.http import Http404
+from django.template.loader import render_to_string
+
+from . import paginators
+from . import settings
+
+
+def paginated(queryset, request, per_page=settings.PAGINATED_PER_PAGE):
+    paginator = Paginator(queryset, per_page)
+
+    try:
+        page = paginator.page(request.GET.get('page', 1))
+    except PageNotAnInteger:
+        raise Http404
+    except EmptyPage:
+        raise Http404
+
+    pagination = render_to_string(
+        settings.PAGINATED_TEMPLATE,
+        paginators.digg_paginator({
+            'request': request,
+            'page_obj': page,
+            'paginator': paginator,
+        })
+    )
+
+    return page.object_list, pagination
