2ea220dfef890bc7cd0dbd7b6ec3d8944d9a2f51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:06 +0000

    Add package createsend/4.1.1/createsend-4.1.1.tar.gz

---
+++ packages/createsend/createsend-4.1.1.tar.gz/createsend-4.1.1/createsend/__init__.py
@@ -0,0 +1,12 @@
+﻿from createsend import __version__
+from createsend import *
+from client import Client
+from template import Template
+from list import List
+from segment import Segment
+from subscriber import Subscriber
+from campaign import Campaign
+from person import Person
+from administrator import Administrator
+from transactional import Transactional
+import utils
