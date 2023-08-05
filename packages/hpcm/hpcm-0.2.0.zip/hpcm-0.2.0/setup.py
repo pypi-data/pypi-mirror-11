e62d98ae6b95e3e2e61ab58bf8563971f1f0ab24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:02 +0000

    Add package hpcm/0.2.0/hpcm-0.2.0.zip

---
+++ packages/hpcm/hpcm-0.2.0.zip/hpcm-0.2.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='hpcm',
+    version='0.2.0',
+    packages=['hpcm'],
+    url='',
+    install_requires=['paramiko'],
+    license='MPL 2.0',
+    author='University of Notre Dame',
+    author_email='CRCSupport@nd.edu',
+    description=''
+)
