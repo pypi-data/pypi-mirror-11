76d2fb931d130d3f6e546358f4858f8a52d004fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:46 +0000

    Add package djangorestframework-jwt/1.7.0/djangorestframework_jwt-1.7.0-py2.py3-none-any.whl

---
+++ packages/djangorestframework-jwt/djangorestframework_jwt-1.7.0-py2.py3-none-any.whl/rest_framework_jwt/compat.py
@@ -0,0 +1,59 @@
+import rest_framework
+from django.forms import widgets
+from distutils.version import StrictVersion
+
+
+if StrictVersion(rest_framework.VERSION) < StrictVersion('3.0.0'):
+    from rest_framework.serializers import Serializer, CharField
+
+    class PasswordField(CharField):
+        widget = widgets.PasswordInput
+else:
+    class Serializer(rest_framework.serializers.Serializer):
+        @property
+        def object(self):
+            return self.validated_data
+
+    class PasswordField(rest_framework.serializers.CharField):
+        style = {
+            'input_type': 'password'
+        }
+
+
+def get_user_model():
+    try:
+        from django.contrib.auth import get_user_model
+    except ImportError:  # Django < 1.5
+        from django.contrib.auth.models import User
+    else:
+        User = get_user_model()
+
+    return User
+
+
+def get_username_field():
+    try:
+        username_field = get_user_model().USERNAME_FIELD
+    except:
+        username_field = 'username'
+
+    return username_field
+
+
+def get_username(user):
+    try:
+        username = user.get_username()
+    except AttributeError:
+        username = user.username
+
+    return username
+
+
+def get_request_data(request):
+    try:
+        data = request.data
+    except:
+        # DRF < 3.2
+        data = request.DATA
+
+    return data
