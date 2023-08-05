dcafe23cfa6c778ba70299fa05726e33d5294c7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:07 +0000

    Add package xym/0.1.1/xym-0.1.1.tar.gz

---
+++ packages/xym/xym-0.1.1.tar.gz/xym-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'xym',
+  packages = ['xym'], # this must be the same as the name above
+  version = '0.1.1',
+  description = 'A tool to extract YANG modules from IETF RFCs and Drafts',
+  author = 'Jan Medved',
+  author_email = 'jmedved@cisco.com',
+  url = 'https://github.com/cmoberg/xym',
+  keywords = ['yang', 'extraction'], # arbitrary keywords
+  classifiers = [],
+)
