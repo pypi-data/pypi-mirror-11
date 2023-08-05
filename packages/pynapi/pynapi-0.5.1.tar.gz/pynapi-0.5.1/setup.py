62a8918bb8d19afe621953e5b09c61e9c3890235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:41 +0000

    Add package pynapi/0.5.1/pynapi-0.5.1.tar.gz

---
+++ packages/pynapi/pynapi-0.5.1.tar.gz/pynapi-0.5.1/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name = "pynapi",
+    author = 'Grzegorz Dzięgielewski',
+    author_email = 'jabbas@jabbas.eu',
+    url = "https://github.com/jabbas/pynapi",
+    version = '0.5.1',
+    description = 'subtitles downloader',
+    entry_points = { 'console_scripts': [ 'pynapi = pynapi.cmdline:cmdline' ] },
+    packages = find_packages()
+)
