bee9a57b0b73b27a6d80fb254fc87a1eb0c6bf26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:29 +0000

    Add package textme/1.0/textme-1.0.tar.gz

---
+++ packages/textme/textme-1.0.tar.gz/textme-1.0/setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup
+
+setup(name='textme',
+      version='1.0',
+      description='deprecated package -- see elkme',
+      url='http://github.com/46elks/elkme',
+      author='Emil Tullstedt',
+      author_email='emil@46elks.com',
+      license='MIT',
+      install_requires=[
+          'elkme',
+      ],
+      zip_safe=False)
