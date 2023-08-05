6bb333b72419e9b26ebcdfca8ba3b771e7d3e698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:05 +0000

    Add package ajenti.plugin.packages/0.19/ajenti.plugin.packages-0.19.tar.gz

---
+++ packages/ajenti.plugin.packages/ajenti.plugin.packages-0.19.tar.gz/ajenti.plugin.packages-0.19/ajenti_plugin_packages/main.py
@@ -0,0 +1,29 @@
+from jadi import component
+
+from aj.plugins.core.api.sidebar import SidebarItemProvider
+from .api import PackageManager
+
+
+@component(SidebarItemProvider)
+class ItemProvider(SidebarItemProvider):
+    def __init__(self, context):
+        self.context = context
+
+    def provide(self):
+        children = [{
+            'attach': 'packages',
+            'name': mgr.name,
+            'icon': 'gift',
+            'url': '/view/packages/%s' % mgr.id,
+            'children': [],
+        } for mgr in PackageManager.all(self.context)]
+        return [
+            {
+                'attach': 'category:system',
+                'id': 'packages',
+                'name': 'Packages',
+                'icon': 'gift',
+                'url': '/view/packages/%s' % PackageManager.all(self.context)[0].id,
+                'children': children,
+            }
+        ]
