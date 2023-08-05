f9967602c0352233d349822de5b62aa6849f2529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:50 +0000

    Add package Thanatos/0.3.2/Thanatos-0.3.2.tar.gz

---
+++ packages/Thanatos/Thanatos-0.3.2.tar.gz/Thanatos-0.3.2/thanatos/categories.py
@@ -0,0 +1,38 @@
+
+
+# The Geography category and it's sub-categories
+geography = {
+    'name': 'Geography',
+    'description': 'Questions regarding the solar systems and regions of New Eden.',
+}
+
+geography_regions = {
+    'name': 'Regions',
+    'description': 'Questions specifically about the regions of New Eden.',
+}
+
+geography_miscellaneous = {
+    'name': 'Miscellaneous',
+    'description': 'Miscellaneous questions about the geography of New Eden.',
+}
+
+# The Inventory category and its sub-categories
+inventory = {
+    'name': 'Inventory',
+    'description': 'Questions regarding the ships, modules, and other items.',
+}
+
+inventory_slots = {
+    'name': 'Slots',
+    'description': 'Questions about the number of different slots ships have.',
+}
+
+inventory_ship_id = {
+    'name': 'ShipID',
+    'description': 'Questions asking to identify what ship is being shown.',
+}
+
+inventory_ship_id_hard = {
+    'name': 'ShipIDHard',
+    'description': 'Questions asking to identify the variation of the ship shown.',
+}
