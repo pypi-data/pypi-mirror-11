75160dcad4b02045c8d691e55f628761e995223c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:27 +0000

    Add package KarmaDbg/0.1.22/KarmaDbg-0.1.22-py2-none-any.whl

---
+++ packages/KarmaDbg/KarmaDbg-0.1.22-py2-none-any.whl/karmadbg/widgets/stdwidgetpack/sources.py
@@ -0,0 +1,370 @@
+
+import cgi
+import os.path
+
+from PySide.QtCore import Qt, QObject, Signal, QTextCodec
+from PySide.QtGui import QDockWidget, QTextEdit, QAction, QTextCursor
+
+from karmadbg.uicore.async import async
+
+
+class SourceView(QTextEdit):
+
+    htmlTemplate = \
+    '''
+        <!DOCTYPE html>
+        <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+        <head>
+        <meta charset="utf-8" />
+        <title></title>
+        <style>
+        %s
+        </style>
+        </head>
+        <body>
+        %s
+        </body>
+        </html>
+    '''
+
+    breakpointAdded = Signal(int)
+    breakpointRemoved = Signal(int)
+
+    def __init__(self, parent, css):
+        super(SourceView,self).__init__(parent)
+        self.setReadOnly(True)
+        self.currentLine = -1
+        self.breakpointLines = set()
+        self.css = css
+        self.codec = QTextCodec.codecForLocale()
+
+    def setContent(self, content):
+        self.rawContent = content
+        self.highlight()
+        self.setHtml(self.highlightedContent)
+
+    def highlight(self):
+        content = self.rawContent.splitlines()
+        body = ""
+        for i in xrange(len(content)):
+            lineno = i + 1
+            line = self.codec.toUnicode(content[i])
+            if line == "":
+                line = " "
+            if lineno == self.currentLine:
+                body += self.highlightCurrentLine(line)
+            elif lineno in self.breakpointLines:
+                body += self.highlightBreakpointLine(line)
+            else:
+                body += self.highlightSourceLine(line)
+
+        self.highlightedContent = self.htmlTemplate % (self.css, body)
+
+    def highlightCurrentLine(self, str):
+        return "<div class=\"current\"><pre>%s</pre></div>\n" % cgi.escape(str)
+
+    def highlightSourceLine(self, str):
+        return "<div class=\"normal\"><pre>%s</pre></div>" % cgi.escape(str)
+
+    def highlightBreakpointLine(self, str):
+        return "<div class=\"breakpoint\"><pre>%s</pre></div>" % cgi.escape(str)
+
+    def setCurrentLine(self, lineno):
+        self.currentLine = lineno
+        self.highlight()
+        self.setHtml(self.highlightedContent)
+        self.showLine(lineno)
+
+    def resetCurrentLine(self):
+        self.currentLine = -1
+        self.highlight()
+        self.setHtml(self.highlightedContent)
+
+    def addBreakpoint(self,lineno):
+        self.breakpointLines.add(lineno)
+        self.highlight()
+        self.setHtml(self.highlightedContent)
+
+    def removeBreakpoint(self, lineno):
+        self.breakpointLines.remove(lineno)
+        self.highlight()
+        self.setHtml(self.highlightedContent)
+
+    def removeAllBreakpoints(self):
+        self.breakpointLines = set()
+        self.highlight()
+        self.setHtml(self.highlightedContent)
+
+    def getBreakpointLines(self):
+        return list(self.breakpointLines)
+
+    def contextMenuEvent(self, event):
+
+        menu = self.createStandardContextMenu()
+
+        def getSeFileEncoding(codecName):
+            return lambda : self.setFileEncoding(codecName) 
+        encodingMenu = menu.addMenu("Encoding")
+
+        codecNames = [codeName.data() for codeName in QTextCodec.availableCodecs()]
+        codecNames.sort()
+
+        currentCodecName = str(self.codec.name())
+
+        for codecName in codecNames:
+            action = QAction(codecName, self)
+            action.setCheckable(True)
+            action.triggered.connect( getSeFileEncoding(codecName) )
+            if codecName == currentCodecName:
+                action.setChecked(True)
+            encodingMenu.addAction(action)
+
+        textPosition = self.cursorForPosition(event.pos()).position()
+
+        bpLineNo = reduce(lambda x,y: x +1 if y=="\n" else x, self.rawContent[:textPosition], 0) + 1
+        bpAction = QAction("Toggle breakpoint",self)
+        bpAction.triggered.connect(lambda : self.setBreakpointOnLine(bpLineNo))
+        menu.addAction(bpAction)
+
+        menu.exec_(event.globalPos())
+
+    def setBreakpointOnLine(self,bpLineNo):
+        if bpLineNo in self.breakpointLines:
+            self.breakpointRemoved.emit(bpLineNo)
+        else:
+            self.breakpointAdded.emit(bpLineNo)
+
+    def setFileEncoding(self,codecName):
+        self.codec = QTextCodec.codecForName(codecName)
+        self.highlight()
+        self.setHtml(self.highlightedContent)
+
+    def showLine(self, line):
+        cursor = QTextCursor(self.document().findBlockByLineNumber(line) )
+        self.setTextCursor(cursor)
+        self.ensureCursorVisible()
+
+class PythonSourceView(SourceView):
+
+    def __init__(self, parent, css):
+        super(PythonSourceView,self).__init__(parent, css)
+
+class CppSourceView(SourceView):
+
+    def __init__(self, parent, css):
+        super(CppSourceView,self).__init__(parent, css)
+
+
+class SourceWidget( QDockWidget ):
+
+    def __init__(self, uimanager, sourceFileName):
+        super(SourceWidget, self).__init__()
+        self.uimanager = uimanager
+        self.mainWnd =uimanager.mainwnd
+        self.sourceFileName = sourceFileName
+        self.setWindowTitle(self.sourceFileName)
+
+    def reload(self):
+        with open(self.sourceFileName) as f:
+            fileContent = f.read()
+            self.sourceView.setContent(fileContent)
+
+    def setCurrentLine(self,lineno):
+        self.sourceView.setCurrentLine(lineno)
+
+    def resetCurrentLine(self):
+        self.sourceView.resetCurrentLine()
+
+    def addBreakpoint(self, lineno):
+        self.sourceView.addBreakpoint(lineno)
+
+    def removeBreakpoint(self,lineno):
+        self.sourceView.removeBreakpoint(lineno)
+
+    def removeAllBreakpoints(self):
+        self.sourceView.removeAllBreakpoints()
+
+    def showLine(self, line):
+        self.sourceView.showLine(line)
+
+class PythonSourceWidget(SourceWidget):
+
+    def __init__(self, uimanager, sourceFileName):
+        super(PythonSourceWidget,self).__init__(uimanager, sourceFileName)
+        self.sourceView = PythonSourceView(self, uimanager.docCss)
+        self.setWidget(self.sourceView)
+        self.sourceView.breakpointAdded.connect(self.onAddBreakpoint)
+        self.sourceView.breakpointRemoved.connect(self.onRemoveBreakpoint)
+
+    def onAddBreakpoint(self, bpline):
+        self.uimanager.debugClient.addPythonBreakpoint(self.sourceFileName, bpline)
+
+    def onRemoveBreakpoint(self, bpline):
+        self.uimanager.debugClient.removePythonBreakpoint(self.sourceFileName, bpline)
+
+
+class NativeSourceWidget(SourceWidget):
+
+    def __init__(self, uimanager, sourceFileName):
+        super(NativeSourceWidget,self).__init__(uimanager, sourceFileName)
+        self.sourceView = CppSourceView(self, uimanager.docCss)
+        self.setWidget(self.sourceView)
+        self.sourceView.breakpointAdded.connect(self.onAddBreakpoint)
+        self.sourceView.breakpointRemoved.connect(self.onRemoveBreakpoint)
+
+    def onAddBreakpoint(self, bpline):
+        self.uimanager.debugClient.addBreakpoint(self.sourceFileName, bpline)
+
+    def onRemoveBreakpoint(self, bpline):
+        self.uimanager.debugClient.removeBreakpoint(self.sourceFileName, bpline)
+
+
+class SourceManager(QObject):
+
+    def __init__(self, widgetSettings, uimanager):
+        QObject.__init__(self)
+        self.uimanager = uimanager
+        self.openSources = {}
+        self.currentNativeSource = None
+        self.currentPythonSource = None
+
+        self.uimanager.showSourceRequired.connect(self.onShowSource)
+
+        self.uimanager.debugClient.targetStopped.connect(self.onTargetStopped)
+        self.uimanager.debugClient.targetRunning.connect(self.onTargetRunning)
+        self.uimanager.debugClient.targetDetached.connect(self.onTargetDetached)
+        self.uimanager.debugClient.targetThreadChanged.connect(self.onTargetDataChanged)
+        self.uimanager.debugClient.targetFrameChanged.connect(self.onTargetDataChanged)
+        self.uimanager.debugClient.targetBreakpointsChanged.connect(self.onTargetDataChanged)
+
+        self.uimanager.debugClient.pythonStopped.connect(self.onPythonStopped)
+        self.uimanager.debugClient.pythonRunning.connect(self.onPythonRunning)
+        self.uimanager.debugClient.pythonBreakpointAdded.connect(self.onPythonBreakpointAdded)
+        self.uimanager.debugClient.pythonBreakpointRemoved.connect(self.onPythonBreakpointRemoved)
+        self.uimanager.debugClient.pythonExit.connect(self.onPythonExit)
+        self.uimanager.debugClient.pythonStarted.connect(self.onPythonStarted)
+
+    def getSourceWidget(self, fileName, pythonSource = False):
+
+        if fileName=="":
+            return None
+
+        if not os.path.isfile(fileName):
+            return None
+
+        if fileName in self.openSources:
+            return  self.openSources[fileName]
+        else:
+            try:
+                if pythonSource:
+                    source = PythonSourceWidget(self.uimanager, fileName)
+                else:
+                    source = NativeSourceWidget(self.uimanager, fileName)
+                source.reload()
+                self.uimanager.mainwnd.addDockWidget(Qt.TopDockWidgetArea,source)
+                if len(self.openSources) > 0:
+                    self.uimanager.mainwnd.tabifyDockWidget(self.openSources.values()[0], source)
+                self.openSources[fileName] = source
+                return source
+            except IOError:
+                return None
+
+        return None
+
+    @async
+    def onTargetStopped(self):
+
+        self.currentTragetSource = None
+
+        fileName, line = yield ( self.uimanager.debugClient.getSourceLineAsync() )
+
+        if fileName == "":
+            return
+
+        if not os.path.exists(fileName):
+            fileName = yield( self.uimanager.debugClient.getSourceFileFromServerAsync() )
+            if fileName == "":
+                return
+
+        source = self.getSourceWidget(fileName)
+
+        if  source:
+            source.setVisible(True)
+            source.raise_()
+            source.setCurrentLine(line)
+            self.currentNativeSource = source
+
+    def onTargetDataChanged(self):
+        self.onTargetStopped()
+
+    def onTargetRunning(self):
+        if self.currentNativeSource:
+            self.currentNativeSource.resetCurrentLine()
+            self.currentNativeSource = None
+
+    def onTargetDetached(self):
+
+        if self.currentNativeSource:
+            self.currentNativeSource.resetCurrentLine()
+            self.currentNativeSource = None
+
+        for source in self.openSources.values():
+            if type(source) is NativeSourceWidget:
+                source.removeAllBreakpoints()
+
+
+    def onPythonStarted(self, fileName):
+        source = self.getSourceWidget(fileName, pythonSource = True)
+        if source:
+            source.reload()
+
+    @async
+    def onPythonStopped(self):
+
+        self.currentPythonSource = None
+
+        fileName,line = yield( self.uimanager.debugClient.getPythonSourceLineAsync() )
+
+        source = self.getSourceWidget(fileName, pythonSource = True)
+
+        if source:
+            source.setCurrentLine(line)
+            source.setVisible(True)
+            source.raise_()
+            self.currentPythonSource = source
+
+
+    def onPythonRunning(self):
+        if self.currentPythonSource:
+            self.currentPythonSource.resetCurrentLine()
+            self.currentPythonSource = None
+
+    def onPythonBreakpointAdded(self, filename, lineno):
+        if filename in self.openSources:
+            source = self.openSources[filename]
+            source.addBreakpoint(lineno)
+
+    def onPythonBreakpointRemoved(self, filename, lineno):
+        if filename in self.openSources:
+            source = self.openSources[filename]
+            source.removeBreakpoint(lineno)
+
+    def onPythonExit(self):
+
+        if self.currentPythonSource:
+            self.currentPythonSource.resetCurrentLine()
+            self.currentPythonSource = None
+
+        for source in self.openSources.values():
+            if type(source) is PythonSourceWidget:
+                source.removeAllBreakpoints()
+
+    def onShowSource(self, fileName, line):
+
+        path, ext  = os.path.splitext(fileName)
+
+        source = self.getSourceWidget(fileName, pythonSource = ext == '.py')
+        if  source:
+            source.setVisible(True)
+            source.raise_()
+            source.showLine(line)
