caaa5c48cef6fc8ab40191a7edc60e9126df0a79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:17 +0000

    Add package biomartian/0.0.3/biomartian-0.0.3.tar.gz

---
+++ packages/biomartian/biomartian-0.0.3.tar.gz/biomartian-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+setup(
+    name = "biomartian",
+    packages = ["biomartian", "biomartian/args", "biomartian/r", "biomartian/lists", "biomartian/config"],
+    scripts = ["bin/biomartian"],
+    version = "0.0.3",
+    description = "Access BioMart from the command line.",
+    author = "Endre Bakken Stovner",
+    author_email = "endrebak@stud.ntnu.no",
+    url = "http://github.com/endrebak/biomartian",
+    keywords = ["BioMart"],
+    license = ["GPL-3.0"],
+    install_requires = ["pandas>=0.16", "ebs", "docopt", "joblib", "widediaper"],
+    classifiers = [
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Topic :: Scientific/Engineering"],
+    long_description = ("Access BioMart from the command line. See webpage for more info."))
