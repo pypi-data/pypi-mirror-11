a296c095926d8dba6b0e6a93f58431b28e65e3f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:49 +0000

    Add package pdtools/0.1.56/pdtools-0.1.56.tar.gz

---
+++ packages/pdtools/pdtools-0.1.56.tar.gz/pdtools-0.1.56/pdtools/lib/exceptions.py
@@ -0,0 +1,33 @@
+'''
+Exceptions and their subclasses
+
+TODO: Distill these down and make a heirarchy.
+'''
+
+
+class PdServerException(Exception):
+    pass
+
+
+class InteralException(Exception):
+    pass
+
+
+class AuthenticationError(PdServerException):
+    pass
+
+
+class InvalidCredentials(PdServerException):
+    pass
+
+
+class PdidError(PdServerException):
+    pass
+
+
+class PdidExclusionError(PdServerException):
+    pass
+
+
+class RiffleError(PdServerException):
+    pass
