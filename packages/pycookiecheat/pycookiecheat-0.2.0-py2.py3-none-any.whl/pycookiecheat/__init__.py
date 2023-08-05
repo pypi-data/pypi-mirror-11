937ae2657c2edd566dd8e38f9b054e03dd7dea2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:30 +0000

    Add package pycookiecheat/0.2.0/pycookiecheat-0.2.0-py2.py3-none-any.whl

---
+++ packages/pycookiecheat/pycookiecheat-0.2.0-py2.py3-none-any.whl/pycookiecheat/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from .pycookiecheat import chrome_cookies  # noqa
+from ._version import __version__  # noqa
+
+__author__ = 'Nathan Henrie'
+__email__ = 'nate@n8henrie.com'
