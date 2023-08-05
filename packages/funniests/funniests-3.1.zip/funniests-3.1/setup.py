a56cc5ad03a512ce6eb7acb8e0753cf7db9afb23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:33 +0000

    Add package funniests/3.1/funniests-3.1.zip

---
+++ packages/funniests/funniests-3.1.zip/funniests-3.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniests',
+      version='3.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniests'],
+      zip_safe=False)
