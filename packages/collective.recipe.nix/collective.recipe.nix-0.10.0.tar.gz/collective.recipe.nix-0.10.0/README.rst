9f798c0a6fa6b0b37c0cb51c165b5b66a16f479e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:47 +0000

    Add package collective.recipe.nix/0.10.0/collective.recipe.nix-0.10.0.tar.gz

---
+++ packages/collective.recipe.nix/collective.recipe.nix-0.10.0.tar.gz/collective.recipe.nix-0.10.0/README.rst
@@ -0,0 +1,152 @@
+collective.recipe.nix
+=====================
+
+This is an experimental buildout recipe for creating nix expression from
+a buildout eggs list. This is work in progress. Please, contribute_.
+
+.. _contribute: https://github.com/datakurre/collective.recipe.nix
+
+Currently the recipe generates three kind of expressions:
+
+* default [name].nix usable with nix-shell
+* buildEnv based [name]-env.nix usable with nix-build
+* buildPythonPackage based [name]-package.nix usable with nix-env -i -f
+
+
+Example of usage
+----------------
+
+At first, define ``./default.nix`` with buildout::
+
+    with import <nixpkgs> {}; {
+      myEnv = stdenv.mkDerivation {
+        name = "myEnv";
+        buildInputs = [
+          pythonPackages.buildout
+        ];
+        shellHook = ''
+          export SSL_CERT_FILE=~/.nix-profile/etc/ca-bundle.crt
+        '';
+      };
+    }
+
+And example ``./buildout.cfg``:
+
+.. code:: cfg
+
+    [buildout]
+    extends = https://dist.plone.org/release/4-latest/versions.cfg
+    parts =
+        plone
+        releaser
+    versions = versions
+
+    [instance]
+    recipe = plone.recipe.zope2instance
+    eggs = Plone
+    user = admin:admin
+
+    [plone]
+    recipe = collective.recipe.nix
+    eggs =
+        ${instance:eggs}
+        plone.recipe.zope2instance
+
+    [releaser]
+    recipe = collective.recipe.nix
+    eggs = zest.releaser[recommended]
+
+    [versions]
+    zc.buildout =
+    setuptools =
+
+Run the buildout:
+
+.. code:: bash
+
+   $ nix-shell --run buildout
+
+Now you should be able to run zest.releaser with recommended plugins with:
+
+.. code:: bash
+
+   $ nix-shell zest.releaser.nix --run fullrelease
+
+And launching python with all Plone dependencies (after removing
+buildout created site.py to remove references from buildout installed
+eggs) with:
+
+.. code:: bash
+
+   $ rm -f parts/instance/site.py parts/instance/site.pyc
+   $ nix-shell plone.nix --run python
+
+Plone could be started by entering the following lines into the
+interpreter:
+
+.. code:: python
+
+    import plone.recipe.zope2instance.ctl
+    plone.recipe.zope2instance.ctl.main(['-C', 'parts/instance/etc/zope.conf', 'fg'])
+
+
+Advanced configuration
+======================
+
+Adding additional nixpkgs buildInputs:
+
+.. code:: cfg
+
+   [plone]
+   ...
+   build-inputs =
+       redis
+
+Mapping nixpkgs buildInputs for generated Python expressions:
+
+.. code:: cfg
+
+   [plone]
+   ...
+   build-inputs =
+      dataflake.fakeldap=pythonPackages."setuptools-git"
+
+Mapping Python packages as propagatedBuildInputs for generated Python
+expressions:
+
+.. code:: cfg
+
+   [robot]
+   ...
+   propagated-build-inputs =
+      robotframework=robotframework-selenium2library
+
+Replacing otherwise generated Python expressions with existing nixpkgs
+expressions:
+
+.. code:: cfg
+
+   [plone]
+   ...
+   nixpkgs =
+       python-ldap=pythonPackages.ldap
+
+Adding URLs for packages not available at PyPI:
+
+.. code:: cfg
+
+   [plone]
+   ...
+   urls =
+       Plone=https://example.com/Plone-4.3.6.zip#md5=c370c0c8eace1081ec5b057b2c4149b7
+
+or:
+
+.. code:: cfg
+
+   [plone]
+   ...
+   urls = urls
+
+   [urls]
+   Plone = https://example.com/Plone-4.3.6.zip#md5=c370c0c8eace1081ec5b057b2c4149b7
---
+++ packages/collective.recipe.nix/collective.recipe.nix-0.10.0.tar.gz/collective.recipe.nix-0.10.0/src/collective/recipe/nix/__init__.py
@@ -0,0 +1,355 @@
+# -*- coding: utf-8 -*-
+import re
+import xmlrpclib
+import zc.recipe.egg
+
+from pkg_resources import Requirement
+from pkg_resources import parse_version
+
+# Map requirements to nixpkgs
+NIXPKGS = {
+    'lxml': 'pythonPackages.lxml',
+    'Pillow': 'pythonPackages.pillow',
+    'python-ldap': 'pythonPackages.ldap'
+}
+
+# Some packages require additional build input
+BUILD_INPUTS = {
+    'Products.DCWorkflow': ['eggtestinfo'],
+    'Products.CMFUid': ['eggtestinfo'],
+    'Products.CMFActionIcons': ['eggtestinfo'],
+    'dataflake.fakeldap': ['pythonPackages."setuptools-git"'],
+    'Products.LDAPUserFolder': ['pythonPackages."setuptools-git"']
+}
+
+# Some packages may be available only from outside PyPI
+URLS = {
+}
+
+# And some of those required build inputs are not yet available at nixpkgs
+EXPRESSIONS = {
+    'eggtestinfo': """\
+  eggtestinfo = buildPythonPackage {
+    name = "eggtestinfo-0.3";
+    src = fetchurl {
+        url = "https://pypi.python.org/packages/source/e/eggtestinfo/eggtestinfo-0.3.tar.gz";
+        md5 = "6f0507aee05f00c640c0d64b5073f840";
+    };
+    doCheck = false;
+  };
+"""
+}
+
+# Filter buildout supported source distributions from eggs and wheels
+SDIST_URL = re.compile('.*tar.gz$|.*zip$')
+
+
+def prefix(s, prefix='_'):
+    return prefix + s
+
+
+def normalize(s):
+    # Normalize given derivation name and prefix it with '_' to avoid
+    # possible nixpkgs conflicts
+    return re.sub('[\.\-\s\[]', '_', s)
+
+
+def listify(s):
+    return filter(bool, map(str.strip, (s or '').split()))
+
+
+def see(project_name, requirements, ws, seen):
+    for req in requirements:
+        key = '{0:s}-{1:s}'.format(*sorted([
+            normalize(project_name), normalize(req.project_name)]))
+        if key not in seen:
+            seen[key] = True
+            see(project_name, ws.find(req).requires(req.extras), ws, seen)
+
+
+def resolve_dependencies(requirements, ws, inject, requires=None, seen=None):
+
+    # Init
+    if seen is None:
+        seen = {}
+    if requires is None:
+        requires = {}
+
+    # Resolve
+    for requirement in requirements:
+        distribution = ws.find(requirement)
+        name = distribution.project_name
+
+        if name in requires:
+            continue
+
+        requires[name] = []
+
+        distribution_requires = distribution.requires(requirement.extras)
+        distribution_requires += inject.get(normalize(name), [])
+
+        for req in [normalize(ws.find(r).project_name)
+                    for r in distribution_requires]:
+            key = '{0:s}-{1:s}'.format(*sorted([normalize(name), req]))
+            if key not in seen and req not in requires[name]:
+                requires[name].append(req)
+
+        see(name, distribution_requires, ws, seen)
+        resolve_dependencies(distribution_requires, ws, inject, requires, seen)
+
+    return requires
+
+
+def spliturl(url):
+    if '#md5=' in url:
+        return url.split('#md5=', 1)
+    elif '#' in url:
+        return url.split('#', 1)
+    return url, ''
+
+
+class Nix(object):
+
+    def __init__(self, buildout, name, options):
+        self.buildout = buildout
+        self.name = (option.get('name') or '').strip() or name
+        self.options = options
+        self.pypi = xmlrpclib.ServerProxy('https://pypi.python.org/pypi')
+
+        # Parse propagatedBuildInputs from buildout already in __init__ to pass
+        # found additional eggs forward to zc.recipe.egg
+        propagated_eggs = []
+        propagated_build_inputs = {}
+        for section in listify(self.options.get('propagated-build-inputs')):
+            if section in self.buildout:
+                for key, value in self.buildout.get(section).items():
+                    propagated_build_inputs[key] = listify(value)
+                    propagated_eggs.extend(listify(value))
+            elif '=' in section:
+                # is not a section, but inline mapping
+                key, value = section.split('=', 1)
+                propagated_build_inputs.setdefault(key, [])
+                propagated_build_inputs[key].extend(value.split(','))
+                propagated_eggs.extend(value.split(','))
+
+        # Update options['eggs'] with found additional propagatedBuildInputs
+        options = options.copy()
+        options['eggs'] = '\n'.join([options.get('eggs', ''),
+                                     '\n'.join(set(propagated_eggs))])
+
+        # Save revolved egg and parsed propagatedBuildInputs
+        self.egg = zc.recipe.egg.Scripts(buildout, name, options)
+        self.propagated_build_inputs = propagated_build_inputs
+
+    def install(self):
+        expressions = EXPRESSIONS.copy()
+        requirements, ws = self.egg.working_set()
+        packages = {}
+
+        # Init package metadata
+        for distribution in ws:
+            packages[normalize(distribution.project_name)] = {
+                'key': prefix(normalize(distribution.project_name)),
+                'name': '%s-%s' % (distribution.project_name,
+                                   distribution.version),
+                'buildInputs': BUILD_INPUTS.get(distribution.project_name, []),
+                'propagatedBuildInputs': []
+            }
+
+        # resolve env buildInputs and package buildInputs from buildout
+        env_build_inputs = []
+        for section in listify(self.options.get('build-inputs')):
+            if section in self.buildout:
+                for key, value in self.buildout.get(section).items():
+                    key = normalize(ws.find(
+                        Requirement.parse(key)).project_name)
+                    packages[key]['buildInputs'] = listify(value)
+            elif '=' in section:
+                # is not a section, but inline mapping
+                key, value = section.split('=', 1)
+                key = normalize(ws.find(Requirement.parse(key)).project_name)
+                packages[key].setdefault('buildInputs', [])
+                packages[key]['buildInputs'].extend(value.split(','))
+            else:
+                # is not a section, but an environment direct buildInput
+                env_build_inputs.append(section)
+
+        # Resolve propagatedBuildInputs from package dependencies and buildout
+        for project_name, requires in resolve_dependencies(
+                map(Requirement.parse, requirements), ws,
+                dict([(normalize(key), map(Requirement.parse, value))
+                      for key, value
+                      in self.propagated_build_inputs.items()])).items():
+            packages[normalize(project_name)]['propagatedBuildInputs'] = \
+                map(prefix, map(normalize, requires))
+
+        # Parse package download urls and md5s from buildout
+        for project_name, url in URLS.items():
+            packages[normalize(project_name)]['url'] = url
+        for section in listify(self.options.get('urls')):
+            if section in self.buildout:
+                for key, value in self.buildout.get(section).items():
+                    key = normalize(ws.find(
+                        Requirement.parse(key)).project_name)
+                    if '#' in value:
+                        url, md5 = spliturl(value.strip())
+                        packages[key]['url'] = url
+                        packages[key]['md5'] = md5
+            elif '=' in section:
+                # is not a section, but inline mapping
+                key, value = section.split('=', 1)
+                key = normalize(ws.find(Requirement.parse(key)).project_name)
+                if '#' in value:
+                    url, md5 = spliturl(value.strip())
+                    packages[key]['url'] = url
+                    packages[key]['md5'] = md5
+
+        # Parse package to nixpkgs mapping from buildout
+        nixpkgs = NIXPKGS.copy()
+        for section in listify(self.options.get('nixpkgs')):
+            if section in self.buildout:
+                for key, value in self.buildout.get(section).items():
+                    key = normalize(ws.find(
+                        Requirement.parse(key)).project_name)
+                    nixpkgs[key] = ' '.join(listify(value))
+            elif '=' in section:
+                # is not a section, but inline mapping
+                key, value = section.split('=', 1)
+                key = normalize(ws.find(Requirement.parse(key)).project_name)
+                nixpkgs[key] = ' '.join(value.split(','))
+
+        output = """\
+with import <nixpkgs> {};
+let dependencies = rec {
+"""
+        for distribution in ws:
+
+            # For mapped packages, use existing nixpkgs package instead
+            if normalize(distribution.project_name) in nixpkgs:
+                output += """\
+  {package_name:s} = {nixpkgs_package_name:s};
+""".format(package_name=prefix(normalize(distribution.project_name)),
+           nixpkgs_package_name=nixpkgs[normalize(distribution.project_name)])
+                continue
+
+            # Build expression for package
+            data = packages[normalize(distribution.project_name)]
+
+            # Resolve URL and MD5
+            if not 'url' or 'md5' not in data:
+                names = [
+                    distribution.project_name,
+                    distribution.project_name.replace('-', '_'),
+                    distribution.project_name.capitalize(),
+                    distribution.project_name.capitalize().replace('-', '_'),
+                ]
+                found_name, found_version = None, None
+                for name in set(names):
+                    for version in sorted(  # find best matching version
+                            self.pypi.package_releases(name, True)):
+                        found_name = name
+                        if parse_version(version) == \
+                                parse_version(distribution.version):
+                            found_version = version
+                            break  # break on exact match
+                        if found_version is None:
+                            found_version = version
+                        if parse_version(found_version) <= \
+                               parse_version(version) <= \
+                               parse_version(distribution.version):
+                            found_version = version
+                    if found_name is not None:
+                        break
+                releases = [release for release in self.pypi.release_urls(
+                            found_name, found_version)
+                            if SDIST_URL.match(release['url'])]
+                assert releases,\
+                    "Distribution {0:s}-{1:s} not found at PyPI".format(
+                        distribution.project_name, distribution.version)
+                data['url'] = releases[0]['url']
+                data['md5'] = releases[0]['md5_digest']
+
+            # Resolve extra expressions
+            data['extras'] = filter(bool, [
+                expressions.pop(req, '') for req in data['buildInputs']])
+
+            # Build substitution dictionary
+            substitutions = dict(
+                key=data['key'], name=data['name'],
+                url=data['url'], md5=data['md5'],
+                buildInputs='\n      '.join(data['buildInputs']),
+                propagatedBuildInputs=
+                '\n      '.join(data['propagatedBuildInputs']),
+                extras='\n'.join(data['extras']))
+
+            output += """\
+  {key:s} = buildPythonPackage {{
+    name = "{name:s}";
+    src = fetchurl {{
+        url = "{url:s}";
+        md5 = "{md5:s}";
+    }};
+    buildInputs = [
+      {buildInputs:s}
+    ];
+    propagatedBuildInputs = [
+      {propagatedBuildInputs:s}
+    ];
+    doCheck = false;
+  }};
+{extras:s}""".format(**substitutions)
+
+        # Filter direct requirements
+        requirements = [ws.find(Requirement.parse(req)).project_name
+                        for req in requirements
+                        if req in self.options.get('eggs', '')]
+
+        # Build substitution dictionary
+        substitutions = dict(
+            name=self.name,
+            paths='\n    '.join(env_build_inputs),
+            extraLibs='\n        '.join(
+                map(prefix, map(normalize, set(requirements)))),
+            buildInputs='\n    '.join(
+                map(prefix, map(normalize, set(requirements)))
+                + env_build_inputs))
+
+        with open(self.name + '.nix', 'w') as handle:
+            handle.write(output + """\
+}};
+in with dependencies; stdenv.mkDerivation {{
+  name = "{name:s}";
+  buildInputs = [
+    {buildInputs:s}
+  ];
+}}
+""".format(**substitutions))
+
+        with open(self.name + '-env.nix', 'w') as handle:
+            handle.write(output + """\
+}};
+in with dependencies; buildEnv {{
+  name = "{name:s}";
+  paths = [
+    {paths:s}
+    (python.buildEnv.override {{
+      ignoreCollisions = true;
+      extraLibs = [
+        {extraLibs:s}
+      ];
+    }})
+  ];
+}}
+""".format(**substitutions))
+
+        for req in requirements:
+            filename = self.name + '-{0:s}.nix'.format(normalize(req))
+            with open(filename, 'w') as handle:
+                handle.write(output + """\
+}};
+in with dependencies; {key:s}
+""".format(key=prefix(normalize(req))))
+
+        return ()
+
+    update = install
