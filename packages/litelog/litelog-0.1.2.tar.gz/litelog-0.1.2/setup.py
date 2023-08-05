6962c39aa8b9280c8de32bc7cc1464397ea25853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:40 +0000

    Add package litelog/0.1.2/litelog-0.1.2.tar.gz

---
+++ packages/litelog/litelog-0.1.2.tar.gz/litelog-0.1.2/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='litelog',
+    version='0.1.2',
+    author='Matthew Cotton',
+    author_email='matt@thecottons.com',
+    packages=[
+        'litelog',
+    ],
+    # scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
+    url='http://pypi.python.org/pypi/litelog/',
+    license='LICENSE.txt',
+    description='Simplified, robust, selective, recursive logging utility for Python.',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+)
