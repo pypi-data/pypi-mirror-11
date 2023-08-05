143bbc2a7069dc8bb59a42efb1baa74d9e5cc820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:44 +0000

    Add package ljm/1.3.0/ljm-1.3.0.zip

---
+++ packages/ljm/ljm-1.3.0.zip/ljm-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name         = 'ljm',
+	version	     = '1.3.0',
+	py_modules   = ['ljm_nester'],
+	author       = 'ljm',
+	author_email = '173750126@qq.com',
+	url          = 'www.ljm.com',
+	description  = 'head first test',
+	)
