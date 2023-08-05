6689cfa1611e25c3da4c4c1e1f0a15cb5d09c18d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:45 +0000

    Add package pyfsm/0.4.2/pyfsm-0.4.2.tar.gz

---
+++ packages/pyfsm/pyfsm-0.4.2.tar.gz/pyfsm-0.4.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'pyfsm',
+    packages = ['pyfsm'], # this must be the same as the name above
+    version = '0.4.2',
+    description = 'A library to make Finite States Machines',
+    author = 'Bernardo Botella',
+    author_email = 'contacto@bernardobotella.com',
+    url = 'https://github.com/bbotella/pyfsm', # use the URL to the github repo
+    download_url = 'https://github.com/bbotella/pyfsm/tarball/0.1', # I'll explain this in a second
+    keywords = ['finite state machine', 'fsm'], # arbitrary keywords
+    classifiers = [],
+)
