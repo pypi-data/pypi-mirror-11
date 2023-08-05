235891fcbd7011852a0da1192985e1eacc49c034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:20 +0000

    Add package buttercms-django/0.2.8/buttercms_django-0.2.8-py2.py3-none-any.whl

---
+++ packages/buttercms-django/buttercms_django-0.2.8-py2.py3-none-any.whl/buttercms-django/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, url
+
+from .feeds import LatestPostFeed
+from .views import BlogHome, BlogPost, AuthorPage, CategoryPage
+
+urlpatterns = [
+    url(r'^$', BlogHome.as_view(), name='blog'),
+    url(r'^rss/$', LatestPostFeed(), name='blog_rss'),
+    url(r'^page/(?P<page>\d+)$', BlogHome.as_view(), name='archive'),
+    url(r'^author/(?P<author_slug>.*)$', AuthorPage.as_view(), name='blog_author'),
+    url(r'^category/(?P<category_slug>.*)$', CategoryPage.as_view(), name='blog_category'),
+    # This must appear last since it's a catch all
+    url(r'^(?P<slug>.*)$', BlogPost.as_view(), name='blog_post'),
+]
