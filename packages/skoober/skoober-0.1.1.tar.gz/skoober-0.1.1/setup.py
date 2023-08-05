009fa3f056e69cb53761d290648b778f777f73df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:42 +0000

    Add package skoober/0.1.1/skoober-0.1.1.tar.gz

---
+++ packages/skoober/skoober-0.1.1.tar.gz/skoober-0.1.1/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from os.path import dirname, join
+from setuptools import setup
+
+with open(join(dirname(__file__), 'requirements.txt'), 'rb') as f:
+    requirements = f.read().splitlines()
+
+setup(name='skoober',
+      version='0.1.1',
+      license='MIT',
+      description='Extract user\'s data from Skoob.com.br',
+      author='Guido Luz Percú',
+      author_email='guidopercu@gmail.com',
+      url='http://github.com/GuidoBR/skoober',
+      platforms=['Any'],
+      py_modules=['skoober'],
+      install_requires=requirements,
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          ],
+      )
