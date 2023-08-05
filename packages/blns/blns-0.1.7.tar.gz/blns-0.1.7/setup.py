bb3ccd77f32b9e7b4ac899c880cccc82375ac624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:50 +0000

    Add package blns/0.1.7/blns-0.1.7.tar.gz

---
+++ packages/blns/blns-0.1.7.tar.gz/blns-0.1.7/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='blns',
+    version='0.1.7',
+    url='https://github.com/danggrianto/big-list-of-naughty-strings',
+    license='MIT',
+    author='Daniel Anggrianto',
+    author_email='daniel@anggrianto.com',
+    description='Big List of Naughty String. Forked from https://github.com/minimaxir/big-list-of-naughty-strings',
+    keywords='Big List of Naughty String',
+    packages=['blns'],
+    platforms='any',
+)
