84d2c128c6cda290a219fa99fe58efaf5418efa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:18 +0000

    Add package zschema/0.0.20/zschema-0.0.20.tar.gz

---
+++ packages/zschema/zschema-0.0.20.tar.gz/zschema-0.0.20/zschema/__init__.py
@@ -0,0 +1,23 @@
+from zschema.keys import Port, DataValidationException
+from zschema.leaves import (
+    DateTime,
+    AnalyzedString,
+    String,
+    Binary,
+    IndexedBinary,
+    Boolean,
+    Double,
+    Float,
+    Long,
+    Short,
+    Byte,
+    Integer,
+    IPv4Address
+)
+from zschema.compounds import ListOf, SubRecord, Record
+from zschema.registry import register_schema, get_schema
+
+
+__version__ = "0.0.18"
+
+
