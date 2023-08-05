039be06ff22b0fa6a16849a57e30ffa412675ea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:12 +0000

    Add package printaol/1.0.5/printaol-1.0.5.zip

---
+++ packages/printaol/printaol-1.0.5.zip/printaol-1.0.5/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'printaol',
+    version = '1.0.5',
+    py_modules = ['printaol'],
+    author = 'chenqiruo',
+    author_email = 'chenqiruo@qq.com',
+    url = 'flansuka.com',
+    description = 'A simple printer of printaol lists',
+    )
