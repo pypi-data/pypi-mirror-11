76434d4fee7e39bef8b5a9dc7ad01eb27f5ededa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:52 +0000

    Add package PuzzleSolver/1.0.0/PuzzleSolver-1.0.0.zip

---
+++ packages/PuzzleSolver/PuzzleSolver-1.0.0.zip/PuzzleSolver-1.0.0/setup.py
@@ -0,0 +1,13 @@
+__author__ = 'Fam. Cowe'
+
+from distutils.core import setup
+
+setup(
+    name            = 'PuzzleSolver',
+    version         = '1.0.0',
+    py_modules      = ['PuzzleSolver'],
+    author          = 'Fam. Cowe',
+    author_email    = 'johancowe@hotmail.com',
+    url             = 'http://www.jimellys.be',
+    description     = 'Puzzle Solving Engine for all your needs.'
+    )
