7e89d11f9d2f38628fa6ac48c9b1a519f2ad3c9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:20 +0000

    Add package sandorm/1.0.1/sandorm-1.0.1.tar.gz

---
+++ packages/sandorm/sandorm-1.0.1.tar.gz/sandorm-1.0.1/sandorm.py
@@ -0,0 +1,12 @@
+
+
+"""This module offers a function to know something about sandorm 
+"""
+
+def whoissandorm():
+  """ Here you get to know the secret.
+  """
+  print("Sandorm is a worm. It's the nickname for Chris in Berlin")
+  
+
+
---
+++ packages/sandorm/sandorm-1.0.1.tar.gz/sandorm-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+  name = 'sandorm',
+  version = '1.0.1',
+  py_modules = ['sandorm'],
+  author = 'christian',
+  author_email = 'hoefig@harung.de',
+  url = 'http://www.harung.de',
+  description = 'print sandorm information'
+)
