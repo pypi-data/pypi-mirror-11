e94902700457ac2d6319f65dab6138f57f3b02d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:37 +0000

    Add package dmm-search/0.2.1/dmm-search-0.2.1.tar.gz

---
+++ packages/dmm-search/dmm-search-0.2.1.tar.gz/dmm-search-0.2.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dmm-search',
+    version='0.2.1',
+    description="DMM search API for Python",
+    py_modules=['dmm'],
+    package_dir={'': 'src'},
+    keywords='dmm, search',
+    author='otknoy',
+    author_email='otknoy@gmail.com',
+    url='https://github.com/otknoy/dmm_search',
+)
