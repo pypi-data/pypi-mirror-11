2fa28d862550dca263fc33b3ac104332a11f77a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:50 +0000

    Add package ajcli/0.1.4/ajcli-0.1.4.tar.gz

---
+++ packages/ajcli/ajcli-0.1.4.tar.gz/ajcli-0.1.4/jira/factory.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+.. module:: TODO
+   :platform: Unix
+   :synopsis: TODO.
+
+.. moduleauthor:: Aljosha Friemann <aljosha.friemann@gmail.com>
+
+"""
+
+class IssueFactory(object):
+
+    """Docstring for IssueFactory. """
+
+    def __init__(self):
+        """TODO: to be defined1. """
+        pass
+
+    def from_json(self, arg):
+        """TODO: Docstring for from_json.
+
+        :arg: TODO
+        :returns: TODO
+
+        """
+        pass
+
+class WorklogFactory(object):
+
+    """Docstring for WorklogFactory. """
+
+    def __init__(self):
+        """TODO: to be defined1. """
+        pass
+
+    def from_json(self, arg):
+        """TODO: Docstring for from_json.
+
+        :arg: TODO
+        :returns: TODO
+
+        """
+        pass
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 fenc=utf-8
