75e53e6ea43f49c950d2bb12111f8da66fe872f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:20 +0000

    Add package cubicweb-signedrequest/0.2.1/cubicweb-signedrequest-0.2.1.tar.gz

---
+++ packages/cubicweb-signedrequest/cubicweb-signedrequest-0.2.1.tar.gz/cubicweb-signedrequest-0.2.1/test/unittest_security.py
@@ -0,0 +1,107 @@
+from cubicweb import Unauthorized
+from cubicweb.devtools.testlib import CubicWebTC
+
+
+class SecurityTC(CubicWebTC):
+
+    def setup_database(self):
+        with self.admin_access.client_cnx() as cnx:
+            self.toto_eid = self.create_user(cnx, 'toto').eid
+
+    def test_other_user_no_token_read(self):
+        with self.admin_access.client_cnx() as cnx:
+            self.create_user(cnx, 'babar')
+
+        with self.new_access('toto').client_cnx() as cnx:
+            cnx.create_entity('AuthToken', id=u'122')
+            cnx.commit()
+            rset = cnx.execute('Any T WHERE AT token T, AT token_for_user U, U login %(login)s',
+                               {'login': 'toto'})
+            self.assertTrue(rset)
+
+        with self.new_access('babar').client_cnx() as cnx:
+            rset = cnx.execute('Any T WHERE AT token T, AT token_for_user U, U login %(login)s',
+                               {'login': 'toto'})
+            self.assertFalse(rset)
+
+    def test_user_token_add(self):
+        with self.new_access('toto').client_cnx() as cnx:
+            tokeneid = cnx.create_entity('AuthToken', id=u'122').eid
+            cnx.commit()
+            with self.assertRaises(Unauthorized):
+                cnx.create_entity('AuthToken', id=u'foo', token=u'forbidden',
+                                  token_for_user=self.toto_eid)
+                cnx.commit()
+
+        with self.admin_access.repo_cnx() as cnx:
+            token = cnx.entity_from_eid(tokeneid)
+            self.assertEqual(128, len(token.token))
+
+    def test_user_token_modify(self):
+        with self.new_access('toto').client_cnx() as cnx:
+            token = cnx.create_entity('AuthToken', id=u'122')
+            cnx.commit()
+            cnx.execute('SET AT enabled True WHERE AT eid %(e)s', {'e':token.eid})
+            cnx.commit()
+            with self.assertRaises(Unauthorized):
+                cnx.execute('SET AT token "babar" WHERE AT eid %(e)s', {'e':token.eid})
+                cnx.commit()
+
+    def test_user_token_delete(self):
+        with self.new_access('toto').client_cnx() as cnx:
+            token = cnx.create_entity('AuthToken')
+            cnx.commit()
+            cnx.execute('DELETE AuthToken T WHERE T eid %(e)s', dict(e=token.eid))
+            cnx.commit()
+
+    def test_manager_do_enabled_modify(self):
+        with self.new_access('toto').client_cnx() as cnx:
+            token = cnx.create_entity('AuthToken', id=u'122')
+            cnx.commit()
+
+        with self.admin_access.repo_cnx() as cnx:
+            token = cnx.entity_from_eid(token.eid)
+            with self.assertRaises(Unauthorized):
+                token.cw_set(enabled=True)
+                cnx.commit()
+
+    def test_manager_no_token_modify(self):
+        with self.admin_access.client_cnx() as cnx:
+            token = cnx.create_entity('AuthToken', id=u'122')
+            cnx.commit()
+            with self.assertRaises(Unauthorized):
+                token.cw_set(token=u'babar')
+                cnx.commit()
+
+    def test_token_for_other_user_permissions(self):
+        with self.admin_access.client_cnx() as cnx:
+            babar_eid = self.create_user(cnx, 'babar').eid
+
+        with self.new_access('toto').client_cnx() as cnx:
+            with self.assertRaises(Unauthorized):
+                # can't create token for someone else.
+                cnx.create_entity('AuthToken', id=u'122',
+                                  token_for_user=babar_eid)
+                cnx.commit()
+            cnx.rollback()
+            cnx.create_entity('AuthToken', id=u'122',
+                              token_for_user=self.toto_eid)
+            cnx.commit()
+
+        with self.new_access('toto').client_cnx() as cnx:
+            with self.assertRaises(Unauthorized):
+                # can't change relation on someone else token.
+                cnx.execute('SET T token_for_user B WHERE T token_for_user U,'
+                            ' U login "toto", B login "babar"')
+                cnx.commit()
+
+        with self.admin_access.client_cnx() as cnx:
+            # admin can change relation.
+            cnx.execute('SET T token_for_user B WHERE T token_for_user U,'
+                        ' U login "toto", B login "babar"')
+            cnx.commit()
+
+
+if __name__ == "__main__":
+    from logilab.common.testlib import unittest_main
+    unittest_main()
