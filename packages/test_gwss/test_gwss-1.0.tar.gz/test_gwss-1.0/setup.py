5b682d8fe88ffd24898d0ff16055e744be8650ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:30 +0000

    Add package test_gwss/1.0/test_gwss-1.0.tar.gz

---
+++ packages/test_gwss/test_gwss-1.0.tar.gz/test_gwss-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='test_gwss',
+      version='1.0',
+      description='Garena gas stats system',
+      url='http://github.com/storborg/funniest',
+      author='Tran Minh Tri',
+      author_email='tranmt@garena.com',
+      license='MIT',
+      packages=['test_gwss'],
+      zip_safe=False)
