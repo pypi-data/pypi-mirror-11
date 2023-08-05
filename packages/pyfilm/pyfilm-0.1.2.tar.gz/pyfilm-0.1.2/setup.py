b290941e644410337e75c1754ee995a501564de1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:18 +0000

    Add package pyfilm/0.1.2/pyfilm-0.1.2.tar.gz

---
+++ packages/pyfilm/pyfilm-0.1.2.tar.gz/pyfilm-0.1.2/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup
+
+setup(
+    name = "pyfilm",
+    version = "0.1.2",
+    author = "Ferdinand van Wyk",
+    author_email = 'ferdinandvwyk@gmail.com',
+    description = "PyFilm: easily create films of 1D and 2D NumPy arrays.",
+    long_description = "PyFilm is a package which enables the quick creation of animations of NumPy arrays. It does this by writing each time step as a separate image and using either ffmpeg or avconv to stitch the images together into an animation. The package is designed to be very easy to use, and allow animations to be created with a single line.",
+    license = "GNU",
+    keywords = ['film', 'animation', 'numpy', 'ffmpeg', 'avconv'],
+    packages=['pyfilm'],
+    url = 'https://github.com/ferdinandvwyk/pyfilm.git',
+    setup_requires =['numpy>1.6'],
+    install_requires =[
+        'matplotlib>1.4',
+        'py-cpuinfo>0.1',
+        'Pillow>2.8',
+        'clint>0.4',
+    ],
+    classifiers=[
+    "Development Status :: 3 - Alpha"
+    ],
+)
