b2bb6477d980e796b09cd51ee172fcb85c45d23a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:59 +0000

    Add package geocoder/1.5.0/geocoder-1.5.0.zip

---
+++ packages/geocoder/geocoder-1.5.0.zip/geocoder-1.5.0/HISTORY.rst
@@ -0,0 +1,15 @@
+.. :changelog:
+
+Release History
+---------------
+
+1.5.0 (2015-07-30)
+++++++++++++++++++
+
+Converted all core Markdown documentation to RST
+
+**Bugfixes**
+
+- Updated W3W's url to HTTPS
+- Updated OSM's url to HTTPS
+- Added Google Road Long/Short attributes
---
+++ packages/geocoder/geocoder-1.5.0.zip/geocoder-1.5.0/setup.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+# coding: utf8
+import os
+import re
+import sys
+
+from codecs import open
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist bdist_wheel upload')
+    sys.exit()
+
+requires = ['requests', 'ratelim', 'click', 'six']
+
+version = ''
+with open('geocoder/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('Cannot find version information')
+
+with open('README.rst', 'r', 'utf-8') as f:
+    readme = f.read()
+
+with open('HISTORY.rst', 'r', 'utf-8') as f:
+    history = f.read()
+
+setup(
+    name='geocoder',
+    version=version,
+    description="Simple and consistent geocoding library.",
+    long_description=readme + '\n\n' + history,
+    author='Denis Carriere',
+    author_email='carriere.denis@gmail.com',
+    url='https://github.com/DenisCarriere/geocoder',
+    download_url='https://github.com/DenisCarriere/geocoder/tarball/master',
+    license=open('LICENSE').read(),
+    entry_points='''
+        [console_scripts]
+        geocode=geocoder.cli:cli
+    ''',
+    packages=['geocoder'],
+    package_data={'': ['LICENSE', 'README.md']},
+    package_dir={'geocoder': 'geocoder'},
+    include_package_data=True,
+    install_requires=requires,
+    zip_safe=False,
+    keywords='geocoder arcgis tomtom opencage google bing here',
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Scientific/Engineering :: GIS',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ),
+)
