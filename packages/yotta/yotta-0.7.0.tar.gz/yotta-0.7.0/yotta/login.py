3416a912718014e08d864fce7f6e7ddaa74c1d5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:59 +0000

    Add package yotta/0.7.0/yotta-0.7.0.tar.gz

---
+++ packages/yotta/yotta-0.7.0.tar.gz/yotta-0.7.0/yotta/login.py
@@ -0,0 +1,26 @@
+# Copyright 2014-2015 ARM Limited
+#
+# Licensed under the Apache License, Version 2.0
+# See LICENSE file for details.
+
+# standard library modules, , ,
+import argparse
+
+# auth, , authenticate users, internal
+from .lib import auth
+# Registry Access, , access modules in the registry, internal
+from .lib import registry_access
+
+def addOptions(parser):
+    # set the registry URL to save an API key for
+    parser.add_argument('--registry', dest='registry', help=argparse.SUPPRESS)
+    # pass the API key for logging into this registry (it will be saved, and
+    # used for future requests to this registry)
+    parser.add_argument('--apikey', '-k', dest='apikey', help=argparse.SUPPRESS)
+
+def execCommand(args, following_args):
+    if args.apikey:
+        registry_access.setAPIKey(args.registry, args.apikey)
+    
+    auth.authorizeUser(args.registry, provider=None, interactive=args.interactive)
+
