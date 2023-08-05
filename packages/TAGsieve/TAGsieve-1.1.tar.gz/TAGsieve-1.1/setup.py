f8c0e07487fa4a7b7e0ba4aaa51524ed5a57f4e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:50 +0000

    Add package TAGsieve/1.1/TAGsieve-1.1.tar.gz

---
+++ packages/TAGsieve/TAGsieve-1.1.tar.gz/TAGsieve-1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+install_requires = [
+    'lxml',
+    'BeautifulSoup4',
+    'bleach',
+]
+
+setup(
+    name='TAGsieve',
+    version='1.1',
+    description='A small HTML/XML stripper GUI application.',
+    long_description=open('README.rst').read(),
+    author='Tom De Keyser',
+    author_email = 'tomdekeyser7@gmail.com',
+    license='GPLv3',
+    url='http://github.com/willibrord/TAGsieve',
+    packages=['TAGsieve'],
+    package_data={'': ['README.rst']},
+    install_requires=install_requires,
+)
