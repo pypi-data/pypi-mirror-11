f03e8fabf28b2a650f9e649a02399890b851e616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:09 +0000

    Add package zipfile2/0.0.9/zipfile2-0.0.9-py2.py3-none-any.whl

---
+++ packages/zipfile2/zipfile2-0.0.9-py2.py3-none-any.whl/zipfile2/_version.py
@@ -0,0 +1,8 @@
+# THIS FILE IS GENERATED FROM ZIPFILE2 SETUP.PY
+version = '0.0.9'
+full_version = '0.0.9'
+git_revision = '58c43e765e963d985aa3ddc4c3c0f1e88f44c03d'
+is_released = True
+
+if not is_released:
+    version = full_version
