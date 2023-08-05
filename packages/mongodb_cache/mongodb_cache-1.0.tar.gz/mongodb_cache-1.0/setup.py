151fb495a14ae3621e173b1fcf3478125e4b734f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:41 +0000

    Add package mongodb_cache/1.0/mongodb_cache-1.0.tar.gz

---
+++ packages/mongodb_cache/mongodb_cache-1.0.tar.gz/mongodb_cache-1.0/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='mongodb_cache',
+    version='1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    author='Anton Chaporgin',
+    author_email='chapson@yandex-team.ru',
+)
