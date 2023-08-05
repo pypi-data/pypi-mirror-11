38c5ee012c2198b7e007abec23d426ebe3ed71ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:46 +0000

    Add package certidude/0.1.8/certidude-0.1.8.tar.gz

---
+++ packages/certidude/certidude-0.1.8.tar.gz/certidude-0.1.8/certidude/wsgi.py
@@ -0,0 +1,32 @@
+
+import os
+import falcon
+from certidude.wrappers import CertificateAuthorityConfig
+from certidude.api import CertificateAuthorityResource, \
+    RequestDetailResource, RequestListResource, \
+    SignedCertificateDetailResource, SignedCertificateListResource, \
+    RevocationListResource, IndexResource, ApplicationConfigurationResource, \
+    CertificateStatusResource
+
+# TODO: deduplicate routing code
+# TODO: set up /run/certidude/api paths and permissions
+
+if not os.path.exists("/run/certidude/api"):
+    os.makedirs("/run/certidude/api")
+
+config = CertificateAuthorityConfig("/etc/ssl/openssl.cnf")
+
+assert os.getenv("CERTIDUDE_EVENT_SUBSCRIBE"), "Please set CERTIDUDE_EVENT_SUBSCRIBE to your web server's subscription URL"
+assert os.getenv("CERTIDUDE_EVENT_PUBLISH"), "Please set CERTIDUDE_EVENT_PUBLISH to your web server's publishing URL"
+
+app = falcon.API()
+app.add_route("/api/{ca}/ocsp/", CertificateStatusResource(config))
+app.add_route("/api/{ca}/signed/{cn}/openvpn", ApplicationConfigurationResource(config))
+app.add_route("/api/{ca}/certificate/", CertificateAuthorityResource(config))
+app.add_route("/api/{ca}/revoked/", RevocationListResource(config))
+app.add_route("/api/{ca}/signed/{cn}/", SignedCertificateDetailResource(config))
+app.add_route("/api/{ca}/signed/", SignedCertificateListResource(config))
+app.add_route("/api/{ca}/request/{cn}/", RequestDetailResource(config))
+app.add_route("/api/{ca}/request/", RequestListResource(config))
+app.add_route("/api/{ca}/", IndexResource(config))
+
