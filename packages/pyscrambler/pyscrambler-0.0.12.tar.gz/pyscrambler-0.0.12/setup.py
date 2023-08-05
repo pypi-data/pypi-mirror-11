75f50a23726c11143d01dcc6952c0812aa2df9f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:31 +0000

    Add package pyscrambler/0.0.12/pyscrambler-0.0.12.tar.gz

---
+++ packages/pyscrambler/pyscrambler-0.0.12.tar.gz/pyscrambler-0.0.12/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from setuptools import setup
+
+setup(
+    name='pyscrambler',
+    version='0.0.12',
+    description='Concept data scrambler using permutations as a basis',
+    url='https://github.com/saxbophone/pyscrambler',
+    author='Joshua Saxby',
+    author_email='joshua.a.saxby@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Security :: Cryptography'
+    ],
+    packages=['pyscrambler', 'pyscrambler.scramblers'],
+    install_requires=['bitstring==3.1.3',
+                      'oset==0.1.3'],
+    extras_require={
+        'test': ['coverage==3.7.1'],
+    },
+    zip_safe=False
+)
