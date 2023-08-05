afb32f50a5afd0635906452bb8896a163cc84cd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:08 +0000

    Add package prompt-toolkit/0.45/prompt_toolkit-0.45-py2-none-any.whl

---
+++ packages/prompt-toolkit/prompt_toolkit-0.45-py2-none-any.whl/prompt_toolkit/enums.py
@@ -0,0 +1,16 @@
+from __future__ import unicode_literals
+
+
+class IncrementalSearchDirection:
+    FORWARD = 'forward'
+    BACKWARD = 'backward'
+
+
+#: Name of the search buffer.
+SEARCH_BUFFER = 'search'
+
+#: Name of the default buffer.
+DEFAULT_BUFFER = 'default'
+
+#: Name of the system buffer.
+SYSTEM_BUFFER = 'system'
