645ada9a6875ab0e3dcb36f53a9293621d18db25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:58 +0000

    Add package lasio/0.7.7/lasio-0.7.7-py2.py3-none-any.whl

---
+++ packages/lasio/lasio-0.7.7-py2.py3-none-any.whl/lasio/test_enhancements.py
@@ -0,0 +1,42 @@
+import fnmatch
+import os
+
+import numpy
+import pytest
+
+from lasio import las, read
+
+
+test_dir = os.path.dirname(__file__)
+
+egfn = lambda fn: os.path.join(os.path.dirname(__file__), "test_examples", fn)
+stegfn = lambda vers, fn: os.path.join(
+    os.path.dirname(__file__), "test_examples", vers, fn)
+
+
+def test_autodepthindex():
+    m = read(egfn("autodepthindex_M.las"))
+    f = read(egfn("autodepthindex_F.las"))
+    ft = read(egfn("autodepthindex_FT.las"))
+    err = read(egfn("autodepthindex_M_FT.las"))
+
+
+def test_autodepthindex_inconsistent():
+    err = read(egfn("autodepthindex_M_FT.las"))
+    with pytest.raises(las.LASUnknownUnitError):
+        print(err.depth_m)
+
+
+def test_autodepthindex_m():
+    l = read(egfn("autodepthindex_M.las"))
+    assert (l.depth_ft[-1] * 0.3048 == l.index[-1])
+
+
+def test_autodepthindex_f():
+    l = read(egfn("autodepthindex_F.las"))
+    assert (l.depth_m[-1] / 0.3048 == l.index[-1])
+
+
+def test_autodepthindex_ft():
+    l = read(egfn("autodepthindex_FT.las"))
+    assert (l.depth_m[-1] / 0.3048 == l.index[-1])
