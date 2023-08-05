3e17067db98e46a1fbf0c5e4376881cad79502f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:29 +0000

    Add package djangocms-blogit/0.4.0/djangocms-blogit-0.4.0.tar.gz

---
+++ packages/djangocms-blogit/djangocms-blogit-0.4.0.tar.gz/djangocms-blogit-0.4.0/blogit/views.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.core.urlresolvers import reverse
+from django.http import Http404
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.dates import (
+    YearArchiveView, MonthArchiveView, DayArchiveView, DateDetailView)
+from django.utils.translation import ugettext_lazy as _
+
+from blogit import settings as bs
+from blogit.models import Category, Post
+
+
+class ToolbarMixin(object):
+    def get(self, request, *args, **kwargs):
+        response = super(ToolbarMixin, self).get(request, *args, **kwargs)
+
+        if hasattr(self, 'object') and hasattr(request, 'toolbar'):
+            request.toolbar.set_object(self.object)
+            menu = request.toolbar.get_menu('blogit-current-menu')
+            if menu:
+                self.update_menu(menu, self.object)
+
+        return response
+
+    def update_menu(self, menu, obj):
+        pass
+
+
+class PostListMixin(object):
+    model = Post
+    template_name = 'blogit/post_list.html'
+    paginate_by = bs.POSTS_PER_PAGE
+
+    def __init__(self, *args, **kwargs):
+        super(PostListMixin, self).__init__(*args, **kwargs)
+        self.filters = {}
+
+    def get_queryset(self):
+        return self.model.objects.published(self.request, **self.filters)
+
+
+# Category views.
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'blogit/category_list.html'
+
+    def get_queryset(self):
+        return self.model.objects.filter(active=True)
+
+
+class CategoryDetailView(ToolbarMixin, PostListMixin, ListView):
+    template_name = 'blogit/category_detail.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs.update({'category': self.object})
+        return super(CategoryDetailView, self).get_context_data(**kwargs)
+
+    def get(self, request, *args, **kwargs):
+        try:
+            self.object = Category.objects.translated(
+                slug=kwargs.get('slug')).get(active=True)
+            self.filters['category_id'] = self.object.pk
+        except Category.DoesNotExist:
+            raise Http404
+
+        return super(CategoryDetailView, self).get(request, *args, **kwargs)
+
+    def update_menu(self, menu, obj):
+        menu.add_break()
+        url = reverse(
+            'admin:blogit_category_change', args=[self.object.pk])
+        menu.add_modal_item(_('Edit this Category'), url=url)
+        menu.add_break()
+        url = reverse(
+            'admin:blogit_category_delete', args=[self.object.pk])
+        menu.add_modal_item(_('Delete Category'), url=url)
+
+
+class PostDateMixin(object):
+    date_field = 'date_published'
+    year_format = '%Y'
+    month_format = '%m'
+    day_format = '%d'
+    make_object_list = True
+    allow_future = True
+
+
+class ArchiveListMixin(PostDateMixin, PostListMixin):
+    template_name = 'blogit/post_list.html'
+
+
+class PostYearArchiveView(ArchiveListMixin, YearArchiveView):
+    pass
+
+
+class PostMonthArchiveView(ArchiveListMixin, MonthArchiveView):
+    pass
+
+
+class PostDayArchiveView(ArchiveListMixin, DayArchiveView):
+    pass
+
+
+class PostListView(PostListMixin, ListView):
+    pass
+
+
+class PostDetailMixin(ToolbarMixin):
+    model = Post
+    template_name = 'blogit/post_detail.html'
+    slug_field = 'translations__slug'
+
+    def get_queryset(self):
+        return Post.objects.published(self.request)
+
+    def update_menu(self, menu, obj):
+        menu.add_break()
+        url = reverse(
+            'admin:blogit_post_change', args=[self.object.pk])
+        menu.add_modal_item(_('Edit this Post'), url=url)
+        menu.add_break()
+
+        url = reverse(
+            'admin:blogit_post_delete', args=[self.object.pk])
+        menu.add_modal_item(_('Delete Post'), url=url)
+
+
+class PostDetailView(PostDetailMixin, DetailView):
+    pass
+
+
+class PostDateDetailView(PostDateMixin, PostDetailMixin, DateDetailView):
+    pass
---
+++ packages/djangocms-blogit/djangocms-blogit-0.4.0.tar.gz/djangocms-blogit-0.4.0/docs/changelog.rst
@@ -0,0 +1,62 @@
+Changelog
+=========
+
+0.4.0
+    + Add langauge param to Post's get_absolute_url method
+    + Add get_search_data method to Post
+    + Remove django-taggit from requirements
+    + Fix: admin get_image should fail silently if image is missing
+
+    .. attention::
+
+        In version **0.4.0** django-taggit is removed as a requirement so the migrations history must be cleared.
+        To migrate from **0.3.x** to **0.4.x** follow the steps:
+
+            - install version **0.3.10** and run ``python manage.py migrate blogit``
+            - remove blogit from the migration history. SQL example: ``DELETE FROM django_migrations WHERE app="blogit"``
+            - install version **0.4.0** and run ``python manage.py migrate blogit --fake``
+
+
+0.3.10
+    + Remove date_hierarchy from admin
+
+0.3.9
+    + Changes in admin layout
+    + Add settings for sitemap config
+    + Add django-mptt as a requirement
+    + Add actions for changing statuses
+    + Add colors to admin statuses
+    + Add previous and next post as a property
+    + Remove active field, add HIDDEN status instead
+
+0.3.8
+    + Add SEO fields
+    + Add extra feed settings
+    + Add status fields, fix active boolean to not display in feeds and detail
+
+0.3.7
+    + Fix migrations
+
+0.3.6
+    + Fix 0005 migration
+
+0.3.5
+    + Create own simple tag model
+    + Remove category and feed urls settings and translate them automatically
+
+0.3.4
+    + Make active boolean work.
+
+0.3.3
+    + Fix not displaying correct language on detail view.
+
+0.3.2
+    + Migration file dependencies fix
+
+0.3.1
+    + Update migration file
+    + Fixes
+
+0.3.0
+    + Simplified models.
+    + Refactored and not compatible with earlier versions.
