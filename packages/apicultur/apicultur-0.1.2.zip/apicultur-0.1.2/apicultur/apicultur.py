fe32391bd20fa13166d9fb8e0734a1888318403f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:57 +0000

    Add package apicultur/0.1.2/apicultur-0.1.2.zip

---
+++ packages/apicultur/apicultur-0.1.2.zip/apicultur-0.1.2/apicultur/apicultur.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from functools import partial
+
+from .throttle import Throttle, NoThrottle
+from .services import load_services
+
+
+class Apicultur(object):
+    app = None
+    access_token = None
+    version = '1.0.0'
+    base_url = 'http://store.apicultur.com/api'
+    throttle = NoThrottle()
+    _endpoints = {}
+
+    def __init__(self, access_token, app=None, load_services=True):
+        self.app = app
+        self.access_token = access_token
+        if load_services:
+            self.add_services(dirname=os.path.join(os.path.dirname(__file__), 'services'))
+
+    def add_services(self, dirname, clear=False):
+        if clear:
+            self._endpoints.clear()
+            self._endpoints = {}
+
+        endpoints = load_services(dirname, self.version)
+        self._endpoints.update(endpoints)  # TODO: What to do with duplicates?
+
+    def check_token(self):
+        # TODO: Is there a way to check this?
+        return True
+
+    def list_services(self):
+        # List all services available in 'services' package and compatible with this version.
+        for endpoint, obj in self._endpoints.iteritems():
+            print(u"%s:\t .%s(%s)" % (obj.__name__, endpoint, ', '.join(obj.arguments)))
+
+    def set_throttle(self, max_messages=0, every_seconds=None):
+        assert(max_messages >= 0)
+        assert(max_messages==0 or every_seconds > 0)
+        if max_messages == 0:
+            self.throttle = NoThrottle()
+        else:
+            self.throttle = Throttle(n_messages=max_messages, n_seconds=every_seconds)
+
+    def __getattr__(self, item):
+        # We are calling a service: build and return as callable
+        service_class = self._endpoints.get(item, None)
+        if not service_class:
+            raise RuntimeError("API endpoint not available.")
+        service = service_class(self.access_token, self.base_url)
+        cur_service = partial(self.call_service, service=service)
+        setattr(self, item, cur_service)
+        return cur_service
+
+    def call_service(self, service, *args, **kwargs):
+        self.throttle.acquire()
+        return service(*args, **kwargs)
+
