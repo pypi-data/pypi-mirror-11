b867302e3957520dc0db4ebe75437f399f311a3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:58 +0000

    Add package pandoc-fignos/0.3/pandoc-fignos-0.3.tar.gz

---
+++ packages/pandoc-fignos/pandoc-fignos-0.3.tar.gz/pandoc-fignos-0.3/setup.py
@@ -0,0 +1,37 @@
+"""setup.py - install script for pandoc-fignos."""
+
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup
+
+LONG_DESCRIPTION = """\
+pandoc-fignos is a pandoc filter for numbering figures and figure references.
+"""
+
+setup(
+    name='pandoc-fignos',
+    version='0.3',
+
+    author='Thomas J. Duck',
+    author_email='tomduck@tomduck.ca',
+    description='Figure number filter for pandoc',
+    long_description=LONG_DESCRIPTION,
+    license='GPL',
+    keywords='pandoc figure numbers filter',
+    url='https://github.com/tomduck/pandoc-fignos',
+    download_url = 'https://github.com/tomduck/pandoc-fignos/tarball/0.3',
+    
+    install_requires=['pandocfilters', 'pandoc-attributes'],
+
+    py_modules=['pandoc_fignos'],
+    entry_points={'console_scripts':['pandoc-fignos = pandoc_fignos:main']},
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python'
+        ],
+)
