d013343568a7ee4b1e6cf50a95922eb40eb1568b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:52 +0000

    Add package aspose_words_java_for_python/1.0.0/aspose_words_java_for_python-1.0.0.tar.gz

---
+++ packages/aspose_words_java_for_python/aspose_words_java_for_python-1.0.0.tar.gz/aspose_words_java_for_python-1.0.0/setup.py
@@ -0,0 +1,19 @@
+__author__ = 'fahadadeel'
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'aspose_words_java_for_python',
+    packages = find_packages(),
+    version = '1.0.0',
+    description = 'Aspose.Words Java for Python',
+    author='Fahad Adeel',
+    author_email='fahadadeel@gmail.com',
+    url='https://github.com/asposewords/Aspose_Words_Java/tree/master/Plugins/Aspose_Words_Java_for_Python',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ]
+)
