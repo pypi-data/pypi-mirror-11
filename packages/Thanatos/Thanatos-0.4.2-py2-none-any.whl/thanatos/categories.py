2a8a8344edf6e21803a527b1d94d0ed5c3c920be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:10 +0000

    Add package Thanatos/0.4.2/Thanatos-0.4.2-py2-none-any.whl

---
+++ packages/Thanatos/Thanatos-0.4.2-py2-none-any.whl/thanatos/categories.py
@@ -0,0 +1,49 @@
+
+
+# The Geography category and it's sub-categories
+geography = {
+    'name': 'Geography',
+    'description': 'Questions regarding the solar systems and regions of New Eden.',
+}
+
+geography_regions = {
+    'name': 'Regions',
+    'description': 'Questions specifically about the regions of New Eden.',
+}
+
+geography_miscellaneous = {
+    'name': 'Miscellaneous',
+    'description': 'Miscellaneous questions about the geography of New Eden.',
+}
+
+# The Inventory category and its sub-categories
+inventory = {
+    'name': 'Inventory',
+    'description': 'Questions regarding the ships, modules, and other items.',
+}
+
+inventory_slots = {
+    'name': 'Slots',
+    'description': 'Questions about the number of different slots ships have.',
+}
+
+inventory_ship_id = {
+    'name': 'ShipID',
+    'description': 'Questions asking to identify what ship is being shown.',
+}
+
+# The Inventory category and its sub-categories
+lore = {
+    'name': 'Lore',
+    'description': 'Questions about the lore of New Eden.',
+}
+
+lore_npc = {
+    'name': 'NPCLore',
+    'description': 'Questions specifically about the NPC lore of New Eden.',
+}
+
+lore_capsuleer = {
+    'name': 'CapsuleerLore',
+    'description': 'Questions specifically about the capsuleer lore of New Eden.',
+}
