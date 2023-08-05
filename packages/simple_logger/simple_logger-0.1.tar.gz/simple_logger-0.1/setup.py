66da8953f06a05dee8fbe52540fd6932a93b0f8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:56 +0000

    Add package simple_logger/0.1/simple_logger-0.1.tar.gz

---
+++ packages/simple_logger/simple_logger-0.1.tar.gz/simple_logger-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='simple_logger',
+      version='0.1',
+      description='',
+      url='https://github.com/kevinslin',
+      author='Kevin S Lin',
+      author_email='kevinslin8@gmail.com',
+      license='MIT',
+      packages=['simple_logger'],
+      zip_safe=False)
