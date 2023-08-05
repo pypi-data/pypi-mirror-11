5550b8a494f64a8ab5d4d0041c791441e84fc767
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:59 +0000

    Add package Flask-Diamond/0.2.11/Flask-Diamond-0.2.11.tar.gz

---
+++ packages/Flask-Diamond/Flask-Diamond-0.2.11.tar.gz/Flask-Diamond-0.2.11/setup.py
@@ -0,0 +1,64 @@
+import re
+import os
+from setuptools import setup
+
+
+# mimicking flask-admin setup.py
+# https://github.com/flask-admin/flask-admin/blob/master/setup.py
+def fpath(name):
+    return os.path.join(os.path.dirname(__file__), name)
+
+
+def read(fname):
+    return open(fpath(fname)).read()
+
+
+file_text = read(fpath('flask_diamond/__init__.py'))
+
+
+def grep(attrname):
+    pattern = r"{0}\W*=\W*'([^']+)'".format(attrname)
+    strval, = re.findall(pattern, file_text)
+    return strval
+
+
+def get_requirements(suffix=''):
+    with open('requirements.txt') as f:
+        rv = f.read().splitlines()
+    return rv
+
+
+def get_long_description():
+    with open('Readme.rst') as f:
+        rv = f.read()
+    return rv
+
+
+setup(
+    version=grep('__version__'),
+    name='Flask-Diamond',
+    description="""\
+        Flask-Diamond provides a path that can guide your thought and development.
+        Flask-Diamond is the road that leads to other ideas.
+        """,
+    packages=[
+        "flask_diamond",
+        "flask_diamond.models",
+        "flask_diamond.views",
+        "flask_diamond.views.diamond",
+        "flask_diamond.utils",
+    ],
+    scripts=[
+        "bin/diamond-scaffold.sh",
+    ],
+    long_description=get_long_description(),
+    classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    include_package_data=True,
+    keywords='',
+    author=grep('__author__'),
+    author_email=grep('__email__'),
+    url='http://flask-diamond.readthedocs.org',
+    install_requires=get_requirements(),
+    license='MIT',
+    zip_safe=False,
+)
