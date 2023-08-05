4b30b0e4334034e8e92da1eaaf6bea2073877b5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:10 +0000

    Add package onegov.form/0.6.6/onegov.form-0.6.6.tar.gz

---
+++ packages/onegov.form/onegov.form-0.6.6.tar.gz/onegov.form-0.6.6/onegov/form/__init__.py
@@ -0,0 +1,32 @@
+import logging
+log = logging.getLogger('onegov.form')  # noqa
+log.addHandler(logging.NullHandler())  # noqa
+
+from onegov.form.collection import (
+    FormCollection,
+    FormSubmissionCollection,
+    FormDefinitionCollection
+)
+from onegov.form.core import Form, with_options
+from onegov.form.display import render_field
+from onegov.form.models import (
+    FormDefinition,
+    FormSubmission,
+    FormSubmissionFile,
+    PendingFormSubmission,
+    CompleteFormSubmission
+)
+
+__all__ = [
+    'Form',
+    'FormCollection',
+    'FormDefinitionCollection',
+    'FormSubmissionCollection',
+    'FormDefinition',
+    'FormSubmission',
+    'FormSubmissionFile',
+    'PendingFormSubmission',
+    'CompleteFormSubmission',
+    'render_field',
+    'with_options'
+]
