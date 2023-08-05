f40209060b78cd54de3e240b947d55389cd832bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:27 +0000

    Add package pywhatsapp/0.0.5/pywhatsapp-0.0.5.tar.gz

---
+++ packages/pywhatsapp/pywhatsapp-0.0.5.tar.gz/pywhatsapp-0.0.5/send.py
@@ -0,0 +1,9 @@
+from whatsapp import Client
+import logging
+logging.basicConfig(level=logging.INFO)
+
+to = '31641370305'
+
+client = Client(login='31615161376', password='AgYZCNGgWYxhoeeH/Ngi3Pd0vr4=')
+client.send_message(to, 'Monkey brain111')
+client.send_media(to, path='/Users/paultax/Desktop/logo.jpg')
