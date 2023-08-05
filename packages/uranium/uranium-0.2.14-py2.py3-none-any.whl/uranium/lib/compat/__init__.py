32b909d3380de617d82f30074781632eddc4b86b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:24 +0000

    Add package uranium/0.2.14/uranium-0.2.14-py2.py3-none-any.whl

---
+++ packages/uranium/uranium-0.2.14-py2.py3-none-any.whl/uranium/lib/compat/__init__.py
@@ -0,0 +1,34 @@
+"""
+A bunch of compat mappings.
+
+This helps catalog python 2 and 3 differences,
+so they can be removed if at some point in 2050.
+"""
+
+try:
+    from UserDict import DictMixin
+except ImportError:
+    from collections import MutableMapping as DictMixin
+
+
+try:
+    from UserDict import IterableUserDict as UserDict
+except ImportError:
+    from collections import UserDict
+
+
+try:
+    from importlib import import_module
+except:
+    from .c_importlib import import_module
+
+try:
+    from subprocess import check_output
+except:
+    from .check_output import check_output
+
+
+try:
+    from urllib.parse import urlparse
+except:
+    from urlparse import urlparse
