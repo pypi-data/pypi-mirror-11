d8ad9d32e9e4c6fd01e7d325e5b34ad3b0fc6449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:37 +0000

    Add package pymysqlblinker/1.2/pymysqlblinker-1.2.tar.gz

---
+++ packages/pymysqlblinker/pymysqlblinker-1.2.tar.gz/pymysqlblinker-1.2/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__version__ = '1.2'
+
+from setuptools import setup
+
+# requirements
+install_requires = [
+    "mysql-replication>=0.7",
+    "blinker>=1.4",
+    'six',
+]
+
+dev_requires = ["pytest",
+                "tox",
+                ] + install_requires
+
+setup(name="pymysqlblinker",
+      version=__version__,
+      description="mysql binlog to blinker signal",
+      keywords="event replication mysql signal",
+      author="Hoc .T Do",
+      author_email="hoc3010@gmail.com",
+      packages=['pymysqlblinker', ],
+      url="https://github.com/tarzanjw/python-mysql-replication-blinker",
+      license="MIT",
+      zip_safe=False,
+      long_description=open("README.rst").read(),
+      install_requires=install_requires,
+      extras_require={
+          "dev": dev_requires,
+      },
+      classifiers=[
+          "Topic :: Software Development",
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.4",
+      ]
+      )
