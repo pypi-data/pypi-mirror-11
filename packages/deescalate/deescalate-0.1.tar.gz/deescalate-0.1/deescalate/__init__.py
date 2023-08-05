0ba6c0b5e341df13789a6e5f0de56a2eea22805e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:18 +0000

    Add package deescalate/0.1/deescalate-0.1.tar.gz

---
+++ packages/deescalate/deescalate-0.1.tar.gz/deescalate-0.1/deescalate/__init__.py
@@ -0,0 +1,7 @@
+__author__ = 'stef'
+
+
+from .main import lockdown_account
+from .main import get_securebits, set_noroot, set_keep_caps, set_no_setuid_fixup, set_no_new_privs
+from .main import permitted, inheritable, effective, bounding_set, CapabilitySet, BoundingSet
+from .constants import C
