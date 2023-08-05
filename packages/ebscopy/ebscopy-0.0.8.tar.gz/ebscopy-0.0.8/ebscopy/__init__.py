2c8c7bf79dab5a059677e59ce4a7dab6aeb199c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:45 +0000

    Add package ebscopy/0.0.8/ebscopy-0.0.8.tar.gz

---
+++ packages/ebscopy/ebscopy-0.0.8.tar.gz/ebscopy-0.0.8/ebscopy/__init__.py
@@ -0,0 +1,28 @@
+# ebscopy __init__
+
+import os
+import logging 
+from ebscopy import *
+#from pkg_resources import get_distribution
+
+log_levels									= {
+												'DEBUG':	logging.DEBUG,
+												'INFO':		logging.INFO,
+												'WARNING':	logging.WARNING,
+												'ERROR':	logging.ERROR,
+												'CRITICAL':	logging.CRITICAL,
+												}
+
+if os.environ.get('EDS_LOG_LEVEL') in log_levels.keys():
+	log_level								= log_levels[os.environ.get('EDS_LOG_LEVEL')]
+else:
+	log_level								= logging.NOTSET
+
+logging.basicConfig(
+	filename='/tmp/ebscopy-%s.log' % (os.getpid()),
+	level=log_level,
+	format='%(asctime)s %(levelname)s %(module)s.%(funcName)s: %(message)s'
+)
+
+
+#EOF
