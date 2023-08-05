0a64fd3251919b0e0ba6cbc86bfef2375cc8396c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:47 +0000

    Add package rabbit-qurator/v1.0.4/rabbit-qurator-v1.0.4.tar.gz

---
+++ packages/rabbit-qurator/rabbit-qurator-v1.0.4.tar.gz/rabbit-qurator-v1.0.4/qurator/worker.py
@@ -0,0 +1,41 @@
+from kombu.mixins import ConsumerMixin
+from . import get_logger
+
+logger = get_logger(__name__)
+
+
+class Worker(ConsumerMixin):
+
+    """Manage server side of RPC connection.
+
+    This code is based on the examples on the Kombu website.
+    """
+
+    def __init__(self, connection, iwmn_consumer):
+        """Worker(connection)
+
+        :connection: Connection object
+        """
+        logger.debug("Called constructor.")
+        self.connection = connection
+        self.iwmn_consumer = iwmn_consumer
+
+    def get_consumers(self, Consumer, channel):
+        """Get a set of consumers.
+
+        :Consumer: Consumer object
+        :channel: a channel
+        :returns: array of Consumer objects
+
+        """
+        consumer_set = []
+        for i in self.iwmn_consumer.queues.keys():
+            queues = self.iwmn_consumer.queues[i]
+            callbacks = self.iwmn_consumer.callbacks[i]
+            logger.info("Queues: {!r}".format(queues))
+            c = Consumer(queues, callbacks=callbacks)
+            consumer_set.append(c)
+            logger.info("Added consumer: {!r}".format(c))
+
+        logger.info("Called get_consumers with {!r}".format(consumer_set))
+        return consumer_set
