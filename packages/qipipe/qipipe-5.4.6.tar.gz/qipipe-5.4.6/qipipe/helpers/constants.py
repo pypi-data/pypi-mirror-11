49484a93acdee02be26e1e656ea970d97383dd7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:22 +0000

    Add package qipipe/5.4.6/qipipe-5.4.6.tar.gz

---
+++ packages/qipipe/qipipe-5.4.6.tar.gz/qipipe-5.4.6/qipipe/helpers/constants.py
@@ -0,0 +1,5 @@
+SUBJECT_FMT = '%s%03d'
+"""The QIN subject name format with argument subject number."""
+
+SESSION_FMT = 'Session%02d'
+"""The QIN session name format with argument session number."""
