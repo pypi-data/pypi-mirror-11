707d91cf4f70da9265ec45e1760736d76e294a33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:56 +0000

    Add package magneto/0.32.1/magneto-0.32.1.zip

---
+++ packages/magneto/magneto-0.32.1.zip/magneto-0.32.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='magneto',
+    version='0.32.1',
+    description='Magneto - Test Automation for Android',
+    author='EverythingMe',
+    author_email='automation@everything.me',
+    url='http://github.com/EverythingMe/magneto',
+    packages=find_packages(),
+    install_requires=[
+        'pytest',
+        'pytest-ordering',
+        'uiautomator',
+        'futures',
+        'coloredlogs',
+        'IPython',
+        'Click'
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'magneto = magneto.main:main',
+            'imagneto = magneto.imagneto:main'
+        ]
+    },
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Testing'
+    ]
+)
