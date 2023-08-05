0b0185bb899b90f3c1059cfc98986db209953e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:48 +0000

    Add package temp_email_validator/0.1/temp_email_validator-0.1.tar.gz

---
+++ packages/temp_email_validator/temp_email_validator-0.1.tar.gz/temp_email_validator-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='temp_email_validator',
+      version='0.1',
+      description='Temporary / Disposable Email Address Validator for Python',
+      url='http://idiotinside.com/disposable-temporary-email-address-validation/',
+      author='Suresh Kumar',
+      author_email='dsk@idiotinside.com',
+      license='MIT',
+      packages=['temp_email_validator'],
+      zip_safe=False)
