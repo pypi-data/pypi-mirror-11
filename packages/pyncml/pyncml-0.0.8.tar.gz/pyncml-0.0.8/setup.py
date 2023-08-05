d89be2a415143531e2cc67e740ccb5687ccbb037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:28 +0000

    Add package pyncml/0.0.8/pyncml-0.0.8.tar.gz

---
+++ packages/pyncml/pyncml-0.0.8.tar.gz/pyncml-0.0.8/setup.py
@@ -0,0 +1,37 @@
+from __future__ import with_statement
+import sys
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+from pyncml import __version__
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+reqs = [line.strip() for line in open('requirements.txt')]
+
+setup(
+    name                = "pyncml",
+    version             = __version__,
+    description         = "A simple python library to apply NcML logic to NetCDF files",
+    long_description    = readme(),
+    license             = 'MIT',
+    author              = "Kyle Wilcox",
+    author_email        = "kyle@axiomdatascience.com",
+    url                 = "https://github.com/axiom-data-science/pyncml",
+    packages            = find_packages(),
+    install_requires    = reqs,
+    classifiers         = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+    ],
+    include_package_data = True,
+)
