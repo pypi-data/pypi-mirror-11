fd65d5aab73deb9b8975a285f0ee133652669c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:12 +0000

    Add package AryaLogger/1.1.6/AryaLogger-1.1.6.tar.gz

---
+++ packages/AryaLogger/AryaLogger-1.1.6.tar.gz/AryaLogger-1.1.6/setup.py
@@ -0,0 +1,52 @@
+"""AryaLogger setup.py."""
+
+import os
+from setuptools import setup, find_packages
+
+with open('LICENSE.txt') as f:
+    LICENSE = f.read()
+
+__version__ = "1.0.0"
+VERSION = open(os.path.join('AryaLogger', 'version.py')).read()
+# pylint:disable=exec-used
+exec(VERSION)
+
+
+PKGNAME = 'AryaLogger'
+URL = 'https://github.com/datacenter/' + PKGNAME
+DOWNLOADURL = URL + '/releases/tag/' + str(__version__)
+
+setup(
+    name=PKGNAME,
+    version=__version__,
+    description=('Use the SimpleAciUiLogServer and arya to convert UI REST ' +
+                 'API calls to ACI Python SDK code.'),
+    long_description=open('README.rst').read(),
+    packages=find_packages(),
+    url='https://github.com/datacenter/AryaLogger',
+    download_url=DOWNLOADURL,
+    license=LICENSE,
+    author='Mike Timm',
+    author_email='mtimm@cisco.com',
+    zip_safe=False,
+    install_requires=[
+        'SimpleAciUiLogServer',
+        'acicobra',
+        'arya'
+    ],
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ),
+    scripts=[os.path.join('AryaLogger', 'AryaLogger.py')],
+    entry_points={
+        "console_scripts": [
+            "aryalogger=AryaLogger:main",
+            "AryaLogger=AryaLogger:main",
+        ],
+    },
+)
