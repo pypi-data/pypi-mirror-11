b7209d5e3eed881b728d8bc6dc64ebcd7da666b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:56 +0000

    Add package catimg/1.0.2/catimg-1.0.2.tar.gz

---
+++ packages/catimg/catimg-1.0.2.tar.gz/catimg-1.0.2/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import sys
+if sys.version_info < (3,):
+    sys.exit("catimg requires Python 3")
+
+from setuptools import setup
+import catimg
+
+setup(
+    name='catimg',
+    version=catimg.__version__,
+    description='''Print an image of a cat from Imgur to iTerm2.''',
+    author='Aaron Meurer',
+    author_email='asmeurer@gmail.com',
+    url='https://github.com/asmeurer/catimg',
+    packages=['catimg'],
+    package_data={'catimg.tests': ['aloha_cat.png']},
+    long_description=catimg.__doc__,
+    entry_points={'console_scripts': [ 'catimg = catimg.__main__:main']},
+    install_requires=[
+        'requests',
+        'imgurpython',
+    ],
+    license="MIT",
+    classifiers=[
+        'Environment :: MacOS X',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 3',
+        ],
+)
