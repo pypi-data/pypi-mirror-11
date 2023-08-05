48317de2632fe5e3e97c957220c8503583bef4d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:28 +0000

    Add package dumpy/0.1.2/dumpy-0.1.2.tar.gz

---
+++ packages/dumpy/dumpy-0.1.2.tar.gz/dumpy-0.1.2/dumpy/tests/test_types.py
@@ -0,0 +1,350 @@
+import unittest
+import random
+import dumpy.config as dconfig
+import dumpy.types as dtypes
+
+
+class TestDumpyMeta(unittest.TestCase):
+    def test_format_endian(self):
+        class A(int, metaclass=dtypes.DumpyMeta):
+            __spec__ = '<i'
+        self.assertEqual(A.__struct__.format, b'<i')
+
+        class A(int, metaclass=dtypes.DumpyMeta):
+            __spec__ = '>i'
+        self.assertEqual(A.__struct__.format, b'>i')
+
+        class A(int, metaclass=dtypes.DumpyMeta):
+            __spec__ = 'i'
+        self.assertTrue(
+            chr(A.__struct__.format[0]) in ['@', '=', '<', '>', '!'])
+        self.assertEqual(A.__struct__.format[1], ord('i'))
+
+    def test_base_class(self):
+        class A(int, metaclass=dtypes.DumpyMeta):
+            __spec__ = 'i'
+        self.assertTrue(issubclass(A, dtypes.PrimitiveStructMixin))
+
+    def test_exceptions(self):
+        class A(int, metaclass=dtypes.DumpyMeta): pass
+        self.assertFalse(issubclass(A, dtypes.PrimitiveStructMixin))
+
+        class A(int, metaclass=dtypes.DumpyMeta):
+            __spec__ = b'<B'
+        self.assertEqual(A.__struct__.format, b'<B')
+
+
+class TestInt8(unittest.TestCase):
+    def test_int8(self):
+        i = dtypes.Int8(0x7f)
+
+        self.assertEqual(i.size, 1)
+
+        self.assertEqual(i.pack(), b'\x7f')
+        self.assertEqual(dtypes.Int8.unpack(i.pack()), 0x7f)
+        self.assertEqual(dtypes.Int8.unpack(i.pack()).pack(), i.pack())
+
+        b = bytearray(4)
+        i.pack_into(b, 1)
+        self.assertEqual(b, b'\x00\x7f\x00\x00')
+        i.pack_into(b)
+        self.assertEqual(b, b'\x7f\x7f\x00\x00')
+
+        i = dtypes.Int8.unpack_from(b'\x7e\x7f\x00\x00')
+        self.assertEqual(i, dtypes.Int8(0x7e))
+        i = dtypes.Int8.unpack_from(b'\x7e\x7f\x00\x00', 1)
+        self.assertEqual(i, dtypes.Int8(0x7f))
+
+
+class TestArray(unittest.TestCase):
+    def test_array(self):
+        class ByteArray(tuple, metaclass=dtypes.DumpyMeta):
+            __spec__ = '4B'
+
+        b = ByteArray((1, 2, 3, 4))
+        self.assertEqual(b.size, 4)
+        self.assertEqual(b.pack(), b'\x01\x02\x03\x04')
+        self.assertEqual(ByteArray.unpack(b.pack()), b)
+        self.assertEqual(ByteArray.unpack(b.pack()).pack(), b.pack())
+
+        bb = bytearray(6)
+        b.pack_into(bb, 1)
+        self.assertEqual(bb, b'\x00\x01\x02\x03\x04\x00')
+        self.assertEqual(ByteArray.unpack_from(bb, 1), b)
+        self.assertEqual(ByteArray.unpack_from(bb, 1).pack(), b.pack())
+
+
+class TestCompositeDumpyMeta(unittest.TestCase):
+    def test_field_specs(self):
+        class A(dict, metaclass=dtypes.DumpyMeta):
+            __field_specs__ = (
+                dtypes.field('field1', dtypes.Int8),
+                dtypes.field('field2', dtypes.Int8, default=9),
+                dtypes.field('field3', dtypes.Int8, 4),
+                dtypes.field('field4', dtypes.Int8, 4, 1),
+            )
+
+        self.assertTrue(issubclass(A, dtypes.CompositeStructMixin))
+
+        a = A()
+
+        with self.assertRaises(KeyError):
+            a.pack()
+
+        with self.assertRaises(TypeError):
+            a['field1'] = [0x7f]
+
+        self.assertEqual(a['field2'], 9)
+
+        a['field1'] = 0x7f
+        with self.assertRaises(ValueError):
+            a.pack()
+
+        with self.assertRaises(TypeError):
+            a['field3'] = 1
+
+        with self.assertRaises(ValueError):
+            a['field3'] = [1, 2]
+
+        a['field3'] = [1, 2, 3, 4]
+
+        self.assertEqual(a['field4'], [1, 1, 1, 1])
+
+        data = a.pack()
+        self.assertEqual(data, b'\x7f\x09\x01\x02\x03\x04\x01\x01\x01\x01')
+
+        a['field2'] = 10
+        a['field4'] = [2, 2, 2, 2]
+        data = a.pack()
+        self.assertEqual(data, b'\x7f\x0a\x01\x02\x03\x04\x02\x02\x02\x02')
+
+        class B(dict, metaclass=dtypes.DumpyMeta):
+            __field_specs__ = (
+                dtypes.field('field', dtypes.Int8, count=0),
+            )
+
+        b = B()
+        self.assertEqual(b.size, 0)
+        with self.assertRaises(ValueError):
+            b['field'] = 0
+        with self.assertRaises(ValueError):
+            b['field']
+
+        self.assertEqual(B.unpack(b''), {})
+        self.assertEqual(B.unpack_from(b'\x00\x00'), {})
+        self.assertEqual(b.pack(), b'')
+        bb = bytearray(b'\x7e\x7f')
+        b.pack_into(bb, 1)
+        self.assertEqual(bb, b'\x7e\x7f')
+
+    def test_multi_level_composite(self):
+        class Header(dict, metaclass=dtypes.DumpyMeta):
+            __field_specs__ = (
+                dtypes.field('field1', dtypes.Int8),
+                dtypes.field('field2', dtypes.Int8),
+            )
+
+        class Body(dict, metaclass=dtypes.DumpyMeta):
+            __field_specs__ = (
+                dtypes.field('field', dtypes.Int8),
+            )
+
+        class Msg(dict, metaclass=dtypes.DumpyMeta):
+            __field_specs__ = (
+                dtypes.field('header', Header),
+                dtypes.field('bodies', Body, count=2),
+            )
+
+        m = Msg()
+        m['header'] = {'field1': 0, 'field2': 0}
+        m['bodies'] = [Body(), Body()]
+
+        self.assertEqual(m['header'].parent(), m)
+        self.assertEqual(m['bodies'][0].parent(), m)
+        self.assertEqual(m['bodies'][1].parent(), m)
+
+        m['header']['field1'] = 0x7e
+        m['header']['field2'] = 0x7f
+        m['bodies'][0]['field'] = 1
+        m['bodies'][1]['field'] = 2
+
+        self.assertEqual(m.pack(), b'\x7e\x7f\x01\x02')
+        self.assertEqual(Msg.unpack(m.pack()), m)
+        self.assertEqual(Msg.unpack(m.pack()).pack(), m.pack())
+
+        mm = Msg.unpack(m.pack())
+        mm['header']['field1'] = 0x7d
+        self.assertEqual(mm['header'].parent(), mm)
+        self.assertEqual(mm['bodies'][0].parent(), mm)
+        self.assertEqual(mm['bodies'][1].parent(), mm)
+
+        b = bytearray(6)
+        m.pack_into(b, 2)
+        self.assertEqual(b, b'\x00\x00\x7e\x7f\x01\x02')
+        m2 = Msg.unpack_from(b, 2)
+        self.assertEqual(m, m2)
+
+        with self.assertRaises(ValueError):
+            m.pack_into(b, 3)
+
+    def test_variable_length_field(self):
+        def check_byte_continue(obj):
+            if len(obj['extra_bytes']) <= 0:
+                return True
+            else:
+                return obj['extra_bytes'][-1] > 0
+
+        class A(dict, metaclass=dtypes.DumpyMeta):
+            __field_specs__ = (
+                dtypes.field('len', dtypes.Int8,
+                             default=dtypes.count_of('numbers')),
+                dtypes.field('numbers', dtypes.Int8,
+                             count=dtypes.counted_by('len')),
+                dtypes.field('extra_bytes', dtypes.Int8,
+                             count=check_byte_continue),
+            )
+
+        a = A()
+        self.assertEqual(a['len'], 0)
+
+        with self.assertRaises(TypeError):
+            a['numbers'] = 1
+
+        with self.assertRaises(TypeError):
+            a['extra_bytes'] = 1
+
+        a['numbers'] = [1, 2, 3, 4]
+        a['extra_bytes'] = [127, 126, 125, -1]
+        self.assertEqual(a['len'], 4)
+        self.assertEqual(a.pack(), b'\x04\x01\x02\x03\x04\x7f\x7e\x7d\xff')
+        self.assertEqual(A.unpack(a.pack()).pack(), a.pack())
+
+        a['numbers'] = []
+        a['extra_bytes'] = [-1]
+        self.assertEqual(a.pack(), b'\x00\xff')
+        self.assertEqual(A.unpack(a.pack()).pack(), a.pack())
+
+        self.assertEqual(
+            A.unpack_from(b'\x02\x01\x02\x03\xff\x04').pack(),
+            b'\x02\x01\x02\x03\xff')
+
+    def test_variable_type(self):
+        def get_type(obj):
+            if obj['type'] == 0:
+                return dtypes.Int8
+            elif obj['type'] == 1:
+                return dtypes.Int32
+
+        class A(dict, metaclass=dtypes.DumpyMeta):
+            __field_specs__ = (
+                dtypes.field('type', dtypes.Int8),
+                dtypes.field('data', dtypes.VariableType(get_type)),
+            )
+
+        a = A()
+        a['type'] = 0
+        a['data'] = 0x7f
+        self.assertEqual(a.size, 2)
+        self.assertEqual(a.pack(), b'\x00\x7f')
+        self.assertEqual(A.unpack(b'\x00\x7f'), a)
+        self.assertEqual(A.unpack(b'\x00\x7f').pack(), a.pack())
+
+        b = bytearray(4)
+        a.pack_into(b, 1)
+        self.assertEqual(b, b'\x00\x00\x7f\x00')
+
+        a['type'] = 1
+        self.assertEqual(a.size, 5)
+        if dconfig.ENDIAN == '<':
+            self.assertEqual(a.pack(), b'\x01\x7f\x00\x00\x00')
+            self.assertEqual(A.unpack(b'\x01\x7f\x00\x00\x00'), a)
+            self.assertEqual(A.unpack(b'\x01\x7f\x00\x00\x00').pack(), a.pack())
+            b = bytearray(7)
+            a.pack_into(b, 1)
+            self.assertEqual(b, b'\x00\x01\x7f\x00\x00\x00\x00')
+        elif dconfig.ENDIAN == '>':
+            self.assertEqual(a.pack(), b'\x01\x00\x00\x00\x7f')
+            self.assertEqual(A.unpack(b'\x01\x00\x00\x00\x7f'), a)
+            self.assertEqual(A.unpack(b'\x01\x00\x00\x00\x7f').pack(), a.pack())
+            b = bytearray(7)
+            a.pack_into(b, 1)
+            self.assertEqual(b, b'\x00\x01\x00\x00\x00\x7f\x00')
+
+    def test_dynamic_default(self):
+        i = 0
+        def dyn_default(_obj):
+            nonlocal i
+            i += 1
+            return i
+
+        class A(dict, metaclass=dtypes.DumpyMeta):
+            __field_specs__ = (
+                dtypes.field('misc', dtypes.UInt8,
+                             count=4,
+                             default=dyn_default),
+            )
+
+        a = A()
+        self.assertEqual(a['misc'], [1, 2, 3, 4])
+        i = 0
+        self.assertEqual(a.pack(), b'\x01\x02\x03\x04')
+
+        class B(dict, metaclass=dtypes.DumpyMeta):
+            __field_specs__ = (
+                dtypes.field('len', dtypes.UInt8,
+                             default=dtypes.count_of('data')),
+                dtypes.field('data', dtypes.UInt8,
+                             count=dtypes.counted_by('len')),
+            )
+
+        b = B.unpack_from(b'\x04\x01\x02\x03\x04\x05')
+        self.assertEqual(b['len'], 4)
+        self.assertEqual(b['data'], [1, 2, 3, 4])
+
+        b = B()
+        b['data'] = [3, 2, 1]
+        self.assertEqual(b['len'], 3)
+        self.assertEqual(b.pack(), b'\x03\x03\x02\x01')
+
+    def test_exceptions(self):
+        with self.assertRaises(RuntimeError):
+            dtypes.NoDefault()
+
+        class A(dict, metaclass=dtypes.DumpyMeta): pass
+        self.assertFalse(issubclass(A, dtypes.CompositeStructMixin))
+
+        class A(dict, metaclass=dtypes.DumpyMeta):
+            __field_specs__ = (
+                dtypes.field('field', dtypes.UInt8, count=4),
+            )
+
+        a = A()
+
+        with self.assertRaises(KeyError):
+            a['not_a_field'] = 1
+
+        with self.assertRaises(ValueError):
+            a['field'] = [1, 2, 3, 4, 5]
+
+        a['field'] = [1, 2, 3, 4]
+        a['field'].append(5)
+        with self.assertRaises(ValueError):
+            a['field']
+        with self.assertRaises(ValueError):
+            a.pack()
+
+    def test_validator(self):
+        def check_non_neg_num(num, _finfo):
+            if num < 0:
+                raise ValueError('num < 0')
+
+        class A(dict, metaclass=dtypes.DumpyMeta):
+            __field_specs__ = (
+                dtypes.field('misc', dtypes.Int8, validator=check_non_neg_num),
+            )
+
+        a = A.unpack(b'\x7f')
+        self.assertEqual(a['misc'], 127)
+
+        with self.assertRaises(ValueError):
+            a = A.unpack(b'\x8f')
---
+++ packages/dumpy/dumpy-0.1.2.tar.gz/dumpy-0.1.2/dumpy/types.py
@@ -0,0 +1,448 @@
+import struct
+import weakref
+import collections
+from collections import abc
+from .config import ENDIAN
+
+
+class PrimitiveStructMixin:
+    def pack(self):
+        return self.__struct__.pack(self)
+
+    def pack_into(self, buf, offset=0):
+        self.__struct__.pack_into(buf, offset, self)
+
+    @classmethod
+    def unpack(cls, buf):
+        (value,) = cls.__struct__.unpack(buf)
+        return cls(value)
+
+    @classmethod
+    def unpack_from(cls, buf, offset=0, parent=None):
+        (value,) = cls.__struct__.unpack_from(buf, offset)
+        return cls(value)
+
+    @property
+    def size(self):
+        return self.__struct__.size
+
+
+class SequenceStructMixin:
+    def pack(self):
+        return self.__struct__.pack(*self)
+
+    def pack_into(self, buf, offset=0):
+        self.__struct__.pack_into(buf, offset, *self)
+
+    @classmethod
+    def unpack(cls, buf):
+        return cls(cls.__struct__.unpack(buf))
+
+    @classmethod
+    def unpack_from(cls, buf, offset=0):
+        return cls(cls.__struct__.unpack_from(buf, offset))
+
+    @property
+    def size(self):
+        return self.__struct__.size
+
+
+class NoDefault:
+    def __init__(self):
+        raise RuntimeError('NoDefault cannot be instantiated')
+
+
+FieldInfo = collections.namedtuple(
+    'FieldInfo', ['name', 'tp', 'count', 'default', 'validator'])
+
+
+def field(name, tp, count=1, default=NoDefault, validator=None):
+    return (name, tp, count, default, validator)
+
+
+def counted_by(name):
+    def counted_by_func(obj):
+        return obj[name]
+    return counted_by_func
+
+
+def count_of(name):
+    def count_of_func(obj):
+        return len(obj[name])
+    return count_of_func
+
+
+class VariableType:
+    def __init__(self, get_type):
+        self._get_type = get_type
+
+    def get_type(self, obj):
+        return self._get_type(obj)
+
+
+class CompositeStructMixin:
+    def _safe_get(self, fname, default=None):
+        try:
+            return super().__getitem__(fname)
+        except KeyError:
+            return default
+
+    def _get_field(self, fname, count, default):
+        if callable(count):
+            # variable length
+            return self._safe_get(fname, [])
+        else:
+            if count > 1:
+                val_list = self._safe_get(fname, [])
+                real_count = len(val_list)
+                # We checked this in __setitem__, but a newly created object may
+                # still have insufficient values to pack.
+                if real_count < count:
+                    if default is NoDefault:
+                        raise ValueError(
+                            'Expected {} values for field {}, '
+                            'but got {}'.format(count, repr(fname), real_count))
+                    elif callable(default):
+                        default_list = \
+                            [default(self) for _i in range(count - real_count)]
+                    else:
+                        default_list = [default] * (count - real_count)
+                    val_list += default_list
+                elif real_count > count:
+                    # We checked this in __setitem__ too, but lists are mutable,
+                    # so we check again here, just to be sure.
+                    raise ValueError(
+                        'Expected {} values for field {}, '
+                        'but got {}'.format(count, repr(fname), real_count))
+                return val_list
+            elif count == 1:
+                if default is NoDefault:
+                    field_val = super().__getitem__(fname)
+                else:
+                    field_val = self._safe_get(fname, None)
+                    if field_val is None:
+                        if callable(default):
+                            default = default(self)
+                        field_val = default
+                return field_val
+            else:
+                return None
+
+    def _normalize_composite(self, value, ftype):
+        if issubclass(ftype, CompositeStructMixin):
+            if not isinstance(value, ftype):
+                value = ftype(value)
+            value.parent = weakref.ref(self)
+        return value
+
+    @classmethod
+    def _validate(cls, fval, finfo):
+        if finfo.validator is not None:
+            finfo.validator(fval, finfo)
+
+    def __getitem__(self, fname):
+        finfo = self.__field_info__[fname]
+        ret = self._get_field(fname, finfo.count, finfo.default)
+        if ret is None:
+            raise ValueError('Field {} cannot be read'.format(fname))
+        return ret
+
+    def __setitem__(self, fname, value):
+        finfo = self.__field_info__[fname]
+
+        if isinstance(finfo.tp, VariableType):
+            ftype = finfo.tp.get_type(self)
+        else:
+            ftype = finfo.tp
+
+        if callable(finfo.count):
+            if not isinstance(value, abc.Sequence):
+                raise TypeError(
+                    'Field {} needs a sequence'.format(repr(fname)))
+            value = [self._normalize_composite(v, ftype) for v in value]
+            super().__setitem__(fname, value)
+        else:
+            if finfo.count > 1:
+                if not isinstance(value, abc.Sequence):
+                    raise TypeError(
+                        'Field {} needs a sequence'.format(repr(fname)))
+                if len(value) > finfo.count:
+                    raise ValueError(
+                        'Field {} needs {} values, but got {}'.format(
+                            repr(fname), finfo.count, len(value)))
+                elif len(value) < finfo.count and finfo.default is NoDefault:
+                    raise ValueError(
+                        'Field {} needs {} values, but got {}'.format(
+                            repr(fname), finfo.count, len(value)))
+
+                value = [self._normalize_composite(v, ftype) for v in value]
+                super().__setitem__(fname, value)
+            elif finfo.count == 1:
+                if isinstance(value, abc.Sequence):
+                    raise TypeError(
+                        'Field {} cannot accept a sequence'.format(repr(fname)))
+
+                value = self._normalize_composite(value, ftype)
+                super().__setitem__(fname, value)
+            else:
+                raise ValueError('No space for field {}'.format(repr(fname)))
+
+    def pack(self):
+        bin_list = []
+        for fname in self.__fields__:
+            finfo = self.__field_info__[fname]
+            val = self._get_field(fname, finfo.count, finfo.default)
+
+            if val is None:
+                continue
+
+            if not isinstance(val, list):
+                val = [val]
+            for v in val:
+                try:
+                    packed = v.pack()
+                except AttributeError:
+                    if isinstance(finfo.tp, VariableType):
+                        ftype = finfo.tp.get_type(self)
+                    else:
+                        ftype = finfo.tp
+                    packed = ftype(v).pack()
+                bin_list.append(packed)
+
+        return b''.join(bin_list)
+
+    def pack_into(self, buf, offset=0):
+        total_size = self.size
+        if len(buf[offset:]) < total_size:
+            raise ValueError(
+                'pack_into needs {} bytes of space, but only got {}'.format(
+                    total_size, len(buf[offset:])))
+
+        for fname in self.__fields__:
+            finfo = self.__field_info__[fname]
+            val = self._get_field(fname, finfo.count, finfo.default)
+
+            if val is None:
+                continue
+
+            if not isinstance(val, list):
+                val = [val]
+            for v in val:
+                try:
+                    v.pack_into(buf, offset)
+                except AttributeError:
+                    if isinstance(finfo.tp, VariableType):
+                        ftype = finfo.tp.get_type(self)
+                    else:
+                        ftype = finfo.tp
+                    v = ftype(v)
+                    v.pack_into(buf, offset)
+                offset += v.size
+
+    @classmethod
+    def unpack(cls, buf):
+        obj = cls.unpack_from(buf, 0)
+        return obj
+
+    @classmethod
+    def unpack_from(cls, buf, offset=0, parent=None):
+        obj = cls()
+
+        if parent is not None:
+            obj.parent = weakref.ref(parent)
+        else:
+            obj.parent = None
+
+        for fname in cls.__fields__:
+            finfo = cls.__field_info__[fname]
+
+            count_known = True
+            if callable(finfo.count):
+                real_count = finfo.count(obj)
+                if isinstance(real_count, bool):
+                    count_known = False
+            else:
+                real_count = finfo.count
+
+            if isinstance(finfo.tp, VariableType):
+                ftype = finfo.tp.get_type(obj)
+            else:
+                ftype = finfo.tp
+
+            if count_known:
+                val_list = []
+                for i in range(real_count):
+                    v = ftype.unpack_from(buf, offset, obj)
+                    offset += v.size
+                    val_list.append(v)
+
+                if callable(finfo.count):
+                    cls._validate(val_list, finfo)
+                    super().__setitem__(obj, fname, val_list)
+                else:
+                    if len(val_list) > 1:
+                        cls._validate(val_list, finfo)
+                        super().__setitem__(obj, fname, val_list)
+                    elif len(val_list) == 1:
+                        cls._validate(val_list[0], finfo)
+                        super().__setitem__(obj, fname, val_list[0])
+            else:
+                val_list = []
+                super().__setitem__(obj, fname, val_list)
+                while finfo.count(obj):
+                    v = ftype.unpack_from(buf, offset, obj)
+                    offset += v.size
+                    val_list.append(v)
+                cls._validate(val_list, finfo)
+
+        return obj
+
+    def _safe_size(self, v, ftype):
+        try:
+            return v.size
+        except AttributeError:
+            return ftype(v).size
+
+    @property
+    def size(self):
+        size = 0
+        for fname in self.__fields__:
+            finfo = self.__field_info__[fname]
+            val = self._get_field(fname, finfo.count, finfo.default)
+
+            if val is None:
+                continue
+
+            if isinstance(finfo.tp, VariableType):
+                ftype = finfo.tp.get_type(self)
+            else:
+                ftype = finfo.tp
+
+            if isinstance(val, list):
+                for v in val:
+                    size += self._safe_size(v, ftype)
+            else:
+                size += self._safe_size(val, ftype)
+        return size
+
+
+class DumpyMeta(type):
+    def __new__(cls, clsname, bases, clsdict):
+        if any([issubclass(c, abc.Mapping) for c in bases]):
+            return cls._new_composite(cls, clsname, bases, clsdict)
+        elif any([issubclass(c, abc.Sequence) for c in bases]):
+            return cls._new_sequence(cls, clsname, bases, clsdict)
+        else:
+            return cls._new_primitive(cls, clsname, bases, clsdict)
+
+    def _normalize_format(fmt, clsdict):
+        first_char = fmt[0]
+        if isinstance(first_char, int):
+            first_char = chr(first_char)
+
+        if first_char not in ['@', '=', '<', '>', '!']:
+            try:
+                endian = clsdict['__endian__']
+            except KeyError:
+                endian = ENDIAN
+            return (endian + fmt)
+        else:
+            return fmt
+
+    def _new_simple(cls, clsname, bases, clsdict, extra_base):
+        try:
+            fmt = clsdict['__spec__']
+        except KeyError:
+            # No meta data, do not process this class
+            return super().__new__(cls, clsname, bases, clsdict)
+
+        if extra_base not in bases:
+            bases = (extra_base,) + bases
+
+        fmt = cls._normalize_format(fmt, clsdict)
+        clsdict['__struct__'] = struct.Struct(fmt)
+
+        return super().__new__(cls, clsname, bases, clsdict)
+
+    def _new_primitive(cls, clsname, bases, clsdict):
+        return cls._new_simple(
+            cls, clsname, bases, clsdict, PrimitiveStructMixin)
+
+    def _new_sequence(cls, clsname, bases, clsdict):
+        return cls._new_simple(
+            cls, clsname, bases, clsdict, SequenceStructMixin)
+
+    def _new_composite(cls, clsname, bases, clsdict):
+        try:
+            fields = clsdict['__field_specs__']
+        except KeyError:
+            # No meta data, do not process this class
+            return super().__new__(cls, clsname, bases, clsdict)
+
+        __fields__ = []
+        __field_info__ = {}
+        for f in fields:
+            fname = f[0]
+            __fields__.append(fname)
+
+            ftype = f[1]
+            ff = list(f[2:])
+
+            try:
+                count = ff.pop(0)
+            except IndexError:
+                count = 1
+
+            try:
+                default = ff.pop(0)
+            except IndexError:
+                default = NoDefault
+
+            try:
+                validator = ff.pop(0)
+            except IndexError:
+                validator = None
+
+            __field_info__[fname] = \
+                FieldInfo(fname, ftype, count, default, validator)
+
+        clsdict['__fields__'] = __fields__
+        clsdict['__field_info__'] = __field_info__
+
+        if CompositeStructMixin not in bases:
+            bases = (CompositeStructMixin,) + bases
+
+        new_cls = super().__new__(cls, clsname, bases, clsdict)
+        return new_cls
+
+
+class Int8(int, metaclass=DumpyMeta):
+    __spec__ = 'b'
+
+
+class UInt8(int, metaclass=DumpyMeta):
+    __spec__ = 'B'
+
+
+class Int16(int, metaclass=DumpyMeta):
+    __spec__ = 'h'
+
+
+class UInt16(int, metaclass=DumpyMeta):
+    __spec__ = 'H'
+
+
+class Int32(int, metaclass=DumpyMeta):
+    __spec__ = 'i'
+
+
+class UInt32(int, metaclass=DumpyMeta):
+    __spec__ = 'I'
+
+
+class Float(float, metaclass=DumpyMeta):
+    __spec__ = 'f'
+
+
+class Double(float, metaclass=DumpyMeta):
+    __spec__ = 'd'
