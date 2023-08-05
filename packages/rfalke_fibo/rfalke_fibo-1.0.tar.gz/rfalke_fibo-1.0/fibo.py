c938a21a002576458a2852c4a9ccedadbb83a162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:47 +0000

    Add package rfalke_fibo/1.0/rfalke_fibo-1.0.tar.gz

---
+++ packages/rfalke_fibo/rfalke_fibo-1.0.tar.gz/rfalke_fibo-1.0/fibo.py
@@ -0,0 +1,8 @@
+def fib(n):
+    '''
+Berechnet die Fibonacci-Nummer. Beispiel:
+>>> fib(15)
+610
+    '''
+    if n <= 2: return 1
+    return fib(n - 1) + fib(n - 2)
---
+++ packages/rfalke_fibo/rfalke_fibo-1.0.tar.gz/rfalke_fibo-1.0/setup.py
@@ -0,0 +1,3 @@
+from distutils.core import setup
+
+setup(name='rfalke_fibo', version='1.0', py_modules=['fibo'] )
