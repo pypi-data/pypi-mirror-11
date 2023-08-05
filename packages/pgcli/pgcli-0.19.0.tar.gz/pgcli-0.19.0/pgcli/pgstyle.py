0aaf9cba03d4338b1025e6045f18e9ebb68f4cab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:18 +0000

    Add package pgcli/0.19.0/pgcli-0.19.0.tar.gz

---
+++ packages/pgcli/pgcli-0.19.0.tar.gz/pgcli-0.19.0/pgcli/pgstyle.py
@@ -0,0 +1,35 @@
+from pygments.token import Token
+from pygments.style import Style
+from pygments.util import ClassNotFound
+from prompt_toolkit.styles import default_style_extensions
+import pygments.styles
+
+
+def style_factory(name):
+    try:
+        style = pygments.styles.get_style_by_name(name)
+    except ClassNotFound:
+        style = pygments.styles.get_style_by_name('native')
+
+    class PGStyle(Style):
+        styles = {}
+
+        styles.update(style.styles)
+        styles.update(default_style_extensions)
+        styles.update({
+            Token.Menu.Completions.Completion.Current: 'bg:#00aaaa #000000',
+            Token.Menu.Completions.Completion: 'bg:#008888 #ffffff',
+            Token.Menu.Completions.Meta.Current: 'bg:#44aaaa #000000',
+            Token.Menu.Completions.Meta: 'bg:#448888 #ffffff',
+            Token.Menu.Completions.ProgressButton: 'bg:#003333',
+            Token.Menu.Completions.ProgressBar: 'bg:#00aaaa',
+            Token.SelectedText: '#ffffff bg:#6666aa',
+            Token.IncrementalSearchMatch: '#ffffff bg:#4444aa',
+            Token.IncrementalSearchMatch.Current: '#ffffff bg:#44aa44',
+            Token.Toolbar: 'bg:#440044 #ffffff',
+            Token.Toolbar: 'bg:#222222 #aaaaaa',
+            Token.Toolbar.Off: 'bg:#222222 #888888',
+            Token.Toolbar.On: 'bg:#222222 #ffffff',
+        })
+
+    return PGStyle
