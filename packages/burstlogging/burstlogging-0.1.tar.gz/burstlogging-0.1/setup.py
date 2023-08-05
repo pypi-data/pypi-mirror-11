d9c752081bc3ff56caa6e01de45c77e8cb1bed72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:29 +0000

    Add package burstlogging/0.1/burstlogging-0.1.tar.gz

---
+++ packages/burstlogging/burstlogging-0.1.tar.gz/burstlogging-0.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "burstlogging",
+    version = "0.1",
+    author = "Jarek Lipski",
+    author_email = "pub@loomchild.net",
+    description = ("Burst logging library."),
+    license = "MIT",
+    keywords = "logging",
+    url = "https://github.com/loomchild/burstlogging",
+    py_modules=['burstlogging'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
+
