6f5c9a7e445a0cdc8b204c76b4462f2848d5f730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:14 +0000

    Add package hbmqtt/0.5.1/hbmqtt-0.5.1-py34-none-any.whl

---
+++ packages/hbmqtt/hbmqtt-0.5.1-py34-none-any.whl/hbmqtt/mqtt/pubrec.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2015 Nicolas JOUANIN
+#
+# See the file license.txt for copying permission.
+from hbmqtt.mqtt.packet import MQTTPacket, MQTTFixedHeader, PUBREC, PacketIdVariableHeader
+from hbmqtt.errors import HBMQTTException
+
+
+class PubrecPacket(MQTTPacket):
+    VARIABLE_HEADER = PacketIdVariableHeader
+    PAYLOAD = None
+
+    def __init__(self, fixed: MQTTFixedHeader=None, variable_header: PacketIdVariableHeader=None):
+        if fixed is None:
+            header = MQTTFixedHeader(PUBREC, 0x00)
+        else:
+            if fixed.packet_type is not PUBREC:
+                raise HBMQTTException("Invalid fixed packet type %s for PubrecPacket init" % fixed.packet_type)
+            header = fixed
+        super().__init__(header)
+        self.variable_header = variable_header
+        self.payload = None
+
+    @classmethod
+    def build(cls, packet_id: int):
+        v_header = PacketIdVariableHeader(packet_id)
+        packet = PubrecPacket(variable_header=v_header)
+        return packet
