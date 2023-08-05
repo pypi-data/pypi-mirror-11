808750f4230673d47c1399f8fb84d80083220e10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:53 +0000

    Add package reversible/0.2.0/reversible-0.2.0.tar.gz

---
+++ packages/reversible/reversible-0.2.0.tar.gz/reversible-0.2.0/README.rst
@@ -0,0 +1,16 @@
+``reversible``
+==============
+
+``reversible`` provides a simple abstraction for actions that can be
+reversed or rolled back and provides methods to construct, chain, and
+consume them in a readable way.
+
+**New in 0.2**:
+
+-  Support for Tornado coroutines as actions
+
+Documentation
+    http://reversible.readthedocs.org/en/latest/
+
+PyPI
+    https://pypi.python.org/pypi/reversible
---
+++ packages/reversible/reversible-0.2.0.tar.gz/reversible-0.2.0/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name='reversible',
+    version='0.2.0',
+    description=(
+        'A Python library to represent, construct, chain, and execute '
+        'reversible actions.'
+    ),
+    long_description=long_description,
+    author='Abhinav Gupta',
+    author_email='mail@abhinavg.net',
+    url='https://github.com/abhinav/reversible',
+    packages=find_packages(exclude=('tests', 'test.*')),
+    license='MIT',
+    tests_require=['pytest', 'mock'],
+    extras_require={
+        'tornado': ['tornado', 'greenlet'],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
