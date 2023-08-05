13c5f7e51f07aa139820f11fa6c5ae7a9b9a95ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:41 +0000

    Add package codefights/0.2/codefights-0.2.tar.gz

---
+++ packages/codefights/codefights-0.2.tar.gz/codefights-0.2/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='codefights',
+    version='0.2',
+    description='Codefights.net SDK for Python developers',
+    author='Gytis Talocka',
+    author_email='kurideja@gmail.com',
+    url='https://github.com/codefights/sdk-python',
+    download_url='https://github.com/codefights/sdk-python/tarball/0.2',
+    packages=['codefights', 'codefights.boilerplate', 'codefights.boilerplate.server', 'codefights.model', 'codefights.samples'],
+)
