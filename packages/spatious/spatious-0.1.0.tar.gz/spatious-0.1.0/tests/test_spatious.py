51bd0f9f967a8e6bb107cc4c95329e16278ef4b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:51 +0000

    Add package spatious/0.1.0/spatious-0.1.0.tar.gz

---
+++ packages/spatious/spatious-0.1.0.tar.gz/spatious-0.1.0/tests/test_spatious.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_spatious
+----------------------------------
+
+Tests for `spatious` module.
+"""
+
+import unittest
+from spatious import geom
+
+
+class TestGeom(unittest.TestCase):
+
+    def test_geom(self):
+        pass
