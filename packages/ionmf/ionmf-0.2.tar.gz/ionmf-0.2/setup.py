e8f2dfa7f558f904e2c6bd9006fc29dd131435bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:32 +0000

    Add package ionmf/0.2/ionmf-0.2.tar.gz

---
+++ packages/ionmf/ionmf-0.2.tar.gz/ionmf-0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ionmf',
+  packages = ['ionmf'],
+  version = '0.2',
+  description = 'Integrative orthogonal non-negative matrix factorization with examples.',
+  author = 'Martin Strazar',
+  author_email = 'martin.strazar@gmail.com',
+  url = 'https://github.com/mstrazar/iONMF',
+  download_url = 'https://github.com/mstrazar/iONMF/tarball/0.2',
+  keywords = ['data integration', 'matrix factorization', 'machine learning'],
+  classifiers = [],
+)
