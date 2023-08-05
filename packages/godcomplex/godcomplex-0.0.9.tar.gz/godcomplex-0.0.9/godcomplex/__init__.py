0860daaade5ce20edc7a81ec845d2a328d156249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:00 +0000

    Add package godcomplex/0.0.9/godcomplex-0.0.9.tar.gz

---
+++ packages/godcomplex/godcomplex-0.0.9.tar.gz/godcomplex-0.0.9/godcomplex/__init__.py
@@ -0,0 +1,45 @@
+import speech_recognition as sr
+from espeak import espeak
+import time, pygame
+
+
+def godcomplex():
+	with sr.Microphone() as mic:
+		rec=sr.Recognizer()
+		#mic=sr.Microphone()
+		rec.adjust_for_ambient_noise(mic)
+		while True:
+			print "listening"
+			audio=rec.listen(mic)
+			print "heard you"
+			try:
+				trans=str(rec.recognize(audio)).lower()
+				print "you said "+trans
+				if "god" in trans:
+					espeak.synth("I believe you said my name")
+					#time.sleep(2)
+			except LookupError:
+				print "did not get that"
+
+def batmancomplex():
+	pygame.mixer.init()
+	pygame.mixer.music.load("/usr/local/lib/python2.7/dist-packages/godcomplex/batman.mp3")
+	with sr.Microphone() as mic:
+		rec=sr.Recognizer()
+		rec.adjust_for_ambient_noise(mic)
+		while True:
+			print "listening"
+			audio=rec.listen(mic)
+			print "heard you"
+			try:
+				trans=str(rec.recognize(audio)).lower()
+				print "you said "+trans
+				if 'batman' in trans or 'gotham' in trans or 'joker' in trans or 'robin' in trans or 'bat' in trans:
+					pygame.mixer.music.play()
+					while pygame.mixer.music.get_busy()==True:
+						continue
+			except LookupError:
+				print "did not get that"
+
+if __name__=="__main__":
+	godcomplex()
---
+++ packages/godcomplex/godcomplex-0.0.9.tar.gz/godcomplex-0.0.9/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+from subprocess import call
+from distutils.command.install import install as _install
+from distutils.command.install import INSTALL_SCHEMES
+import sys
+
+for scheme in INSTALL_SCHEMES.values():
+	scheme['data']=scheme['purelib']
+
+def _post_install(dir):
+	global call
+	try:
+		import espeak
+	except:
+		print "missing espeak. installing it for you"
+		call(['sudo','apt-get','install','python-espeak','flac','-y'])
+	print "finished post install"
+	call(['chmod','777',"/usr/local/lib/python2.7/dist-packages/godcomplex/batman.mp3"])
+
+class install(_install):
+	def run(self):
+		_install.run(self)
+		_post_install(self.install_lib)	
+
+setup(name="godcomplex",
+	cmdclass={'install':install},
+	data_files=[('godcomplex',['batman.mp3'])],
+	version="0.0.9",
+	description="this will say something if you say god",
+	author="Isaac Robinson",
+	author_email="isaacrob@me.com",
+	license="GNU",
+	packages=["godcomplex"],
+	scripts=['bin/godcomplex','bin/batmancomplex'],
+	keywords=["god","irony","speech recognition"],
+	install_requires=['SpeechRecognition'],
+	zip_safe=False)
