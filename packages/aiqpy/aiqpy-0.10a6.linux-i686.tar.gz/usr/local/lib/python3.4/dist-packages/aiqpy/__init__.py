463cd24f7221372733832df6e7d904ba31945b07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:16 +0000

    Add package aiqpy/0.10a6/aiqpy-0.10a6.linux-i686.tar.gz

---
+++ packages/aiqpy/aiqpy-0.10a6.linux-i686.tar.gz/usr/local/lib/python3.4/dist-packages/aiqpy/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""
+AIQPY
+-----
+
+AIQPY is a Python library for facilitating communication with an AIQ8 platform.
+
+
+"""
+
+__title__ = 'aiqpy'
+__version__ = '0.10a6'
+__author__ = 'Erik Lundberg'
+__email__ = 'elu@appearnetworks.com'
+
+from .connection import Connection
