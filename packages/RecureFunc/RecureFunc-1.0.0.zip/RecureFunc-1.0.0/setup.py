8d113cee34ebf60e8d62d1ec33b1f64122201da1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:46 +0000

    Add package RecureFunc/1.0.0/RecureFunc-1.0.0.zip

---
+++ packages/RecureFunc/RecureFunc-1.0.0.zip/RecureFunc-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="RecureFunc",
+    version = '1.0.0',
+    py_modules=['recursive'],
+    author=["Dean"],
+    author_email=["ssdysshy@163.com"],
+    url="http://www.baidu.com",
+    description="A function to retrieve list",
+    )
