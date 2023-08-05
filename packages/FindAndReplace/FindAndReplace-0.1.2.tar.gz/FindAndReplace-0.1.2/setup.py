07c38cbfa297294591fdda23b7d8361c97a4c167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:35 +0000

    Add package FindAndReplace/0.1.2/FindAndReplace-0.1.2.tar.gz

---
+++ packages/FindAndReplace/FindAndReplace-0.1.2.tar.gz/FindAndReplace-0.1.2/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name = 'FindAndReplace',
+  packages = ['FindAndReplace'], 
+  version = '0.1.2',
+  description = 'A find and replace tool  ',
+  author = 'Shantanu Khandelwal',
+  author_email = 'shantanu561993@gmail.com',
+  url = 'https://github.com/shantanu561993/FAndR', # use the URL to the github repo
+  download_url = 'https://github.com/shantanu561993/FAndR/tarball/0.1.2',
+  keywords = ['Find', 'replace', 'Replace','replace text','text','Find and replace','replace tags','tags','Replace text'],
+classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
+
