914f09649b93dc05481bcf986d5ee3aecc9112ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:40 +0000

    Add package ahoy/0.1.0/ahoy-0.1.0.tar.gz

---
+++ packages/ahoy/ahoy-0.1.0.tar.gz/ahoy-0.1.0/README.rst
@@ -0,0 +1,20 @@
+===============================
+ahoy
+===============================
+
+.. image:: https://img.shields.io/travis/eddiejessup/ahoy.svg
+        :target: https://travis-ci.org/eddiejessup/ahoy
+
+.. image:: https://img.shields.io/pypi/v/ahoy.svg
+        :target: https://pypi.python.org/pypi/ahoy
+
+
+Agent-based simulations of active particles
+
+* Free software: BSD license
+* Documentation: https://ahoy.readthedocs.org.
+
+Features
+--------
+
+* TODO
---
+++ packages/ahoy/ahoy-0.1.0.tar.gz/ahoy-0.1.0/ahoy/turners.py
@@ -0,0 +1,50 @@
+from __future__ import print_function, division
+import numpy as np
+from ciabatta import vector, crandom
+from ciabatta.vector import smallest_signed_angle as angle_dist
+from ciabatta.meta import make_repr_str
+
+
+class Turner(object):
+
+    def get_angle(self, th_in, *args, **kwargs):
+        return th_in
+
+    def get_norm_angle(self, *args, **kwargs):
+        return vector.normalise_angle(self.get_angle(*args, **kwargs))
+
+    def turn(self, obs, ds, *args, **kwargs):
+        ds.th[obs] = self.get_norm_angle(ds.th[obs], *args, **kwargs)
+
+    def __repr__(self):
+        fs = []
+        return make_repr_str(self, fs)
+
+
+class BounceBackTurner(Turner):
+
+    def get_angle(self, th_in, *args, **kwargs):
+        return th_in + np.pi
+
+
+class ReflectTurner(Turner):
+
+    def get_angle(self, th_in, th_normal, *args, **kwargs):
+        th_rel = th_in - th_normal
+        return th_normal + np.where(th_rel == 0.0,
+                                    np.pi,
+                                    np.sign(th_rel) * np.pi - th_rel)
+
+
+class AlignTurner(Turner):
+
+    def get_angle(self, th_in, th_normal, rng, *args, **kwargs):
+        if rng is None:
+            rng = np.random
+        th_rel = vector.normalise_angle(th_in - th_normal)
+        antiparallels = np.isclose(np.abs(angle_dist(th_in, th_normal)), np.pi)
+        signs = np.where(antiparallels,
+                         crandom.randbool(antiparallels.shape[0], rng),
+                         np.sign(th_rel))
+        th_rel = signs * np.pi / 2.0
+        return th_normal + th_rel
