e3f804477965c5128db1dd2d0a073d44201b2466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:25 +0000

    Add package py-goldsberry/0.5.0/py-goldsberry-0.5.0.zip

---
+++ packages/py-goldsberry/py-goldsberry-0.5.0.zip/py-goldsberry-0.5.0/goldsberry/__init__.py
@@ -0,0 +1,11 @@
+__author__ = 'Bradley Fay'
+__email__ = 'bradley.fay@gmail.com'
+__version__ = '0.5.0'
+
+import requests as _requests
+from goldsberry._apiFunc import *
+import goldsberry.game
+import goldsberry.league
+import goldsberry.player
+import goldsberry.team
+from goldsberry.player._Player import PlayerList
---
+++ packages/py-goldsberry/py-goldsberry-0.5.0.zip/py-goldsberry-0.5.0/goldsberry/league/__init__.py
@@ -0,0 +1,2 @@
+from goldsberry.league._League import *
+from goldsberry.league import _Draft as draft
