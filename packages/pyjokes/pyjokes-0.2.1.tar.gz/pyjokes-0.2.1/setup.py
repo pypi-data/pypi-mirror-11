d7ed9bbd80802b149c054c7071f38001ab72d92d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:08 +0000

    Add package pyjokes/0.2.1/pyjokes-0.2.1.tar.gz

---
+++ packages/pyjokes/pyjokes-0.2.1.tar.gz/pyjokes-0.2.1/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="pyjokes",
+    version="0.2.1",
+    author="Pyjokes Society",
+    description="One line jokes for programmers (jokes as a service)",
+    license="BSD",
+    keywords=[
+        "pyjokes",
+        "jokes",
+    ],
+    url="https://github.com/pyjokes/pyjokes",
+    packages=find_packages(),
+    long_description=read('README.rst'),
+    scripts=['scripts/pyjoke'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+)
