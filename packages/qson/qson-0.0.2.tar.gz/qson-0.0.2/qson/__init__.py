8f0726cf3ed9f68436bb3f8d326c66488382b325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:14 +0000

    Add package qson/0.0.2/qson-0.0.2.tar.gz

---
+++ packages/qson/qson-0.0.2.tar.gz/qson-0.0.2/qson/__init__.py
@@ -0,0 +1,1 @@
+from qson import *
---
+++ packages/qson/qson-0.0.2.tar.gz/qson-0.0.2/qson/__main__.py
@@ -0,0 +1,62 @@
+"""
+qson
+
+Quick JSON based key value store over http
+"""
+import os
+import sys
+
+import server
+import client
+
+VERSION = "0.0.2"
+
+def make_daemon():
+    # Daemonize to run in background
+    pid = os.fork()
+    if pid > 0:
+        # exit first parent
+        sys.exit(0)
+    pid = os.fork()
+    if pid > 0:
+        # exit second parent
+        sys.exit(0)
+    else:
+        output = open("/dev/null", 'wb')
+    sys.stdout = output
+    sys.stderr = output
+
+def start(port=server.PORT, daemon=False):
+    """
+    Starts the webserver, first argument is port number, default is 9898
+    """
+    if type(port) != int:
+        port = int(port)
+    address = (server.ADDRESS, port)
+    test_server = server.ThreadedHTTPServer(address, server.Handler)
+    if daemon is not False:
+        make_daemon()
+    print test_server.port()
+    test_server.serve_forever()
+
+def method(*args):
+    """
+    Runs client methods
+    """
+    test_client = client.client(port=args[0])
+    action = getattr(test_client, args[1])
+    print action(*args[2:])
+
+def query(*args):
+    """
+    Runs client get or set
+    """
+    test_client = client.client(port=args[0])
+    print test_client(*args[1:])
+
+def main():
+    action = getattr(sys.modules[__name__], sys.argv[1])
+    action(*sys.argv[2:])
+
+if __name__ == '__main__':
+    main()
