d95f0ab7282b0af91b4f589cc0bc2b15d6afb53f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:58 +0000

    Add package kroman/1.1/kroman-1.1.tar.gz

---
+++ packages/kroman/kroman-1.1.tar.gz/kroman-1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='kroman',
+      version='1.1',
+      description='Kroman is Korean romanization tool.',
+      author='Zhang Kai Yu',
+      author_email='yeannylam@gmail.com',
+      license='MIT',
+      packages=['kroman'],
+      zip_safe=False,
+      url='https://github.com/cheunghy/kroman-py')
