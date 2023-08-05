1913a62a4444e3c5f7713f31ef31437e5a305432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:07 +0000

    Add package olimex-ekg-emg/0.1.6/olimex-ekg-emg-0.1.6.tar.gz

---
+++ packages/olimex-ekg-emg/olimex-ekg-emg-0.1.6.tar.gz/olimex-ekg-emg-0.1.6/HISTORY.rst
@@ -0,0 +1,42 @@
+.. :changelog:
+
+Release History
+---------------
+
+0.1.6 (2015-08-14)
+++++++++++++++++++
+
+* Added mock data
+* Flipped EKG signal long horizontal axis for a more familiar waveform.
+
+0.1.5 (2015-07-25)
+++++++++++++++++++
+
+* Speedups
+* Add performance measuring logic for debugging slow traces
+* Added ECG grid to main axes
+* Update docs theme
+
+0.1.4 (2015-05-09)
+++++++++++++++++++
+
+* Added ``exg`` script for starting gui from command line.
+
+0.1.2 (2015-01-25)
+++++++++++++++++++
+
+* New ``FakeSerial`` classes
+* New tool: slsnif
+
+0.1.1 (2014-12-10)
+++++++++++++++++++
+
+* Refactored 'Fake' serial objects for mocking a :class:`serial.Serial` object.
+* Update Arduino sketch so that the Arduino can send
+  EXG data to either an SD card or to a serial port.
+
+0.1.0 (2014-11-27)
+++++++++++++++++++
+
+* Getting started
+
---
+++ packages/olimex-ekg-emg/olimex-ekg-emg-0.1.6.tar.gz/olimex-ekg-emg-0.1.6/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+
+import olimex
+
+with open("README.rst") as fd:
+    README = fd.read()
+
+with open("LICENSE") as fd:
+    LICENSE = fd.read()
+
+
+setup(
+    name='olimex-ekg-emg',
+    version=olimex.__version__,
+    description='A package for gathering data from the Olimex EKG/EMG Shield.',
+    license=LICENSE,
+    long_description=README,
+    author=olimex.__author__,
+    author_email=olimex.__email__,
+    url='https://github.com/logston/olimex-ekg-emg',
+    packages=['olimex'],
+    include_package_data=True,
+    test_suite='tests',
+    keywords=['Olimex', 'EKG', 'EMG', 'Arduino'],
+    entry_points = {
+        'console_scripts': ['exg=olimex.gui:run_gui'],
+    },
+    install_requires=[
+        'pyserial>=2.7',
+        'numpy>=1.9.1',
+        'matplotlib>=1.4.2',
+        'scipy>=0.15.1',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Education',
+        'Intended Audience :: Healthcare Industry',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Human Machine Interfaces',
+    ],
+)
