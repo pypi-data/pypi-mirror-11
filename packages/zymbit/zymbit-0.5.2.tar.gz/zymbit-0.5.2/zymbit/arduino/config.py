067b23cf7c2b8af9fa06ea36494527c4ba805767
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:32 +0000

    Add package zymbit/0.5.2/zymbit-0.5.2.tar.gz

---
+++ packages/zymbit/zymbit-0.5.2.tar.gz/zymbit-0.5.2/zymbit/arduino/config.py
@@ -0,0 +1,8 @@
+from zymbit.config import *
+
+FILES_ROOT = '/usr/files/arduino'
+KNOWN_HOSTS = '/.ssh/known_hosts'
+
+TUNNEL_SSH_KEY_PATH = os.path.expanduser(os.environ.get('TUNNEL_SSH_KEY_PATH', '/.ssh/id_tunnel'))
+
+ZYMBIT_KNOWN_HOSTS = '/etc/dropbear/zymbit_known_hosts'
