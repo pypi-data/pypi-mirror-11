94e6ce9c4214a972f5374c8cfd3cf03efc8eb226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:17 +0000

    Add package labellio_cli/0.0.3/labellio_cli-0.0.3.tar.gz

---
+++ packages/labellio_cli/labellio_cli-0.0.3.tar.gz/labellio_cli-0.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="labellio_cli",
+    version="0.0.3",
+    url="https://github.com/AlpacaDB/labellio_cli",
+    description="Labellio command line interface",
+    author="AlpacaDB, Inc.",
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='deep learning caffe alpacadb alpaca db labellio',
+    packages=find_packages(),
+    install_requires=['numpy', 'scikit-image', 'protobuf'],
+    scripts=['bin/labellio_classify'],
+)
