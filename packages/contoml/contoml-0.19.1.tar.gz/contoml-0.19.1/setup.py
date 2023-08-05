b2ea024a15585f494a0f21b431cee6b8d9b5418e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:57 +0000

    Add package contoml/0.19.1/contoml-0.19.1.tar.gz

---
+++ packages/contoml/contoml-0.19.1.tar.gz/contoml-0.19.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='contoml',
+    packages=find_packages(),
+    version='0.19.1',
+    description='Consistent TOML for Python',
+    author='Amr Hassan',
+    author_email='amr.hassan@gmail.com',
+    url='https://github.com/Jumpscale/python-consistent-toml',
+    keywords=['toml'],
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4'
+    ],
+    install_requires=[
+        'six',
+        'strict_rfc3339',
+        'pytz',
+        'timestamp'
+    ]
+)
