132e8627b52fdbfdbd61d70c8fc122790bcbd1e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:15 +0000

    Add package satnogsclient/0.2.4/satnogsclient-0.2.4.tar.gz

---
+++ packages/satnogsclient/satnogsclient-0.2.4.tar.gz/satnogsclient-0.2.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+
+setup(name='satnogsclient',
+      packages=find_packages(),
+      version='0.2.4',
+      author='SatNOGS team',
+      author_email='client-dev@satnogs.org',
+      url='https://github.com/satnogs/satnogs-client/',
+      description='SatNOGS Client',
+      install_requires=['APScheduler',
+                        'SQLAlchemy',
+                        'requests',
+                        'validators',
+                        'python-dateutil',
+                        'ephem',
+                        'pytz'],
+      dependency_links=[
+          'git+https://github.com/brandon-rhodes/pyephem.git@47d0ba3616ee6c308f2eed319af3901592d00f70#egg=ephem'
+      ],
+      scripts=['satnogsclient/bin/satnogs-poller',
+               'satnogsclient/bin/satnogs-task'])
