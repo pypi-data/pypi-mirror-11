417df1218846ca82a285f7dbfa238d6507a3e078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:42 +0000

    Add package wpm_api_client/2.5/wpm_api_client-2.5.tar.gz

---
+++ packages/wpm_api_client/wpm_api_client-2.5.tar.gz/wpm_api_client-2.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='wpm_api_client',
+    version='2.5',
+    description='A Python library for the Web Performance Management API',
+    url='https://github.com/neustar/wpm_api_client',
+    author='Shane Barbetta',
+    author_email='shane@barbetta.me',
+    license='Apache License, Version 2.0',
+    keywords='wpm_api_client',
+    packages=['wpm_api'],
+    install_requires=['requests'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+    zip_safe=False
+)
