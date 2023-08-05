7d1d3594eae99aef94955617b673ab80eb1f38c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:21 +0000

    Add package tlslite-ng/0.5.0-beta1/tlslite-ng-0.5.0-beta1.tar.gz

---
+++ packages/tlslite-ng/tlslite-ng-0.5.0-beta1.tar.gz/tlslite-ng-0.5.0-beta1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# Author: Trevor Perrin
+# See the LICENSE file for legal information regarding use of this file.
+
+from distutils.core import setup
+
+setup(name="tlslite-ng",
+      version="0.5.0-beta1",
+      author="Hubert Kario",
+      author_email="hkario@redhat.com",
+      url="https://github.com/tomato42/tlslite-ng",
+      description="tlslite implements SSL and TLS.",
+      license="LGPLv2",
+      scripts=["scripts/tls.py", "scripts/tlsdb.py"],
+      packages=["tlslite", "tlslite.utils", "tlslite.integration"],
+      package_data={
+                    'package1': ['LICENSE', 'README.md']},
+      )
