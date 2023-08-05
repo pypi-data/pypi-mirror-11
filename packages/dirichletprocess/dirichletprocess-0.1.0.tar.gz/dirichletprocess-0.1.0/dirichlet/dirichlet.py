dff1fe771220e653ffe9ab71978994cb87195733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:06 +0000

    Add package dirichletprocess/0.1.0/dirichletprocess-0.1.0.tar.gz

---
+++ packages/dirichletprocess/dirichletprocess-0.1.0.tar.gz/dirichletprocess-0.1.0/dirichlet/dirichlet.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from random import betavariate, uniform
+
+
+def weighted_choice(weights):
+    choices = range(len(weights))
+    total = sum(weights)
+    r = uniform(0, total)
+    upto = 0
+    for c, w in zip(choices, weights):
+        if upto + w > r:
+            return c
+        upto += w
+    raise Exception("Error in weighted_choice.")
+
+
+class DirichletProcess():
+    def __init__(self, base_measure, alpha):
+        self.base_measure = base_measure
+        self.alpha = alpha
+
+        self.cache = []
+        self.weights = []
+        self.total_stick_used = 0.
+
+    def __call__(self):
+        remaining = 1.0 - self.total_stick_used
+        i = weighted_choice(self.weights + [remaining])
+        if i < len(self.weights):
+            return self.cache[i]
+        else:
+            stick_piece = betavariate(1, self.alpha) * remaining
+            self.total_stick_used += stick_piece
+            self.weights.append(stick_piece)
+            new_value = self.base_measure()
+            self.cache.append(new_value)
+            return new_value
---
+++ packages/dirichletprocess/dirichletprocess-0.1.0.tar.gz/dirichletprocess-0.1.0/tests/test_dirichlet.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_dirichlet
+----------------------------------
+
+Tests for `dirichlet` module.
+"""
+from random import normalvariate
+
+from dirichlet import DirichletProcess
+
+
+def test_basic():
+    dp = DirichletProcess(lambda: normalvariate(0, 1), alpha=0.1)
+    for _ in range(1000):
+        dp()
