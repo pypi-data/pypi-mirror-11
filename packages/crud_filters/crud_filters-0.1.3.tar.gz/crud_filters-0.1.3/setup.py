58dbbe82faa12df6d4a348909244c70a1f07ab30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:22 +0000

    Add package crud_filters/0.1.3/crud_filters-0.1.3.tar.gz

---
+++ packages/crud_filters/crud_filters-0.1.3.tar.gz/crud_filters-0.1.3/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='crud_filters',
+    # packages=['crud_filters'],
+    packages=find_packages(exclude=['tests*']),
+    version='0.1.3',
+    description='django-crud-filters works with django-restframework to provide easily configurable role-based filtering for API endpoints.',
+    author='Bellhops',
+    author_email='tech@getbellhops.com',
+    url='https://github.com/bellhops/django-crud-filters',
+    download_url='https://github.com/bellhops/django-crud-filters/dist/crud_filters-0.1.3.tar.gz',
+    keywords=['django', 'authorization', 'api', 'security'],
+    classifiers=[],
+    install_requires=['Django', 'djangorestframework', 'djangorestframework-expiring-authtoken', 'django-dynamic-fixture', 'Pillow'],
+)
