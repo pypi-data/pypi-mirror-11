4f6e9e12454485fe5986d5291068680dd504fb9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:20 +0000

    Add package brick_wall_build/0.0.1a/brick_wall_build-0.0.1a.tar.gz

---
+++ packages/brick_wall_build/brick_wall_build-0.0.1a.tar.gz/brick_wall_build-0.0.1a/brick_wall_build/__init__.py
@@ -0,0 +1,13 @@
+"""
+Lightweight Python Build Tool
+"""
+
+__version__ = "0.0.1a"
+__license__ = "MIT License"
+__contact__ = "https://github.com/IndeedTokyo-BrickWall/brick-wall-build"
+from ._brick_wall_build import task, main, set_artifact_path, set_tmp_path, run
+import pkgutil
+
+__path__ = pkgutil.extend_path(__path__,__name__)
+
+__all__ = ["task",  "main", "set_artifact_path", "set_tmp_path", "run"]
