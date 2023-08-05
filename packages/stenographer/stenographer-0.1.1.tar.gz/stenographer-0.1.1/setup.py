3d5c4f02dc294d60c2449c08a08ec046c17f5d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:58 +0000

    Add package stenographer/0.1.1/stenographer-0.1.1.tar.gz

---
+++ packages/stenographer/stenographer-0.1.1.tar.gz/stenographer-0.1.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+setup(
+    name='stenographer',
+    description='An HTTP interaction recorder for Twisted Web',
+    version='0.1.1',
+    author='Kevin Xiwei Zheng',
+    author_email='kxz+stenographer@room208.org',
+    url='https://github.com/kxz/stenographer',
+    license='X11',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: Twisted',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2 :: Only',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Testing'],
+    packages=find_packages(),
+    package_data={
+        'stenographer': [
+            'test/fixtures/cassettes/*']},
+    install_requires=[
+        'Twisted>=14.0.0'])
---
+++ packages/stenographer/stenographer-0.1.1.tar.gz/stenographer-0.1.1/stenographer/__init__.py
@@ -0,0 +1,7 @@
+"""Stenographer, an HTTP interaction recorder for Twisted Web."""
+
+
+from .agent import CassetteAgent
+
+
+__version__ = '0.1.1'
