cd6da60322b752b3d0cb6ef030339d37ec0c88eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:05 +0000

    Add package digital_ocean_api/0.0.8/digital_ocean_api-0.0.8.tar.gz

---
+++ packages/digital_ocean_api/digital_ocean_api-0.0.8.tar.gz/digital_ocean_api-0.0.8/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(name='digital_ocean_api',
+      version='0.0.8',
+      description='DigitalOcean API wrapper. It is for personal use only, not generic API.',
+      author='Tyler Long',
+      author_email='tyler4long@gmail.com',
+      packages=['digital_ocean_api'],
+      install_requires=['requests>=2.7.0'],
+      url='https://github.com/tylingsoft',
+      license='GPL',
+)
