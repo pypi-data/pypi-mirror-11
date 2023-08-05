1ea058ad5e40fed8dbf7fb5c46f8da0b2bda80f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:14 +0000

    Add package sns/0.1/sns-0.1.tar.gz

---
+++ packages/sns/sns-0.1.tar.gz/sns-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'sns',
+  packages = ['sns'], # this must be the same as the name above
+  version = '0.1',
+  description = 'SNS utils',
+  author = 'Brett Cullen',
+  author_email = 'brett.cullen@digitalglobe.com',
+  url = 'https://github.com/Brett55/sns_test', # use the URL to the github repo
+  download_url = 'https://github.com/Brett55/sns_test/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
