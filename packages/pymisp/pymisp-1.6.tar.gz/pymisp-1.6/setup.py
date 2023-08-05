11161534c37c0f5063e5c6eaef4237a6a299c83e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:02 +0000

    Add package pymisp/1.6/pymisp-1.6.tar.gz

---
+++ packages/pymisp/pymisp-1.6.tar.gz/pymisp-1.6/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='pymisp',
+    version='1.6',
+    author='Raphaël Vinot',
+    author_email='raphael.vinot@circl.lu',
+    maintainer='Raphaël Vinot',
+    url='https://github.com/MISP/PyMISP',
+    description='Python API for MISP.',
+    packages=['pymisp'],
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Telecommunications Industry',
+        'Programming Language :: Python',
+        'Topic :: Security',
+        'Topic :: Internet',
+    ],
+    install_requires=['requests'],
+)
