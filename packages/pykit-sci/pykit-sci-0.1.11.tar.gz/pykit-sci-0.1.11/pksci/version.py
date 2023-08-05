612d6395b45144b84e66416e58221feab58fa3ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:48 +0000

    Add package pykit-sci/0.1.11/pykit-sci-0.1.11.tar.gz

---
+++ packages/pykit-sci/pykit-sci-0.1.11.tar.gz/pykit-sci-0.1.11/pksci/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM PKSCI SETUP.PY
+short_version = '0.1.11'
+version = '0.1.11'
+full_version = '0.1.11'
+git_revision = 'e13b03679c17778299201694f3c4228ed39451f7'
+release = True
+
+if not release:
+    version = full_version
