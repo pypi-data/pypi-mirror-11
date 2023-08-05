9605bccb1577fd9ef78fc79280b15f31836c760f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:03 +0000

    Add package ep/0.1.8/ep-0.1.8-py2-none-any.whl

---
+++ packages/ep/ep-0.1.8-py2-none-any.whl/ep/npm.py
@@ -0,0 +1,19 @@
+import os
+
+from .shell import error, run
+
+
+class Npm(object):
+
+    def __init__(self, spec):
+        self._file = 'package.json'
+        self._spec = spec
+
+    def check(self):
+        file_check = os.path.exists(self._file)
+        if not file_check:
+            error('File not found in path: {0}'.format(self._file))
+        return file_check
+
+    def setup(self):
+        run('npm install')
