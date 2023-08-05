f50ca460a25ca9d72f41be05cbf9e866350046db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:28 +0000

    Add package feedback/0.1-2/feedback-0.1_2-py2.py3-none-any.whl

---
+++ packages/feedback/feedback-0.1_2-py2.py3-none-any.whl/feedback/__init__.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+__version__ = '0.1-2'
+from render import _Render
+
+class Feedback(object):
+    def __init__(self, **kwargs):
+        
+        # Render manager / handler
+        self.render   = _Render(**kwargs)
+    
+    def _set_message(self, msg):
+        """
+        Set and store the message.
+        """
+        if isinstance(msg, str):
+            _msg = msg
+        if isinstance(msg, list):
+            indent   = ' ' * 3
+            _msg = '\n\n'
+            for line in msg:
+                _msg += '{}{}\n'.format(indent, line)
+            _msg += '\n'
+        
+        # Pass the message to the rendering handler
+        self.render.set_message(_msg)
+    
+    def get_response(self, key, default=None):
+        """
+        Return then clear the response variable.
+        """
+        return self.render.get_response(key, default)
+        
+    def success(self, msg): 
+        """
+        Display a success message on the screen.
+        """
+        self._set_message(msg)
+        self.render.show('SUCCESS', color='green')
+        
+    def warn(self, msg): 
+        """
+        Display a warning message on the screen.
+        """
+        self._set_message(msg)
+        self.render.show('WARNING', color='yellow')
+        
+    def error(self, msg): 
+        """
+        Display an error message on the screen.
+        """
+        self.render.show('ERROR', color='red')
+
+    def block(self, msg, label='INFO'):
+        """
+        Display a block of indented text.
+        """
+        self._set_message(msg)
+        self.render.show(label, newline=False, color='white')
+
+    def input(self, msg, key, secure=False, confirm=False, yes_no=False):
+        """
+        Display an input prompt on the screen.
+        """
+        self._set_message(msg)
+        self.render.show('INPUT', color='white', input_key=key, input_get=True, input_secure=secure, input_confirm=confirm, input_yn=yes_no)
+
+    def info(self, msg): 
+        """
+        Display an informational message on the screen.
+        """
+        self._set_message(msg)
+        self.render.show('INFO', color='white')
