a0433f80469ea04ad9dac52481a1be04135b4b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:53 +0000

    Add package netdisco/0.2/netdisco-0.2-py2.py3-none-any.whl

---
+++ packages/netdisco/netdisco-0.2-py2.py3-none-any.whl/netdisco/discoverables/belkin_wemo.py
@@ -0,0 +1,20 @@
+""" Discovers Belkin Wemo devices. """
+
+from . import SSDPDiscoverable
+
+
+class Discoverable(SSDPDiscoverable):
+    """ Adds support for discovering Belkin WeMo platform devices. """
+
+    def info_from_entry(self, entry):
+        """ Returns most important info from a uPnP entry. """
+        device = entry.description.find('device')
+
+        return (device.find('friendlyName').text,
+                device.find('modelName').text,
+                entry.values['location'])
+
+    def get_entries(self):
+        """ Returns all Belkin Wemo entries. """
+        return self.find_by_device_description(
+            {'manufacturer': 'Belkin International Inc.'})
