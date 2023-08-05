f5b032441e4bbcb957127e6651bc30d416e8bf6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:20 +0000

    Add package sample-lines/0.0.4/sample-lines-0.0.4.tar.gz

---
+++ packages/sample-lines/sample-lines-0.0.4.tar.gz/sample-lines-0.0.4/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+
+setup(name='sample-lines',
+      author='Thomas Levine',
+      author_email='_@thomaslevine.com',
+      description='Sample lines from a file.',
+      url='http://dada.pink/sample-lines/',
+      py_modules=['sample_lines'],
+      version='0.0.4',
+      license='LGPL',
+      entry_points = {'console_scripts': ['sample-lines = sample_lines:main']},
+)
