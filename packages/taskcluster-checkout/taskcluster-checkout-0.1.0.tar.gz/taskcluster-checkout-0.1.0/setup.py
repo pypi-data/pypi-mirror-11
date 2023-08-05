56f22f44b1870bf4ae7169e845605df4f4440b36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:51 +0000

    Add package taskcluster-checkout/0.1.0/taskcluster-checkout-0.1.0.tar.gz

---
+++ packages/taskcluster-checkout/taskcluster-checkout-0.1.0.tar.gz/taskcluster-checkout-0.1.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='taskcluster-checkout',
+      package=['taskcluster-checkout'],
+      version='0.1.0',
+      description='Checkout cached repositories served by TaskCluster',
+      author='Anthony Miyaguchi',
+      author_email='amiyaguchi@mozilla.com',
+      url='https://github.com/acmiyaguchi/taskcluster-checkout.git',
+      download_url='https://github.com/acmiyaguchi/taskcluster-checkout/tarball/0.1.0',
+      py_modules=['checkout'],
+      license='MPL2',
+      install_requires=['python-hglib'],
+      extras_require={
+          'test': [
+              'nose',
+              'mock',
+              'pep8',
+              'pyflakes',
+              'coverage',
+              'testfixtures',
+          ]
+      },
+      entry_points={
+          'console_scripts': ['tc-checkout = checkout:main'],
+      })
