c418b2869b788aec0d2c4aa69ec7e6b380618467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:56 +0000

    Add package planet/0.0.1/planet-0.0.1.tar.gz

---
+++ packages/planet/planet-0.0.1.tar.gz/planet-0.0.1/planet/api/__init__.py
@@ -0,0 +1,337 @@
+# Copyright 2015 Planet Labs, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from datetime import datetime
+import logging
+import os
+import re
+
+from requests_futures.sessions import FuturesSession
+
+_logger = logging.getLogger(__name__)
+
+ENV_KEY = 'PL_API_KEY'
+
+_ISO_FMT = '%Y-%m-%dT%H:%M:%S.%f+00:00'
+
+
+class APIException(Exception):
+    '''also used as placeholder for unexpected response status_code'''
+    pass
+
+
+class BadQuery(APIException):
+    pass
+
+
+class InvalidAPIKey(APIException):
+    pass
+
+
+class NoPermission(APIException):
+    pass
+
+
+class MissingResource(APIException):
+    pass
+
+
+class OverQuota(APIException):
+    pass
+
+
+class ServerError(APIException):
+    pass
+
+
+def _check_status(response):
+    '''check the status of the response and if needed raise an APIException'''
+    status = response.status_code
+    if status == 200:
+        return
+    exception = {
+        400: BadQuery,
+        401: InvalidAPIKey,
+        403: NoPermission,
+        404: MissingResource,
+        429: OverQuota,
+        500: ServerError
+    }.get(status, None)
+
+    if exception:
+        raise exception(response.text)
+
+    raise APIException('%s: %s' % (status, response.text))
+
+
+def _get_filename(response):
+    cd = response.headers.get('content-disposition', '')
+    match = re.search('filename="?([^"]+)"?', cd)
+    if match:
+        return match.group(1)
+
+
+def _find_api_key():
+    return os.getenv(ENV_KEY)
+
+
+def write_to_file(directory=None, callback=None):
+    def writer(body):
+        file = os.path.join(directory, body.name) if directory else None
+        body.write(file, callback)
+    return writer
+
+
+def strp_timestamp(value):
+    return datetime.strptime(value, _ISO_FMT)
+
+
+def strf_timestamp(when):
+    return datetime.strftime(when, _ISO_FMT)
+
+
+class Response(object):
+
+    def __init__(self, request, dispatcher):
+        self.request = request
+        self._dispatcher = dispatcher
+        self._body = None
+        self._future = None
+
+    def _create_body(self, response):
+        return self.request.body_type(response, self._dispatcher)
+
+    def get_body(self):
+        if self._body is None:
+            resp = self._dispatcher._dispatch(self.request)
+            self._body = self._create_body(resp)
+        return self._body
+
+    def _async_callback(self, session, response):
+        _check_status(response)
+        self._body = self._create_body(response)
+        self._handler(self._body)
+        if self._await:
+            self._await(self._body)
+
+    def get_body_async(self, handler, await=None):
+        if self._future is None:
+            self._handler = handler
+            self._await = await
+            self._future = self._dispatcher._dispatch_async(
+                self.request, self._async_callback
+            )
+
+    def await(self):
+        if self._future:
+            self._future.result()
+            return self._body
+
+
+class Request(object):
+
+    def __init__(self, url, params=None, body_type=Response):
+        self.url = url
+        self.params = params
+        self.body_type = body_type
+
+
+class Body(object):
+
+    def __init__(self, http_response, dispatcher):
+        self.response = http_response
+        self._dispatcher = dispatcher
+        self.size = int(self.response.headers.get('content-length', 0))
+        self.name = _get_filename(self.response)
+
+    def __len__(self):
+        return self.size
+
+    def __iter__(self):
+        return (chunk for chunk in self.response.iter_content(chunk_size=8192))
+
+    def last_modified(self):
+        lm = self.response.headers['last-modified']
+        return datetime.strptime(lm, '%a, %d %b %Y %H:%M:%S GMT')
+
+    def get_raw(self):
+        return self.response.content.decode('utf-8')
+
+    def _write(self, fp, callback):
+        total = 0
+        if not callback:
+            callback = lambda x: None
+        for chunk in self:
+            fp.write(chunk)
+            size = len(chunk)
+            total += size
+            callback(size)
+        # seems some responses don't have a content-length header
+        if self.size is 0:
+            self.size = total
+        callback(self)
+
+    def write(self, file=None, callback=None):
+        if not file:
+            file = self.name
+        if not file:
+            raise ValueError('no file name provided or discovered in response')
+        if isinstance(file, basestring):
+            with open(file, 'wb') as fp:
+                self._write(fp, callback)
+        else:
+            self._write(file, callback)
+
+
+class JSON(Body):
+
+    def get(self):
+        return self.response.json()
+
+
+class Scenes(JSON):
+
+    def next(self):
+        links = self.get()['links']
+        next = links.get('next', None)
+        if next:
+            request = Request(next, body_type=Scenes)
+            return self._dispatcher.response(request).get_body()
+
+    def iter(self, pages=None):
+        pages = int(10e10) if pages is None else pages
+        page = self
+        if pages > 0:
+            yield page
+            pages -= 1
+        while pages > 0:
+            page = page.next()
+            yield page
+            pages -= 1
+
+
+class Image(Body):
+    pass
+
+
+class Dispatcher(object):
+
+    def __init__(self, api_key, workers=4):
+        self.session = FuturesSession(max_workers=workers)
+        self.set_api_key(api_key)
+
+    def set_api_key(self, api_key):
+        if api_key:
+            self.session.headers.update({
+                'Authorization': 'api-key %s' % api_key
+            })
+        elif 'authorization' in self.session.headers:
+            self.session.headers.pop('authorization')
+
+    def response(self, request):
+        return Response(request, self)
+
+    def _dispatch_async(self, request, callback):
+        if not 'authorization' in self.session.headers:
+            raise InvalidAPIKey('No API key provided')
+        return self.session.get(request.url, params=request.params,
+                                stream=True, background_callback=callback)
+
+    def _dispatch(self, request, callback=None):
+        response = self._dispatch_async(request, callback).result()
+        _check_status(response)
+        return response
+
+
+class Client(object):
+
+    def __init__(self, api_key=None, base_url='https://api.planet.com/v0/',
+                 workers=4):
+        api_key = api_key or _find_api_key()
+        self.base_url = base_url
+        self.dispatcher = Dispatcher(api_key, workers)
+
+    def _get(self, path, body_type=JSON, params=None, callback=None):
+        if path.startswith('http'):
+            url = path
+        else:
+            url = self.base_url + path
+        request = Request(url, params, body_type)
+        response = self.dispatcher.response(request)
+        if callback:
+            response.get_body_async(callback)
+        return response
+
+    def _download_many(self, paths, params, callback):
+        return [self._get(path, params=params, callback=callback)
+                for path in paths]
+
+    def list_scene_types(self):
+        return self._get('scenes').get_body()
+
+    def get_scenes_list(self, scene_type='ortho', order_by=None, count=None,
+                        intersects=None, **filters):
+        params = {
+            'order_by': order_by,
+            'count': count,
+            'intersects': intersects
+        }
+        params.update(**filters)
+        return self._get('scenes/%s' % scene_type,
+                         Scenes, params=params).get_body()
+
+    def get_scene_metadata(self, scene_id, scene_type='ortho'):
+        """
+        Get metadata for a given scene.
+
+        .. todo:: Generalize to accept multiple scene ids.
+        """
+        return self._get('scenes/%s/%s' % (scene_type, scene_id)).get_body()
+
+    def fetch_scene_geotiffs(self, scene_ids, scene_type='ortho',
+                             product='visual', callback=None):
+        params = {
+            'product': product
+        }
+        paths = ['scenes/%s/%s/full' % (scene_type, sid) for sid in scene_ids]
+        return self._download_many(paths, params, callback)
+
+    def fetch_scene_thumbnails(self, scene_ids, scene_type='ortho', size='md',
+                               fmt='png', callback=None):
+        params = {
+            'size': size,
+            'format': fmt
+        }
+        paths = ['scenes/%s/%s/thumb' % (scene_type, sid) for sid in scene_ids]
+        return self._download_many(paths, params, callback)
+
+
+    def list_mosaics(self):
+        """
+        List all mosaics.
+        
+        .. todo:: Pagination
+        """
+        return self._get('mosaics').get_body()
+
+
+    def get_mosaic(self, name):
+        """
+        Get metadata for a given mosaic.
+        
+        :param name:
+            Mosaic name as returned by `list_mosaics`.
+        """
+        return self._get('mosaics/%s' % name).get_body()
+
---
+++ packages/planet/planet-0.0.1.tar.gz/planet-0.0.1/planet/scripts/__init__.py
@@ -0,0 +1,343 @@
+# Copyright 2015 Planet Labs, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import logging
+
+import click
+
+import planet
+from planet import api
+from os import path
+from requests.packages.urllib3 import exceptions as urllib3exc
+import sys
+import time
+import warnings
+
+client_params = {}
+
+
+def client():
+    return api.Client(**client_params)
+
+
+pretty = click.option('-pp', '--pretty', default=False, is_flag=True)
+scene_type = click.option('-s', '--scene-type', default='ortho')
+dest_dir = click.option('-d', '--dest', help='Destination directory',
+                        type=click.Path(file_okay=False, resolve_path=True))
+
+
+# monkey patch warnings module to hide InsecurePlatformWarning - the warning
+# notes 'may cause certain SSL connections to fail' so it doesn't seem to
+# introduce any vulnerabilities
+# we capture the warning if present and present this if any SSLError is caught
+# just in case this configuration is an issue
+_insecure_warning = []
+showwarning = warnings.showwarning
+
+
+def hack(message, category, filename, lineno):
+    if category is urllib3exc.InsecurePlatformWarning:
+        if len(_insecure_warning) == 0:
+            _insecure_warning.append(message)
+        return
+    showwarning(message, category, filename, lineno)
+warnings.showwarning = hack
+
+
+def configure_logging(verbosity):
+    '''configure logging via verbosity level of between 0 and 2 corresponding
+    to log levels warning, info and debug respectfully.'''
+    log_level = max(logging.DEBUG, logging.WARNING - logging.DEBUG*verbosity)
+    logging.basicConfig(stream=sys.stderr, level=log_level)
+
+
+def click_exception(ex):
+    if type(ex) is api.APIException:
+        raise click.ClickException('Unexpected response: %s' % str(ex))
+    msg = "%s: %s" % (type(ex).__name__, str(ex))
+    raise click.ClickException(msg)
+
+
+def call_and_wrap(func, *args, **kw):
+    '''call the provided function and wrap any API exception with a click
+    exception. this means no stack trace is visible to the user but instead
+    a (hopefully) nice message is provided.
+    note: could be a decorator but didn't play well with click
+    '''
+    try:
+        return func(*args, **kw)
+    except api.APIException as ex:
+        click_exception(ex)
+    except urllib3exc.SSLError:
+        # see monkey patch above re InsecurePlatformWarning
+        if _insecure_warning:
+            click.echo(click.style(str(_insecure_warning[0]), fg='red'))
+        raise
+
+
+def check_futures(futures):
+    for f in futures:
+        try:
+            f.await()
+        except api.InvalidAPIKey as invalid:
+            click_exception(invalid)
+        except api.APIException as other:
+            click.echo('WARNING %s' % other.message)
+
+
+def summarize_throughput(bytes, start_time):
+    elapsed = time.time() - start_time
+    mb = float(bytes) / (1024 * 1024)
+    click.echo('transferred %s bytes in %.2f seconds (%.2f MB/s)' %
+               (bytes, elapsed, mb/elapsed))
+
+
+def total_bytes(responses):
+    return sum([len(r.get_body()) for r in responses])
+
+
+@click.group()
+@click.option('-w', '--workers', default=4,
+              help=('The number of concurrent downloads when requesting '
+                    'multiple scenes.'))
+@click.option('-v', '--verbose', count=True)
+@click.option('-k', '--api-key',
+              help='Valid API key - or via env variable %s' % api.ENV_KEY)
+@click.option('-u', '--base-url', help='Optional for testing')
+@click.version_option(version=planet.__version__, message='%(version)s')
+def cli(verbose, api_key, base_url, workers):
+    '''Planet API Client'''
+
+    configure_logging(verbose)
+
+    client_params.clear()
+    client_params['api_key'] = api_key
+    client_params['workers'] = workers
+    if base_url:
+        client_params['base_url'] = base_url
+
+
+@cli.command('help')
+@click.argument("command", default="")
+@click.pass_context
+def help(context, command):
+    if command:
+        cmd = cli.commands.get(command, None)
+        if cmd:
+            click.echo(cmd.get_help(context))
+        else:
+            raise click.ClickException('no command: %s' % command)
+    else:
+        click.echo(cli.get_help(context))
+
+
+@cli.command('list-scene-types')
+def list_scene_types():
+    '''List all scene types.'''
+    click.echo(call_and_wrap(client().list_scene_types).get_raw())
+
+
+@pretty
+@scene_type
+@cli.command('search')
+@click.argument("aoi", default="-", required=False)
+@click.option('--count', type=click.INT, required=False,
+              help="Set the number of returned scenes.")
+@click.option("--where", nargs=3, multiple=True,
+              help=("Provide additional search criteria. See "
+                    "https://www.planet.com/docs/v0/scenes/#metadata for "
+                    " search metadata fields."))
+def get_scenes_list(scene_type, pretty, aoi, count, where):
+    '''Get a list of scenes'''
+
+    if aoi == "-":
+        src = click.open_file('-')
+        if not src.isatty():
+            lines = src.readlines()
+            aoi = ''.join([line.strip() for line in lines])
+        else:
+            aoi = None
+
+    if where:
+        conditions = {
+            "%s.%s" % condition[0:2]: condition[2]
+            for condition in where
+        }
+    else:
+        conditions = {}
+
+    res = call_and_wrap(client().get_scenes_list, scene_type=scene_type,
+                        intersects=aoi, count=count, **conditions).get_raw()
+    if pretty:
+        res = json.dumps(json.loads(res), indent=2)
+    click.echo(res)
+
+
+@pretty
+@scene_type
+@click.argument('scene_id', nargs=1)
+@cli.command('metadata')
+def metadata(scene_id, scene_type, pretty):
+    '''Get scene metadata'''
+
+    res = call_and_wrap(client().get_scene_metadata,
+                        scene_id, scene_type).get_raw()
+
+    if pretty:
+        res = json.dumps(json.loads(res), indent=2)
+
+    click.echo(res)
+
+
+@scene_type
+@dest_dir
+@click.argument('scene_ids', nargs=-1)
+@click.option('--product',
+              type=click.Choice(
+                  ["band_%d" % i for i in range(1, 12)] +
+                  ['visual', 'analytic', 'qa']
+              ), default='visual')
+@cli.command('download')
+@click.pass_context
+def fetch_scene_geotiff(ctx, scene_ids, scene_type, product, dest):
+    '''Fetch full scene image(s)'''
+
+    if len(scene_ids) == 0:
+        src = click.open_file('-')
+        if not src.isatty():
+            scene_ids = map(lambda s: s.strip(), src.readlines())
+        else:
+            click.echo(ctx.get_usage())
+
+    start_time = time.time()
+    futures = client().fetch_scene_geotiffs(scene_ids, scene_type, product,
+                                            api.write_to_file(dest))
+    check_futures(futures)
+    summarize_throughput(total_bytes(futures), start_time)
+
+
+@scene_type
+@dest_dir
+@click.argument("scene-ids", nargs=-1)
+@click.option('--size', type=click.Choice(['sm', 'md', 'lg']), default='md')
+@click.option('--format', 'fmt', type=click.Choice(['png', 'jpg', 'jpeg']),
+              default='png')
+@cli.command('thumbnails')
+def fetch_scene_thumbnails(scene_ids, scene_type, size, fmt, dest):
+    '''Fetch scene thumbnail(s)'''
+
+    if len(scene_ids) == 0:
+        src = click.open_file('-')
+        if not src.isatty():
+            scene_ids = map(lambda s: s.strip(), src.readlines())
+
+    futures = client().fetch_scene_thumbnails(scene_ids, scene_type, size, fmt,
+                                              api.write_to_file(dest))
+    check_futures(futures)
+
+
+@scene_type
+@click.argument("destination")
+@click.option("--limit", default=-1, help='limit scene syncing')
+@cli.command('sync')
+def sync(destination, scene_type, limit):
+    '''Synchronize a directory to a specified AOI'''
+    if not path.exists(destination) or not path.isdir(destination):
+        raise click.ClickException('destination must exist and be a directory')
+    aoi_file = path.join(destination, 'aoi.geojson')
+    if not path.exists(aoi_file):
+        raise click.ClickException(
+            'provide an aoi.geojson file in "%s"' % destination
+        )
+    aoi = None
+    with open(aoi_file) as fp:
+        aoi = fp.read()
+    sync_file = path.join(destination, 'sync.json')
+    if path.exists(sync_file):
+        with open(sync_file) as fp:
+            sync = json.loads(fp.read())
+    else:
+        sync = {}
+    filters = {}
+    if 'latest' in sync:
+        filters['acquired.gt'] = sync['latest']
+    start_time = time.time()
+    transferred = 0
+    _client = client()
+    res = call_and_wrap(_client.get_scenes_list, scene_type=scene_type,
+                        intersects=aoi, count=100, order_by='acquired asc',
+                        **filters)
+    click.echo('total scenes to fetch: %s' % res.get()['count'])
+    if limit > 0:
+        click.echo('limiting to %s' % limit)
+    counter = type('counter', (object,),
+                   {'remaining': res.get()['count'] if limit < 1 else limit})()
+    latest = None
+
+    def progress_callback(arg):
+        if not isinstance(arg, int):
+            counter.remaining -= 1
+            click.echo('downloaded %s, remaining %s' %
+                       (arg.name, counter.remaining))
+    write_callback = api.write_to_file(destination, progress_callback)
+    for page in res.iter():
+        features = page.get()['features'][:counter.remaining]
+        if not features:
+            break
+        ids = [f['id'] for f in features]
+        futures = _client.fetch_scene_geotiffs(
+            ids, scene_type, callback=write_callback
+        )
+        for f in features:
+            metadata = path.join(destination, '%s_metadata.json' % f['id'])
+            with open(metadata, 'wb') as fp:
+                fp.write(json.dumps(f, indent=2))
+        check_futures(futures)
+        transferred += total_bytes(futures)
+        recent = max([
+            api.strp_timestamp(f['properties']['acquired']) for f in features]
+        )
+        latest = max(latest, recent) if latest else recent
+        if counter.remaining <= 0:
+            break
+    if latest:
+        sync['latest'] = api.strf_timestamp(latest)
+        with open(sync_file, 'wb') as fp:
+            fp.write(json.dumps(sync, indent=2))
+    if transferred:
+        summarize_throughput(transferred, start_time)
+
+
+#
+#   Mosaics CLI
+#
+
+
+@cli.command('mosaics')
+def list_mosaics():
+    """
+    List all mosaics
+    """
+    click.echo(call_and_wrap(client().list_mosaics).get_raw())
+
+@cli.command('mosaic')
+@click.argument('mosaic_name', nargs=1)
+def get_mosaic(mosaic_name):
+    """
+    Describe a specified mosaic
+    """
+    click.echo(call_and_wrap(client().get_mosaic, mosaic_name).get_raw())
+
+
