cd0550998f662e0b9ac6292fde0e856a7b9af0d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:22 +0000

    Add package website_status/0.0.3/website_status-0.0.3.tar.gz

---
+++ packages/website_status/website_status-0.0.3.tar.gz/website_status-0.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='website_status',
+      version='0.0.3',
+      description='Check whether a website is up or down',
+      long_description='Check whether a website is up or down with just one function call',
+      keywords='website status website down website up',
+      url='http://github.com/sahildua2305/website_status',
+      author='Sahil Dua',
+      author_email='sahildua2305@gmail.com',
+      license='MIT',
+      packages=['website_status'],
+      install_requires=[
+            'requests',
+      ],
+      zip_safe=False)
