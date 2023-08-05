86f3d8ab8f4a0fa2cb6d050715ff76d9d83f223c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:02 +0000

    Add package miniorm/1.2.8/miniorm-1.2.8.tar.gz

---
+++ packages/miniorm/miniorm-1.2.8.tar.gz/miniorm-1.2.8/setup.py
@@ -0,0 +1,15 @@
+#coding: utf-8
+from setuptools import find_packages, setup
+
+setup(name='miniorm',
+      version='1.2.8',
+      keywords='mini mysql db orm :: miniorm',
+      description='mini mysql db orm :: miniorm',
+      long_description=open("README.rst", 'rb').read(),
+      packages=find_packages(exclude=["*.tests", "*.tests.*"]),
+      author="inwn",
+      author_email="ininwn@gmail.com",
+      include_package_data=True,
+      license="MIT",
+      zip_safe=False,
+      )
