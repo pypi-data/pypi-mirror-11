4a7a8d07d0b395f4d741bc96dac2c658504d56db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:11 +0000

    Add package kraken/0.4.5.dev1/kraken-0.4.5.dev1-py2.py3-none-any.whl

---
+++ packages/kraken/kraken-0.4.5.dev1-py2.py3-none-any.whl/kraken/lib/exceptions.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+kraken.lib.exceptions
+~~~~~~~~~~~~~~~~~~~~~
+
+All custom exceptions raised by kraken's modules and packages. Packages should
+always define their exceptions here.
+"""
+
+from __future__ import absolute_import, division, print_function
+from __future__ import unicode_literals
+
+
+class KrakenRecordException(Exception):
+
+    def __init__(self, message=None):
+        Exception.__init__(self, message)
+
+
+class KrakenInvalidModelException(Exception):
+
+    def __init__(self, message=None):
+        Exception.__init__(self, message)
+
+class KrakenInputException(Exception):
+
+    def __init__(self, message=None):
+        Exception.__init__(self, message)
