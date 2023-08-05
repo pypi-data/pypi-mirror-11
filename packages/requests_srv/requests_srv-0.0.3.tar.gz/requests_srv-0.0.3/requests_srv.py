2f7338dd49e5f73bdca5e6d23f1184cde3adf413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:07 +0000

    Add package requests_srv/0.0.3/requests_srv-0.0.3.tar.gz

---
+++ packages/requests_srv/requests_srv-0.0.3.tar.gz/requests_srv-0.0.3/requests_srv.py
@@ -0,0 +1,56 @@
+from urlparse import urlparse, urlunparse
+
+import dns.resolver
+import dns.exception
+from requests import Session, ConnectionError
+from requests.adapters import HTTPAdapter
+
+
+class SRVResolverHTTPAdapter(HTTPAdapter):
+    def __init__(self, dns_hosts=None, dns_port=None, **kwargs):
+        self.dns_hosts = dns_hosts
+        self.dns_port = dns_port
+        self.resolver = dns.resolver.Resolver()
+        if dns_hosts is not None:
+            self.resolver.nameservers = dns_hosts
+        if dns_port is not None:
+            self.resolver.port = dns_port
+        super(SRVResolverHTTPAdapter, self).__init__(**kwargs)
+
+    def get_connection(self, url, proxies=None):
+        parsed = urlparse(url)
+        host, port = self.__resolve(parsed.netloc)
+        redirected_url = urlunparse((
+            parsed.scheme,
+            '%s:%d' % (host, port),
+            parsed.path,
+            parsed.params,
+            parsed.query,
+            parsed.fragment
+        ))
+        return super(SRVResolverHTTPAdapter, self).get_connection(redirected_url, proxies=proxies)
+
+    def __resolve(self, service):
+        try:
+            answers = self.resolver.query(service, 'SRV')
+        except dns.exception.DNSException as e:
+            raise ConnectionError('DNS error: ' + e.__class__.__name__)
+        return answers[0].target, answers[0].port
+
+session = Session()
+session.mount('http://_', SRVResolverHTTPAdapter())
+session.mount('https://_', SRVResolverHTTPAdapter())
+
+request = session.request
+head = session.head
+get = session.get
+post = session.post
+put = session.put
+patch = session.patch
+delete = session.delete
+
+
+if __name__ == '__main__':
+    from sys import argv
+    response = get(argv[1])
+    print response.text
---
+++ packages/requests_srv/requests_srv-0.0.3.tar.gz/requests_srv-0.0.3/setup.py
@@ -0,0 +1,26 @@
+# encoding: utf8
+
+import setuptools
+
+setuptools.setup(
+    name="requests_srv",
+    version="0.0.3",
+    py_modules=["requests_srv"],
+    author=u"Paweł Stiasny",
+    author_email="pawelstiasny@gmail.com",
+    url="http://github.com/pstiasny/requests-srv",
+    license="Apache 2.0",
+    description="A wrapper for Requests supporting DNS SRV records.",
+    keywords=['requests'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    install_requires=[
+        'requests',
+        'dnspython',
+    ],
+)
