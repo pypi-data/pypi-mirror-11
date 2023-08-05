ad291e9e8ca72114e40738ca4fbcbab211f06358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:58 +0000

    Add package findex_common/0.1.17/findex_common-0.1.17.tar.gz

---
+++ packages/findex_common/findex_common-0.1.17.tar.gz/findex_common-0.1.17/findex_common/exceptions.py
@@ -0,0 +1,39 @@
+import logging
+
+
+class ConfigError(Exception):
+    def __init__(self, message, errors=None):
+        super(ConfigError, self).__init__(message)
+
+        self.errors = errors
+        logging.error(message)
+
+
+class BrowseException(Exception):
+    def __init__(self, message):
+        super(BrowseException, self).__init__(message)
+
+        self.message = message
+        logging.error(message)
+
+
+class SearchException(Exception):
+    def __init__(self, message, errors=None):
+        super(SearchException, self).__init__(message)
+        logging.error(message)
+
+
+class SearchEmptyException(Exception):
+    def __init__(self, message):
+        super(SearchEmptyException, self).__init__(message)
+        logging.error(message)
+
+
+class AmqpParseException(Exception):
+    def __init__(self, message, errors=None):
+        super(AmqpParseException, self).__init__(message)
+
+
+class CrawlException(Exception):
+    def __init__(self, message, errors=None):
+        super(CrawlException, self).__init__(message)
