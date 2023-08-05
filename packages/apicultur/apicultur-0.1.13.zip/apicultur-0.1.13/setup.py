e1f12486fab78cf34509d91bff13eecdfa906c43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:23 +0000

    Add package apicultur/0.1.13/apicultur-0.1.13.zip

---
+++ packages/apicultur/apicultur-0.1.13.zip/apicultur-0.1.13/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+from io import open
+
+def get_version():
+    with open(path.join('apicultur', '__init__.py'), encoding='utf-8') as f:
+        for line in f:
+            if line.startswith('__version__ ='):
+                return line.split('=')[1].strip().strip('"\'')
+
+
+setup(
+    name='apicultur',
+    version=get_version(),
+    description='Python client to Apicultur API services',
+    long_description='Python client to Apicultur API services',
+    url='https://github.com/jgsogo/apicultur-python',
+    author='Javier G. Sogo',
+    author_email='jgsogo@gmail.com',
+
+    #license='',
+
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+
+        # Pick your license as you wish (should match "license" above)
+        # 'License :: OSI Approved :: MIT License',
+
+        'Operating System :: OS Independent',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    keywords='api linguistic apicultur',
+    packages=find_packages(),
+    #packages = ['apicultur'],
+    install_requires=['requests'],
+)
