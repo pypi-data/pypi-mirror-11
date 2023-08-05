6af3d756d31eed401dc865762e380d1a00d661ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:44 +0000

    Add package python-surveilclient/0.13.0/python_surveilclient-0.13.0-py2-none-any.whl

---
+++ packages/python-surveilclient/python_surveilclient-0.13.0-py2-none-any.whl/surveilclient/tests/v2_0/status/test_hosts.py
@@ -0,0 +1,67 @@
+# Copyright 2015 - Savoir-Faire Linux inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import json
+
+import httpretty
+
+from surveilclient.tests.v2_0 import clienttest
+
+
+class TestHosts(clienttest.ClientTest):
+
+    @httpretty.activate
+    def test_list(self):
+        httpretty.register_uri(
+            httpretty.POST, "http://localhost:5311/v2/status/hosts",
+            body='[{"host_name": "host1"}]'
+        )
+
+        hosts = self.client.status.hosts.list()
+
+        self.assertEqual(
+            hosts,
+            [{"host_name": "host1"}]
+        )
+
+    @httpretty.activate
+    def test_get(self):
+        httpretty.register_uri(
+            httpretty.GET, "http://localhost:5311/v2/status/hosts/hostname",
+            body='{"host_name": "host1"}'
+        )
+
+        host = self.client.status.hosts.get("hostname")
+
+        self.assertEqual(
+            host,
+            {"host_name": "host1"}
+        )
+
+    @httpretty.activate
+    def test_submit_host_check_result(self):
+        httpretty.register_uri(
+            httpretty.POST, "http://localhost:5311/v2/status/hosts/localhost"
+                            "/results",
+            body=''
+        )
+
+        self.client.status.hosts.submit_check_result(
+            "localhost", output="someoutput"
+        )
+
+        self.assertEqual(
+            json.loads(httpretty.last_request().body.decode()),
+            {"output": u"someoutput"}
+        )
---
+++ packages/python-surveilclient/python_surveilclient-0.13.0-py2-none-any.whl/surveilclient/v2_0/config/services.py
@@ -0,0 +1,58 @@
+# Copyright 2014-2015 - Savoir-Faire Linux inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import json
+
+from surveilclient.common import surveil_manager
+
+
+class ServicesManager(surveil_manager.SurveilManager):
+    base_url = '/config/services'
+
+    def list(self, query=None, templates=False):
+        """Get a list of hosts."""
+        query = query or {}
+        if templates:
+            if 'filters' not in query:
+                query["filters"] = '{}'
+            filters = json.loads(query["filters"])
+            temp_filter = {"register": ["0"]}
+            if 'is' not in filters:
+                filters["is"] = temp_filter
+            else:
+                filters["is"].update(temp_filter)
+            query['filters'] = json.dumps(query['filters'])
+        resp, body = self.http_client.json_request(
+            ServicesManager.base_url, 'POST',
+            body=query
+        )
+        return body
+
+    def create(self, **kwargs):
+        """Create a new host."""
+        resp, body = self.http_client.json_request(
+            ServicesManager.base_url, 'PUT',
+            body=kwargs
+        )
+        return body
+
+    def delete(self, host_name, service_description):
+        """Delete a service."""
+        resp, body = self.http_client.request(
+            '/config/hosts' + '/'
+            + host_name + '/services/' + service_description,
+            'DELETE',
+            body=''
+        )
+        return body
