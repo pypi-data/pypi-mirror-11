3f244a4ddcc0fdfef2ca1cb6b2c887bca200948d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:36 +0000

    Add package szd/1.0.0/szd-1.0.0.zip

---
+++ packages/szd/szd-1.0.0.zip/szd-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name 		='szd',
+	version 	='1.0.0',
+	py_modules 	=['szd'],
+	author 		='suzidong',
+	author_email 	='1038774332@qq.com',
+	url 		='http:\\www.headfirstlabs.com',
+	description 	='buchenggongshaleni'
+	)
