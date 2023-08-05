7ef6a45bcd9ce5c96f855663909928f506e1999a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:52 +0000

    Add package djmicrosip-mail/1.1.5/djmicrosip_mail-1.1.5.zip

---
+++ packages/djmicrosip-mail/djmicrosip_mail-1.1.5.zip/djmicrosip_mail-1.1.5/djmicrosip_mail/static/js/djmicrosip_mail/saldos_clientes/tarea_preferencias.js
@@ -0,0 +1,60 @@
+
+$("#id_period_start_datetime, #id_period_end_datetime").datetimepicker({'dateFormat':'yy-mm-dd'});
+var fecha = new Date($("#id_next_execution").val());
+
+countdown.setLabels(
+    ' milissegundo| segundo| minuto| hora| dia| semana| mes| año| década| século| milênio',
+    ' milissegundos| segundos| minutos| horas| dias| semanas| meses| años| décadas| séculos| milênios',
+    ' y ',
+    ', ',
+    'ahora');
+
+function get_next_execution_text(){
+    var fecha_inicio = new Date($("#id_period_start_datetime").val());
+    var fecha_siguiente = new Date($("#id_next_execution").val());
+    var fecha_ahora = new Date();
+    var html = "";
+    /* Si nunca se a calculado siguiente ejecución */
+    if ($("#id_next_execution").val() === ""){
+        if (fecha_inicio > fecha_ahora) {
+            fecha_siguiente = fecha_inicio;
+        }else{
+            fecha_siguiente = fecha_ahora;
+        }
+
+
+        if(fecha_siguiente <= fecha_ahora ) {
+            html += "Siguiente ejecución programada <strong>ahora</strong><br/>";
+        }
+        else{
+            html += "Siguiente ejecución programada en <strong>"+ countdown(fecha_siguiente).toString() +"</strong><br/>";
+        }
+    }
+    else {
+        if( fecha_siguiente <= fecha_ahora){
+            fecha_siguiente = fecha_ahora;
+            html += "Siguiente ejecución programada <strong>ahora</strong><br/>";
+        }
+        else{
+            html += "Siguiente ejecución programada en <strong>"+ countdown(fecha_siguiente).toString() +"</strong><br/>";
+        }
+    }
+    html += "<strong>Se repetira cada "+$("#id_period_quantity").val()+" "+ $("#id_period_unit option:selected").text()+"</strong>";
+    return html;
+}
+if ($("#id_next_execution").val() === ""){
+    $("#id_siguiente_ejecucion_alert").hide();
+}
+
+$("#id_btnshow_modal").on("click", function(){
+    setInterval(function () {
+        $(".info_guardar_preview").html(get_next_execution_text());
+    }, 1000);
+    
+    $("#id_modal_seguro").modal();
+
+});
+
+setInterval(function () {
+    $("#id_siguiente_ejecucion_text").html(get_next_execution_text());
+}, 1000);
