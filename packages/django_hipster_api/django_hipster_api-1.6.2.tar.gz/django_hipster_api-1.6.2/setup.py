f75e10b15e6d13bf967d36801cde837d8dc87c01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:49 +0000

    Add package django_hipster_api/1.6.2/django_hipster_api-1.6.2.tar.gz

---
+++ packages/django_hipster_api/django_hipster_api-1.6.2.tar.gz/django_hipster_api-1.6.2/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import codecs
+import os
+from distutils.core import setup
+from setuptools import find_packages
+
+
+def read(fname):
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='django_hipster_api',
+    version='1.6.2',
+    packages=find_packages(),
+    long_description=read("README.md"),
+    install_requires='',
+    url='https://github.com/RustoriaRu/hipster_api',
+    license='MIT',
+    author='vir-mir',
+    keywords='django rest framework',
+    author_email='virmir49@gmail.com',
+    description='wrapper django rest framework',
+    zip_safe=False,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
