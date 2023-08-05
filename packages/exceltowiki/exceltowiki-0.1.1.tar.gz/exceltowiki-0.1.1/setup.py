cf115053fdaa3ccd6bbaffdca1ee991647626c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:28 +0000

    Add package exceltowiki/0.1.1/exceltowiki-0.1.1.tar.gz

---
+++ packages/exceltowiki/exceltowiki-0.1.1.tar.gz/exceltowiki-0.1.1/setup.py
@@ -0,0 +1,19 @@
+'''
+Created on Jul 28, 2015
+
+@author: venkman69@yahoo.com
+'''
+from setuptools import setup
+
+setup(name='exceltowiki',
+      version='0.1.1',
+      description='Convert Excel to Wiki while maintaining formatting',
+      url='http://github.com/venkman69/ExcelToWiki',
+      author='Narayan Natarajan',
+      author_email='venkman69@yahoo.com',
+      license='MIT',
+      packages=['exceltowiki','example'],
+      install_requires=[
+          'openpyxl',
+      ],
+      zip_safe=False)
