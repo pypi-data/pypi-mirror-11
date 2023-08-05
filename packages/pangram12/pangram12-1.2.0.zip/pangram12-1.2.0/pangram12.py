c11546b2f46a4edc56f9ab960fa57f5f512059fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:29 +0000

    Add package pangram12/1.2.0/pangram12-1.2.0.zip

---
+++ packages/pangram12/pangram12-1.2.0.zip/pangram12-1.2.0/pangram12.py
@@ -0,0 +1,24 @@
+# Check if sentence is pangram or not
+def pangram (string):
+    i = 0
+    P = 0
+    string_lower = string.lower ()
+    alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    for i in alphabet:
+        if i not in string_lower:
+            P = 1
+            #print (i)
+        #else :
+            #print (i,"'")
+    print (string)
+    if P == 1:
+        print ('Is not a Pangram')
+    else :
+        print ('Is a Pangram')
+    print (P)
+    
+
+
+    
+    
+    
