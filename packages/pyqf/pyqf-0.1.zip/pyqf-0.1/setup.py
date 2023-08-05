0c3b8a72c1373d52a6ea01e65c494bfde434b53f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:17 +0000

    Add package pyqf/0.1/pyqf-0.1.zip

---
+++ packages/pyqf/pyqf-0.1.zip/pyqf-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyqf',
+  packages = ['pyqf'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A set of tools for practicing quantitative finance',
+  author = 'John Evers',
+  author_email = 'john@wildcatinvestor.com',
+  url = 'https://github.com/jtev222/pyqf', # use the URL to the github repo
+  download_url = 'https://github.com/jtev222/pyqf/tarball/0.1', # I'll explain this in a second
+  keywords = ['hobby'], # arbitrary keywords
+  classifiers = [],
+)
