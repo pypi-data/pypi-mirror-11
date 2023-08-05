2536b618e87c2c397bc74dce456a4b1f70471c1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:24 +0000

    Add package MetPy/0.2.1/MetPy-0.2.1-py2.py3-none-any.whl

---
+++ packages/MetPy/MetPy-0.2.1-py2.py3-none-any.whl/metpy/calc/__init__.py
@@ -0,0 +1,8 @@
+from .basic import *  # noqa
+from .kinematics import *  # noqa
+from .thermo import *  # noqa
+from . import turbulence  # noqa
+__all__ = []
+__all__.extend(basic.__all__)  # pylint: disable=undefined-variable
+__all__.extend(kinematics.__all__)  # pylint: disable=undefined-variable
+__all__.extend(thermo.__all__)  # pylint: disable=undefined-variable
