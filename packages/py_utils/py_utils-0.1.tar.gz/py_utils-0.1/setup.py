c21a9a491aea6ed11787b9a9c9584efee14ece5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:20 +0000

    Add package py_utils/0.1/py_utils-0.1.tar.gz

---
+++ packages/py_utils/py_utils-0.1.tar.gz/py_utils-0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='py_utils',
+    version='0.1',
+    description='Utils for various scenarios',
+    long_description=long_description,
+    url='https://github.com/eyalev/py_utils',
+    author='Eyal Levin',
+    author_email='eyalev@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ],
+    install_requires=['six'],
+)
