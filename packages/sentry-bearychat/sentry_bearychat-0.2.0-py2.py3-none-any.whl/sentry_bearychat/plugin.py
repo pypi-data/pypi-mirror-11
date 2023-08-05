62f6a105c72e72f3131f8159ced1d5ed0167ad3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:56 +0000

    Add package sentry-bearychat/0.2.0/sentry_bearychat-0.2.0-py2.py3-none-any.whl

---
+++ packages/sentry-bearychat/sentry_bearychat-0.2.0-py2.py3-none-any.whl/sentry_bearychat/plugin.py
@@ -0,0 +1,71 @@
+"""
+sentry_bearychat.plugin
+~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2015 by BearyInnovative Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+import sentry_bearychat
+
+from django import forms
+
+import logging
+
+from sentry.plugins.bases import notify
+from sentry.http import safe_urlopen
+from sentry.utils.safe import safe_execute
+
+class BearyChatOptionsForm(notify.NotificationConfigurationForm):
+    webhook = forms.CharField(
+        help_text='Your custom BearyChat webhook URL',
+        widget=forms.TextInput(attrs={'class': 'span8'}))
+
+
+class BearyChatPlugin(notify.NotificationPlugin):
+    author = 'BearyInnovative Team'
+    author_url = 'https://github.com/bearyinnovative'
+    resource_links = [
+        ('Bug Tracker', 'https://github.com/bearyinnovative/sentry-bearychat/issues'),
+        ('Source', 'https://github.com/bearyinnovative/sentry-bearychat'),
+    ]
+    version = sentry_bearychat.VERSION
+    title = 'BearyChat'
+    slug = 'bearychat'
+    description = "Post notifications to a BeayChat channel."
+    conf_title = title
+    conf_key = 'bearychat'
+    project_conf_form = BearyChatOptionsForm
+    logger = logging.getLogger('sentry.plugins.bearychat')
+
+    def is_configured(self, project):
+        return bool(self.get_option('webhook', project))
+
+    # use same data structure as Webhook plugin
+    def get_group_data(self, group, event):
+        data = {
+            'id': str(group.id),
+            'project': group.project.slug,
+            'project_name': group.project.name,
+            'logger': group.logger,
+            'level': group.get_level_display(),
+            'culprit': group.culprit,
+            'message': event.message,
+            'url': group.get_absolute_url(),
+        }
+        data['event'] = dict(event.data or {})
+        data['event']['tags'] = event.get_tags()
+        return data
+
+    def send_webhook(self, url, payload):
+        return safe_urlopen(
+            url=url,
+            json=payload,
+            timeout=3,
+            verify_ssl=False,
+        )
+
+    def notify_users(self, group, event, fail_silently=False):
+        if not self.is_configured(project):
+            return
+        payload = self.get_group_data(group, event)
+        safe_execute(self.send_webhook, webhook, payload)
