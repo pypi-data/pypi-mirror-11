0c4fd9ad4f4ccead459732c211632a4d359046fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:10 +0000

    Add package pygal.js/0.0/pygal.js-0.0.tar.gz

---
+++ packages/pygal.js/pygal.js-0.0.tar.gz/pygal.js-0.0/setup.py
@@ -0,0 +1,20 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='Mounier Florian',
+    author_email='paradoxxx.zero@gmail.com',
+    description='Python package providing assets from https://github.com/Kozea/pygal.js',
+    install_requires=['pygaljs'],
+    long_description='''Use `pygaljs <https://pypi.python.org/pypi/pygaljs/>`_ instead.''',
+    maintainer='Mounier Florian',
+    maintainer_email='paradoxxx.zero@gmail.com',
+    name='pygal.js',
+    platforms=['all'],
+    py_modules=['wheel-platform-tag-is-broken-on-empty-wheels-see-issue-141'],
+    url='https://pypi.python.org/pypi/pygaljs/',
+    version="0.0",
+    zip_safe=False,
+)
