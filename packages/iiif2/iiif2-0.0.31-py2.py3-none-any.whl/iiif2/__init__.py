bbef47234ffb2f7dce568864663b56dcf762117d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:12 +0000

    Add package iiif2/0.0.31/iiif2-0.0.31-py2.py3-none-any.whl

---
+++ packages/iiif2/iiif2-0.0.31-py2.py3-none-any.whl/iiif2/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+    iiif2
+    ~~~~~
+    An Image Processing Library for the IIIF Image API 2.0
+    Specification.
+
+    :copyright: (c) 2015 by Authors
+    :license: see LICENSE for more details.
+"""
+
+__title__ = 'iiif2'
+__version__ = '0.0.31'
+__author__ = [
+    'Mek <michael.karpeles@gmail.com>'
+    ]
+
+from .iiif import IIIF  # NOQA
+from .web import Parse  # NOQA
