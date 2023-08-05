9bc176773fcb9dfda6b61331866678afc78ec74d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:17 +0000

    Add package pyvXRAY/0.2.1/pyvXRAY-0.2.1.zip

---
+++ packages/pyvXRAY/pyvXRAY-0.2.1.zip/pyvXRAY-0.2.1/pyvXRAY/version.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2015 Michael Hogg
+
+# This file is part of pyvXRAY - See LICENSE.txt for information on usage and redistribution
+
+# Version and date
+version     = '0.2.1'
+releasedate = '2015-08-12'
