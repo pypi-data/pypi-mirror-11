a90a5da4fb617313dcf00761cc2c8242ad298045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:29 +0000

    Add package hexes/0.4.0/hexes-0.4.0-py2.py3-none-any.whl

---
+++ packages/hexes/hexes-0.4.0-py2.py3-none-any.whl/hexes/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Kit La Touche'
+__email__ = 'kit@transneptune.net'
+__version__ = '0.4.0'
+
+
+from .hexes import (
+    Application,
+    Box,
+    Style,
+)
+
+__all__ = (
+    'Application',
+    'Box',
+    'Style',
+    '__author__',
+    '__email__',
+    '__version__',
+)
