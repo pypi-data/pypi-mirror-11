b36ef3455500c76ebf3b8fccf665189907815534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:21 +0000

    Add package nester_test_first/1.0.0/nester_test_first-1.0.0.zip

---
+++ packages/nester_test_first/nester_test_first-1.0.0.zip/nester_test_first-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        = 'nester_test_first',
+    version     = '1.0.0',
+    py_modules  = ['nester'],
+    author      = 'JeongWoo',
+    author_email= 'skeksk91@gmail.com',
+    url         = 'nate.com',
+    description = 'Good man',
+    )
