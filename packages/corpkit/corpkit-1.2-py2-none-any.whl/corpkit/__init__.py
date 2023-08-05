a4c0268f29dfb784a7d6acf8affeff4aaea4faaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:12 +0000

    Add package corpkit/1.2/corpkit-1.2-py2-none-any.whl

---
+++ packages/corpkit/corpkit-1.2-py2-none-any.whl/corpkit/__init__.py
@@ -0,0 +1,88 @@
+__all__ = ["interrogator",
+           "editor",
+           "plotter",
+           "conc",
+           "keywords",
+           "ngrams",
+           "collocates",
+           "quickview",
+           "concprinter",
+           "save_result",
+           "load_result",
+           "report_display",
+           "ipyconverter",
+           "conv",
+           "pytoipy",
+           "new_project",
+           "multiquery",
+           "interroplot",
+           "quicktree",
+           "searchtree",
+           "tregex_engine",
+           "load_all_results",
+           "dictmaker",
+           "texify",
+           "make_nltk_text",
+           "pmultiquery",
+           "as_regex",
+           "sim",
+           "show"]
+
+import os
+import sys
+
+# these lines here are to let ipython find tregex and potentially
+# anything in 'dictionaries'
+
+import corpkit
+path_to_corpkit = os.path.dirname(corpkit.__file__)
+thepath, corpkitname = os.path.split(path_to_corpkit)
+import dictionaries
+os.environ["PATH"] += os.pathsep + path_to_corpkit + os.pathsep + os.path.join(thepath, 'dictionaries')
+
+# these add to pythonpath, mostly so we can find dictionaries
+# doesn't seem to be working at the moment, though.
+sys.path.append(path_to_corpkit)
+sys.path.append(os.path.join(thepath, 'dictionaries'))
+sys.path.append(thepath)
+
+from interrogator import interrogator
+from editor import editor
+from plotter import plotter
+from conc import conc
+from keys import keywords
+from keys import ngrams
+from colls import collocates
+from plugins import HighlightLines, InteractiveLegendPlugin
+
+from other import quickview
+from other import concprinter
+from other import save_result
+from other import load_result
+from other import report_display
+from other import ipyconverter
+from other import conv
+from other import pytoipy
+from other import new_project
+from other import multiquery
+from other import interroplot
+from other import quicktree
+from other import searchtree
+from other import datareader
+from other import tregex_engine
+from other import load_all_results
+from other import texify
+from other import make_nltk_text
+from other import pmultiquery
+from other import as_regex
+from other import show
+from context import sim
+
+from build import dictmaker
+from build import correctspelling
+
+from dictionaries.process_types import processes
+from dictionaries.wordlists import wordlists
+from dictionaries.roles import roles
+
+
