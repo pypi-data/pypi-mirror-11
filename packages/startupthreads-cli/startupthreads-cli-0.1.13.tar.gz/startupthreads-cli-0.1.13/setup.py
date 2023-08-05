30701dae20bf5d2849728aea10b9c50497db81fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:05 +0000

    Add package startupthreads-cli/0.1.13/startupthreads-cli-0.1.13.tar.gz

---
+++ packages/startupthreads-cli/startupthreads-cli-0.1.13.tar.gz/startupthreads-cli-0.1.13/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='startupthreads-cli',
+    version='0.1.13',
+    author='AJ Bowen',
+    license='MIT',
+    author_email='aj@gandi.net',
+    packages=find_packages(),
+    description='Startup Threads CLI',
+    long_description=open('README.rst').read(),
+    url='https://github.com/soulshake/startup-threads-cli',
+    keywords='startup threads startupthreads cli swag shirts',
+    install_requires=['click==3.3',
+                      'arrow==0.5.4',
+                      'requests==2.7.0',
+                      'tabulate==0.7.5'],
+    entry_points="""\
+[console_scripts]
+swag = startupthreads.__main__:main
+""",
+    )
