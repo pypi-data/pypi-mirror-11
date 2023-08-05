633c5b50d45acd473f137479fd4a1a81a3a1ed47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:18 +0000

    Add package estnltk-textclassifier/1.2.2/estnltk-textclassifier-1.2.2.tar.gz

---
+++ packages/estnltk-textclassifier/estnltk-textclassifier-1.2.2.tar.gz/estnltk-textclassifier-1.2.2/textclassifier/__init__.py
@@ -0,0 +1,4 @@
+from .settings import Settings, SettingsFileReader
+from .synunifier import SynUnifier, SynFileReader
+from .classifier import Clf
+
---
+++ packages/estnltk-textclassifier/estnltk-textclassifier-1.2.2.tar.gz/estnltk-textclassifier-1.2.2/textclassifier/paths.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Module defining the paths of the package."""
+from __future__ import unicode_literals, print_function, absolute_import
+
+import os
+
+PACKAGE_PATH = os.path.dirname(__file__)
+TEST_PATH = os.path.join(PACKAGE_PATH, 'tests')
