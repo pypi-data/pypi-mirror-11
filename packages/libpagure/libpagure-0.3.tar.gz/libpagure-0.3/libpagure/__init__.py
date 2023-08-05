4cc7c1441bf9e2d50e6252740c5e43cd1d1f4e47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:54 +0000

    Add package libpagure/0.3/libpagure-0.3.tar.gz

---
+++ packages/libpagure/libpagure-0.3.tar.gz/libpagure-0.3/libpagure/__init__.py
@@ -0,0 +1,1 @@
+from .libpagure import *
---
+++ packages/libpagure/libpagure-0.3.tar.gz/libpagure-0.3/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+    name='libpagure',
+    packages=['libpagure'],
+    version='0.3',
+    description='A Python library for Pagure APIs.',
+    author='Lei Yang',
+    author_email='yltt1234512@gmail.com',
+    url='https://github.com/yangl1996/libpagure',
+    keywords=['pagure', 'api', 'library'],
+    classifiers=[
+        'Programming Language :: Python',
+    ],
+    license='GNU General Public License v2.0',
+    install_requires=['requests'],
+)
