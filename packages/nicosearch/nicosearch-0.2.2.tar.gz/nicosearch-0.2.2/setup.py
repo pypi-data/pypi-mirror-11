ac159f399a472c7960d8ea16e56b7ac57b734a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:43 +0000

    Add package nicosearch/0.2.2/nicosearch-0.2.2.tar.gz

---
+++ packages/nicosearch/nicosearch-0.2.2.tar.gz/nicosearch-0.2.2/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name="nicosearch",
+    packages=['nicosearch', 'nicosearch.api'],
+    version="0.2.2",
+    license='MIT License',
+    download_url="https://github.com/ymizushi/nicosearch/archive/master.zip",
+    platforms=['POSIX'],
+    description="https://github.com/ymizushi/nicosearch",
+    author="ymizushi",
+    author_email="mizushi@gmail.com",
+    url="https://github.com/ymizushi/nicosearch",
+    keywords=["search", "niconico", "library"],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
+        "Topic :: Software Development",
+    ],
+    long_description="The wrapper library for niconico search API with python."
+)
