61485eed77001785ef1e2434695f970ad661af64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:41 +0000

    Add package libpebble2/0.0.5/libpebble2-0.0.5.tar.gz

---
+++ packages/libpebble2/libpebble2-0.0.5.tar.gz/libpebble2-0.0.5/libpebble2/protocol/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+__author__ = 'katharine'
+
+from .appmessage import *
+from .apps import *
+from .audio import *
+from .blobdb import *
+from .datalogging import *
+from .logs import *
+from .music import *
+from .screenshots import *
+from .phone import *
+from .system import *
+from .timeline import *
+from .transfers import *
