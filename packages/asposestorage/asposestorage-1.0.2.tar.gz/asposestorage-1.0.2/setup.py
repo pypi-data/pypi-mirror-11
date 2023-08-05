4156ec493048647462c38846113e329ef4bb18ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:31 +0000

    Add package asposestorage/1.0.2/asposestorage-1.0.2.tar.gz

---
+++ packages/asposestorage/asposestorage-1.0.2.tar.gz/asposestorage-1.0.2/setup.py
@@ -0,0 +1,19 @@
+__author__ = 'imranwar'
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'asposestorage',
+    packages = find_packages(),
+    version = '1.0.2',
+    description = 'Aspose Cloud SDK for Python allows you to use Aspose API in your Python applications',
+    author='Imran Anwar',
+    author_email='imranwar@gmail.com',
+    url='http://www.aspose.com',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ]
+)
