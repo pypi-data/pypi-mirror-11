caaa53fe1d931e84f0b7c5521d07406a30a87703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:45 +0000

    Add package openlabs_nereid_webshop/3.4.3.0/openlabs_nereid_webshop-3.4.3.0.tar.gz

---
+++ packages/openlabs_nereid_webshop/openlabs_nereid_webshop-3.4.3.0.tar.gz/openlabs_nereid_webshop-3.4.3.0/__init__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+'''
+
+    :copyright: (c) 2013-14 by Openlabs Technologies & Consulting (P) Ltd.
+    :license: GPLv3, see LICENSE for more details
+
+'''
+from trytond.pool import Pool
+from webshop import WebShop, BannerCategory, Banner, Article, \
+    Website, ArticleCategory, MenuItem
+from product import Product
+from invoice import Invoice
+from sale import Sale, SaleLine
+from party import Address
+from shipment import ShipmentOut
+from tree import Node
+
+
+def register():
+    Pool.register(
+        WebShop,
+        BannerCategory,
+        Banner,
+        Article,
+        Product,
+        Invoice,
+        Address,
+        ShipmentOut,
+        Sale,
+        SaleLine,
+        Website,
+        ArticleCategory,
+        Node,
+        MenuItem,
+        module='nereid_webshop', type_='model'
+    )
