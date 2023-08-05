7416e2b486fb922545487167f1e853ef036854a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:57 +0000

    Add package godcomplex/0.0.3/godcomplex-0.0.3.tar.gz

---
+++ packages/godcomplex/godcomplex-0.0.3.tar.gz/godcomplex-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from subprocess import call
+from distutils.command.install import install as _install
+
+def _post_install(dir):
+	global call
+	try:
+		import espeak
+	except:
+		print "missing espeak. installing it for you"
+		call(['sudo','apt-get','install','espeak','flac','-y'])
+
+class install(_install):
+	def run(self):
+		_install.run(self)
+		self.execute(_post_install,(self.install_lib),msg='running post install')	
+
+setup(name="godcomplex",
+	cmdclass={'install':install},
+	version="0.0.3",
+	description="this will say something if you say god",
+	author="Isaac Robinson",
+	author_email="isaacrob@me.com",
+	license="GNU",
+	packages=["godcomplex"],
+	scripts=['bin/godcomplex'],
+	keywords=["god","irony","speech recognition"],
+	install_requires=['SpeechRecognition'],
+	zip_safe=False)
