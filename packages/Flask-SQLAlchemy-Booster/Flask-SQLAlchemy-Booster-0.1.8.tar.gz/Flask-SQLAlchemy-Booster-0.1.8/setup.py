85ce3a207a94190b4e4d2e54f586a8ac06c2fb4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:58 +0000

    Add package Flask-SQLAlchemy-Booster/0.1.8/Flask-SQLAlchemy-Booster-0.1.8.tar.gz

---
+++ packages/Flask-SQLAlchemy-Booster/Flask-SQLAlchemy-Booster-0.1.8.tar.gz/Flask-SQLAlchemy-Booster-0.1.8/setup.py
@@ -0,0 +1,32 @@
+"""
+Flask-SQLAlchemy-Booster
+------------------------
+
+Adds further methods to Flask-SQLAlchemy by subclassing the
+Model class and adding
+
+    1. Class methods to enable easy querying
+
+    2. Methods to convert the model object to a JSON convertible
+    dictionary.
+
+"""
+
+from setuptools import setup
+
+setup(
+    name='Flask-SQLAlchemy-Booster',
+    version='0.1.8',
+    long_description='A wrapper around Flask-SQLAlchemy',
+    packages=['flask_sqlalchemy_booster'],
+    include_package_data=True,
+    install_requires=[
+        "toolspy>=0.1.",
+        "Flask>=0.10.1",
+        "SQLAlchemy>=0.9.8",
+        "Flask-SQLAlchemy>=2.0"
+    ],
+    license='MIT',
+    url='https://github.com/inkmonk/flask-sqlalchemy-booster',
+    author='SuryaSankar',
+    author_email='suryashankar.m@gmail.com')
