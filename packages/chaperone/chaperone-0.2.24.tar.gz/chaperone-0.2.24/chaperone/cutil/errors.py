0056c158865f070ca871b65e55aae0389da19ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:53 +0000

    Add package chaperone/0.2.24/chaperone-0.2.24.tar.gz

---
+++ packages/chaperone/chaperone-0.2.24.tar.gz/chaperone-0.2.24/chaperone/cutil/errors.py
@@ -0,0 +1,14 @@
+class ChError(Exception):
+    pass
+
+class ChParameterError(ChError):
+    pass
+
+class ChNotFoundError(ChError):
+    pass
+
+class ChSystemError(ChError):
+    pass
+
+class ChProcessError(ChError):
+    pass
