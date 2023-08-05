00809c705af42601ebdbe83124333c8a1239d9d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:48 +0000

    Add package firstvivek/0.0.1/firstvivek-0.0.1.zip

---
+++ packages/firstvivek/firstvivek-0.0.1.zip/firstvivek-0.0.1/first.py
@@ -0,0 +1,8 @@
+""" Stupid
+comments
+for python"""
+def first_meth(arg1):
+	print(arg1)
+	
+first_meth("hi there")
+	
---
+++ packages/firstvivek/firstvivek-0.0.1.zip/firstvivek-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name = 'firstvivek',
+	version = '0.0.1',
+	py_modules = ['first'],
+	author = 'vivek',
+	author_email = 'testspg26@gmail.com',
+	url = 'http://sportografy.com',
+	description = 'Test'
+	)
+	
