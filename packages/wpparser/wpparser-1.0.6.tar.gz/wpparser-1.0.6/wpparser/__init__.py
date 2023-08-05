44b04008bf173e65d51448aae86035c29eeae711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:43 +0000

    Add package wpparser/1.0.6/wpparser-1.0.6.tar.gz

---
+++ packages/wpparser/wpparser-1.0.6.tar.gz/wpparser-1.0.6/wpparser/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""
+wpparser
+---
+
+wpparser is a library that parses the wordpress export file into a easy
+readable dictionary.
+
+:copyright: (c) 2014-2015 by Martin Sandström
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = "wpparser"
+__version__ = "1.0.6"
+__build__ = 106
+__author__ = "Martin Sandström"
+__license__ = "MIT"
+__copyright__ = "Copyright 2014-2015 Martin Sandström"
+
+
+from .parser import parse  # NOQA
