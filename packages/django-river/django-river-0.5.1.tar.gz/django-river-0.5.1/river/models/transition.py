04b7d873cd3daed8fe8da5744b0fe8cdc4cfb300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:12 +0000

    Add package django-river/0.5.1/django-river-0.5.1.tar.gz

---
+++ packages/django-river/django-river-0.5.1.tar.gz/django-river-0.5.1/river/models/transition.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from river.models.base_model import BaseModel
+from river.models.state import State
+from river.services.config import RiverConfig
+
+__author__ = 'ahmetdal'
+
+BACKWARD = 0
+FORWARD = 1
+
+DIRECTIONS = [
+    (BACKWARD, _("Backward")),
+    (FORWARD, _("Forward"))
+]
+
+
+class Transition(BaseModel):
+    class Meta:
+        app_label = 'river'
+        verbose_name = _("Transition")
+        verbose_name_plural = _("Transitions")
+
+    content_type = models.ForeignKey(RiverConfig.CONTENT_TYPE_CLASS, verbose_name=_('Content Type'))
+    field = models.CharField(verbose_name=_('Field'), max_length=200)
+    source_state = models.ForeignKey(State, verbose_name=_("Source State"), related_name='transitions_as_source')
+    destination_state = models.ForeignKey(State, verbose_name=_("Next State"), related_name='transitions_as_destination')
+    direction = models.SmallIntegerField(_("Transition Direction"), default=FORWARD)
+
+    def __unicode__(self):
+        return '%s -> %s (%s)' % (self.source_state, self.destination_state, self.content_type)
