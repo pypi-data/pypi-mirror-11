b6efb9fc4836f62a4cf1a87fdc0d2af092bb0186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:08 +0000

    Add package aliyun-python-sdk/2.0.0/aliyun-python-sdk-2.0.0.zip

---
+++ packages/aliyun-python-sdk/aliyun-python-sdk-2.0.0.zip/aliyun-python-sdk-2.0.0/setup.py
@@ -0,0 +1,14 @@
+__author__ = 'yudan.lyd'
+
+from distutils.core import setup
+
+setup(
+    name='aliyun-python-sdk',
+    version='2.0.0',
+    author='Septimus',
+    author_email='yudan.lyd@alibaba-inc.com',
+    packages=[],
+    scripts=[],
+    description='All modules of aliyun python sdk.',
+    install_requires=[]
+)
