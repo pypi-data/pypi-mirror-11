b8f5c7b021f9fb9ce83d8d275029d739d26e6c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:26 +0000

    Add package ConnectorDB/0.3.0a1/ConnectorDB-0.3.0a1.tar.gz

---
+++ packages/ConnectorDB/ConnectorDB-0.3.0a1.tar.gz/ConnectorDB-0.3.0a1/connectordb/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""The official python client for ConnectorDB.
+
+The client enables quick usage of the database for IoT stuff and data analysis::
+
+    import time
+    import connectordb
+
+    cdb = connectordb.ConnectorDB("apikey")
+
+    temp = cdb["temperature"]
+
+    if not temp.exists():
+        temp.create({"type": "number"})
+
+    while True:
+        time.sleep(1)
+        t = get_temperature()
+        temp.insert(t)
+
+"""
+
+from _connectordb import *
+from _connection import AuthenticationError, ServerError
+
+__version__ = "0.3.0"
