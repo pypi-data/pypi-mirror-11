1970380add9ac676fd696c59368cf1fcf0ce765b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:00 +0000

    Add package django-alo-forms/1.9.3/django-alo-forms-1.9.3.tar.gz

---
+++ packages/django-alo-forms/django-alo-forms-1.9.3.tar.gz/django-alo-forms-1.9.3/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name             = 'django-alo-forms',
+    version          = '1.9.3',
+    author           = "Diogo Laginha",
+    author_email     = "diogo.laginha.machado@gmail.com",
+    url              = 'https://github.com/laginha/django-alo-forms',
+    description      = "Additional Logic on Forms",
+    packages         = find_packages(where='src'),
+    package_dir      = {'': 'src'},
+    install_requires = [
+        'django', 
+        'django_easy_response',
+    ],
+    extras_require   = {},
+    zip_safe         = False,
+    license          = 'MIT',
+    classifiers      = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Intended Audience :: Developers',
+    ]
+)
