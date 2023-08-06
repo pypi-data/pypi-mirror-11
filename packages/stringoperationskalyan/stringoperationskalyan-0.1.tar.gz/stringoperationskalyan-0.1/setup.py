d2e6ff4ab9b12c37414be2168c922d399a507763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:07 +0000

    Add package stringoperationskalyan/0.1/stringoperationskalyan-0.1.tar.gz

---
+++ packages/stringoperationskalyan/stringoperationskalyan-0.1.tar.gz/stringoperationskalyan-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name='stringoperationskalyan',
+	long_description='provide functions for string operations',
+	py_module = ['numoperations','stroperations'],
+	version = '0.1',
+	author = 'kalyan',
+	author_email = 'kalyan.solasa90@gmail.com',
+	url = 'https://www.aviso.com'
+	
+
+
+)
