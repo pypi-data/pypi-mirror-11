146e010464c4bca7a0ff0c4d264a38f13219cd00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:28 +0000

    Add package round/0.9.7/round-0.9.7.tar.gz

---
+++ packages/round/round-0.9.7.tar.gz/round-0.9.7/round/config.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# config.py
+#
+# Copyright 2014 BitVault, Inc. dba Gem
+
+from __future__ import unicode_literals
+
+SUPPORTED_NETWORKS = ['bitcoin_testnet', 'bitcoin', 'dogecoin', 'litecoin']
