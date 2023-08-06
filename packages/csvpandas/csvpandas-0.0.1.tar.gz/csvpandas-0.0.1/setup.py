0d1efb47c65b57035b2e23e81b0fc3a317411fe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:59 +0000

    Add package csvpandas/0.0.1/csvpandas-0.0.1.tar.gz

---
+++ packages/csvpandas/csvpandas-0.0.1.tar.gz/csvpandas-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from csvpandas import utils
+from setuptools import setup, find_packages
+
+setup(author='Chris Rosenthal',
+      author_email='crosenth@gmail.com',
+      description='Interface to manipulate csv files as Pandas DataFrames.',
+      name='csvpandas',
+      packages=find_packages(exclude=['tests']),
+      entry_points={'console_scripts': {'csvpandas = csvpandas:main'}},
+      version=utils.version(),
+      url='https://github.com/crosenth/csvpandas',
+      install_requires=['pandas>=0.16.2'],
+      license='GPLv3',
+      classifiers=[
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Development Status :: 1 - Planning'
+          ]
+      )
