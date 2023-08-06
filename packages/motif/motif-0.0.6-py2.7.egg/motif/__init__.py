44ddf3ca6b5d49c33c62328ce4fa811f0ceacd5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:18 +0000

    Add package motif/0.0.6/motif-0.0.6-py2.7.egg

---
+++ packages/motif/motif-0.0.6-py2.7.egg/motif/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Wu Fuheng(wufuheng@gmail.com)'
+__version__ = '0.0.6'
+
+from os.path import dirname, realpath
+import sys
+sys.path.append(dirname(realpath(__file__)))
+
+from motif import Motif
