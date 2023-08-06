4b6c145ef2d58b1c92810844706168be4b06e948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:40 +0000

    Add package fabrik_cli/1.1.2/fabrik_cli-1.1.2.tar.gz

---
+++ packages/fabrik_cli/fabrik_cli-1.1.2.tar.gz/fabrik_cli-1.1.2/frojd_fabric_cli/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+fabrik_cli
+------------
+:copyright: (c) 2015 by Fröjd Interactive AB
+:license: MIT, see LICENSE for more details.
+"""
+
+
+__title__ = "fabrik_cli"
+__version__ = "1.1.2"
+__build__ = 112
+__license__ = "MIT"
+__copyright__ = "Copyright 2015 Fröjd Interactive AB"
