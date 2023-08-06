98567e0d915baca120eaa5e02856440505ebd5b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:52 +0000

    Add package TextGrid/1.0/TextGrid-1.0.tar.gz

---
+++ packages/TextGrid/TextGrid-1.0.tar.gz/TextGrid-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='TextGrid',
+    version='1.0',
+    author='Kyle Gorman et al.',
+    author_email='gormanky@ohsu.edu',
+    packages=['textgrid'],
+    test_suite='tests',
+    description='Praat TextGrid manipulation.'
+)
