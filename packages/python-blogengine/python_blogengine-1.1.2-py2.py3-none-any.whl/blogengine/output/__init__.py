15c3c6061649692898836c5d4172528d414c02a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:10 +0000

    Add package python-blogengine/1.1.2/python_blogengine-1.1.2-py2.py3-none-any.whl

---
+++ packages/python-blogengine/python_blogengine-1.1.2-py2.py3-none-any.whl/blogengine/output/__init__.py
@@ -0,0 +1,92 @@
+import os, shutil
+
+
+def copytree(src, dst, symlinks=False, ignore=None):
+    """Recursively copy a directory tree using copy2().
+
+    The destination directory must not already exist.
+    If exception(s) occur, an Error is raised with a list of reasons.
+
+    If the optional symlinks flag is true, symbolic links in the
+    source tree result in symbolic links in the destination tree; if
+    it is false, the contents of the files pointed to by symbolic
+    links are copied.
+
+    The optional ignore argument is a callable. If given, it
+    is called with the `src` parameter, which is the directory
+    being visited by copytree(), and `names` which is the list of
+    `src` contents, as returned by os.listdir():
+
+        callable(src, names) -> ignored_names
+
+    Since copytree() is called recursively, the callable will be
+    called once for each directory that is copied. It returns a
+    list of names relative to the `src` directory that should
+    not be copied.
+
+    XXX Consider this example code rather than the ultimate tool.
+
+    """
+    names = os.listdir(src)
+    if ignore is not None:
+        ignored_names = ignore(src, names)
+    else:
+        ignored_names = set()
+
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+    errors = []
+    for name in names:
+        if name in ignored_names:
+            continue
+        srcname = os.path.join(src, name)
+        dstname = os.path.join(dst, name)
+        try:
+            if symlinks and os.path.islink(srcname):
+                linkto = os.readlink(srcname)
+                os.symlink(linkto, dstname)
+            elif os.path.isdir(srcname):
+                copytree(srcname, dstname, symlinks, ignore)
+            else:
+                # Will raise a SpecialFileError for unsupported file types
+                shutil.copy2(srcname, dstname)
+        # catch the Error from the recursive copytree so that we can
+        # continue with other files
+        except shutil.Error, err:
+            errors.extend(err.args[0])
+        except EnvironmentError, why:
+            errors.append((srcname, dstname, str(why)))
+    try:
+        shutil.copystat(src, dst)
+    except OSError, why:
+        if shutil.WindowsError is not None and isinstance(why, shutil.WindowsError):
+            # Copying file access times may fail on Windows
+            pass
+        else:
+            errors.append((src, dst, str(why)))
+    if errors:
+        raise shutil.Error, errors
+
+class Output(object):
+
+    themepath = None
+
+    def render(self, site, **kwargs):
+        raise NotImplementedError()
+
+    def copyAssets(self, directory):
+        source = os.path.join(self.themepath, 'assets')
+        dest = os.path.join(directory)
+
+        copytree(source, dest)
+
+    @staticmethod
+    def storeData(file, content):
+        directory = os.path.dirname(file)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        with open(file, 'w') as f:
+            f.write(content.encode('utf-8'))
+
+from websiteoutput import WebsiteOutput
