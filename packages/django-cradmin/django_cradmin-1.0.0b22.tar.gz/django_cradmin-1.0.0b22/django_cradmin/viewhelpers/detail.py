17e594108e850a5857341eb803f3e4ca724a97be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:54 +0000

    Add package django-cradmin/1.0.0b22/django_cradmin-1.0.0b22.tar.gz

---
+++ packages/django-cradmin/django_cradmin-1.0.0b22.tar.gz/django_cradmin-1.0.0b22/django_cradmin/viewhelpers/detail.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+from django.views.generic.detail import DetailView as DjangoDetailView
+from django_cradmin.viewhelpers.mixins import QuerysetForRoleMixin
+
+
+class DetailView(QuerysetForRoleMixin, DjangoDetailView):
+    """
+    Base detail view for Django cradmin views.
+    """
