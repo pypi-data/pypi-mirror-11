a3595678556ad3236b80c24dbe89b25b8d2f4490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:57 +0000

    Add package mwget/1.5.2/mwget-1.5.2.tar.gz

---
+++ packages/mwget/mwget-1.5.2.tar.gz/mwget-1.5.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name = "mwget",
+    version = "1.5.2",
+    author = "ruifengyun",
+    author_email = "rfyiamcool@163.com",
+    description = "support threading download file",
+    license = "MIT",
+    keywords = "wget threading and support break",
+    url = "https://github.com/rfyiamcool/mwget",
+    packages = ['mwget'],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Topic :: Internet :: WWW/HTTP",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
