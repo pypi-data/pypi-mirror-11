4a6b83ea1d3864934d5010ed633a46583dd51650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:59 +0000

    Add package Balert/1.0.4/Balert-1.0.4.tar.gz

---
+++ packages/Balert/Balert-1.0.4.tar.gz/Balert-1.0.4/setup.py
@@ -0,0 +1,36 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import sys
+
+extra = {}
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
+requirement_list = [r.strip() for r in open('requirements.txt', 'r').readlines() if r]
+setup(name='Balert',
+      version='1.0.4',
+      install_requires=requirement_list,
+      author='Tushar Gautam',
+      author_email='tushar.rishav@gmail.com',
+      packages=['balert', ],
+      entry_points={
+          'console_scripts': ['balert=balert:main'],
+      },
+      license='GNU General Public License v3 (GPLv3)',
+      url='https://github.com/tushar-rishav/balert/',
+      description="A speaking battery's charge level",
+      long_description=open('README').read(),
+      keywords=['reminder', 'battery', 'voice alert', 'python'],
+      classifiers=[
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Topic :: System :: Monitoring'
+      ],
+      )
