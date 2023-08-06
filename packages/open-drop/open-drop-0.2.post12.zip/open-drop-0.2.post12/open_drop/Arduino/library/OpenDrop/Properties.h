f2882edd9d82b7af1f4776d9081d489881fc50fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:21 +0000

    Add package open-drop/0.2.post12/open-drop-0.2.post12.zip

---
+++ packages/open-drop/open-drop-0.2.post12.zip/open-drop-0.2.post12/open_drop/Arduino/library/OpenDrop/Properties.h
@@ -0,0 +1,17 @@
+#ifndef ___OPEN_DROP__PROPERTIES___
+#define ___OPEN_DROP__PROPERTIES___
+
+#ifndef BASE_NODE__NAME
+#define BASE_NODE__NAME  ("open_drop")
+#endif
+#ifndef BASE_NODE__MANUFACTURER
+#define BASE_NODE__MANUFACTURER  ("Wheeler Lab")
+#endif
+#ifndef BASE_NODE__SOFTWARE_VERSION
+#define BASE_NODE__SOFTWARE_VERSION  ("0.2.post12")
+#endif
+#ifndef BASE_NODE__URL
+#define BASE_NODE__URL  ("http://github.com/wheeler-microfluidics/open-drop.git")
+#endif
+
+#endif
