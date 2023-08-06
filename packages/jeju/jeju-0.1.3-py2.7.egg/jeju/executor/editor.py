c19231c085917e57a67856f62ebea17e065f0aed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:22 +0000

    Add package jeju/0.1.3/jeju-0.1.3-py2.7.egg

---
+++ packages/jeju/jeju-0.1.3-py2.7.egg/jeju/executor/editor.py
@@ -0,0 +1,26 @@
+
+def find_file_path(lookahead):
+    print lookahead
+    if lookahead == None:
+        return None
+    ctx = lookahead['text']
+    items = ctx.split()
+    if items[0] == 'edit':
+        return items[1]
+
+
+def editor_run(**kwargs):
+    lookahead = kwargs['lookahead']
+    code = kwargs['code']
+
+    file_path = find_file_path(kwargs['lookahead'])
+    if file_path == None:
+        msg = "[DEBUG] I don't know how to edit!"
+        print msg
+        return msg
+    fp = open(file_path, 'w')
+    fp.write(code)
+    fp.close()
+    return "[DEBUG] success to create : %s" % file_path
+
+
