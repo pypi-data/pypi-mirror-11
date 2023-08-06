7a757772d7189094e061730069771dba493038db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:17 +0000

    Add package demo-package/0.0.2/demo-package-0.0.2.tar.gz

---
+++ packages/demo-package/demo-package-0.0.2.tar.gz/demo-package-0.0.2/setup.py
@@ -0,0 +1,19 @@
+# encoding: utf-8
+from __future__ import print_function
+
+from setuptools import setup
+
+
+version = (0, 0, 2)
+
+setup(
+    name='demo-package',
+    version="%d.%d.%d" % version,
+    author="Uwe Schmitt",
+    author_email="uwe.schmitt@id.ethz.ch",
+    license="BSD",
+
+    #entry_points={
+        #'console_scripts': ["say_hi=demo_package.greeter:greet"]
+    #},
+)
