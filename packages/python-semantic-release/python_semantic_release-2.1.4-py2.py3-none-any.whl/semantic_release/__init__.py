a5bf6824de424166ea3e152e7d7ab1cd887771ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:15 +0000

    Add package python-semantic-release/2.1.4/python_semantic_release-2.1.4-py2.py3-none-any.whl

---
+++ packages/python-semantic-release/python_semantic_release-2.1.4-py2.py3-none-any.whl/semantic_release/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '2.1.4'
+
+
+def setup_hook(argv):
+    """
+    A hook to be used in setup.py to enable `python setup.py publish`.
+
+    :param argv: sys.argv
+    """
+    if len(argv) > 1 and argv[1] in ['version', 'publish', 'changelog']:
+        from semantic_release.cli import main
+        main()
