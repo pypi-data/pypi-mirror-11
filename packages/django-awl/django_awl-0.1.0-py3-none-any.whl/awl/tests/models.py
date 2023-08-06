6fd161142252cdeda12ea96580763d0bb326bd9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:35 +0000

    Add package django-awl/0.1.0/django_awl-0.1.0-py3-none-any.whl

---
+++ packages/django-awl/django_awl-0.1.0-py3-none-any.whl/awl/tests/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+# ============================================================================
+# Waelsteng Models
+# ============================================================================
+
+class Link(models.Model):
+    url = models.CharField(max_length=80)
+    text = models.CharField(max_length=80)
+
+# ============================================================================
+# Admintools Models 
+# ============================================================================
+
+@python_2_unicode_compatible
+class Nested(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return 'Nested(id=%s %s)' % (self.id, self.name)
+
+
+@python_2_unicode_compatible
+class Inner(models.Model):
+    name = models.CharField(max_length=10)
+    nested = models.ForeignKey(Nested)
+
+    def __str__(self):
+        return 'Inner(id=%s %s)' % (self.id, self.name)
+
+
+@python_2_unicode_compatible
+class Outer(models.Model):
+    name = models.CharField(max_length=10)
+    inner = models.ForeignKey(Inner)
