191e48b1979f2058e77867f7e8708b7e33f33a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:46 +0000

    Add package enstaller/4.8.8/enstaller-4.8.8-py2.py3-none-any.whl

---
+++ packages/enstaller/enstaller-4.8.8-py2.py3-none-any.whl/enstaller/_version.py
@@ -0,0 +1,8 @@
+# THIS FILE IS GENERATED FROM ENSTALLER SETUP.PY
+version = '4.8.8'
+full_version = '4.8.8'
+git_revision = 'ad36f4a87d6456b837a7e9f95166d934602437ad'
+is_released = True
+
+if not is_released:
+    version = full_version
