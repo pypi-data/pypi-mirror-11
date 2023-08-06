b3a26e0a54f7be2d60b720459d055768a649b3c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:12 +0000

    Add package taboo/0.3.0/taboo-0.3.0-py2.py3-none-any.whl

---
+++ packages/taboo/taboo-0.3.0-py2.py3-none-any.whl/taboo/__main__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+taboo.__main__
+~~~~~~~~~~~~~~~~
+The main entry point for the command line interface.
+
+Invoke as ``taboo`` (if installed)
+or ``python -m taboo`` (no install required).
+"""
+from __future__ import absolute_import, unicode_literals
+import sys
+
+import taboo.cli
+
+
+if __name__ == '__main__':
+    # exit using whatever exit code the CLI returned
+    sys.exit(taboo.cli.cli())
