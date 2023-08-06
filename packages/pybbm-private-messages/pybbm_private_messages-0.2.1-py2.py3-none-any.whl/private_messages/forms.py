c9244b928f8807edb30f9a944885be3d514da64d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:15 +0000

    Add package pybbm-private-messages/0.2.1/pybbm_private_messages-0.2.1-py2.py3-none-any.whl

---
+++ packages/pybbm-private-messages/pybbm_private_messages-0.2.1-py2.py3-none-any.whl/private_messages/forms.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+from pybb import defaults, util
+from private_messages.models import PrivateMessage
+
+
+class MessageForm(forms.ModelForm):
+
+    parent = forms.UUIDField(required=False, widget=forms.HiddenInput)
+
+    class Meta(object):
+        model = PrivateMessage
+        fields = ('receivers', 'subject', 'body', 'parent')
+        widgets = {
+            'body': util.get_markup_engine().get_widget_cls(),
+        }
+        labels = {
+            'receivers': _('To'),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(MessageForm, self).__init__(*args, **kwargs)
+        self.available_smiles = defaults.PYBB_SMILES
+        self.smiles_prefix = defaults.PYBB_SMILES_PREFIX
