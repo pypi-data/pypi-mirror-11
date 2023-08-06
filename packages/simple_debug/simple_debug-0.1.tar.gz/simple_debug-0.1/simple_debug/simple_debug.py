3b4cdf5c18eb3cb8ebaa57063386f55c8e5e76c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:00 +0000

    Add package simple_debug/0.1/simple_debug-0.1.tar.gz

---
+++ packages/simple_debug/simple_debug-0.1.tar.gz/simple_debug-0.1/simple_debug/simple_debug.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#    Copyright (C) 2015 by
+#    Moreno Bonaventura <morenobonaventura@gmail.com>
+#    This is Free Software - You can use and distribute it under
+#    the terms of the GNU General Public License, version 3 or later.
+"""
+simple debug
+"""
+__author__ = 'Moreno Bonaventura (morenobonaventura@gmail.com)'
+__all__ = ['DEBUG,DEBUGr,clockSLEEP']
+
+import sys,time
+import simple_debug
+
+def DEBUG(s):
+     sys.stderr.write(str(s)+'\n')
+
+def DEBUGr(s):
+     sys.stderr.write(str(s)+'\r')
+
+def clockSLEEP(t):
+	for i in range(0,t):
+		DEBUGr(str(i)+'-----')
+		time.sleep(1)
+
+	
