4e8c5d5894e1c081dda55b657c5c8ffba7fbb7bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:06 +0000

    Add package glasseye/0.1/glasseye-0.1.tar.gz

---
+++ packages/glasseye/glasseye-0.1.tar.gz/glasseye-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='glasseye',
+      version='0.1',
+      description='A python module for converting markdown documents into the glasseye format',
+      url='https://github.com/coppeliaMLA/glasseye',
+      author='coppeliamla',
+      author_email='info@coppelia.io',
+      license='MIT',
+      packages=['glasseye'],
+      zip_safe=False)
