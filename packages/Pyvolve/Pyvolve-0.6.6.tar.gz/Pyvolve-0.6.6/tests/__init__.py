5e4078fef2783db7150c6db69b6202a950b31609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:38 +0000

    Add package Pyvolve/0.6.6/Pyvolve-0.6.6.tar.gz

---
+++ packages/Pyvolve/Pyvolve-0.6.6.tar.gz/Pyvolve-0.6.6/tests/__init__.py
@@ -0,0 +1,17 @@
+"""``pyvolve`` package for simulating evolutionary sequences along a phylogeny.
+Written by Stephanie J. Spielman.
+
+Test modules
+----------------
+
+
+* state_freqs_test
+
+* matrix_builder_test
+
+* model_test
+
+* evolver_test 
+
+"""
+from pyvolve import *
