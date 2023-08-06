dae585a4fe85b9eb914529554352decfd7d85877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:43 +0000

    Add package infi.storagemodel/0.4.7/infi.storagemodel-0.4.7.tar.gz

---
+++ packages/infi.storagemodel/infi.storagemodel-0.4.7.tar.gz/infi.storagemodel-0.4.7/src/infi/storagemodel/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.4.7"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(HEAD detached at v0.4.7)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '8885a541a26ab7e0db5e495c9bfd888156968332'
+__git_head_subject__ = 'Finished release v0.4.7.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
