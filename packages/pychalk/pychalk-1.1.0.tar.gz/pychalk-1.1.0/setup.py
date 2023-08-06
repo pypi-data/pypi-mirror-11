dad517b6d46c3e775d5012ba2689315e02aafdfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:05 +0000

    Add package pychalk/1.1.0/pychalk-1.1.0.tar.gz

---
+++ packages/pychalk/pychalk-1.1.0.tar.gz/pychalk-1.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+version = '1.1.0'
+
+
+setup(
+    author="Anthony Almarza",
+    author_email="anthony@reeliolabs.com",
+    name="pychalk",
+    packages=find_packages(),
+    version=version,
+    url="https://github.com/anthonyalmarza/chalk",
+    download_url="https://github.com/anthonyalmarza/chalk/tarball/" + version,
+    description="Color printing in python",
+    long_description="Also includes a logging handler for printing in color.",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    install_requires=['six', ],
+    extras_require={'dev': ['ipdb', 'fabric', 'coverage']},
+    keywords=["print", "color", "chalk", "logging"],
+)
