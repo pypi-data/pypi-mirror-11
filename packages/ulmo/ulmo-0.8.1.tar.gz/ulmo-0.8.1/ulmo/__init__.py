09eccc4441258f9ab3fb8dcf37e63c23c8b9cfdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:38 +0000

    Add package ulmo/0.8.1/ulmo-0.8.1.tar.gz

---
+++ packages/ulmo/ulmo-0.8.1.tar.gz/ulmo-0.8.1/ulmo/__init__.py
@@ -0,0 +1,19 @@
+"""
+    ulmo
+    ~~~~
+
+    an open source library for clean, simple and fast access to public hydrology and climatology data
+"""
+from __future__ import (absolute_import, division, print_function)
+
+# ulmo version PEP-0440
+__version__ = '0.8.1'
+
+from . import cdec
+from . import cpc
+from . import cuahsi
+from . import twc
+from . import ncdc
+from . import usace
+from . import usgs
+from . import util
