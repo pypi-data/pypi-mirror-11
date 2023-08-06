a87c8d8aaafc752962f9004964b53744a4cb87e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:49 +0000

    Add package andluo_pytest/1.0.0/andluo_pytest-1.0.0.zip

---
+++ packages/andluo_pytest/andluo_pytest-1.0.0.zip/andluo_pytest-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'andluo_pytest',
+    version = '1.0.0',
+    py_module = ['andluo_pytest'],
+    author = 'zf',
+    author_email = 'abc@qq.com',
+    url = '',
+    description = 'aaa'
+    )
