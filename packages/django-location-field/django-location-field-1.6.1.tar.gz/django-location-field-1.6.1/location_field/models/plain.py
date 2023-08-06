ebf10b364bd73cd71b6dcd6ac7325d88ce6a3e36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:01 +0000

    Add package django-location-field/1.6.1/django-location-field-1.6.1.tar.gz

---
+++ packages/django-location-field/django-location-field-1.6.1.tar.gz/django-location-field-1.6.1/location_field/models/plain.py
@@ -0,0 +1,26 @@
+from django.db.models import CharField
+
+from location_field.forms import plain as forms
+from location_field.models.base import BaseLocationField
+
+
+class PlainLocationField(BaseLocationField, CharField):
+    formfield_class = forms.PlainLocationField
+
+    def __init__(self, max_length=63, *args, **kwargs):
+        super(PlainLocationField, self).__init__(*args, **kwargs)
+
+        kwargs.pop('based_fields', None)
+        kwargs.pop('zoom', None)
+        kwargs.pop('suffix', None)
+
+        CharField.__init__(self, max_length=max_length, *args, **kwargs)
+
+
+# south compatibility
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules(
+        [], ["^location_field\.models\.plain\.PlainLocationField"])
+except:
+    pass
