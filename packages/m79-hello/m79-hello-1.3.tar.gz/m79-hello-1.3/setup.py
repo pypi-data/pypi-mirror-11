5043306179f51efa3e913dddb11147942bf6164e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:33 +0000

    Add package m79-hello/1.3/m79-hello-1.3.tar.gz

---
+++ packages/m79-hello/m79-hello-1.3.tar.gz/m79-hello-1.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+ 
+setup(
+     name='m79-hello',    # This is the name of your PyPI-package.
+     version='1.3',                          # Update the version number for new releases
+     scripts=['helloworld']                  # The name of your scipt, and also the command you'll be using for calling it
+)
+
