e39e49d19bacb25f0b367270670e2a4f84fa44ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:14 +0000

    Add package tg-profiler/0.1.3/tg-profiler-0.1.3.tar.gz

---
+++ packages/tg-profiler/tg-profiler-0.1.3.tar.gz/tg-profiler-0.1.3/tg_profiler/control.py
@@ -0,0 +1,41 @@
+# encoding: utf-8
+
+"""tg_profiler startup and shutdown interface.
+"""
+
+
+import logging
+from tg_profiler.profilers.memory_snapshot import MemorySnapshotProfiler
+
+log = logging.getLogger(__name__)
+
+class ControlClass(object):
+    """Control tg_profiler startup and shutdown."""
+
+    def __init__(self):
+        self.config = dict()
+
+    def start(self, config, extra_classes=None):
+        self.config = config
+        if not self.config.get("tg_profiler.on", False):
+            return
+
+        log.info("tg_profiler extension starting up.")
+
+        self.profiler = MemorySnapshotProfiler(
+            self.config.get("tg_profiler.log_path", "tg_profiler/"),
+            self.config.get("tg_profiler.interval", 60),
+            self.config.get("tg_profiler.rotation", 10)
+        )
+        self.profiler.start()
+
+        log.info("tg_profiler extension started successfully.")
+
+    def stop(self, force=False):
+        if not self.config.get("tg_profiler.on", False) or (not self.profiler.running and not force):
+            return
+        log.info("tg_profiler extension shutting down.")
+
+        self.profiler.running = False
+
+interface = ControlClass()
