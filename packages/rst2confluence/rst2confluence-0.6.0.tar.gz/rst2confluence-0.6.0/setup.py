269232f6fb22f3b207a6e6c2606267b9a7efda22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:22 +0000

    Add package rst2confluence/0.6.0/rst2confluence-0.6.0.tar.gz

---
+++ packages/rst2confluence/rst2confluence-0.6.0.tar.gz/rst2confluence-0.6.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import os
+import shutil
+from distutils.core import setup
+
+if not os.path.exists('scripts'):
+    os.makedirs('scripts')
+
+shutil.copyfile('rst2confluence.py', 'scripts/rst2confluence')
+
+setup(
+    name='rst2confluence',
+    version='0.6.0',
+    description='reStructuredText-to-Confluence markup converter',
+
+    author='Kenichiro TANAKA',
+    author_email='tanaka.kenichiro@gmail.com',
+
+    maintainer='Christian Weiske',
+    maintainer_email='christian.weiske@netresearch.de',
+
+    url='https://github.com/netresearch/rst2confluence',
+
+    license='AGPL',
+
+    py_modules=['rst2confluence.confluence'],
+    # package_dir={'rst2confluence': 'src/rst2confluence'},
+    scripts=['scripts/rst2confluence']
+)
