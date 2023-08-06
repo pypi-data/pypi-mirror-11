cb21f1e2040e724f3c3c7504857634581aa19dfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:00 +0000

    Add package mooltipy/0.2.1/mooltipy-0.2.1.tar.gz

---
+++ packages/mooltipy/mooltipy-0.2.1.tar.gz/mooltipy-0.2.1/mooltipy/utilities/mooltipy_wrapper.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+#
+# This file is part of Mooltipy.
+#
+# Mooltipy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Mooltipy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Mooltipy.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Wraps all utilities under a mooltipy command."""
+
+# README:
+# This file is a hectic wrapper. If you're looking to understand a
+# particular utility, it would be best if you viewed that utility under
+# the utilities directory -- see /mooltipy/utilities/mp*.py
+
+import argparse
+import sys
+
+from mooltipy.utilities import mpdata
+from mooltipy.utilities import mplogin
+
+# Dictionary key:values == "utility name": module
+#   Used to print out usage and as a switch/case to branch on chosen
+#   utility. Entrypoint is assumed to be main() and the description
+#   of what the utility does is pulled from the module's docstring.
+utilities = {
+    'data': mpdata,
+    'login': mplogin,
+}
+
+def main_options():
+    """Handles command-line interface, arguments & options."""
+
+    utility_list = ''
+    for key, value in utilities.items():
+        utility_list += ' '*7 + key.upper() + '\t- ' + value.__doc__ + '\n'
+
+    usage = 'Usage: %(prog)s UTILITY <utility arguments & options>\n' + \
+            'Avalilable utilities:\n' + \
+            utility_list
+
+    # All arguments after argv[1] are passed through to the utility and
+    # should not be left for mooltipy_wrapper's call to parse_args() 
+    passthrough_args = None
+    if len(sys.argv) > 2:
+        passthrough_args = sys.argv[2:]
+        sys.argv = sys.argv[0:2]
+        for x in sys.argv:
+            print(x)
+        # Add a dummy entry in argv for args option
+        sys.argv.append(None)
+
+    parser = argparse.ArgumentParser(usage=usage)
+    parser.add_argument('utility', help='Utility to use.')
+    parser.add_argument('args', help='Arguments & options to pass onto utility.',
+            nargs='+')
+
+    args = parser.parse_args()
+
+    if not args.utility.lower() in utilities.keys():
+        parser.print_help()
+        sys.exit(1)
+
+    # Reattatch arguments intended for the utility.
+    if passthrough_args:
+        del sys.argv[2]
+        sys.argv += passthrough_args
+
+    return args
+
+def main():
+
+    args = main_options()
+    utilities[args.utility.lower()].main()
+
+if __name__ == '__main__':
+    main()
+
+    # TODO:
+    #   * Providing no argument list to utility results in
+    #       mooltipy_wrapper's help displaying, not the utilities.
+    #       E.g. $ mooltipy data
+    #           should show mpdata help dialog, not wrapper's
---
+++ packages/mooltipy/mooltipy-0.2.1.tar.gz/mooltipy-0.2.1/mooltipy/utilities/mplogin.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+#
+# This file is part of Mooltipy.
+#
+# Mooltipy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Mooltipy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Mooltipy.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Manage contexts containing usernames & passwords."""
+
+from optparse import OptionParser
+import os
+import time
+import sys
+
+from mooltipy.mooltipass_client import MooltipassClient
+
+def main_options():
+    """Handles command-line interface, arguments & options. """
+
+    # Fix usage message if executed from wrapper
+    utility_called = ''
+    print('utility called: ' + os.path.split(sys.argv[0])[1])
+    if os.path.split(sys.argv[0])[1] in ['./mooltipy.py', 'mooltipy']:
+        utility_called = sys.argv[1]
+        del sys.argv[1]
+
+    usage = 'Usage: %prog {utility} [OPTIONS]\n'.format(utility=utility_called)
+    usage +='Example: %prog {utility} '.format(utility=utility_called)
+    usage += 'Lycos.com --login=jsmith --password="not_random"'
+
+    parser = OptionParser(usage)
+    parser.add_option('--login', dest='login', metavar='USER',
+            help='login for context')
+    parser.add_option('--password', dest='password', metavar='PASS',
+            help='password for login')
+
+    (options, args) = parser.parse_args()
+
+    return (options, args)
+
+def main():
+
+    (options, args) = main_options()
+
+    mooltipass = MooltipassClient()
+
+    # Ping the mooltipass, an integral part of the initialization process.
+    if not mooltipass.ping():
+        logging.error('Mooltipass did not reply to a ping request!')
+        print('failure')
+        sys.exit(0)
+
+    # Ensure Mooltipass status
+    quiet_bool = False
+    while True:
+        status = mooltipass.get_status()
+        if status == 5:
+            break
+        logging.debug('Status != 5... it is: {0}'.format(status))
+        if not quiet_bool:
+            print('Insert a card and unlock the Mooltipass...')
+        quiet_bool = True
+        time.sleep(2)
+    quiet_bool = False
+
+    while not mooltipass.set_context(args[0]):
+        mooltipass.add_context(args[0])
+
+    mooltipass.set_login(options.login)
+    mooltipass.set_password(options.password)
+
+if __name__ == '__main__':
+
+    main()
+
+    #TODO: theres a lot left to be desired here
+
