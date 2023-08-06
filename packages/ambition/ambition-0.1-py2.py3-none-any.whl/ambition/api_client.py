27858c263f21cab7ef95b3fe43cf459c69fb6f08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:02 +0000

    Add package ambition/0.1/ambition-0.1-py2.py3-none-any.whl

---
+++ packages/ambition/ambition-0.1-py2.py3-none-any.whl/ambition/api_client.py
@@ -0,0 +1,280 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""Swagger generic API client. This client handles the client-
+server communication, and is invariant across implementations. Specifics of
+the methods and models for each application are generated from the Swagger
+templates."""
+
+from __future__ import absolute_import
+from .rest import RESTClient
+from . import models
+
+import os
+import re
+import datetime
+import mimetypes
+
+# python 2 and python 3 compatibility library
+from six import iteritems
+
+try:
+    # for python3
+    from urllib.parse import quote
+except ImportError:
+    # for python2
+    from urllib import quote
+
+
+class ApiClient(object):
+    """
+    Generic API client for Swagger client library builds
+
+    :type configuration: ``ApiConfiguration``
+    :param configuration: The ApiConfiguration for this client
+    """
+    def __init__(self, configuration):
+        self.configuration = configuration
+        self.default_headers = {}
+        self.host = configuration.host
+        self.cookie = None
+        # Set default User-Agent.
+        self.user_agent = 'Python-Swagger'
+
+    @property
+    def user_agent(self):
+        return self.default_headers['User-Agent']
+
+    @user_agent.setter
+    def user_agent(self, value):
+        self.default_headers['User-Agent'] = value
+
+    def call_api(
+        self, resource_path, method, path_params=None, query_params=None,
+        header_params=None, body=None, post_params=None, files=None,
+        response=None, auth_settings=['default']
+    ):
+
+        # headers parameters
+        header_params = header_params or {}
+        header_params.update(self.default_headers)
+        if self.cookie:
+            header_params['Cookie'] = self.cookie
+        header_params = self.sanitize_for_serialization(header_params)
+
+        # path parameters
+        if path_params:
+            path_params = self.sanitize_for_serialization(path_params)
+            for k, v in iteritems(path_params):
+                replacement = quote(str(self.to_path_value(v)))
+                resource_path = resource_path.replace('{' + k + '}', replacement)
+
+        # query parameters
+        if query_params:
+            query_params = self.sanitize_for_serialization(query_params)
+            query_params = {k: self.to_path_value(v) for k, v in iteritems(query_params)}
+
+        # post parameters
+        if post_params:
+            post_params = self.prepare_post_parameters(post_params, files)
+            post_params = self.sanitize_for_serialization(post_params)
+
+        # auth setting
+        self.update_params_for_auth(header_params, query_params, auth_settings)
+
+        # body
+        if body:
+            body = self.sanitize_for_serialization(body)
+
+        # request url
+        url = self.host + resource_path
+
+        # perform request and return response
+        response_data = self.request(
+            method, url, query_params=query_params, headers=header_params,
+            post_params=post_params, body=body)
+
+        # deserialize response data
+        if response:
+            # return a tuple of models if the response is a list of objects
+            if type(response_data) in [list, tuple]:
+                return (
+                    self.deserialize(model, response)
+                    for model in response_data
+                )
+            return self.deserialize(response_data, response)
+        else:
+            return None
+
+    def to_path_value(self, obj):
+        """
+        Convert a string or object to a path-friendly value
+
+        :param obj: object or string value
+
+        :return string: quoted value
+        """
+        if type(obj) == list:
+            return ','.join(obj)
+        else:
+            return str(obj)
+
+    def sanitize_for_serialization(self, obj):
+        """
+        Sanitize an object for Request.
+
+        If obj is None, return None.
+        If obj is str, int, float, bool, unicode, return directly.
+        If obj is datetime.datetime, datetime.date convert to string in iso8601 format.
+        If obj is list, santize each element in the list.
+        If obj is dict, return the dict.
+        If obj is swagger model, return the properties dict.
+        """
+        if isinstance(obj, type(None)):
+            return None
+        elif isinstance(obj, (str, int, float, bool, tuple, unicode)):
+            return obj
+        elif isinstance(obj, list):
+            return [self.sanitize_for_serialization(sub_obj) for sub_obj in obj]
+        elif isinstance(obj, (datetime.datetime, datetime.date)):
+            return obj.isoformat()
+        else:
+            if isinstance(obj, dict):
+                obj_dict = obj
+            else:
+                # Convert model obj to dict except attributes `swagger_types`, `attribute_map`
+                # and attributes which value is not None.
+                # Convert attribute name to json key in model definition for request.
+                obj_dict = {
+                    obj.attribute_map[key]: val
+                    for key, val in iteritems(obj.__dict__)
+                    if key not in ['swagger_types', 'attribute_map']
+                    and val is not None
+                }
+            return {
+                key: self.sanitize_for_serialization(val)
+                for key, val in iteritems(obj_dict)
+            }
+
+    def deserialize(self, obj, obj_class):
+        """
+        Derialize a JSON string into an object.
+
+        :param obj: string or object to be deserialized
+        :param obj_class: class literal for deserialzied object, or string of class name
+
+        :return object: deserialized object
+        """
+        # Have to accept obj_class as string or actual type. Type could be a
+        # native Python type, or one of the model classes.
+
+        if 'list[' in obj_class:
+            match = re.match('list\[(.*)\]', obj_class)
+            sub_class = match.group(1)
+            return [self.deserialize(sub_obj, sub_class) for sub_obj in obj]
+        if obj_class in ['int', 'float', 'dict', 'list', 'str', 'bool']:
+            try:
+                return eval(obj_class)(obj)
+            except UnicodeEncodeError:
+                return unicode(obj)
+            except TypeError:
+                return obj
+        if obj_class == 'datetime':
+            return self.__parse_string_to_datetime(obj)
+
+        model_class = getattr(models, obj_class)
+        return self.deserialize_model(model_class, obj)
+
+    def deserialize_model(self, model_class, obj):
+        instance = model_class()
+        if obj is None or type(obj) not in [list, dict]:
+            return instance
+        for attr, attr_type in iteritems(instance.swagger_types):
+            if instance.attribute_map[attr] not in obj:
+                continue
+            value = obj[instance.attribute_map[attr]]
+            setattr(instance, attr, self.deserialize(value, attr_type))
+        return instance
+
+    def __parse_string_to_datetime(self, string):
+        """
+        Parse datetime in string to datetime.
+
+        The string should be in iso8601 datetime format.
+        """
+        from dateutil.parser import parse
+        return parse(string)
+
+    def request(self, method, url, query_params=None, headers=None, post_params=None, body=None):
+        """
+        Perform http request using RESTClient.
+        """
+        if method == "GET":
+            return RESTClient.GET(url, query_params=query_params, headers=headers)
+        elif method == "HEAD":
+            return RESTClient.HEAD(url, query_params=query_params, headers=headers)
+        elif method == "POST":
+            return RESTClient.POST(url, headers=headers, post_params=post_params, body=body)
+        elif method == "PUT":
+            return RESTClient.PUT(url, headers=headers, post_params=post_params, body=body)
+        elif method == "PATCH":
+            return RESTClient.PATCH(url, headers=headers, post_params=post_params, body=body)
+        elif method == "DELETE":
+            return RESTClient.DELETE(url, query_params=query_params, headers=headers)
+        else:
+            raise ValueError("http method must be `GET`, `HEAD`, `POST`, `PATCH`, `PUT` or `DELETE`")
+
+    def prepare_post_parameters(self, post_params=None, files=None):
+        params = {}
+
+        if post_params:
+            params.update(post_params)
+
+        if files:
+            for k, v in iteritems(files):
+                with open(v, 'rb') as f:
+                    filename = os.path.basename(f.name)
+                    filedata = f.read()
+                    mimetype = mimetypes.guess_type(filename)[0] or 'application/octet-stream'
+                    params[k] = tuple([filename, filedata, mimetype])
+
+        return params
+
+    def select_header_accept(self, accepts):
+        """
+        Return `Accept` based on an array of accepts provided
+        """
+        if not accepts:
+            return
+
+        accepts = list(map(lambda x: x.lower(), accepts))
+
+        if 'application/json' in accepts:
+            return 'application/json'
+        else:
+            return ', '.join(accepts)
+
+    def select_header_content_type(self, content_types):
+        """
+        Return `Content-Type` baseed on an array of content_types provided
+        """
+        if not content_types:
+            return 'application/json'
+
+        content_types = list(map(lambda x: x.lower(), content_types))
+
+        if 'application/json' in content_types:
+            return 'application/json'
+        else:
+            return content_types[0]
+
+    def update_params_for_auth(self, headers, querys, auth_settings):
+        """
+        Update header and query params based on authentication setting
+        """
+        if not auth_settings:
+            return
+
+        for auth in auth_settings:
+            auth_setting = self.configuration.auth_settings().get(auth)
+            headers[auth_setting['key']] = auth_setting['value']
---
+++ packages/ambition/ambition-0.1-py2.py3-none-any.whl/ambition/tests/rest_tests.py
@@ -0,0 +1,177 @@
+import json
+from functools import partial
+import unittest
+from mock import patch, MagicMock
+
+from ambition.rest import ApiException
+
+
+class RestClientObjectTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    @patch('ambition.rest.RESTClientObject.agent')
+    def test_content_types(self, rest_agent):
+        """
+        Verify that the rest client supports the extended set of content types
+        """
+        request_mock = MagicMock(return_value=MagicMock(status=200, data=''))
+        rest_agent.return_value.request = request_mock
+        from ..rest import RESTClient
+        test_headers = {
+            'Content-Type': 'application/vnd.ms-excel'
+        }
+        RESTClient.POST('fake_url', headers=test_headers)
+        request_mock.assert_called_with(
+            'POST', 'fake_url', body=None, headers=test_headers)
+
+    @patch('ambition.rest.RESTClientObject.agent')
+    def test_get_query_params(self, rest_agent):
+        """
+        Verify that the rest client supports the extended set of content types
+        """
+        request_mock = MagicMock(return_value=MagicMock(status=200, data=''))
+        rest_agent.return_value.request = request_mock
+        from ..rest import RESTClient
+        query_params = {'foo': 'bar'}
+        RESTClient.GET('fake_url', query_params=query_params)
+        request_kwargs = {
+            'fields': query_params,
+            'headers': {
+                'Content-Type': 'application/json',
+            },
+        }
+        request_mock.assert_called_with('GET', 'fake_url', **request_kwargs)
+
+    def test_agent_selector(self):
+        """
+        Verifies that the pool with the right scheme is returned
+        """
+        from ..rest import RESTClientObject
+        client = RESTClientObject()
+        http_url = 'http://www.google.com'
+        self.assertIsNone(client.agent(http_url).connection_pool_kw.get('cert_reqs'))
+        https_url = 'https://www.google.com'
+        self.assertIsNotNone(client.agent(https_url).connection_pool_kw.get('cert_reqs'))
+
+    @patch('ambition.rest.RESTClientObject.request')
+    def test_put(self, request_mock):
+        """
+        Verifies that put correctly wraps the request method
+        """
+        from ..rest import RESTClientObject
+        client = RESTClientObject()
+        url = 'http://www.example.com'
+        client.PUT(url)
+        request_mock.assert_called_with(
+            'PUT', url, headers=None, post_params=None, body=None)
+
+    def test_error_response(self):
+        """
+        Verifies that API errors result in well constructed objects
+        """
+        from ..rest import ApiException
+        response = {
+            'status': 500,
+            'reason': 'just because',
+            'data': 'not}{json',
+            'headers': []
+        }
+        exception = ApiException(MagicMock(**response))
+        self.assertEqual(type(str(exception)), str)
+
+    @patch('ambition.rest.RESTClientObject.PATCH')
+    def test_rest_client_patch(self, patch):
+        """
+        Verifies that the patch method invokes the corresponding method on the
+        rest client object
+        """
+        from ..rest import RESTClient
+        client = RESTClient()
+        client.PATCH()
+        patch.assert_called_with()
+
+    @patch('ambition.rest.RESTClientObject.DELETE')
+    def test_rest_client_delete(self, delete):
+        """
+        Verifies that the delete method invokes the corresponding method on the
+        rest client object
+        """
+        from ..rest import RESTClient
+        client = RESTClient()
+        client.DELETE()
+        delete.assert_called_with()
+
+    @patch('ambition.rest.RESTClientObject.agent')
+    def test_multipart_boundary_allowed(self, rest_agent):
+        """
+        Verifies that the API client doesn't override urllib's definition of
+        the multipart form boundary
+        """
+        request_mock = MagicMock(return_value=MagicMock(status=200, data=''))
+        rest_agent.return_value.request = request_mock
+        from ..rest import RESTClientObject
+        client = RESTClientObject()
+        post_params = {'foo': 'bar'}
+        headers = {'Content-Type': 'multipart/form-data'}
+        client.request(
+            'POST', 'http://example.com/', post_params=post_params,
+            headers=headers)
+        request_mock.assert_called_with(
+            'POST', 'http://example.com/', fields=post_params,
+            encode_multipart=True, headers={}
+        )
+
+    @patch('ambition.rest.RESTClientObject.agent')
+    def test_query_params_are_added_to_url(self, rest_agent):
+        """
+        Verify that query params are appended to the url correctly
+        """
+        request_mock = MagicMock(return_value=MagicMock(status=200, data=''))
+        rest_agent.return_value.request = request_mock
+        from ..rest import RESTClientObject
+        client = RESTClientObject()
+        query_params = {'foo': 'bar'}
+        client.request(
+            'POST', 'http://example.com/', query_params=query_params,
+            body=query_params
+        )
+        expected_headers = {'Content-Type': 'application/json'}
+        request_mock.assert_called_with(
+            'POST', 'http://example.com/?foo=bar', body=json.dumps(query_params),
+            headers=expected_headers
+        )
+
+    @patch('ambition.rest.RESTClientObject.agent')
+    def test_api_error_raises_exception(self, rest_agent):
+        """
+        Verify that an api error response will raise an exception
+        """
+        request_mock = MagicMock(return_value=MagicMock(status=500, data=''))
+        rest_agent.return_value.request = request_mock
+        from ..rest import RESTClientObject
+        client = RESTClientObject()
+        query_params = {'foo': 'bar'}
+        request = partial(
+            client.request, 'POST', 'http://example.com/',
+            query_params=query_params, body='')
+        self.assertRaises(ApiException, request)
+
+    @patch('ambition.rest.RESTClientObject.agent')
+    @patch('ambition.rest.sys')
+    def test_python_3_response_is_decoded(self, rest_sys, rest_agent):
+        """
+        Verify that the bytestream in python 3 responses is decoded properly
+        """
+        rest_sys.version_info.major = 3
+        # create bytestring
+        data = '\xe0\xb2\xa0_\xe0\xb2\xa0'
+        request_mock = MagicMock(return_value=MagicMock(status=500, data=data))
+        rest_agent.return_value.request = request_mock
+        from ..rest import RESTClientObject
+        client = RESTClientObject()
+        # change content type
+        headers = {'Content-Type': 'application/octet-stream'}
+        request = partial(
+            client.request, 'POST', 'http://example.com/', headers=headers)
+        self.assertRaises(ApiException, request)
