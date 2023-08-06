cd54a75d640cc22d95bbc15076649bf2803e2693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:32 +0000

    Add package mf2util/0.2.3/mf2util-0.2.3.tar.gz

---
+++ packages/mf2util/mf2util-0.2.3.tar.gz/mf2util-0.2.3/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+from distutils.core import setup
+from setuptools.command.test import test as TestCommand
+import os.path
+import sys
+import codecs
+
+
+def readme():
+    with codecs.open(
+            os.path.join(os.path.dirname(__file__), 'README.rst'),
+            encoding='utf-8') as f:
+        return f.read()
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+setup(name='mf2util',
+      version='0.2.3',
+      description='Python Microformats2 utilities, a companion to mf2py',
+      long_description=readme(),
+      author='Kyle Mahan',
+      author_email='kyle@kylewm.com',
+      url='http://indiewebcamp.com/mf2util',
+      packages=['mf2util'],
+      tests_require=['pytest'],
+      cmdclass={'test': PyTest},
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Natural Language :: English',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Topic :: Text Processing :: Markup :: HTML'
+      ]
+  )
