8f9a0cbeaae3d98134b8511fe7c83c8bebb54593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:29 +0000

    Add package keyor/1.1.0/keyor-1.1.0.zip

---
+++ packages/keyor/keyor-1.1.0.zip/keyor-1.1.0/keyor.py
@@ -0,0 +1,8 @@
+def print_jx(the_list，level):
+	for each_item in the_list:
+		if isinstance(each_item,list):
+			print_jx(each_item,level)
+		else:
+			for tab_stop in range(level):
+				print("\t",end='')
+			print(each_item)
