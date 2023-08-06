3f29e854366bbfee6cf75bdc50fa470fda965ff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:30 +0000

    Add package pidan/1.0.7/pidan-1.0.7.zip

---
+++ packages/pidan/pidan-1.0.7.zip/pidan-1.0.7/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+ 
+setup (
+    name = 'pidan',
+    version = '1.0.7',
+    packages=["pidan"],
+    platforms=['any'],
+    author = 'chengang',
+    author_email = 'chengang.net@gmail.com',
+    description = 'html module add filter_tags2',
+    package_data = {
+        '': ['*.data'],
+    },
+)
