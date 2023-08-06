99396eaa68a9b48e4e1e99b0f0ecdd0cb76d99ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:24 +0000

    Add package pyjavaprops/1.0.0/pyjavaprops-1.0.0.tar.gz

---
+++ packages/pyjavaprops/pyjavaprops-1.0.0.tar.gz/pyjavaprops-1.0.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open("README.rst") as src:
+    readme = src.read()
+
+version = '1.0.0'
+
+setup(
+    name='pyjavaprops',
+    version=version,
+    description='Tool to parse Java style properties file',
+    long_description=readme,
+    author='Luis Carlos Berrocal',
+    author_email ='luis.berrocal.1942@gmail.com',
+    packages=find_packages(),
+    url='https://github.com/luiscberrocal/pyjavaprops',
+    download_url = 'https://github.com/luiscberrocal/pyjavaprops/archive/v%s.zip' % version,
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: User Interfaces',
+        'Topic :: Terminals'
+    ],
+    install_requires=[
+    ],
+    entry_points = {
+        'console_scripts': ['list-settings=pyjavaprops.settings.base:main'],
+    },
+    test_suite="tests",
+)
