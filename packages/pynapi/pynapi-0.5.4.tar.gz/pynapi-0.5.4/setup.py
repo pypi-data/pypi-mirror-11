e059b8470f893ed09b5632f296482b9dd2a02dd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:26 +0000

    Add package pynapi/0.5.4/pynapi-0.5.4.tar.gz

---
+++ packages/pynapi/pynapi-0.5.4.tar.gz/pynapi-0.5.4/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+long_desc = """
+cmdline tool for downloading subtitle files from various subtitle sites
+(currently only napiprojekt.pl is supported)
+"""
+
+setup(
+    name = "pynapi",
+    author = 'Grzegorz Dzięgielewski',
+    author_email = 'jabbas@jabbas.eu',
+    url = "https://github.com/jabbas/pynapi",
+    license = 'GPLv3',
+    version = '0.5.4',
+    description = 'subtitles downloader',
+    long_description = long_desc,
+    entry_points = { 'console_scripts': [ 'pynapi = pynapi.cmdline:cmdline' ] },
+    packages = find_packages()
+)
