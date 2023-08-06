09c582abd1ca257f4fee27e0056281c050b5ffa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:10 +0000

    Add package mailchimp3/1.0.11/mailchimp3-1.0.11-py2.py3-none-any.whl

---
+++ packages/mailchimp3/mailchimp3-1.0.11-py2.py3-none-any.whl/mailchimp3/__init__.py
@@ -0,0 +1,50 @@
+"""
+Mailchimp v3 Api SDK
+
+"""
+from mailchimpclient import MailChimpClient
+from entities.authorizedapp import AuthorizedApp
+from entities.automation import Automation
+from entities.message import Message
+from entities.campaign import Campaign
+from entities.feedback import Feedback
+from entities.conversation import Conversation
+from entities.abuse import Abuse
+from entities.listactivity import ListActivity
+from entities.memberactivity import MemberActivity
+from entities.client import Client
+from entities.list import List
+from entities.growth import Growth
+from entities.template import Template
+from entities.interest import Interest
+from entities.category import Category
+from entities.goal import Goal
+from entities.member import Member
+
+
+class MailChimp(MailChimpClient):
+    """
+    MailChimp class to communicate with the v3 API
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize the class with your user_id and secret_key
+        """
+        super(MailChimp, self).__init__(*args, **kwargs)
+        self.authorized_app = AuthorizedApp(self)
+        self.automation = Automation(self)
+        self.campaign = Campaign(self)
+        self.campaignfeedback = Feedback(self)
+        self.conversation = Conversation(self)
+        self.message = Message(self)
+        self.listactivity = ListActivity(self)
+        self.abuse = Abuse(self)
+        self.client = Client(self)
+        self.list = List(self)
+        self.growth = Growth(self)
+        self.template = Template(self)
+        self.category = Category(self)
+        self.interest = Interest(self)
+        self.memberactivity = MemberActivity(self)
+        self.goal = Goal(self)
+        self.member = Member(self)
---
+++ packages/mailchimp3/mailchimp3-1.0.11-py2.py3-none-any.whl/mailchimp3/mailchimpclient.py
@@ -0,0 +1,41 @@
+"""
+Mailchimp v3 Api SDK
+
+"""
+import requests
+from requests.auth import HTTPBasicAuth
+from urlparse import urljoin
+
+
+class MailChimpClient(object):
+    """
+    MailChimp class to communicate with the v3 API
+    """
+    def __init__(self, mc_user, mc_secret):
+        """
+        Initialize the class with you user_id and secret_key
+        """
+        super(MailChimpClient, self).__init__()
+        self.auth = HTTPBasicAuth(mc_user, mc_secret)
+        datacenter = mc_secret.split('-').pop()
+        self.base_url = 'https://%s.api.mailchimp.com/3.0/' % datacenter
+
+    def _post(self, url, data=None):
+        url = urljoin(self.base_url, url)
+        r = requests.post(url, auth=self.auth, json=data)
+        return r.json()
+
+    def _get(self, url):
+        url = urljoin(self.base_url, url)
+        r = requests.get(url, auth=self.auth)
+        return r.json()
+
+    def _delete(self, url):
+        url = urljoin(self.base_url, url)
+        r = requests.delete(url, auth=self.auth)
+        return r.json()
+
+    def _patch(self, url, data=None):
+        url = urljoin(self.base_url, url)
+        r = requests.patch(url, auth=self.auth, json=data)
+        return r.json()
