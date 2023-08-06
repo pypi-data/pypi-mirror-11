4123ce6371382fb0b8e6d6b96db9801cb0074576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:29 +0000

    Add package keybaseio-api/1.0.3.post1/keybaseio-api-1.0.3.post1.tar.gz

---
+++ packages/keybaseio-api/keybaseio-api-1.0.3.post1.tar.gz/keybaseio-api-1.0.3.post1/setup.py
@@ -0,0 +1,57 @@
+from setuptools import setup
+
+import keybaseapi
+
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest, sys
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+
+setup(
+    name='keybaseio-api',
+    version=keybaseapi.__version__,
+    packages=['keybaseapi'],
+    install_requires=[
+        "requests",
+        "ConfigMaster>=2.3.5",
+        "py-pgp>=0.0.1",
+    ],
+    tests_require=[
+        'pytest',
+        'pytest-cov',
+        'coverage',
+        'coveralls'
+    ],
+    cmdclass = {'test': PyTest},
+    url='https://github.com/SkierPGP/keybase-python3',
+    license='GPLv3',
+    author='Isaac Dickinson',
+    author_email='eyesismine@gmail.com',
+    description='A keybase.io API for Python 3',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Security",
+        "Topic :: Security :: Cryptography"
+    ]
+)
