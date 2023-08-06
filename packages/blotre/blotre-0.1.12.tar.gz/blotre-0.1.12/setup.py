3ebfce6c073586d70bec4bd834f5bf60362ec166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:48 +0000

    Add package blotre/0.1.12/blotre-0.1.12.tar.gz

---
+++ packages/blotre/blotre-0.1.12.tar.gz/blotre-0.1.12/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='blotre',
+    version='0.1.12',
+    author="Matt Bierner",
+    author_email="mattbierner@users.noreply.github.com",
+    license="MIT",
+    description="Thin Python Blot're client.",
+    url="https://github.com/mattbierner/blotre-py",
+    py_modules=['blotre'],
+    install_requires=[
+        'requests'],
+    keywords = ['blotre', "blot're", 'REST', 'oauth2']
+)
