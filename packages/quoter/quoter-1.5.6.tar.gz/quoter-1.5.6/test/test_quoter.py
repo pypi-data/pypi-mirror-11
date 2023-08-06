97f9417a1238545a02f3f9a6fa0e270ddb14f0a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:19 +0000

    Add package quoter/1.5.6/quoter-1.5.6.tar.gz

---
+++ packages/quoter/quoter-1.5.6.tar.gz/quoter-1.5.6/test/test_quoter.py
@@ -0,0 +1,209 @@
+from quoter import *
+import pytest
+
+
+def test_braces():
+    assert braces('this') == '{this}'
+    assert braces('this', padding=1) == '{ this }'
+    assert braces('this', margin=1) == ' {this} '
+    assert braces('this', padding=1, margin=1) == ' { this } '
+
+
+def test_brackets():
+    assert brackets('this') == '[this]'
+    assert brackets('this', padding=1) == '[ this ]'
+    assert brackets('this', margin=1) == ' [this] '
+    assert brackets('this', padding=1, margin=1) == ' [ this ] '
+
+
+def test_clone():
+    # make sure clones properly registered just like normally defined
+    # styles, that clones are different from original, that they yield
+    # the expected results, and that after cloning, the originals
+    # behave exactly as before
+
+    bar2 = brackets.clone(margin=2, name='bar2')
+    assert bar2 is not brackets
+    assert bar2('this') == '  [this]  '
+    assert brackets('this') == '[this]'
+    assert quote.bar2('this') == '  [this]  '
+
+    bb = html.b.clone(padding=1, margin=2)
+    assert bb is not html.b
+    assert 'bb' not in HTMLQuoter.styles
+    assert bb('this') == '  <b> this </b>  '
+    assert html.b('this') == '<b>this</b>'
+
+    sp = html.b.but(atts='span')
+    assert sp is not html.b
+    assert sp('something') == '<span>something</span>'
+    assert sp('else', '.other') == "<span class='other'>else</span>"
+
+
+def test_but():
+    """
+    Test the but alias for clone.
+    """
+    bar2 = brackets.but(margin=2)
+    assert bar2 is not brackets
+    assert bar2('this') == '  [this]  '
+    assert brackets('this') == '[this]'
+
+    bb = html.b.but(padding=1, margin=2)
+    assert bb is not html.b
+    assert bb('this') == '  <b> this </b>  '
+    assert html.b('this') == '<b>this</b>'
+
+
+def test_set():
+    bar2 = brackets.clone(margin=2, name='bar2')
+    assert bar2('this') == '  [this]  '
+    assert quote.bar2('this') == '  [this]  '
+    # check that original object is not changed
+    assert brackets('this') == '[this]'
+    assert quote.brackets('this') == '[this]'
+
+    # check setting options
+    bar2.set(margin=1, padding=1)
+    assert bar2('this') == ' [ this ] '
+    assert quote.bar2('this') == ' [ this ] '
+    # check that original object is not changed
+    assert brackets('this') == '[this]'
+    assert quote.brackets('this') == '[this]'
+
+    # check setting options
+    bar2.set(prefix='||', padding=1)
+    assert bar2('this') == ' || this ] '
+    assert quote.bar2('this') == ' || this ] '
+    # check that original object is not changed
+    assert brackets('this') == '[this]'
+    assert quote.brackets('this') == '[this]'
+
+def test_set_example():
+    bars = Quoter('|')
+    assert bars('x') == '|x|'
+    bars.set(prefix='||', suffix='||')
+    assert bars('x') == '||x||'
+    bars.set(padding=1)
+    assert bars('x') == '|| x ||'
+
+    bart = bars.clone(prefix=']', suffix = '[')
+    assert bart('x') == '] x ['
+
+
+# NB There is an error in namespace managment.
+# HTML attributes should be limited to HTMLQuoter,
+# but there is currently leakage. Styles seem to be
+# defined in Quoter, not the most proximate superclass
+
+
+@pytest.mark.skipif(True, reason='road closed')
+def test_chars():
+    percent = Quoter(chars='%%')
+    assert percent('something') == '%something%'
+    doublea = Quoter(chars='<<>>')
+    assert doublea('AAA') == '<<AAA>>'
+
+# From a quoter point of view, the chars kwarg is totally gilding the
+# lily, and unnecessary. But from the point of view of exploring and
+# exercising the underlying options package, it is quite interesting,
+# in that it requires a mapping of one user-level argument into two
+# different underlying parameters. It's a good edge-case for testing.
+
+
+def test_auto_stringification():
+    assert brackets(12) == '[12]'
+    assert braces(4.4) == '{4.4}'
+    assert double(None) == '"None"'
+    assert single(False) == "'False'"
+
+
+def test_shortcuts():
+    assert ' '.join([qs('one'), qd('two'), qt('three'), qb('and'), qdb('four')]) == \
+        "'one' \"two\" \"\"\"three\"\"\" `and` ``four``"
+
+
+def test_instant():
+    assert Quoter('+[ ', ' ]+')('castle') == '+[ castle ]+'
+
+
+def test_lambda():
+    f = lambda v: ('(', abs(v), ')') if v < 0 else ('', v, '')
+    financial = LambdaQuoter(f)
+    assert financial(-10) == '(10)'
+    assert financial(44) == '44'
+
+    password = LambdaQuoter(lambda v: ('', 'x' * len(v), ''))
+    assert password('secret!') == 'xxxxxxx'
+
+    wf = lambda v:  ('**', v, '**') if v < 0 else ('', v, '')
+    warning = LambdaQuoter(wf, name='warning')
+    assert warning(12) == '12'
+    assert warning(-99) == '**-99**'
+    assert warning(-99, padding=1) == '** -99 **'
+
+    assert lambdaq.warning(12) == '12'
+    assert lambdaq.warning(-99) == '**-99**'
+    assert lambdaq.warning(-99, padding=1) == '** -99 **'
+
+
+def test_lambdaq_named_style():
+    assert lambdaq(44, style='warning') == '44'
+    assert lambdaq(-44, style='warning') == '**-44**'
+    
+
+def test_examples():
+    assert single('this') == "'this'"
+    assert double('that') == '"that"'
+    assert backticks('ls -l') == "`ls -l`"
+    assert braces('curlycue') == "{curlycue}"
+    assert braces('curlysue', padding=1) == '{ curlysue }'
+
+    bars = Quoter('|')
+    assert bars('x') == '|x|'
+
+    plus = Quoter('+', '')
+    assert plus('x') == '+x'
+
+    variable = Quoter('${', '}', name='variable')
+    assert variable('x') == '${x}'
+
+
+def test_attribute_invocations():
+    assert single('something') == quote.single('something')
+    assert single('something', margin=2, padding=3) == quote.single('something', margin=2, padding=3)
+    assert braces('b') == quote.braces('b')
+
+    # now test wholesale
+    names = 'braces brackets angles parens qs qd qt qb single double triple ' +\
+            'backticks anglequote guillemet curlysingle curlydouble'
+    for name in names.split():
+        main = eval(name)
+        attr = eval('quote.' + name)
+        assert main is attr
+        assert main('string') == attr('string')
+
+
+def test_quote_shortcut():
+    variable = Quoter('${', '}', name='variable')
+    assert variable('x') == '${x}'
+
+    assert quote('myvar', style='variable') == '${myvar}'
+
+    assert quote('this', style='braces') == '{this}'
+
+
+def test_named_quote_style():
+    assert quote('this', style='variable') == '${this}'
+    assert braces('this', style='variable') == '${this}'
+
+
+def test_redef():
+    braces = Quoter('{', '}', padding=1, name='braces')
+    assert braces('this') == '{ this }'
+    assert braces('this', padding=0) == '{this}'
+
+
+def test_bad_style_names():
+    with pytest.raises(BadStyleName):
+        v = Quoter('v', name="_v")
