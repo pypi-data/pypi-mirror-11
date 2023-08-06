c1ade7c26272ed5821135381318826afa6e96345
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:23 +0000

    Add package letter-list/1.0.0/letter-list-1.0.0.zip

---
+++ packages/letter-list/letter-list-1.0.0.zip/letter-list-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name ='letter-list',
+        version='1.0.0',
+        py_modules=['letter'],
+        author='lcy',
+        author_email='1510474430@qq.com',
+        url='',
+        description='letter list',
+      )
