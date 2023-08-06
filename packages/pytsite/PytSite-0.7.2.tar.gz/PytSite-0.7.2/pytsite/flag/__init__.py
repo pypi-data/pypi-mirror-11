06bdaf33c046691807430364e84292053e092433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:04 +0000

    Add package pytsite/0.7.2/PytSite-0.7.2.tar.gz

---
+++ packages/pytsite/PytSite-0.7.2.tar.gz/PytSite-0.7.2/pytsite/flag/__init__.py
@@ -0,0 +1,30 @@
+"""Flag Plugin Init.
+"""
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+
+def __init():
+    import sys
+    from pytsite.core import odm, lang, tpl, assetman, events
+    from . import _model
+
+    def router_dispatch_eh():
+        assetman.add('pytsite.flag@css/common.css')
+        assetman.add('pytsite.flag@js/common.js')
+
+    lang.register_package(__name__)
+    assetman.register_package(__name__)
+    tpl.register_package(__name__)
+    tpl.register_global('flag', sys.modules[__package__])
+    odm.register_model('flag', _model.Flag)
+
+    events.listen('pytsite.core.router.dispatch', router_dispatch_eh)
+
+__init()
+
+
+# Public API
+from . import _widget as widget
+from ._functions import count
