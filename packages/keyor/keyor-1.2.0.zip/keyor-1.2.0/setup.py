757252a993df019c490403a3e2e55c5f3ecaa01b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:32 +0000

    Add package keyor/1.2.0/keyor-1.2.0.zip

---
+++ packages/keyor/keyor-1.2.0.zip/keyor-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup 
+setup( 					#下面都是setup函数的参数名
+		 name = 'keyor', 
+		 version = '1.2.0', 		#版本号
+		 py_modules = ['keyor'], 
+		 author = 'resalg', 		#作者
+		 author_email = 'rainsky512@gmail.com', 
+		 url = 'http://yuitaku.github.io', 
+		 description = 'A simple printer of nester lists  This is Mode Exercise  '  	#简介内容
+		 )
