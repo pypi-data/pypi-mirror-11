1c5597f477cb968225472a6cea04e3ca9d5d19b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:05 +0000

    Add package django-ddp/0.12.2/django_ddp-0.12.2-py2-none-any.whl

---
+++ packages/django-ddp/django_ddp-0.12.2-py2-none-any.whl/dddp/apps.py
@@ -0,0 +1,32 @@
+"""Django DDP app config."""
+
+from __future__ import print_function
+
+from django.apps import AppConfig
+from django.conf import settings, ImproperlyConfigured
+
+from dddp import autodiscover
+
+
+class DjangoDDPConfig(AppConfig):
+
+    """Django app config for django-ddp."""
+
+    api = None
+    name = 'dddp'
+    verbose_name = 'Django DDP'
+    _in_migration = False
+
+    def ready(self):
+        """Initialisation for django-ddp (setup lookups and signal handlers)."""
+        if not settings.DATABASES:
+            raise ImproperlyConfigured('No databases configured.')
+        for (alias, conf) in settings.DATABASES.items():
+            if conf['ENGINE'] != 'django.db.backends.postgresql_psycopg2':
+                raise ImproperlyConfigured(
+                    '%r uses %r: django-ddp only works with PostgreSQL.' % (
+                        alias, conf['backend'],
+                    )
+                )
+        self.api = autodiscover()
+        self.api.ready()
