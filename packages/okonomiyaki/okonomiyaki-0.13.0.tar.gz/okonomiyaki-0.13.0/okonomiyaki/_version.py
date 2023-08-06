56d681220f788b9fed2a5cfd58a3a06f5af16f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:37 +0000

    Add package okonomiyaki/0.13.0/okonomiyaki-0.13.0.tar.gz

---
+++ packages/okonomiyaki/okonomiyaki-0.13.0.tar.gz/okonomiyaki-0.13.0/okonomiyaki/_version.py
@@ -0,0 +1,11 @@
+# THIS FILE IS GENERATED FROM OKONOMIYAKI SETUP.PY
+version = '0.13.0'
+full_version = '0.13.0'
+git_revision = '266f18c0813828996d9b95f0bdccac627c22ad3d'
+is_released = True
+
+if is_released:
+    version_info = (0, 13, 0, 'final', 0)
+else:
+    version = full_version
+    version_info = (0, 13, 0, 'dev', 611)
