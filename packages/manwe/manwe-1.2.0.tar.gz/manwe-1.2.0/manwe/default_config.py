c919873936b6ca340ec0ea4b2dc08db62f09ad07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:15 +0000

    Add package manwe/1.2.0/manwe-1.2.0.tar.gz

---
+++ packages/manwe/manwe-1.2.0.tar.gz/manwe-1.2.0/manwe/default_config.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Manwë default configuration settings.
+"""
+
+
+#: Varda API root endpoint.
+API_ROOT = 'http://127.0.0.1:5000'
+
+#: Varda API authentication token.
+TOKEN = None
+
+#: Number of resources to query per collection request.
+COLLECTION_CACHE_SIZE = 20
+
+#: Size of chunks to yield from data iterator in bytes.
+DATA_BUFFER_SIZE = 1024 * 1024
