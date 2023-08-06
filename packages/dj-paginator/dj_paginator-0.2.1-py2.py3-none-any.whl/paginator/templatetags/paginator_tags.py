ba22c05ef91a774207800f961658feffb736f020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:14 +0000

    Add package dj-paginator/0.2.1/dj_paginator-0.2.1-py2.py3-none-any.whl

---
+++ packages/dj-paginator/dj_paginator-0.2.1-py2.py3-none-any.whl/paginator/templatetags/paginator_tags.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django import template
+
+from paginator.paginators import paginate
+
+register = template.Library()
+
+PAGINATOR_THEME = getattr(settings, "PAGINATOR_THEME", "bootstrap")
+BASIC_THEME = 'paginator/{}/paginator.html'.format(PAGINATOR_THEME)
+BOOTSTRAP_THEME = 'paginator/bootstrap/paginator.html'
+
+
+@register.inclusion_tag(BOOTSTRAP_THEME, takes_context=True)
+def bootstrap_paginator(context):
+    return paginate(context)
+
+
+@register.inclusion_tag(BASIC_THEME, takes_context=True)
+def paginator(context):
+    return paginate(context)
