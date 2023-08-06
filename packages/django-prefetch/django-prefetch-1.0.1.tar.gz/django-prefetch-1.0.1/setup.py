7d08a17e28671774fed0042ed747d38cfca26f25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:30 +0000

    Add package django-prefetch/1.0.1/django-prefetch-1.0.1.tar.gz

---
+++ packages/django-prefetch/django-prefetch-1.0.1.tar.gz/django-prefetch-1.0.1/setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from __future__ import absolute_import, print_function
+
+import io
+import os
+import re
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import relpath
+from os.path import splitext
+
+from setuptools import find_packages
+from setuptools import setup
+
+def read(*names, **kwargs):
+    return io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ).read()
+
+
+setup(
+    name='django-prefetch',
+    version='1.0.1',
+    license='BSD',
+    description='Generic model related data prefetch framework for Django.',
+    long_description=read('README.rst'),
+    author='Ionel Cristian Maries',
+    author_email='contact@ionelmc.ro',
+    url='https://github.com/ionelmc/django-prefetch',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Utilities',
+    ],
+    install_requires=['Django>=1.1']
+)
