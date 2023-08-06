d27d8bd24de3accfadd7e9f34f54cfb045602241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:41 +0000

    Add package pfp/0.1.12/pfp-0.1.12.tar.gz

---
+++ packages/pfp/pfp-0.1.12.tar.gz/pfp-0.1.12/tests/utils.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import six
+import sys
+import pfp
+import pfp.utils
+
+class UtilsMixin(object):
+	def _test_parse_build(self, data, template, stdout=None, debug=False, predefines=False, verify=True):
+		if stdout is not None:
+			fake_stdout = sys.stdout = six.StringIO()
+
+		dom = pfp.parse(six.StringIO(data), template, debug=debug, predefines=predefines)
+
+		if stdout is not None:
+			sys.stdout = sys.__stdout__
+			output = fake_stdout.getvalue()
+			self.assertEqual(output, stdout)
+
+		if verify:
+			self.assertEqual(dom._pfp__build(), pfp.utils.binary(data))
+
+		return dom
