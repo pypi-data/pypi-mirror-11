235715738aa175d9f3a2cf769189185a17db51ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:50 +0000

    Add package rg_pynester/1.2.0/rg_pynester-1.2.0.tar.gz

---
+++ packages/rg_pynester/rg_pynester-1.2.0.tar.gz/rg_pynester-1.2.0/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(
+  name  = 'rg_pynester',
+  version  = '1.2.0',
+  author  = 'rajat',
+  author_email  = 'rajat.gupta@nsitonline.in',
+  description  = 'List Printer')
