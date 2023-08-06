45d2868d97c566cb02ce04cb23d18c3c9533dc59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:21 +0000

    Add package signalfd/0.1.0/signalfd-0.1.0.tar.gz

---
+++ packages/signalfd/signalfd-0.1.0.tar.gz/signalfd-0.1.0/tests/test_signalfd.py
@@ -0,0 +1,22 @@
+import os
+import signal
+
+import pytest
+
+import signalfd
+
+def test_block_and_read():
+    fd = signalfd.signalfd(-1, [signal.SIGUSR1], signalfd.SFD_CLOEXEC)
+    try:
+        signalfd.sigprocmask(signalfd.SIG_BLOCK, [signal.SIGUSR1])
+        os.kill(os.getpid(), signal.SIGUSR1)
+        si = signalfd.read_siginfo(fd)
+        assert si.ssi_signo == signal.SIGUSR1
+    finally:
+        os.close(fd)
+
+
+def test_read_closed():
+    fd = signalfd.signalfd(-1, [signal.SIGUSR1], signalfd.SFD_CLOEXEC)
+    os.close(fd)
+    pytest.raises(OSError, signalfd.read_siginfo, fd)
