8b65fe9772b14dc6721fc401a7aead670b106fa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:40 +0000

    Add package cotoolz/0.1.3/cotoolz-0.1.3.tar.gz

---
+++ packages/cotoolz/cotoolz-0.1.3.tar.gz/cotoolz-0.1.3/cotoolz/__init__.py
@@ -0,0 +1,17 @@
+from .include import get_include
+from ._coiter import coiter
+from ._comap import comap
+from ._cozip import cozip
+from ._emptycoroutine import emptycoroutine
+
+
+__version__ = '0.1.3'
+
+
+__all__ = [
+    'coiter',
+    'comap',
+    'cozip',
+    'emptycoroutine',
+    'get_include',
+]
