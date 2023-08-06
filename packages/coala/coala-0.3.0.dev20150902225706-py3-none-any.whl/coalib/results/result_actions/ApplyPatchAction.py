17902a9d20d096de2ac35b9dbe58983a2c9668de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:52 +0000

    Add package coala/0.3.0.dev20150902225706/coala-0.3.0.dev20150902225706-py3-none-any.whl

---
+++ packages/coala/coala-0.3.0.dev20150902225706-py3-none-any.whl/coalib/results/result_actions/ApplyPatchAction.py
@@ -0,0 +1,22 @@
+from coalib.results.result_actions.ResultAction import ResultAction
+from coalib.results.PatchResult import PatchResult
+
+
+class ApplyPatchAction(ResultAction):
+    @staticmethod
+    def is_applicable(result):
+        if isinstance(result, PatchResult):
+            return True
+        return False
+
+    def apply(self, result, original_file_dict, file_diff_dict):
+        """
+        Apply the patch automatically.
+        """
+        for filename in result.diffs:
+            if filename in file_diff_dict:
+                file_diff_dict[filename] += result.diffs[filename]
+            else:
+                file_diff_dict[filename] = result.diffs[filename]
+
+        return file_diff_dict
