312c6864b8519a3386fde7e3fa9026b0ad590f51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:29 +0000

    Add package civeng/0.2.1/civeng-0.2.1.tar.gz

---
+++ packages/civeng/civeng-0.2.1.tar.gz/civeng-0.2.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='civeng',
+      version='0.2.1',
+      description='civil structural engineering tools',
+      url='http://github.com/storborg/funniest',
+      author='dutu8ure',
+      author_email='dutu8ure@gmail.com',
+      license='MIT',
+      packages=['civeng'],
+      zip_safe=False)
