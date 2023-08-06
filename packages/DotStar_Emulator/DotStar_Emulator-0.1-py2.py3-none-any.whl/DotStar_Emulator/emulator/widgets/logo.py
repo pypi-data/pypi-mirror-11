c5eaaa397a662e39ea2e0c6176a580b47b7a46a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:28 +0000

    Add package DotStar_Emulator/0.1/DotStar_Emulator-0.1-py2.py3-none-any.whl

---
+++ packages/DotStar_Emulator/DotStar_Emulator-0.1-py2.py3-none-any.whl/DotStar_Emulator/emulator/widgets/logo.py
@@ -0,0 +1,30 @@
+from DotStar_Emulator.emulator.gui import widget
+from DotStar_Emulator.emulator import globals, config
+
+
+class LogoWidget(widget.Widget):
+
+    def __init__(self, use_surface=True):
+        """
+        Widget for display the emulator apps logo
+
+        :param use_surface: `Boolean` Use widget surface
+        :return:
+        """
+
+        super(LogoWidget, self).__init__(use_surface=use_surface)
+
+        self.layout.margin.set(0, 5, 10, 5)
+
+    def on_render(self):
+        self.surface.fill(config.get("BRAND_BOX_BG"))
+
+        font = globals.current_app.get_font(22)
+
+        c = config.get("BRAND_TITLE")
+        text = font.render(config.get("WINDOW_CAPTION"), True, c)
+        text_pos = text.get_rect()
+
+        text_pos.center = self.surface.get_rect().center
+        self.surface.blit(text, text_pos)
+
