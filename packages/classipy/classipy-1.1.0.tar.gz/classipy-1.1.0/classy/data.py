1f4ec5f9434340760fd9d5d91af4b1ea5fba7994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:30 +0000

    Add package classipy/1.1.0/classipy-1.1.0.tar.gz

---
+++ packages/classipy/classipy-1.1.0.tar.gz/classipy-1.1.0/classy/data.py
@@ -0,0 +1,174 @@
+"""
+.. py:module:: classy.data
+   :synopsis: Data and I/O methods for classy.
+
+.. moduleauthor:: Florian Leitner <florian.leitner@gmail.com>
+.. License: GNU Affero GPL v3 (http://www.gnu.org/licenses/agpl.html)
+"""
+
+import logging
+import pickle
+from numpy.core.umath import isfinite
+from numpy import typecodes, asarray, unique, where, array
+from sklearn.preprocessing import label_binarize
+from random import sample
+from collections import namedtuple, Counter
+
+L = logging.getLogger(__name__)
+Data = namedtuple('Data', 'text_ids index labels label_names min_label')
+
+
+def make_data(inverted_index, text_ids=None, labels=None):
+    """
+    Create a namedtuple "Data" with the following properties:
+
+    - ``text_ids``: a list of document IDs (or None)
+    - ``index``: the actual, inverted index (as SciPy CSR matrix)
+    - ``labels``: a (binarized) array of document labels, as integers (or None)
+    - ``label_names``: the list of label names per 0-based label integer
+    - ``min_label``: the label integer that has the least number of examples
+                     in the dataset (i.e., the likely hardest label to learn)
+
+    :param inverted_index: the inverted index; having a ``tocsr()`` method
+    :param text_ids: the list of document IDs (or None)
+    :param labels: the list of document labels (or None)
+    :return: a Data namedtuple
+    """
+    if text_ids is not None and not isinstance(text_ids, list):
+        text_ids = list(text_ids)
+
+    if labels is not None:
+        labels = asarray(labels, str)
+        # noinspection PyTupleAssignmentBalance
+        uniq, inverse = unique(labels, return_inverse=True)
+        n_classes = len(uniq)
+        min_label = Counter(labels).most_common()[-1][0]
+        min_label = where(uniq == min_label)[0][0]
+
+        if n_classes == 1:
+            L.warn("only one target label", uniq[0])
+        elif n_classes > 2:
+            labels = label_binarize(labels, classes=uniq)
+        else:
+            labels = inverse
+
+        print('ordered labels:', "'{}'".format("', '".join(uniq)))
+    else:
+        uniq, min_label = None, None
+
+    size = inverted_index.shape[0] * inverted_index.shape[1]
+    zeros = size - inverted_index.nnz
+    L.debug("sparsity: %s/%s (%s%%)", zeros, size, (zeros * 100 // size))
+    return Data(text_ids, inverted_index.tocsr(), labels, uniq, min_label)
+
+
+def make_inverted_vocabulary(vocabulary_path, data):
+    voc = load_vocabulary(vocabulary_path, data)
+    cov = array(list(voc.keys()))
+    for word, idx in voc.items():
+        cov[idx] = word
+    return cov
+
+
+def get_n_rows(data):
+    """Get the number of rows/documents in ``data``."""
+    return data.index.shape[0]
+
+
+def get_n_cols(data):
+    """Get the number of columns/features (words) in ``data``."""
+    return data.index.shape[1]
+
+
+def check_integrity(data):
+    """Ensure the semantic integrity of the ``data``."""
+    L.info("index shape: %s doc_ids: %s, labels: %s", data.index.shape,
+           'None' if data.text_ids is None else len(data.text_ids),
+           'None' if data.labels is None else len(data.labels))
+    rows = get_n_rows(data)
+
+    if data.text_ids is not None and data.labels is not None:
+        if len(data.text_ids) != len(data.labels):
+            msg = 'length of IDs (%d) != length of labels (%d)'
+            raise ValueError(msg % (len(data.text_ids), len(data.labels)))
+
+    if data.text_ids is not None:
+        if len(data.text_ids) != rows:
+            msg = 'length of IDs (%d) != number of index rows (%d)'
+            raise ValueError(msg % (len(data.text_id), rows))
+
+    if data.labels is not None:
+        if len(data.labels) != rows:
+            msg = 'length of labels (%d) != number of index rows (%d)'
+            raise ValueError(msg % (len(data.labels), rows))
+
+    if data.index.dtype.char in typecodes['AllFloat'] and \
+            not isfinite(data.index.sum()) and \
+            not isfinite(data.index).all():
+        raise ValueError("index contains NaN, infinity"
+                         " or a value too large for %r." % data.index.dtype)
+
+
+def save_index(data, path):
+    """Save the ``data`` to ``path``."""
+    check_integrity(data)
+    L.debug("saving inverted index to '%s'", path)
+
+    with open(path, 'wb') as f:
+        pickle.dump(data, f)
+
+
+def load_index(path):
+    """Load a Data structure from ``path``."""
+    L.debug("loading inverted index from '%s'", path)
+
+    with open(path, 'rb') as f:
+        data = pickle.load(f)
+
+    check_integrity(data)
+    data = data._replace(index=data.index.asfptype())
+    return data
+
+
+def save_vocabulary(vocabulary, data, path):
+    """Save the ``vocabulary`` dict for ``data`` to ``path``."""
+    assert isinstance(vocabulary, dict), "vocabulary not a dict"
+    size = len(vocabulary)
+    L.info("vocabulary size: %s", size)
+    rnd_sample = sample(vocabulary.keys(), min(size, 50))
+    L.debug("vocabulary sample: %s", ', '.join(rnd_sample))
+    L.debug("saving vocabulary to '%s'", path)
+    n_cols = get_n_cols(data)
+
+    if len(vocabulary) != n_cols:
+        msg = 'length of vocabulary (%d) != number of index columns (%d)'
+        raise ValueError(msg % (len(vocabulary), n_cols))
+
+    with open(path, 'wb') as f:
+        pickle.dump(vocabulary, f)
+
+
+def load_vocabulary(path, data=None):
+    """
+    Load a vocabulary dict from ``path``,
+    optionally ensuring it could be appropriate for ``data``.
+    """
+    L.debug("loading vocabulary from '%s'", path)
+
+    with open(path, 'rb') as f:
+        vocabulary = pickle.load(f)
+
+    assert isinstance(vocabulary, dict), "vocabulary not a dict"
+    size = len(vocabulary)
+    L.info("vocabulary size: %s", size)
+    rnd_sample = sample(vocabulary.keys(), min(size, 10))
+    L.debug("vocabulary sample: %s", ', '.join(rnd_sample))
+
+    if data is not None:
+        n_cols = get_n_cols(data)
+
+        if len(vocabulary) != n_cols:
+            msg = 'length of vocabulary (%d) != number of index columns (%d)'
+            raise ValueError(msg % (len(vocabulary), n_cols))
+
+    return vocabulary
---
+++ packages/classipy/classipy-1.1.0.tar.gz/classipy-1.1.0/classy/select.py
@@ -0,0 +1,120 @@
+"""
+.. py:module:: classy.select
+   :synopsis: Feature selection techniques to minimize an inverted index.
+
+.. moduleauthor:: Florian Leitner <florian.leitner@gmail.com>
+.. License: GNU Affero GPL v3 (http://www.gnu.org/licenses/agpl.html)
+"""
+
+import logging
+from sklearn.feature_selection import SelectKBest, chi2, RFE
+from numpy import diff, ones, cumsum, where
+from scipy.sparse import csc_matrix
+from .data import load_vocabulary, save_index, load_index, save_vocabulary, \
+    get_n_cols
+from sklearn.svm import LinearSVC
+
+
+L = logging.getLogger(__name__)
+
+
+def select_features(args):
+    L.debug("%s", args)
+    data = load_index(args.index)
+
+    if args.vocabulary:
+        vocabulary = load_vocabulary(args.vocabulary)
+    else:
+        vocabulary = None
+
+    if args.cutoff > 1:
+        data = drop_features(args.cutoff, data, vocabulary)
+
+    if args.select > 0:
+        data = select_best(args.select, data, vocabulary)
+
+    if args.eliminate > 0:
+        data = eliminate_features(args.eliminate, data, vocabulary)
+
+    save_index(data, args.new_index)
+
+    if vocabulary and args.new_vocabulary:
+        save_vocabulary(vocabulary, data, args.new_vocabulary)
+
+
+def drop_features(min_df, data, vocabulary=None):
+    """
+    Prune features below some minimum document frequency ``min_df`` from the
+    vocabulary (in-place) and drop those columns from the inverted index.
+
+    :param vocabulary: vocabulary dictionary (will be updated in-place)
+    :param data: ``Data`` structure
+    :param min_df: integer; minimum document frequency
+    :return: a new ``Data`` structure
+    """
+    L.debug("dropping features below df=%s", min_df)
+    df = diff(csc_matrix(data.index, copy=False).indptr)  # document frequency
+    mask = ones(len(df), dtype=bool)  # mask: columns that can/cannot stay
+    mask &= df >= min_df  # create a "mask" of columns above cutoff
+    keep = where(mask)[0]  # determine which columns to keep
+    data = data._replace(index=data.index[:, keep])  # drop unused columns
+    prune(vocabulary, mask)
+    return data
+
+
+def eliminate_features(k, data, vocabulary=None, steps=100):
+    """
+    Recursively eliminate features from the set using a Linear SVM.
+    This will remove redundant features from the vocabulary and index.
+
+    :param k: number of features to keep
+    :param data: ``Data`` structure
+    :param vocabulary: vocabulary dictionary (will be updated in-place)
+    :param steps: number of recursive eliminations to make to get to k
+    :return: a new ``Data`` structure
+    """
+    L.debug("recursively eliminating features down to %s", k)
+    estimator = LinearSVC(penalty='l2', loss='hinge', class_weight='auto')
+    step = (get_n_cols(data) - k) // steps + 1
+    selector = RFE(estimator, k, step=step)
+    selector = selector.fit(data.index, data.labels)
+    mask = selector.support_
+    data = data._replace(index=data.index[:, mask])
+    prune(vocabulary, mask)
+    return data
+
+
+def select_best(k, data, vocabulary=None):
+    """
+    Select the top ``k`` most informative features (using a chi-square test)
+    and drop everything else from the index and vocabulary.
+
+    :param k: integer; the most informative features to maintain
+    :param data: ``Data`` structure
+    :param vocabulary: vocabulary dictionary (will be updated in-place)
+    :return: a new ``Data`` structure
+    """
+    L.debug("selecting K=%s best features", k)
+    selector = SelectKBest(chi2, k=k)
+    selector.fit(data.index, data.labels)
+    mask = selector._get_support_mask()
+    data = data._replace(index=data.index[:, mask])
+    prune(vocabulary, mask)
+    return data
+
+
+def prune(vocabulary, mask):
+    """Clean up the vocabulary with the given mask and new indices."""
+    if vocabulary:
+        new_idx = cumsum(mask) - 1  # new indices (with array len as old)
+
+        for word in list(vocabulary.keys()):
+            idx = vocabulary[word]
+
+            if mask[idx]:
+                # noinspection PyUnresolvedReferences
+                vocabulary[word] = new_idx[idx]
+            else:
+                del vocabulary[word]
+
+        L.info("vocabulary - new size: %s", len(vocabulary))
