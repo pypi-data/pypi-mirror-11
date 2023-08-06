b118ca680e26676d0b52c6b45e2cc85f62e9a38d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:32 +0000

    Add package pokeball/0.2/pokeball-0.2.tar.gz

---
+++ packages/pokeball/pokeball-0.2.tar.gz/pokeball-0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pokeball',
+  packages = ['pokeball'], # this must be the same as the name above
+  version = '0.2',
+  description = 'A wrapper for pika (RabbitMQ Python Client)',
+  author = 'Abhinav Desor',
+  author_email = 'abhinav.desor@gmail.com',
+  url = 'https://github.com/adesor/Pokeball',
+  download_url = 'https://github.com/adesor/pokeball/tarball/0.2',
+  keywords = ['rabbitmq', 'wrapper', 'pokemon', 'pika'],
+  classifiers = [],
+)
