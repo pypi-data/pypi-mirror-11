057561a8ae3b849d7a93d0cca143f36e7db7fb27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:21 +0000

    Add package railroad/0.1.0/railroad-0.1.0.tar.gz

---
+++ packages/railroad/railroad-0.1.0.tar.gz/railroad-0.1.0/railroad/__init__.py
@@ -0,0 +1,10 @@
+__author__ = 'Jindrich K. Smitka'
+__email__ = 'smitka.j@gmail.com'
+__version__ = '0.1.0'
+
+from .railroad import (  # noqa
+    prepare,
+    catch,
+    get_or_reraise,
+    compose,
+)
