2b4825a641fb3dd747580dc959226790540a5195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:15 +0000

    Add package wechatpy/1.0.5/wechatpy-1.0.5-py2.py3-none-any.whl

---
+++ packages/wechatpy/wechatpy-1.0.5-py2.py3-none-any.whl/wechatpy/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import, unicode_literals
+
+from wechatpy.parser import parse_message  # NOQA
+from wechatpy.replies import create_reply  # NOQA
+from wechatpy.client import WeChatClient  # NOQA
+from wechatpy.exceptions import WeChatException  # NOQA
+from wechatpy.exceptions import WeChatClientException  # NOQA
+from wechatpy.oauth import WeChatOAuth  # NOQA
+from wechatpy.exceptions import WeChatOAuthException  # NOQA
+from wechatpy.pay import WeChatPay  # NOQA
+from wechatpy.exceptions import WeChatPayException  # NOQA
+
+
+__version__ = '1.0.5'
+__author__ = 'messense'
