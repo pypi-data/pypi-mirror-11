4a7dd09e1102d81960c5b9e0d2908e6801b3e421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:20 +0000

    Add package djsubdomains/2.1.0/djsubdomains-2.1.0-py2.py3-none-any.whl

---
+++ packages/djsubdomains/djsubdomains-2.1.0-py2.py3-none-any.whl/subdomains/tests/urls/application.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, url
+
+from subdomains.tests.urls.default import urlpatterns as default_patterns
+from subdomains.tests.views import view
+
+
+urlpatterns = default_patterns + patterns(
+    '',  # prefix
+    url(regex=r'^view/$', view=view, name='view'),
+    url(regex=r'^application/$', view=view, name='application'),
+)
