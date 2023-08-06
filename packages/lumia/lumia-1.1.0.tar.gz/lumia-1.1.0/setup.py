57913d31c3e1016e02ad4511c46d19bf9a07ab08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:57 +0000

    Add package lumia/1.1.0/lumia-1.1.0.tar.gz

---
+++ packages/lumia/lumia-1.1.0.tar.gz/lumia-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name	= 'lumia',
+		version	= '1.1.0',
+		py_modules = ['nester'],
+		author = 'lumia',
+		author_email = 'lumiahuan@gmail.com',
+		url	= 'http://www.lumiahuan.com',
+		description = 'A simple printer of nested listed',
+	)
