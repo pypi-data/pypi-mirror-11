06c2e473777e3304c5ec4c090672ce2f8fbf767d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:03 +0000

    Add package bank_transactions/0.1/bank_transactions-0.1.tar.gz

---
+++ packages/bank_transactions/bank_transactions-0.1.tar.gz/bank_transactions-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bank_transactions',
+      version='0.1',
+      description='Python package for importing bank transaction files.',
+      url='https://github.com/henkvanramshorst/bank_transactions',
+      author='Henk van Ramshorst',
+      author_email='henk@vanramshorst.nl',
+      license='MIT',
+      packages=['bank_transactions'],
+      zip_safe=False)
