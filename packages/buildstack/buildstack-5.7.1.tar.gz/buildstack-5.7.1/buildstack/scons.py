cbe021af32062451243f91fffb31dee5bb9f4596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:39 +0000

    Add package buildstack/5.7.1/buildstack-5.7.1.tar.gz

---
+++ packages/buildstack/buildstack-5.7.1.tar.gz/buildstack-5.7.1/buildstack/scons.py
@@ -0,0 +1,8 @@
+# copyright (c) 2015 fclaerhout.fr, released under the MIT license.
+
+# REF: http://www.scons.org/doc/production/HTML/scons-man.html
+
+MANIFEST = {
+	"filenames": ("SConstruct", "Sconstruct", "sconstruct"),
+}
+
