c3f5f57158e2f780a545b02db1d63bad3e290f9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:57 +0000

    Add package nflfan/0.2.0/nflfan-0.2.0.tar.gz

---
+++ packages/nflfan/nflfan-0.2.0.tar.gz/nflfan-0.2.0/nflfan/web/js/lib/error.js
@@ -0,0 +1,10 @@
+define(['jq'], function($) {
+
+$(document).ready(function() {
+    var $err = $('#nflfan-error');
+    $err.find('button').click(function() {
+      $err.addClass('hidden');
+    });
+});
+
+});
