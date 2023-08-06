729567fa85b75f5b48a2d080cb85d3aca4a3cce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:34 +0000

    Add package athelelist/1.0.0/athelelist-1.0.0.tar.gz

---
+++ packages/athelelist/athelelist-1.0.0.tar.gz/athelelist-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+		name = 'athelelist',
+		version = '1.0.0',
+		py_modules = ['Athelelist'],
+		author = 'Jim',
+		author_email = 'jimlin7694@gmail.com',
+		url = 'http://www.epsyln.com/',
+		description = 'Python Learning'
+	)
