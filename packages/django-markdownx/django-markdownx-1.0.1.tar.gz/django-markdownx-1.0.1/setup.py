7ed7f0d2368211f4bbbe5fdb15989d646281aa72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:49 +0000

    Add package django-markdownx/1.0.1/django-markdownx-1.0.1.tar.gz

---
+++ packages/django-markdownx/django-markdownx-1.0.1.tar.gz/django-markdownx-1.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+import os
+if 'vagrant' in str(os.environ):
+    del os.link
+
+setup(
+    name='django-markdownx',
+    version='1.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Simple markdown editor (without any shitty UI controls) with image uploads (stored in MEDIA_ROOT folder) and live preview',
+    url='https://github.com/adi-/django-markdownx',
+    author='adi-',
+    author_email='aaadeji@gmail.com',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: User Interfaces',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: JavaScript',
+    ],
+    keywords='django markdown live preview images upload',
+    install_requires=['Pillow', 'Markdown'],
+)
