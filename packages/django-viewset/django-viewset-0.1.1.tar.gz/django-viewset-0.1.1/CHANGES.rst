b8d92598ddd1f648d78d8aa417400cd70ad0ef54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:52 +0000

    Add package django-viewset/0.1.1/django-viewset-0.1.1.tar.gz

---
+++ packages/django-viewset/django-viewset-0.1.1.tar.gz/django-viewset-0.1.1/CHANGES.rst
@@ -0,0 +1,12 @@
+Changelog
+---------
+
+0.1.1
+~~~~~
+
+* First PyPI release.
+
+0.1.0
+-----
+
+* Initial release.
---
+++ packages/django-viewset/django-viewset-0.1.1.tar.gz/django-viewset-0.1.1/django_viewset/__init__.py
@@ -0,0 +1,7 @@
+from .mixins import ViewSetView
+from .utils import viewset_view
+from .viewset import ViewSet, ModelViewSet
+from .views import NamedView, URLView
+
+
+__version__ = '0.1.1'
