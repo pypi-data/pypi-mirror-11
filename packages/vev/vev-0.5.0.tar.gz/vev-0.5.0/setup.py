6c3af1e0d5fedc8f4c92bcf9bad8f89a78a9ee7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:27 +0000

    Add package vev/0.5.0/vev-0.5.0.tar.gz

---
+++ packages/vev/vev-0.5.0.tar.gz/vev-0.5.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name="vev",
+    packages=["vev"],
+    version="0.5.0",
+    description="Simple HTTP server request routing",
+    author="Christian Stigen Larsen",
+    author_email="csl@csl.name",
+    url="https://github.com/cslarsen/vev",
+    download_url="https://github.com/cslarsen/vev/tarball/v0.5.0",
+    license="https://www.gnu.org/licenses/lgpl-2.1.html",
+    long_description=open("README.rst").read(),
+    zip_safe=True,
+    test_suite="tests",
+
+    keywords=["web", "http", "routing", "server", "url"],
+    platforms=['unix', 'linux', 'osx', 'cygwin', 'win32'],
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Natural Language :: English",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python",
+    ],
+)
