8b87044adaa534f53770d98a921ca8ff7f1460f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:11 +0000

    Add package Shangertest/1.0.0/Shangertest-1.0.0.zip

---
+++ packages/Shangertest/Shangertest-1.0.0.zip/Shangertest-1.0.0/Shangertest.py
@@ -0,0 +1,10 @@
+
+'''Shangertest.py can print the essntial elements of a list'''
+
+def print_lol(the_list):
+
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item)
+        else:
+            print each_item
---
+++ packages/Shangertest/Shangertest-1.0.0.zip/Shangertest-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(
+
+    name        =   'Shangertest',
+    version     =   '1.0.0',
+    py_modules  =   ['Shangertest'],
+    author      =   'Shanger',
+    author_email=   'r00222020@ntu.edu.tw',
+    url         =   'www.yahoo.com',
+    description =   'A printer of list',
+      )
