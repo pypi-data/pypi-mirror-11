1f3429fe55c43e2cf6096143bf1a67e8b223042c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:49 +0000

    Add package easyci/1.0.1/easyci-1.0.1.tar.gz

---
+++ packages/easyci/easyci-1.0.1.tar.gz/easyci-1.0.1/easyci/commands/init.py
@@ -0,0 +1,21 @@
+import click
+import os
+import yaml
+
+from easyci.hooks import hooks_manager
+from easyci.vcs.git import GitVcs
+
+
+@click.command()
+def init():
+    git = GitVcs()
+    click.echo("Installing hooks")
+    git.install_hook('pre-commit', hooks_manager.get_hook('pre-commit'))
+    git.install_hook('pre-push', hooks_manager.get_hook('pre-push'))
+
+    config_path = os.path.join(git.path, 'eci.yaml')
+    if not os.path.exists(config_path):
+        click.echo("Placing a trivial config file in your project.")
+        with open(config_path, 'w') as f:
+            f.write(yaml.safe_dump(
+                {'tests': ['echo please modify to run your tests', 'true']}))
---
+++ packages/easyci/easyci-1.0.1.tar.gz/easyci-1.0.1/easyci/commands/test.py
@@ -0,0 +1,46 @@
+import click
+import os
+
+from easyci.utils import contextmanagers
+from easyci.vcs.git import GitVcs
+
+
+@click.command()
+@click.option('--staged-only', '-s', is_flag=True, default=False, help='Test against staged version of the repo. Remove all unstaged and ignored files before running.')
+@click.option('--head-only', '-h', is_flag=True, default=False, help='Test against the current HEAD. Resets to HEAD and remove all ignored files before running.')
+@click.pass_context
+def test(ctx, staged_only, head_only):
+    git = GitVcs()
+    evidence_path = os.path.join(git.private_dir(), 'passed')
+    known_signatures = []
+    if os.path.exists(evidence_path):
+        with open(evidence_path, 'r') as f:
+            known_signatures = f.read().split()
+    with git.temp_copy() as copy:
+        copy.remove_ignored_files()
+        if head_only:
+            copy.clear('HEAD')
+        elif staged_only:
+            copy.remove_unstaged_files()
+        new_signature = copy.get_signature()
+        if new_signature in known_signatures:
+            click.echo(click.style('OK', bg='green', fg='black') + ' Files not changed.')
+            ctx.exit(0)
+        with contextmanagers.chdir(copy.path):
+            all_passed = True
+            for test in ctx.obj['config']['tests']:
+                click.echo('Running test: {}'.format(test))
+                ret = os.system(test)
+                if ret == 0:
+                    click.secho('Passed', bg='green', fg='black')
+                else:
+                    click.secho('Failed', bg='red', fg='black')
+                    all_passed = False
+    if not all_passed:
+        ctx.exit(1)
+    else:
+        known_signatures.append(new_signature)
+        string = '\n'.join(known_signatures[-ctx.obj['config']['history_limit']:])
+        # update evidence file
+        with open(evidence_path, 'w') as f:
+            f.write(string)
