55f7b7903f653efef612c6eae55a8f0078234f2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:40 +0000

    Add package django-event-logger/0.8.15/django-event-logger-0.8.15.zip

---
+++ packages/django-event-logger/django-event-logger-0.8.15.zip/django-event-logger-0.8.15/django_events/EventLogger.py
@@ -0,0 +1,36 @@
+from django.contrib.auth import get_user_model
+from .models import Event
+
+
+def log_event(action, response, request, additional=None, invoker=None):
+    auth_user = get_user_model()
+
+    action = action
+    response = response
+    additional = additional
+
+    request = request
+    if request.user is None:
+        if invoker is None:
+            raise Exception('No invoker was specified, and no user is detected!')
+        invoker = auth_user.objects.get(username=invoker)
+    else:
+        invoker = request.user
+
+    # Let's grab the current IP of the user.
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+
+    Event(invoker=invoker, action=action, response=response, ip=ip,
+          additional=additional).save()
+
+
+def get_logs(account=None):
+    if account:
+        # We want logs for a specific account.
+        return Event.objects.filter(account=account)
+    # We want logs in general.
+    return Event.objects.all()
