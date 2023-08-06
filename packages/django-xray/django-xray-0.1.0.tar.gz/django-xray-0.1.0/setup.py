46bed8daa8523af9023ef53fce25609a2e7e6fe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:27 +0000

    Add package django-xray/0.1.0/django-xray-0.1.0.tar.gz

---
+++ packages/django-xray/django-xray-0.1.0.tar.gz/django-xray-0.1.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import sys
+from setuptools import setup, find_packages
+
+install_requires = [
+    'docutils',
+]
+
+
+setup(
+    name='django-xray',
+    version='0.1.0',
+    description='Xray is an developer helper tool for Django.',
+    long_description='',
+    author='Kindy Lin',
+    author_email='kindy61@gmail.com',
+    url='https://github.com/kindy/django-xray',
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    zip_safe=False,
+    install_requires=install_requires,
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+    ]
+)
