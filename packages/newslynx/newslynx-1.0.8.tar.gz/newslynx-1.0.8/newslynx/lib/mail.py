497b7a88469aca2889f24b79acd3ed15f4db8b61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:42 +0000

    Add package newslynx/1.0.8/newslynx-1.0.8.tar.gz

---
+++ packages/newslynx/newslynx-1.0.8.tar.gz/newslynx-1.0.8/newslynx/lib/mail.py
@@ -0,0 +1,14 @@
+"""
+All things related to sending + recieving of emails.
+"""
+from newslynx.lib.pkg.validate_email import validate_email
+
+
+def validate(address):
+    """
+    Validates an email address via regex.
+    """
+    return validate_email(address, check_mx=False, verify=False)
+
+if __name__ == '__main__':
+    print validate('brianabelson@gmail')
