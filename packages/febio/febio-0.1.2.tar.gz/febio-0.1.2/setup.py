d637176b6a4a334f2571c6d4c46d686726b3a11f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:00 +0000

    Add package febio/0.1.2/febio-0.1.2.tar.gz

---
+++ packages/febio/febio-0.1.2.tar.gz/febio-0.1.2/setup.py
@@ -0,0 +1,22 @@
+'''
+Created on 2013-05-15
+
+@author: Scott Sibole
+'''
+from distutils.core import setup
+
+setup(
+    name = 'febio',
+    version = '0.1.2',
+    packages = ['febio',],
+    py_modules = ['febio.__init__','febio.MatDef','febio.MeshDef','febio.Boundary','febio.Control','febio.Load','febio.Model'],
+    author = 'Scott Sibole',
+    author_email = 'ssibole@kin.ucalgary.ca',
+    license = 'MIT',
+    package_data = {'febio': ['verification/*'],},
+    url = 'https://github.com/siboles/pyFEBio',
+    download_url = 'https://github.com/siboles/pyFEBio/tarball/0.1.2',
+    description = 'A Python API for FEBio',
+    install_requires = [
+        "numpy",],
+)
