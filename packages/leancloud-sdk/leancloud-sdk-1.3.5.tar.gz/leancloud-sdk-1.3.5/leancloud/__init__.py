b6f1981ca67cc2574ac0f6212373ad2fadf7965b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:36 +0000

    Add package leancloud-sdk/1.3.5/leancloud-sdk-1.3.5.tar.gz

---
+++ packages/leancloud-sdk/leancloud-sdk-1.3.5.tar.gz/leancloud-sdk-1.3.5/leancloud/__init__.py
@@ -0,0 +1,51 @@
+# coding: utf-8
+
+"""LeanCloud Python SDK
+"""
+
+import client
+import push
+from .push import Installation
+from .acl import ACL
+from .client import init
+from .client import use_production
+from .errors import LeanCloudError
+from .file_ import File
+from .geo_point import GeoPoint
+from .object_ import Object
+from .query import FriendshipQuery
+from .query import Query
+from .relation import Relation
+from .user import User
+from .role import Role
+from .engine import Engine
+from .engine import cloudfunc
+from .engine import LeanEngineError
+from .engine.https_redirect_middleware import HttpsRedirectMiddleware
+
+
+__author__ = 'asaka <lan@leancloud.rocks>'
+__version__ = '1.3.5'
+
+
+__all__ = [
+    'ACL',
+    'File',
+    'HttpsRedirectMiddleware',
+    'FriendshipQuery',
+    'GeoPoint',
+    'LeanCloudError',
+    'LeanEngineError',
+    'Object',
+    'Query',
+    'Relation',
+    'User',
+    'client',
+    'use_production',
+    'init',
+    'push',
+    'cloudfunc',
+    'Role',
+    'Installation',
+    'Engine',
+]
