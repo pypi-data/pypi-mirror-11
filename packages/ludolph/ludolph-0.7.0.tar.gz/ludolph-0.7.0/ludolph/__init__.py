300654d08c2c9918fbedf579c43cf974045b0d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:30 +0000

    Add package ludolph/0.7.0/ludolph-0.7.0.tar.gz

---
+++ packages/ludolph/ludolph-0.7.0.tar.gz/ludolph-0.7.0/ludolph/__init__.py
@@ -0,0 +1,10 @@
+"""
+Ludolph: Monitoring Jabber Bot
+Version: 0.7.0
+Homepage: https://github.com/erigones/Ludolph
+Copyright (C) 2012-2015 Erigones, s. r. o.
+
+See the LICENSE file for copying permission.
+"""
+
+__version__ = '0.7.0'
