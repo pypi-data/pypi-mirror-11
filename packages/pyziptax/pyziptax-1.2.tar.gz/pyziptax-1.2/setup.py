cc4beb417ab6f8a28c230b39c0d5965b49a8875d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:17 +0000

    Add package pyziptax/1.2/pyziptax-1.2.tar.gz

---
+++ packages/pyziptax/pyziptax-1.2.tar.gz/pyziptax-1.2/setup.py
@@ -0,0 +1,41 @@
+#/usr/bin/env python
+from setuptools import setup
+
+try:
+    readme = open("README.rst")
+    long_description = str(readme.read())
+finally:
+    readme.close()
+
+setup(
+    name='pyziptax',
+    version='1.2',
+    description='Python API for accessing sales tax information from Zip-Tax.com',
+    long_description=long_description,
+    author='Albert Wang',
+    author_email='aywang31@gmail.com',
+    url='http://github.com/albertyw/pyziptax',
+    packages=['pyziptax', ],
+    install_requires=[
+        'requests>=1.1.0',
+    ],
+    license='Apache',
+    test_suite="tests",
+    tests_require=[
+        'mock>=1.0.1',
+        'tox',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Localization',
+        'Topic :: Office/Business :: Financial :: Accounting',
+    ],
+)
