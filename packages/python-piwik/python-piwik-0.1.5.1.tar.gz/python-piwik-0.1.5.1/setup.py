9227f688541e570fe55e767d48c7a4d19de1c312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:14 +0000

    Add package python-piwik/0.1.5.1/python-piwik-0.1.5.1.tar.gz

---
+++ packages/python-piwik/python-piwik-0.1.5.1.tar.gz/python-piwik-0.1.5.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+	name='python-piwik', # The name is 'pypiwik' but it's taken on PyPI by an abandoned project
+	version='0.1.5.1',
+	description='Python implementation of the Piwik HTTP API',
+#	long_description=open('README.md').read(),
+	author='Johann Schmitz',
+	author_email='johann@j-schmitz.net',
+	url='https://ercpe.de/projects/pypiwik',
+	download_url='https://code.not-your-server.de/pypiwik.git/tags/',
+	packages=find_packages('src'),
+	package_dir={'': 'src'},
+	include_package_data=True,
+	zip_safe=False,
+	license='GPL-3',
+)
