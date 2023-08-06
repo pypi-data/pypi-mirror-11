5099d67ad03b244b28b92192d8144d275d826479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:46 +0000

    Add package eea.pdf/5.9/eea.pdf-5.9.zip

---
+++ packages/eea.pdf/eea.pdf-5.9.zip/eea.pdf-5.9/eea/pdf/skins/eea_pdf/javascripts/pdf.print.js
@@ -0,0 +1,16 @@
+/*
+FIX for How do I avoid a page break immediately after a header
+http://stackoverflow.com/questions/9238868/how-do-i-avoid-a-page-break-immediately-after-a-header
+*/
+
+$(document).ready(function(){
+    var $content_core = $('#content-core');
+    $content_core.find('h2').each(function(i, e){
+      $(e).nextUntil('h2').andSelf().wrapAll('<div class="nobreak">');
+    });
+    $content_core.find('h3').each(function(i, e){
+      if (!$(e).closest('.nobreak').length){
+        $(e).nextUntil('h3').andSelf().wrapAll('<div class="nobreak">');
+      }
+    });
+});
