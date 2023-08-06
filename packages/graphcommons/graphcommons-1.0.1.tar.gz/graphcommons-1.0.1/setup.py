27e0d84d116efc3262f0664250e238482437b7a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:50 +0000

    Add package graphcommons/1.0.1/graphcommons-1.0.1.tar.gz

---
+++ packages/graphcommons/graphcommons-1.0.1.tar.gz/graphcommons-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='graphcommons',
+    version='1.0.1',
+    py_modules=['graphcommons'],
+    url='https://github.com/graphcommons/graphcommons-python',
+    license='MIT',
+    author='fatiherikli',
+    author_email='fatiherikli@gmail.com',
+    description='Python Wrapper For Graph Commons API.',
+    install_requires=['requests==2.5.3'],
+)
