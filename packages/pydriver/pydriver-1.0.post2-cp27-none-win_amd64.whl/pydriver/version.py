5bcc7b3aa9e477127f14b258578b6fbf508f3268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:56 +0000

    Add package pydriver/1.0/pydriver-1.0.post2-cp27-none-win_amd64.whl

---
+++ packages/pydriver/pydriver-1.0.post2-cp27-none-win_amd64.whl/pydriver/version.py
@@ -0,0 +1,6 @@
+# this file was created automatically by setup.py on 2015-10-01 21:25:37.989000
+__version__ = '1.0'
+__version_info__ = {
+    'full': __version__,
+    'short': '.'.join(__version__.split('.')[:2])
+}
