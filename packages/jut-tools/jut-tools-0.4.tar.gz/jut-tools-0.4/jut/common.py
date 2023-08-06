dcaed5d10d258459d2206a451f3d59bc9d374dfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:26 +0000

    Add package jut-tools/0.4/jut-tools-0.4.tar.gz

---
+++ packages/jut-tools/jut-tools-0.4.tar.gz/jut-tools-0.4/jut/common.py
@@ -0,0 +1,65 @@
+"""
+"""
+
+import sys
+import os
+
+DEBUG = os.environ.get('JUT_DEBUG', False)
+
+def info(message, *args, **kwargs):
+    """
+    write a message to stdout
+
+    """
+    if 'end' in kwargs:
+        end = kwargs['end']
+    else:
+        end = '\n'
+
+    if len(args) == 0:
+        sys.stdout.write(message)
+    else:
+        sys.stdout.write(message % args)
+    sys.stdout.write(end)
+    sys.stdout.flush()
+
+
+def error(message, *args, **kwargs):
+    """
+    write a message to stderr
+
+    """
+    if 'end' in kwargs:
+        end = kwargs['end']
+    else:
+        end = '\n'
+
+    if len(args) == 0:
+        sys.stderr.write(message)
+    else:
+        sys.stderr.write(message % args)
+    sys.stderr.write(end)
+    sys.stderr.flush()
+
+
+def debug(message, *args, **kwargs):
+    """
+    debug output goes to stderr so you can still redirect the stdout to a file
+    or another program. Controlled by the JUT_DEBUG environment variable being
+    present
+
+    """
+    if 'end' in kwargs:
+        end = kwargs['end']
+    else:
+        end = '\n'
+
+    if DEBUG: 
+        if len(args) == 0:
+            sys.stderr.write(message)
+        else:
+            sys.stderr.write(message % args)
+
+        sys.stderr.write(end)
+        sys.stderr.flush()
+
