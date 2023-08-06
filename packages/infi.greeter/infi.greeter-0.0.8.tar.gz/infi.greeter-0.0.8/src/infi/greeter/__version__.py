d5a06ddbe46d9a1acb72d86ab631934f9a889903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:19 +0000

    Add package infi.greeter/0.0.8/infi.greeter-0.0.8.tar.gz

---
+++ packages/infi.greeter/infi.greeter-0.0.8.tar.gz/infi.greeter-0.0.8/src/infi/greeter/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.8"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(HEAD detached at v0.0.8)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '4bc349cf2a5087e1d3b42bba5bbefc694cdda3b4'
+__git_head_subject__ = 'Finished release v0.0.8.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
