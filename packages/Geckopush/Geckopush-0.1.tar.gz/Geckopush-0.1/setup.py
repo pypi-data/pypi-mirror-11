d2f1e8e4d69af41a7fce917decd6c9cb2cd60dba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:15 +0000

    Add package Geckopush/0.1/Geckopush-0.1.tar.gz

---
+++ packages/Geckopush/Geckopush-0.1.tar.gz/Geckopush-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='Geckopush',
+      packages=['Geckopush'],
+      version='0.1',
+      description="A library for geckoboard push functionality",
+      author="Patrick Lee",
+      author_email="me@patricklee.nyc",
+      url="http://www.github.com/patleeman/geckopush",
+      download_url="http://www.github.com/patleeman/geckopush/tarball/0.1",
+      keywords = ['geckoboard', 'api', 'widget'],
+      classifiers=[]
+      )
