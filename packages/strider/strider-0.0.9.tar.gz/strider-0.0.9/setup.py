69208eed4a08343822e1ecd461f2de6e3560e324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:11 +0000

    Add package strider/0.0.9/strider-0.0.9.tar.gz

---
+++ packages/strider/strider-0.0.9.tar.gz/strider-0.0.9/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+setup(name='strider',
+      version="0.0.9",
+      description='Strider builds dev VMs and bakes cloud images.',
+      author='Michael DeHaan',
+      author_email='michael.dehaan@gmail.com',
+      url='http://github.com/mpdehaan/strider/',
+      license='Apache2',
+      install_requires=['boto'],
+      package_dir={ '': 'lib' },
+      packages=find_packages('lib'),
+      classifiers=[
+      ],
+      scripts=[
+      ],
+      data_files=[
+      ],
+)
