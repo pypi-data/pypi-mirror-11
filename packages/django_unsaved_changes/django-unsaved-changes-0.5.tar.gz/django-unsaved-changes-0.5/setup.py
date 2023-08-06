51219cda515dcf378fd7d9d669cdcb5f26cb14f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:48 +0000

    Add package django_unsaved_changes/0.5/django-unsaved-changes-0.5.tar.gz

---
+++ packages/django_unsaved_changes/django-unsaved-changes-0.5.tar.gz/django-unsaved-changes-0.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'django-unsaved-changes',
+    packages = ['django_unsaved_changes'],
+    version = '0.5',
+    description = 'UX improvements for admin',
+    author = 'Nina Pavlich',
+    author_email='nina@ninalp.com',
+    url = 'https://github.com/ninapavlich/django-unsaved-changes',
+    keywords = ['libraries', 'web development', 'cms', 'django', 'django-grappelli'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ]
+)
