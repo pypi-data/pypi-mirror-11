c970bd199d26f37c78fa94113549cb0f1e5b1a44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:11 +0000

    Add package chainer-libdnn/2.0.2/chainer-libdnn-2.0.2.tar.gz

---
+++ packages/chainer-libdnn/chainer-libdnn-2.0.2.tar.gz/chainer-libdnn-2.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(name='chainer-libdnn',
+      version='2.0.2',
+      description='General utilities for neural networs with chainer',
+      author='Aiga SUZUKI',
+      author_email='ai-suzuki@aist.go.jp',
+      license='MIT',
+      url='http://git.tochikuji.com/tochikuji/chainer-libcnn',
+      packages=find_packages()
+      )
