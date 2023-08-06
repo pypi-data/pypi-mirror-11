3efaf70c102c5d5cb61004efac7ca17034cc2c7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:07 +0000

    Add package py-parallel/1.0.0/py-parallel-1.0.0.tar.gz

---
+++ packages/py-parallel/py-parallel-1.0.0.tar.gz/py-parallel-1.0.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='py-parallel',
+    description='Utility for simple asynchronous function calls',
+    version='1.0.0',
+    author='Shapeways',
+    author_email='api@shapeways.com',
+    url='http://github.com/Shapeways/py-parallel',
+    download_url='',
+    py_modules=['pyparallel'],
+    test_suite='tests'
+)
