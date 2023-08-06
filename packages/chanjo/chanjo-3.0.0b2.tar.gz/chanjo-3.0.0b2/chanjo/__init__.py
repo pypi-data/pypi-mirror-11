9d2c38736626e6a31c5d1238415b8af268382b2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:51 +0000

    Add package chanjo/3.0.0b2/chanjo-3.0.0b2.tar.gz

---
+++ packages/chanjo/chanjo-3.0.0b2.tar.gz/chanjo-3.0.0b2/chanjo/__init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+chanjo
+~~~~~~~
+Coverage analysis for clinical sequencing. It's intuatively documented
+and embraces functional programming patterns.
+
+:copyright: (c) 2014 by Robin Andeer
+:licence: MIT, see LICENCE for more details
+"""
+
+import logging
+logger = logging.getLogger()
+
+__banner__ = r"""
+       ______               ________
+ _________  /_______ _____________(_)_____
+ _  ___/_  __ \  __ `/_  __ \____  /_  __ \
+ / /__ _  / / / /_/ /_  / / /___  / / /_/ /   by Robin Andeer
+ \___/ /_/ /_/\__,_/ /_/ /_/___  /  \____/
+                            /___/
+"""
+
+__title__ = 'chanjo'
+__summary__ = 'coverage analysis tool for clinical sequencing'
+__uri__ = 'http://www.chanjo.co/'
+
+__version__ = '3.0.0-beta2'
+__codename__ = 'Radical Red Panda'
+
+__author__ = 'Robin Andeer'
+__email__ = 'robin.andeer@gmail.com'
+
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2015 Robin Andeer'
