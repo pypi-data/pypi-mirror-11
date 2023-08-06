3679ae6f48c9bc2bceeb34de839f768000ae444f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:05 +0000

    Add package pytest-mock/0.8.1/pytest-mock-0.8.1.zip

---
+++ packages/pytest-mock/pytest-mock-0.8.1.zip/pytest-mock-0.8.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+
+setup(
+    name='pytest-mock',
+    version='0.8.1',
+    entry_points={
+        'pytest11': ['pytest_mock = pytest_mock'],
+    },
+    py_modules=['pytest_mock'],
+    platforms='any',
+    install_requires=[
+        'pytest>=2.4',
+    ],
+    extras_require={
+        ':python_version=="2.6" or python_version=="2.7"': ['mock'],
+    },
+    url='https://github.com/pytest-dev/pytest-mock/',
+    license='LGPL',
+    author='Bruno Oliveira',
+    author_email='nicoddemus@gmail.com',
+    description='Thin-wrapper around the mock package for easier use with py.test',
+    long_description=open('README.rst').read(),
+    keywords="pytest mock",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Testing',
+    ]
+)
