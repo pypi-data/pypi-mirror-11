ff570f1874b809523c8849e8038be6d1d96b009b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:05 +0000

    Add package metabolin/1.1.0/metabolin-1.1.0.zip

---
+++ packages/metabolin/metabolin-1.1.0.zip/metabolin-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup (
+			name = 'metabolin',
+			version='1.1.0' , 
+			py_modules = ['metabolin'],
+			author = 'metabolin',
+			author_email='metabolin@gmail.com',
+			url='www.maobolin.com',
+			description = 'a test file from a beginner',
+			)
+			
+			
