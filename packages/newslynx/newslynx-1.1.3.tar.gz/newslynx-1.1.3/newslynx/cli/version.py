0a15581fea9e7842b0cbd08461b066867770e484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:54 +0000

    Add package newslynx/1.1.3/newslynx-1.1.3.tar.gz

---
+++ packages/newslynx/newslynx-1.1.3.tar.gz/newslynx-1.1.3/newslynx/cli/version.py
@@ -0,0 +1,17 @@
+"""
+Report the version of newslynx.
+"""
+
+
+def setup(parser):
+    parser.add_parser("version", help="Report the version.")
+    return 'version', run
+
+
+def run(opts, log, **kwargs):
+    """
+    Report the version.
+    """
+    import pkg_resources
+    v = pkg_resources.get_distribution("newslynx").version + "\n"
+    log.info(v, line=False, color='blue')
