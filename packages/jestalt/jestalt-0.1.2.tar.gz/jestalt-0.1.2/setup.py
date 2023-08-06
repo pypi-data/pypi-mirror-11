2d16bc12dd9ca514cb632192178d500838d0efb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:45 +0000

    Add package jestalt/0.1.2/jestalt-0.1.2.tar.gz

---
+++ packages/jestalt/jestalt-0.1.2.tar.gz/jestalt-0.1.2/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+packages = ['jestalt']
+
+package_data = {
+}
+
+setup(name='jestalt',
+      version='0.1.2',
+      author='James Percent',
+      packages=packages,
+      author_email='jpercent@shift5.net')
