688bc0411df7c3ee7048c85d298c9c6e4e369f54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:14 +0000

    Add package gmm-lbd/0.0.4/gmm-lbd-0.0.4.tar.gz

---
+++ packages/gmm-lbd/gmm-lbd-0.0.4.tar.gz/gmm-lbd-0.0.4/gmm_lbd/__init__.py
@@ -0,0 +1,5 @@
+# Easy to develop but a bit dirty
+# coding: utf8
+from gmm import LbdGMM
+from data_manager import GmmManager, plot_2D_mean_covars, SanitizeRecordsForGmm
+from operations import conc, seq
