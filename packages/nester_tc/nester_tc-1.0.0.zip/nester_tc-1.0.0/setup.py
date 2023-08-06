ef7f60cdd9f5464b1eb8961137432e135ef2e6c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:59 +0000

    Add package nester_tc/1.0.0/nester_tc-1.0.0.zip

---
+++ packages/nester_tc/nester_tc-1.0.0.zip/nester_tc-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name          = 'nester_tc',
+    version       = '1.0.0',
+    py_modules    = ['nester'],
+    author        = 'Tian Chao',
+    author_email  = 'tian_chaoyes@hotmail.com',
+    url           = '',
+    description   = 'An example for building distribution',
+    )
