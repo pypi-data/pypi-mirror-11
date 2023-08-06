26fdef23a850144b24edc17e04607f5a646bb523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:02 +0000

    Add package netjsonconfig/0.1.alpha/netjsonconfig-0.1a0-py2.py3-none-any.whl

---
+++ packages/netjsonconfig/netjsonconfig-0.1a0-py2.py3-none-any.whl/netjsonconfig/backends/openwrt/openwrt.py
@@ -0,0 +1,33 @@
+import json
+
+from .schema import schema
+from .renderers import NetworkRenderer
+from ...exceptions import ValidationError
+
+from jinja2 import Environment, PackageLoader
+
+
+class OpenWrt(object):
+    """ OpenWrt Backend """
+    schema = schema
+    renderers = [
+        NetworkRenderer
+    ]
+
+    def __init__(self, config):
+        self.config = config
+        self.env = Environment(loader=PackageLoader('netjsonconfig.backends.openwrt', 'templates'),
+                               trim_blocks=True)
+
+    def render(self):
+        output = ''
+        for renderer_class in self.renderers:
+            renderer = renderer_class(self.config, self.env)
+            output += renderer.render()
+        return output
+
+    def validate(self):
+        raise ValidationError()
+
+    def json(self, *args, **kwargs):
+        return json.dumps(self.config, *args, **kwargs)
