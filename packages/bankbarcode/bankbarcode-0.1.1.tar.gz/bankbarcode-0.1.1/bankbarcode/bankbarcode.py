21efa5cae6c2e45eda65fe5e281f77684ac44e42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:03 +0000

    Add package bankbarcode/0.1.1/bankbarcode-0.1.1.tar.gz

---
+++ packages/bankbarcode/bankbarcode-0.1.1.tar.gz/bankbarcode-0.1.1/bankbarcode/bankbarcode.py
@@ -0,0 +1,66 @@
+from barcode import generate
+from StringIO import StringIO
+
+
+class BankBarcode(object):
+    """
+    Base class for barcodes
+    """
+
+    def _check_length(self, name, value, expected_length, description):
+        """
+        Check length of a value.
+
+        :param name: name of the value
+        :param value: the value itself
+        :param expected_length: the expected length of the value
+        :param description: definition of the expected value
+        :return: True if value have the expected length, otherwise False
+        """
+        length = len(value)
+        if length < expected_length:
+            raise ValueError('{} is too short, {}'.format(name, description))
+        elif length > expected_length:
+            raise ValueError('{} is too long, {}'.format(name, description))
+        elif length == expected_length:
+            return True
+
+    def _strip_dotsvg(self, path):
+        if isinstance(path, basestring) and path[-4:] == '.svg':
+            new_path = path[:-4]
+        else:
+            new_path = path
+        return new_path
+
+    def code(self):
+        """
+        Code generation. To implement in child classes
+        """
+        raise NotImplementedError('This method is not implemented!')
+
+    def save(self, path):
+        """
+        Save barcode in SVG format.
+
+        :param path: path to SVG file with or without ".svg" extension
+        :return: a string with the name of the file generated
+        """
+        path = self._strip_dotsvg(path)
+
+        writer_options = {'font_size': 6}
+        return generate(
+            'code128',
+            self.code(),
+            output=path,
+            writer_options=writer_options
+        )
+
+    def svg(self):
+        """
+        Generate a SVG with the barcode.
+
+        :return: a string with the barcode in SVG format
+        """
+        f = StringIO()
+        self.save(f)
+        return f.getvalue()
---
+++ packages/bankbarcode/bankbarcode-0.1.1.tar.gz/bankbarcode-0.1.1/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='bankbarcode',
+    version='0.1.1',
+    packages=find_packages(),
+    url='https://github.com/gisce/bankbarcode',
+    license='GNU Affero General Public License v3',
+    author='GISCE-TI, S.L.',
+    author_email='devel@gisce.net',
+    # We need python-barcode v0.8, to have Code128 (EAN128), not released yet
+    # https://bitbucket.org/whitie/python-barcode/issues/16/pypi-08-release-request
+    dependency_links = [
+        "https://bitbucket.org/whitie/python-barcode/get/6c22b96a2ca2.zip"
+    ],
+    install_requires=[
+        'pybarcode>=0.8b1'
+    ],
+    description='barcodes for financial documents'
+)
