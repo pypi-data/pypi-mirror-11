2551dd75c79cc6b4fe346c93231fbd06a98f2d34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:00 +0000

    Add package byrlogin/1.0.2/byrlogin-1.0.2.tar.gz

---
+++ packages/byrlogin/byrlogin-1.0.2.tar.gz/byrlogin-1.0.2/byrlogin/byrlogin.py
@@ -0,0 +1,49 @@
+#!/usr/local/env python
+# -*- coding: utf-8 -*-
+import requests
+import sys
+studentID = ''
+passwd=''
+
+def login():
+    # My API (POST http://10.3.8.211/a11.htm)
+    try:
+        print "Login in ..."
+        r = requests.post(
+            url="http://10.3.8.211/a11.htm",
+            data = {
+                "DDDDD":str(studentID),
+                "upass":str(passwd),
+                "AMKKey":"",
+            },
+        )
+        print('Response HTTP Status Code : {status_code}'.format(status_code=r.status_code))
+        #print('Response HTTP Response Body : {content}'.format(content=r.content))
+    except requests.exceptions.RequestException as e:
+        print('HTTP Request failed')
+
+def logout():
+    # My API (2) (GET http://gw.bupt.edu.cn/F.html)
+    try:
+        print "Login out ..."
+        r = requests.get(
+            url="http://gw.bupt.edu.cn/F.html",
+        )
+        print('Response HTTP Status Code : {status_code}'.format(status_code=r.status_code))
+        #print('Response HTTP Response Body : {content}'.format(content=r.content))
+    except requests.exceptions.RequestException as e:
+        print('HTTP Request failed')
+
+def main():
+    if len(sys.argv) > 1:
+        logout()
+    else:
+        studentID = raw_input("Please Input Your Student ID:\t")
+        passwd = raw_input("Please Input Your Password:\t")
+        login()
+
+
+if __name__ == '__main__':
+    main()
+
+
