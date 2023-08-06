01f79e0dd8cf45758851b1af2090184c694b9179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:36 +0000

    Add package pydemon/0.0.5/pydemon-0.0.5.tar.gz

---
+++ packages/pydemon/pydemon-0.0.5.tar.gz/pydemon-0.0.5/setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+import os
+
+with open('README.txt') as file:
+	long_description = file.read()
+
+name = 'pydemon'
+src_path = '/usr/local/lib/python2.7/site-packages/'+ name + '.py'
+trg_path = '/usr/bin/pydemon'
+
+if os.path.islink(trg_path):
+	os.remove(trg_path)
+os.symlink(src_path,trg_path)
+
+setup(
+	name=name,
+	version='0.0.5',
+	py_modules=['pydemon'],
+	author ='becxer',
+	author_email='becxer87@gmail.com',
+	url = 'https://github.com/becxer/pydemon',
+	description ='Monitor for any changes in your Project',
+	long_description = long_description,
+	license='MIT',
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+		'Topic :: Software Development :: Build Tools',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python :: 2.7',
+	],
+)
+
+
+	
