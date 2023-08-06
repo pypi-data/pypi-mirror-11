c409c47d825c8f6079aef04f4497bf80e1ec9240
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:45 +0000

    Add package onegov.form/0.6.9/onegov.form-0.6.9.tar.gz

---
+++ packages/onegov.form/onegov.form-0.6.9.tar.gz/onegov.form-0.6.9/onegov/form/errors.py
@@ -0,0 +1,19 @@
+class FormError(Exception):
+    pass
+
+
+class DuplicateLabelError(FormError):
+
+    def __init__(self, label):
+        self.label = label
+
+    def __repr__(self):
+        return "DuplicateLabelError(label='{}')".format(self.label)
+
+
+class InvalidMimeType(FormError):
+    pass
+
+
+class UnableToComplete(FormError):
+    pass
