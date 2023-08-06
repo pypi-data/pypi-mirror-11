6d407192e03d665a14db29fab03ef90e0977434b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:47 +0000

    Add package netease-musicbox/0.1.8.1/NetEase-MusicBox-0.1.8.1.tar.gz

---
+++ packages/netease-musicbox/NetEase-MusicBox-0.1.8.1.tar.gz/NetEase-MusicBox-0.1.8.1/NEMbox/__init__.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# encoding: UTF-8
+
+'''
+网易云音乐 Entry
+'''
+
+import curses, traceback
+from menu import Menu
+import argparse
+import sys
+
+
+def start():
+    nembox_menu = Menu()
+    try:
+        nembox_menu.start()
+    except:
+        # clean up terminal while failed
+        nembox_menu.screen.keypad(1)
+        curses.echo()
+        curses.nocbreak()
+        curses.endwin()
+        traceback.print_exc()
+
+
+if __name__ == '__main__':
+    start()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--version", help="show this version and exit", action="store_true")
+args = parser.parse_args()
+if args.version:
+    print "NetEase-MusicBox 0.1.8.1"
+    sys.exit()
