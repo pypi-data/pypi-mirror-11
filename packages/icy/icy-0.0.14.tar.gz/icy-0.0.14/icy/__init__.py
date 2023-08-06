60cb1f02ac232289d0c8b43745e35fda9df9ba37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:57 +0000

    Add package icy/0.0.14/icy-0.0.14.tar.gz

---
+++ packages/icy/icy-0.0.14.tar.gz/icy-0.0.14/icy/__init__.py
@@ -0,0 +1,10 @@
+from .icy import preview
+from .icy import read
+from .icy import mem
+from .icy import merge
+from .icy import _path_to_objs
+from .icy import run_examples
+from .utils import str_remove_accents
+from .utils import pdf_extract_text
+
+__version__ = '0.0.14'
