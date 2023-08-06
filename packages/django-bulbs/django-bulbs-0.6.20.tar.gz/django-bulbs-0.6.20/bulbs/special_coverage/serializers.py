8ec1fc5ff999790de0f5b3d61a19a1da2839d262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:58 +0000

    Add package django-bulbs/0.6.20/django-bulbs-0.6.20.tar.gz

---
+++ packages/django-bulbs/django-bulbs-0.6.20.tar.gz/django-bulbs-0.6.20/bulbs/special_coverage/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+from bulbs.utils.serializers import JSONField
+
+from .models import SpecialCoverage
+
+
+class SpecialCoverageSerializer(serializers.ModelSerializer):
+
+    query = JSONField(required=False, default={})
+    videos = JSONField(required=False, default=[])
+
+    class Meta:
+        model = SpecialCoverage
