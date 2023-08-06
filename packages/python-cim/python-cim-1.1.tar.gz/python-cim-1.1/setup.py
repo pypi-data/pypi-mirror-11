3353a86dba1fae4f6edc8774d69e494613612fa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:32 +0000

    Add package python-cim/1.1/python-cim-1.1.tar.gz

---
+++ packages/python-cim/python-cim-1.1.tar.gz/python-cim-1.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+description = "Pure Python parser for Windows WMI CIM respository databases."
+setup(name="python-cim",
+      version="1.1",
+      description=description,
+      long_description=description,
+      author="Willi Ballenthin",
+      author_email="william.ballenthin@fireeye.com",
+      url="https://github.com/williballenthin/python-cim",
+      license="Apache 2.0 License",
+      install_requires=[
+          "hexdump",
+          "funcy",
+          "intervaltree",
+          "vivisect-vstruct-wb",
+          "python-pyqt5-hexview",
+          "python-pyqt5-vstructui"],
+      packages=["cim"])
