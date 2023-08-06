bf4753bdf0f705099c8de731e462933395d8a086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:46 +0000

    Add package jestalt/0.1.3/jestalt-0.1.3.tar.gz

---
+++ packages/jestalt/jestalt-0.1.3.tar.gz/jestalt-0.1.3/jestalt/__init__.py
@@ -0,0 +1,8 @@
+from jestalt.assembly import *
+from jestalt.runtime import *
+
+__author__ = 'jpercent'
+
+del assembly
+del runtime
+
---
+++ packages/jestalt/jestalt-0.1.3.tar.gz/jestalt-0.1.3/setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup
+
+packages = ['jestalt']
+
+package_data = {
+}
+
+setup(name='jestalt',
+      version='0.1.3',
+      author='James Percent',
+      packages=packages,
+      zip_safe=False,
+      author_email='jpercent@shift5.net')
