a2bd5f02b3cddc382e324fefc4e53faf0f1dff49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:56 +0000

    Add package miku/1.2.1/miku-1.2.1.zip

---
+++ packages/miku/miku-1.2.1.zip/miku-1.2.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            = 'miku',
+        version         = '1.2.1',
+        py_modules      = ['miku'],
+        author          = 'keyor',
+        author_email    = 'rainsky123@123.com',
+        url             = 'http://xxx.com',
+        description     = 'Sorry,This is Practice'
+    )
