a5291724fa590747ddc6b865cfb04e318cfb9018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:45 +0000

    Add package sacred/0.6.5/sacred-0.6.5.tar.gz

---
+++ packages/sacred/sacred-0.6.5.tar.gz/sacred-0.6.5/sacred/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import division, print_function, unicode_literals
+
+from .__about__ import __version__, __author__, __author_email__, __url__
+
+from .experiment import Experiment
+from .ingredient import Ingredient
+
+__all__ = ('Experiment', 'Ingredient', '__version__', '__author__',
+           '__author_email__', '__url__')
