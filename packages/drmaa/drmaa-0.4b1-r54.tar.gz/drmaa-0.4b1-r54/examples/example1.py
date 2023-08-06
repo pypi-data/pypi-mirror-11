c061ab52b57e9750e516fb51c491ee4acb2c0c2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:11 +0000

    Add package drmaa/0.4b1-r54/drmaa-0.4b1-r54.tar.gz

---
+++ packages/drmaa/drmaa-0.4b1-r54.tar.gz/drmaa-0.4b1-r54/examples/example1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import drmaa
+
+def main():
+    """Create a drmaa session and exit"""
+    s=drmaa.Session()
+    print 'A session was started successfully'
+    s.exit()
+
+if __name__=='__main__':
+    main()
