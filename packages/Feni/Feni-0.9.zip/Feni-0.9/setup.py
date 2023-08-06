df9a35d03bd9a87aebbef5af0266cab4fabf1872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:46 +0000

    Add package Feni/0.9/Feni-0.9.zip

---
+++ packages/Feni/Feni-0.9.zip/Feni-0.9/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Feni",
+    description='A simple static site generator.',
+    url='http://bitbucket.org/sras/feni',
+    author='Sandeep.C.R',
+    author_email='sandeepcr2@gmail.com',
+    license='MIT',
+    version = "0.9",
+    packages = ['feni'],
+    entry_points = {
+        "console_scripts":['feni=feni.feni:main']
+    },
+    install_requires=[
+      'lesscpy',
+      'markdown',
+      'pyyaml'
+    ]
+)
