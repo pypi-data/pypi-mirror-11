9e1aa2d03b9353e3c48d3b6a928591fb5e52662c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:20 +0000

    Add package Flask-Alembic/1.2.1/Flask-Alembic-1.2.1.tar.gz

---
+++ packages/Flask-Alembic/Flask-Alembic-1.2.1.tar.gz/Flask-Alembic-1.2.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='Flask-Alembic',
+    version='1.2.1',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    url='https://bitbucket.org/davidism/flask-alembic',
+    license='BSD',
+    author='David Lord',
+    author_email='davidism@gmail.com',
+    description='Flask extension to integrate Alembic migrations',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Database :: Front-Ends',
+    ],
+    install_requires=[
+        'alembic>=0.7,<0.8',
+        'Flask>=0.10',
+        'Flask-SQLAlchemy',
+        'SQLAlchemy'
+    ],
+)
