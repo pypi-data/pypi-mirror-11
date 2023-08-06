f794e7f38cba335242087b22cdfc70785772378b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:10 +0000

    Add package bdates/0.2/bdates-0.2.tar.gz

---
+++ packages/bdates/bdates-0.2.tar.gz/bdates-0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'bdates',
+  packages = ['bdates'],
+  version = '0.2',
+  description = 'Better Dates (bdates) helps you work with dates, including extracting dates from text.',
+  author = 'Better Know The Opposition (BKTO)',
+  author_email = 'hello@betterknowtheopposition.com',
+  url = 'https://github.com/BKTO/bdates',
+  download_url = 'https://github.com/BKTO/bdates/tarball/0.5',
+  keywords = ['date','nlp','scraping'],
+  classifiers = [],
+)
