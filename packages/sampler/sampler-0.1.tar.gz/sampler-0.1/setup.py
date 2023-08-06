2d846b600112c32913605979697aa97fb7d1e38a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:40 +0000

    Add package sampler/0.1/sampler-0.1.tar.gz

---
+++ packages/sampler/sampler-0.1.tar.gz/sampler-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='sampler',
+    version='0.1',
+    description='Sample data generator for Python',
+    keywords='sampler test data generator',
+    url='',
+    author='Sven Perkmann',
+    author_email='sven.perkmann@gmail.com',
+    license='MIT',
+    packages=['sampler'],
+    install_requires=['fake-factory'],
+)
