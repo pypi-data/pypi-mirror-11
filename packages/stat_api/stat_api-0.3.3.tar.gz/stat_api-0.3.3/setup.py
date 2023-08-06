e0bf801b04f0de674ec2b48c2b8d15d20af90609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:05 +0000

    Add package stat_api/0.3.3/stat_api-0.3.3.tar.gz

---
+++ packages/stat_api/stat_api-0.3.3.tar.gz/stat_api-0.3.3/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'stat_api',
+  packages = ['stat_api'], # this must be the same as the name above
+  version = '0.3.3',
+  description = 'A wrapper for the getstat.com API',
+  author = 'Austin Platt',
+  author_email = 'austin.platt@distilled.net',
+  url = 'https://github.com/DistilledLtd/get-stat-api',
+  keywords = ['stat', 'getstat', 'api', 'keyword'],
+  classifiers = [],
+  install_requires=[
+      'requests>=2.7'
+  ]
+)
