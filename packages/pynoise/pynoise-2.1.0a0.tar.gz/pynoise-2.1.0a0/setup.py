3b9f7d2b88d826838f28ba886a4b3a645e1a4c59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:24 +0000

    Add package pynoise/2.1.0a0/pynoise-2.1.0a0.tar.gz

---
+++ packages/pynoise/pynoise-2.1.0a0.tar.gz/pynoise-2.1.0a0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name = 'pynoise',
+    packages = ['pynoise'],
+    version = '2.1.0a',
+    description = 'An implementation of libnoise in python. Allows the creation of verious noise maps using a series of interconnected noise modules.',
+    author = 'Tim Butram',
+    author_email = 'tim@timchi.me',
+    url = 'https://gitlab.com/atrus6/pynoise',
+    download_url = 'https://gitlab.com/atrus6/pynoise/repository/archive.tar.gz?ref=1.0a',
+    keywords = ['perlin', 'noise', 'procedural'],
+    install_requires = ['sortedcontainers>=0.9.6', 'colormath', 'pillow', 'numpy']
+)
