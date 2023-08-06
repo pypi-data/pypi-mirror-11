8ddc0651154ee20025bb08697d084589d0f5ae23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:45 +0000

    Add package seqrecord-expanded/0.1.0/seqrecord_expanded-0.1.0.tar.gz

---
+++ packages/seqrecord-expanded/seqrecord_expanded-0.1.0.tar.gz/seqrecord_expanded-0.1.0/setup.py
@@ -0,0 +1,26 @@
+#! coding: utf-8
+import setuptools
+
+
+setuptools.setup(
+    name="seqrecord_expanded",
+    version="0.1.0",
+    url="https://github.com/carlosp420/seqrecord-expanded",
+
+    author="Carlos Peña",
+    author_email="mycalesis@gmail.com",
+
+    description="Another SeqRecord class with methods: degenerate seqs, codon positions based on reading frames, etc.",
+    long_description=open('README.rst').read(),
+
+    packages=['seqrecord_expanded'],
+
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+    ],
+    test_suite='tests',
+)
