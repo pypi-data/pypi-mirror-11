61b98d7f296af731cd456e45e1e758809a16d1ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:48 +0000

    Add package django-email-authenticator/0.4.8/django-email-authenticator-0.4.8.tar.gz

---
+++ packages/django-email-authenticator/django-email-authenticator-0.4.8.tar.gz/django-email-authenticator-0.4.8/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='django-email-authenticator',
+    version='0.4.8',
+    packages=['email_auth'],
+    url='https://bitbucket.org/jochangmin/django-email-user',
+    license='MIT',
+    author='Anderson Jo',
+    author_email='a141890@gmail.com',
+    description=''
+)
