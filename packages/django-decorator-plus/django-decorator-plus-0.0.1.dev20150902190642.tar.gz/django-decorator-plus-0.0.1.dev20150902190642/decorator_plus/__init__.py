0db02f4ff355c6b7a6558e26f6356f41247f984d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:40 +0000

    Add package django-decorator-plus/0.0.1.dev20150902190642/django-decorator-plus-0.0.1.dev20150902190642.tar.gz

---
+++ packages/django-decorator-plus/django-decorator-plus-0.0.1.dev20150902190642.tar.gz/django-decorator-plus-0.0.1.dev20150902190642/decorator_plus/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Version and public API."""
+
+from __future__ import print_function, unicode_literals
+
+from decorator_plus.version import __version__
+
+__all__ = [
+    '__version__',
+]
