fdeac6a7fa1025991a8bf9551900a3979673d60f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:26 +0000

    Add package mcrunner/0.1.3/mcrunner-0.1.3.tar.gz

---
+++ packages/mcrunner/mcrunner-0.1.3.tar.gz/mcrunner-0.1.3/mcrunner/server.py
@@ -0,0 +1,112 @@
+from __future__ import absolute_import
+
+try:
+    # Python 2.x
+    import subprocess32 as subprocess
+except ImportError:
+    # Python 3.x
+    import subprocess
+
+
+from mcrunner.exceptions import ServerNotRunningException, ServerStartException
+
+
+SERVER_STOP_TIMEOUT_SEC = 60
+
+
+class MinecraftServer(object):
+
+    """
+    Minecraft Server class. Interface for communication to the Minecraft
+    server jar instance.
+    """
+
+    name = None
+    path = None
+    jar = None
+    opts = None
+    pipe = None
+
+    output = None
+
+    def __init__(self, name, path, jar, opts):
+        self.name = name
+        self.path = path
+        self.jar = jar
+        self.opts = opts
+
+    def _start_jar(self, args):
+        self.pipe = subprocess.Popen(
+            args,
+            cwd=self.path,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+
+    def start(self, connection=None):
+        """
+        Start the Minecraft server jar.
+        """
+        args = ['/usr/bin/java']
+        args.extend(self.opts.split())
+        args.extend([
+            '-jar',
+            '%s/%s' % (self.path, self.jar)
+        ])
+
+        if connection:
+            connection.send_message('Starting server %s...' % self.name)
+
+        try:
+            self._start_jar(args)
+        except OSError as e:
+            raise ServerStartException(e)
+
+        if connection:
+            connection.send_message('Server %s started.' % self.name)
+
+    def stop(self, connection=None):
+        """
+        Attempt to stop the running jar.
+        """
+        if not self.pipe:
+            raise ServerNotRunningException
+
+        if connection:
+            connection.send_message('Stopping server %s...' % self.name)
+
+        self.run_command('stop')
+
+        if connection:
+            try:
+                self.pipe.wait(timeout=SERVER_STOP_TIMEOUT_SEC)
+            except subprocess.TimeoutExpired:
+                connection.send_message('Server did not stop within %s seconds. Killing...' % SERVER_STOP_TIMEOUT_SEC)
+                self.pipe.terminate()
+            else:
+                connection.send_message('Server %s stopped.' % self.name)
+
+    def get_status(self):
+        """
+        Get the status of the server jar.
+        Returns either 'Running' or 'Not running'
+        """
+        try:
+            self.run_command('ping')
+        except ServerNotRunningException:
+            return 'Not running'
+
+        return 'Running'
+
+    def run_command(self, command, connection=None):
+        """
+        Attempt to run a command on the server.
+        """
+        if not self.pipe:
+            raise ServerNotRunningException
+
+        try:
+            self.pipe.stdin.write('%s\n' % command)
+        except Exception:
+            raise ServerNotRunningException
