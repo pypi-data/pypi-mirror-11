383e4030de34f39422c76e74c725991fc0fe731d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:17 +0000

    Add package GPyOpt/0.1.3/GPyOpt-0.1.3.tar.gz

---
+++ packages/GPyOpt/GPyOpt-0.1.3.tar.gz/GPyOpt-0.1.3/GPyOpt/__init__.py
@@ -0,0 +1,13 @@
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+import core
+import methods
+import util
+import fmodels
+import plotting
+import demos
+import interface
+
+
+
