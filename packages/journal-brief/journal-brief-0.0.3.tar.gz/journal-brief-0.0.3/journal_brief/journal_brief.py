ee17c385e3e67c837194476bb3beab82c747152a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:47 +0000

    Add package journal-brief/0.0.3/journal-brief-0.0.3.tar.gz

---
+++ packages/journal-brief/journal-brief-0.0.3.tar.gz/journal-brief-0.0.3/journal_brief/journal_brief.py
@@ -0,0 +1,142 @@
+"""
+Copyright (c) 2015 Tim Waugh <tim@cyberelk.net>
+
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+"""
+
+from collections.abc import Iterator
+import errno
+from journal_brief.constants import CONFIG_DIR
+from logging import getLogger
+import os
+from systemd import journal
+
+
+log = getLogger(__name__)
+
+
+class LatestJournalEntries(Iterator):
+    """
+    Iterate over new journal entries since last time
+    """
+
+    def __init__(self, cursor_file=None, log_level=None, reader=None,
+                 dry_run=False, this_boot=False):
+        """
+        Constructor
+
+        :param cursor_file: str, filename of cursor bookmark file
+        :param log_level: int, minimum log level
+        :param reader: systemd.journal.Reader instance
+        """
+        super(LatestJournalEntries, self).__init__()
+
+        self.cursor_file = cursor_file
+        try:
+            with open(self.cursor_file, "rt") as fp:
+                self.cursor = fp.read()
+        except IOError as ex:
+            if ex.errno == errno.ENOENT:
+                self.cursor = None
+            else:
+                raise
+
+        if reader is None:
+            reader = journal.Reader()
+
+        if log_level is not None:
+            reader.log_level(log_level)
+
+        if self.cursor and not this_boot:
+            log.debug("Seeking to %s", self.cursor)
+            reader.seek_cursor(self.cursor)
+            reader.get_next()
+        else:
+            reader.this_boot()
+
+        self.reader = reader
+        self.dry_run = dry_run
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.dry_run:
+            return
+
+        path = os.path.dirname(self.cursor_file)
+        try:
+            os.makedirs(path)
+        except OSError as ex:
+            if ex.errno == errno.EEXIST and os.path.isdir(path):
+                pass
+            else:
+                raise
+
+        with open(self.cursor_file, "wt") as fp:
+            fp.write(self.cursor)
+
+    def __next__(self):
+        fields = self.reader.get_next()
+        if not fields:
+            raise StopIteration
+
+        if '__CURSOR' in fields:
+            self.cursor = fields['__CURSOR']
+
+        return fields
+
+
+class EntryFormatter(object):
+    """
+    Convert a journal entry into a string
+    """
+
+    FORMAT = '{__REALTIME_TIMESTAMP} {_HOSTNAME} {SYSLOG_IDENTIFIER}: {MESSAGE}'
+    TIMESTAMP_FORMAT = '%b %d %T'
+
+    def format_timestamp(self, entry, field):
+        """
+        Convert entry field from datetime.datetime instance to string
+
+        Uses strftime() and TIMESTAMP_FORMAT
+        """
+
+        if field in entry:
+            dt = entry[field]
+            entry[field] = dt.strftime(self.TIMESTAMP_FORMAT)
+
+    def format(self, entry):
+        """
+        Format a journal entry using FORMAT
+
+        :param entry: dict, journal entry
+        :return: str, formatted string
+        """
+
+        self.format_timestamp(entry, '__REALTIME_TIMESTAMP')
+
+        if '_HOSTNAME' not in entry:
+            entry['_HOSTNAME'] = 'localhost'
+
+        if 'SYSLOG_IDENTIFIER' not in entry:
+            entry['SYSLOG_IDENTIFIER'] = entry.get('_COMM', '?')
+
+        if '_PID' in entry:
+            entry['SYSLOG_IDENTIFIER'] += '[{0}]'.format(entry['_PID'])
+        elif 'SYSLOG_PID' in entry:
+            entry['SYSLOG_IDENTIFIER'] += '[{0}]'.format(entry['SYSLOG_PID'])
+
+        return self.FORMAT.format(**entry)
---
+++ packages/journal-brief/journal-brief-0.0.3.tar.gz/journal-brief-0.0.3/tests/test_journal_brief.py
@@ -0,0 +1,181 @@
+"""
+Copyright (c) 2015 Tim Waugh <tim@cyberelk.net>
+
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+"""
+
+from datetime import datetime, timezone, timedelta
+from flexmock import flexmock
+from inspect import getsourcefile
+import journal_brief
+from journal_brief import LatestJournalEntries, EntryFormatter
+from systemd import journal
+import os
+import pytest
+import re
+
+
+class TestLatestJournalEntries(object):
+    def test_without_cursor(self, tmpdir):
+        final_cursor = '2'
+        (flexmock(journal.Reader)
+            .should_receive('seek_cursor')
+            .never())
+        (flexmock(journal.Reader)
+            .should_receive('this_boot')
+            .once())
+        (flexmock(journal.Reader)
+            .should_receive('get_next')
+            .and_return({'__CURSOR': '1'})
+            .and_return({'__CURSOR': final_cursor})
+            .and_return({}))
+
+        cursor_file = os.path.join(str(tmpdir), 'cursor')
+        with LatestJournalEntries(cursor_file=cursor_file) as entries:
+            e = list(entries)
+
+        assert len(e) == 2
+        with open(cursor_file, 'rt') as fp:
+            assert fp.read() == final_cursor
+
+    def test_with_cursor(self, tmpdir):
+        last_cursor = '2'
+        final_cursor = '4'
+        results = [{'__CURSOR': last_cursor},
+                   {'__CURSOR': '3'},
+                   {'__CURSOR': final_cursor}]
+        (flexmock(journal.Reader)
+            .should_receive('seek_cursor')
+            .with_args(last_cursor)
+            .once())
+        (flexmock(journal.Reader)
+            .should_receive('this_boot')
+            .never())
+        (flexmock(journal.Reader)
+            .should_receive('get_next')
+            .and_return(results[0])
+            .and_return(results[1])
+            .and_return(results[2])
+            .and_return({}))
+
+        cursor_file = os.path.join(str(tmpdir), 'cursor')
+        with open(cursor_file, 'wt') as fp:
+            fp.write(last_cursor)
+
+        with LatestJournalEntries(cursor_file=cursor_file) as entries:
+            e = list(entries)
+
+        assert e == results[1:]
+        with open(cursor_file, 'rt') as fp:
+            assert fp.read() == final_cursor
+
+    def test_this_boot(self, tmpdir):
+        last_cursor = '2'
+        final_cursor = '3'
+        results = [{'__CURSOR': '1'},
+                   {'__CURSOR': last_cursor},
+                   {'__CURSOR': final_cursor}]
+
+        (flexmock(journal.Reader)
+            .should_receive('seek_cursor')
+            .never())
+        (flexmock(journal.Reader)
+            .should_receive('this_boot')
+            .once())
+        (flexmock(journal.Reader)
+            .should_receive('get_next')
+            .and_return(results[0])
+            .and_return(results[1])
+            .and_return(results[2])
+            .and_return({}))
+
+        cursor_file = os.path.join(str(tmpdir), 'cursor')
+        with open(cursor_file, 'wt') as fp:
+            fp.write(last_cursor)
+
+        with LatestJournalEntries(cursor_file=cursor_file,
+                                  this_boot=True) as entries:
+            e = list(entries)
+
+        assert e == results
+        with open(cursor_file, 'rt') as fp:
+            fp.read() == final_cursor
+
+
+class TestEntryFormatter(object):
+    def test_timestamp(self):
+        dt = datetime.fromtimestamp(0, tz=timezone(timedelta(hours=1)))
+        entry = {'__REALTIME_TIMESTAMP': dt,
+                 'MESSAGE': 'epoch'}
+
+        formatter = EntryFormatter()
+
+        # Should output in local time
+        expected = 'Jan 01 01:00:00'
+
+        assert expected in formatter.format(entry)
+
+    @pytest.mark.parametrize(('entry', 'expected'), [
+        ({'MESSAGE': 'message'},
+         'localhost ?: message'),
+
+        ({'_HOSTNAME': 'host',
+          'MESSAGE': 'message'},
+         'host ?: message'),
+
+        ({'_HOSTNAME': 'host',
+          '_COMM': 'comm',
+          'MESSAGE': 'message'},
+         'host comm: message'),
+
+        ({'_HOSTNAME': 'host',
+          '_COMM': 'comm',
+          '_PID': '1',
+          'MESSAGE': 'message'},
+         'host comm[1]: message'),
+
+        ({'_HOSTNAME': 'host',
+          'SYSLOG_IDENTIFIER': 'syslogid',
+          '_COMM': 'comm',
+          'MESSAGE': 'message'},
+         'host syslogid: message'),
+
+        ({'_HOSTNAME': 'host',
+          'SYSLOG_IDENTIFIER': 'syslogid',
+          '_PID': '1',
+          'MESSAGE': 'message'},
+         'host syslogid[1]: message'),
+    ])
+    def test_format(self, entry, expected):
+        entry['__REALTIME_TIMESTAMP'] = datetime.fromtimestamp(0,
+                                                               tz=timezone.utc)
+        formatter = EntryFormatter()
+        formatted = formatter.format(entry)
+        date = 'Jan 01 00:00:00 '
+        assert formatted.startswith(date)
+        assert formatted[len(date):] == expected
+
+
+def test_version():
+    """
+    Check the version numbers agree
+    """
+    this_file = getsourcefile(test_version)
+    regexp = re.compile(r"\s+version='([0-9.]+)',  # also update")
+    with open(os.path.join(os.path.dirname(this_file), '../setup.py')) as fp:
+        matches = map(regexp.match, fp.readlines())
+        matches = [match for match in matches if match]
+        assert len(matches) == 1
+        assert matches[0].groups()[0] == journal_brief.__version__
