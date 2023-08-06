9e4cb0f573b0a78dec9d90890008007b527037e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:38 +0000

    Add package tracestack/0.1.9.5/tracestack-0.1.9.5.tar.gz

---
+++ packages/tracestack/tracestack-0.1.9.5.tar.gz/tracestack-0.1.9.5/tracestack/console.py
@@ -0,0 +1,23 @@
+import sys
+from code import InteractiveConsole
+from tracestack.handler import ExceptionHandler
+
+
+class TracestackConsole(InteractiveConsole):
+    """An interactive console for running code or REPL loops that handle
+    errors using the tracestack handler."""
+
+    def __init__(self, *args, **kwargs):
+        self.handler = ExceptionHandler(*args, **kwargs)
+        kwargs.pop("engine")
+        kwargs.pop("prompt")
+        kwargs.pop("arguments")
+        InteractiveConsole.__init__(self, *args, **kwargs)
+
+    def showtraceback(self):
+        InteractiveConsole.showtraceback(self)
+        self.handler.handle_error(*sys.exc_info())
+
+    def showsyntaxerror(self, filename=None):
+        InteractiveConsole.showsyntaxerror(self, filename)
+        self.handler.handle_error(*sys.exc_info())
