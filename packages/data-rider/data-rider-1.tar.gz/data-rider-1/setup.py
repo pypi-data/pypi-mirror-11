aed3d96452b72c20c191afae29dc5b4d4370bd69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:27 +0000

    Add package data-rider/1/data-rider-1.tar.gz

---
+++ packages/data-rider/data-rider-1.tar.gz/data-rider-1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='data-rider',
+      version='1',
+      description='Sources for riding on data.',
+      author='Mark',
+      author_email='juhoon.kim@me.com',
+      url='http://data-rider.blogspot.kr',
+      packages=['data_rider'],
+     )
