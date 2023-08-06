a07a3facd6efb8a890191f046b881c49a257eda3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:31 +0000

    Add package pyhacrf/0.1.1/pyhacrf-0.1.1.tar.gz

---
+++ packages/pyhacrf/pyhacrf-0.1.1.tar.gz/pyhacrf-0.1.1/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, Extension
+from codecs import open
+from os import path
+
+
+# from Michael Hoffman's http://www.ebi.ac.uk/~hoffman/software/sunflower/
+class NumpyExtension(Extension):
+
+    def __init__(self, *args, **kwargs):
+        from numpy import get_include
+        from numpy.distutils.misc_util import get_info
+        kwargs.update(get_info('npymath'))
+        kwargs['include_dirs'] += [get_include()]
+
+        Extension.__init__(self, *args, **kwargs)
+
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='pyhacrf',
+    version='0.1.1',
+    packages=['pyhacrf'],
+    install_requires=['numpy>=1.9', 'PyLBFGS>=0.1.3'],
+    ext_modules=[NumpyExtension('pyhacrf.algorithms', 
+                                ['pyhacrf/algorithms.c'])],
+    url='https://github.com/dirko/pyhacrf',
+    download_url='https://github.com/dirko/pyhacrf/tarball/0.1.1',
+    license='BSD',
+    author='Dirko Coetsee',
+    author_email='dpcoetsee@gmail.com',
+    description='Hidden alignment conditional random field, a discriminative string edit distance',
+    long_description=long_description,
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        ],
+    )
