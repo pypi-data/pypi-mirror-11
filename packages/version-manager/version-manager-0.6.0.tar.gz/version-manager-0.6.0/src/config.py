7a9e8421bdb9ab53e20459d2e43e7a8e62dcdd7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:12 +0000

    Add package version-manager/0.6.0/version-manager-0.6.0.tar.gz

---
+++ packages/version-manager/version-manager-0.6.0.tar.gz/version-manager-0.6.0/src/config.py
@@ -0,0 +1,93 @@
+from copy import deepcopy
+import json
+from os.path import expanduser
+from colorama import Fore
+
+__author__ = 'ahmetdal'
+
+_RC_FILE = '.vmrc'
+
+_RC_LOOKUP_PATHS = [
+    expanduser("~/"),
+    './'
+]
+
+_INITIAL_CONFIG = {
+    'files': [
+        {
+            'name': 'setup.py',
+            'color': Fore.GREEN,
+            'parser': 'regexp',
+            'kwargs': {
+                'regex': '(?P<match_left>version\s*=\s*(?:"|\'))(?P<version>(?:(?:\d+)+.?)+)(?P<match_right>"|\')'
+            }
+
+        },
+        {
+            'name': 'conf.py',
+            'color': Fore.BLUE,
+            'parser': 'regexp',
+            'kwargs': {
+                'regex': '(?P<match_left>version\s*=\s*(?:"|\'))(?P<version>(?:(?:\d+)+.?)+)(?P<match_right>"|\')'
+            }
+
+        },
+        {
+            'name': 'conf.py',
+            'color': Fore.BLUE,
+            'parser': 'regexp',
+            'kwargs': {
+                'regex': '(?P<match_left>release\s*=\s*(?:"|\'))(?P<version>(?:(?:\d+)+.?)+)(?P<match_right>"|\')'
+            }
+
+        },
+        {
+            'name': 'bower.json',
+            'color': Fore.YELLOW,
+            'parser': 'regexp',
+            'kwargs': {
+                'regex': '(?P<match_left>"version"\s*:\s*(?:"|\'))(?P<version>(?:(?:\d+)+.?)+)(?P<match_right>"|\')'
+            }
+        },
+        {
+            'name': 'package.json',
+            'color': Fore.RED,
+            'parser': 'regexp',
+            'kwargs': {
+                'regex': '(?P<match_left>"version"\s*:\s*(?:"|\'))(?P<version>(?:(?:\d+)+.?)+)(?P<match_right>"|\')'
+            }
+        },
+        {
+            'name': 'pom.xml',
+            'color': Fore.MAGENTA,
+            'parser': 'xml',
+            'kwargs': {
+                'xpaths': ['./mvn:version', './mvn:parent/mvn:version'],
+                'namespaces': {'mvn': 'http://maven.apache.org/POM/4.0.0'}
+            }
+        },
+    ],
+    'excludes': [
+        '.git',
+        '.tmp',
+        'dist',
+        'build',
+        'node_modules',
+        'bower_components',
+        '.tox'
+    ]
+}
+
+
+def load_config():
+    _LOADED_CONFIG = deepcopy(_INITIAL_CONFIG)
+    for lookup_path in _RC_LOOKUP_PATHS:
+        try:
+            with open("%s%s" % (lookup_path, _RC_FILE)) as f:
+                config = json.loads(f.read())
+                _LOADED_CONFIG["files"].extend(config.get("files", []))
+                _LOADED_CONFIG["excludes"].extend(config.get("excludes", []))
+        except IOError as e:
+            pass
+
+    return _LOADED_CONFIG
---
+++ packages/version-manager/version-manager-0.6.0.tar.gz/version-manager-0.6.0/src/file.py
@@ -0,0 +1,93 @@
+import os
+import re
+from repoze.lru import CacheMaker
+import semver
+from src.config import load_config
+from src.parser.parser import PARSER_REGISTRY
+from colorama import Fore, Back, Style
+
+cache_maker = CacheMaker()
+
+__author__ = 'ahmetdal'
+
+version_pattern = re.compile(r'((\d)+.?)+')
+
+
+class File:
+    supported_bumps = ['major', 'minor', 'patch']
+
+    def __init__(self, name, path, parser, color=Fore.WHITE):
+        self.name = name
+        self.path = path
+        self.parser = parser
+        self.color = color
+        if not self.parser:
+            raise Exception("Parser must be given.")
+
+    @property
+    @cache_maker.lrucache(maxsize=300, name="content")
+    def content(self):
+        with open(self.path, 'r') as f:
+            return f.read()
+
+    @property
+    @cache_maker.lrucache(maxsize=300, name="current_version")
+    def current_version(self):
+        current_version = self.parser.current_version(self.content)
+        if not current_version:
+            print("%sNo version definition is found in file %s" % (self.color, self.path))
+        return current_version
+
+    def update_version(self, new_version):
+        print('%sFile %s WAS on version %s' % (self.color, self.path, self.current_version))
+        new_content = self.parser.update_version(self.content, new_version)
+        with open(self.path, 'w') as f:
+            f.write(new_content)
+        cache_maker.clear("current_version")
+        cache_maker.clear("content")
+        print('%sFile %s IS NOW on version %s\n' % (self.color, self.path, self.current_version))
+
+    def bump_version(self, bump):
+        if bump not in self.supported_bumps:
+            raise Exception('%s is not one among %s' % (bump, self.supported_bumps))
+        new_version = getattr(semver, 'bump_%s' % bump)(self.current_version)
+        self.update_version(new_version)
+
+
+loaded_files = []
+
+
+class FileLoader:
+    def __init__(self):
+        self.config = load_config()
+        self.files = self.config.get("files", [])
+        self.excludes = self.config.get("excludes", [])
+
+    def load(self):
+        for dirpath, dirnames, files in os.walk('./'):
+            for exclude in self.excludes:
+                try:
+                    dirnames.remove(exclude)
+                except ValueError:
+                    pass
+
+                try:
+                    files.remove(exclude)
+                except ValueError:
+                    pass
+
+            for f in files:
+                config_files = filter(lambda x: x.get('name') == f, self.files)
+                for config_file in config_files:
+                    parser_type = config_file.get('parser', 'regexp')
+                    color = config_file.get('color', Fore.WHITE)
+                    ParserClass = PARSER_REGISTRY.get(parser_type)
+                    if ParserClass:
+                        parser = ParserClass(*config_file.get('args', []), **config_file.get('kwargs', {}))
+                        loaded_files.append(File(config_file.get('name'), os.path.abspath(os.path.join(dirpath, f)), parser, color=color))
+                    else:
+                        raise Exception("No registered with parser type %s. Available parsers are %s" % (parser_type, ','.join(PARSER_REGISTRY.keys())))
+
+
+FileLoader().load()
+loaded_files = sorted(loaded_files, key=lambda f: f.name)
