ba68dfcc5d3eca72ee3c4e84f3fe452302a048f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:20 +0000

    Add package pytsite/0.8.6/PytSite-0.8.6.tar.gz

---
+++ packages/pytsite/PytSite-0.8.6.tar.gz/PytSite-0.8.6/pytsite/admin/eps.py
@@ -0,0 +1,12 @@
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+from pytsite import metatag as _metatag, tpl as _tpl, lang as _lang
+
+
+def dashboard(args: dict, inp: dict):
+    """Dashboard endpoint.
+    """
+    _metatag.t_set('title', _lang.t('pytsite.admin@dashboard'))
+    return _tpl.render('pytsite.admin@html')
