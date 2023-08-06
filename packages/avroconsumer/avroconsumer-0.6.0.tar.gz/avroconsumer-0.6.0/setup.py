0b3bd06a6423a73bf6109e4a235fc1eda40fc4a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:49 +0000

    Add package avroconsumer/0.6.0/avroconsumer-0.6.0.tar.gz

---
+++ packages/avroconsumer/avroconsumer-0.6.0.tar.gz/avroconsumer-0.6.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+classifiers = ['Development Status :: 4 - Beta',
+               'Intended Audience :: Developers',
+               'License :: OSI Approved :: BSD License',
+               'Operating System :: OS Independent',
+               'Programming Language :: Python :: 2',
+               'Programming Language :: Python :: 2.6',
+               'Programming Language :: Python :: 2.7',
+               'Topic :: Software Development :: Libraries',
+               'Topic :: Software Development :: Libraries :: Python Modules']
+
+setup(name='avroconsumer',
+      version='0.6.0',
+      description="Simplified PostgreSQL client built upon Psycopg2",
+      maintainer="Gavin M. Roy",
+      maintainer_email="gavinr@aweber.com",
+      url="https://github.com/aweber/avroconsumer",
+      install_requires=['rejected', 'avro'],
+      license='BSDv3',
+      package_data={'': ['LICENSE', 'README.rst']},
+      py_modules=['avroconsumer'],
+      classifiers=classifiers)
