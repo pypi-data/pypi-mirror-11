7ef14d2b0f7509b6e59327b2a910717eebaffda0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:46 +0000

    Add package pytsite/0.16/PytSite-0.16.tar.gz

---
+++ packages/pytsite/PytSite-0.16.tar.gz/PytSite-0.16/pytsite/auth_ui/_widget.py
@@ -0,0 +1,41 @@
+"""Auth UI Widgets.
+"""
+from pytsite import auth as _auth, widget as _widget, html as _html, tpl as _tpl, reg as _reg
+
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+
+class Profile(_widget.Base):
+    """User Profile Widget.
+    """
+    def __init__(self, user, tpl: str='pytsite.auth_ui@widget/profile', **kwargs):
+        """Init.
+
+        :type user: pytsite.auth._model.User|pytsite.auth_ui._model.UserUI
+        """
+        super().__init__(**kwargs)
+        self._user = user
+        self._tpl = tpl
+        self._css += ' widget-auth-ui-profile'
+
+    def render(self) -> _html.Element:
+        """Render the widget.
+        """
+        current_user = _auth.get_current_user()
+
+        if not self._user.profile_is_public and current_user.id != self._user.id:
+            return ''
+
+        profile_is_editable = False
+        if not current_user.is_anonymous:
+            if current_user.id == self._user.id or current_user.has_permission('pytsite.odm_ui.modify.user'):
+                profile_is_editable = True
+
+        wrapper = _html.TagLessElement(_tpl.render(self._tpl, {
+            'user': self._user,
+            'profile_is_editable': profile_is_editable
+        }))
+
+        return self._group_wrap(wrapper)
---
+++ packages/pytsite/PytSite-0.16.tar.gz/PytSite-0.16/pytsite/flag/__init__.py
@@ -0,0 +1,33 @@
+"""PytSite Flag Package.
+"""
+# Public API
+from . import _widget as widget
+from ._functions import count, delete
+
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+
+def __init():
+    import sys
+    from pytsite import assetman
+    from pytsite import odm
+    from pytsite import events
+    from pytsite import tpl
+    from pytsite import lang
+    from . import _model
+
+    def router_dispatch_eh():
+        assetman.add('pytsite.flag@css/common.css')
+        assetman.add('pytsite.flag@js/common.js')
+
+    lang.register_package(__name__)
+    assetman.register_package(__name__)
+    tpl.register_package(__name__)
+    tpl.register_global('flag', sys.modules[__package__])
+    odm.register_model('flag', _model.Flag)
+
+    events.listen('pytsite.router.dispatch', router_dispatch_eh)
+
+__init()
