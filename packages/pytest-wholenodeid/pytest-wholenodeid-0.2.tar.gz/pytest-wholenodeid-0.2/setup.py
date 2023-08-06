4ceacee8f3db16d609f8794b8d86076a0084de68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:33 +0000

    Add package pytest-wholenodeid/0.2/pytest-wholenodeid-0.2.tar.gz

---
+++ packages/pytest-wholenodeid/pytest-wholenodeid-0.2.tar.gz/pytest-wholenodeid-0.2/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import re
+from setuptools import setup
+
+
+READMEFILE = 'README.rst'
+VERSIONFILE = 'pytest_wholenodeid.py'
+VSRE = r"""^__version__ = ['"]([^'"]*)['"]"""
+
+
+def get_version():
+    version_file = open(VERSIONFILE, 'rt').read()
+    return re.search(VSRE, version_file, re.M).group(1)
+
+
+setup(
+    name='pytest-wholenodeid',
+    version=get_version(),
+    description='pytest addon for displaying the whole node id for failures',
+    long_description=open(READMEFILE).read(),
+    license='Simplified BSD License',
+    author='Will Kahn-Greene',
+    author_email='willkg@bluesock.org',
+    keywords='py.test pytest',
+    url='https://github.com/willkg/pytest-wholenodeid',
+    zip_safe=True,
+    py_modules=['pytest_wholenodeid'],
+    entry_points={
+        'pytest11': [
+            'wholenodeid = pytest_wholenodeid'
+        ]
+    },
+    install_requires=[
+        'pytest>=2.0'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Unix',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ]
+)
