acee6f76afb55b87dad084fdf2c4b70494e7c5fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:42 +0000

    Add package poll/1.0/poll-1.0.tar.gz

---
+++ packages/poll/poll-1.0.tar.gz/poll-1.0/setup.py
@@ -0,0 +1,34 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+
+    from setuptools import setup, find_packages
+
+
+setup(
+    name='poll',
+    version='1.0',
+    author="Benjamin Hodgson",
+    author_email="benjamin.hodgson@huddle.net",
+    url="https://github.com/benjamin-hodgson/poll",
+    description="Utilities for polling, retrying, and exception handling",
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    install_requires=["setuptools"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
