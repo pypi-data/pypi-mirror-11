85e6e20fda5509d24c786fca5daad9c470b5c971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:18 +0000

    Add package StoreData/0.6/StoreData-0.6-py2.7.egg

---
+++ packages/StoreData/StoreData-0.6-py2.7.egg/StoreData/__init__.py
@@ -0,0 +1,14 @@
+"""
+    create a test class to test pickle
+"""
+
+class StoreData:
+
+    def __init__(self):
+        self.data = [1,2,3]
+
+    def set_data(self, data):
+        self.data = data
+
+
+
