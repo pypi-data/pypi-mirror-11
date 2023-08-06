69e53357dd8c22c858cad6ef0e8b45a8befcd41f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:46 +0000

    Add package output/1.0.0/output-1.0.0.zip

---
+++ packages/output/output-1.0.0.zip/output-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name = 'output',
+      version = '1.0.0',
+      py_modules =['output'],
+      author = 'Jay Han',
+      description = 'print list',
+      
+
+
+    )
