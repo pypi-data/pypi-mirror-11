f9821f029ac912ebdb36140f865ddd95c9387955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:35 +0000

    Add package wagtail/1.1/wagtail-1.1-py2.py3-none-any.whl

---
+++ packages/wagtail/wagtail-1.1-py2.py3-none-any.whl/wagtail/utils/deprecation.py
@@ -0,0 +1,9 @@
+class RemovedInWagtail12Warning(DeprecationWarning):
+    pass
+
+
+removed_in_next_version_warning = RemovedInWagtail12Warning
+
+
+class RemovedInWagtail13Warning(PendingDeprecationWarning):
+    pass
