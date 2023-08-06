68fd80c3c17776af67bab7912010b00852d988d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:34 +0000

    Add package digital_ocean_api/0.1.2/digital_ocean_api-0.1.2.tar.gz

---
+++ packages/digital_ocean_api/digital_ocean_api-0.1.2.tar.gz/digital_ocean_api-0.1.2/digital_ocean_api/__init__.py
@@ -0,0 +1,9 @@
+from droplet import list_all as list_droplets
+from droplet import get_by_name as find_droplet_by_name
+from droplet import get_attribute as get_droplet_attribute
+from droplet import create as create_droplet
+from droplet import delete_by_name as delete_droplet_by_name
+from droplet import get_by_prefix as get_droplets_by_prefix
+
+
+from ssh_key import list_all as list_ssh_keys
