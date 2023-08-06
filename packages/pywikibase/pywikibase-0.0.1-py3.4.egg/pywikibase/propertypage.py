fab50b23e3e64e554c4e12231e785f88ac6051b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:56 +0000

    Add package pywikibase/0.0.1/pywikibase-0.0.1-py3.4.egg

---
+++ packages/pywikibase/pywikibase-0.0.1-py3.4.egg/pywikibase/propertypage.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8  -*-
+"""
+Handling a Wikibase property pages.
+"""
+
+#
+# (C) Pywikibot team, 2008-2015
+#
+# Distributed under the terms of the MIT license.
+#
+
+
+from pywikibase.wikibasepage import WikibasePage
+from pywikibase.wbproperty import Property
+from pywikibase.claim import Claim
+
+
+class PropertyPage(WikibasePage, Property):
+
+    """
+    A Wikibase entity in the property namespace.
+
+    Should be created as::
+
+        PropertyPage('P21')
+    """
+
+    def __init__(self, source=None, title=""):
+        """
+        Constructor.
+
+        @param title: page name of property, like "P##"
+        @type title: str
+        """
+        WikibasePage.__init__(self, source, title)
+        if not title or not self.id.startswith('P'):
+            raise ValueError(
+                "'%s' is not an property page title" % title)
+        Property.__init__(self, source, self.id)
+
+    def get(self, force=False, *args):
+        """
+        Fetch the property entity, and cache it.
+
+        @param force: override caching
+        @param args: values of props
+        """
+        if force or not hasattr(self, '_content'):
+            WikibasePage.get(self, force=force, *args)
+
+    def newClaim(self, *args, **kwargs):
+        """
+        Helper function to create a new claim object for this property.
+
+        @return: Claim
+        """
+        return Claim(self.getID(), datatype=self.type,
+                     *args, **kwargs)
