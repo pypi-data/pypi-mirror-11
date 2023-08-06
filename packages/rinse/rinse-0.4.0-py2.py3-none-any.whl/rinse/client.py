720243525a27b4f80898123326f529adc2fa0ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:46 +0000

    Add package rinse/0.4.0/rinse-0.4.0-py2.py3-none-any.whl

---
+++ packages/rinse/rinse-0.4.0-py2.py3-none-any.whl/rinse/client.py
@@ -0,0 +1,44 @@
+"""SOAP client."""
+from __future__ import print_function
+import requests
+from rinse import ENVELOPE_XSD
+from rinse.util import SCHEMA, cached_property
+from rinse.response import RinseResponse
+
+
+class SoapClient(object):
+
+    """Rinse SOAP client."""
+
+    def __init__(self, url, debug=False, **kwargs):
+        """Set base attributes."""
+        self.url = url
+        self.debug = debug
+        self.kwargs = kwargs
+        self.operations = {}
+        self.soap_schema = SCHEMA[ENVELOPE_XSD]
+
+    @cached_property
+    def _session(self):
+        """Cached instance of requests.Session."""
+        return requests.Session()
+
+    def __call__(self, msg, action="", build_response=RinseResponse,
+                 debug=False):
+        """Post 'msg' to remote service."""
+        # generate HTTP request from msg
+        request = msg.request(self.url, action).prepare()
+        if debug or self.debug:
+            print('{} {}'.format(request.method, self.url))
+            print(
+                ''.join(
+                    '{}: {}\n'.format(name, val)
+                    for name, val
+                    in sorted(request.headers.items())
+                )
+            )
+            print(request.body.decode('utf-8'))
+
+        # perform HTTP(s) POST
+        resp = self._session.send(request)
+        return build_response(resp)
