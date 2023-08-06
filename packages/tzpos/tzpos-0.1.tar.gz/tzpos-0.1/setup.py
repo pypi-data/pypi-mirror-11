4a9b5e1de624a3d27df3bc7bc27558275fd66d2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:47 +0000

    Add package tzpos/0.1/tzpos-0.1.tar.gz

---
+++ packages/tzpos/tzpos-0.1.tar.gz/tzpos-0.1/setup.py
@@ -0,0 +1,48 @@
+import os
+import sys
+
+import tzpos
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+packages = [
+    'tzpos'
+]
+
+requires = ['pytz']
+
+setup(
+    name='tzpos',
+    version=tzpos.__version__,
+    description='Time zone name calculation from lon and lat database.',
+    long_description=open('README.txt').read(),
+    author='Gamaliel Espinoza Macedo',
+    author_email='gamaliel.espinoza@gmail.com',
+    url='',
+    packages=packages,
+    package_data={
+        '': ['LICENSE', 'NOTICE'],
+        'tzpos': ['*.pem'],
+        'tzpos': ['tzposdb.data'],
+    },
+    package_dir={'tzpos': 'tzpos'},
+    include_package_data=True,
+    install_requires=requires,
+    # license='Apache 2.0',
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ),
+)
