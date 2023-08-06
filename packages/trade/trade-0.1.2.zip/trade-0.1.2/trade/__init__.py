b68ffc933030feb41ca5dfcb37097bbf70dcc452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:38 +0000

    Add package trade/0.1.2/trade-0.1.2.zip

---
+++ packages/trade/trade-0.1.2.zip/trade-0.1.2/trade/__init__.py
@@ -0,0 +1,37 @@
+"""trade: Tools For Stock Trading Applications.
+
+Copyright (c) 2015 Rafael da Silva Rocha
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+from __future__ import absolute_import
+
+from .asset import Asset,Derivative
+from .operation import Operation
+from .accumulator import Accumulator
+from .event import Event
+from .tax_manager import TaxManager
+from .operation_container import OperationContainer
+from .portfolio import Portfolio
+from .utils import average_price, same_sign
+from .plugins import *
+
+__author__ = 'rocha.rafaelsilva@gmail.com'
+__version__ = '0.1.2'
