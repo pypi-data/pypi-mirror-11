75baaccf5b3fa7cc56e7a3926150d61a87b6b079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:52 +0000

    Add package pyGoogleSearch/2.0.0/pyGoogleSearch-2.0.0.zip

---
+++ packages/pyGoogleSearch/pyGoogleSearch-2.0.0.zip/pyGoogleSearch-2.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyGoogleSearch',
+    version='2.0.0',
+    packages=['pyGoogleSearch'],
+    url='https://github.com/mdonnalley/pyGoogleSearch',
+    license='',
+    author='donnalley',
+    author_email='michael.donnalley@gmail.com',
+    description='Package for collecting and aggregating data from various Google sources (Web, News, and Scholar)'
+)
