c1e231e9ba6fcb97ae64a96ab5eee921a7510020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:44 +0000

    Add package western/1.0.0/western-1.0.0.zip

---
+++ packages/western/western-1.0.0.zip/western-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'western',
+		version = '1.0.0',
+		py_modules = ['western'],
+		author = 'zxk',
+		author_email = 'flashpacker@163.com',
+		url = 'www.github.com',
+		description = 'simple function',
+	)
