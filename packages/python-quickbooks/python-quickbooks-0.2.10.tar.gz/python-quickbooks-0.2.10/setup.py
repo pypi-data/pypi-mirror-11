33dcba41872bb219dd40884b05d5c993fc958ec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:45 +0000

    Add package python-quickbooks/0.2.10/python-quickbooks-0.2.10.tar.gz

---
+++ packages/python-quickbooks/python-quickbooks-0.2.10.tar.gz/python-quickbooks-0.2.10/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+VERSION = (0, 2, 10)
+version = '.'.join(map(str, VERSION))
+
+setup(
+    name='python-quickbooks',
+    version=version,
+    author='Edward Emanuel Jr.',
+    author_email='edward@sidecarsinc.com',
+    description='A Python library for accessing the Quickbooks API.',
+    url='https://github.com/sidecars/python-quickbooks',
+    license='MIT',
+    keywords=['quickbooks', 'qbo', 'accounting'],
+
+    install_requires=[
+        'setuptools',
+        'rauth>=0.7.1',
+        'requests>=2.7.0',
+        'simplejson>=2.2.0',
+        'six>=1.4.0',
+        'python-dateutil',
+    ],
+
+    packages=find_packages(),
+)
