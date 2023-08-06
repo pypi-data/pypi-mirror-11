16039988bc583b2cedde74d7607b4080677bdc51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:12 +0000

    Add package nbgrader/0.2.1/nbgrader-0.2.1-py2.py3-none-any.whl

---
+++ packages/nbgrader/nbgrader-0.2.1-py2.py3-none-any.whl/nbgrader/tests/apps/test_nbgrader.py
@@ -0,0 +1,24 @@
+import os
+
+from nbgrader.tests import run_command
+from nbgrader.tests.apps.base import BaseTestApp
+
+
+class TestNbGrader(BaseTestApp):
+
+    def test_help(self):
+        """Does the help display without error?"""
+        run_command(["nbgrader", "--help-all"])
+
+    def test_no_subapp(self):
+        """Is the help displayed when no subapp is given?"""
+        run_command(["nbgrader"], retcode=1)
+
+    def test_generate_config(self):
+        """Is the config file properly generated?"""
+
+        run_command(["nbgrader", "--generate-config"])
+        assert os.path.isfile("nbgrader_config.py")
+
+        # does it fail if it already exists?
+        run_command(["nbgrader", "--generate-config"], retcode=1)
