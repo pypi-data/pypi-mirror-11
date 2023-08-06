11454a765a0feccaa9d756620eca4c6866a0aa07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:02 +0000

    Add package imap-utils/0.5/imap-utils-0.5.tar.bz2

---
+++ packages/imap-utils/imap-utils-0.5.tar.bz2/imap-utils-0.5/IMAPUtils/Codec.py
@@ -0,0 +1,92 @@
+# -*- coding: UTF-8 -*-
+# vim: expandtab sw=4 ts=4 sts=4:
+'''
+IMAP UTF-7 codec
+'''
+__author__ = 'Michal Čihař'
+__email__ = 'michal@cihar.com'
+__license__ = '''
+Copyright (c) 2003 - 2014 Michal Čihař
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import codecs
+
+
+def modified_base64(s):
+    s_utf7 = s.encode('utf-7')
+    return s_utf7[1:-1].replace('/', ',')
+
+
+def modified_unbase64(s):
+    s_utf7 = '+' + s.replace(',', '/') + '-'
+    return s_utf7.decode('utf-7')
+
+
+def encoder(s):
+    r = []
+    _in = []
+    for c in s:
+        if ord(c) in (range(0x20, 0x26) + range(0x27, 0x7f)):
+            if _in:
+                r.extend(['&', modified_base64(''.join(_in)), '-'])
+                del _in[:]
+            r.append(str(c))
+        elif c == '&':
+            if _in:
+                r.extend(['&', modified_base64(''.join(_in)), '-'])
+                del _in[:]
+            r.append('&-')
+        else:
+            _in.append(c)
+    if _in:
+        r.extend(['&', modified_base64(''.join(_in)), '-'])
+    return (''.join(r), len(s))
+
+
+def decoder(s):
+    r = []
+    decode = []
+    for c in s:
+        if c == '&' and not decode:
+            decode.append('&')
+        elif c == '-' and decode:
+            if len(decode) == 1:
+                r.append('&')
+            else:
+                r.append(modified_unbase64(''.join(decode[1:])))
+            decode = []
+        elif decode:
+            decode.append(c)
+        else:
+            r.append(c)
+    if decode:
+        r.append(modified_unbase64(''.join(decode[1:])))
+    return (u''.join(r), len(s))
+
+
+class StreamReader(codecs.StreamReader):
+    def decode(self, s, errors='strict'):
+        return decoder(s)
+
+
+class StreamWriter(codecs.StreamWriter):
+    def decode(self, s, errors='strict'):
+        return encoder(s)
+
+
+def imap4_utf_7(name):
+    if name == 'imap4-utf-7':
+        return (encoder, decoder, StreamReader, StreamWriter)
---
+++ packages/imap-utils/imap-utils-0.5.tar.bz2/imap-utils-0.5/README.rst
@@ -0,0 +1,87 @@
+IMAP Utils
+==========
+
+Collection of small script useful to manage IMAP account.
+
+.. image:: https://landscape.io/github/nijel/imap-utils/master/landscape.png
+    :alt: Code Health
+    :target: https://landscape.io/github/nijel/imap-utils/master
+
+Homepage
+--------
+
+<http://cihar.com/software/imap-utils/>
+
+License
+-------
+
+GNU GPL version 3 or later.
+
+Configuration
+-------------
+
+All utils share ~/.imap-utils configuration file. IMAP connection is
+configuration section::
+
+    [IMAP]
+    host = hostname ; Host where to connect
+    login = user ; User name
+    password = password ; Password
+    ssl = 1 ; Whether to use SSL
+    simulate = 1 ; Do not perform actions, only simulate
+    verbose = 1 ; Print what is going on
+
+
+imap-purge
+----------
+
+Purges old emails from defined folders. Configuration sections::
+
+    [purge]
+    folder = <read>,<unread> ; Purges mails in folder
+
+    [purge-recursive]
+    folder = <read>,<unread> ; Purges mails in folder and sub folders
+
+Read mails are purged after <read> days, unread after <unread> days.
+
+
+imap-folders
+------------
+
+Creates folders on IMAP. Configuration sections::
+
+    [create]
+    folder = 1
+    anotherfolder = 1
+
+
+imap-stats
+----------
+
+Lists number of unread/unseen mails on IMAP.
+
+
+store-message
+-------------
+
+Stores message from stdin to folder defined by first argument.
+
+
+process-messages
+----------------
+
+Reads all messages from folder (first argument) and passes them to
+external program (second argument).
+
+
+Bug reporting
+-------------
+
+Please report found bugs to <https://github.com/nijel/imap-utils/issues>.
+
+
+Version control
+---------------
+
+Developed in Git on GitHub: https://github.com/nijel/imap-utils
