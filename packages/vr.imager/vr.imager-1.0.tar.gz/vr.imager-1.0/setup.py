453718ca343ef76c6468b9419793b8dba3ea9b60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:36 +0000

    Add package vr.imager/1.0/vr.imager-1.0.tar.gz

---
+++ packages/vr.imager/vr.imager-1.0.tar.gz/vr.imager-1.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+from setuptools import setup, find_packages
+
+setup(
+    name='vr.imager',
+    namespace_packages=['vr'],
+    version='1.0',
+    author='Brent Tubbs',
+    author_email='brent.tubbs@gmail.com',
+    url='https://bitbucket.org/yougov/vr.imager',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'vr.runners>=2.4.3,<3',
+        'jaraco.ui',
+    ],
+    entry_points={
+        'console_scripts': [
+            'vimage = vr.imager.command:main',
+        ],
+    },
+    setup_requires=[
+        'setuptools_scm',
+    ],
+    description='Command line tool to create system image tarballs.',
+)
