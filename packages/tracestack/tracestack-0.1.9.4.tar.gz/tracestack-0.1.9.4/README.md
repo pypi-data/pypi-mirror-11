2196dc946d115e33d42a5a3a859cd8c304f68c88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:37 +0000

    Add package tracestack/0.1.9.4/tracestack-0.1.9.4.tar.gz

---
+++ packages/tracestack/tracestack-0.1.9.4.tar.gz/tracestack-0.1.9.4/README.md
@@ -0,0 +1,109 @@
+# Tracestack
+
+Tracestack lets you search your latest Python error message on Stackoverflow or Google, with a single keystroke.
+
+## Features
+
+* Integrate tracestack into your code in seconds, or run it from the command line
+* Post-mortem mode
+* REPL mode
+* Compatible with Python 2 and 3
+* Can search for answers on:
+    * Google, limited to stackoverflow.com (default)
+    * Google, searching the entire web
+    * StackOverflow's own search engine
+
+## Installation
+
+    pip install tracestack
+
+## Uses and Examples
+
+Do a post-mortem autopsy of your last exception ...
+
+    >>> 1 / 0
+    Traceback (most recent call last):
+      File "<console>", line 1, in <module>
+    ZeroDivisionError: integer division or modulo by zero
+
+    >>> tracestack.pm()
+    Traceback (most recent call last):
+      File "<console>", line 1, in <module>
+    ZeroDivisionError: integer division or modulo by zero
+    Hit spacebar to search this error message on Stack Overflow (using Google):
+
+... or catch all future exceptions ...
+
+    >>> tracestack.enable()
+    >>> 1 / 0
+    Traceback (most recent call last):
+      File "<console>", line 1, in <module>
+    ZeroDivisionError: integer division or modulo by zero
+    Hit spacebar to search this error message on Stack Overflow (using Google):
+
+    >>> tracestack.disable()
+
+... or catch exceptions in only one function ...
+
+    from tracestack import trace
+
+    >>> @trace
+    >>> def divide_by_zero():
+            1 / 0
+
+... or in any Python script run from the command line ...
+
+    $ tracestack manage.py runserver
+
+... or in a read-evaluate-print loop ... 
+
+    $ tracestack
+    Python 2.7.6 (default, Sep  9 2014, 15:04:36) 
+    [GCC 4.2.1 Compatible Apple LLVM 6.0 (clang-600.0.39)] on darwin
+    Type "help", "copyright", "credits" or "license" for more information.
+    (TracestackConsole)
+    >>> 1 / 0
+
+## Options
+
+    usage: tracestack [-h] [-s] [-e ENGINE] [SCRIPT] [ARGUMENTS [ARGUMENTS ...]]
+    
+    instantly search your Python error messages on the web
+    
+    positional arguments:
+      SCRIPT                the Python script
+      ARGUMENTS             any arguments to the script
+    
+    optional arguments:
+      -h, --help            show this help message and exit
+      -s, --skip            skip the prompt and immediately search each exception
+      -e ENGINE, --engine ENGINE
+                            the search engine to use:
+                              'default': Google limited to stackoverflow.com, 
+                              'google': full web search on Google, 
+                              'stackoverflow': StackOverflow site search
+
+In addition to being used on the command line, any of these arguments can be passed to the `pm`, `enable`, and `trace` functions:
+    
+    >>> 1/0
+    >>> tracestack.pm(skip=True) # immediately runs search based on last exception
+    
+    >>> tracestack.enable(engine="google")
+    >>> 1/0
+    
+    >>> tracestack.trace(engine="stackoverflow")
+    >>> def divide_by_zero():
+    ...     1/0
+
+## Excellent Alternatives 
+
+* [lukasschwab/Stackit](https://github.com/lukasschwab/stackit)
+    * search and browse StackOverflow in the command line
+    * pipe error messages directly into queries
+* [gleitz/howdoi](https://github.com/gleitz/howdoi)
+    * get programming answers to plaintext questions in the command line
+    * works using inexplicable Jedi magic
+* [dgrtwo/tracestack](https://github.com/dgrtwo/tracestack)
+    * original tracestack package for the R language
+    * written on a phone
+
---
+++ packages/tracestack/tracestack-0.1.9.4.tar.gz/tracestack-0.1.9.4/tests/test_tracestack.py
@@ -0,0 +1,109 @@
+import unittest
+import sys
+import tracestack
+import webbrowser
+
+try:
+	from mock import Mock, patch
+except ImportError:
+	from unittest.mock import Mock, patch
+
+exc_return_value = (type(Exception("exception message")), Exception("exception message"), Mock())
+mock_sys_exc_info = Mock(return_value=exc_return_value)
+mock_webbrowser = Mock()
+mock_traceback_print_exception = Mock()
+mock_getch = Mock(return_value=" ")
+mock_sys = Mock()
+mock_sys.last_type = exc_return_value[0]
+mock_sys.last_value = exc_return_value[1]
+mock_sys.last_traceback = exc_return_value[2]
+
+
+@patch('tracestack.handler.sys.exc_info', mock_sys_exc_info)
+@patch('tracestack.handler.webbrowser', mock_webbrowser)
+@patch('tracestack.handler.traceback.print_exception', mock_traceback_print_exception)
+@patch('tracestack.handler.getch', mock_getch)
+class TestHandler(unittest.TestCase):
+
+	default_url = 		('http://www.google.com/search?q=Exception%3A' + 
+						 '+exception+message%0A+python+site%3Astackoverflow.com' +
+						 '+inurl%3Aquestions')
+	google_url = 		('http://www.google.com/search?q=Exception%3A' + 
+						 '+exception+message%0A+python')
+	stackoverflow_url = ('http://www.stackoverflow.com/search?' + 
+						 'q=Exception%3A+exception+message%0A+%5Bpython%5D')
+	default_prompt = 	   'Hit spacebar to search this error message on Stack Overflow (using Google): '
+	google_prompt =		   'Hit spacebar to search this error message on the web (using Google): '
+	stackoverflow_prompt = 'Hit spacebar to search this error message on Stack Overflow: '
+
+	def setUp(self):
+		pass
+
+	def tearDown(self):
+		mock_sys_exc_info.reset_mock()
+		mock_webbrowser.reset_mock()
+		mock_traceback_print_exception.reset_mock()
+		mock_getch.reset_mock()
+
+	def test_default(self):
+		handler = tracestack.handler.ExceptionHandler()
+		handler()
+		self.check_results()
+
+	def test_skip(self):
+		handler = tracestack.handler.ExceptionHandler(skip=True)
+		handler()
+		self.skip=True
+		self.check_results(skip=True)
+
+	def test_google(self):
+		handler = tracestack.handler.ExceptionHandler(engine="google")
+		handler()
+		self.check_results(prompt=self.google_prompt, url=self.google_url)
+
+	def test_stackoverflow(self):
+		handler = tracestack.handler.ExceptionHandler(engine="stackoverflow")
+		handler()
+		self.check_results(prompt=self.stackoverflow_prompt, url=self.stackoverflow_url)
+
+	def check_results(self, skip=False, prompt=None, 
+					  url=None):
+		prompt = prompt or self.default_prompt
+		url = url or self.default_url
+		mock_traceback_print_exception.assert_called_once_with(*exc_return_value)
+		if skip:
+			mock_getch.assert_not_called()
+		else:
+			mock_getch.assert_called_with(prompt)
+		mock_webbrowser.open.assert_called_once_with(url)
+
+	@patch('tracestack.debugger.sys', mock_sys)
+	def test_pm(self):
+		tracestack.pm()
+		self.check_results()
+
+	def test_nothing(self):
+		with self.assertRaises(AssertionError):
+			self.check_results()		
+
+
+class TestFunctions(unittest.TestCase):
+
+	def test_enable(self):
+		tracestack.enable()
+		self.assertIsNot(sys.excepthook, sys.__excepthook__)
+		self.assertTrue(isinstance(sys.excepthook, 
+								   tracestack.handler.ExceptionHandler))
+		tracestack.disable()
+		self.assertIs(sys.excepthook, sys.__excepthook__)
+
+	def test_decorator(self):
+		@tracestack.trace
+		def buggy_function():
+			self.assertIsNot(sys.excepthook, sys.__excepthook__)
+			self.assertTrue(isinstance(sys.excepthook, 
+									   tracestack.handler.ExceptionHandler))
+		buggy_function()
+		self.assertIs(sys.excepthook, sys.__excepthook__)
+
+
