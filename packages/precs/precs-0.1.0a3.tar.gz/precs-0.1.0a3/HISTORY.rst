cac97310ba5a9b4b8c1f5ce9e649543107ff8f40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:27 +0000

    Add package precs/0.1.0a3/precs-0.1.0a3.tar.gz

---
+++ packages/precs/precs-0.1.0a3.tar.gz/precs-0.1.0a3/HISTORY.rst
@@ -0,0 +1,27 @@
+.. :changelog:
+
+History
+-------
+
+0.1.0a3 (2015-09-28)
+--------------------
+
+- Final fix to get the pip installation working against py2.7
+
+
+0.1.0a2 (2015-09-28)
+--------------------
+
+- Fixing requirements packaging.
+
+
+0.1.0a1 (2015-09-28)
+--------------------
+
+- Fixed setup.py to correctly package requiments.
+
+
+0.1.0a0 (2015-09-28)
+--------------------
+
+* First release on PyPI.
---
+++ packages/precs/precs-0.1.0a3.tar.gz/precs-0.1.0a3/setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
+
+def requirements(*filenames):
+    requires = []
+
+    for filename in filenames:
+        with open(filename, 'r') as f:
+            for line in f:
+                line = line.strip()
+                if not line or line.startswith('#'):
+                    # Skip blank lines and comments
+                    continue
+                requires.append(line)
+
+    return requires
+
+setup(
+    name='precs',
+    version='0.1.0a3',
+    description="Python implementation of record stream processing utilities.",
+    long_description=readme + '\n\n' + history,
+    author="Muhammad Umer Azad",
+    author_email='umer.azad@gmail.com',
+    url='https://github.com/umerazad/precs',
+    packages=[
+        'precs',
+        'precs.processors'
+    ],
+    py_modules=['precs/precs'],
+    entry_points='''
+        [console_scripts]
+        precs=precs.precs:cli
+    ''',
+    package_dir={'precs':
+                 'precs'},
+    include_package_data=True,
+    install_requires=requirements('requirements.txt'),
+    license="MIT",
+    zip_safe=False,
+    keywords='precs',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7'
+    ],
+    test_suite='tests',
+    tests_require=requirements('requirements-test.txt')
+)
