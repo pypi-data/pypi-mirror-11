d3652539ad5e75e877f7b2ba3b09511f02fe5094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:55 +0000

    Add package pdfjinja/0.0.6/pdfjinja-0.0.6.tar.gz

---
+++ packages/pdfjinja/pdfjinja-0.0.6.tar.gz/pdfjinja-0.0.6/tests.py
@@ -0,0 +1,53 @@
+import json
+import os
+import cStringIO as StringIO
+import sys
+import unittest
+
+from contextlib import contextmanager
+from pdfjinja import Attachment, PdfJinja
+
+
+class PdfJinjaTestCase(unittest.TestCase):
+
+    datadir = os.path.join(os.path.dirname(__file__), "examples")
+
+    def setUp(self):
+        pdffile = os.path.join(self.datadir, "sample.pdf")
+        jsonfile = os.path.join(self.datadir, "sample.json")
+        Attachment.font = "examples/open-sans/regular.ttf"
+        self.data = json.loads(open(jsonfile).read())
+        self.attachments = [
+            Attachment(**kwargs) for kwargs in self.data.pop("attachments")
+        ]
+        self.pdfjinja = PdfJinja(pdffile)
+
+    def tearDown(self):
+        del self.data
+        del self.pdfjinja
+
+    def assertIsNone(self, value):
+        self.assertTrue(value is None, '%r is not None' % value)
+
+    def assertIsNotNone(self, value):
+        self.assertFalse(value is None)
+
+    def test_init(self):
+        output = self.pdfjinja(self.data, self.attachments)
+        outfile = StringIO.StringIO()
+        output.write(outfile)
+        outfile.seek(0)
+        self.assertTrue(len(outfile.read()) > 0, "Output PDF is not empty.")
+
+    @contextmanager
+    def assertRaisesCtx(self, exc_class):
+        try:
+            yield
+        except exc_class:
+            return
+        else:
+            raise AssertionError('Exception %s not raised.' % exc_class)
+
+
+if __name__ == '__main__':
+    unittest.main(argv=sys.argv)
