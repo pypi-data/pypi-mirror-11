3c6b6b9589e10703ff100d71d40484fdedb81fa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:30 +0000

    Add package onegov.search/0.0.4/onegov.search-0.0.4-py2-none-any.whl

---
+++ packages/onegov.search/onegov.search-0.0.4-py2-none-any.whl/onegov.search-0.0.4.dist-info/DESCRIPTION.rst
@@ -0,0 +1,91 @@
+
+Run the Tests
+-------------
+
+Install tox and run it::
+
+    pip install tox
+    tox
+
+Limit the tests to a specific python version::
+
+    tox -e py27
+
+Conventions
+-----------
+
+Onegov Search follows PEP8 as close as possible. To test for it run::
+
+    tox -e pep8
+
+Onegov Search uses `Semantic Versioning <http://semver.org/>`_
+
+Build Status
+------------
+
+.. image:: https://travis-ci.org/OneGov/onegov.search.png
+  :target: https://travis-ci.org/OneGov/onegov.search
+  :alt: Build Status
+
+Coverage
+--------
+
+.. image:: https://coveralls.io/repos/OneGov/onegov.search/badge.png?branch=master
+  :target: https://coveralls.io/r/OneGov/onegov.search?branch=master
+  :alt: Project Coverage
+
+Latests PyPI Release
+--------------------
+.. image:: https://pypip.in/v/onegov.search/badge.png
+  :target: https://crate.io/packages/onegov.search
+  :alt: Latest PyPI Release
+
+License
+-------
+onegov.search is released under GPLv2
+
+Changelog
+---------
+
+Unreleased
+~~~~~~~~~~
+
+0.0.4 (2015-09-22)
+~~~~~~~~~~~~~~~~~~~
+
+- Fixes localized mapping not working correctly in certain cases.
+  [href]
+
+- Stops the reindex command to create unwanted indices.
+  [href]
+
+- Exclude all _source fields by default.
+  [href]
+
+- Adds support for polymorphic SQLAlchemy models.
+  [href]
+
+- ORM Models now may use any name for their primary key attribute.
+  [href]
+
+0.0.3 (2015-09-18)
+~~~~~~~~~~~~~~~~~~~
+
+- No longer require elasticsearch to run when configuring the application.
+  [href]
+
+0.0.2 (2015-09-18)
+~~~~~~~~~~~~~~~~~~~
+
+- Adds the ability to reindex all elasticsearch records.
+  [href]
+
+- Fixes a number of issues with the onegov.town integration.
+  [href]
+
+0.0.1 (2015-09-17)
+~~~~~~~~~~~~~~~~~~~
+
+- Initial Release
+
+
---
+++ packages/onegov.search/onegov.search-0.0.4-py2-none-any.whl/onegov/search/integration.py
@@ -0,0 +1,130 @@
+import morepath
+
+from elasticsearch import Elasticsearch
+from more.transaction.main import transaction_tween_factory
+from onegov.search import Search
+from onegov.search.indexer import (
+    Indexer,
+    ORMEventTranslator,
+    TypeMappingRegistry
+)
+
+
+class ElasticsearchApp(morepath.App):
+    """ Provides elasticsearch integration for
+    :class:`onegov.core.framework.Framework` based applications.
+
+    The application must be connected to a database.
+
+    Usage::
+
+        from onegov.core import Framework
+
+        class MyApp(Framework, ESIntegration):
+            pass
+
+    """
+
+    def configure_search(self, **cfg):
+        """ Configures the elasticsearch client, leaving it as a property
+        on the class::
+
+            app.es_client
+
+        The following configuration options are accepted:
+
+        :elasticsearch_hosts:
+            A list of elasticsearch clusters, including username, password,
+            protocol and port.
+
+            For example: ``https://user:secret@localhost:443``
+
+            By default the client connects to the localhost on port 9200
+            (the default), and on port 19200 (the default of boxen).
+
+            At least one host in the list of servers must be up at startup.
+
+        """
+
+        hosts = cfg.get('elasticsearch_hosts', (
+            'http://localhost:9200',
+            'http://localhost:19200'
+        ))
+
+        max_queue_size = int(cfg.get('elasticsarch_max_queue_size', '10000'))
+
+        self.es_client = Elasticsearch(hosts)
+
+        if self.has_database_connection:
+            self.es_mappings = TypeMappingRegistry()
+
+            for base in self.session_manager.bases:
+                self.es_mappings.register_orm_base(base)
+
+            self.es_orm_events = ORMEventTranslator(
+                self.es_mappings,
+                max_queue_size=max_queue_size
+            )
+
+            self.es_indexer = Indexer(
+                self.es_mappings,
+                self.es_orm_events.queue,
+                es_client=self.es_client
+            )
+
+            self.session_manager.on_insert.connect(
+                self.es_orm_events.on_insert)
+            self.session_manager.on_update.connect(
+                self.es_orm_events.on_update)
+            self.session_manager.on_delete.connect(
+                self.es_orm_events.on_delete)
+
+    def es_search(self, languages='*', types='*', include_private=False):
+        """ Returns a search scoped to the current application, with the
+        given languages, types and private documents excluded by default.
+
+        """
+
+        search = Search(
+            session=self.session(),
+            mappings=self.es_mappings,
+            using=self.es_client,
+            index=self.es_indexer.ixmgr.get_external_index_names(
+                schema=self.schema,
+                languages=languages,
+                types=types
+            )
+        )
+
+        if not include_private:
+            search = search.filter("term", es_public=True)
+
+        # by default, do not include any fields (this will still include
+        # the id and the type, which is enough for the orm querying)
+        search = search.fields([])
+
+        return search
+
+    def es_search_by_request(self, request, types='*'):
+        """ Takes the current :class:`~onegov.core.request.CoreRequest` and
+        returns an elastic search scoped to the current application, the
+        requests language and it's access rights.
+
+        """
+
+        return self.es_search(
+            languages=[request.locale],
+            types=types,
+            include_private=request.is_logged_in
+        )
+
+
+@ElasticsearchApp.tween_factory(over=transaction_tween_factory)
+def process_indexer_tween_factory(app, handler):
+    def process_indexer_tween(request):
+
+        result = handler(request)
+        request.app.es_indexer.process()
+        return result
+
+    return process_indexer_tween
