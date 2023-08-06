d0d340f9de04d3a4d7be8417d9423b58a0b0ff0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:04 +0000

    Add package dewildcard/0.1/dewildcard-0.1.tar.gz

---
+++ packages/dewildcard/dewildcard-0.1.tar.gz/dewildcard-0.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name="dewildcard",
+      # packages = ['dewildcard'],
+      scripts = ['dewildcard'],
+      version = "0.1",
+      description = "Expand wildcard imports in Python code",
+      author = "Quentin Stafford-Fraser",
+      author_email = "quentin@pobox.com",
+      url = "http://github.com/quentinsf/dewildcard",
+      download_url = 'https://github.com/quentinsf/dewildcard/tarball/0.1',
+      keywords = 'pylint'
+    )
+
