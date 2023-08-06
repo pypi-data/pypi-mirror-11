9641a41808de58030b01699b28ef68342060bb35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:03 +0000

    Add package subprocess_manager/0.1/subprocess_manager-0.1.tar.gz

---
+++ packages/subprocess_manager/subprocess_manager-0.1.tar.gz/subprocess_manager-0.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='subprocess_manager',
+      version='0.1',
+      description='Python Subprocess Manager',
+      author='Abtin Gramian',
+      author_email='abtin.gramian@gmail.com',
+      url='https://github.com/agramian/subprocess-manager',
+      packages=['subprocess_manager'],
+      download_url = 'https://github.com/agramian/subprocess-manager/tarball/v0.1',
+      keywords = ['subprocess', 'daemon', 'timeout', 'process', 'spawning', 'non-blocking'],
+      classifiers = [],
+     )
