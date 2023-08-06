90230ce2708ea03422f8fadeb384011c1e89f95c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:58 +0000

    Add package django-generic-counter/0.0.1/django-generic-counter-0.0.1.tar.gz

---
+++ packages/django-generic-counter/django-generic-counter-0.0.1.tar.gz/django-generic-counter-0.0.1/django_generic_counter/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class Counter(models.Model):
+    """
+    A simple counter which can be used to store a count of anything.
+    """
+    name = models.CharField(max_length=128)
+    count = models.IntegerField(default=0)
+
+    def __iadd__(self, other):
+        """
+        Use the database to increment the counter atomically.
+        """
+        cls = type(self)
+        cls.objects.filter(pk=self.pk).update(
+            count=models.F("count")+other)
+        self.count = cls.objects.get(pk=self.pk).count
+        return self
+
+    def __isub__(self, other):
+        """
+        Use the database to decrement the counter atomically.
+        """
+        cls = type(self)
+        cls.objects.filter(pk=self.pk).update(
+            count=models.F("count")-other)
+        self.count = cls.objects.get(pk=self.pk).count
+        return self
+
+    def __int__(self):
+        """
+        Enable casting to int to display the counter value.
+        """
+        return self.count
