5d5ce5add5fa609ab68d693e14a820e12468f311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:00 +0000

    Add package secp256k1/0.5.1/secp256k1-0.5.1.tar.gz

---
+++ packages/secp256k1/secp256k1-0.5.1.tar.gz/secp256k1-0.5.1/secp256k1.py
@@ -0,0 +1,366 @@
+import os
+import hashlib
+
+from _libsecp256k1 import ffi, lib
+
+
+FLAG_SIGN = lib.SECP256K1_CONTEXT_SIGN
+FLAG_VERIFY = lib.SECP256K1_CONTEXT_VERIFY
+ALL_FLAGS = FLAG_SIGN | FLAG_VERIFY
+
+HAS_RECOVERABLE = hasattr(lib, 'secp256k1_ecdsa_sign_recoverable')
+
+
+class Base(object):
+
+    def __init__(self, ctx, flags):
+        self._destroy = None
+        if ctx is None:
+            assert flags in (0, FLAG_SIGN, FLAG_VERIFY, ALL_FLAGS)
+            ctx = lib.secp256k1_context_create(flags)
+            self._destroy = lib.secp256k1_context_destroy
+
+        self.flags = flags
+        self.ctx = ctx
+
+    def __del__(self):
+        if not hasattr(self, '_destroy'):
+            return
+
+        if self._destroy and self.ctx:
+            self._destroy(self.ctx)
+            self.ctx = None
+
+
+class ECDSA:  # Use as a mixin; instance.ctx is assumed to exist.
+
+    def ecdsa_serialize(self, raw_sig):
+        len_sig = 74
+        output = ffi.new('unsigned char[%d]' % len_sig)
+        outputlen = ffi.new('int *', len_sig)
+
+        res = lib.secp256k1_ecdsa_signature_serialize_der(
+            self.ctx, output, outputlen, raw_sig)
+        assert res == 1
+
+        return bytes(ffi.buffer(output, outputlen[0]))
+
+    def ecdsa_deserialize(self, ser_sig):
+        raw_sig = ffi.new('secp256k1_ecdsa_signature_t *')
+        res = lib.secp256k1_ecdsa_signature_parse_der(
+            self.ctx, raw_sig, ser_sig, len(ser_sig))
+        assert res == 1
+
+        return raw_sig
+
+    def ecdsa_recover(self, msg, recover_sig, raw=False, digest=hashlib.sha256):
+        if not HAS_RECOVERABLE:
+            raise Exception("secp256k1_recoverable not enabled")
+        if self.flags & ALL_FLAGS != ALL_FLAGS:
+            raise Exception("instance not configured for ecdsa recover")
+
+        msg32 = _hash32(msg, raw, digest)
+        pubkey = ffi.new('secp256k1_pubkey_t *')
+
+        recovered = lib.secp256k1_ecdsa_recover(
+            self.ctx, pubkey, recover_sig, msg32)
+        if recovered:
+            return pubkey
+        raise Exception('failed to recover ECDSA public key')
+
+    def ecdsa_recoverable_serialize(self, recover_sig):
+        if not HAS_RECOVERABLE:
+            raise Exception("secp256k1_recoverable not enabled")
+
+        outputlen = 64
+        output = ffi.new('unsigned char[%d]' % outputlen)
+        recid = ffi.new('int *')
+
+        lib.secp256k1_ecdsa_recoverable_signature_serialize_compact(
+            self.ctx, output, recid, recover_sig)
+
+        return bytes(ffi.buffer(output, outputlen)), recid[0]
+
+    def ecdsa_recoverable_deserialize(self, ser_sig, rec_id):
+        if not HAS_RECOVERABLE:
+            raise Exception("secp256k1_recoverable not enabled")
+
+        recover_sig = ffi.new('secp256k1_ecdsa_recoverable_signature_t *')
+
+        parsed = lib.secp256k1_ecdsa_recoverable_signature_parse_compact(
+            self.ctx, recover_sig, ser_sig, rec_id)
+        if parsed:
+            return recover_sig
+        else:
+            raise Exception('failed to parse ECDSA compact sig')
+
+    def ecdsa_recoverable_convert(self, recover_sig):
+        if not HAS_RECOVERABLE:
+            raise Exception("secp256k1_recoverable not enabled")
+
+        normal_sig = ffi.new('secp256k1_ecdsa_signature_t *')
+
+        lib.secp256k1_ecdsa_recoverable_signature_convert(
+            self.ctx, normal_sig, recover_sig)
+
+        return normal_sig
+
+
+class PublicKey(Base, ECDSA):
+
+    def __init__(self, pubkey=None, raw=False, flags=FLAG_VERIFY, ctx=None):
+        Base.__init__(self, ctx, flags)
+        if pubkey is not None:
+            if raw:
+                if not isinstance(pubkey, bytes):
+                    raise TypeError('raw pubkey must be bytes')
+                self.public_key = self.deserialize(pubkey)
+            else:
+                self.public_key = pubkey
+        else:
+            self.public_key = None
+
+    def serialize(self, compressed=True):
+        assert self.public_key, "No public key defined"
+
+        len_compressed = 33 if compressed else 65
+        res_compressed = ffi.new('char [%d]' % len_compressed)
+        outlen = ffi.new('int *', len_compressed)
+
+        serialized = lib.secp256k1_ec_pubkey_serialize(
+            self.ctx, res_compressed, outlen, self.public_key, int(compressed))
+        assert serialized == 1
+
+        return bytes(ffi.buffer(res_compressed, len_compressed))
+
+    def deserialize(self, pubkey_ser):
+        if len(pubkey_ser) not in (33, 65):
+            raise Exception("unknown public key size (expected 33 or 65)")
+
+        pubkey = ffi.new('secp256k1_pubkey_t *')
+
+        res = lib.secp256k1_ec_pubkey_parse(
+            self.ctx, pubkey, pubkey_ser, len(pubkey_ser))
+        if not res:
+            raise Exception("invalid public key")
+
+        self.public_key = pubkey
+        return pubkey
+
+    def ecdsa_verify(self, msg, raw_sig, raw=False, digest=hashlib.sha256):
+        assert self.public_key, "No public key defined"
+        if self.flags & FLAG_VERIFY != FLAG_VERIFY:
+            raise Exception("instance not configured for sig verification")
+
+        msg32 = _hash32(msg, raw, digest)
+
+        verified = lib.secp256k1_ecdsa_verify(
+            self.ctx, raw_sig, msg32, self.public_key)
+
+        return bool(verified)
+
+
+class PrivateKey(Base, ECDSA):
+
+    def __init__(self, privkey=None, raw=True, flags=ALL_FLAGS, ctx=None):
+        assert flags in (ALL_FLAGS, FLAG_SIGN)
+
+        Base.__init__(self, ctx, flags)
+        self.public_key = None
+        self.private_key = None
+        if privkey is None:
+            self.set_raw_privkey(_gen_private_key())
+        else:
+            if raw:
+                if len(privkey) != 32 or not isinstance(privkey, bytes):
+                    raise TypeError('privkey must be composed of 32 bytes')
+                self.set_raw_privkey(privkey)
+            else:
+                self.deserialize(privkey)
+
+    def _update_public_key(self):
+        public_key = self._gen_public_key(self.private_key)
+        self.public_key = PublicKey(
+            public_key, raw=False, ctx=self.ctx, flags=self.flags)
+
+    def set_raw_privkey(self, privkey):
+        if not lib.secp256k1_ec_seckey_verify(self.ctx, privkey):
+            raise Exception("invalid private key")
+        self.private_key = privkey
+        self._update_public_key()
+
+    def serialize(self, compressed=True):
+        privser = ffi.new('char [279]')
+        keylen = ffi.new('int *')
+
+        res = lib.secp256k1_ec_privkey_export(
+            self.ctx, privser, keylen, self.private_key, int(compressed))
+        assert res == 1
+
+        return bytes(ffi.buffer(privser, keylen[0]))
+
+    def deserialize(self, privkey_ser):
+        privkey = ffi.new('char [32]')
+
+        res = lib.secp256k1_ec_privkey_import(
+            self.ctx, privkey, privkey_ser, len(privkey_ser))
+        if not res:
+            raise Exception("invalid private key")
+
+        rawkey = bytes(ffi.buffer(privkey, 32))
+        self.set_raw_privkey(rawkey)
+        return self.private_key
+
+    def _gen_public_key(self, privkey):
+        pubkey_ptr = ffi.new('secp256k1_pubkey_t *')
+
+        created = lib.secp256k1_ec_pubkey_create(self.ctx, pubkey_ptr, privkey)
+        assert created == 1
+
+        return pubkey_ptr
+
+    def ecdsa_sign(self, msg, raw=False, digest=hashlib.sha256):
+        msg32 = _hash32(msg, raw, digest)
+        raw_sig = ffi.new('secp256k1_ecdsa_signature_t *')
+
+        signed = lib.secp256k1_ecdsa_sign(
+            self.ctx, raw_sig, msg32, self.private_key, ffi.NULL, ffi.NULL)
+        assert signed == 1
+
+        return raw_sig
+
+    def ecdsa_sign_recoverable(self, msg, raw=False, digest=hashlib.sha256):
+        if not HAS_RECOVERABLE:
+            raise Exception("secp256k1_recoverable not enabled")
+
+        msg32 = _hash32(msg, raw, digest)
+        raw_sig = ffi.new('secp256k1_ecdsa_recoverable_signature_t *')
+
+        signed = lib.secp256k1_ecdsa_sign_recoverable(
+            self.ctx, raw_sig, msg32, self.private_key, ffi.NULL, ffi.NULL)
+        assert signed == 1
+
+        return raw_sig
+
+
+def _hash32(msg, raw, digest):
+    if not raw:
+        msg32 = digest(msg).digest()
+    else:
+        msg32 = msg
+    if len(msg32) * 8 != 256:
+        raise Exception("digest function must produce 256 bits")
+    return msg32
+
+
+def _gen_private_key():
+    key = os.urandom(32)
+    return key
+
+
+def _main_cli(args, out, encoding='utf-8'):
+    import binascii
+
+    def show_public(public_key):
+        rawp = public_key.serialize()
+        out.write(u"Public key: {}\n".format(
+            binascii.hexlify(rawp).decode(encoding)))
+
+    def sign(funcname, params):
+        raw = bytes(bytearray.fromhex(params.private_key))
+        priv = PrivateKey(raw)
+        func = getattr(priv, funcname)
+        sig = func(params.message)
+        return priv, sig
+
+    if args.action == 'privkey':
+        if args.private_key:
+            rawkey = bytes(bytearray.fromhex(args.private_key))
+        else:
+            rawkey = None
+        priv = PrivateKey(rawkey)
+        raw = priv.private_key
+        out.write(u"{}\n".format(binascii.hexlify(raw).decode(encoding)))
+        if args.show_pubkey:
+            show_public(priv.public_key)
+
+    elif args.action == 'sign':
+        priv, sig_raw = sign('ecdsa_sign', args)
+        sig = priv.ecdsa_serialize(sig_raw)
+        out.write(u"{}\n".format(binascii.hexlify(sig).decode(encoding)))
+        if args.show_pubkey:
+            show_public(priv.public_key)
+
+    elif args.action == 'checksig':
+        raw = bytes(bytearray.fromhex(args.public_key))
+        sig = bytes(bytearray.fromhex(args.signature))
+        pub = PublicKey(raw, raw=True)
+        try:
+            sig_raw = pub.ecdsa_deserialize(sig)
+            good = pub.ecdsa_verify(args.message, sig_raw)
+        except:
+            good = False
+        out.write(u"{}\n".format(good))
+        return 0 if good else 1
+
+    elif args.action == 'signrec':
+        priv, sig = sign('ecdsa_sign_recoverable', args)
+        sig, recid = priv.ecdsa_recoverable_serialize(sig)
+        out.write(u"{} {}\n".format(binascii.hexlify(sig).decode(encoding), recid))
+        if args.show_pubkey:
+            show_public(priv.public_key)
+
+    elif args.action == 'recpub':
+        empty = PublicKey(flags=ALL_FLAGS)
+        sig_raw = bytes(bytearray.fromhex(args.signature))
+        sig = empty.ecdsa_recoverable_deserialize(sig_raw, args.recid)
+        pubkey = empty.ecdsa_recover(args.message, sig)
+        show_public(PublicKey(pubkey))
+
+    return 0
+
+
+def _parse_cli():
+    import sys
+    from argparse import ArgumentParser
+
+    py2 = sys.version_info.major == 2
+    enc = sys.getfilesystemencoding()
+    def bytes_input(s):
+        return s if py2 else s.encode(enc)
+
+    parser = ArgumentParser()
+    subparser = parser.add_subparsers(dest='action')
+
+    genparser = subparser.add_parser('privkey')
+    genparser.add_argument('-p', '--show-pubkey', action='store_true')
+    genparser.add_argument('-k', '--private_key')
+
+    sign = subparser.add_parser('sign')
+    sign.add_argument('-k', '--private-key', required=True)
+    sign.add_argument('-m', '--message', required=True, type=bytes_input)
+    sign.add_argument('-p', '--show-pubkey', action='store_true')
+
+    signrec = subparser.add_parser('signrec')
+    signrec.add_argument('-k', '--private-key', required=True)
+    signrec.add_argument('-m', '--message', required=True, type=bytes_input)
+    signrec.add_argument('-p', '--show-pubkey', action='store_true')
+
+    check = subparser.add_parser('checksig')
+    check.add_argument('-p', '--public-key', required=True)
+    check.add_argument('-m', '--message', required=True, type=bytes_input)
+    check.add_argument('-s', '--signature', required=True)
+
+    recpub = subparser.add_parser('recpub')
+    recpub.add_argument('-m', '--message', required=True, type=bytes_input)
+    recpub.add_argument('-i', '--recid', required=True, type=int)
+    recpub.add_argument('-s', '--signature', required=True)
+
+    return parser, enc
+
+
+if __name__ == '__main__':
+    import sys
+    parser, enc = _parse_cli()
+    args = parser.parse_args(sys.argv[1:])
+    sys.exit(_main_cli(args, sys.stdout, enc))
