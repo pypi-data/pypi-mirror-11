835f0a8aa25ba7606266ab1952fe26f8706cdcdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:31 +0000

    Add package m79commons/1.2.1/m79commons-1.2.1.tar.gz

---
+++ packages/m79commons/m79commons-1.2.1.tar.gz/m79commons-1.2.1/setup.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='m79commons',
+    packages=['m79commons'],
+    version='1.2.1',
+    description="minus79's commons lib",
+    author='Minus Seventynine',
+    author_email='gergely06@gmail.com',
+    url='https://github.com/youraccount/foolib',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: GIS'
+    ]
+)
