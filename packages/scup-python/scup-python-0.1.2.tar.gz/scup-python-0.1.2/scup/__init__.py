1a737ea2295363b051ffaca6e8e57a2e3fd1d0f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:18 +0000

    Add package scup-python/0.1.2/scup-python-0.1.2.tar.gz

---
+++ packages/scup-python/scup-python-0.1.2.tar.gz/scup-python-0.1.2/scup/__init__.py
@@ -0,0 +1,14 @@
+from scup.exceptions import (
+    ScupPythonError,
+    ScupError,
+    ScupClientError,
+    HTTPError
+)
+from scup.scup_api import ScupAPI
+
+__all__ = [
+    ScupPythonError,
+    ScupError,
+    HTTPError,
+    ScupAPI
+]
