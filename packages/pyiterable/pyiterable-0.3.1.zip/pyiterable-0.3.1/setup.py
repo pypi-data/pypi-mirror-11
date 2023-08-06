eda727be8aef4d7d287b23634fab851307a27736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:44 +0000

    Add package pyiterable/0.3.1/pyiterable-0.3.1.zip

---
+++ packages/pyiterable/pyiterable-0.3.1.zip/pyiterable-0.3.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='pyiterable',
+    version='0.3.1',
+    description='Iterable chains',
+    long_description='Write more expressive code by chaining built-in transformations together.',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    url='https://github.com/neverendingqs/pyiterable',
+    author='neverendingqs',
+    author_email='mark.tse@neverendingqs.com',
+    license='MIT',
+    packages=['pyiterable'],
+    test_suite='nose.collector',
+    tests_require=['nose', 'unittest2'],
+    zip_safe=False
+)
