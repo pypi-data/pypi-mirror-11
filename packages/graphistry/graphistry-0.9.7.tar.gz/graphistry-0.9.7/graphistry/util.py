61d8dc55d18afc7100dfe9d929989859bce4ba31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:42 +0000

    Add package graphistry/0.9.7/graphistry-0.9.7.tar.gz

---
+++ packages/graphistry/graphistry-0.9.7.tar.gz/graphistry-0.9.7/graphistry/util.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+from builtins import str
+
+import sys
+import platform as p
+import uuid
+import hashlib
+
+
+def fingerprint():
+    md5 = hashlib.md5()
+    # Hostname, OS, CPU, MAC,
+    data = [p.node(), p.system(), p.machine(), str(uuid.getnode())]
+    md5.update(''.join(data).encode('utf8'))
+    return "%s-pygraphistry-%s" % (md5.hexdigest()[:8], sys.modules['graphistry'].__version__)
+
+def in_ipython():
+        try:
+            __IPYTHON__
+            return True
+        except NameError:
+            return False
+
+def warn(msg):
+    if in_ipython:
+        import IPython
+        IPython.utils.warn.warn(msg)
+    else:
+        print('WARNING: ', msg, file=sys.stderr)
+
+def error(msg):
+    if in_ipython:
+        import IPython
+        IPython.utils.warn.error(msg)
+    raise ValueError(msg)
