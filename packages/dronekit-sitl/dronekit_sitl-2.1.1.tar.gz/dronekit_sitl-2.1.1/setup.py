8d7633dc62da65372d5fd0354b816b79140caceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:52 +0000

    Add package dronekit-sitl/2.1.1/dronekit_sitl-2.1.1.tar.gz

---
+++ packages/dronekit-sitl/dronekit_sitl-2.1.1.tar.gz/dronekit_sitl-2.1.1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='dronekit_sitl',
+      version='2.1.1',
+      description='Runs SITL as part of DroneKit.',
+      author='Tim Ryan',
+      author_email='tim@3drobotics.com',
+      url='https://github.com/tcr3dr/dronekit-sitl-runner/',
+      install_requires = ['psutil>=3.0'],
+      entry_points={
+          'console_scripts': [
+              'dronekit-sitl = dronekit.sitl.__main__'
+          ]
+      },
+      packages = ['dronekit', 'dronekit.sitl'],
+      namespace_packages = ['dronekit']
+      )
