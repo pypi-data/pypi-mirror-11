95b5e7e7eebf18e53b3fabb7822a5e077ac5a285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:03 +0000

    Add package debatecrunch/0.1/debatecrunch-0.1.tar.gz

---
+++ packages/debatecrunch/debatecrunch-0.1.tar.gz/debatecrunch-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='debatecrunch',
+      version='0.1',
+      description='Tools for analyzing debate transcripts',
+      url='',
+      author='Dave Goodsmith',
+      author_email='',
+      license='MIT',
+      packages=['debatecrunch'],
+      zip_safe=False)
