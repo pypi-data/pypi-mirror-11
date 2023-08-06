8e4aa6184966f55ff58021d92457641c9435b8b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:23 +0000

    Add package xetracker/0.1.3/xetracker-0.1.3.tar.gz

---
+++ packages/xetracker/xetracker-0.1.3.tar.gz/xetracker-0.1.3/setup.py
@@ -0,0 +1,35 @@
+import os
+
+try:
+  from setuptools import setup, Command
+except:
+  from distutils.core import setup, Command
+
+class CleanCommand(Command):
+  """Custom clean command to tidy up the project root."""
+  user_options = []
+  def initialize_options(self):
+    pass
+  def finalize_options(self):
+    pass
+  def run(self):
+    os.system("rm -vrf ./build ./dist ./*.pyc ./*.tar.gz ./*.egg-info")
+
+config = {
+  "name": "xetracker",
+  "description": "Track the rates of the foreign currency traders",
+  "author": "tasyrkin",
+  "url": "https://github.com/tasyrkin/xetracker",
+  "author_email": "tasyrkin@gmail.com",
+  "version": "0.1.3",
+  "install_requires": ["lxml", "psycopg2"],
+  "packages": ["xetracker.model", "xetracker.persistance", "xetracker.fetchers"],
+  "scripts": ["bin/xetracker"],
+  "cmdclass": {
+    "clean": CleanCommand
+  },
+  "test_suite": 'nose.collector',
+  "tests_require": ['nose']
+}
+
+setup(**config)
