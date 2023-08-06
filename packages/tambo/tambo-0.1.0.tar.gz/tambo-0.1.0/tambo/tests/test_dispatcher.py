e52bf1186eb9cb376484a0c4fd6da814edff8010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:06 +0000

    Add package tambo/0.1.0/tambo-0.1.0.tar.gz

---
+++ packages/tambo/tambo-0.1.0.tar.gz/tambo-0.1.0/tambo/tests/test_dispatcher.py
@@ -0,0 +1,39 @@
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+from tambo import dispatcher
+
+
+class MySubCommand(object):
+
+    def __init__(self, argv):
+        self.argv = argv
+
+    def parse_args(self):
+        return self.argv
+
+
+class Test_dispatching_mapped_classes(object):
+
+    def test_does_nothing_if_the_mapper_is_empty(self):
+        transport = dispatcher.Transport([])
+        assert transport.dispatch() is None
+
+    def test_does_nothing_if_the_mapper_cannot_match_the_subcommand(self):
+        transport = dispatcher.Transport(['bin/foo', 'bar', 'boo'])
+        assert transport.dispatch() is None
+
+    def test_returns_parse_args_called_with_the_instance(self):
+        transport = dispatcher.Transport(['/usr/bin/foo', 'foo'])
+        transport.mapper = {'foo': MySubCommand}
+        result = transport.dispatch()
+        assert result == ['foo']
+
+    def test_complains_about_unknown_commands(self):
+        fake_out = StringIO()
+        transport = dispatcher.Transport(['bin/foo', 'bar', 'boo'],
+                                         writer=fake_out)
+        transport.dispatch()
+        assert fake_out.getvalue() == 'Unknown command(s): bar boo\n'
