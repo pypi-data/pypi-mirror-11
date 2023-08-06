26f6102317da8188b8e006125a6e91f49c972f1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:15 +0000

    Add package clif/0.2.2/clif-0.2.2.tar.gz

---
+++ packages/clif/clif-0.2.2.tar.gz/clif-0.2.2/clif/logger.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import logging
+from datetime import datetime
+from . import CliError
+
+logging.VERBOSE = 5
+logging.addLevelName(logging.VERBOSE, 'VERBOSE')
+logging.Logger.verbose = (lambda inst, msg, *args, **kwargs:
+                          inst.log(logging.VERBOSE, msg, *args, **kwargs))
+
+FORMATTER = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+
+
+def init(name, logdir, loglevel):
+    if not os.path.exists(logdir):
+        try:
+            os.makedirs(logdir)
+        except OSError as err:
+            raise CliError('unable to create log directory: %s' % err)
+
+    logger = logging.getLogger(name)
+    logger.setLevel('VERBOSE') # log level is overloader by each handlers.
+    logfile = os.path.abspath(os.path.join(logdir, name))
+
+    # Add file handler.
+    filename =  '%s.log' % datetime.now().strftime('%Y%m%d%H%M')
+    file_handler = logging.FileHandler(os.path.join(logdir, filename))
+    file_handler.setFormatter(FORMATTER)
+    file_handler.setLevel('INFO')
+    logger.addHandler(file_handler)
+
+    # Add cli handler.
+    if loglevel != 'none':
+        cli_handler = logging.StreamHandler()
+        cli_handler.setFormatter(FORMATTER)
+        cli_handler.setLevel(loglevel.upper())
+        logger.addHandler(cli_handler)
+
+    setattr(sys.modules[__name__], 'logger', logger)
+
+
+def log(msg, loglevel, **kwargs):
+    quit = kwargs.get('quit', False)
+    return_code = kwargs.get('return_code', 0)
+    confidential = kwargs.get('confidential', False)
+    if confidential:
+        logger.handlers[0].setLevel(100)
+        getattr(logger, loglevel)(msg)
+        logger.handlers[0].setLevel('INFO')
+    else:
+        getattr(logger, loglevel)(msg)
+
+    if quit:
+        sys.exit(return_code)
+
+
+def verbose(msg, **kwargs):
+    log(msg,
+        'verbose',
+        quit=kwargs.get('quit', False),
+        return_code=kwargs.get('return_code', 0),
+        confidential=kwargs.get('confidential', False))
+
+
+def debug(msg, **kwargs):
+    log(msg,
+        'debug',
+        quit=kwargs.get('quit', False),
+        return_code=kwargs.get('return_code', 0),
+        confidential=kwargs.get('confidential', False))
+
+
+def info(msg, **kwargs):
+    log(msg,
+        'info',
+        quit=kwargs.get('quit', False),
+        return_code=kwargs.get('return_code', 0),
+        confidential=kwargs.get('confidential', False))
+
+
+def warn(msg, **kwargs):
+    log(msg,
+        'warn',
+        quit=kwargs.get('quit', False),
+        return_code=kwargs.get('return_code', 0),
+        confidential=kwargs.get('confidential', False))
+
+
+def error(msg, **kwargs):
+    log(msg,
+        'error',
+        quit=kwargs.get('quit', False),
+        return_code=kwargs.get('return_code', 1),
+        confidential=kwargs.get('confidential', False))
---
+++ packages/clif/clif-0.2.2.tar.gz/clif-0.2.2/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='clif',
+    version='0.2.2',
+    author='François Ménabé',
+    author_email='francois.menabe@gmail.com',
+    url = 'http://github.com/fmenabe/python-clif',
+    download_url = 'http://github.com/fmenabe/python-clif',
+    license='MIT License',
+    description='Framework for generating command-line',
+    long_description=open('README.rst').read(),
+    keywords=['command-line', 'argparse', 'wrapper', 'clg', 'framework'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities'
+    ],
+    packages=['clif'],
+    install_requires=['clg', 'pyyaml', 'yamlordereddictloader'])
