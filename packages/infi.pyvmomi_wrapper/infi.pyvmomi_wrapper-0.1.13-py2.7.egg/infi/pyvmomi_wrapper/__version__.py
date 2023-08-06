cafa44b79244c6d7cfd1c402db281fb8434b7b8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:19 +0000

    Add package infi.pyvmomi_wrapper/0.1.13/infi.pyvmomi_wrapper-0.1.13-py2.7.egg

---
+++ packages/infi.pyvmomi_wrapper/infi.pyvmomi_wrapper-0.1.13-py2.7.egg/infi/pyvmomi_wrapper/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.13"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(HEAD detached at v0.1.13)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '350f73a870a73a3518c9cd710dbd66fd88e631d5'
+__git_head_subject__ = 'Finished release v0.1.13.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
