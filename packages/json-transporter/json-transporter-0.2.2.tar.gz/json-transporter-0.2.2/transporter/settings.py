728ae372be9daa8e0abb1c71593b463f6b913b1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:51 +0000

    Add package json-transporter/0.2.2/json-transporter-0.2.2.tar.gz

---
+++ packages/json-transporter/json-transporter-0.2.2.tar.gz/json-transporter-0.2.2/transporter/settings.py
@@ -0,0 +1,23 @@
+ES_SETTINGS = {
+    'host': 'localhost:9200',
+    'ssl': False
+}
+
+KAFKA_SETTINGS = {
+    'broker': 'localhost:9092'
+}
+
+S3_SETTINGS = {
+    'access_key': 'use_localsettings',
+    'secret_key': 'use_localsettings'
+}
+
+MONGO_SETTINGS = {
+    'host': 'localhost',
+    'db': 'local'
+}
+
+HBASE_SETTINGS = {
+    'host': 'localhost'
+}
+
