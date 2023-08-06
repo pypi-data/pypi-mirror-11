d25d2682d79dcef7291a436e428cd703caf30ceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:41 +0000

    Add package performanceplatform-collector/0.2.1/performanceplatform-collector-0.2.1.tar.gz

---
+++ packages/performanceplatform-collector/performanceplatform-collector-0.2.1.tar.gz/performanceplatform-collector-0.2.1/performanceplatform/collector/ga/__init__.py
@@ -0,0 +1,18 @@
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
+from performanceplatform.collector.ga.core \
+    import create_client, query_documents_for
+
+from performanceplatform.utils.data_pusher import Pusher
+
+
+def main(credentials, data_set_config, query, options, start_at, end_at):
+    client = create_client(credentials)
+
+    documents = query_documents_for(
+        client, query, options,
+        data_set_config['data-type'],
+        start_at, end_at)
+
+    Pusher(data_set_config, options).push(documents)
