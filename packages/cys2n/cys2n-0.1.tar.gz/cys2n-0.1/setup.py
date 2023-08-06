62245e4b58b4da0fdfdc937858af08d2140e2a95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:38 +0000

    Add package cys2n/0.1/cys2n-0.1.tar.gz

---
+++ packages/cys2n/cys2n-0.1.tar.gz/cys2n-0.1/setup.py
@@ -0,0 +1,23 @@
+# -*- Mode: Python -*-
+
+from setuptools import setup, find_packages
+from distutils.extension import Extension
+
+from Cython.Build import cythonize
+
+ext_modules = cythonize ([ Extension ('cys2n.cys2n', ['cys2n/cys2n.pyx'], libraries = ['s2n'])])
+
+setup (
+    name='cys2n',
+    version='0.1',
+    description='Cython wrapper for Amazon s2n',
+    author='Sam Rushing',
+    author_email='gitsam@rushing.nightmare.com',
+    license="BSD",
+    url="http://github.com/samrushing/cys2n",
+    packages = find_packages(),
+    package_data = {'cys2n': ['*.pxd']},
+    ext_modules= ext_modules,
+    download_url = 'https://pypi.python.org/pypi?name=cys2n',
+)
+
