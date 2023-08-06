e0fe4b83c962cc5eb85b08ed3d5cbb69aa50757c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:05 +0000

    Add package metaknowledge/0.3.2/metaknowledge-0.3.2.tar.gz

---
+++ packages/metaknowledge/metaknowledge-0.3.2.tar.gz/metaknowledge-0.3.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+setup(name='metaknowledge',
+    version='0.3.2',
+    description = "A library for handling Web of science files",
+    author="Reid McIlroy-Young, John McLevey",
+    author_email = "rmcilroy@uwaterloo.ca",
+    url="https://github.com/networks-lab/metaknowledge",
+    download_url = "https://github.com/networks-lab/isilib/archive/0.3.2.tar.gz",
+    keywords= 'isi wos testing',
+    classifiers = [],
+    install_requires= ['networkx'],
+    setup_requires = ['networkx'],
+    #extras_require={'visualizer' : ['matplotlib']},
+    packages=find_packages(),#['metaknowledge', 'metaknowledge.journalAbbreviations'],
+    scripts=['metaknowledge/bin/metaknowledge-CLI'],
+    test_suite='metaknowledge.tests',
+    include_package_data = True,
+    package_data = {'': ['manualj9Abbreviations.*']}
+    #data_files= [('/tests', ["testFile.isi", "OnePaper.isi", "ManyAuthors.isi"])]
+)
