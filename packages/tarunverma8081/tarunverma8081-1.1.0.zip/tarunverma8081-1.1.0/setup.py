019da3cfe4730e04b9731273ca5f17366de75035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:16 +0000

    Add package tarunverma8081/1.1.0/tarunverma8081-1.1.0.zip

---
+++ packages/tarunverma8081/tarunverma8081-1.1.0.zip/tarunverma8081-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(         
+	name         = 'tarunverma8081',         
+	version      = '1.1.0',         
+	py_modules   = ['tarunverma8081'],         
+	author       = 'tarunverma8081',
+	author_email = 'tarunverma8081@gmail.com',        
+	url          = 'http://www.apara.com',        
+	description  = 'A simple print function',
+	)
