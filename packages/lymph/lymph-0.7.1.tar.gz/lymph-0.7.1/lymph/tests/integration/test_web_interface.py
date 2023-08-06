3fb05987a6dd43f9929affd5620e5c6abbf2bc35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:04 +0000

    Add package lymph/0.7.1/lymph-0.7.1.tar.gz

---
+++ packages/lymph/lymph-0.7.1.tar.gz/lymph-0.7.1/lymph/tests/integration/test_web_interface.py
@@ -0,0 +1,60 @@
+from werkzeug.routing import Map, Rule
+from werkzeug.wrappers import Response
+
+from lymph.testing import WebServiceTestCase
+from lymph.web.interfaces import WebServiceInterface
+from lymph.web.handlers import RequestHandler
+from lymph.web.routing import HandledRule
+
+
+class RuleHandler(RequestHandler):
+    def get(self):
+        return Response("Rule Handler")
+
+
+class HandledRuleHandler(RequestHandler):
+    def get(self):
+        return Response("Handled Rule Handler")
+
+
+class Web(WebServiceInterface):
+    url_map = Map([
+        Rule("/test/", endpoint="test"),
+        Rule("/foo/", endpoint=RuleHandler),
+        HandledRule("/bar/", endpoint="bar", handler=HandledRuleHandler),
+        Rule("/fail/", endpoint="fail"),
+        Rule("/fail-wrong-endpoint/", endpoint=42),
+    ])
+
+    def test(self, request):
+        return Response("method test")
+
+
+class WebIntegrationTest(WebServiceTestCase):
+
+    service_class = Web
+
+    def test_dispatch_rule_with_string_endpoint(self):
+        response = self.client.get("/test/")
+        self.assertEqual(response.data.decode("utf8"), "method test")
+        self.assertEqual(response.status_code, 200)
+
+    def test_dispatch_rule_with_callable_endpoint(self):
+        response = self.client.get("/foo/")
+        self.assertEqual(response.data.decode("utf8"), "Rule Handler")
+        self.assertEqual(response.status_code, 200)
+
+    def test_dispatch_handled_rule(self):
+        response = self.client.get("/bar/")
+        self.assertEqual(response.data.decode("utf8"), "Handled Rule Handler")
+        self.assertEqual(response.status_code, 200)
+
+    def test_dispatch_failing_rule_to_500(self):
+        response = self.client.get("/fail/")
+        self.assertEqual(response.data.decode("utf8"), "")
+        self.assertEqual(response.status_code, 500)
+
+    def test_dispatch_failing_endpoint_to_500(self):
+        response = self.client.get("/fail-wrong-endpoint/")
+        self.assertEqual(response.data.decode("utf8"), "")
+        self.assertEqual(response.status_code, 500)
---
+++ packages/lymph/lymph-0.7.1.tar.gz/lymph-0.7.1/lymph/web/handlers.py
@@ -0,0 +1,36 @@
+import json
+
+from werkzeug.exceptions import MethodNotAllowed
+
+
+http_methods = ('get', 'post', 'head', 'options', 'put', 'patch', 'delete')
+
+
+class RequestHandler(object):
+
+    def __init__(self, interface, request):
+        self.request = request
+        self.interface = interface
+        self._json = None
+
+    @property
+    def allowed_methods(self):
+        return [method.upper() for method in http_methods if callable(getattr(self, method, None))]
+
+    def json(self):
+        if not "application/json" == self.request.mimetype:
+            raise ValueError("The request Content-Type is not JSON")
+
+        if self._json is None:
+            self._json = json.loads(self.request.get_data(as_text=True))
+        return self._json
+
+    def dispatch(self, args):
+        method = self.request.method.lower()
+        if method not in http_methods:
+            raise MethodNotAllowed(self.allowed_methods)
+        try:
+            func = getattr(self, method)
+        except AttributeError:
+            raise MethodNotAllowed(self.allowed_methods)
+        return func(**args)
