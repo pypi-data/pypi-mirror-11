ab549c21fd9efbe6ccc7ee938ac27bd6540b4cf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:50 +0000

    Add package basicrpc/0.0.1/basicrpc-0.0.1.tar.gz

---
+++ packages/basicrpc/basicrpc-0.0.1.tar.gz/basicrpc-0.0.1/basicrpc/config.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+    basicrpc
+    ~~~~~
+    authors: Jude Nelson and Muneeb Ali
+    license: MIT, see LICENSE for more details.
+"""
+
+DEBUG = True
+
+MAX_RPC_LEN = 1024 * 1024 * 1024
+
+DEFAULT_SEVER = '0.0.0.0'
+DEFAULT_PORT = 5000
