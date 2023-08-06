4beec2618e2ddac344529a27864bf3846c10c1ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:55 +0000

    Add package msmbuilder/3.3.0/msmbuilder-3.3.0.zip

---
+++ packages/msmbuilder/msmbuilder-3.3.0.zip/msmbuilder-3.3.0/msmbuilder/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM MDTRAJ SETUP.PY
+short_version = '3.3.0'
+version = '3.3.0'
+full_version = '3.3.0'
+git_revision = '63cfd96e2f6008d21a24848a252f4a0bdb3444d3'
+release = True
+
+if not release:
+    version = full_version
