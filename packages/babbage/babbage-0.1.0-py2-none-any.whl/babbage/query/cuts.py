47274e4f44f412fe5f0c180b761d20fe66eb3862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:47 +0000

    Add package babbage/0.1.0/babbage-0.1.0-py2-none-any.whl

---
+++ packages/babbage/babbage-0.1.0-py2-none-any.whl/babbage/query/cuts.py
@@ -0,0 +1,31 @@
+import six
+from sqlalchemy import type_coerce
+
+from babbage.query.parser import Parser
+from babbage.exc import QueryException
+
+
+class Cuts(Parser):
+    """ Handle parser output for cuts. """
+    start = "cuts"
+
+    def cut(self, ast):
+        value = ast[2]
+        if isinstance(value, six.string_types) and len(value.strip()) == 0:
+            value = None
+        # TODO: can you filter measures or aggregates?
+        if ast[0] not in self.cube.model:
+            raise QueryException('Invalid cut: %r' % ast[0])
+        self.results.append((ast[0], ast[1], value))
+
+    def apply(self, q, cuts):
+        """ Apply a set of filters, which can be given as a set of tuples in
+        the form (ref, operator, value), or as a string in query form. If it
+        is ``None``, no filter will be applied. """
+        info = []
+        for (ref, operator, value) in self.parse(cuts):
+            info.append({'ref': ref, 'operator': operator, 'value': value})
+            table, column = self.cube.model[ref].bind(self.cube)
+            q = self.ensure_table(q, table)
+            q = q.where(column == type_coerce(value, column.type))
+        return info, q
