25d5576b58f0ffb923f4a729800d114f7dbd119f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:51 +0000

    Add package django-fernet-fields/0.4/django-fernet-fields-0.4.tar.gz

---
+++ packages/django-fernet-fields/django-fernet-fields-0.4.tar.gz/django-fernet-fields-0.4/fernet_fields/fields.py
@@ -0,0 +1,124 @@
+from cryptography.fernet import Fernet, MultiFernet
+from django.conf import settings
+from django.core.exceptions import FieldError, ImproperlyConfigured
+from django.db import models
+from django.utils.encoding import force_bytes, force_text
+from django.utils.functional import cached_property
+
+from . import hkdf
+
+
+__all__ = [
+    'EncryptedField',
+    'EncryptedTextField',
+    'EncryptedCharField',
+    'EncryptedEmailField',
+    'EncryptedIntegerField',
+    'EncryptedDateField',
+    'EncryptedDateTimeField',
+]
+
+
+class EncryptedField(models.Field):
+    """A field that encrypts values using Fernet symmetric encryption."""
+    _internal_type = 'BinaryField'
+
+    def __init__(self, *args, **kwargs):
+        if kwargs.get('primary_key'):
+            raise ImproperlyConfigured(
+                "%s does not support primary_key=True."
+                % self.__class__.__name__
+            )
+        if kwargs.get('unique'):
+            raise ImproperlyConfigured(
+                "%s does not support unique=True."
+                % self.__class__.__name__
+            )
+        if kwargs.get('db_index'):
+            raise ImproperlyConfigured(
+                "%s does not support db_index=True."
+                % self.__class__.__name__
+            )
+        super(EncryptedField, self).__init__(*args, **kwargs)
+
+    @cached_property
+    def keys(self):
+        keys = getattr(settings, 'FERNET_KEYS', None)
+        if keys is None:
+            keys = [settings.SECRET_KEY]
+        return keys
+
+    @cached_property
+    def fernet_keys(self):
+        if getattr(settings, 'FERNET_USE_HKDF', True):
+            return [hkdf.derive_fernet_key(k) for k in self.keys]
+        return self.keys
+
+    @cached_property
+    def fernet(self):
+        if len(self.fernet_keys) == 1:
+            return Fernet(self.fernet_keys[0])
+        return MultiFernet([Fernet(k) for k in self.fernet_keys])
+
+    def get_internal_type(self):
+        return self._internal_type
+
+    def get_db_prep_save(self, value, connection):
+        value = super(
+            EncryptedField, self
+        ).get_db_prep_save(value, connection)
+        if value is not None:
+            retval = self.fernet.encrypt(force_bytes(value))
+            return connection.Database.Binary(retval)
+
+    def get_prep_lookup(self, lookup_type, value):
+        if lookup_type == 'isnull':
+            return super(EncryptedField, self).get_prep_lookup(lookup_type,
+                                                               value)
+        else:
+            raise FieldError(
+                "%s '%s' does not support lookups."
+                % (self.__class__.__name__, self.name)
+            )
+
+    def from_db_value(self, value, expression, connection, context):
+        if value is not None:
+            value = bytes(value)
+            return self.to_python(force_text(self.fernet.decrypt(value)))
+
+    @cached_property
+    def validators(self):
+        # Temporarily pretend to be whatever type of field we're masquerading
+        # as, for purposes of constructing validators (needed for
+        # IntegerField and subclasses).
+        self.__dict__['_internal_type'] = super(
+            EncryptedField, self
+        ).get_internal_type()
+        try:
+            return super(EncryptedField, self).validators
+        finally:
+            del self.__dict__['_internal_type']
+
+
+class EncryptedTextField(EncryptedField, models.TextField):
+    pass
+
+
+class EncryptedCharField(EncryptedField, models.CharField):
+    pass
+
+
+class EncryptedEmailField(EncryptedField, models.EmailField):
+    pass
+
+
+class EncryptedIntegerField(EncryptedField, models.IntegerField):
+    pass
+
+
+class EncryptedDateField(EncryptedField, models.DateField):
+    pass
+
+
+class EncryptedDateTimeField(EncryptedField, models.DateTimeField):
+    pass
