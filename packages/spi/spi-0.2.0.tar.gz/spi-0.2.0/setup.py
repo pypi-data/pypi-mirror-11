da2f6b852cebbe969fc4f8b9a572cdee0534713a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:03 +0000

    Add package spi/0.2.0/spi-0.2.0.tar.gz

---
+++ packages/spi/spi-0.2.0.tar.gz/spi-0.2.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="spi",
+      version="0.2.0",
+      description="Pure Python SPI Interface using spidev",
+      long_description="Pure Python SPI interface using spidev",
+      author="Tom Stokes",
+      author_email="tomstokes@gmail.com",
+      url="http://github.com/tomstokes/python-spi",
+      license="MIT",
+      py_modules=['spi'])
