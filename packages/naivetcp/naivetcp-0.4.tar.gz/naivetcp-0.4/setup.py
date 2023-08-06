66fd28251c4912db6bbafd6e1a09c611d3720054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:43 +0000

    Add package naivetcp/0.4/naivetcp-0.4.tar.gz

---
+++ packages/naivetcp/naivetcp-0.4.tar.gz/naivetcp-0.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'naivetcp',
+  packages = ['naivetcp'], # this must be the same as the name above
+  version = '0.4',
+  description = 'A basic library for implementing tcp client and server',
+  author = 'rosesonfire',
+  author_email = 'ishti_iks@yahoo.com',
+  url = 'https://github.com/rosesonfire/naive_tcp', # use the URL to the github repo
+  download_url = 'https://github.com/rosesonfire/naive_tcp/tarball/0.2', # I'll explain this in a second
+  keywords = ['tcp', 'client', 'server', 'distributed systems', 'networking', 'data link layer', 'socket programming'], # arbitrary keywords
+  classifiers = [],
+)
