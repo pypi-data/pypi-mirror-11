bd812654c8008409edba6f9e4da3e7c32df4f665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:46 +0000

    Add package yopp/0.1.1/yopp-0.1.1-py2.py3-none-any.whl

---
+++ packages/yopp/yopp-0.1.1-py2.py3-none-any.whl/yopp/constants.py
@@ -0,0 +1,11 @@
+# -*- coding: utf8 -*-
+# vim: ts=4 sts=4 sw=4 et:
+
+
+YAML_MIME_TYPES = [
+    'text/yaml',
+    'application/raml+yaml',
+    'text/x-yaml',
+    'application/yaml',
+    'application/x-yaml',
+]
