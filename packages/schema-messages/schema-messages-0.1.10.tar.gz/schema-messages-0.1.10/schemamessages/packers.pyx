87f6bd087b2e5cf95e6a7f82e471c9124b23114b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:45 +0000

    Add package schema-messages/0.1.10/schema-messages-0.1.10.tar.gz

---
+++ packages/schema-messages/schema-messages-0.1.10.tar.gz/schema-messages-0.1.10/schemamessages/packers.pyx
@@ -0,0 +1,30 @@
+import io
+from ctypes import create_string_buffer
+
+def pack_message(message):
+    return message.struct.pack(message.__class__.id, *message.deflate())
+
+def pack_messages(messages):
+    lng = 0
+    for msg in messages:
+        lng += msg.binary_length
+
+    buf = create_string_buffer(lng)
+    offset = 0
+
+    for msg in messages:
+        msg.struct.pack_into(buf, offset, msg.__class__.id, *msg.deflate())
+        offset += msg.binary_length
+
+    return buf.raw
+
+
+def pack_messages_of_single_type(messages):
+    buf = create_string_buffer(messages[0].binary_length * len(messages))
+    offset = 0
+
+    for msg in messages:
+        msg.struct.pack_into(buf, offset, msg.__class__.id, *msg.deflate())
+        offset += msg.binary_length
+
+    return buf.raw
