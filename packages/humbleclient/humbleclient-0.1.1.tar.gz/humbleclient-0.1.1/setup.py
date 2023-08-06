a71e92a4c3040f97ef110712a38d0294c871c58e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:19 +0000

    Add package humbleclient/0.1.1/humbleclient-0.1.1.tar.gz

---
+++ packages/humbleclient/humbleclient-0.1.1.tar.gz/humbleclient-0.1.1/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2
+
+from distutils.core import setup
+
+import humble
+
+name     = "humbleclient"
+base_url = "http://chadok.info/humbleclient"
+version  = humble.version
+
+setup(
+    name         = name,
+    packages     = [],
+    version      = version,
+    description  = "Non official client for Humble Bundle",
+    author       = "Olivier Schwander",
+    author_email = "olivier.schwander@ens-lyon.org",
+    url          = base_url,
+    download_url = base_url + "/" + name + "-" + version + ".tar.gz",
+    py_modules   = ["humble"],
+    scripts      = ["hb"],
+    requires     = ["PyYAML", "requests"],
+    classifiers  = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        ],
+)
