1099b60c2462aff9582f2500e6edc488aae090a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:10 +0000

    Add package nesterChenChengKuan/1.1.0/nesterChenChengKuan-1.1.0.zip

---
+++ packages/nesterChenChengKuan/nesterChenChengKuan-1.1.0.zip/nesterChenChengKuan-1.1.0/setup.py
@@ -0,0 +1,4 @@
+from distutils.core import setup
+
+setup(name='nesterChenChengKuan',version='1.1.0',PY_modules=['nester'],author='hfpython',author_email='sttagomantis@gmail.com',url='http://www.headfirstlabs.com',description='SIMPLE TEST' 
+)
