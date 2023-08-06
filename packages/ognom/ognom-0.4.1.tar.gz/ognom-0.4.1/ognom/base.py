2a8c742f290118a2ac96a597f6e52c6e4c5e1125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:21 +0000

    Add package ognom/0.4.1/ognom-0.4.1.tar.gz

---
+++ packages/ognom/ognom-0.4.1.tar.gz/ognom-0.4.1/ognom/base.py
@@ -0,0 +1,10 @@
+"""
+@deprecated
+"""
+
+# TODO: remove this import
+from ognom.document import Document
+from ognom.connection import ConnectionManager
+from ognom.collection import Collection
+
+
