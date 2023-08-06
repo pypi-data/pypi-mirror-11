67e79000ded84ba373d893da214563e9eef0387e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:29 +0000

    Add package CAS_bottle/1.1/CAS_bottle-1.1.tar.gz

---
+++ packages/CAS_bottle/CAS_bottle-1.1.tar.gz/CAS_bottle-1.1/test_cas_bottle.py
@@ -0,0 +1,60 @@
+import bottle
+from bottle import route
+import CAS_bottle
+from beaker.middleware import SessionMiddleware
+
+# Enable beaker sessions 
+session_opts = {
+    'session.type': 'memory',
+    'session.cookie_expires': 300,
+    'session.auto': True
+}
+app = SessionMiddleware(bottle.app(), session_opts)
+
+# Create the plugin
+auth=CAS_bottle.CASAuth(cas_server="https://your.cas.server/cas", #<= Your CAS server
+                        service_url="http://localhost:8080/login")
+# If some callbacks require auth, and some dont.
+# use apply=[auth] as a @route parameter to force CAS authentification
+# Any callback can get the username (None if user is not authentified)
+
+# Instead, use : bottle.install(auth) if you want authentification on for all 
+# callbacks
+
+@route(['/index','/'])
+def index() :
+    stri="""
+    Index page
+    <ul>
+    <li> <a href='authornot'>You can access this page wether you are logged in or not</a></li>
+    <li> <a href='authforced'>You must log in to access this page</a></<li>
+    <li> <a href='login'>Log in (if you are not) and go back to index</a></li>
+    <li> <a href='logout'>Log out</a></li>
+    </ul>
+    """
+    return stri 
+
+@route('/authornot') 
+def authornot() :
+    username=auth.username()
+    stri="You accessed this page with login : {}.".format(username)
+    if not username : stri+=" Anonymous access"
+    return stri
+
+@route('/authforced',apply=[auth]) 
+def authforced() :
+    username=auth.username()
+    stri="You accessed this page with login : {}.".format(username)
+    return stri
+
+
+@route('/logout')
+def logout() :
+    auth.logout()
+    return ""
+
+@route('/login',apply=[auth])
+def login() :
+    bottle.redirect('/index')
+
+bottle.run(app, host='localhost', port=8080,debug= True, reloader=True)
