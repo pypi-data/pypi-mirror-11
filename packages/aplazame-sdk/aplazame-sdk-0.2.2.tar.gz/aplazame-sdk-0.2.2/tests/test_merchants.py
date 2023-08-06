da2892a95068d5ca634bc8aaac9f18e6ac18111b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:36 +0000

    Add package aplazame-sdk/0.2.2/aplazame-sdk-0.2.2.tar.gz

---
+++ packages/aplazame-sdk/aplazame-sdk-0.2.2.tar.gz/aplazame-sdk-0.2.2/tests/test_merchants.py
@@ -0,0 +1,33 @@
+from .base import PrivateTestCase
+from .decorators import instance_required
+
+
+class MerchantsTestCase(PrivateTestCase):
+
+    def setUp(self):
+        super(MerchantsTestCase, self).setUp()
+
+        results = self.client.merchants().json()['results']
+        self.instance = results[0] if results else None
+
+    def test_list(self):
+        response = self.client.merchants()
+        self.assertEqual(response.status_code, 200)
+
+    @instance_required
+    def test_detail(self):
+        response = self.client.merchant_detail(self.instance['id'])
+        self.assertEqual(response.status_code, 200)
+
+    def test_me(self):
+        response = self.client.me()
+        self.assertEqual(response.status_code, 200)
+
+    def test_operations(self):
+        response = self.client.operations()
+        self.assertEqual(response.status_code, 200)
+
+    @instance_required
+    def test_merchant_operations(self):
+        response = self.client.merchant_operations(self.instance['id'])
+        self.assertEqual(response.status_code, 200)
