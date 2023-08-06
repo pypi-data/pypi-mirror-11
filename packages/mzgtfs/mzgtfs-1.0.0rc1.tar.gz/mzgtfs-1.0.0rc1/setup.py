383b23d6f565914811f162e46f52c12dfbbd4358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:50 +0000

    Add package mzgtfs/1.0.0rc1/mzgtfs-1.0.0rc1.tar.gz

---
+++ packages/mzgtfs/mzgtfs-1.0.0rc1.tar.gz/mzgtfs-1.0.0rc1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+import mzgtfs
+
+setup(
+  name='mzgtfs',
+  version=mzgtfs.__version__,
+  description='Mapzen GTFS',
+  author='Ian Rees',
+  author_email='ian@mapzen.com',
+  url='https://github.com/transitland/mapzen-gtfs',
+  license='License :: OSI Approved :: MIT License',
+  packages=['mzgtfs'],
+  install_requires=['unicodecsv', 'pytz'],
+  zip_safe=False,
+  # Include examples.
+  package_data = {
+    '': ['*.txt', '*.md', '*.zip']
+  }
+)
