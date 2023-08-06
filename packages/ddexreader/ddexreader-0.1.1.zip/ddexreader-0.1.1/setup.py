46e417dfb3979e5cbe0c65f626b10279e53f139c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:58 +0000

    Add package ddexreader/0.1.1/ddexreader-0.1.1.zip

---
+++ packages/ddexreader/ddexreader-0.1.1.zip/ddexreader-0.1.1/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages
+from setuptools.command.test import test as TestCommand
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
+requirements = [
+    'PyXB',
+]
+
+test_requirements = [
+    'pytest',
+]
+
+class PyTest(TestCommand):
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        pytest.main(self.test_args)
+
+setup(
+    name='ddexreader',
+    version='0.1.1',
+    description="This small project allows you to decode DDEX files into friendly Python data types.",
+    long_description=readme + '\n\n' + history,
+    author="Flavian Hautbois",
+    author_email='flavian@trax-air.com',
+    url='https://github.com/Trax-air/ddexreader',
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    include_package_data=True,
+    install_requires=requirements,
+    license='MIT',
+    zip_safe=False,
+    keywords='ddexreader',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    cmdclass={'test': PyTest},
+    tests_require=test_requirements
+)
