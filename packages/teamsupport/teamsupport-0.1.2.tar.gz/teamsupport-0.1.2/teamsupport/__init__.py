dac074f4f7e3a5bbfc16773147ac0924d3aa17ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:12 +0000

    Add package teamsupport/0.1.2/teamsupport-0.1.2.tar.gz

---
+++ packages/teamsupport/teamsupport-0.1.2.tar.gz/teamsupport-0.1.2/teamsupport/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from teamsupport.models import Action, Ticket, User
+from teamsupport.services import TeamSupportService
+
+__author__ = 'Yola Engineers'
+__email__ = 'engineers@yola.com'
+__version__ = '0.1.2'
+
+__all__ = (Action, TeamSupportService, Ticket, User)
