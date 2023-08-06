7cef67d585ba3f265315cf5e3dac35b431ded880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:48 +0000

    Add package myql/1.2.6/myql-1.2.6.tar.gz

---
+++ packages/myql/myql-1.2.6.tar.gz/myql-1.2.6/tests/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+from tests.tests import TestMYQL
+from tests.tests import TestWeather
+from tests.tests import TestStockScraper
+from tests.tests import TestTable
+from tests.tests import TestOAuth
+from tests.tests import TestSocial
+from tests.tests import TestFilters
+from tests.tests import TestRemoteFilters
+from tests.tests import TestFuncFilters
+from tests.tests import TestPaging
