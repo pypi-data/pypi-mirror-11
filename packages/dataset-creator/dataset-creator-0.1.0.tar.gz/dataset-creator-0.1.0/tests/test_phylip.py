1c9f19ad6c00578655cabee2587eeac844065494
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:17 +0000

    Add package dataset-creator/0.1.0/dataset-creator-0.1.0.tar.gz

---
+++ packages/dataset-creator/dataset-creator-0.1.0.tar.gz/dataset-creator-0.1.0/tests/test_phylip.py
@@ -0,0 +1,26 @@
+import os
+import unittest
+
+from dataset_creator.dataset import Dataset
+from dataset_creator.creator import Creator
+
+from .data import test_data
+
+PHYLIP_DATA_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'Phylip')
+
+
+class TestPhylip(unittest.TestCase):
+    def setUp(self):
+        self.maxDiff = None
+
+    def test_dataset(self):
+        dataset = Dataset(test_data, format='PHYLIP', partitioning='by gene')
+        result = dataset.dataset_str
+        expected = open(os.path.join(PHYLIP_DATA_PATH, 'dataset.phy')).read()
+        self.assertEqual(expected, result)
+
+    def test_charset_file(self):
+        dataset = Dataset(test_data, format='PHYLIP', partitioning='by gene')
+        result = dataset.extra_dataset_str
+        expected = open(os.path.join(PHYLIP_DATA_PATH, 'charset_block_file.txt')).read()
+        self.assertEqual(expected, result)
