c6c7dc45615213b02ac07174c41c866d90f4da2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:43 +0000

    Add package notebook-ok-extension/0.1.1/notebook-ok-extension-0.1.1.tar.gz

---
+++ packages/notebook-ok-extension/notebook-ok-extension-0.1.1.tar.gz/notebook-ok-extension-0.1.1/setup.py
@@ -0,0 +1,26 @@
+"""Setup script for Notebook Ok Extension"""
+from setuptools import setup
+
+try:
+    from jupyterpip import cmdclass
+except:
+    import pip, importlib
+    pip.main(['install', 'jupyter-pip'])
+    cmdclass = importlib.import_module('jupyterpip').cmdclass
+
+VERSION = '0.1.1'
+
+setup(
+    name                = 'notebook-ok-extension',
+    packages            = ['ok'],
+    version             = VERSION,
+    install_requires    = ['jupyter-pip'],
+    description         = """An extension of the Jupyter Notebook that integrates testing with OK client.""",
+    cmdclass            = cmdclass('ok', 'ok/ok'),
+    author              = 'Alvin Wan',
+    author_email        = 'hi@alvinwan.com',
+    url                 = 'https://github.com/dsten/notebook-ok-extension',
+    download_url        = 'https://github.com/dsten/notebook-ok-extension/archive/%s.zip' % VERSION,
+    keywords            = ['data', 'tools', 'berkeley'],
+    classifiers         = ['Programming Language :: Python']
+)
