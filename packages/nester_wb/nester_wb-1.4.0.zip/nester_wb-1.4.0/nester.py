44546f043d6f47e9cae28d053110f383eb37a8c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:34 +0000

    Add package nester_wb/1.4.0/nester_wb-1.4.0.zip

---
+++ packages/nester_wb/nester_wb-1.4.0.zip/nester_wb-1.4.0/nester.py
@@ -0,0 +1,15 @@
+"""This is a nester.py  module. It offers a function to print a list(maybe every element in this list is another list too.) recursive. From book 'Header First Python'"""
+
+def printList(dataList,needtab=False, level=0):
+	"""do iteration."""
+
+	for data in dataList:
+		if isinstance(data, list):
+			printList(data, needtab, level+1)
+		else:
+			if needtab:
+				for tab_stop in range(level):
+					print("\t", end='')
+			print(data)
+
+	#example : print([1,2,3,4])
---
+++ packages/nester_wb/nester_wb-1.4.0.zip/nester_wb-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'nester_wb',
+	version = '1.4.0',
+	py_modules = ['nester'],
+	author = 'true14fans',
+	author_email = 'wbcommon@163.com',
+	url = 'http://www.headerfirstlabs.com',
+	desciption = 'A simple printer of nested lists'
+)
