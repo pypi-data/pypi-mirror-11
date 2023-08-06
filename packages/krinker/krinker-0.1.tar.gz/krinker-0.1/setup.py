51383f0b2084db2aa62617d26f864db625fdc47e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:41 +0000

    Add package krinker/0.1/krinker-0.1.tar.gz

---
+++ packages/krinker/krinker-0.1.tar.gz/krinker-0.1/setup.py
@@ -0,0 +1,16 @@
+'''
+Created on Sep 29, 2015
+
+@author: alkrinker
+'''
+from setuptools import setup
+
+setup(name='krinker',
+      version='0.1',
+      description='Testing python\'s distribute',
+      url='http://al-krinker.blogspot.com/',
+      author='Al Krinker',
+      author_email='al.krinker@gmail.com',
+      license='MIT',
+      packages=['krinker'],
+      zip_safe=False)
