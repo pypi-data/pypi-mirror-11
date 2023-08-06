b4b0a3c6b715a7e6668b2146d4b29023b540107c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:19 +0000

    Add package custom_text_test_runner/0.1/custom_text_test_runner-0.1.tar.gz

---
+++ packages/custom_text_test_runner/custom_text_test_runner-0.1.tar.gz/custom_text_test_runner-0.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='custom_text_test_runner',
+      version='0.1',
+      description='Python Custom Text Test Runner',
+      author='Abtin Gramian',
+      author_email='abtin.gramian@gmail.com',
+      url='https://github.com/agramian/custom-text-test-runner',
+      packages=['custom_text_test_runner'],
+      install_requires=['table_printer'],
+      download_url = 'https://github.com/agramian/custom-text-test-runner/tarball/v0.1',
+      keywords = ['unittest', 'custom', 'test', 'runner', 'result', 'json'],
+      classifiers = [],
+     )
