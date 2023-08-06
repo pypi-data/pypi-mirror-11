993addce2a56f59c600f5a7e0c25ec11548d2f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:49 +0000

    Add package minima/0.1/minima-0.1.tar.gz

---
+++ packages/minima/minima-0.1.tar.gz/minima-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name = 'minima',
+    packages = ['minima'],
+    version = '0.1',
+    description = 'Python library for generating HTML, JS, CSS.',
+    author = 'Tomer Filiba',
+    url = 'https://github.com/kevinsl/minima',
+    download_url = 'https://github.com/kevinsl/minima/tarball/0.1',
+    keywords = [],
+    classifiers = [],
+)
