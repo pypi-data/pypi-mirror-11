cbff0333409f10bde8cf3a8526fd3ec69e79ce49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:18 +0000

    Add package minecraft-data/0.3.1/minecraft_data-0.3.1.tar.gz

---
+++ packages/minecraft-data/minecraft_data-0.3.1.tar.gz/minecraft_data-0.3.1/minecraft_data/v1_9/__init__.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+from minecraft_data.tools import convert
+
+
+_dir = os.path.dirname(__file__)
+
+for name, data in convert(_dir).items():
+    setattr(sys.modules[__name__], name, data)
