7638e88de717a087fa87c062ca828a9296427a75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:13 +0000

    Add package ragool/0.0.2a/ragool-0.0.2a.tar.gz

---
+++ packages/ragool/ragool-0.0.2a.tar.gz/ragool-0.0.2a/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+setup(name="ragool",
+      version="0.0.2a",
+      description="Handle Bottle.py errors with love <3",
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.4",
+        "Framework :: Bottle",
+        "Topic :: Utilities",
+      ],
+      keywords="error handling",
+      url="https://github.com/aybb/ragool",
+      author="aybb",
+      author_email="lasthitknxx@gmail.com",
+      license="MIT",
+      packages=["Ragool"],
+      install_requires=[
+          "bottle"
+      ],
+      include_package_data=True,
+      zip_safe=False)
