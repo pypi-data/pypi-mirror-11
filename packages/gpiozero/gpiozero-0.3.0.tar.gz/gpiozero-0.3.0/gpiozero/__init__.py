7a84d3db0b34301c13a0edcce9c23e60cd28cb88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:06 +0000

    Add package gpiozero/0.3.0/gpiozero-0.3.0.tar.gz

---
+++ packages/gpiozero/gpiozero-0.3.0.tar.gz/gpiozero-0.3.0/gpiozero/__init__.py
@@ -0,0 +1,42 @@
+from __future__ import absolute_import
+
+import atexit
+
+from RPi import GPIO
+
+from .devices import (
+    _gpio_threads_shutdown,
+    GPIODeviceError,
+    GPIODevice,
+)
+from .input_devices import (
+    InputDeviceError,
+    InputDevice,
+    Button,
+    MotionSensor,
+    LightSensor,
+    TemperatureSensor,
+)
+from .output_devices import (
+    OutputDevice,
+    LED,
+    Buzzer,
+    Motor,
+)
+from .boards import (
+    TrafficLights,
+    PiTraffic,
+    FishDish,
+    PiLiter,
+)
+
+
+def gpiozero_shutdown():
+    _gpio_threads_shutdown()
+    GPIO.cleanup()
+
+atexit.register(gpiozero_shutdown)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+__version__ = '0.3.0'
