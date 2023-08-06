4ee70f0f3b7ea5a4f19ee7a2870fc68405c5d2a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:19 +0000

    Add package django-yadt/1/django-yadt-1.tar.gz

---
+++ packages/django-yadt/django-yadt-1.tar.gz/django-yadt-1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='django-yadt',
+    version=1,
+    packages=(
+        'django_yadt',
+        'django_yadt.management',
+        'django_yadt.management.commands',
+    ),
+)
