5cb90ed656dce8c1c394fd00010b5963d11d9ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:15 +0000

    Add package prompt/0.2.0/prompt-0.2.0-py3-none-any.whl

---
+++ packages/prompt/prompt-0.2.0-py3-none-any.whl/prompt-0.2.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,82 @@
+Python Prompt Package
+=====================
+
+|PyPI Version| |PyPI Downloads| |Travis|
+
+| This is a library for prompting input on the command line.
+| Python 3.2+ and Wheels are supported.
+| It was initiated by Stefan Fischer and is developed and maintained by
+  many others.
+
+-  `Questions <mailto:sfischer13@ymail.com>`__ can be asked via e-mail.
+-  `Source code <https://github.com/sfischer13/python-prompt>`__ is
+   tracked on GitHub.
+-  `Changes <https://github.com/sfischer13/python-prompt/blob/master/CHANGELOG.rst>`__
+   between releases are documented.
+-  `Bugs <https://github.com/sfischer13/python-prompt/issues>`__ can be
+   reported on the issue tracker.
+
+Install
+-------
+
+|PyPI Python Versions| |PyPI Wheel|
+
+The package is available on
+`PyPI <https://pypi.python.org/pypi/prompt>`__:
+
+::
+
+    $ pip install prompt
+
+Use
+---
+
+The package may be imported directly:
+
+::
+
+    import prompt
+
+    # modify default prompt
+    integer = prompt.integer(prompt="Please enter a number: ")
+
+    # allow empty response and return None
+    real = prompt.real(empty=True)
+
+    # require a two digit number
+    regex = prompt.regex("^\d\d$")
+
+Contribute
+----------
+
+| Write a bug report or send a pull request.
+| Other
+  `contributors <https://github.com/sfischer13/python-prompt/graphs/contributors>`__
+  have done so before.
+
+-  `Roadmap <https://github.com/sfischer13/python-prompt/blob/master/TODO.rst>`__
+   of planned improvements
+-  `Issues <https://github.com/sfischer13/python-prompt/issues>`__ that
+   have been reported
+
+License
+-------
+
+| Copyright (c) 2015 Stefan Fischer
+| The source code is available under the **MIT License**.
+| See
+  `LICENSE <https://github.com/sfischer13/python-prompt/blob/master/LICENSE>`__
+  for further details.
+
+.. |PyPI Version| image:: https://img.shields.io/pypi/v/prompt.svg
+   :target: https://pypi.python.org/pypi/prompt
+.. |PyPI Downloads| image:: https://img.shields.io/pypi/dm/prompt.svg
+   :target: https://pypi.python.org/pypi/prompt
+.. |Travis| image:: https://img.shields.io/travis/sfischer13/python-prompt.svg
+   :target: https://travis-ci.org/sfischer13/python-prompt
+.. |PyPI Python Versions| image:: https://img.shields.io/pypi/pyversions/prompt.svg
+   :target: https://pypi.python.org/pypi/prompt
+.. |PyPI Wheel| image:: https://img.shields.io/pypi/wheel/prompt.svg
+   :target: https://pypi.python.org/pypi/prompt
+
+
---
+++ packages/prompt/prompt-0.2.0-py3-none-any.whl/prompt/__init__.py
@@ -0,0 +1,135 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2015 Stefan Fischer
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"""
+This is a library for prompting input on the command line.
+
+The package may be imported directly::
+
+    import prompt
+
+The library was initiated by Stefan Fischer and is developed and maintained by many others.
+"""
+
+import getpass
+import re
+
+__author__ = "Stefan Fischer"
+__contact__ = "Stefan Fischer <sfischer13@ymail.com>"
+__copyright__ = "Copyright (c) 2015 Stefan Fischer"
+__credits__ = []
+__date__ = "2015-09-27"
+__license__ = "MIT"
+__status__ = "development"
+__version__ = "0.2.0"
+
+PROMPT = "? "
+RE_EMAIL_SIMPLE = re.compile(r"^[^@]+@[^@]+\.[^@]+$")
+
+
+def character(prompt=None, empty=False):
+    s = _prompt_input(prompt)
+    if empty and not s:
+        return None
+    elif len(s) == 1:
+        return s
+    else:
+        return character(prompt=prompt, empty=empty)
+
+
+def email(prompt=None, empty=False, mode="simple"):
+    if mode == "simple":
+        s = _prompt_input(prompt)
+        if empty and not s:
+            return None
+        else:
+            if RE_EMAIL_SIMPLE.match(s):
+                return s
+            else:
+                return email(prompt=prompt, empty=empty, mode=mode)
+    else:
+        raise ValueError
+
+
+def integer(prompt=None, empty=False):
+    s = _prompt_input(prompt)
+    if empty and not s:
+        return None
+    else:
+        try:
+            return int(s)
+        except ValueError:
+            return integer(prompt=prompt, empty=empty)
+
+
+def real(prompt=None, empty=False):
+    s = _prompt_input(prompt)
+    if empty and not s:
+        return None
+    else:
+        try:
+            return float(s)
+        except ValueError:
+            return real(prompt=prompt, empty=empty)
+
+
+def regex(pattern, prompt=None, empty=False, flags=0):
+    s = _prompt_input(prompt)
+    if empty and not s:
+        return None
+    else:
+        m = re.match(pattern, s, flags=flags)
+        if m:
+            return m
+        else:
+            return regex(pattern, prompt=prompt, empty=empty, flags=flags)
+
+
+# TODO: getpass.GetPassWarning
+def secret(prompt=None, empty=False):
+    if prompt is None:
+        prompt = PROMPT
+    s = getpass.getpass(prompt=prompt)
+    if empty and not s:
+        return None
+    else:
+        if s:
+            return s
+        else:
+            return secret(prompt=prompt, empty=empty)
+
+
+def string(prompt=None, empty=False):
+    s = _prompt_input(prompt)
+    if empty and not s:
+        return None
+    else:
+        if s:
+            return s
+        else:
+            return string(prompt=prompt, empty=empty)
+
+
+def _prompt_input(prompt):
+    if prompt is None:
+        return input(PROMPT)
+    else:
+        return input(prompt)
