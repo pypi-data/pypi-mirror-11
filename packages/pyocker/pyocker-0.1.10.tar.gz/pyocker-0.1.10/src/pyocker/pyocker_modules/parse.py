38b7b82929ef9901b44d5df5a36fbcb2439f83c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:18 +0000

    Add package pyocker/0.1.10/pyocker-0.1.10.tar.gz

---
+++ packages/pyocker/pyocker-0.1.10.tar.gz/pyocker-0.1.10/src/pyocker/pyocker_modules/parse.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import yaml
+
+def parse_config(filepath):
+    try:
+        with open(filepath, 'r') as f:
+            config = yaml.load(f)
+    except Exception as e:
+        sys.stderr.write('Cannot read config file %s\n' % filepath)
+        exit()
+    return config
