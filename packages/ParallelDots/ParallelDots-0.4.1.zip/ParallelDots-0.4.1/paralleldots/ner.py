f2bec662cbc290963b430c458273802bc23f6bcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:58 +0000

    Add package ParallelDots/0.4.1/ParallelDots-0.4.1.zip

---
+++ packages/ParallelDots/ParallelDots-0.4.1.zip/ParallelDots-0.4.1/paralleldots/ner.py
@@ -0,0 +1,17 @@
+import urllib3, urllib
+import json
+from paralleldots.config import get_api_key
+http = urllib3.PoolManager()
+
+def get_ner(s1):
+	apikey  = get_api_key()
+	if not apikey == None:
+		params = { 'sentence1' : s1, 'apikey' : apikey}
+		en_params = urllib.urlencode(params)
+		url = 'http://apis.paralleldots.com/ner?'+en_params
+		r =  http.request('GET',url)
+		return json.loads(r.data)
+	else:
+		return "API key does not exist"
+
+
