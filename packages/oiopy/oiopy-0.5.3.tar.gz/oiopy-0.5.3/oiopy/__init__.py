ed0c7fb9678d8426ad25c9f6949a1df0bed62cf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:27 +0000

    Add package oiopy/0.5.3/oiopy-0.5.3.tar.gz

---
+++ packages/oiopy/oiopy-0.5.3.tar.gz/oiopy-0.5.3/oiopy/__init__.py
@@ -0,0 +1,11 @@
+
+__all__ = ['__version__']
+
+import pbr.version
+
+version_info = pbr.version.VersionInfo('oiopy')
+
+try:
+    __version__ = version_info.version_string()
+except AttributeError:
+    __version__ = None
