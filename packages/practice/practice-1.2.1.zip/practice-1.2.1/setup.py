c23ed9856c65a803eba3123658862c78c9e17bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:44 +0000

    Add package practice/1.2.1/practice-1.2.1.zip

---
+++ packages/practice/practice-1.2.1.zip/practice-1.2.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+        name = 'practice',
+        version = '1.2.1',
+        py_modules = ['practice'],
+        author = 'yuitaku',
+        author_email = '',
+        url = '',
+        description = 'Sorry,This is practice'
+
+
+
+
+    )
