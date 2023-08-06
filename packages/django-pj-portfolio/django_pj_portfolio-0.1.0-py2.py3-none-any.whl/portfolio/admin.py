a2115a7ff3bcfd9fab9c6a4c2dd6f189a93ba832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:18 +0000

    Add package django-pj-portfolio/0.1.0/django_pj_portfolio-0.1.0-py2.py3-none-any.whl

---
+++ packages/django-pj-portfolio/django_pj_portfolio-0.1.0-py2.py3-none-any.whl/portfolio/admin.py
@@ -0,0 +1,7 @@
+from portfolio.models import Transaction, Security, Price, Account
+from django.contrib import admin
+
+admin.site.register(Transaction)
+admin.site.register(Security)
+admin.site.register(Price)
+admin.site.register(Account)
