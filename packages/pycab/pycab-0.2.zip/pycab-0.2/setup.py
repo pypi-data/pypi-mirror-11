3e41e1b769b94c133981376f2ab5e5d67d7d55f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:59 +0000

    Add package pycab/0.2/pycab-0.2.zip

---
+++ packages/pycab/pycab-0.2.zip/pycab-0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pycab',
+  packages = ['pycab'],
+  version = '0.2',
+  description = 'A library to manipulate Microsoft Cabinet files',
+  author = 'Enrique Nissim',
+  author_email = 'n3k1990@gmail.com',
+  url = 'https://github.com/n3k/PyCAB',
+  download_url = 'https://github.com/n3k/PyCAB/tarball/0.2',
+  keywords = ['cabinet', 'cab'],
+  classifiers = [],
+)
