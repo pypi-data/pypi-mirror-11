e434ed47f9387adbf8cb6d9ca6902b0ee3713a62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:36 +0000

    Add package django-jython/1.8.0b3/django_jython-1.8.0b3-py2.7.egg

---
+++ packages/django-jython/django_jython-1.8.0b3-py2.7.egg/doj/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__VERSION = (1, 8, 0, 'b', 3)
+
+
+def get_version():
+    """
+    Gets the version of the library
+    :return: Version
+    """
+    return tuple(__VERSION)
