d525b2fc6a5c59e782af165dd153553ad70f88e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:10 +0000

    Add package dockdev/0.1.3/dockdev-0.1.3.tar.gz

---
+++ packages/dockdev/dockdev-0.1.3.tar.gz/dockdev-0.1.3/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "dockdev",
+    version = "0.1.3",
+    author = "Ian Maddison",
+    author_email = "ian.maddison@digital.cabinet-office.gov.uk",
+    description = ("A simple development setup tool for docker containerised apps"),
+    license = "MIT",
+    keywords = "docker container development setup",
+    url = "https://github.com/alphagov/dockdev",
+    packages=['dockdev'],
+    scripts=['bin/dockdev'],
+    install_requires=['docker-py>=1.3.1', 'GitPython>=1.0.1'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
