6cb2dc63d1385101f2dc0c32b9964cc8738faa23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:51 +0000

    Add package sunshine/1.0.0/sunshine-1.0.0.tar.gz

---
+++ packages/sunshine/sunshine-1.0.0.tar.gz/sunshine-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name='sunshine',
+	version='1.0.0',
+	py_modules=['sunshine'],
+	author='mngxiaoxiao',
+	author_email='mng1990@163.com',
+	url='',
+	description='a simple printer of sunshine lists',
+	)
