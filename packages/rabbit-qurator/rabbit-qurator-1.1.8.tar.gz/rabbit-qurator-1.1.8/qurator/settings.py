0a6a3f3b03fa017a9f9df4d67bb12c5b4d5c4f19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:42 +0000

    Add package rabbit-qurator/1.1.8/rabbit-qurator-1.1.8.tar.gz

---
+++ packages/rabbit-qurator/rabbit-qurator-1.1.8.tar.gz/rabbit-qurator-1.1.8/qurator/settings.py
@@ -0,0 +1,68 @@
+import os
+import sys
+
+DEPLOY_ENVIRONMENT = os.environ.get('DEPLOY_ENVIRONMENT', 'development')
+if DEPLOY_ENVIRONMENT is None:
+    raise Exception("Please define the local DEPLOY_ENVIRONMENT.")
+RABBITMQ_TRANSPORT_SERVICE_HOST = os.environ.get(
+    'RABBITMQ_TRANSPORT_SERVICE_HOST',
+    'localhost')
+RABBITMQ_TRANSPORT_SERVICE_PORT = os.environ.get(
+    'RABBITMQ_TRANSPORT_SERVICE_PORT',
+    5672)
+RABBITMQ_USER = os.environ.get('RABBITMQ_USER', 'guest')
+RABBITMQ_PASSWORD = os.environ.get('RABBITMQ_PASSWORD', 'guest')
+RABBITMQ_VHOST = os.environ.get('RABBITMQ_VHOST', '/')
+RABBITMQ_SSL = os.environ.get('RABBITMQ_SSL', False)
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "formatters": {
+        "simple": {
+            "format": "[level=%(levelname)s] %(pathname)s:line %(lineno)d: %(name)s - %(message)s"
+        }
+    },
+    "handlers": {
+        "consoleHandler": {
+            "class": "logging.StreamHandler",
+            "formatter": "simple",
+            "level": "DEBUG",
+        }
+    },
+    "loggers": {
+        "root": {
+            "level": "ERROR",
+            "handlers": ["consoleHandler"],
+        },
+        "qurator": {
+            "level": "INFO",
+            "propagate": True
+        }
+    }
+}
+
+# Copy pasted (with impunity) from domainsage.settings
+try:
+    if 'QURATOR_SETTINGS_DIR' in os.environ:
+        local_settings_dir = os.path.realpath(
+            os.path.expanduser(
+                os.environ['QURATOR_SETTINGS_DIR']))
+        sys.path.insert(0, local_settings_dir)
+    from local_settings import *
+except ImportError:
+    pass
+
+for mq_key in [RABBITMQ_USER, RABBITMQ_PASSWORD, RABBITMQ_VHOST]:
+    if mq_key is None:
+        raise Exception('Must define RABBITMQ_USER, RABBITMQ_PASSWORD and '
+                        'RABBITMQ_VHOST.')
+
+CONN_DICT = {
+    'hostname': RABBITMQ_TRANSPORT_SERVICE_HOST,
+    'port': RABBITMQ_TRANSPORT_SERVICE_PORT,
+    'userid': RABBITMQ_USER,
+    'password': RABBITMQ_PASSWORD,
+    'ssl': RABBITMQ_SSL,
+    'virtual_host': RABBITMQ_VHOST,
+    'heartbeat': 2
+}
