98870f0215610ca26c5828f61e7f20d61ead5277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:40 +0000

    Add package selection/0.0.11/selection-0.0.11.tar.gz

---
+++ packages/selection/selection-0.0.11.tar.gz/selection-0.0.11/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'selection',
+    version = '0.0.11',
+    description = 'API to extract content from HTML & XML documents',
+    author = 'Gregory Petukhov',
+    author_email = 'lorien@lorien.name',
+    install_requires = ['lxml', 'weblib', 'six'],
+    packages = find_packages(exclude=['test']),
+    license = "MIT",
+    classifiers = (
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ),
+)
