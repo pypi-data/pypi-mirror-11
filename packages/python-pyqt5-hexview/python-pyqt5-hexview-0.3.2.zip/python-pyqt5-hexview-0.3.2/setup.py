b6d67b439fdc459564cddcc5644a2c01870cdbe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:30 +0000

    Add package python-pyqt5-hexview/0.3.2/python-pyqt5-hexview-0.3.2.zip

---
+++ packages/python-pyqt5-hexview/python-pyqt5-hexview-0.3.2.zip/python-pyqt5-hexview-0.3.2/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from hexview import __version__
+from setuptools import setup
+
+
+description = "PyQt5 hex viewer widget."
+setup(name="python-pyqt5-hexview",
+      version=__version__,
+      description=description,
+      long_description=description,
+      author="Willi Ballenthin",
+      author_email="willi.ballenthin@gmail.com",
+      url="https://github.com/williballenthin/python-pyqt5-hexview",
+      license="Apache 2.0 License",
+      install_requires=["hexdump", "intervaltree", "funcy"],
+      packages=["hexview"],
+      package_dir={"hexview": "hexview"},
+      package_data={"hexview": ["hexview.ui"]})
