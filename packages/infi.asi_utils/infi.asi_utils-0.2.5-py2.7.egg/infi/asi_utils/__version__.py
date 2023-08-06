d94ccfe427c010d2f6abaff7fa331bcc880ae476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:36 +0000

    Add package infi.asi_utils/0.2.5/infi.asi_utils-0.2.5-py2.7.egg

---
+++ packages/infi.asi_utils/infi.asi_utils-0.2.5-py2.7.egg/infi/asi_utils/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.2.5"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(HEAD detached at v0.2.5)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'e6e1b294ee49ea35d531e1a16a639e5947976889'
+__git_head_subject__ = 'update isolated python version'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
+__git_commit_date__ = '2015-10-01 17:49:45'
