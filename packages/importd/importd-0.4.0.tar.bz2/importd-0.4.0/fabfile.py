050661b59cbca0e569fc6b73e90b923a46ea5caa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:43 +0000

    Add package importd/0.4.0/importd-0.4.0.tar.bz2

---
+++ packages/importd/importd-0.4.0.tar.bz2/importd-0.4.0/fabfile.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from fabric.api import local
+
+
+def docs():
+    local("./bin/docs")
+    local("./bin/python setup.py upload_sphinx --upload-dir=docs/html")
+
+
+def release():
+    "Update version id in setup.py, changelog and docs/source/conf.py."
+
+    local(
+        "python setup.py bdist_egg bdist_wheel sdist "
+        "--formats=bztar,gztar,zip upload"
+    )
