d4a2af523eadbb81de07dcf1226c581af0aa1c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:34 +0000

    Add package thermotools/0.1.7/thermotools-0.1.7.tar.gz

---
+++ packages/thermotools/thermotools-0.1.7.tar.gz/thermotools-0.1.7/ext/dtram/dtram.pyx
@@ -0,0 +1,318 @@
+# This file is part of thermotools.
+#
+# Copyright 2015 Computational Molecular Biology Group, Freie Universitaet Berlin (GER)
+#
+# thermotools is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+r"""
+Python interface to the dTRAM estimator's lowlevel functions.
+"""
+
+import numpy as _np
+cimport numpy as _np
+
+__all__ = [
+    'set_lognu',
+    'iterate_lognu',
+    'iterate_fi',
+    'get_pk',
+    'get_p',
+    'get_fk',
+    'normalize',
+    'estimate']
+
+cdef extern from "_dtram.h":
+    void _set_lognu(
+        double *log_nu_K_i, int *C_K_ij, int n_therm_states, int n_markov_states)
+    void _iterate_lognu(
+        double *log_nu_K_i, double *b_K_i, double *f_i, int *C_K_ij, int n_therm_states,
+        int n_markov_states, double *scratch_M, double *new_log_nu_K_i)
+    void _iterate_fi(
+        double *log_nu_K_i, double *b_K_i, double *f_i, int *C_K_ij, int n_therm_states,
+        int n_markov_states, double *scratch_TM, double *new_f_i)
+    void _get_p(
+        double *log_nu_i, double *b_i, double *f_i, int *C_ij,
+        int n_markov_states, double *scratch_M, double *p_ij)
+    void _get_fk(
+        double *b_K_i, double *f_i, int n_therm_states, int n_markov_states,
+        double *scratch_M, double *f_K)
+    void _normalize(
+        double *f_K, double *f_i, int n_therm_states, int n_markov_states, double *scratch_M)
+
+def set_lognu(
+    _np.ndarray[double, ndim=2, mode="c"] log_nu_K_i not None,
+    _np.ndarray[int, ndim=3, mode="c"] C_K_ij not None):
+    r"""
+    Set the logarithm of the Lagrangian multipliers with an initial guess based
+    on the transition counts
+
+    Parameters
+    ----------
+    log_nu_K_i : numpy.ndarray(shape=(T, M), dtype=numpy.float64)
+        log of the Lagrangian multipliers (allocated but unset)
+    C_K_ij : numpy.ndarray(shape=(T, M, M), dtype=numpy.intc)
+        multistate count matrix
+    """
+    _set_lognu(
+        <double*> _np.PyArray_DATA(log_nu_K_i),
+        <int*> _np.PyArray_DATA(C_K_ij),
+        log_nu_K_i.shape[0],
+        log_nu_K_i.shape[1])
+
+def iterate_lognu(
+    _np.ndarray[double, ndim=2, mode="c"] log_nu_K_i not None,
+    _np.ndarray[double, ndim=2, mode="c"] b_K_i not None,
+    _np.ndarray[double, ndim=1, mode="c"] f_i not None,
+    _np.ndarray[int, ndim=3, mode="c"] C_K_ij not None,
+    _np.ndarray[double, ndim=1, mode="c"] scratch_M not None,
+    _np.ndarray[double, ndim=2, mode="c"] new_log_nu_K_i not None):
+    r"""
+    Update the logarithms of the Lagrangian multipliers
+
+    Parameters
+    ----------
+    log_nu_K_i : numpy.ndarray(shape=(T, M), dtype=numpy.float64)
+        log of the Lagrangian multipliers
+    b_K_i : numpy.ndarray(shape=(T, M), dtype=numpy.intc)
+        reduced bias energies of the T thermodynamic and M markov states
+    f_i : numpy.ndarray(shape=(M,), dtype=numpy.float64)
+        reduced unbiased free energies
+    C_K_ij : numpy.ndarray(shape=(T, M, M), dtype=numpy.intc)
+        multistate count matrix
+    scratch_M : numpy.ndarray(shape=(M,), dtype=numpy.float64)
+        scratch array for logsumexp operations
+    new_log_nu_K_i : numpy.ndarray(shape=(T, M), dtype=numpy.float64)
+        target array for the log of the Lagrangian multipliers
+    """
+    _iterate_lognu(
+        <double*> _np.PyArray_DATA(log_nu_K_i),
+        <double*> _np.PyArray_DATA(b_K_i),
+        <double*> _np.PyArray_DATA(f_i),
+        <int*> _np.PyArray_DATA(C_K_ij),
+        log_nu_K_i.shape[0],
+        log_nu_K_i.shape[1],
+        <double*> _np.PyArray_DATA(scratch_M),
+        <double*> _np.PyArray_DATA(new_log_nu_K_i))
+
+def iterate_fi(
+    _np.ndarray[double, ndim=2, mode="c"] log_nu_K_i not None,
+    _np.ndarray[double, ndim=2, mode="c"] b_K_i not None,
+    _np.ndarray[double, ndim=1, mode="c"] f_i not None,
+    _np.ndarray[int, ndim=3, mode="c"] C_K_ij not None,
+    _np.ndarray[double, ndim=2, mode="c"] scratch_TM not None,
+    _np.ndarray[double, ndim=1, mode="c"] new_f_i not None):
+    r"""
+    Update the reduced unbiased free energies
+
+    Parameters
+    ----------
+    log_nu_K_i : numpy.ndarray(shape=(T, M), dtype=numpy.float64)
+        log of the Lagrangian multipliers
+    b_K_i : numpy.ndarray(shape=(T, M), dtype=numpy.intc)
+        reduced bias energies of the T thermodynamic and M markov states
+    f_i : numpy.ndarray(shape=(M,), dtype=numpy.float64)
+        reduced unbiased free energies
+    C_K_ij : numpy.ndarray(shape=(T, M, M), dtype=numpy.intc)
+        multistate count matrix
+    scratch_TM : numpy.ndarray(shape=(T, M), dtype=numpy.float64)
+        scratch array for logsumexp operations
+    new_f_i : numpy.ndarray(shape=(M,), dtype=numpy.float64)
+        target array for the reduced unbiased free energies
+    """
+    _iterate_fi(
+        <double*> _np.PyArray_DATA(log_nu_K_i),
+        <double*> _np.PyArray_DATA(b_K_i),
+        <double*> _np.PyArray_DATA(f_i),
+        <int*> _np.PyArray_DATA(C_K_ij),
+        log_nu_K_i.shape[0],
+        log_nu_K_i.shape[1],
+        <double*> _np.PyArray_DATA(scratch_TM),
+        <double*> _np.PyArray_DATA(new_f_i))
+
+def get_pk(
+    _np.ndarray[double, ndim=2, mode="c"] log_nu_K_i not None,
+    _np.ndarray[double, ndim=2, mode="c"] b_K_i not None,
+    _np.ndarray[double, ndim=1, mode="c"] f_i not None,
+    _np.ndarray[int, ndim=3, mode="c"] C_K_ij not None,
+    _np.ndarray[double, ndim=1, mode="c"] scratch_M not None):
+    r"""
+    Compute the transition matrices for all thermodynamic states
+
+    Parameters
+    ----------
+    log_nu_K_i : numpy.ndarray(shape=(T, M), dtype=numpy.float64)
+        log of the Lagrangian multipliers
+    b_K_i : numpy.ndarray(shape=(T, M), dtype=numpy.intc)
+        reduced bias energies of the T thermodynamic and M markov states
+    f_i : numpy.ndarray(shape=(M,), dtype=numpy.float64)
+        reduced unbiased free energies
+    C_K_ij : numpy.ndarray(shape=(T, M, M), dtype=numpy.intc)
+        multistate count matrix
+    scratch_M : numpy.ndarray(shape=(M,), dtype=numpy.float64)
+        scratch array for logsumexp operations
+
+    Returns
+    -------
+    p_K_ij : numpy.ndarray(shape=(T, M, M), dtype=numpy.float64)
+        transition matrices for all thermodynamic states
+    """
+    p_K_ij = _np.zeros(shape=(C_K_ij.shape[0], C_K_ij.shape[1], C_K_ij.shape[2]), dtype=_np.float64)
+    for K in range(log_nu_K_i.shape[0]):
+        p_K_ij[K, :, :] = get_p(log_nu_K_i, b_K_i, f_i, C_K_ij, scratch_M, K)[:, :]
+    return p_K_ij
+
+def get_p(
+    _np.ndarray[double, ndim=2, mode="c"] log_nu_K_i not None,
+    _np.ndarray[double, ndim=2, mode="c"] b_K_i not None,
+    _np.ndarray[double, ndim=1, mode="c"] f_i not None,
+    _np.ndarray[int, ndim=3, mode="c"] C_K_ij not None,
+    _np.ndarray[double, ndim=1, mode="c"] scratch_M not None,
+    therm_state):
+    r"""
+    Compute the transition matrices for all thermodynamic states
+
+    Parameters
+    ----------
+    log_nu_K_i : numpy.ndarray(shape=(T, M), dtype=numpy.float64)
+        log of the Lagrangian multipliers
+    b_K_i : numpy.ndarray(shape=(T, M), dtype=numpy.intc)
+        reduced bias energies of the T thermodynamic and M markov states
+    f_i : numpy.ndarray(shape=(M,), dtype=numpy.float64)
+        reduced unbiased free energies
+    C_K_ij : numpy.ndarray(shape=(T, M, M), dtype=numpy.intc)
+        multistate count matrix
+    scratch_M : numpy.ndarray(shape=(M,), dtype=numpy.float64)
+        scratch array for logsumexp operations
+    therm_state : int
+        target thermodynamic state
+
+    Returns
+    -------
+    p_ij : numpy.ndarray(shape=(M, M), dtype=numpy.float64)
+        transition matrix for the target thermodynamic state
+    """
+    p_ij = _np.zeros(shape=(f_i.shape[0], f_i.shape[0]), dtype=_np.float64)
+    _get_p(
+        <double*> _np.PyArray_DATA(_np.ascontiguousarray(log_nu_K_i[therm_state, :])),
+        <double*> _np.PyArray_DATA(_np.ascontiguousarray(b_K_i[therm_state, :])),
+        <double*> _np.PyArray_DATA(f_i),
+        <int*> _np.PyArray_DATA(_np.ascontiguousarray(C_K_ij[therm_state, :, :])),
+        f_i.shape[0],
+        <double*> _np.PyArray_DATA(scratch_M),
+        <double*> _np.PyArray_DATA(p_ij))
+    return p_ij
+
+def get_fk(
+    _np.ndarray[double, ndim=2, mode="c"] b_K_i not None,
+    _np.ndarray[double, ndim=1, mode="c"] f_i not None,
+    _np.ndarray[double, ndim=1, mode="c"] scratch_M not None):
+    r"""
+    Compute the transition matrices for all thermodynamic states
+
+    Parameters
+    ----------
+    b_K_i : numpy.ndarray(shape=(T, M), dtype=numpy.intc)
+        reduced bias energies of the T thermodynamic and M markov states
+    f_i : numpy.ndarray(shape=(M,), dtype=numpy.float64)
+        reduced unbiased free energies
+    scratch_M : numpy.ndarray(shape=(M,), dtype=numpy.float64)
+        scratch array for logsumexp operations
+
+    Returns
+    -------
+    f_K : numpy.ndarray(shape=(T,), dtype=numpy.float64)
+        reduced thermodynamic free energies
+    """
+    f_K = _np.zeros(shape=(b_K_i.shape[0],), dtype=_np.float64)
+    _get_fk(
+        <double*> _np.PyArray_DATA(b_K_i),
+        <double*> _np.PyArray_DATA(f_i),
+        b_K_i.shape[0],
+        b_K_i.shape[1],
+        <double*> _np.PyArray_DATA(scratch_M),
+        <double*> _np.PyArray_DATA(f_K))
+    return f_K
+
+def normalize(
+    _np.ndarray[double, ndim=1, mode="c"] f_K not None,
+    _np.ndarray[double, ndim=1, mode="c"] f_i not None,
+    _np.ndarray[double, ndim=1, mode="c"] scratch_M not None):
+    r"""
+    Normalize the unbiased reduced free energies and shift the reduced thermodynamic
+    free energies accordingly
+
+    Parameters
+    ----------
+    f_K : numpy.ndarray(shape=(T), dtype=numpy.intc)
+        reduced thermodynamic free energies
+    f_i : numpy.ndarray(shape=(M), dtype=numpy.float64)
+        reduced unbiased free energies
+    scratch_M : numpy.ndarray(shape=(M), dtype=numpy.float64)
+        scratch array for logsumexp operations
+    """
+    _normalize(
+        <double*> _np.PyArray_DATA(f_K),
+        <double*> _np.PyArray_DATA(f_i),
+        f_K.shape[0],
+        f_i.shape[0],
+        <double*> _np.PyArray_DATA(scratch_M))
+
+def estimate(C_K_ij, b_K_i, maxiter=1000, maxerr=1.0E-8, log_nu_K_i=None, f_i=None):
+    r"""
+    Estimate the unbiased reduced free energies and thermodynamic free energies
+        
+    Parameters
+    ----------
+    C_K_ij : numpy.ndarray(shape=(T, M, M), dtype=numpy.intc)
+        transition count matrices for all T thermodynamic states
+    b_K_i : numpy.ndarray(shape=(T, M), dtype=numpy.float64)
+        reduced bias energies in the T thermodynamic and M discrete states
+    maxiter : int
+        maximum number of iterations
+    maxerr : float
+        convergence criterion based on absolute change in free energies
+    f_i : numpy.ndarray(shape=(M), dtype=numpy.float64), OPTIONAL
+        initial guess for the reduced unbiased free energies of the M discrete states
+    log_nu_K_i : numpy.ndarray(shape=(T, M), dtype=numpy.float64), OPTIONAL
+        initial guess for the logarithm of the Lagrangian multipliers
+
+    Returns
+    -------
+    f_K : numpy.ndarray(shape=(T), dtype=numpy.float64)
+        reduced free energies of the T thermodynamic states
+    f_i : numpy.ndarray(shape=(M), dtype=numpy.float64)
+        reduced unbiased free energies of the M discrete states
+    log_nu_K_i : numpy.ndarray(shape=(T, M), dtype=numpy.float64)
+        logarithm of the Lagrangian multipliers
+    """
+    log_nu_K_i = _np.zeros(shape=b_K_i.shape, dtype=_np.float64)
+    f_i = _np.zeros(shape=b_K_i.shape[1], dtype=_np.float64)
+    set_lognu(log_nu_K_i, C_K_ij)
+    scratch_TM = _np.zeros(shape=b_K_i.shape, dtype=_np.float64)
+    scratch_M = _np.zeros(shape=f_i.shape, dtype=_np.float64)
+    old_log_nu_K_i = log_nu_K_i.copy()
+    old_f_i = f_i.copy()
+    for m in range(maxiter):
+        iterate_lognu(old_log_nu_K_i, b_K_i, f_i, C_K_ij, scratch_M, log_nu_K_i)
+        iterate_fi(log_nu_K_i, b_K_i, old_f_i, C_K_ij, scratch_TM, f_i)
+        delta_log_nu_K_i = _np.max(_np.abs((log_nu_K_i - old_log_nu_K_i)))
+        delta_f_i = _np.max(_np.abs((f_i - old_f_i)))
+        if delta_log_nu_K_i < maxerr and delta_f_i < maxerr:
+            stop = True
+        else:
+            old_log_nu_K_i[:] = log_nu_K_i[:]
+            old_f_i[:] = f_i[:]
+    f_K = get_fk(b_K_i, f_i, scratch_M)
+    normalize(f_K, f_i, scratch_M)
+    return f_K, f_i, log_nu_K_i
---
+++ packages/thermotools/thermotools-0.1.7.tar.gz/thermotools-0.1.7/ext/util/util.pyx
@@ -0,0 +1,135 @@
+# This file is part of thermotools.
+#
+# Copyright 2015 Computational Molecular Biology Group, Freie Universitaet Berlin (GER)
+#
+# thermotools is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+r"""
+Python interface to utility functions
+"""
+
+import numpy as _np
+cimport numpy as _np
+from scipy.sparse import csr_matrix as _csr
+from msmtools.estimation import count_matrix as _cm
+
+__all__ = ['count_matrices', 'state_counts']
+
+cdef extern from "_util.h":
+    int _get_therm_state_break_points(int *T_x, int seq_length, int *break_points)
+
+def get_therm_state_break_points(
+    _np.ndarray[int, ndim=1, mode="c"] T_x not None):
+    r"""
+    Find thermodynamic state changes within a trajectory
+
+    Parameters
+    ----------
+    T_x : numpy.ndarray(shape=(X), dtype=numpy.intc)
+        Thermodynamic state sequence of a trajectory of length X
+
+    Returns
+    -------
+    T_B : numpy.ndarray(shape=(B), dtype=numpy.intc)
+        Sequence of first subsequence starting frames
+    """
+    T_B = _np.zeros(shape=(T_x.shape[0],), dtype=_np.intc)
+    nb = _get_therm_state_break_points(
+        <int*> _np.PyArray_DATA(T_x),
+        T_x.shape[0],
+        <int*> _np.PyArray_DATA(T_B))
+    return _np.ascontiguousarray(T_B[:nb])
+
+def count_matrices(dtraj, lag, sliding=True, sparse_return=True, nthermo=None, nstates=None):
+    r"""
+    Count transitions at given lagtime
+
+    Parameters
+    ----------
+    dtraj : list of numpy.ndarray(shape=(X, 2), dtype=np.intc)
+        list of discretized trajectories
+    lag : int
+        lagtime in trajectory steps
+    sliding : bool, optional
+        if true the sliding window approach is used for transition counting
+    sparse_return : bool (optional)
+        whether to return a 3D dense matrix or a list 2D sparse matrices
+    nthermo : int, optional
+        enforce nthermo count-matrices with shape=(nstates, nstates)
+    nstates : int, optional
+        enforce count-matrices with shape=(nstates, nstates) for all thermodynamic states
+
+    Returns
+    -------
+    C_K : [scipy.sparse.coo_matrix] or numpy.ndarray(shape=(T, M, M))
+        count matrices at given lag in coordinate list format
+    """
+    kmax = _np.max([d[:, 0].max() for d in dtraj])
+    nmax = _np.max([d[:, 1].max() for d in dtraj])
+    if nthermo is None:
+        nthermo = kmax + 1
+    elif nthermo < kmax + 1:
+        raise ValueError("nthermo is smaller than the number of observed thermodynamic states")
+    if nstates is None:
+        nstates = nmax + 1
+    elif nstates < nmax + 1:
+        raise ValueError("nstates is smaller than the number of observed microstates")
+    C_K = [_csr((nstates, nstates), dtype=_np.intc)] * nthermo
+    for d in dtraj:
+        bp = get_therm_state_break_points(_np.ascontiguousarray(d[:, 0]))
+        for b in range(1, bp.shape[0]):
+            C_K[d[bp[b - 1], 0]] += _cm(
+                _np.ascontiguousarray(d[bp[b - 1]:bp[b], 1]), lag,
+                sliding=sliding, sparse_return=True, nstates=nstates)
+        C_K[d[bp[-1], 0]] += _cm(
+            _np.ascontiguousarray(d[bp[-1]:, 1]), lag,
+            sliding=sliding, sparse_return=True, nstates=nstates)
+    if sparse_return:
+        return C_K
+    return _np.array([C.todense() for C in C_K], dtype=_np.intc)
+
+def state_counts(dtraj, nstates=None, nthermo=None):
+    r"""
+    Count discrete states in all thermodynamic states
+
+    Parameters
+    ----------
+    dtraj : list of numpy.ndarray(shape=(X, 2), dtype=np.intc)
+        list of discretized trajectories
+    nstates : int, optional
+        enforce state count matrix with shape=(nthermo, nstates)
+    nthermo : int, optional
+        enforce state count matrix with shape=(nthermo, nstates)
+
+    Returns
+    -------
+    N : numpy.ndarray(shape=(T, M))
+        state counts
+    """
+    kmax = _np.max([d[:, 0].max() for d in dtraj])
+    nmax = _np.max([d[:, 1].max() for d in dtraj])
+    if nthermo is None:
+        nthermo = kmax + 1
+    elif nthermo < kmax + 1:
+        raise ValueError("nthermo is smaller than the number of observed thermodynamic states")
+    if nstates is None:
+        nstates = nmax + 1
+    elif nstates < nmax + 1:
+        raise ValueError("nstates is smaller than the number of observed microstates")
+    N = _np.zeros(shape=(nthermo, nstates), dtype=_np.intc)
+    for d in dtraj:
+        for K in range(nthermo):
+            for i in range(nstates):
+                N[K, i] += ((d[:, 0] == K) * (d[:, 1] == i)).sum()
+    return N
