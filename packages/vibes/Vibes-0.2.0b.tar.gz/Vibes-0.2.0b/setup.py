0e12bf7e1a3e6f92713e2d778bfa023f3c05ce26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:43 +0000

    Add package vibes/0.2.0b/Vibes-0.2.0b.tar.gz

---
+++ packages/vibes/Vibes-0.2.0b.tar.gz/Vibes-0.2.0b/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Vibes',
+      version='0.2.0b',
+      description='python interface Visualizer for Intervals and Boxes',
+      author='EnstaRobotics',
+      author_email='ben.ensta@gmail.com',
+      url='http://enstabretagnerobotics.github.io/VIBES/',
+      packages=['vibes'],
+     )
