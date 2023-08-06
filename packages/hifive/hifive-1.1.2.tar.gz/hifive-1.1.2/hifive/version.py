0e80ef9a8abe37f400a02eed8d55902c02c080b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:15 +0000

    Add package hifive/1.1.2/hifive-1.1.2.tar.gz

---
+++ packages/hifive/hifive-1.1.2.tar.gz/hifive-1.1.2/hifive/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM HIFIVE SETUP.PY
+short_version = '1.1.2'
+version = '1.1.2'
+full_version = '1.1.2'
+git_revision = 'b1b76152e0dd041dc4226588b275b199e36cfccf'
+release = True
+
+if not release:
+    version = full_version
