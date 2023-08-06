d61c844b53d4417f00d02c553b629229f01236c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:23 +0000

    Add package easy-menu/1.0.9/easy-menu-1.0.9.tar.gz

---
+++ packages/easy-menu/easy-menu-1.0.9.tar.gz/easy-menu-1.0.9/src/easy_menu/controller/command_executor.py
@@ -0,0 +1,19 @@
+from __future__ import division, print_function, absolute_import, unicode_literals
+
+from easy_menu.util import cmd_util
+
+
+class CommandExecutor(object):
+    def __init__(self, work_dir=None, logger=None):
+        self.work_dir = work_dir
+        self.logger = logger
+
+    def _log_info(self, msg):
+        if self.logger:
+            self.logger.info(msg)
+
+    def execute(self, cmd, stdin, stdout, stderr, encoding):
+        self._log_info('Command started: %s' % cmd)
+        ret_code = cmd_util.execute_command(cmd, self.work_dir, stdin, stdout, stderr, encoding)
+        self._log_info('Command ended with return code: %d' % ret_code)
+        return ret_code
