5dd5b8c7faa0e241b47a8bcf7466d045941fade8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:50 +0000

    Add package journal-brief/0.0.4/journal-brief-0.0.4.tar.gz

---
+++ packages/journal-brief/journal-brief-0.0.4.tar.gz/journal-brief-0.0.4/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Copyright (c) 2015 Tim Waugh <tim@cyberelk.net>
+
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='journal-brief',
+    version='0.0.4',  # also update journal_brief/__init__.py
+    description='Show new journal entries since last run',
+    author='Tim Waugh',
+    author_email='tim@cyberelk.net',
+    url='https://github.com/twaugh/journal-brief',
+    license="GPLv2+",
+    entry_points={
+        'console_scripts': ['journal-brief=journal_brief.cli.main:run'],
+    },
+    install_requires=['PyYAML'],
+    packages=find_packages(),
+    package_data={'': ['conf/journal-brief.conf']},
+)
+
---
+++ packages/journal-brief/journal-brief-0.0.4.tar.gz/journal-brief-0.0.4/tests/test_filter.py
@@ -0,0 +1,130 @@
+"""
+Copyright (c) 2015 Tim Waugh <tim@cyberelk.net>
+
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+"""
+
+from flexmock import flexmock
+from journal_brief import JournalFilter
+from journal_brief.filter import Exclusion
+import logging
+from systemd import journal
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+class TestExclusion(object):
+    def test_and(self):
+        exclusion = Exclusion({'MESSAGE': ['exclude this'],
+                               'SYSLOG_IDENTIFIER': ['from this']})
+        assert exclusion.matches({'MESSAGE': 'exclude this',
+                                  'SYSLOG_IDENTIFIER': 'from this',
+                                  'IGNORE': 'ignore this'})
+        assert not exclusion.matches({'MESSAGE': 'exclude this'})
+
+    def test_or(self):
+        exclusion = Exclusion({'MESSAGE': ['exclude this', 'or this']})
+        assert exclusion.matches({'MESSAGE': 'exclude this',
+                                  'IGNORE': 'ignore this'})
+        assert not exclusion.matches({'MESSAGE': 'not this',
+                                      'IGNORE': 'ignore this'})
+
+    def test_and_or(self):
+        exclusion = Exclusion({'MESSAGE': ['exclude this', 'or this'],
+                               'SYSLOG_IDENTIFIER': ['from this']})
+        assert exclusion.matches({'MESSAGE': 'exclude this',
+                                  'SYSLOG_IDENTIFIER': 'from this',
+                                  'IGNORE': 'ignore this'})
+        assert not exclusion.matches({'MESSAGE': 'exclude this',
+                                      'SYSLOG_IDENTIFIER': 'at your peril',
+                                      'IGNORE': 'ignore this'})
+
+
+class TestJournalFilter(object):
+    def test_no_exclusions(self):
+        entries = [{'MESSAGE': 'message 1'},
+                   {'MESSAGE': 'message 2'}]
+        (flexmock(journal.Reader)
+            .should_receive('get_next')
+            .and_return(entries[0])
+            .and_return(entries[1])
+            .and_return({}))
+
+        jfilter = JournalFilter(journal.Reader())
+        assert list(jfilter) == entries
+
+    def test_exclusion(self):
+        entries = [{'MESSAGE': 'exclude this',
+                    'SYSLOG_IDENTIFIER': 'from here'},
+
+                   {'MESSAGE': 'message 1',
+                    'SYSLOG_IDENTIFIER': 'foo'},
+
+                   {'MESSAGE': 'exclude this',
+                    'SYSLOG_IDENTIFIER': 'at your peril'},
+
+                   {'MESSAGE': 'message 2'}]
+
+        (flexmock(journal.Reader)
+            .should_receive('get_next')
+            .and_return(entries[0])
+            .and_return(entries[1])
+            .and_return(entries[2])
+            .and_return(entries[3])
+            .and_return({}))
+        exclusions = [{'MESSAGE': ['exclude this',
+                                   'and this'],
+                       'SYSLOG_IDENTIFIER': ['from here']}]
+        jfilter = JournalFilter(journal.Reader(), exclusions=exclusions)
+        assert list(jfilter) == entries[1:]
+
+    def test_exclusion_regexp(self):
+        entries = [{'MESSAGE': 'exclude this'},
+                   {'MESSAGE': 'message 1'},
+                   {'MESSAGE': 'exclude that'},
+                   {'MESSAGE': 'message 2'}]
+
+        (flexmock(journal.Reader)
+            .should_receive('get_next')
+            .and_return(entries[0])
+            .and_return(entries[1])
+            .and_return(entries[2])
+            .and_return(entries[3])
+            .and_return({}))
+        exclusions = [{'MESSAGE': ['/1/']},  # shouldn't exclude anything
+                      {'MESSAGE': ['/exclude th/']},
+                      {'MESSAGE': ['/exclude/']}]
+        jfilter = JournalFilter(journal.Reader(), exclusions=exclusions)
+        assert list(jfilter) == [entries[1]] + [entries[3]]
+        stats = jfilter.get_statistics()
+        for stat in stats:
+            if stat.exclusion['MESSAGE'] == ['/1/']:
+                assert stat.hits == 0
+                break
+
+    def test_statistics(self):
+        (flexmock(journal.Reader)
+            .should_receive('get_next')
+            .and_return({'MESSAGE': 'exclude'})
+            .and_return({'MESSAGE': 'include'})
+            .and_return({}))
+
+        exclusions = [{'MESSAGE': ['exclude']}]
+        jfilter = JournalFilter(journal.Reader(), exclusions=exclusions)
+        list(jfilter)
+        statistics = jfilter.get_statistics()
+        assert len(statistics) == 1
+        assert statistics[0].hits == 1
