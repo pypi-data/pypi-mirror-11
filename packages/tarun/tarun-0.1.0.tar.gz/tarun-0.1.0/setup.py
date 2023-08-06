0b28c21e89db06c5e40431461111a894343c4540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:24 +0000

    Add package tarun/0.1.0/tarun-0.1.0.tar.gz

---
+++ packages/tarun/tarun-0.1.0.tar.gz/tarun-0.1.0/setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup
+
+
+setup(
+    name='tarun',
+    version='0.1.0',
+    packages=['tarun'],
+    url='https://github.com/wangyu190810/tarun',
+    license='MIT',
+    author='22too',
+    entry_points={
+            "console_scripts":"""
+                tarun = tarun.run:main
+            """,
+        },
+    author_email='wo190810401@gmail.com',
+    description='Extract file'
+)
