993855ce50da3393de306e42f7d8ac7e64a6a384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:15 +0000

    Add package openjudge/0.2.4/openjudge-0.2.4.tar.gz

---
+++ packages/openjudge/openjudge-0.2.4.tar.gz/openjudge-0.2.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+import openjudge
+
+version = '.'.join(map(str, openjudge.__version__))
+with open('README.md', 'r') as f:
+    long_desc = f.read()
+setup(name='openjudge',
+      version=version,
+      description='LAN programming judge',
+      long_description=long_desc,
+      url='https://github.com/theSage21/openJudge',
+      author='Arjoonn Sharma',
+      author_email='Arjoonn Sharma',
+      license='MIT',
+      packages=['openjudge'],
+      zip_safe=False,
+      include_package_data=True,
+      classifiers=['Development Status :: 4 - Beta',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python :: 3.4',
+                   ],
+      keywords='judge programming check proof openjudge'.split(' ')
+      )
