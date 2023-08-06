88029716da8d60b110fad91f79bfe28ff0ef90ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:11 +0000

    Add package jampack/0.9.1/jampack-0.9.1.tar.gz

---
+++ packages/jampack/jampack-0.9.1.tar.gz/jampack-0.9.1/lib/jampack/settings.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ------------------------------------------------------------------------------
+# Application Name
+# ------------------------------------------------------------------------------
+app_name = 'jampack'
+
+# ------------------------------------------------------------------------------
+# Version Number
+# ------------------------------------------------------------------------------
+major_version = "0"
+minor_version = "9"
+patch_version = "1"
+
+# ------------------------------------------------------------------------------
+# Debug Flag (switch to False for production release code)
+# ------------------------------------------------------------------------------
+debug = False
+
+# ------------------------------------------------------------------------------
+# Usage String
+# ------------------------------------------------------------------------------
+usage = """
+jam [command] [directory path]
+unjam [archive path]
+
+Use 'jam help' or 'unjam help' for additional information
+"""
+
+# ------------------------------------------------------------------------------
+# Help String
+# ------------------------------------------------------------------------------
+help = """
+-------------------------------------------------
+ JamPack
+ Simple file archives for everyone
+ Copyright 2015 Christopher Simpkins
+ MIT license
+ Source: http://github.com/chrissimpkins/jampack
+-------------------------------------------------
+
+ Archive and Compress Directory
+ ==============================
+
+   jam [zip|bz] [directory path]
+
+ The default archive type is a tar.gz compressed archive if you do not include a secondary command.  The default path is the current working directory if you do not include a directory path.
+
+ Decompress and Unpack Archive
+ =============================
+
+   unjam <archive path>
+
+ Include the archive path as the argument for this command.  The archive type is interpreted from the file extension.
+"""
