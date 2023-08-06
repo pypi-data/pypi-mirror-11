b3da80c0025fe5d71350c7d27e4f66986cbfa162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:27 +0000

    Add package qcbacktester/0.1/qcbacktester-0.1.tar.gz

---
+++ packages/qcbacktester/qcbacktester-0.1.tar.gz/qcbacktester-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='qcbacktester',
+      version='0.1',
+      description='The qcbacktester is the high performance trading engine that runs behind quantcollab',
+      url='http://github.com/ishandutta2007/qcbacktester',
+      author='Ishan',
+      author_email='ishandutta2007@gmail.com',
+      license='MIT',
+      packages=['qcbacktester'],
+      zip_safe=False)
