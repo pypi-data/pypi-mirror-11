6138686b22d7ad02c5425fd9c7a2b96996135692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:00 +0000

    Add package rede/1.2/rede-1.2.tar.gz

---
+++ packages/rede/rede-1.2.tar.gz/rede-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name = 'rede',
+      version = '1.2',
+      description = 'Funcionalidades para representacao da rede eletrica utilizando grafos com representacao no-profundidade',
+      install_requires = ['numpy==1.8.2', 'terminaltables'],
+      author = 'Lucas S Melo',
+      packages = find_packages(),
+      )
