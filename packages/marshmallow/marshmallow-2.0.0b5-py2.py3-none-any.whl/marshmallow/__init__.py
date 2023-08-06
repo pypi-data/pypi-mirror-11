a9a182f06e5d0bb624c53cac746d62b01b601bfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:56 +0000

    Add package marshmallow/2.0.0b5/marshmallow-2.0.0b5-py2.py3-none-any.whl

---
+++ packages/marshmallow/marshmallow-2.0.0b5-py2.py3-none-any.whl/marshmallow/__init__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from marshmallow.schema import (
+    Schema,
+    SchemaOpts,
+    MarshalResult,
+    UnmarshalResult,
+)
+from marshmallow.decorators import (
+    pre_dump, post_dump, pre_load, post_load, validates, validates_schema
+)
+from marshmallow.utils import pprint, missing
+from marshmallow.exceptions import MarshallingError, UnmarshallingError, ValidationError
+
+__version__ = '2.0.0b5'
+__author__ = 'Steven Loria'
+__license__ = 'MIT'
+
+__all__ = [
+    'Schema',
+    'SchemaOpts',
+    'validates',
+    'validates_schema',
+    'pre_dump',
+    'post_dump',
+    'pre_load',
+    'post_load',
+    'pprint',
+    'MarshalResult',
+    'UnmarshalResult',
+    'MarshallingError',
+    'UnmarshallingError',
+    'ValidationError',
+    'missing',
+]
