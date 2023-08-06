e1e7c6a515b5a5ff86f949fd101933456a660334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:20 +0000

    Add package junebug/0.0.2/junebug-0.0.2-py2-none-any.whl

---
+++ packages/junebug/junebug-0.0.2-py2-none-any.whl/junebug/tests/helpers.py
@@ -0,0 +1,152 @@
+from copy import deepcopy
+import logging
+from twisted.internet import reactor
+from twisted.internet.defer import inlineCallbacks, returnValue
+from twisted.trial.unittest import TestCase
+from twisted.web.server import Site
+from txamqp.client import TwistedDelegate
+from vumi.utils import vumi_resource_path
+from vumi.service import get_spec
+from vumi.tests.fake_amqp import FakeAMQPBroker, FakeAMQPChannel
+from vumi.tests.helpers import PersistenceHelper, WorkerHelper
+
+from junebug import JunebugApi
+from junebug.amqp import JunebugAMQClient, MessageSender
+from junebug.channel import Channel
+from junebug.service import JunebugService
+
+
+class FakeAmqpClient(JunebugAMQClient):
+    '''Amqp client, base upon the real JunebugAMQClient, that uses a
+    FakeAMQPBroker instead of a real broker'''
+    def __init__(self, spec):
+        super(FakeAmqpClient, self).__init__(TwistedDelegate(), '', spec)
+        self.broker = FakeAMQPBroker()
+
+    @inlineCallbacks
+    def channel(self, id):
+        yield self.channelLock.acquire()
+        try:
+            try:
+                ch = self.channels[id]
+            except KeyError:
+                ch = FakeAMQPChannel(id, self)
+                self.channels[id] = ch
+        finally:
+            self.channelLock.release()
+        returnValue(ch)
+
+
+class JunebugTestBase(TestCase):
+    '''Base test case that all junebug tests inherit from. Contains useful
+    helper functions'''
+
+    default_channel_config = {
+        'type': 'telnet',
+        'config': {
+            'transport_name': 'dummy_transport1',
+            'twisted_endpoint': 'tcp:0',
+        },
+        'mo_url': 'http://foo.bar',
+    }
+
+    def patch_logger(self):
+        ''' Patches the logger with an in-memory logger, which is acccessable
+        at "self.logging_handler".'''
+        self.logging_handler = logging.handlers.MemoryHandler(100)
+        logging.getLogger().addHandler(self.logging_handler)
+        self.addCleanup(self._cleanup_logging_patch)
+
+    def _cleanup_logging_patch(self):
+        self.logging_handler.close()
+        logging.getLogger().removeHandler(self.logging_handler)
+
+    @inlineCallbacks
+    def create_channel(
+            self, service, redis, transport_class,
+            config=default_channel_config, id=None):
+        '''Creates and starts, and saves a channel, with a
+        TelnetServerTransport transport'''
+        config = deepcopy(config)
+        channel = Channel(redis, {}, config, id=id)
+        config['config']['transport_name'] = channel.id
+        wh = WorkerHelper()
+        transport_worker = yield wh.get_worker(
+            transport_class, config['config'])
+        self.addCleanup(wh.cleanup)
+        yield channel.start(self.service, transport_worker)
+        yield channel.save()
+        self.addCleanup(channel.stop)
+        returnValue(channel)
+
+    def create_channel_from_id(self, service, redis, id):
+        '''Creates an existing channel given the channel id'''
+        return Channel.from_id(redis, {}, id, service)
+
+    @inlineCallbacks
+    def get_redis(self):
+        '''Creates and returns a redis manager'''
+        if hasattr(self, 'redis'):
+            returnValue(self.redis)
+        persistencehelper = PersistenceHelper()
+        yield persistencehelper.setup()
+        self.redis = yield persistencehelper.get_redis_manager()
+        self.addCleanup(persistencehelper.cleanup)
+        returnValue(self.redis)
+
+    @inlineCallbacks
+    def start_server(self):
+        '''Starts a junebug server. Stores the service to "self.service", and
+        the url at "self.url"'''
+        redis = yield self.get_redis()
+        self.service = JunebugService('127.0.0.1', 0, redis._config, {})
+        self.api = JunebugApi(
+            self.service, redis._config, {'hostname': '', 'port': ''})
+        self.api.redis = redis
+
+        self.api.message_sender = self.get_message_sender()
+
+        port = reactor.listenTCP(
+            0, Site(self.api.app.resource()),
+            interface='127.0.0.1')
+        self.addCleanup(port.stopListening)
+        addr = port.getHost()
+        self.url = "http://%s:%s" % (addr.host, addr.port)
+
+    def get_message_sender(self):
+        '''Creates a new MessageSender object, with a fake amqp client'''
+        message_sender = MessageSender('amqp-spec-0-8.xml', None)
+        spec = get_spec(vumi_resource_path('amqp-spec-0-8.xml'))
+        client = FakeAmqpClient(spec)
+        message_sender.client = client
+        return message_sender
+
+    @inlineCallbacks
+    def patch_worker_creation(
+            self, transport_class, config=default_channel_config):
+        '''Patches the channel start function to start a worker with the given
+        worker class and config using a worker helper.'''
+        worker_helper = WorkerHelper()
+        transport_worker = yield worker_helper.get_worker(
+            transport_class, config['config'])
+        yield transport_worker.startService()
+        self.addCleanup(transport_worker.stopService)
+        self._original_channel_start = old_start = Channel.start
+
+        def new_start(self, service):
+            return old_start(
+                self, service, transport_worker)
+
+        Channel.start = new_start
+        self.addCleanup(self._unpatch_worker_creation)
+
+    def _unpatch_worker_creation(self):
+        Channel.start = self._original_channel_start
+
+    def get_dispatched_messages(self, queue):
+        '''Gets all messages that have been dispatched to the amqp broker.
+        Should only be called after start_server, as it looks in the api for
+        the amqp client'''
+        amqp_client = self.api.message_sender.client
+        return amqp_client.broker.get_messages(
+            'vumi', queue)
---
+++ packages/junebug/junebug-0.0.2-py2-none-any.whl/junebug/tests/test_channel.py
@@ -0,0 +1,208 @@
+from copy import deepcopy
+import json
+from twisted.internet.defer import inlineCallbacks
+from vumi.message import TransportUserMessage
+from vumi.transports.telnet import TelnetServerTransport
+
+from junebug.channel import Channel, ChannelNotFound, InvalidChannelType
+from junebug.tests.helpers import JunebugTestBase
+
+
+class TestChannel(JunebugTestBase):
+    @inlineCallbacks
+    def setUp(self):
+        self.patch_logger()
+
+        self.redis = yield self.get_redis()
+        yield self.start_server()
+
+    @inlineCallbacks
+    def test_save_channel(self):
+        channel = yield self.create_channel(
+            self.service, self.redis, TelnetServerTransport)
+        properties = yield self.redis.get('%s:properties' % channel.id)
+        expected = deepcopy(self.default_channel_config)
+        expected['config']['transport_name'] = channel.id
+        self.assertEqual(json.loads(properties), expected)
+
+    @inlineCallbacks
+    def test_delete_channel(self):
+        channel = yield self.create_channel(
+            self.service, self.redis, TelnetServerTransport)
+        properties = yield self.redis.get('%s:properties' % channel.id)
+        expected = deepcopy(self.default_channel_config)
+        expected['config']['transport_name'] = channel.id
+        self.assertEqual(json.loads(properties), expected)
+
+        yield channel.delete()
+        properties = yield self.redis.get('%s:properties' % channel.id)
+        self.assertEqual(properties, None)
+
+    @inlineCallbacks
+    def test_start_channel(self):
+        channel = yield self.create_channel(
+            self.service, self.redis, TelnetServerTransport)
+        self.assertEqual(
+            self.service.namedServices[channel.id], channel.transport_worker)
+
+    @inlineCallbacks
+    def test_create_channel_invalid_type(self):
+        channel = yield self.create_channel(
+            self.service, self.redis, TelnetServerTransport)
+        channel._properties['type'] = 'foo'
+        err = self.assertRaises(InvalidChannelType, channel.start, None)
+        self.assertTrue(all(
+            s in err.message for s in ('xmpp', 'telnet', 'foo')))
+
+    @inlineCallbacks
+    def test_update_channel(self):
+        channel = yield self.create_channel(
+            self.service, self.redis, TelnetServerTransport)
+        self.assertEqual(
+            self.service.namedServices[channel.id], channel.transport_worker)
+
+        update = yield channel.update({'foo': 'bar'})
+        expected = deepcopy(self.default_channel_config)
+        expected.update({
+            'foo': 'bar',
+            'status': {},
+            'id': channel.id,
+            })
+        expected['config']['transport_name'] = channel.id
+        self.assertEqual(update, expected)
+
+    @inlineCallbacks
+    def test_stop_channel(self):
+        channel = yield self.create_channel(
+            self.service, self.redis, TelnetServerTransport)
+        self.assertEqual(
+            self.service.namedServices[channel.id], channel.transport_worker)
+
+        yield channel.stop()
+        self.assertEqual(self.service.namedServices.get(channel.id), None)
+
+    @inlineCallbacks
+    def test_create_channel_from_id(self):
+        channel1 = yield self.create_channel(
+            self.service, self.redis, TelnetServerTransport)
+
+        channel2 = yield self.create_channel_from_id(
+            self.service, self.redis, channel1.id)
+        self.assertEqual((yield channel1.status()), (yield channel2.status()))
+
+    @inlineCallbacks
+    def test_create_channel_from_unknown_id(self):
+        yield self.assertFailure(
+            self.create_channel_from_id(
+                self.service, self.redis, 'unknown-id'),
+            ChannelNotFound)
+
+    @inlineCallbacks
+    def test_channel_status(self):
+        channel = yield self.create_channel(
+            self.service, self.redis, TelnetServerTransport, id='channel-id')
+        expected = deepcopy(self.default_channel_config)
+        expected.update({
+            'id': 'channel-id',
+            'status': {},
+            })
+        expected['config']['transport_name'] = channel.id
+        self.assertEqual((yield channel.status()), expected)
+
+    @inlineCallbacks
+    def test_get_all_channels(self):
+        channels = yield Channel.get_all(self.redis)
+        self.assertEqual(channels, set())
+
+        channel1 = yield self.create_channel(
+            self.service, self.redis, TelnetServerTransport)
+        channels = yield Channel.get_all(self.redis)
+        self.assertEqual(channels, set([channel1.id]))
+
+        channel2 = yield self.create_channel(
+            self.service, self.redis, TelnetServerTransport)
+        channels = yield Channel.get_all(self.redis)
+        self.assertEqual(channels, set([channel1.id, channel2.id]))
+
+    @inlineCallbacks
+    def test_convert_unicode(self):
+        channel = yield self.create_channel(
+            self.service, self.redis, TelnetServerTransport, id='channel-id')
+        resp = channel._convert_unicode({
+            u'both': u'unicode',
+            u'key': 'unicode',
+            'value': u'unicode',
+            'nested': {
+                u'unicode': u'nested'
+                },
+            })
+        for key, value in resp.iteritems():
+            self.assertTrue(isinstance(key, str))
+            if not isinstance(value, dict):
+                self.assertTrue(isinstance(value, str))
+        for key, value in resp['nested'].iteritems():
+            self.assertTrue(isinstance(key, str))
+            self.assertTrue(isinstance(value, str))
+
+        self.assertTrue(isinstance(channel._convert_unicode(1), int))
+
+    @inlineCallbacks
+    def test_send_message(self):
+        '''The send_message function should place the message on the correct
+        queue'''
+        channel = yield self.create_channel(
+            self.service, self.redis, TelnetServerTransport, id='channel-id')
+        msg = yield channel.send_message(
+            self.api.message_sender, {
+                'from': '+1234',
+                'content': 'testcontent',
+            })
+        self.assertEqual(msg['channel_id'], 'channel-id')
+        self.assertEqual(msg['from'], '+1234')
+        self.assertEqual(msg['content'], 'testcontent')
+
+        [dispatched_message] = self.get_dispatched_messages(
+            'channel-id.outbound')
+        self.assertEqual(msg['message_id'], dispatched_message['message_id'])
+
+    @inlineCallbacks
+    def test_api_from_message(self):
+        '''The api from message function should take a vumi message, and
+        return a dict with the appropriate values'''
+        channel = yield self.create_channel(
+            self.service, self.redis, TelnetServerTransport, id='channel-id')
+        message = TransportUserMessage.send(
+            content=None, from_addr='+1234', to_addr='+5432',
+            transport_name='testtransport', continue_session=True,
+            helper_metadata={'voice': {}})
+        dct = channel._api_from_message(message)
+        [dct.pop(f) for f in ['timestamp', 'message_id']]
+        self.assertEqual(dct, {
+            'channel_data': {
+                'continue_session': True,
+                'voice': {},
+                },
+            'from': '+1234',
+            'to': '+5432',
+            'channel_id': 'testtransport',
+            'content': None,
+            'reply_to': None,
+            })
+
+    @inlineCallbacks
+    def test_message_from_api(self):
+        channel = yield self.create_channel(
+            self.service, self.redis, TelnetServerTransport, id='channel-id')
+        msg = channel._message_from_api({
+            'from': '+1234',
+            'content': None,
+            'channel_data': {
+                'continue_session': True,
+                'voice': {},
+                },
+            })
+        msg = TransportUserMessage.send(**msg)
+        self.assertEqual(msg.get('continue_session'), True)
+        self.assertEqual(msg.get('helper_metadata'), {'voice': {}})
+        self.assertEqual(msg.get('from_addr'), '+1234')
+        self.assertEqual(msg.get('content'), None)
