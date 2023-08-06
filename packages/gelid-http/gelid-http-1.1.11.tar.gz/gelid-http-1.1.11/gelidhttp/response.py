a35fa563814fbbb3e45d3d00fd908a1c0d5cc6d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:31 +0000

    Add package gelid-http/1.1.11/gelid-http-1.1.11.tar.gz

---
+++ packages/gelid-http/gelid-http-1.1.11.tar.gz/gelid-http-1.1.11/gelidhttp/response.py
@@ -0,0 +1,43 @@
+# -*- coding: UTF-8 -*-
+# lvjiyong on 2015/6/7.
+
+"""
+封装了Request输出的基本内容格式
+"""
+
+__all__ = ['Response']
+
+from gelidhttp.parameter import Parameter
+
+
+class Response(Parameter):
+    """
+    封装输出
+    >>> response = Response(url='http://www.baidu.com')
+    >>> response.url
+    'http://www.baidu.com'
+    >>> response = Response('http://www.baidu.com', encoding='gbk')
+    >>> response.encoding
+    'gbk'
+    >>> response.body_as_unicode()
+
+    """
+
+    def __init__(self, seq=None, **kwargs):
+        super(Response, self).__init__(**kwargs)
+        self.encoding = kwargs.get('encoding') or 'utf-8'
+        self.body = kwargs.get('body')
+        self.cookies = kwargs.get('cookies') or {}
+        self.headers = {}
+        self.url = None
+        if not self.meta:
+            self.meta = {}
+
+    def body_as_unicode(self):
+        return self._body_as_unicode
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
