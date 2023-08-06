fae20ea6cdaa9da3a8f9fefce77ea226b90d7a27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:12 +0000

    Add package adi.devgen/0.6/adi.devgen-0.6.tar.gz

---
+++ packages/adi.devgen/adi.devgen-0.6.tar.gz/adi.devgen-0.6/README.md
@@ -0,0 +1,96 @@
+Introduction
+============
+
+Yet another command-line Plone-Add-On-Generator, just the way I like it:
+
+No dependencies, no possible conflicts, some Python-methods, that's all.
+
+
+Installation
+=============
+
+    $ pip install adi.devgen
+
+
+Alternatively add `adi.devgen` as an egg to your buildout, then
+the `devgen`-executable should be available in its bin-directory
+and callable with `./bin/devgen`.
+
+
+If you want to play around or develop adi.devgen,
+install the latest dev-version with:
+
+    $ pip install -e git+https://github.com/ida/adi.devgen.git#egg=adi.devgen
+
+You should find the package for manipulations then in the 'src'-directory
+next to pip's bin-directory, f.e. `cd .myVirtualEnviroment/src`.
+
+
+Usage
+=====
+
+Type the command alone, to get a help-text, what it can do for you:
+
+    $ devgen
+
+
+That'll also list the available generator-functions, to get a function's help-text, type:
+
+    $ devgen [FUNCTION_NAME]
+
+
+Examples
+========
+
+Create boilerplate for an addon, that can do nothing, but be installed in a Plonesite:
+
+    $ devgen addProfileSkel your.addon
+
+
+Create it not in the directory, where you are, but somewhere else:
+
+    $ devgen addProfileSkel some/where/else/your.addon
+
+
+Register another addon as a dependency to your addon:
+
+    $ devgen addDep collective.bestaddonever path/to/your.addon
+
+Or, first locate into your addon, then you can omit the appended path (defaults to '.'):
+
+    $ cd path/to/your.addon
+    $ devgen addDep collective.bestaddonever
+
+
+Create an installable Plone-Addon, with a stylesheet, a javascript and a template in a skin folder:
+
+    $ devgen addSkinSkel your.addon
+
+
+Create an installable Plone-Addon, with a stylesheet and a javascript in a browser's resource-folder.
+
+    $ devgen addBrowserSkel your.addon
+
+
+Add docs-folder and read defaults for setup.py of a config:
+
+    $ devgen addMetaSkel
+
+If a file '~/.buildout/devgen.cfg' is present, values will be read of it and
+inserted to setup.py. Its format is expected to be like:
+
+    author=Arbi Trary
+    author_email=arbi@tra.ry
+    url=https://github.com/arbitrary/your.addon
+
+
+TODO
+====
+
+- Regard more than one dotted namespace for addon.
+
+- Split functions into smaller reusable chunks.
+
+- Possibly transfer:
+https://github.com/ida/skriptz/blob/master/plone/Dexterity/addField.py
+
---
+++ packages/adi.devgen/adi.devgen-0.6.tar.gz/adi.devgen-0.6/adi/devgen/devgen.py
@@ -0,0 +1,74 @@
+import sys
+import inspect
+from adi.devgen.scripts.skels import AddSkel
+
+def devgen():
+    """
+    Create Plone-development-skeletons of the commandline.
+    
+    Usage:
+
+        $ devgen [function_name] [argument(s)]
+
+    To see which arguments a function expects, type:
+
+        $ devgen [function_name]
+"""
+
+    required_args_amount = 0
+
+    args = sys.argv                                 # Get user's input as a list, using sys.argv.
+
+    this_script_path = args.pop(0)                  # Remove sys.argv's inbuilt first default-arg.
+
+    # First aid:
+    if len(args) < 1:                               # No argument was provided of user,
+        usable_funcs = ''                           # get functions of AddSkel,
+        all_funcs = dir(AddSkel)
+        for fun in all_funcs:
+            if not fun.startswith('__'):            # except built-in methods,
+                usable_funcs += ' ' + fun           # collect all others, show them and this func's docstr, abort.
+        exit(devgen.__doc__ + '\n\
+    The available functions are:\n\n        ' + usable_funcs + '\n\n')
+
+    # Get corresponding function and its expected arguments:
+    function_name = args.pop(0)                     # At least one arg was passed, it must be the function-name.
+    function = getattr(AddSkel, function_name)      # Get function of AddSkel-class by corresponding name.
+    expected_args = inspect.getargspec(function)[0] # Get the function's expected arguments,
+    if 'self' in expected_args:                     # except self-keyword of expected arguments,
+        expected_args.remove('self')                # user can't pass that one ;)
+    defaults = inspect.getargspec(function)[3]      # Get possible default-values.
+    if defaults:                                    # Compute amount of required args.
+        required_args_amount = len(expected_args) - len(defaults)
+    else:
+        required_args_amount = len(expected_args)
+
+    # If user gave less args than expected, add default-vals to passed args:
+    if required_args_amount > len(args):
+        if defaults:
+            i = 0
+            while i*-1 < len(defaults):
+                i -= 1                                          
+                expected_args[i] = expected_args[i] + '="' + defaults[i] + '"'
+            if len(args) < len(expected_args):
+                missing_args = len(expected_args) - len(args)
+                while missing_args > 0:
+                    args.append(defaults[i])
+                    missing_args -= 1
+
+    # Compare expectations with given arguments:
+    if required_args_amount > len(args) or required_args_amount < len(args) :
+        helptxt = "\nThis didn't work out, less or more arguments are given, than expected, try again:\n"
+        helptxt += '\n     '
+        helptxt += function_name + '(' + ', '.join(expected_args) + '):'
+        helptxt += '\n        """' + getattr(function, '__doc__') + '"""'
+        exit(helptxt)
+
+    # Now, after validating user-input, finally execute the function:
+    getattr(AddSkel(), function_name)(*args)
+
+# Only execute this, if triggered of commandline:
+# ('__name__' is '__devgen__', if executed of commandline it's '__main__')
+if __name__ == '__main__':
+    devgen()
+
