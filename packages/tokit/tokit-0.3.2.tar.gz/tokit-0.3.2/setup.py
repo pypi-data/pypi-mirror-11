a06a1c60e68c950bb95283b477dbc9f788d46c99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:17 +0000

    Add package tokit/0.3.2/tokit-0.3.2.tar.gz

---
+++ packages/tokit/tokit-0.3.2.tar.gz/tokit-0.3.2/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+# Thanks to http://peterdowns.com/posts/first-time-with-pypi.html
+tokit_version = '0.3.2'
+setup(
+    name='tokit',
+    packages=['tokit'],
+    version=tokit_version,
+    description=r'''A kit for development with Tornado web framework.
+        'See https://github.com/manhg/writekit for usage demo.''',
+    author='Giang Manh',
+    author_email='manhgd@yahoo.com',
+    url='https://github.com/manhg/tokit',
+    download_url='https://github.com/manhg/tokit/tarball/' + tokit_version,
+    keywords=['tornado', 'web', 'tokit'],
+    classifiers=[
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
