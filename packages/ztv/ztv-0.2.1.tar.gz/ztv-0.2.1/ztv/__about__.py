15b73e76288dca3ee4a710b966ce64d0f9e88959
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:49 +0000

    Add package ztv/0.2.1/ztv-0.2.1.tar.gz

---
+++ packages/ztv/ztv-0.2.1.tar.gz/ztv-0.2.1/ztv/__about__.py
@@ -0,0 +1,29 @@
+try:
+    from astropy_helpers.git_helpers import get_git_devstr
+    git_devstr = get_git_devstr(False)
+except ImportError:
+    git_devstr = ""
+    
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "ztv"
+__summary__ = "Simple python image viewer, largely intended for astronomical applications"
+__uri__ = "https://github.com/henryroe/ztv"
+
+# VERSION should be PEP386 compatible (http://www.python.org/dev/peps/pep-0386)
+__version__ = "0.2.1"
+
+# Indicates if this version is a release version
+RELEASE = 'dev' not in __version__
+
+if not RELEASE:
+    __version__ += ""
+
+__author__ = "Henry Roe"
+__email__ = "hroe@hroe.me"
+
+__license__ = "MIT License"
+__copyright__ = "2015 %s" % __author__
