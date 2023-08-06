fad1ccf2b07a19a8b7e332a572d4e279043a7116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:12 +0000

    Add package cryptoexchange/0.0.3/cryptoexchange-0.0.3.tar.gz

---
+++ packages/cryptoexchange/cryptoexchange-0.0.3.tar.gz/cryptoexchange-0.0.3/setup.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2015 Bitquant Research Laboratories (Asia) Limited
+# Released under the Simplified BSD License
+
+from setuptools import (
+    setup,
+    find_packages,
+    )
+
+setup(
+    name="cryptoexchange",
+    version = "0.0.3",
+    author="Joseph C Wang",
+    author_email='joequant@gmail.com',
+    url="https://github.com/joequant/cryptoexchange",
+    description="API's for bitcoin exchanges",
+    long_description="""API's for cryptocurrency exchanges """,
+    license="BSD",
+    packages=['cryptoexchange',
+              "cryptoexchange/auth",
+              "cryptoexchange/ws"],
+    install_requires = ["websocket-client"],
+    use_2to3 = True
+)
+                                
