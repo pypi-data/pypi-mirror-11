2bde61bda17c2d5ed25b08b915899ba110d3563e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:10 +0000

    Add package bench/2.5/bench-2.5.tar.gz

---
+++ packages/bench/bench-2.5.tar.gz/bench-2.5/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+    name = "bench",
+    scripts = ['scripts/monitor.py'],
+    requires = ['psutil'],
+    version = "2.5",
+    description = "Benchmark resources usage",
+    author = "Xiaowei Zhan",
+    author_email = "zhanxw@gmail.com",
+    url = "http://zhanxw.com/bench",
+    download_url = "https://pypi.python.org/pypi/bench",
+    keywords = ["benchmark", "process", "monitor"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development :: Testing",
+        "Topic :: System :: Monitoring",
+        ],
+    long_description = open('README.txt').read()
+)
