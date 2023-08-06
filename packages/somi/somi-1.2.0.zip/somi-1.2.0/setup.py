dd8381b57270963b83c1fe7a121ccc51765a6887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:12 +0000

    Add package somi/1.2.0/somi-1.2.0.zip

---
+++ packages/somi/somi-1.2.0.zip/somi-1.2.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name='somi',
+        version='1.2.0',
+        py_modules=['somi'],
+        author='sds',
+        author_email='512@123.com',
+        url='xxx',
+        description='Sorry,This is Practice!'
+
+    )
