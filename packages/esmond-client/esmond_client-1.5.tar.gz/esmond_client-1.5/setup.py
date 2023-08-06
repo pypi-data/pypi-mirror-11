79d280f40f7dd2fa443af489702c1858089ff3c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:38 +0000

    Add package esmond-client/1.5/esmond_client-1.5.tar.gz

---
+++ packages/esmond-client/esmond_client-1.5.tar.gz/esmond_client-1.5/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+def read(*paths):
+    """Build a file path from *paths* and return the contents."""
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
+
+setup(
+    name='esmond_client',
+    version='1.5',
+    description='API client libraries and command line tools for the ESnet Monitoring Daemon (esmond).',
+    long_description=(read('README.rst')),
+    author='Monte M. Goode',
+    author_email='MMGoode@lbl.gov',
+    url='http://software.es.net/esmond/',
+    packages=['esmond_client', 'esmond_client.perfsonar'],
+    scripts=[
+        'clients/esmond-ps-get',
+        'clients/esmond-ps-get-bulk',
+        'clients/esmond-ps-get-endpoints', 
+        'clients/esmond-ps-get-metadata',
+    ],
+    install_requires=['requests', 'python-dateutil'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Telecommunications Industry',
+        'Environment :: Console',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2',
+        'Topic :: Internet',
+        'Topic :: System :: Networking',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
