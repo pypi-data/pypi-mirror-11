a6403769c331ad0b92a9d3d0029389fd5a096858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:59 +0000

    Add package waylonlee/1.1.1/waylonlee-1.1.1.tar.gz

---
+++ packages/waylonlee/waylonlee-1.1.1.tar.gz/waylonlee-1.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name           = 'waylonlee',
+	version        = '1.1.1',
+	py_modules 	   = ['waylonlee'],
+	author         = 'SangjongLee',
+	author_email   = 'feuuis@naver.com',
+	url            = 'http://waylon.tistory.com',
+	description    = 'Hello world!',
+	)
