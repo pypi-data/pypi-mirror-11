e576ec4060b64e0cc33befee1e9673344d43c952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:25 +0000

    Add package python-mistralclient/1.0.2/python_mistralclient-1.0.2-py2-none-any.whl

---
+++ packages/python-mistralclient/python_mistralclient-1.0.2-py2-none-any.whl/mistralclient/tests/unit/v2/test_cli_tasks.py
@@ -0,0 +1,109 @@
+# Copyright 2014 Mirantis, Inc.
+# All Rights Reserved
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+import json
+
+import mock
+
+from mistralclient.api.v2 import tasks
+from mistralclient.commands.v2 import tasks as task_cmd
+from mistralclient.tests.unit import base
+
+TASK_DICT = {
+    'id': '123',
+    'name': 'some',
+    'workflow_name': 'thing',
+    'workflow_execution_id': '321',
+    'state': 'RUNNING',
+}
+
+TASK_RESULT = {"test": "is", "passed": "successfully"}
+TASK_PUBLISHED = {"bar1": "val1", "var2": 2}
+
+TASK_WITH_RESULT_DICT = TASK_DICT.copy()
+TASK_WITH_RESULT_DICT.update({'result': json.dumps(TASK_RESULT)})
+TASK_WITH_PUBLISHED_DICT = TASK_DICT.copy()
+TASK_WITH_PUBLISHED_DICT.update({'published': json.dumps(TASK_PUBLISHED)})
+
+TASK = tasks.Task(mock, TASK_DICT)
+TASK_WITH_RESULT = tasks.Task(mock, TASK_WITH_RESULT_DICT)
+TASK_WITH_PUBLISHED = tasks.Task(mock, TASK_WITH_PUBLISHED_DICT)
+
+
+class TestCLITasksV2(base.BaseCommandTest):
+    @mock.patch('mistralclient.api.v2.tasks.TaskManager.list')
+    def test_list(self, mock):
+        mock.return_value = (TASK,)
+
+        result = self.call(task_cmd.List)
+
+        self.assertEqual([('123', 'some', 'thing', '321', 'RUNNING')],
+                         result[1])
+
+    @mock.patch('mistralclient.api.v2.tasks.TaskManager.list')
+    def test_list_with_workflow_execution(self, mock):
+        mock.return_value = (TASK,)
+
+        result = self.call(task_cmd.List, app_args=['workflow_execution'])
+
+        self.assertEqual([('123', 'some', 'thing', '321', 'RUNNING')],
+                         result[1])
+
+    @mock.patch('mistralclient.api.v2.tasks.TaskManager.get')
+    def test_get(self, mock):
+        mock.return_value = TASK
+
+        result = self.call(task_cmd.Get, app_args=['id'])
+
+        self.assertEqual(('123', 'some', 'thing', '321', 'RUNNING'),
+                         result[1])
+
+    @mock.patch('mistralclient.api.v2.tasks.TaskManager.get')
+    def test_get_result(self, mock):
+        mock.return_value = TASK_WITH_RESULT
+
+        self.call(task_cmd.GetResult, app_args=['id'])
+
+        self.app.stdout.write.assert_called_with(
+            json.dumps(TASK_RESULT, indent=4) + "\n")
+
+    @mock.patch('mistralclient.api.v2.tasks.TaskManager.get')
+    def test_get_published(self, mock):
+        mock.return_value = TASK_WITH_PUBLISHED
+
+        self.call(task_cmd.GetPublished, app_args=['id'])
+
+        self.app.stdout.write.assert_called_with(
+            json.dumps(TASK_PUBLISHED, indent=4) + "\n"
+        )
+
+    @mock.patch('mistralclient.api.v2.tasks.TaskManager.rerun')
+    def test_rerun(self, mock):
+        mock.return_value = TASK
+
+        result = self.call(task_cmd.Rerun, app_args=['id'])
+
+        self.assertEqual(('123', 'some', 'thing', '321', 'RUNNING'),
+                         result[1])
+
+    @mock.patch('mistralclient.api.v2.tasks.TaskManager.rerun')
+    def test_rerun_no_reset(self, mock):
+        mock.return_value = TASK
+
+        result = self.call(task_cmd.Rerun, app_args=['id', '--resume'])
+
+        self.assertEqual(('123', 'some', 'thing', '321', 'RUNNING'),
+                         result[1])
---
+++ packages/python-mistralclient/python_mistralclient-1.0.2-py2-none-any.whl/mistralclient/tests/unit/v2/test_cli_workbooks.py
@@ -0,0 +1,132 @@
+# Copyright 2014 - Mirantis, Inc.
+# Copyright 2015 - StackStorm, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import mock
+
+from mistralclient.api.v2 import workbooks
+from mistralclient.commands.v2 import workbooks as workbook_cmd
+from mistralclient import exceptions as exc
+from mistralclient.tests.unit import base
+
+
+WORKBOOK_DICT = {
+    'name': 'a',
+    'tags': ['a', 'b'],
+    'created_at': '1',
+    'updated_at': '1'
+}
+
+
+WB_DEF = """
+---
+version: '2.0
+
+name: wb
+
+workflows:
+  wf1:
+    tasks:
+      task1:
+        action: nova.servers_get server="1"
+"""
+
+WB_WITH_DEF_DICT = WORKBOOK_DICT.copy()
+WB_WITH_DEF_DICT.update({'definition': WB_DEF})
+WORKBOOK = workbooks.Workbook(mock, WORKBOOK_DICT)
+WORKBOOK_WITH_DEF = workbooks.Workbook(mock, WB_WITH_DEF_DICT)
+
+
+class TestCLIWorkbooksV2(base.BaseCommandTest):
+    @mock.patch('argparse.open', create=True)
+    @mock.patch('mistralclient.api.v2.workbooks.WorkbookManager.create')
+    def test_create(self, mock, mock_open):
+        mock.return_value = WORKBOOK
+        mock_open.return_value = mock.MagicMock(spec=open)
+
+        result = self.call(workbook_cmd.Create, app_args=['wb.yaml'])
+
+        self.assertEqual(('a', 'a, b', '1', '1'), result[1])
+
+    @mock.patch('argparse.open', create=True)
+    @mock.patch('mistralclient.api.v2.workbooks.WorkbookManager.update')
+    def test_update(self, mock, mock_open):
+        mock.return_value = WORKBOOK
+        mock_open.return_value = mock.MagicMock(spec=open)
+
+        result = self.call(workbook_cmd.Update, app_args=['definition'])
+
+        self.assertEqual(('a', 'a, b', '1', '1'), result[1])
+
+    @mock.patch('mistralclient.api.v2.workbooks.WorkbookManager.list')
+    def test_list(self, mock):
+        mock.return_value = (WORKBOOK,)
+
+        result = self.call(workbook_cmd.List)
+
+        self.assertEqual([('a', 'a, b', '1', '1')], result[1])
+
+    @mock.patch('mistralclient.api.v2.workbooks.WorkbookManager.get')
+    def test_get(self, mock):
+        mock.return_value = WORKBOOK
+
+        result = self.call(workbook_cmd.Get, app_args=['name'])
+
+        self.assertEqual(('a', 'a, b', '1', '1'), result[1])
+
+    @mock.patch('mistralclient.api.v2.workbooks.WorkbookManager.delete')
+    def test_delete(self, del_mock):
+        self.call(workbook_cmd.Delete, app_args=['name'])
+
+        del_mock.assert_called_once_with('name')
+
+    @mock.patch('mistralclient.api.v2.workbooks.WorkbookManager.delete')
+    def test_delete_with_multi_names(self, del_mock):
+        self.call(workbook_cmd.Delete, app_args=['name1', 'name2'])
+
+        self.assertEqual(2, del_mock.call_count)
+        self.assertEqual(
+            [mock.call('name1'), mock.call('name2')],
+            del_mock.call_args_list
+        )
+
+    @mock.patch('mistralclient.api.v2.workbooks.WorkbookManager.get')
+    def test_get_definition(self, mock):
+        mock.return_value = WORKBOOK_WITH_DEF
+
+        self.call(workbook_cmd.GetDefinition, app_args=['name'])
+
+        self.app.stdout.write.assert_called_with(WB_DEF)
+
+    @mock.patch('argparse.open', create=True)
+    @mock.patch('mistralclient.api.v2.workbooks.WorkbookManager.validate')
+    def test_validate(self, mock, mock_open):
+        mock.return_value = {'valid': True}
+        mock_open.return_value = mock.MagicMock(spec=open)
+
+        result = self.call(workbook_cmd.Validate, app_args=['wb.yaml'])
+
+        self.assertEqual(result[0], tuple())
+        self.assertEqual(result[1], tuple())
+
+    @mock.patch('argparse.open', create=True)
+    @mock.patch('mistralclient.api.v2.workbooks.WorkbookManager.validate')
+    def test_validate_failed(self, mock, mock_open):
+        mock.return_value = {'valid': False, 'error': 'Invalid DSL...'}
+        mock_open.return_value = mock.MagicMock(spec=open)
+
+        self.assertRaises(exc.MistralClientException,
+                          self.call,
+                          workbook_cmd.Validate,
+                          app_args=['wb.yaml'])
