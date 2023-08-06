472b4791be9499851a7983a97145a9ba7353aa8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:56 +0000

    Add package cliquet/2.6.2/cliquet-2.6.2-pp2.pp3-none-any.whl

---
+++ packages/cliquet/cliquet-2.6.2-pp2.pp3-none-any.whl/cliquet/authorization.py
@@ -0,0 +1,180 @@
+import re
+import six
+import functools
+from pyramid.settings import aslist
+from pyramid.security import IAuthorizationPolicy, Authenticated
+from zope.interface import implementer
+
+from cliquet import utils
+from cliquet.storage import exceptions as storage_exceptions
+
+# A permission is called "dynamic" when it's computed at request time.
+DYNAMIC = 'dynamic'
+
+# When permission is set to "private", only the current user is allowed.
+PRIVATE = 'private'
+
+
+@implementer(IAuthorizationPolicy)
+class AuthorizationPolicy(object):
+    # Callable that takes an object id and a permission and returns
+    # a list of tuples (<object id>, <permission>).
+    get_bound_permissions = None
+
+    def permits(self, context, principals, permission):
+        if permission == PRIVATE:
+            return Authenticated in principals
+
+        # Add prefixed user id to principals.
+        if context.prefixed_userid:
+            principals = principals + [context.prefixed_userid]
+            prefix, user_id = context.prefixed_userid.split(':', 1)
+            # Remove unprefixed user id to avoid conflicts.
+            # (it is added via Pyramid Authn policy effective principals)
+            if user_id in principals:
+                principals.remove(user_id)
+            # Retro-compatibility with cliquet 2.0 '_' user id prefixes.
+            # Just in case it was used in permissions definitions.
+            principals.append('%s_%s' % (prefix, user_id))
+
+        if permission == DYNAMIC:
+            permission = context.required_permission
+
+        if permission == 'create':
+            permission = '%s:%s' % (context.resource_name, permission)
+
+        if context.allowed_principals:
+            allowed = bool(set(context.allowed_principals) & set(principals))
+        else:
+            allowed = context.check_permission(
+                permission,
+                principals,
+                get_bound_permissions=self.get_bound_permissions)
+
+        # If not allowed on this collection, but some records are shared with
+        # the current user, then authorize.
+        # The ProtectedResource class will take care of the filtering.
+        is_list_operation = (context.on_collection and
+                             'create' not in permission)
+        if not allowed and is_list_operation:
+            shared_records = context.fetch_shared_records(
+                permission,
+                principals,
+                get_bound_permissions=self.get_bound_permissions)
+            allowed = len(shared_records) > 0
+
+        return allowed
+
+    def principals_allowed_by_permission(self, context, permission):
+        raise NotImplementedError()  # PRAGMA NOCOVER
+
+
+class RouteFactory(object):
+    method_permissions = {
+        "head": "read",
+        "get": "read",
+        "post": "create",
+        "delete": "write",
+        "patch": "write"
+    }
+
+    def __init__(self, request):
+        # Make it available for the authorization policy.
+        self.prefixed_userid = getattr(request, "prefixed_userid", None)
+
+        # Store service, resource, record and required permission.
+        service = utils.current_service(request)
+
+        is_on_resource = (service is not None and
+                          hasattr(service, 'viewset') and
+                          hasattr(service, 'resource'))
+
+        object_id = None
+        permission = None
+        resource_name = None
+        check_permission = None
+        on_collection = False
+        get_shared_ids = None
+
+        if is_on_resource:
+            on_collection = getattr(service, "type", None) == "collection"
+            object_id = get_object_id(request.path)
+
+            # Decide what the required unbound permission is depending on the
+            # method that's being requested.
+            if request.method.lower() == "put":
+                # In the case of a "PUT", check if the targetted record already
+                # exists, return "write" if it does, "create" otherwise.
+
+                # If the view exists, call it with the request and catch an
+                # eventual NotFound.
+                resource = service.resource(request=request, context=self)
+                try:
+                    resource.collection.get_record(resource.record_id)
+                except storage_exceptions.RecordNotFoundError:
+                    object_id = service.collection_path.format(
+                        **request.matchdict)
+                    permission = "create"
+                else:
+                    permission = "write"
+            else:
+                method = request.method.lower()
+                permission = self.method_permissions.get(method)
+
+            resource_name = service.viewset.get_name(service.resource)
+            check_permission = functools.partial(
+                request.registry.permission.check_permission,
+                object_id)
+
+            if on_collection:
+                record_uri = record_uri_from_collection(request, '*')
+                object_id_match = get_object_id(record_uri.replace('%2A', '*'))
+
+                get_shared_ids = functools.partial(
+                    request.registry.permission.principals_accessible_objects,
+                    object_id_match=object_id_match)
+
+        settings = request.registry.settings
+        settings_key = 'cliquet.%s_%s_principals' % (resource_name, permission)
+        self.allowed_principals = aslist(settings.get(settings_key, ''))
+        self.on_collection = on_collection
+        self.object_id = object_id
+        self.required_permission = permission
+        self.resource_name = resource_name
+        self.check_permission = check_permission
+        self.get_shared_ids = get_shared_ids
+
+        # Partial collections of ProtectedResource:
+        self.shared_ids = []
+
+    def fetch_shared_records(self, perm, principals, get_bound_permissions):
+        ids = self.get_shared_ids(
+            permission=perm,
+            principals=principals,
+            get_bound_permissions=get_bound_permissions)
+        # Store for later use in ``ProtectedResource``.
+        self.shared_ids = [extract_object_id(id_) for id_ in ids]
+        return self.shared_ids
+
+
+def get_object_id(object_uri):
+    # Remove potential version prefix in URI.
+    return re.sub(r'^(/v\d+)?', '', six.text_type(object_uri))
+
+
+def extract_object_id(object_id):
+    # XXX: Help needed: use something like route.matchdict.get('id').
+    return object_id.split('/')[-1]
+
+
+def record_uri_from_collection(request, record_id):
+    """ With the current request on a collection, the record URI must
+    be found out by inspecting the collection service and its sibling
+    record service.
+    """
+    service = utils.current_service(request)
+    record_service = service.name.replace('-collection', '-record')
+    matchdict = request.matchdict.copy()
+    matchdict['id'] = record_id
+    record_uri = request.route_path(record_service, **matchdict)
+    return record_uri
