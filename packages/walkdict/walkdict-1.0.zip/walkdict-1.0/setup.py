04c63ee8a88fd9721c91fadb46c59fb6b6bd4d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:15 +0000

    Add package walkdict/1.0/walkdict-1.0.zip

---
+++ packages/walkdict/walkdict-1.0.zip/walkdict-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name="walkdict",
+    version="1.0",
+    description="deep traverse dict or list",
+    author="kk",
+    url="https://github.com/guyskk/walkdict",
+    license="MIT",
+    py_modules=['walkdict'],
+)
