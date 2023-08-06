b3025319f7e0f06f0b318cec13dfc4fa21d759c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:57 +0000

    Add package open-drop/0.2.post19/open-drop-0.2.post19.zip

---
+++ packages/open-drop/open-drop-0.2.post19.zip/open-drop-0.2.post19/open_drop/Arduino/open_drop/open_drop.ino
@@ -0,0 +1,41 @@
+#include "Arduino.h"
+#include "EEPROM.h"
+#include "SPI.h"
+#include "Wire.h"
+#include "Memory.h"
+#include "nanopb.h"
+#include "NadaMQ.h"
+#include "Array.h"
+#include "RPCBuffer.h"
+#include "OpenDrop/CommandProcessor.h"
+#include "NodeCommandProcessor.h"
+#include "Node.h"
+
+
+open_drop::Node node_obj;
+open_drop::CommandProcessor<open_drop::Node> command_processor(node_obj);
+
+
+void i2c_receive_event(int byte_count) { node_obj.i2c_handler_.receiver()(byte_count); }
+void serialEvent() { node_obj.serial_handler_.receiver()(Serial.available()); }
+
+
+void setup() {
+  node_obj.begin();
+  Wire.onReceive(i2c_receive_event);
+}
+
+
+void loop() {
+#ifndef DISABLE_SERIAL
+  /* Parse all new bytes that are available.  If the parsed bytes result in a
+   * completed packet, pass the complete packet to the command-processor to
+   * process the request. */
+  if (node_obj.serial_handler_.packet_ready()) {
+    node_obj.serial_handler_.process_packet(command_processor);
+  }
+#endif  // #ifndef DISABLE_SERIAL
+  if (node_obj.i2c_handler_.packet_ready()) {
+    node_obj.i2c_handler_.process_packet(command_processor);
+  }
+}
