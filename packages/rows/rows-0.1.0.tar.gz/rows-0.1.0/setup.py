65d1439a836b748ef7c981f56ae31c05697a515f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:55 +0000

    Add package rows/0.1.0/rows-0.1.0.tar.gz

---
+++ packages/rows/rows-0.1.0.tar.gz/rows-0.1.0/setup.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+
+from setuptools import setup
+
+
+setup(name='rows',
+    description='Import and export tabular data easily with Python',
+    long_description='',
+    version='0.1.0',
+    author=u'Álvaro Justen',
+    author_email='alvarojusten@gmail.com',
+    url='https://github.com/turicas/rows/',
+    packages=['rows', 'rows.plugins'],
+    install_requires=['unicodecsv', 'click', 'filemagic', 'requests'],
+    extras_require = {
+        'csv': ['unicodecsv'],
+        'html': ['lxml'], # apt: libxslt-dev libxml2-dev
+        'cli': ['click', 'filemagic', 'requests'],
+        'xls': ['xlrd', 'xlwt'],
+        'all': ['unicodecsv',
+                'lxml',
+                'click', 'filemagic', 'requests',
+                'xlrd', 'xlwt'],
+    },
+    keywords=['tabular', 'table', 'csv', 'xls', 'html', 'rows'],
+    entry_points = {
+        'console_scripts': [
+            'rows = rows.cli:cli',
+            ],
+    },
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
