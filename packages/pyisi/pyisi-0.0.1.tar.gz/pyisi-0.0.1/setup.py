56c45cc6b3d27eb45dee559376781c328deefa29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:12 +0000

    Add package pyisi/0.0.1/pyisi-0.0.1.tar.gz

---
+++ packages/pyisi/pyisi-0.0.1.tar.gz/pyisi-0.0.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pyisi',
+      version='0.0.1',
+      description='Unofficial Isilon interface for Python',
+      author="Jason Kingsbury",
+      author_email="jason@relva.co.uk",
+      url="https://bitbucket.org/jrelva/pyisi",
+      download_url="https://bitbucket.org/jrelva/pyisi/get/0.0.1.tar.gz",
+      keywords=['onefs', 'isilon', 'api'],
+      packages=["pyisi"]
+      )
+
