c978427f8cb4b11c88d7d7ad89564609728aa32c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:52 +0000

    Add package ffmpy/0.0.1/ffmpy-0.0.1.tar.gz

---
+++ packages/ffmpy/ffmpy-0.0.1.tar.gz/ffmpy-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ffmpy',
+    version='0.0.1',
+    description='A Python wrapper around ffmpeg/ffprobe',
+    author='Andriy Yurchuk',
+    author_email='ay@mntw.re',
+    url='https://github.com/Ch00k/ffmpy',
+    packages=find_packages(),
+)
