117884e6d7bd4f002d2e99fff1464083450d9788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:36 +0000

    Add package Flask-JSONAPIView/0.0.6/Flask-JSONAPIView-0.0.6.tar.gz

---
+++ packages/Flask-JSONAPIView/Flask-JSONAPIView-0.0.6.tar.gz/Flask-JSONAPIView-0.0.6/flask_jsonapiview/exceptions.py
@@ -0,0 +1,11 @@
+__all__ = ('IncorrectTypeError',)
+
+# -----------------------------------------------------------------------------
+
+
+class IncorrectTypeError(ValueError):
+    def __init__(self, actual, expected):
+        super(IncorrectTypeError, self).__init__(
+            "incorrect object type, got {} but expected {}"
+            .format(actual, expected)
+        )
