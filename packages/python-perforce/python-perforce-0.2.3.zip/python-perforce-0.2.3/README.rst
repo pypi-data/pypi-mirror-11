2c89485a33cde53c804d590afa64ada3d447c917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:25 +0000

    Add package python-perforce/0.2.3/python-perforce-0.2.3.zip

---
+++ packages/python-perforce/python-perforce-0.2.3.zip/python-perforce-0.2.3/README.rst
@@ -0,0 +1,38 @@
+===============================
+Python Perforce
+===============================
+
+.. image:: https://travis-ci.org/theiviaxx/python-perforce.png?branch=master
+        :target: https://travis-ci.org/theiviaxx/python-perforce
+
+
+
+Pure python perforce API
+
+* Free software: BSD license
+* Documentation: https://python-perforce.readthedocs.org.
+
+Features
+--------
+
+* Pythonic api to Perforce
+* Pure python, no compiled extension
+
+Usage
+-----
+
+    >>> from perforce import connection
+    >>> p4 = connection.Connection()
+    >>> revisions = p4.ls('//depot/path/to/file.txt')
+    >>> print(revisions)
+    [<Revision 1: file.txt>]
+    >>> p4.ls('//depot/path/....txt')
+    [<Revision 1: file.txt>, <Revision 2: foo.txt>]
+    >>> cl = p4.findChangelist('my description')
+    >>> with cl:
+    ...     cl.append(revisions[0])
+    ...     p4.add('path/to/add.txt', cl)
+    >>> cl.description
+    'my description'
+    >>> cl.description = 'something else'
+    >>> cl.submit()
---
+++ packages/python-perforce/python-perforce-0.2.3.zip/python-perforce-0.2.3/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+requirements = [
+    'path.py==5.2'
+]
+
+test_requirements = [
+    'path.py==5.2'
+]
+
+setup(
+    name='python-perforce',
+    version='0.2.3',
+    description='Pure python Perforce API',
+    long_description=readme + '\n\n' + history,
+    author='Brett Dixon',
+    author_email='theiviaxx@gmail.com',
+    url='https://github.com/theiviaxx/python-perforce',
+    packages=[
+        'perforce',
+    ],
+    package_dir={'python-perforce':
+                 'python-perforce'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD",
+    zip_safe=False,
+    keywords='python-perforce',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
