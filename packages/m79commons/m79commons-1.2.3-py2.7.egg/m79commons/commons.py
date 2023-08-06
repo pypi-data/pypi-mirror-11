545de7e5fb38b677996e28903a8bd1e743092d72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:01 +0000

    Add package m79commons/1.2.3/m79commons-1.2.3-py2.7.egg

---
+++ packages/m79commons/m79commons-1.2.3-py2.7.egg/m79commons/commons.py
@@ -0,0 +1,4 @@
+#! /usr/bin/env python
+
+def testm79():
+    print 'm79'
---
+++ packages/m79commons/m79commons-1.2.3-py2.7.egg/m79commons/m79commons.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+class argparser:
+    def __init__(self):
+        pass
+
+    def run(self):
+        print 'test arg parser from m79commons'
