cf7552314cac346f2afc4a43faf48c510fc9e569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:58 +0000

    Add package IPChecker/1.1/IPChecker-1.1.tar.gz

---
+++ packages/IPChecker/IPChecker-1.1.tar.gz/IPChecker-1.1/setup.py
@@ -0,0 +1,13 @@
+from  distutils.core import setup
+setup(
+	name="IPChecker",
+	version="1.1",
+	author = "Boumediene Kaddour",
+	author_email="snboumediene@gmail.com",
+	packages=["IPChecker"],
+	scripts=[],
+	url='http://pypi.python.org/pypi/IPchecker',
+	license="LICENSE.txt",
+	description="Check if IP is Private, Public or Invalid",
+	long_description=open('README.txt').read(),
+	install_requires=[],)
