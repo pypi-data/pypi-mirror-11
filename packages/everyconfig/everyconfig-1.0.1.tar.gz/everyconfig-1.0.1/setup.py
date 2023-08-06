4e481364b375c66f3b6a3b135f0067ccff848541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:45 +0000

    Add package everyconfig/1.0.1/everyconfig-1.0.1.tar.gz

---
+++ packages/everyconfig/everyconfig-1.0.1.tar.gz/everyconfig-1.0.1/setup.py
@@ -0,0 +1,46 @@
+"""Use the same .yaml config files in every language
+
+See: https://github.com/pbrandt1/everyconfig
+"""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='everyconfig',
+    version='1.0.1',
+    description='use the same .yaml config files in every language',
+    url='https://github.com/pbrandt1/everyconfig',
+    author='Peter Brandt',
+    author_email='peter.m.brandt@gmail.com',
+    license='MIT',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 5 - Production/Stable',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='configuration, configuration management, environment',
+    packages=find_packages(),
+    install_requires=['pyyaml', 'easydict']
+)
+    
