fb945851b70a1d0c9f99e165529d9deddcbeaf44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:10 +0000

    Add package fluentcms-teaser/1.0.1/fluentcms_teaser-1.0.1-py2.py3-none-any.whl

---
+++ packages/fluentcms-teaser/fluentcms_teaser-1.0.1-py2.py3-none-any.whl/fluentcms_teaser/content_plugins.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from django.utils.translation import ugettext_lazy as _
+
+from fluent_contents.extensions import ContentPlugin, plugin_pool
+
+from .models import TeaserItem
+
+
+@plugin_pool.register
+class TeaserPlugin(ContentPlugin):
+    model = TeaserItem
+    category = _('Media')
+    render_template = "fluentcms_teaser/teaser.html"
+    admin_init_template = "admin/fluentcms_teaser/admin_init.html" # TODO: remove the need for this.
