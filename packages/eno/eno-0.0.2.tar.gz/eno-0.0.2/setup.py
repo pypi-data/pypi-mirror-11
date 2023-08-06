9a23683990fe741a5f801591b65a6333fc9a3024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:34 +0000

    Add package eno/0.0.2/eno-0.0.2.tar.gz

---
+++ packages/eno/eno-0.0.2.tar.gz/eno-0.0.2/setup.py
@@ -0,0 +1,41 @@
+"""The eno-python package."""
+
+from setuptools import setup
+
+
+with open('readme.md') as f:
+  README = f.read()
+
+
+VERSION = '0.0.2'
+
+
+setup(
+  name='eno',
+  version=VERSION,
+  description='eno test client',
+  long_description=README,
+  url='http://github.com/endaga/eno-python',
+  download_url='https://github.com/endaga/eno-python/tarball/%s' % VERSION,
+  author='Matt Ball',
+  author_email='matt@endaga.com',
+  license='MIT',
+  packages=['eno'],
+  install_requires=[
+    "Adafruit-BBIO==0.0.30",
+    "Flask==0.10.1",
+    "Jinja2==2.8",
+    "MarkupSafe==0.23",
+    "PyYAML==3.11",
+    "Werkzeug==0.10.4",
+    "argparse==1.2.1",
+    "itsdangerous==0.24",
+    "pyserial==2.7",
+    "python-gsmmodem==0.9",
+    "wsgiref==0.1.2",
+  ],
+  zip_safe=False,
+  scripts=[
+    'scripts/run_server.py',
+  ]
+)
