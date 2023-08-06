df1acda9de0f9ce67a3dbfc294128ea76d0d6286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:21 +0000

    Add package frigg-test-discovery/1.2.0/frigg_test_discovery-1.2.0-py2.py3-none-any.whl

---
+++ packages/frigg-test-discovery/frigg_test_discovery-1.2.0-py2.py3-none-any.whl/frigg_test_discovery.py
@@ -0,0 +1,30 @@
+# -*- coding: utf8 -*-
+import logging
+
+logger = logging.getLogger(__name__)
+
+__version__ = '1.2.0'
+
+
+def detect_test_tasks(files):
+    if 'Makefile' in files:
+        return ['make test']
+    if 'tox.ini' in files:
+        return ['tox']
+    if 'Rakefile' in files:
+        return ['rake test']
+    if 'pom.xml' in files:
+        return ['mvn -B test']
+    if 'setup.py' in files:
+        return ['python setup.py test']
+    if 'manage.py' in files:
+        return ['python manage.py test']
+    if 'package.json' in files:
+        return ['npm install', 'npm test']
+    if 'build.sbt' in files:
+        return ['sbt test']
+    if 'Cargo.toml' in files:
+        return ['cargo test']
+    if '_config.yml' in files:
+        return ['jekyll build']
+    return []
