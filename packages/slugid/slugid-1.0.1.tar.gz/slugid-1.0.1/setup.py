7a9d2318d994760c0a77e12f25f6a6e76d140c35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:23 +0000

    Add package slugid/1.0.1/slugid-1.0.1.tar.gz

---
+++ packages/slugid/slugid-1.0.1.tar.gz/slugid-1.0.1/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import os
+import re
+import sys
+
+from codecs import open
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+packages = [
+    'slugid',
+]
+
+version = ''
+with open('slugid/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('Cannot find version information')
+
+with open('README.md', 'r', 'utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='slugid',
+    version=version,
+    description='Base64 encoded uuid v4 slugs',
+    long_description=readme,
+    author='Pete Moore',
+    author_email='pmoore@mozilla.com',
+    url='http://taskcluster.github.io/slugid.py',
+    packages=packages,
+    package_data={'': ['LICENSE', 'README.md']},
+    license='MPL 2.0',
+    classifiers=(
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+    ),
+)
