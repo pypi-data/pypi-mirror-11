a04096c64661a4d3187d5c2b5857dd643904e7e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:55 +0000

    Add package music21/2.0.10/music21-2.0.10.tar.gz

---
+++ packages/music21/music21-2.0.10.tar.gz/music21-2.0.10/music21/test/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from music21.test.dedent import dedent
+
+__all__ = [
+    'dedent',
+    'testDocumentation', 
+    'testExternal', 
+    'testPerformance', 
+    'testRunner',
+    'timeGraphs', 
+    'testStream',
+    'helpers',
+    ]
+
+_DOC_IGNORE_MODULE_OR_PACKAGE = True
+
+#------------------------------------------------------------------------------
+# eof
+
