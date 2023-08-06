386c158091adb6f223724af0f52570588cd4c18f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:16 +0000

    Add package codesters/0.0.14/codesters-0.0.14.tar.gz

---
+++ packages/codesters/codesters-0.0.14.tar.gz/codesters-0.0.14/codesters/execute.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import run, sys
+
+def execute():
+    if len(sys.argv) != 2:
+        print 'You must supply a filename to run with the codesters library e.g. "python run.py example1.py"'
+        exit()
+    filename = sys.argv[1]
+
+    run.run(filename)
+
+def example():
+    if len(sys.argv) != 2:
+        print 'You must supply a filename to run with the codesters library e.g. "python example.py example1.py"'
+        exit()
+
+    filename = sys.argv[1]
+    example.run(filename)
