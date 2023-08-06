cda501ea8cb807fa65f8d2eebdb1dd6ed781e6c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:26 +0000

    Add package pyetcdlock/1.1.0/pyetcdlock-1.1.0.tar.gz

---
+++ packages/pyetcdlock/pyetcdlock-1.1.0.tar.gz/pyetcdlock-1.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+
+version = '1.1.0'
+
+install_requires = [
+    'python-etcd'
+]
+
+setup(name='pyetcdlock',
+    version=version,
+    description="python etcd network mutux lock,support watch ,force ,incr lock time",
+    classifiers = [
+         'Development Status :: 2 - Pre-Alpha',
+         'Intended Audience :: Developers',
+         'License :: OSI Approved :: MIT License',
+         'Programming Language :: Python :: 2.7',
+         'Programming Language :: Python :: 3.0',
+         'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords = ['mutux network lock based on etcd','fengyun'],
+    author='ruifengyun',
+    author_email='rfyiamcool@163.com',
+    url='https://github.com/rfyiamcool',
+    license='MIT',
+    packages=['pyetcdlock'],
+    install_requires=install_requires,
+)
