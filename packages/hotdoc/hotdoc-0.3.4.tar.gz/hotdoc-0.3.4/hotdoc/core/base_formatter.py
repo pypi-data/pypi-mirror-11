a9c23e5dc55056a30dfd8bcbef8ca82b35b4154a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:42 +0000

    Add package hotdoc/0.3.4/hotdoc-0.3.4.tar.gz

---
+++ packages/hotdoc/hotdoc-0.3.4.tar.gz/hotdoc-0.3.4/hotdoc/core/base_formatter.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+
+import os
+import shutil
+import CommonMark
+
+from xml.sax.saxutils import unescape
+
+from .doc_tool import doc_tool, ConfigError
+from .symbols import Symbol
+from ..utils.simple_signals import Signal
+from ..utils.loggable import progress_bar
+
+class Formatter(object):
+    def __init__ (self):
+        # Used to warn subclasses a method isn't implemented
+        self.__not_implemented_methods = {}
+
+        self.formatting_symbol_signals = {}
+        for klass in doc_tool.symbol_factory.symbol_subclasses:
+            self.formatting_symbol_signals[klass] = Signal()
+        self._output = doc_tool.output
+
+        # Hardcoded for now
+        self.__cmp = CommonMark.DocParser()
+        self.__cmr = CommonMark.HTMLRenderer()
+
+    def format (self):
+        self.__total_sections = 0
+        self.__total_rendered_sections = 0
+        for section in doc_tool.sections:
+            self.__total_sections += self.__get_subsections_count (section)
+
+        self.__progress_bar = progress_bar.get_progress_bar ()
+        if self.__progress_bar is not None:
+            self.__progress_bar.set_header("Rendering Sections (2 / 2)")
+            self.__progress_bar.clear()
+            self.__update_progress ()
+        
+        for section in doc_tool.sections:
+            self.__format_section (section)
+
+        self.__copy_extra_files ()
+
+    def __update_progress (self):
+        if self.__progress_bar is None:
+            return
+
+        if self.__total_sections == 0:
+            return
+
+        percent = float (self.__total_rendered_sections) / float (self.__total_sections)
+        self.__progress_bar.update (percent, "%d / %d" %
+                (self.__total_rendered_sections, self.__total_sections))
+
+    def format_symbol (self, symbol):
+        res = self.formatting_symbol_signals[type(symbol)](symbol)
+
+        if False in res:
+            return False
+
+        res = self.formatting_symbol_signals[Symbol](symbol)
+
+        if False in res:
+            return False
+
+        symbol.formatted_doc = self.__format_doc (symbol.comment)
+        out, standalone = self._format_symbol (symbol)
+        symbol.detailed_description = out
+        if standalone:
+            self.__write_symbol (symbol)
+
+        return True
+
+    def __get_subsections_count (self, section):
+        count = 1
+        for subsection in section.sections:
+            count += self.__get_subsections_count (subsection)
+        return count
+
+    def __format_section (self, section):
+        out = ""
+        section.do_format ()
+        self.__total_rendered_sections += 1
+        self.__update_progress()
+
+        for section in section.sections:
+            self.__format_section (section)
+
+    def __copy_extra_files (self):
+        for f in self._get_extra_files():
+            basename = os.path.basename (f)
+            shutil.copy (f, os.path.join (self._output, basename))
+
+    def __write_symbol (self, symbol):
+        path = os.path.join (self._output, symbol.link.pagename)
+        with open (path, 'w') as f:
+            out = symbol.detailed_description
+            f.write (out.encode('utf-8'))
+
+    def __format_doc_string (self, docstring):
+        if not docstring:
+            return ""
+
+        out = ""
+        docstring = unescape (docstring)
+        docstring = doc_tool.doc_parser.translate (docstring)
+        ast = self.__cmp.parse (docstring.encode('utf-8'))
+        rendered_text = self.__cmr.render(ast)
+        return rendered_text
+
+    def __format_doc (self, comment):
+        out = ""
+        if comment:
+            out += self.__format_doc_string (comment.description)
+        return out
+
+    def __warn_not_implemented (self, func):
+        if func in self.__not_implemented_methods:
+            return
+        self.__not_implemented_methods [func] = True
+
+    # Virtual methods
+
+    def _get_extension (self):
+        """
+        The extension to append to the filename
+        ('markdown', 'html')
+        """
+        self.__warn_not_implemented (self._get_extension)
+        return ""
+
+    def _get_extra_files (self):
+        return []
---
+++ packages/hotdoc/hotdoc-0.3.4.tar.gz/hotdoc-0.3.4/hotdoc/core/base_page_parser.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+import os
+from .doc_tool import doc_tool
+from ..utils.loggable import Loggable
+
+class ParsedPage(object):
+    def __init__(self):
+        self.ast = None
+        self.headers = []
+
+class PageParser(Loggable):
+    def __init__(self):
+        Loggable.__init__(self)
+        self.sections = []
+        self._current_section = None
+        self.__parsed_pages = []
+        self._prefix = ""
+        self.__total_documented_symbols = 0
+
+    def create_section (self, section_name, filename):
+        comment = doc_tool.comments.get("SECTION:%s" % section_name.lower())
+        symbol = doc_tool.source_scanner.symbols.get(section_name)
+        if not symbol:
+            symbol = section_name
+        section = doc_tool.symbol_factory.make_section (symbol, comment)
+        section.source_file = filename
+        section.link.pagename = "%s.%s" % (section_name, "html")
+
+        if self._current_section:
+            self._current_section.sections.append (section)
+        else:
+            self.sections.append (section)
+
+        self._current_section = section
+
+    def create_symbol (self, symbol_name):
+        if not self._current_section:
+            return
+
+        symbol = doc_tool.source_scanner.symbols.get (symbol_name)
+        if symbol:
+            comment_block = doc_tool.comments.get (symbol_name)
+            if comment_block:
+                sym = doc_tool.symbol_factory.make (symbol,
+                        comment_block)
+                if sym:
+                    self._current_section.add_symbol (sym)
+                    self.__total_documented_symbols += 1
+
+    def __update_dependencies (self, sections):
+        for s in sections:
+            if not s.symbols:
+                doc_tool.dependency_tree.add_dependency (s.source_file,
+                        None)
+            for sym in s.symbols:
+                if not hasattr (sym._symbol, "location"):
+                    continue
+
+                filename = str (sym._symbol.location.file)
+                doc_tool.dependency_tree.add_dependency (s.source_file, filename)
+                comment_filename = sym.comment.filename
+                doc_tool.dependency_tree.add_dependency (s.source_file, comment_filename)
+
+            self.__update_dependencies (s.sections)
+
+    def _parse_page (self, filename, section_name):
+        filename = os.path.abspath (filename)
+        if not os.path.isfile (filename):
+            return None
+
+        if filename in self.__parsed_pages:
+            return None
+
+        self.__parsed_pages.append (filename)
+        self.create_section (section_name, filename)
+
+        with open (filename, "r") as f:
+            contents = f.read()
+
+        old_section = self._current_section
+        old_section.parsed_page = self.do_parse_page (contents, self._current_section)
+
+        return old_section
+
+    def create_symbols(self):
+        self._prefix = os.path.dirname (doc_tool.index_file)
+        if doc_tool.dependency_tree.initial:
+            self._parse_page (doc_tool.index_file, "index")
+        else:
+            for filename in doc_tool.dependency_tree.stale_sections:
+                section_name = os.path.splitext(os.path.basename (filename))[0]
+                self._parse_page (filename, section_name)
+        self.__update_dependencies (self.sections)
+        self.info ("total documented symbols : %d" %
+                self.__total_documented_symbols)
