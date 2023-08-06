75065461d43964e52954278ab861623b877577c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:43 +0000

    Add package BatAlgorithm/0.1/BatAlgorithm-0.1.tar.gz

---
+++ packages/BatAlgorithm/BatAlgorithm-0.1.tar.gz/BatAlgorithm-0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='BatAlgorithm',
+      description='Bat algorithm implementation',
+      author='Iztok Fister Jr. and Marko Burjek',
+      version='0.1',
+      url='https://github.com/firefly-cpp/BatAlgorithm',
+      py_modules=['BatAlgorithm']
+      ) 
+
