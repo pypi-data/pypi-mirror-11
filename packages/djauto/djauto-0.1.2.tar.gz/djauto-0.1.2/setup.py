e62078618d083e8f663e0d1690380f64117e1b84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:57 +0000

    Add package djauto/0.1.2/djauto-0.1.2.tar.gz

---
+++ packages/djauto/djauto-0.1.2.tar.gz/djauto-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(name='djauto',
+      version='0.1.2',
+      description='Django project generator',
+      url='https://github.com/vikas-parashar/django-project-generator',
+      author='Vikas Parashar',
+      author_email='svnitvikas@gmai.com',
+      license='MIT',
+      classifiers=[
+              'Development Status :: 3 - Alpha',
+              'Environment :: Web Environment',
+              'Intended Audience :: Developers',
+              'Operating System :: OS Independent',
+              'Programming Language :: Python',
+              'Topic :: Software Development',
+              'Programming Language :: Python :: 2.6',
+              'Programming Language :: Python :: 2.7',
+          ],
+      packages=['djauto'],
+      entry_points = {
+        'console_scripts': ['djauto=djauto.auto:main'],
+        },
+      zip_safe=False)
