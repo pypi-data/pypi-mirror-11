ccc2a9650470a5fab85d1ea53dd3cf69c6c7619d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:45 +0000

    Add package ityou.thumbnails/1.3.1/ityou.thumbnails-1.3.1.tar.gz

---
+++ packages/ityou.thumbnails/ityou.thumbnails-1.3.1.tar.gz/ityou.thumbnails-1.3.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.3.1'
+
+setup(name='ityou.thumbnails',
+      version=version,
+      description="Generate Thumnails",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='ITYOU/LM',
+      author_email='lm@ityou.de',
+      url='http://www.ityou.de/esi/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ityou'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'python-magic',
+          # -*- Extra requirements: -*-
+          'ityou.esi.theme',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      #setup_requires=["PasteScript"],
+      #paster_plugins=["ZopeSkel"],
+      )
