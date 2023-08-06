e70d963e850cad776d0f8e277b740300b86b085a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:08 +0000

    Add package PyForms/0.1.2/PyForms-0.1.2.tar.gz

---
+++ packages/PyForms/PyForms-0.1.2.tar.gz/PyForms-0.1.2/pyforms/gui/Controls/ControlCombo.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+""" pyforms.gui.Controls.ControlCombo """
+
+import pyforms.Utils.tools as tools
+from PyQt4 import uic
+from pyforms.gui.Controls.ControlBase import ControlBase
+
+__author__ = "Ricardo Ribeiro"
+__copyright__ = ""
+__credits__ = "Ricardo Ribeiro"
+__license__ = "MIT"
+__version__ = "0.0"
+__maintainer__ = ["Ricardo Ribeiro", "Carlos Mão de Ferro"]
+__email__ = ["ricardojvr at gmail.com", "cajomferro at gmail.com"]
+__status__ = "Development"
+
+
+class ControlCombo(ControlBase):
+    """This class represents a wrapper to the combo box"""
+
+    _items = None
+
+    def initForm(self):
+        control_path = tools.getFileInSameDirectory(__file__, "comboInput.ui")
+        self._form = uic.loadUi(control_path)
+        self._form.label.setText(self._label)
+        self._form.comboBox.currentIndexChanged.connect(
+            self.currentIndexChanged)
+        self._form.comboBox.activated.connect(
+            self._activated)
+        self._form.comboBox.highlighted.connect(
+            self._highlighted)
+
+        self._items = {}
+
+        self._addingItem = False
+
+    def currentIndexChanged(self, index):
+        """Called when the user chooses an item in the combobox and 
+        the selected choice is different from the last one selected.
+        @index: item's index
+        """
+        if not self._addingItem:
+            item = self._form.comboBox.currentText()
+            if len(item) >= 1:
+                ControlBase.value.fset(self, self._items[str(item)])
+
+    def _activated(self, index):
+        self.activated(index)
+
+    def activated(self, index):
+        """Called when the user chooses an item in the combobox.
+        Note that this signal happens even when the choice is not changed
+        @index: item's index
+        """
+        pass
+
+    def _highlighted(self, index):
+        """Called when an item in the combobox popup
+         list is highlighted by the user.
+         @index: item's index
+        """
+        self.highlighted(index)
+
+    def highlighted(self, index):
+        pass
+
+    def addItem(self, label, value=None):
+        self._addingItem = True
+        if not (label in self._items.keys()):
+            self._form.comboBox.addItem(label)
+
+        firstValue = False
+        if self._items == {}:
+            firstValue = True
+
+        if value is None:
+            self._items[label] = label
+        else:
+            self._items[label] = value
+        self._addingItem = False
+
+        if firstValue:
+            self.value = self._items[label]
+
+    def clearItems(self):
+        self._items = {}
+        self._value = None
+        self._form.comboBox.clear()
+
+    @property
+    def items(self): return self._items.items()
+
+    @property
+    def value(self): return self._value
+
+    @value.setter
+    def value(self, value):
+        for key, val in self.items:
+            if value == val:
+                index = self._form.comboBox.findText(key)
+                self._form.comboBox.setCurrentIndex(index)
+                if self._value != value:
+                    self.changed()
+                self._value = val
+
+    @property
+    def text(self): return str(self._form.comboBox.currentText())
+
+    @text.setter
+    def text(self, value):
+        for key, val in self.items:
+            if value == key:
+                self.value = val
+                break
---
+++ packages/PyForms/PyForms-0.1.2.tar.gz/PyForms-0.1.2/pyforms/gui/Controls/ControlEventTimeline/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+""" pyforms.gui.Controls.ControlEventTimeline.__init__
+
+"""
+
+from pyforms.gui.Controls.ControlEventTimeline import ControlEventTimeline
+from pyforms.gui.Controls.ControlEventTimeline import TimelineWidget
+from pyforms.gui.Controls.ControlEventTimeline import TimelineDelta
+from pyforms.gui.Controls.ControlEventTimeline import TimelinePointer
+from pyforms.gui.Controls.ControlEventTimeline import TimelinePopupWindow
+
+__author__ = ["Ricardo Ribeiro", "Hugo Cachitas"]
+__credits__ = ["Ricardo Ribeiro", "Hugo Cachitas"]
+__license__ = "MIT"
+__version__ = "0.0"
+__maintainer__ = "Ricardo Ribeiro"
+__email__ = "ricardojvr@gmail.com"
+__status__ = "Development"
+
+__all__ = ['ControlEventTimeline',
+           'TimelineWidget',
+           'TimelineDelta',
+           'TimelinePointer',
+           'TimelinePopupWindow']
