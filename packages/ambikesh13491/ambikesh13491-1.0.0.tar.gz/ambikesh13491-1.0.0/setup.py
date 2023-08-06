2944332e3231dc8785671b9fdf8722fab5923d5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:27 +0000

    Add package ambikesh13491/1.0.0/ambikesh13491-1.0.0.tar.gz

---
+++ packages/ambikesh13491/ambikesh13491-1.0.0.tar.gz/ambikesh13491-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+name = 'ambikesh13491',
+version = '1.0.0',
+py_modules = ['ambikesh13491'],
+author = 'Ambikesh',
+author_email = 'ambikeshofficial@gmail.com',
+url = 'http://www.headfirstlabs.com',
+description = 'Printer of nested lists',
+)
