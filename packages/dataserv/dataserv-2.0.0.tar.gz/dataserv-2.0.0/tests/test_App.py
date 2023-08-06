b7536f5203e629c7629a32ebeaa3a9b9168620c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:52 +0000

    Add package dataserv/2.0.0/dataserv-2.0.0.tar.gz

---
+++ packages/dataserv/dataserv-2.0.0.tar.gz/dataserv-2.0.0/tests/test_App.py
@@ -0,0 +1,230 @@
+import unittest
+import json
+from btctxstore import BtcTxStore
+from dataserv.run import app, db
+from dataserv.app import secs_to_mins, online_farmers
+from email.utils import formatdate
+from datetime import datetime
+from datetime import timedelta
+from time import mktime
+
+
+class AppTest(unittest.TestCase):
+
+    # setup
+    def setUp(self):
+        app.config["SKIP_AUTHENTICATION"] = True  # monkey patch
+        self.app = app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    # simple index test
+    def test_hello_world(self):
+        rv = self.app.get('/')
+        self.assertEqual(b"Hello World.", rv.data)
+
+    # register call
+    def test_register(self):
+        addr = '191GVvAaTRxLmz3rW3nU5jAV1rF186VxQc'
+        rv = self.app.get('/api/register/{0}'.format(addr))
+
+        # good registration
+        self.assertEqual(b"User registered.", rv.data)
+        self.assertEqual(rv.status_code, 200)
+
+        # duplicate registration
+        rv = self.app.get('/api/register/{0}'.format(addr))
+        self.assertEqual(b"Registration Failed: Address already is registered.", rv.data)
+        self.assertEqual(rv.status_code, 409)
+
+    def test_register_invalid_address(self):
+        addr = '191GVvAaTRxLmz3rW3nU5jAV1rF186VxQc_this_is_not_an_address'
+        rv = self.app.get('/api/register/{0}'.format(addr))
+
+        # invalid address
+        self.assertEqual(b"Registration Failed: Invalid Bitcoin address.", rv.data)
+        self.assertEqual(rv.status_code, 400)
+
+    # ping call
+    def test_ping_good(self):
+        addr = '191GVvAaTRxLmz3rW3nU5jAV1rF186VxQc'
+        rv = self.app.get('/api/register/{0}'.format(addr))
+
+        # good registration
+        self.assertEqual(b"User registered.", rv.data)
+        self.assertEqual(rv.status_code, 200)
+
+        # now test ping
+        rv = self.app.get('/api/ping/{0}'.format(addr))
+
+        # good ping
+        self.assertEqual(b"Ping accepted.", rv.data)
+        self.assertEqual(rv.status_code, 200)
+
+    def test_ping_not_found(self):
+        addr = '191GVvAaTRxLmz3rW3nU5jAV1rF186VxQc'
+        # no registration
+
+        # now test ping
+        rv = self.app.get('/api/ping/{0}'.format(addr))
+
+        # good ping
+        self.assertEqual(b"Ping Failed: Farmer not found.", rv.data)
+        self.assertEqual(rv.status_code, 404)
+
+    def test_ping_invalid_address(self):
+        addr = 'notvalidaddress'
+
+        # now test ping
+        rv = self.app.get('/api/ping/{0}'.format(addr))
+
+        # good ping
+        self.assertEqual(b"Ping Failed: Invalid Bitcoin address.", rv.data)
+        self.assertEqual(rv.status_code, 400)
+
+    # time helper
+    def test_helper_time(self):
+        time1 = 15
+        time2 = 75
+        time3 = 4000
+
+        self.assertEqual(secs_to_mins(time1), "15 second(s)")
+        self.assertEqual(secs_to_mins(time2), "1 minute(s)")
+        self.assertEqual(secs_to_mins(time3), "1 hour(s)")
+
+    # online call
+    def test_online(self):
+        addr = '191GVvAaTRxLmz3rW3nU5jAV1rF186VxQc'
+        rv = self.app.get('/api/register/{0}'.format(addr))
+
+        # good registration
+        self.assertEqual(b"User registered.", rv.data)
+        self.assertEqual(rv.status_code, 200)
+
+        # now test ping
+        self.app.get('/api/ping/{0}'.format(addr))
+
+        # get online data
+        rv = self.app.get('/api/online')
+
+        # see if that address is in the online status
+        self.assertTrue(addr in str(rv.data))
+
+    def test_farmer_set_height(self):
+        addr1 = '191GVvAaTRxLmz3rW3nU5jAV1rF186VxQc'
+        addr2 = 'notvalidaddress'
+
+        # not found
+        rv = self.app.get('/api/height/{0}/1'.format(addr1))
+        self.assertEqual(rv.status_code, 404)
+
+        # register farmer
+        self.app.get('/api/register/{0}'.format(addr1))
+
+        # correct
+        rv = self.app.get('/api/height/{0}/5'.format(addr1))
+        self.assertEqual(rv.status_code, 200)
+        rv = self.app.get('/api/online'.format(addr1))
+        self.assertTrue(b"5" in rv.data)
+
+        # invalid btc address
+        rv = self.app.get('/api/height/{0}/1'.format(addr2))
+        self.assertEqual(rv.status_code, 400)
+
+    def test_farmer_total_bytes(self):
+        addr1 = '191GVvAaTRxLmz3rW3nU5jAV1rF186VxQc'
+        addr2 = '18c2qnUAfgF3UnJCjAz2rpWQph5xugEfkr'
+        addr3 = '1NqtfdHe3X6rqHRQjsGq5CT9LYYjTFJ1qD'
+        addr4 = '1JnaPB29Un3FBSf3e4Jzabwi1ekeKoh1Gr'
+
+        # register farmers
+        self.app.get('/api/register/{0}'.format(addr1))
+        self.app.get('/api/register/{0}'.format(addr2))
+        self.app.get('/api/register/{0}'.format(addr3))
+        self.app.get('/api/register/{0}'.format(addr4))
+
+        # set height
+        self.app.get('/api/height/{0}/{1}'.format(addr1, 0))
+        self.app.get('/api/height/{0}/{1}'.format(addr2, 2475))
+        self.app.get('/api/height/{0}/{1}'.format(addr3, 2525))
+        self.app.get('/api/height/{0}/{1}'.format(addr4, 5000))
+
+        # check online
+        rv = self.app.get('/api/online')
+        self.assertTrue(b"0" in rv.data)
+        self.assertTrue(b"2475" in rv.data)
+        self.assertTrue(b"2525" in rv.data)
+        self.assertTrue(b"5000" in rv.data)
+
+        # check total bytes
+        rv = self.app.get('/api/total')
+        json_data =  b'"total_TB": 1.22\n}'
+
+        self.assertTrue(json_data in  rv.data)
+
+    def test_farmer_order(self):
+        addr1 = '191GVvAaTRxLmz3rW3nU5jAV1rF186VxQc'
+        addr2 = '18c2qnUAfgF3UnJCjAz2rpWQph5xugEfkr'
+        addr3 = '1NqtfdHe3X6rqHRQjsGq5CT9LYYjTFJ1qD'
+
+        # register farmers
+        self.app.get('/api/register/{0}'.format(addr1))
+        self.app.get('/api/register/{0}'.format(addr2))
+        self.app.get('/api/register/{0}'.format(addr3))
+
+        # set height
+        self.app.get('/api/height/{0}/{1}'.format(addr1, 0))
+        self.app.get('/api/height/{0}/{1}'.format(addr2, 2475))
+        self.app.get('/api/height/{0}/{1}'.format(addr3, 2525))
+
+        # get farmers
+        farmers = online_farmers()
+        self.assertEqual(farmers[0].btc_addr, addr3)
+        self.assertEqual(farmers[1].btc_addr, addr2)
+        self.assertEqual(farmers[2].btc_addr, addr1)
+
+        # set height
+        self.app.get('/api/height/{0}/{1}'.format(addr1, 5000))
+
+        # get farmers
+        farmers = online_farmers()
+        self.assertEqual(farmers[0].btc_addr, addr1)
+
+    def test_get_address(self):
+        rv = self.app.get('/api/address')
+        self.assertEqual(rv.status_code, 200)
+        data = rv.data.decode("utf-8")
+        self.assertEqual(app.config["ADDRESS"], json.loads(data)["address"])
+
+
+class AppAuthenticationHeadersTest(unittest.TestCase):
+
+    def setUp(self):
+        app.config["SKIP_AUTHENTICATION"] = False  # monkey patch
+        self.app = app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_success(self):
+        blockchain = BtcTxStore()
+        wif = blockchain.create_key()
+        address = blockchain.get_address(wif)
+
+        # create header date and authorization signature
+        header_date = formatdate(timeval=mktime(datetime.now().timetuple()),
+                                 localtime=True, usegmt=True)
+        message = app.config["ADDRESS"] + " " + header_date
+        header_authorization = blockchain.sign_unicode(wif, message)
+
+        headers = {"Date": header_date, "Authorization": header_authorization }
+        url = '/api/register/{0}'.format(address)
+        rv = self.app.get(url, headers=headers)
+        self.assertEqual(b"User registered.", rv.data)
+        self.assertEqual(rv.status_code, 200)
+
---
+++ packages/dataserv/dataserv-2.0.0.tar.gz/dataserv-2.0.0/tests/test_Farmer.py
@@ -0,0 +1,155 @@
+import json
+import unittest
+from btctxstore import BtcTxStore
+from dataserv.app import db, app
+from dataserv.Farmer import sha256
+from dataserv.Farmer import Farmer
+from email.utils import formatdate
+from datetime import datetime
+from datetime import timedelta
+from time import mktime
+
+
+class FarmerTest(unittest.TestCase):
+
+    def setUp(self):
+        app.config["SKIP_AUTHENTICATION"] = True  # monkey patch
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_repr(self):
+        farmer = Farmer('191GVvAaTRxLmz3rW3nU5jAV1rF186VxQc')
+        ans = "<Farmer BTC Address: '191GVvAaTRxLmz3rW3nU5jAV1rF186VxQc'>"
+        self.assertEqual(repr(farmer), ans)
+
+    def test_sha256(self):
+        ans = 'c059c8035bbd74aa81f4c787c39390b57b974ec9af25a7248c46a3ebfe0f9dc8'
+        self.assertEqual(sha256("storj"), ans)
+        self.assertNotEqual(sha256("not storj"), ans)
+
+    def test_register(self):
+        addr1 = '191GVvAaTRxLmz3rW3nU5jAV1rF186VxQc'
+        addr2 = '191GVvAaTRxLmz3rW3nU5jAV1rF186VxQc9999ghjfghj99'
+        addr3 = 'not valid address'
+
+        # create Farmer objects
+        farmer1 = Farmer(addr1)
+        farmer2 = Farmer(addr2)
+        farmer3 = Farmer(addr3)
+
+        # test registration of first farmer
+        self.assertFalse(farmer1.exists())
+        farmer1.register()
+        self.assertTrue(farmer1.exists())
+
+        # test duplicate
+        self.assertRaises(LookupError, farmer1.register)
+
+        # these should not be inserted
+        self.assertRaises(ValueError, farmer2.register)
+        self.assertRaises(ValueError, farmer3.register)
+
+        # double check they are not in the db
+        self.assertFalse(farmer2.exists())
+        self.assertFalse(farmer3.exists())
+
+    def test_ping(self):
+        farmer = Farmer('191GVvAaTRxLmz3rW3nU5jAV1rF186VxQc')
+
+        # test ping before registration
+        self.assertRaises(LookupError, farmer.ping)
+
+        # register farmer
+        farmer.register()
+
+        # get register time, and make sure the ping work
+        register_time = farmer.last_seen
+        farmer.ping()  # update last seen
+        ping_time = farmer.last_seen
+        self.assertTrue(register_time < ping_time)
+
+    def test_height(self):
+        farmer = Farmer('191GVvAaTRxLmz3rW3nU5jAV1rF186VxQc')
+        farmer.register()
+
+        # set height and check function output
+        self.assertEqual(farmer.height, 0)
+        self.assertEqual(farmer.set_height(5), 5)
+        self.assertEqual(farmer.height, 5)
+
+        # check the db object as well
+        farmer2 = farmer.lookup()
+        self.assertEqual(farmer2.height, 5)
+
+    def test_audit(self):
+        farmer = Farmer('191GVvAaTRxLmz3rW3nU5jAV1rF186VxQc')
+
+        # test audit before registration
+        self.assertRaises(LookupError, farmer.audit)
+
+        # register farmer
+        farmer.register()
+
+        # get register time, and make sure the ping work
+        register_time = farmer.last_seen
+        farmer.audit()
+        ping_time = farmer.last_seen
+        self.assertTrue(register_time < ping_time)
+
+    def test_to_json(self):
+        farmer = Farmer('191GVvAaTRxLmz3rW3nU5jAV1rF186VxQc')
+        farmer.register()
+
+        farmer.ping()
+        farmer.set_height(50)
+
+        test_payload = u'{"height": 50, "btc_addr": "191GVvAaTRxLmz3rW3nU5jAV1rF186VxQc", "last_seen": 0}'
+        test_json = json.loads(test_payload)
+        call_payload = json.loads(farmer.to_json())
+        self.assertEqual(test_json, call_payload)
+
+
+class FarmerAuthenticationTest(unittest.TestCase):
+
+    def setUp(self):
+        app.config["SKIP_AUTHENTICATION"] = False  # monkey patch
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_authentication_success(self):
+        blockchain = BtcTxStore()
+        wif = blockchain.create_key()
+        address = blockchain.get_address(wif)
+        farmer = Farmer(address)
+
+        header_date = formatdate(timeval=mktime(datetime.now().timetuple()),
+                                 localtime=True, usegmt=True)
+        message = farmer.get_server_address() + " " + header_date
+        header_authorization = blockchain.sign_unicode(wif, message)
+        self.assertTrue(farmer.authenticate(header_authorization, header_date))
+
+    def test_authentication_timeout(self):
+        def callback():
+            blockchain = BtcTxStore()
+            wif = blockchain.create_key()
+            address = blockchain.get_address(wif)
+            farmer = Farmer(address)
+
+            timeout = farmer.get_server_authentication_timeout()
+
+            date = datetime.now() - timedelta(seconds=timeout)
+            header_date = formatdate(timeval=mktime(date.timetuple()),
+                                     localtime=True, usegmt=True)
+            message = farmer.get_server_address() + " " + header_date
+            header_authorization = blockchain.sign_unicode(wif, message)
+            farmer.authenticate(header_authorization, header_date)
+        self.assertRaises(ValueError, callback)
+
+    # TODO test incorrect address
+    # TODO test incorrect signature
