30c3436194a7f169dd52097a4200220732a29a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:18 +0000

    Add package collectd-haproxy/1.0.1/collectd-haproxy-1.0.1.tar.gz

---
+++ packages/collectd-haproxy/collectd-haproxy-1.0.1.tar.gz/collectd-haproxy-1.0.1/collectd_haproxy/__init__.py
@@ -0,0 +1,15 @@
+version_info = (1, 0, 1)
+
+__version__ = ".".join(map(str, version_info))
+
+try:
+    import collectd
+    collectd_present = True
+except ImportError:
+    collectd_present = False
+
+from .plugin import HAProxyPlugin
+
+
+if collectd_present:
+    HAProxyPlugin.register(collectd)
