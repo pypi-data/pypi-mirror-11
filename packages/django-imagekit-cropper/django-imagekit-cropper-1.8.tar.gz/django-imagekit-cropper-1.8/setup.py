6a274482486e74314f23186732bc185c3818e40a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:50 +0000

    Add package django-imagekit-cropper/1.8/django-imagekit-cropper-1.8.tar.gz

---
+++ packages/django-imagekit-cropper/django-imagekit-cropper-1.8.tar.gz/django-imagekit-cropper-1.8/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'django-imagekit-cropper',
+    packages = ['imagekit_cropper'],
+    version = '1.8',
+    description = 'Allow users to manually specify image variant crops',
+    author = 'Nina Pavlich',
+    author_email='nina@ninalp.com',
+    url = 'https://github.com/ninapavlich/django-imagekit-cropper',
+    keywords = ['libraries', 'web development', 'cms', 'django', 'django-grappelli'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ]
+)
