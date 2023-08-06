3cfda9da25e0854655de683e8a494257363812b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:57 +0000

    Add package SQLAlchemy-JSONAPI/2.1.1/SQLAlchemy-JSONAPI-2.1.1.zip

---
+++ packages/SQLAlchemy-JSONAPI/SQLAlchemy-JSONAPI-2.1.1.zip/SQLAlchemy-JSONAPI-2.1.1/sqlalchemy_jsonapi/__init__.py
@@ -0,0 +1,4 @@
+from .flaskext import FlaskJSONAPI, Method, Endpoint
+from .serializer import (JSONAPI, AttributeActions, RelationshipActions,
+                         Permissions, attr_descriptor, relationship_descriptor,
+                         permission_test)
