025a8293c3f7cd5d188b569032915ef0484fd0ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:58 +0000

    Add package thu/1.0.0/thu-1.0.0.zip

---
+++ packages/thu/thu-1.0.0.zip/thu-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name='thu',
+        version='1.0.0',
+        py_modules=['thu'],
+        author='kijo',
+        author_email='123@163.com',
+        url='1123.com',
+        description='Sorry ,This is practice!'
+
+    )
