df6b2f72d64e93b086df7ddd923b84c6087087ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:29 +0000

    Add package ipfs-api/0.1.5/ipfs-api-0.1.5.tar.gz

---
+++ packages/ipfs-api/ipfs-api-0.1.5.tar.gz/ipfs-api-0.1.5/ipfsApi/exceptions.py
@@ -0,0 +1,14 @@
+class InvalidCommand(Exception):
+    pass
+
+class InvalidArguments(Exception):
+    pass
+
+class InvalidPath(Exception):
+    pass
+
+class FileCommandException(Exception):
+    pass
+
+class EncodingException(Exception):
+    pass
