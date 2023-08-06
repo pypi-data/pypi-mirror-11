6b0952f2f5598f76beb5d22c9d14a5ce0c18cc82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:40 +0000

    Add package aliyun-python-sdk/1.0.0/aliyun-python-sdk-1.0.0.zip

---
+++ packages/aliyun-python-sdk/aliyun-python-sdk-1.0.0.zip/aliyun-python-sdk-1.0.0/setup.py
@@ -0,0 +1,18 @@
+__author__ = 'yudan.lyd'
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'aliyun-python-sdk',
+    version = '1.0.0',
+    keywords = ('aliyun', 'sdk', 'openapi'),
+    description = 'All modules of Aliyun python sdk',
+    license = 'License',
+    install_requires = [],
+
+    author = 'Septimus',
+    author_email = 'yudan.lyd@alibaba-inc.com',
+
+    packages = find_packages(),
+    platforms = 'any',
+)
