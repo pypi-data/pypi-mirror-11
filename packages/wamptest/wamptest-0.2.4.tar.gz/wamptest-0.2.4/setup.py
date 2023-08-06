7136670c2fda5b61ad110bafd2a01cd73f52060c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:13 +0000

    Add package wamptest/0.2.4/wamptest-0.2.4.tar.gz

---
+++ packages/wamptest/wamptest-0.2.4.tar.gz/wamptest-0.2.4/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+    name='wamptest',
+    packages=['wamptest'],
+    version='0.2.4',
+    description='This is a library designed to test WAMP Crossbar connections',
+    author='Eric Chapman',
+    license='MIT',
+    author_email='eric@headquartershq.com',
+    url='',
+    download_url='',
+    keywords=['testing', 'wamp', 'crossbar'],
+    install_requires=['crossbar'],
+    classifiers=[],
+)
