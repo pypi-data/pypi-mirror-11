475e7e13543eea1a095b5967e4cf593b4a2b3e19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:48 +0000

    Add package analyst-remote-control/0.2/analyst-remote-control-0.2.zip

---
+++ packages/analyst-remote-control/analyst-remote-control-0.2.zip/analyst-remote-control-0.2/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import sys
+from distutils.core import setup
+sys.path.insert(0, '.')
+import version
+
+
+setup(name='analyst-remote-control',
+      version=version.getVersion(),
+      description=open('README.md', 'rb').read(),
+      author='Christian Fobel',
+      author_email='christian@fobel.net',
+      url='https://github.com/wheeler-microfluidics/analyst-remote-control',
+      install_requires=['pyzmq'],
+      packages=['analyst_remote_control'])
