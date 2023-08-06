2bd57cdc02c38fa550c8fa3f04d3a7f44cc8c5e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:51 +0000

    Add package WordBuilder/0.1.1/WordBuilder-0.1.1.tar.gz

---
+++ packages/WordBuilder/WordBuilder-0.1.1.tar.gz/WordBuilder-0.1.1/setup.py
@@ -0,0 +1,16 @@
+''' setup script '''
+from distutils.core import setup
+
+setup(
+    name='WordBuilder',
+    version='0.1.1',
+    author='Mouse Reeve',
+    author_email='mouse.reeve@gmail.com',
+
+    packages=['app'],
+
+    include_package_data=True,
+
+    url='http://pypi.python.org/pypi/WordBuilder_v011/',
+)
+
