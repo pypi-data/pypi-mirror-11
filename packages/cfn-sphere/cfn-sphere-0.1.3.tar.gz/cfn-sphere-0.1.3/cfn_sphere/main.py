604fb7a70504f324494768247837ef303d020809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:34 +0000

    Add package cfn-sphere/0.1.3/cfn-sphere-0.1.3.tar.gz

---
+++ packages/cfn-sphere/cfn-sphere-0.1.3.tar.gz/cfn-sphere-0.1.3/cfn_sphere/main.py
@@ -0,0 +1,39 @@
+from cfn_sphere.resolver.dependency_resolver import DependencyResolver
+from cfn_sphere.resolver.parameter_resolver import ParameterResolver
+from cfn_sphere.cloudformation.api import CloudFormation, CloudFormationTemplate
+from cfn_sphere.util import get_logger
+
+
+class StackActionHandler(object):
+    def __init__(self, config, working_dir, user_input_handler=None):
+        self.working_dir = working_dir
+        self.logger = get_logger()
+        self.config = config
+        self.region = config.region
+        self.cfn = CloudFormation(region=self.region)
+        self.parameter_resolver = ParameterResolver(region=self.region)
+        self.user_input_handler = user_input_handler
+
+    def create_or_update_stacks(self):
+        existing_stacks = self.cfn.get_stack_names()
+        desired_stacks = self.config.stacks
+        stack_processing_order = DependencyResolver().get_stack_order(desired_stacks)
+
+        self.logger.info("Will process stacks in the following order: {0}".format(", ".join(stack_processing_order)))
+
+        for stack_name in stack_processing_order:
+            stack_config = self.config.stacks.get(stack_name)
+
+            template_url = stack_config.template
+            template = CloudFormationTemplate(template_url, working_dir=self.working_dir)
+
+            parameters = self.parameter_resolver.resolve_parameter_values(stack_config.parameters)
+
+            if stack_name in existing_stacks:
+
+                if not self.cfn.stack_is_in_good_state(stack_name):
+                    raise Exception("Stack {0} is in bad state".format(stack_name))
+
+                self.cfn.update_stack(stack_name=stack_name, template=template, parameters=parameters)
+            else:
+                self.cfn.create_stack(stack_name=stack_name, template=template, parameters=parameters)
---
+++ packages/cfn-sphere/cfn-sphere-0.1.3.tar.gz/cfn-sphere-0.1.3/cfn_sphere/resolver/dependency_resolver.py
@@ -0,0 +1,63 @@
+import networkx
+from networkx.exception import NetworkXUnfeasible
+
+
+class DependencyResolver(object):
+    @staticmethod
+    def get_stack_name_from_ref_value(value):
+        assert value, "No value given"
+        assert not value.startswith('.'), "Reference value should not start with a dot"
+        assert value.__contains__('.'), "Reference value must contain a dot"
+        return value.split('.')[0]
+
+    @staticmethod
+    def is_parameter_reference(value):
+        if not isinstance(value, str):
+            return False
+
+        if value.lower().startswith("ref::"):
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def get_parameter_key_from_ref_value(value):
+        if not value:
+            return None
+
+        stripped_value = value.partition('::')[2]
+        return stripped_value
+
+    @classmethod
+    def create_stacks_directed_graph(cls, desired_stacks):
+        graph = networkx.DiGraph()
+        for name in desired_stacks.keys():
+            graph.add_node(name)
+        for name, data in desired_stacks.items():
+            if data:
+                for key, value in data.parameters.items():
+                    if cls.is_parameter_reference(value):
+                        dependant_stack = cls.get_stack_name_from_ref_value(cls.get_parameter_key_from_ref_value(value))
+                        graph.add_edge(dependant_stack, name)
+
+        return graph
+
+    @staticmethod
+    def filter_unmanaged_stacks(managed_stacks, stacks):
+        return [stack for stack in stacks if stack in managed_stacks]
+
+    @classmethod
+    def get_stack_order(cls, desired_stacks):
+        graph = cls.create_stacks_directed_graph(desired_stacks)
+        try:
+            order = networkx.topological_sort_recursive(graph)
+        except NetworkXUnfeasible as e:
+            raise Exception("Could not define an order of stacks: {0}".format(e))
+
+        return cls.filter_unmanaged_stacks(desired_stacks, order)
+
+
+if __name__ == "__main__":
+    stacks = ['a', 'b', 'c']
+    managed_stacks = []
+    print DependencyResolver.filter_unmanaged_stacks(managed_stacks, stacks)
