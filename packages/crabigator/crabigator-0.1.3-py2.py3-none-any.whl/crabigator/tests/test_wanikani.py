3f31388edb6d0a9febcfd362bb86d919cb95f658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:10 +0000

    Add package crabigator/0.1.3/crabigator-0.1.3-py2.py3-none-any.whl

---
+++ packages/crabigator/crabigator-0.1.3-py2.py3-none-any.whl/crabigator/tests/test_wanikani.py
@@ -0,0 +1,23 @@
+from crabigator.wanikani import WaniKani
+import os
+from unittest import TestCase
+
+api_key = os.environ['WANIKANI_API_KEY']
+
+class TestWaniKani(TestCase):
+    def test_wanikani(self):
+        wanikani = WaniKani(api_key)
+        print(wanikani.user_information)
+        print(wanikani.study_queue)
+        print(wanikani.level_progression)
+        print(wanikani.srs_distribution)
+        print(wanikani.recent_unlocks)
+        print(wanikani.get_recent_unlocks(3))
+        print(wanikani.critical_items)
+        print(wanikani.get_recent_unlocks(65))
+        print(wanikani.radicals)
+        print(wanikani.get_radicals([1,2]))
+        print(wanikani.kanji)
+        print(wanikani.get_kanji([1,2]))
+        print(wanikani.vocabulary)
+        print(wanikani.get_vocabulary([1,2]))
