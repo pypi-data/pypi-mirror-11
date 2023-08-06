4ca43df8276d62a6167bf188b7e1641ba1d737e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:26 +0000

    Add package cmsplugin_oembed/0.1.0b1/cmsplugin_oembed-0.1.0b1.tar.gz

---
+++ packages/cmsplugin_oembed/cmsplugin_oembed-0.1.0b1.tar.gz/cmsplugin_oembed-0.1.0b1/README.md
@@ -0,0 +1,17 @@
+## djangocms oembed plugin
+
+WORK IN PROGRESS
+
+### Installation
+
+* ```pip install cmsplugin_oembed```
+* Add 
+
+  ```
+  'embed_video',
+  'cmsplugin_oembed'
+  ```
+
+  to `INSTALLED_APPS`
+
+* Sync the database, collect the static files
---
+++ packages/cmsplugin_oembed/cmsplugin_oembed-0.1.0b1.tar.gz/cmsplugin_oembed-0.1.0b1/cmsplugin_oembed/cms_plugins.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from cms.plugin_pool import plugin_pool
+from cms.plugin_base import CMSPluginBase
+
+from .models import VideoPluginModel
+
+
+class VideoEmbedPlugin(CMSPluginBase):
+
+        model = VideoPluginModel
+        name = ("Embedded Video")
+        render_template = "djangocms_oembed/_video.html"
+
+        def render(self, context, instance, placeholder):
+            video = instance.video_url
+            name = instance.name
+            context.update({
+                'name': name,
+                'item': video,
+            })
+
+            return context
+
+plugin_pool.register_plugin(VideoEmbedPlugin)
