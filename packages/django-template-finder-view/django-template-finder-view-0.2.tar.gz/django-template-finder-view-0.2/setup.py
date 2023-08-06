a430614f96f91b889fa6f2b5622e38ea594b4681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:12 +0000

    Add package django-template-finder-view/0.2/django-template-finder-view-0.2.tar.gz

---
+++ packages/django-template-finder-view/django-template-finder-view-0.2.tar.gz/django-template-finder-view-0.2/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Django template finder view
+"""
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='django-template-finder-view',
+    version='0.2',
+    url='',
+    license='LGPLv3',
+    author='Robin',
+    description='A view for automatically finding and rendering Django templates',
+    packages=['django_template_finder_view'],
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        "Django >= 1.3",
+    ],
+)
+
