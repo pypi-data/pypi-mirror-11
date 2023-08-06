d287e060a9bffe866273bd2ddb3f2adf8f332e10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:33 +0000

    Add package PyKazoo/0.0a4/PyKazoo-0.0a4.zip

---
+++ packages/PyKazoo/PyKazoo-0.0a4.zip/PyKazoo-0.0a4/pykazoo/cdrs.py
@@ -0,0 +1,20 @@
+class CDRs:
+    """  2600hz Kazoo CDRs API.
+
+        :param rest_request: The request client to use.
+            (optional, default: pykazoo.RestRequest())
+        :type rest_request: pykazoo.restrequest.RestRequest
+    """
+
+    def __init__(self, rest_request):
+        self.rest_request = rest_request
+
+    def get_cdrs(self, filters=None):
+        """ Get all CDRs.
+
+        :param filters: Kazoo Filter Parameters (see official API docs).
+        :return: Kazoo Data (see official API docs).
+        :type filters: dict, None
+        :rtype: dict
+        """
+        return self.rest_request.get('cdrs', filters)
