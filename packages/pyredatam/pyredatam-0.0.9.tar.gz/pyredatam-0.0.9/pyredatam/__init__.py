dc56f6139f035f423258f220ce79bd5e0f48ffa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:12 +0000

    Add package pyredatam/0.0.9/pyredatam-0.0.9.tar.gz

---
+++ packages/pyredatam/pyredatam-0.0.9.tar.gz/pyredatam-0.0.9/pyredatam/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Agustín Benassi'
+__email__ = 'agusbenassi@gmail.com'
+
+from pyredatam import arealist_query
+import cpv2010arg
---
+++ packages/pyredatam/pyredatam-0.0.9.tar.gz/pyredatam-0.0.9/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open("requirements.txt") as f:
+    requirements = [req.strip() for req in f.readlines()]
+
+with open("README_PIP.rst") as f:
+    readme = f.read()
+
+test_requirements = [
+    "nose",
+    "pypandoc"
+]
+
+setup(
+    name='pyredatam',
+    version='0.0.9',
+    description="Genera consultas REDATAM en python.",
+    long_description=readme,
+    author="Agustín Benassi",
+    author_email='agusbenassi@gmail.com',
+    url='https://github.com/abenassi/pyredatam',
+    packages=[
+        'pyredatam'
+    ],
+    package_dir={'pyredatam':
+                 'pyredatam'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="GPLv3+",
+    zip_safe=False,
+    keywords='pyredatam',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7'
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
