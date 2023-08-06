d0d9bf0e7e17043f2827e4c5c12dcc1f7c0cc1ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:03 +0000

    Add package testing.postgresql/1.2.1/testing.postgresql-1.2.1.tar.gz

---
+++ packages/testing.postgresql/testing.postgresql-1.2.1.tar.gz/testing.postgresql-1.2.1/setup.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import sys
+from setuptools import setup, find_packages
+
+classifiers = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: Apache Software License",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.2",
+    "Programming Language :: Python :: 3.3",
+    "Topic :: Database",
+    "Topic :: Software Development",
+    "Topic :: Software Development :: Testing",
+]
+
+install_requires = ['pg8000 >= 1.10']
+if sys.version_info < (2, 7):
+    install_requires.append('unittest2')
+
+
+setup(
+    name='testing.postgresql',
+    version='1.2.1',
+    description='automatically setups a postgresql instance in a temporary " \
+        "directory, and destroys it after testing',
+    long_description=open('README.rst').read(),
+    classifiers=classifiers,
+    keywords=[],
+    author='Takeshi Komiya',
+    author_email='i.tkomiya at gmail.com',
+    url='https://github.com/tk0miya/testing.postgresql',
+    license='Apache License 2.0',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    package_data={'': ['buildout.cfg']},
+    include_package_data=True,
+    install_requires=install_requires,
+    extras_require=dict(
+        testing=[
+            'nose',
+            'psycopg2',
+            'SQLAlchemy',
+        ],
+    ),
+    test_suite='nose.collector',
+)
