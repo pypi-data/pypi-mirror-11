8c3be09bb7a0b4298c3deaa51d295e6d4fcfe63f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:17 +0000

    Add package codestyle/0.0.17/codestyle-0.0.17.tar.gz

---
+++ packages/codestyle/codestyle-0.0.17.tar.gz/codestyle-0.0.17/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='codestyle',
+    version='0.0.17',
+    author=u'Sergey Levitin',
+    author_email='selevit@gmail.com',
+    packages=find_packages(),
+    package_data={
+        'codestyle': ['standards/*'],
+    },
+    include_package_data=True,
+    zip_safe=False,
+    url='https://github.com/selevit/codestyle',
+    license='GPL licence, see LICENCE',
+    description='Extendable codestyle checker and fixer',
+    long_description=open('README.rst').read(),
+    scripts=['scripts/codestyle'],
+    install_requires=['pep8', 'autopep8', 'pylint']
+)
