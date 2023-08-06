c5f9eece0cd68f8ca8c752c17d2e91f7e138b0c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:20 +0000

    Add package django-yaat/1.3.1/django-yaat-1.3.1.tar.gz

---
+++ packages/django-yaat/django-yaat-1.3.1.tar.gz/django-yaat-1.3.1/setup.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+from setuptools import setup
+
+vcs_dependencies = [
+    # This is a special fork where order_with_respect_to can be a tuple
+    'git+https://github.com/lovasb/django-ordered-model.git#egg=django-ordered-model'
+]
+
+setup(
+    name='django-yaat',
+    version='1.3.1',
+    packages=['yaat'],
+    install_requires=['Django>=1.8.4', 'django-restify-framework==0.23', 'django-ordered-model'],
+    include_package_data=True,
+    dependency_links=vcs_dependencies,
+    description='The django-restify-framework resource of the yaat AngularJs module',
+    author='Slapec',
+    url='https://github.com/slapec/django-yaat',
+    license='MIT',
+    zip_safe=False,
+)
