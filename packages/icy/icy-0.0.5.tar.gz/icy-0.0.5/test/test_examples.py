4c846f59a9c2c3c7eefa570f7a8c74131ec113be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:39 +0000

    Add package icy/0.0.5/icy-0.0.5.tar.gz

---
+++ packages/icy/icy-0.0.5.tar.gz/icy-0.0.5/test/test_examples.py
@@ -0,0 +1,34 @@
+import icy
+import contextlib
+import os
+from datetime import datetime
+
+class DummyFile(object):
+    def write(self): pass
+
+if __name__ == '__main__':
+    print('running examples tests ...')
+    t0 = datetime.now()
+    results = [0, 0, 0, 0]
+    for ex in sorted(icy.examples):
+        t1 = datetime.now()
+        try:
+            with contextlib.redirect_stdout(DummyFile):
+                data = icy.read(icy.examples[ex])
+            n_keys = len(data.keys())
+            if n_keys > 0:
+                print('data {:<15} [SUCCESS]   {:.1f}s, {} dfs, {}'.format(
+                    ex, (datetime.now()-t1).total_seconds(), n_keys, icy.mem(data)))
+                results[0] += 1
+            else:
+                print('data {:<15} [NO IMPORT] {:.1f}s'.format(ex, (datetime.now()-t1).total_seconds()))
+                results[1] += 1
+        except:
+            print('data {:<15} [EXCEPTION] {:.1f}s'.format(ex, (datetime.now()-t1).total_seconds()))
+            results[2] += 1
+    print()
+    print('ran {} tests in {:.1f} seconds'.format(len(icy.examples),
+        (datetime.now()-t0).total_seconds()))
+    print('{} success / {} no import / {} exception'.format(
+        str(results[0]), str(results[1]), str(results[2])))
+        
