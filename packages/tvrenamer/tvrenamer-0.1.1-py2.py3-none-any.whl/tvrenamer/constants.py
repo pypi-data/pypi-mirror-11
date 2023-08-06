c4b7f250b789eb81d9556af76e3451c04148a142
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:53 +0000

    Add package tvrenamer/0.1.1/tvrenamer-0.1.1-py2.py3-none-any.whl

---
+++ packages/tvrenamer/tvrenamer-0.1.1-py2.py3-none-any.whl/tvrenamer/constants.py
@@ -0,0 +1,15 @@
+"""Application constants."""
+
+INIT = 'initialized'
+PREVALID = 'validating'
+POSTVALID = 'validated'
+PREPARSE = 'parsing'
+POSTPARSE = 'parsed'
+PREENHANCE = 'enhancing'
+POSTENHANCE = 'enhanced'
+PREFORMAT = 'formatting'
+POSTFORMAT = 'formatted'
+PRENAME = 'renaming'
+POSTNAME = 'renamed'
+DONE = 'finished'
+FAILED = 'failed'
