1c02b47c09e455d26cc8f265eace4e477a66f443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:51 +0000

    Add package django-stagesetting/0.4.2/django_stagesetting-0.4.2-py2.py3-none-any.whl

---
+++ packages/django-stagesetting/django_stagesetting-0.4.2-py2.py3-none-any.whl/stagesetting/apps.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+import logging
+from django.core.checks import registry as django_check_registry
+from django.utils.translation import ugettext_lazy as _
+from django.apps import AppConfig
+
+
+logger = logging.getLogger(__name__)
+
+
+class StageSettingAppConfig(AppConfig):
+    name = 'stagesetting'
+    verbose_name = _("Run configuration")
+
+    def ready(self):
+        from .models import RuntimeSetting
+        from .checks import check_setting
+        from .utils import registry as stagesetting_registry
+        django_check_registry.register(check_setting)
+        stagesetting_registry.ready(sender=self.__class__, instance=self,
+                                        model=RuntimeSetting)
