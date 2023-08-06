36387aa9e04c8ca6190c38fe2e49bc79362e0d99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:54 +0000

    Add package jry_test/1.0.0/jry_test-1.0.0.tar.gz

---
+++ packages/jry_test/jry_test-1.0.0.tar.gz/jry_test-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup (
+		name		= 'jry_test',
+		version		= '1.0.0',
+		py_modules	= ['jry_test'],
+		author		= 'jiaorenyu',
+		author_email	= 'jiaorenyu@miaozhen.com',
+		url			= 'http://locahost',
+		description	= 'A Test',
+
+		)
