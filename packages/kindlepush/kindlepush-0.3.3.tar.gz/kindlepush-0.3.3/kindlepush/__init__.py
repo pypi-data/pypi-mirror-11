3f8788ab4a2eec899f08093ad9192977872423da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:57 +0000

    Add package kindlepush/0.3.3/kindlepush-0.3.3.tar.gz

---
+++ packages/kindlepush/kindlepush-0.3.3.tar.gz/kindlepush-0.3.3/kindlepush/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    kindlepush
+    ~~~~~~~~~~
+
+    Kindlepush is trying to rescue you from manually clicking the deliver
+    button to send the doc from your kindle library to your kindle. It
+    is for 3G devices, such as kindle dx.
+    It was created by @blahgeek, now maintained by @lord63.
+
+    :copyright: (c) 2014 BlahGeek.
+    :copyright: (c) 2014 lord63.
+    :license: MIT, see LICENSE for more details.
+"""
+
+__version__ = "0.3.3"
+__title__ = "kindlepush"
+__author__ = "BlahGeek"
+__maintainer__ = "lord63"
+__license__ = "MIT"
+__copyright__ = "Copyright 2014 BlahGeek 2014 lord63"
