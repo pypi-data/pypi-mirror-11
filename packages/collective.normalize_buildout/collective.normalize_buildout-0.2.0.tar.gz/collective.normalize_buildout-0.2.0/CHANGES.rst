a98bd61a8fa5c030341989b2e430004da3a4567f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:25 +0000

    Add package collective.normalize_buildout/0.2.0/collective.normalize_buildout-0.2.0.tar.gz

---
+++ packages/collective.normalize_buildout/collective.normalize_buildout-0.2.0.tar.gz/collective.normalize_buildout-0.2.0/CHANGES.rst
@@ -0,0 +1,22 @@
+Changelog
+=========
+
+
+0.2.0 (2015-09-11)
+------------------
+
+- Support piping.
+  [do3cc]
+
+- Remove unneeded dependency.
+  [do3cc]
+
+- Fix 3 bugs resulting in bad buildout.cfgs.
+  [do3cc]
+
+
+0.1 (2015-09-10)
+----------------
+
+- Initial release.
+  [do3cc]
---
+++ packages/collective.normalize_buildout/collective.normalize_buildout-0.2.0.tar.gz/collective.normalize_buildout-0.2.0/src/collective/normalize_buildout/tests/test_script.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+from StringIO import StringIO
+from collective.normalize_buildout.cmd import sort
+from collective.normalize_buildout.testing import BaseTestCase
+
+
+class TestScript(BaseTestCase):
+
+    def test_good_case(self):
+        cfg = self.given_a_file_in_test_dir('buildout.cfg', '\n'.join([
+            '[buildout]',
+            '[bla]',
+            'a=1',
+            'recipe=xxx',
+            '# comment',
+            'bla=1',
+            '[versions]',
+            'a=1',
+            '[sources]',
+            'xxx = git http:aaa branch=xxx',
+            'yyy = git xfdsfdsfsdfsdfdsfdsfsdfdsfsdfdsf branch=yyy'
+        ]))
+        output = StringIO()
+
+        sort(file(cfg), output)
+        output.seek(0)
+
+        expected = '''[buildout]
+
+[bla]
+recipe=xxx
+a=1
+# comment
+bla=1
+
+[sources]
+xxx = git http:aaa                         branch=xxx
+yyy = git xfdsfdsfsdfsdfdsfdsfsdfdsfsdfdsf branch=yyy
+
+[versions]
+a=1
+'''
+
+        self.assertEqual(expected, output.read())
+
+    def test_regression1(self):
+        cfg = self.given_a_file_in_test_dir('buildout.cfg', '\n'.join([
+            '[sources]',
+            '# xxx',
+            '# yyy',
+            'a = git http...',
+        ]))
+        output = StringIO()
+
+        sort(file(cfg), output)
+        output.seek(0)
+
+        expected = '''[sources]
+# xxx
+# yyy
+a = git http...
+'''
+
+        self.assertEqual(expected, output.read())
+
+    def test_regression2(self):
+        cfg = self.given_a_file_in_test_dir('buildout.cfg', '''
+[filter]
+extra-field-types =
+            <charFilter class="solr.PatternReplaceCharFilterFactory" pattern="(/)+$" replacement=""/>
+'''.strip())  # NOQA
+        output = StringIO()
+
+        sort(file(cfg), output)
+        output.seek(0)
+
+        expected = '''
+[filter]
+extra-field-types =
+            <charFilter class="solr.PatternReplaceCharFilterFactory" pattern="(/)+$" replacement=""/>
+'''.strip()  # NOQA
+
+        self.assertEqual(expected, output.read())
