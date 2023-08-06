6050d7977bb73623774ae8ab78c6a8febf81f162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:04 +0000

    Add package sobol-seq/0.1.1/sobol_seq-0.1.1.zip

---
+++ packages/sobol-seq/sobol_seq-0.1.1.zip/sobol_seq-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='sobol_seq',
+      version='0.1.1',
+      description='Sobol sequence generator',
+      #long_description=open('README.rst').read(),
+      url='https://github.com/naught101/sobol_seq',
+      author='naught101',
+      author_email='naught101@gmail.com',
+      license='MIT',
+      packages=['sobol_seq'],
+      zip_safe=False)
