9b34bc88e050f2d587bed6e7757bf2362fdb37f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:19 +0000

    Add package claw/1.0.0rc5/claw-1.0.0rc5.tar.gz

---
+++ packages/claw/claw-1.0.0rc5.tar.gz/claw-1.0.0rc5/setup.py
@@ -0,0 +1,35 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='claw',
+    version='1.0.0rc5',
+    description='Library to extract message quotations and signatures.',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    author='Adam Renberg',
+    author_email='tgwizard@gmail.com',
+    url='https://github.com/tgwizard/claw',
+    license='APACHE2',
+    packages=find_packages(exclude=['tests']),
+    zip_safe=True,
+    install_requires=[
+        'lxml==2.3.3',
+        'regex>=2015.07.19',
+        'html2text',
+        'setuptools>=17.1',
+    ],
+    extras_require={
+        'tests': [
+            'nose==1.2.1',
+            'mock',
+            'coverage',
+            'flanker',
+            'tox',
+        ],
+        'dev': [
+            'twine'
+        ]
+    },
+)
