213c7caf2d854477c690633a6b05839e27e1cd19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:40 +0000

    Add package containator/0.0.1/containator-0.0.1.tar.gz

---
+++ packages/containator/containator-0.0.1.tar.gz/containator-0.0.1/containator/defs.py
@@ -0,0 +1,6 @@
+__version__ = '0.0.1'
+app_name = 'Containator'
+app_description = 'Runs docker containers with pre-defined parameters'
+app_name_desc = '{} - {}'.format(app_name, app_description)
+
+CONFIG_FILES = ['/etc/containator.conf', '~/.config/containator.conf', '~/.containator.conf']
