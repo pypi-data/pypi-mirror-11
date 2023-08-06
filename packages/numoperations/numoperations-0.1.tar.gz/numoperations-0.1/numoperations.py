2a5ef2c5e3b6818fa6aa7e8fcd3ec43f17083132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:07 +0000

    Add package numoperations/0.1/numoperations-0.1.tar.gz

---
+++ packages/numoperations/numoperations-0.1.tar.gz/numoperations-0.1/numoperations.py
@@ -0,0 +1,2 @@
+def my_addition(num1, num2):
+    return num1 + num2
---
+++ packages/numoperations/numoperations-0.1.tar.gz/numoperations-0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='numoperations',
+    long_description='provide addition of two numbers',
+    py_modules = ['numoperations'],
+    version = '0.1',
+    author = 'sujith',
+    author_email = 'ksnvsujith@gmail.com',
+    url = 'https://www.aviso.com'
+)
