e7d968b25372afd3c406a521d61139016a495b47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:02 +0000

    Add package simple_debug/0.1.1/simple_debug-0.1.1.tar.gz

---
+++ packages/simple_debug/simple_debug-0.1.1.tar.gz/simple_debug-0.1.1/simple_debug/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#    Copyright (C) 2015 by
+#    Moreno Bonaventura <morenobonaventura@gmail.com>
+#    This is Free Software - You can use and distribute it under
+#    the terms of the GNU General Public License, version 3 or later.
+"""
+simple debug
+"""
+__author__   = 'Moreno Bonaventura <morenobonaventura@gmail.com>'
+
+import sys
+
+if sys.version_info[:2] < (2, 6):
+    m = "Python version 2.6 or later is required for simple_debug (%d.%d detected)."
+    raise ImportError(m % sys.version_info[:2])
+
+
+import simple_debug.simple_debug
+from simple_debug.simple_debug import  *
