78d475355ce7c507aa42ff9e6e4be861ab474e9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:32 +0000

    Add package corpkit/1.41/corpkit-1.41.tar.gz

---
+++ packages/corpkit/corpkit-1.41.tar.gz/corpkit-1.41/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(name='corpkit',
+      version='1.41',
+      description='A toolkit for working with linguistic corpora',
+      url='http://github.com/interrogator/corpkit',
+      author='Daniel McDonald',
+      package_data={'corpkit': ['*.jar', 'corpkit/*.jar', '*.sh', 'corpkit/*.sh', 
+                                '*.ipynb', 'corpkit/*.ipynb'],
+                    'dictionaries': ['*.p', 'dictionaries/*.p']},
+      author_email='mcdonaldd@unimelb.edu.au',
+      license='MIT',
+      packages=['corpkit', 'dictionaries'],
+      install_requires=["matplotlib >= 1.4.3",
+                        "nltk >= 3.0.0",
+                        "pandas >= 0.16.1",
+                        "mpld3 >= 0.2", 
+                        "beautifulsoup4>=4.3.2",
+                        "pattern>=2.6",
+                        "tkintertable>=1.2"],
+      zip_safe = False)
