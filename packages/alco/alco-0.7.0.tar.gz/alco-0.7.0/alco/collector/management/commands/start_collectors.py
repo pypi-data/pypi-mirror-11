53dba8ae84199eef2fa384b888b8a62b4ac8c53f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:25 +0000

    Add package alco/0.7.0/alco-0.7.0.tar.gz

---
+++ packages/alco/alco-0.7.0.tar.gz/alco-0.7.0/alco/collector/management/commands/start_collectors.py
@@ -0,0 +1,95 @@
+# coding: utf-8
+
+# $Id: $
+import os
+from time import sleep
+from daemon import DaemonContext
+from daemon.pidfile import TimeoutPIDLockFile
+
+from django.core.management import BaseCommand
+from django.utils.log import getLogger
+from multiprocessing import Process
+import signal
+from alco.collector.collector import Collector
+from alco.collector.models import LoggerIndex
+
+logger = getLogger('alco.collector')
+
+
+class Command(BaseCommand):
+    def __init__(self, stdout=None, stderr=None, no_color=False):
+        super(Command, self).__init__(stdout=stdout, stderr=stderr,
+                                      no_color=no_color)
+        self.processes = {}
+        self.started = False
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--no-daemon', action='store_false', dest='daemon',
+            default=True, help="Don't daemonize process")
+        parser.add_argument(
+            '--pidfile', action='store', dest='pidfile',
+            default="collector.pid", help="pidfile location")
+
+    def handle(self, *args, **options):
+        if not options['daemon']:
+            return self.start_worker_pool()
+
+        path = os.path.join(os.getcwd(), options['pidfile'])
+        pidfile = TimeoutPIDLockFile(path)
+
+        with DaemonContext(pidfile=pidfile):
+            logger.info("daemonized")
+            self.start_worker_pool()
+
+    def start_worker_pool(self):
+        indices = list(LoggerIndex.objects.all())
+        logger.info("Starting worker pool with %s indexers" % len(indices))
+        n = 0
+        for index in indices:
+            c = Collector(index)
+            p = Process(target=c)
+            p.start()
+            logger.info("Indexer for %s started pid=%s" % (index.name, p.pid))
+            self.processes[n] = (p, index)
+            n += 1
+        self.started = True
+        signal.signal(signal.SIGINT, self.handle_sigint)
+
+        while len(self.processes) > 0:
+            if not self.started:
+                logger.info("Stopping indexers")
+                for n in self.processes:
+                    (p, index) = self.processes[n]
+                    os.kill(p.pid, signal.SIGTERM)
+            sleep(0.5)
+            for n in list(self.processes):
+                (p, index) = self.processes[n]
+                if p.exitcode is None:
+                    if not p.is_alive() and self.started:
+                        logger.debug("Indexer with pid %s not finished "
+                                     "and not running" % p.pid)
+                        # Not finished and not running
+                        os.kill(p.pid, signal.SIGKILL)
+                        c = Collector(index)
+                        p = Process(target=c)
+                        p.start()
+                        logger.warning("Indexer for %s restarted pid=%s"
+                                       % (index.name, p.pid))
+                        self.processes[n] = (p, index)
+                elif p.exitcode < 0 and self.started:
+                    logger.warning('Process %s exited with an error '
+                                   'or terminated' % p.pid)
+                    c = Collector(index)
+                    p = Process(target=c)
+                    p.start()
+                    self.processes[n] = (p, index)
+                else:
+                    logger.debug('Process %s exited correctly' % p.pid)
+                    p.join()
+                    del self.processes[n]
+
+    def handle_sigint(self, sig_num, frame):
+        logger.info("Got signal %s, stopping" % sig_num)
+        self.started = False
+
