b07872528f3212a2d6cfe0b05f1dc7d35c6d9cb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:13 +0000

    Add package chrome-panel/0.0.1/chrome_panel-0.0.1-py3-none-any.whl

---
+++ packages/chrome-panel/chrome_panel-0.0.1-py3-none-any.whl/chrome_panel/utils.py
@@ -0,0 +1,13 @@
+def get_name_from_obj(obj):
+    if hasattr(obj, '__name__'):
+        name = obj.__name__
+    elif hasattr(obj, '__class__') and hasattr(obj.__class__, '__name__'):
+        name = obj.__class__.__name__
+    else:
+        name = '<unknown>'
+
+    if hasattr(obj, '__module__'):
+        module = obj.__module__
+        name = '%s.%s' % (module, name)
+
+    return name
