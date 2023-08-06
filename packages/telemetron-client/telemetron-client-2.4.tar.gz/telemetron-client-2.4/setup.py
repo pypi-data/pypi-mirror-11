c3de76dcf86468b3af63320ae82d4c8131103af7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:22 +0000

    Add package telemetron-client/2.4/telemetron-client-2.4.tar.gz

---
+++ packages/telemetron-client/telemetron-client-2.4.tar.gz/telemetron-client-2.4/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+from telemetronclient import __version__
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+
+setup(
+      name='telemetron-client',
+      version=__version__,
+      description='Client for Telemetron',
+      author='Joao Coutinho',
+      author_email='joao.coutinho@mindera.com',
+      url='https://bitbucket.org/mindera/telemetry-client-python',
+      long_description=README,
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[],
+      setup_requires=['nose==1.0.0', 'pylint>=1.4.0', 'setuptools-lint']
+      )
