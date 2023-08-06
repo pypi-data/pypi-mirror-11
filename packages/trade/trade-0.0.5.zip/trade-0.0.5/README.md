c014b78b4e35247130e7c4c623ec95728fd08b94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:59 +0000

    Add package trade/0.0.5/trade-0.0.5.zip

---
+++ packages/trade/trade-0.0.5.zip/trade-0.0.5/README.md
@@ -0,0 +1,189 @@
+trade: Tools For Stock Trading Applications.
+============================================
+Copyright (c) 2015 Rafael da Silva Rocha  
+rocha.rafaelsilva@gmail.com  
+http://rochars.github.io/trade/  
+http://trade.readthedocs.org  
+
+
+What is it?
+-----------
+trade is a Python module with functions and classes for the development
+of investment applications in Python. It provides basic notions of assets,
+operations, daytrades, cost deduction, asset accumulation and so on.
+
+You may use the trade module as a framework to create applications that
+implement specific rules of stock markets around the world or use it "as is"
+in any  application that requires the notion of buying things, selling things
+and controlling how much you spent to buy and how much you profited from the
+trades.
+
+
+How do I install the trade module?
+------------------------------
+The trade module can be installated via pip:
+
+> pip install trade
+
+To check if everything went OK, open the Python console and import the module:
+
+```python
+import trade
+asset = trade.Asset('some asset')
+print(asset.name)
+#>> some asset
+```
+
+
+How can I use it?
+-----------------
+A basic example of the trade module in action:
+
+```python
+import trade
+
+# create the asset that we are going to trade
+asset = trade.Asset('Some asset')
+
+# create the accumulator to accumulate trades with the asset
+accumulator = trade.Accumulator(asset)
+
+
+print(accumulator.asset.name)
+#>> Some asset
+
+print(accumulator.quantity)
+#>> 0
+
+print(accumulator.price)
+#>> 0
+
+print(accumulator.results)
+#>> {'trades': 0}
+
+
+# create a trade operation buying the asset
+purchase = trade.Operation(asset=asset, quantity=10, price=2, date='2015-09-22')
+
+# accumulate the trade
+accumulator.accumulate_operation(purchase)
+
+
+print(accumulator.quantity)
+#>> 10
+
+print(accumulator.price)
+#>> 2.0
+
+print(accumulator.results)
+#>> {'trades': 0}
+
+
+# create a new trade operation selling the asset
+sale = trade.Operation(asset=asset, quantity=-5, price=3, date='2015-09-23')
+
+# accumulate the new trade
+accumulator.accumulate_operation(sale)
+
+
+print(accumulator.quantity)
+#>> 5
+
+print(accumulator.price)
+#>> 2.0
+
+print(accumulator.results)
+#>> {'trades': 5.0}
+```
+
+You may also use only the Accumulator and ignore everything else:
+```python
+import trade
+
+# create the asset that we are going to trade
+asset = 'Some asset'
+
+# create the accumulator to accumulate trades with the asset
+accumulator = trade.Accumulator(asset)
+
+
+print(accumulator.asset)
+#>> Some asset
+
+print(accumulator.quantity)
+#>> 0
+
+print(accumulator.price)
+#>> 0
+
+print(accumulator.results)
+#>> {'trades': 0}
+
+
+# accumulate trade data
+accumulator.accumulate(quantity=10, price=2, date='2015-09-22')
+
+
+print(accumulator.quantity)
+#>> 10
+
+print(accumulator.price)
+#>> 2.0
+
+print(accumulator.results)
+#>> {'trades': 0}
+
+
+# accumulate more trade
+accumulator.accumulate(quantity=-5, price=3, date='2015-09-23')
+
+
+print(accumulator.quantity)
+#>> 5
+
+print(accumulator.price)
+#>> 2.0
+
+print(accumulator.results)
+#>> {'trades': 5.0}
+
+```
+
+Operation objects may include taxes and comissions that are considered by the
+accumulator when calculating results and portfolio prices.
+
+The Accumulator can also log the accumulated operations and their results.
+
+There are more features, like rating and applying discounts automatically
+to a group of operations, separating daytrades from common trades in a group of
+operations, calculating the resulting position from a group of operations and so
+on. Check the documentation and the examples for more information on how to
+use the trade module.
+
+
+What about compatibility?
+-------------------------
+The trade module is compatible with Python 2.7 and Python 3.x.
+
+
+License
+-------
+Copyright (c) 2015 Rafael da Silva Rocha
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ packages/trade/trade-0.0.5.zip/trade-0.0.5/trade/operation.py
@@ -0,0 +1,156 @@
+"""trade: Tools For Stock Trading Applications.
+
+Copyright (c) 2015 Rafael da Silva Rocha
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+
+import math
+import copy
+
+from .tax_manager import TaxManager
+from .utils import (
+    average_price, daytrade_condition, same_sign, find_purchase_and_sale
+)
+
+
+class Asset:
+    """An asset represents anything that can be traded.
+
+    Attributes:
+        name: A string representing the name of the asset.
+        expiration_date: A string 'YYYY-mm-dd' representing the
+            expiration date of the asset, if any.
+    """
+
+    def __init__(self, name='', expiration_date=None):
+        self.name = name
+        self.expiration_date = expiration_date
+
+    def __deepcopy__(self, memo):
+        return self
+
+
+class Operation:
+    """An operation represents the purchase or sale of an asset.
+
+    Attributes:
+        date: A string 'YYYY-mm-dd', the date the operation occurred.
+        asset: An Asset instance, the asset that is being traded.
+        quantity: A number representing the quantity being traded.
+        price: The raw unitary price of the asset being traded.
+        comissions: A dict of discounts. String keys and float values
+            representing the name of the discounts and the values
+            to be deducted from the operation.
+        taxes: A dict of taxes. string keys and float values
+            representing the name of the taxes and the percentual
+            values of the taxes to be applyed to the operation.
+            Taxes are applyed based on the volume of the operation.
+    """
+
+    def __init__(self, quantity, price,
+                    date=None, asset=None, comissions=None, taxes=None):
+        self.date = date
+        self.asset = asset
+        self.quantity = quantity
+        self.price = price
+        if comissions is None: comissions={}
+        if taxes is None: taxes={}
+        self.comissions = comissions
+        self.taxes = taxes
+
+    @property
+    def real_value(self):
+        """Return the quantity * the real price of the operation."""
+        return self.quantity * self.real_price
+
+    @property
+    def real_price(self):
+        """Return the real price of the operation.
+
+        The real price is the price with all comissions and taxes
+        already deducted or added.
+        """
+        return self.price + math.copysign(
+                                self.total_comission / self.quantity,
+                                self.quantity
+                            ) + math.copysign(
+                                    self.total_tax_value / self.quantity,
+                                    self.quantity
+                                )
+
+    @property
+    def total_comission(self):
+        """Return the sum of all comissions included in this operation."""
+        return sum(self.comissions.values())
+
+    @property
+    def volume(self):
+        """Return the quantity of the operation * its raw price."""
+        return abs(self.quantity) * self.price
+
+    @property
+    def total_tax_value(self):
+        """Return the total tax value for this operation."""
+        if self.taxes:
+            return sum(
+                [self.volume * value / 100  for value in self.taxes.values()]
+            )
+        return 0
+
+
+class Daytrade:
+    """A daytrade operation.
+
+    Daytrades are operations of purchase and sale of an Asset on
+    the same date.
+
+    Attributes:
+        asset: An asset instance, the asset that is being traded.
+        quantity: The traded quantity of the asset.
+        purchase: A Operation instance representing the purchase of the
+            asset.
+        sale: A Operation instance representing the sale of the asset.
+    """
+
+    # TODO docstring explaining the creation of the
+    #       purchase and sale operations
+    def __init__(self, date, asset, quantity, buy_price, sale_price):
+        self.date = date
+        self.asset = asset
+        self.quantity = quantity
+        self.purchase = Operation(
+            date=date,
+            asset=asset,
+            quantity=quantity,
+            price=buy_price
+        )
+        self.sale = Operation(
+            date=date,
+            asset=asset,
+            quantity=quantity*-1,
+            price=sale_price
+        )
+
+    @property
+    def result(self):
+        return abs(self.sale.real_value) - abs(self.purchase.real_value)
