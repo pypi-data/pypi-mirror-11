e15fe79523abc31709001f7cc0d9dcab713a7d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:58 +0000

    Add package neighborhoods/0.4/neighborhoods-0.4-py2-none-any.whl

---
+++ packages/neighborhoods/neighborhoods-0.4-py2-none-any.whl/neighborhoods-0.4.dist-info/DESCRIPTION.rst
@@ -0,0 +1,4 @@
+Neighborhoods converts a longitude and latitude to a neighborhood name
+based on Zillow's data.
+
+
---
+++ packages/neighborhoods/neighborhoods-0.4-py2-none-any.whl/neighborhoods/__init__.py
@@ -0,0 +1,14 @@
+#-----------------------------------------------------------------------------#
+#   __init__.py                                                               #
+#                                                                             #
+#   Copyright (c) 2015, StyleSeat, Inc.                                       #
+#   All rights reserved.                                                      #
+#-----------------------------------------------------------------------------#
+
+__title__ = 'neighborhoods'
+__version__ = '0.4'
+__author__ = 'StyleSeat, Inc.'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright (c) 2015, {}'.format(__author__)
+
+from .neighborhoods import Neighborhood
