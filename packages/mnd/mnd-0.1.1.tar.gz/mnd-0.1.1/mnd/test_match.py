19aa46a231bfc72de2bdf33137b1d80a044afba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:23 +0000

    Add package mnd/0.1.1/mnd-0.1.1.tar.gz

---
+++ packages/mnd/mnd-0.1.1.tar.gz/mnd-0.1.1/mnd/test_match.py
@@ -0,0 +1,19 @@
+import collections
+import unittest
+
+from mnd import arg_match
+
+# we will test arg matching against instances of this Msg type
+Msg = collections.namedtuple("Msg", "note type")
+
+class ArgMatchTest(unittest.TestCase):
+    def test_arg(self):
+        self.assertTrue(arg_match(1, 1))
+
+    def test_args(self):
+        self.assertTrue(arg_match([1], [1]))
+
+    def test_subarg(self):
+        note1 = Msg(note=1, type="note_on")
+        self.assertTrue(arg_match(dict(note=1), note1))
+
