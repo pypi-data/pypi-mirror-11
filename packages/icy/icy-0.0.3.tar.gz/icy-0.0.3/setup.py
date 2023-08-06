1435c65bfe827ec79af6c3b585d80ccf74653e60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:06 +0000

    Add package icy/0.0.3/icy-0.0.3.tar.gz

---
+++ packages/icy/icy-0.0.3.tar.gz/icy-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+__version__ = '0.0.3'
+__doc__ = 'data wrangling glue code'
+
+if __name__ == '__main__':
+    setup(
+        name = 'icy',
+        version = __version__,
+        description = __doc__,
+        install_requires = ['pandas'],
+        author = 'Jonathan Rahn',
+        author_email = 'jr@rcs-analytics.com',
+        url = 'https://github.com/rcs-analytics/icy',
+        license = 'MIT',
+        packages = ['icy'],
+        long_description=read('README'),
+    )
