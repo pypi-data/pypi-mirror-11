1d14deebbcbaf29843314c94349b61579546e0d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:04 +0000

    Add package codesters/0.0.5/codesters-0.0.5.tar.gz

---
+++ packages/codesters/codesters-0.0.5.tar.gz/codesters-0.0.5/setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+
+VERSION = '0.0.5'
+CLASSIFIERS = [
+    'Programming Language :: Python',
+
+]
+install_requires = [
+    'Pillow',
+]
+setup(
+    name="codesters",
+    description="Offline replication of the graphics on codesters.com",
+    version=VERSION,
+    author="Codesters",
+    author_email="matthew@codesters.com",
+    url="https://github.com/codestersnyc/codesters-graphics",
+    #download_url="https://bitbucket.org/guillaumepiot/cotidia-admin-tools/downloads/cotidia-admin-tools-0.4.1.tar.gz",
+    #package_dir={'codesters': 'codesters'},
+    packages=['codesters'],
+    include_package_data = True,
+    package_data = {
+        'codesters': ['sprites/codestersLogo.gif']
+    },
+    #package_data={'admin_tools': data_files},
+    #include_package_data=True,
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts':[
+            'codesters = '
+        ]
+    },
+    classifiers=CLASSIFIERS,
+)
