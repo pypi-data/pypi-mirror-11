f3115315feacb4d6637f31deac45241ecd627b29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:46 +0000

    Add package cityhall/0.0.11/cityhall-0.0.11.tar.gz

---
+++ packages/cityhall/cityhall-0.0.11.tar.gz/cityhall-0.0.11/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name = 'cityhall',
+    packages = ['cityhall'], # this must be the same as the name above
+    version = '0.0.11',
+    description = 'A library for accessing City Hall Setting Server',
+    license = 'AGPL',
+    author = 'Alex Popa',
+    author_email = 'alex.popa@digitalborderlands.com',
+    url = 'https://github.com/f00f-nyc/cityhall-python',
+    download_url = 'https://codeload.github.com/f00f-nyc/cityhall-python/legacy.tar.gz/v0.0.11',
+    install_requires=['requests==2.7.0','six==1.9.0'],
+    keywords = ['cityhall', 'enterprise settings', 'settings', 'settings server', 'cityhall', 'City Hall'],
+    test_suite='test',
+    tests_require=['requests==2.7.0','six==1.9.0','mock==1.0.1'],
+    classifiers = [],
+)
