c50bca75189bf4e2d937d7772e3713473b8284b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:31 +0000

    Add package pysqldf/1.0.0/pysqldf-1.0.0.tar.gz

---
+++ packages/pysqldf/pysqldf-1.0.0.tar.gz/pysqldf-1.0.0/pysqldf/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from .sqldf import SQLDF
+from .load import load_meat, load_births
+
+__all__ = [
+    "SQLDF",
+    "load_meat",
+    "load_births"
+]
---
+++ packages/pysqldf/pysqldf-1.0.0.tar.gz/pysqldf-1.0.0/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup(
+    name="pysqldf",
+    version="1.0.0",
+    author="Ryoji Ishii",
+    author_email="airtoxin@icloud.com",
+    url="https://github.com/airtoxin/pysqldf/",
+    license="MIT",
+    packages=find_packages(),
+    package_dir={"pysqldf": "pysqldf"},
+    package_data={"pysqldf": ["data/*.csv"]},
+    description="sqldf for pandas",
+    long_description=open("README.rst").read(),
+    install_requires=[
+        "numpy",
+        "pandas"
+    ],
+    tests_require=[
+        "pandas",
+        "nose"
+    ]
+)
+
