7898fbfa70d21aad1ac53efcbc158ba4c646b98f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:43 +0000

    Add package nagios/0.0.3/nagios-0.0.3.tar.gz

---
+++ packages/nagios/nagios-0.0.3.tar.gz/nagios-0.0.3/setup.py
@@ -0,0 +1,19 @@
+__author__ = 'viet.pham'
+__copyright__ = 'GiQ GmbH'
+__date__ = '30.07.2015'
+__version__ = '0.0.1'
+__status__ = 'beta'
+
+from distutils.core import setup
+
+setup(
+    name='nagios',
+    version='0.0.3',
+    packages=['nagios'],
+    url='',
+    #license='APACHE LICENSE 2.0',
+    author='Viet Pham',
+    author_email='viet.pham@getgoods.de',
+    description='module to develop plugin for nagios, icinga',
+    keywords=['nagios', 'icinga', 'plugin'],
+)
