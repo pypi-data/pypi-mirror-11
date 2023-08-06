eac7b6bce315d5fb12efbbc7f5dea3b0a8504964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:40 +0000

    Add package simplelibs/0.1/simplelibs-0.1.tar.gz

---
+++ packages/simplelibs/simplelibs-0.1.tar.gz/simplelibs-0.1/README.txt
@@ -0,0 +1,13 @@
+Algo:
+=========
+
+Bunch of algorithms
+
+data_structures
+    Contains a bunch of useful data structures
+
+    enum
+        An enum
+
+    graph
+
---
+++ packages/simplelibs/simplelibs-0.1.tar.gz/simplelibs-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='simplelibs',
+      version='0.1',
+      description='simple libraries',
+      url='',
+      author='Kevin S Lin',
+      author_email='kevinslin8@gmail.com',
+      license='MIT',
+      packages=['simplelibs'],
+      zip_safe=False)
