410e09779949196750d3894026e6cb8a56e9c7ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:06 +0000

    Add package Js2Py/0.26/Js2Py-0.26.tar.gz

---
+++ packages/Js2Py/Js2Py-0.26.tar.gz/Js2Py-0.26/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Js2Py',
+    version='0.26',
+    packages=['js2py', 'js2py.utils', 'js2py.prototypes', 'js2py.translators', 'js2py.constructors', 'js2py.host'],
+    url='https://github.com/PiotrDabkowski/Js2Py',
+    install_requires = ['tzlocal>=1.2'],
+    license='GPL',
+    author='Piotr Dabkowski',
+    author_email='piotr.dabkowski@balliol.ox.ac.uk',
+    description='JavaScript to Python Translator & JavaScript interpreter written in 100% pure Python.'
+)
