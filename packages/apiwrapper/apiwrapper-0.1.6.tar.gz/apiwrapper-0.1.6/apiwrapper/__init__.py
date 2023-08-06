6e6d68e560d273d39fe8a0d139f40f82d3424bfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:34 +0000

    Add package apiwrapper/0.1.6/apiwrapper-0.1.6.tar.gz

---
+++ packages/apiwrapper/apiwrapper-0.1.6.tar.gz/apiwrapper-0.1.6/apiwrapper/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: ardydedase
+# @Date:   2015-08-30 11:19:30
+# @Last Modified by:   ardydedase
+# @Last Modified time: 2015-09-14 17:38:35
+
+__author__ = 'Ardy Dedase'
+__email__ = 'ardy.dedase@gmail.com'
+__version__ = '0.1.6'
+
+from .apiwrapper import (
+    APIWrapper,
+    ExceededRetries,
+    EmptyResponse,
+    InvalidResponse,
+    MissingParameter,
+    InvalidParameter,
+    STRICT,
+    GRACEFUL,
+    IGNORE)
