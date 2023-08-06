467b786ace721ccd171cb340ba2184bc1363c790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:27 +0000

    Add package sentry-youtrack/0.2.4/sentry-youtrack-0.2.4.tar.gz

---
+++ packages/sentry-youtrack/sentry-youtrack-0.2.4.tar.gz/sentry-youtrack-0.2.4/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from sentry_youtrack import VERSION
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_settings')
+
+install_requires = [
+    'sentry>=6.1.0',
+]
+
+setup(
+    name='sentry-youtrack',
+    version=VERSION,
+    author='Adam Bogdal',
+    author_email='adam@bogdal.pl',
+    url='http://github.com/bogdal/sentry-youtrack',
+    description='A Sentry extension which integrates with YouTrack',
+    long_description=open('README.rst').read(),
+    license='BSD',
+    packages=find_packages(),
+    install_requires=install_requires,
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'sentry.apps': [
+            'sentry_youtrack = sentry_youtrack',
+        ],
+        'sentry.plugins': [
+            'sentry_youtrack = sentry_youtrack.plugin:YouTrackPlugin'
+        ],
+    },
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: BSD License',
+    ],
+    test_suite='sentry_youtrack.tests',
+)
