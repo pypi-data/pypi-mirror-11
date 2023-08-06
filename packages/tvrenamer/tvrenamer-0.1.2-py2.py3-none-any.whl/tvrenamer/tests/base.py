1d8a191646fbe3fdb6e622f1d72b77da58778348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:29 +0000

    Add package tvrenamer/0.1.2/tvrenamer-0.1.2-py2.py3-none-any.whl

---
+++ packages/tvrenamer/tvrenamer-0.1.2-py2.py3-none-any.whl/tvrenamer/tests/base.py
@@ -0,0 +1,18 @@
+import logging
+
+from oslo_config import fixture as fixture_config
+from oslotest import base as test_base
+
+import tvrenamer
+
+# see urllib3 regarding InsecureRequestWarning and InsecurePlatformWarning
+logging.captureWarnings(True)
+
+
+class BaseTest(test_base.BaseTestCase):
+
+    def setUp(self):
+        super(BaseTest, self).setUp()
+
+        self.CONF = self.useFixture(fixture_config.Config()).conf
+        self.CONF([], project=tvrenamer.PROJECT_NAME)
