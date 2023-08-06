3e0fc311a2a7ce1c06865f4503917b3e43c0a55b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:18 +0000

    Add package infi.app-repo/1.0.15/infi.app_repo-1.0.15-py2.7.egg

---
+++ packages/infi.app-repo/infi.app_repo-1.0.15-py2.7.egg/infi/app_repo/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "1.0.15"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(HEAD detached at v1.0.15)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '11696c96eec3522878aef17f6d4d92da5d08652a'
+__git_head_subject__ = 'Finished release v1.0.15.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
