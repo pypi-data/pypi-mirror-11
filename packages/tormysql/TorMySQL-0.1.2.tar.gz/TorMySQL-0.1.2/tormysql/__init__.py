80a01b56e9fcecb4cd87c8e1c5c0aba7f477469c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:10 +0000

    Add package tormysql/0.1.2/TorMySQL-0.1.2.tar.gz

---
+++ packages/tormysql/TorMySQL-0.1.2.tar.gz/TorMySQL-0.1.2/tormysql/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# 14-8-8
+# create by: snower
+
+from pymysql import *
+from .client import Client
+from .cursor import Cursor, DictCursor, SSCursor, SSDictCursor
+from .pool import ConnectionPool
+
+version = "0.1.2"
+version_info = (0,1,2)
+
+def Connection(*args, **kwargs):
+    client = Client(*args, **kwargs)
+    return client.connect()
+
+connect = Connection
