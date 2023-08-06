d4bbd76ce494c5764f28d1049ce7775040ad8544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:26 +0000

    Add package lcy123/1.1.0/lcy123-1.1.0.zip

---
+++ packages/lcy123/lcy123-1.1.0.zip/lcy123-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name ='lcy123',
+        version='1.1.0',
+        py_modules=['nester'],
+        author='lcy',
+        author_email='1510474430@qq.com',
+        url='',
+        description='simple',
+      )
