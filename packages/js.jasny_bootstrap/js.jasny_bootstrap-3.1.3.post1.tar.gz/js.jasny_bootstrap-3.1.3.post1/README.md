69cc28c2be2b5fe4fadf89dc01df24acd25f9218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:37 +0000

    Add package js.jasny_bootstrap/3.1.3.post1/js.jasny_bootstrap-3.1.3.post1.tar.gz

---
+++ packages/js.jasny_bootstrap/js.jasny_bootstrap-3.1.3.post1.tar.gz/js.jasny_bootstrap-3.1.3.post1/README.md
@@ -0,0 +1,11 @@
+Introduction
+============
+
+This library packages `Jasny Bootstrap`_ for `fanstatic`_.
+
+.. _`fanstatic`: http://fanstatic.org
+.. _`Jasny Bootstrap`: http://www.jasny.net/bootstrap/
+
+This requires integration between your web framework and ``fanstatic``,
+and making sure that the original resources (shipped in the ``resources``
+directory in ``js.jasny_bootstrap``) are published to some URL.
---
+++ packages/js.jasny_bootstrap/js.jasny_bootstrap-3.1.3.post1.tar.gz/js.jasny_bootstrap-3.1.3.post1/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+import os
+
+# The version of the wrapped library is the starting point for the
+# version number of the python package.
+# In bugfix releases of the python package, add a '-' suffix and an
+# incrementing integer.
+# For example, a packaging bugfix release version 1.4.4 of the
+# js.jquery package would be version 1.4.4-1 .
+
+version = '3.1.3.post1'
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.md')
+    + '\n' +
+    read('js', 'jasny_bootstrap', 'test_jasny_bootstrap.txt')
+    + '\n' +
+    read('CHANGES.txt'))
+
+setup(
+    name='js.jasny_bootstrap',
+    version=version,
+    description="Fanstatic packaging of Jasny Bootstrap",
+    long_description=long_description,
+    classifiers=[],
+    keywords='',
+    author='Fanstatic Developers',
+    author_email='fanstatic@googlegroups.com',
+    license='BSD',
+    packages=find_packages(),namespace_packages=['js'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'fanstatic',
+        'setuptools',
+        'js.jquery',
+        ],
+    entry_points={
+        'fanstatic.libraries': [
+            'jasny_bootstrap = js.jasny_bootstrap:library',
+            ],
+        },
+    )
