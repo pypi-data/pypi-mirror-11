e69006776194bd676279b243bcbb44a7a565197d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:46 +0000

    Add package django-jira-helpdesk/0.1.0/django-jira-helpdesk-0.1.0.tar.gz

---
+++ packages/django-jira-helpdesk/django-jira-helpdesk-0.1.0.tar.gz/django-jira-helpdesk-0.1.0/django_jira_helpdesk/settings.py
@@ -0,0 +1,22 @@
+__author__ = 'Gorman'
+
+
+TYPE_CHOICES = (
+                ('Bug', 'Bug'),
+                ('New Feature', 'New Feature'),
+                ('Improvement', 'Improvement'),
+                )
+PRIORITY_CHOICES = (
+                    ('Trivial', 'Trivial'),
+                    ('Minor', 'Minor'),
+                    ('Major', 'Major'),
+                    ('Critical', 'Critical'),
+                    )
+
+
+MODULE_CHOICES = (
+                  ('Admin site', 'Admin site'),
+                  ('Blog', 'Blog'),
+                  ('CMS and front editing', 'CMS and front editing'),
+                  ('General site issues', 'General site issues'),
+                  )
---
+++ packages/django-jira-helpdesk/django-jira-helpdesk-0.1.0.tar.gz/django-jira-helpdesk-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'django-jira-helpdesk',
+  packages = ['django_jira_helpdesk'], # this must be the same as the name above
+  version = '0.1.0',
+  description = 'jira helpdesk for django',
+  author = 'bll-z',
+  author_email = '4f8c94d4@opayq.com',
+  url = 'https://github.com/bll-z/django-jira-helpdesk', # use the URL to the github repo
+  download_url = 'https://github.com/bll-z/django-jira-helpdesk/tarball/0.1.0', # I'll explain this in a second
+  keywords = ['helpdesk', 'jira', 'django'], # arbitrary keywords
+  classifiers = [],
+)
