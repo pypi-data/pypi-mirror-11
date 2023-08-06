11add7e32f37624cd4601f06dbbfb41413321a1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:47 +0000

    Add package sqlite4dummy/0.0.5/sqlite4dummy-0.0.5.zip

---
+++ packages/sqlite4dummy/sqlite4dummy-0.0.5.zip/sqlite4dummy-0.0.5/sqlite4dummy/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .dtype import dtype
+from .engine import Sqlite3Engine
+from .row import Row
+from .schema import (Column, Table, Index, MetaData,
+    Insert, Select, Update, Delete)
+from .func import func
+from .sql import and_, or_, asc, desc
+
+__version__ = "0.0.5"
+__all__ = [
+    "dtype", 
+    "Sqlite3Engine", 
+    "Row",
+    "Column", "Table", "Index", "MetaData", "Insert", "Select", "Update", "Delete",
+    "func",
+    "and_", "or_", "asc", "desc",
+]
