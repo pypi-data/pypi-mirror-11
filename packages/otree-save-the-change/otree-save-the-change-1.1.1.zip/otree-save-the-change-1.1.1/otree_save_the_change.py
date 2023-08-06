45db004e51ac9ce077dfcf2d5510177862da3a8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:00 +0000

    Add package otree-save-the-change/1.1.1/otree-save-the-change-1.1.1.zip

---
+++ packages/otree-save-the-change/otree-save-the-change-1.1.1.zip/otree-save-the-change-1.1.1/otree_save_the_change.py
@@ -0,0 +1,4 @@
+# so that this package can be imported with a different name than save_the_change,
+# to make sure that this version of the package is imported, rather than Karan Lyons',
+# which as at 2015-09-06 is not compatible with Django 1.8
+from save_the_change import *
