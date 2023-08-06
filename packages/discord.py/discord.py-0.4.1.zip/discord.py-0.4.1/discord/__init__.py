f34c0f73eb338b7a6c2eb763d13f44483919389a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:38 +0000

    Add package discord.py/0.4.1/discord.py-0.4.1.zip

---
+++ packages/discord.py/discord.py-0.4.1.zip/discord.py-0.4.1/discord/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""
+Discord API Wrapper
+~~~~~~~~~~~~~~~~~~~
+
+A basic wrapper for the Discord API.
+
+:copyright: (c) 2015 Rapptz
+:license: MIT, see LICENSE for more details.
+
+"""
+
+__title__ = 'discord'
+__author__ = 'Rapptz'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2015 Rapptz'
+__version__ = '0.4.1'
+__build__ = 0x004010
+
+from .client import Client
+from .user import User
+from .channel import Channel, PrivateChannel
+from .server import Server, Member, Permissions, Role
+from .message import Message
+from .errors import *
+from .permissions import Permissions
