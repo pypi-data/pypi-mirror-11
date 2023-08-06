f7de64fd5d50be007c6ff25da8a8af12b2844203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:48 +0000

    Add package dataql/0.1.4/dataql-0.1.4.tar.gz

---
+++ packages/dataql/dataql-0.1.4.tar.gz/dataql-0.1.4/dataql/__init__.py
@@ -0,0 +1,9 @@
+"""``dataql`` is for "Data Query Language". It allows to query data in a simple way."""
+
+__author__ = 'Stephane "Twidi" Angel'
+__contact__ = 's.angel@twidi.com'
+__homepage__ = 'https://github.com/twidi/py-dataql'
+__license__ = 'BSD'
+
+__version__ = '0.1.4'
+VERSION = tuple(__version__.split('.'))
