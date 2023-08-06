932eb76394006da6373a233b64bc468cef50925b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:13 +0000

    Add package skidmarks/0.0.6/skidmarks-0.0.6.tar.gz

---
+++ packages/skidmarks/skidmarks-0.0.6.tar.gz/skidmarks-0.0.6/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+version="0.0.6"
+
+setup(name='skidmarks',
+      version=version,
+      py_modules=['skidmarks'],
+      description="find runs (non-randomness) in sequences",
+      url="http://github.com/brentp/skidmarks/",
+      long_description=open('README.md').read(),
+      keywords='bioinformatics sequence randomness test',
+      author='brentp',
+      author_email='bpederse@gmail.com',
+      license='MIT',
+      test_suite='skidmarks.test_suite',
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      scripts=[],
+      entry_points={
+      },
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 2",
+          "Topic :: Scientific/Engineering :: Bio-Informatics",
+          ],
+)
+
+
