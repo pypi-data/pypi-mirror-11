f043e2f5c569b7cc94ec82e506252bbea4354919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:26 +0000

    Add package restretto/0.1/restretto-0.1-py3-none-any.whl

---
+++ packages/restretto/restretto-0.1-py3-none-any.whl/restretto/cli.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    CLI support for restretto
+    ~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
+
+import sys
+import argparse
+from . import load
+from .errors import ExpectError
+
+
+parser = argparse.ArgumentParser(description="REST resources/endpoints testing tool")
+parser.add_argument("path", help="path to look for tests (file or directory)")
+parser.add_argument("--xunit", dest="xunit_dir", default=None,
+                    help="output xunit reports to this dir")
+parser.add_argument("-q", "--quiet", action="store_true")
+
+
+def main(args=sys.argv[1:]):
+    arguments = parser.parse_args(args)
+
+    sessions = load(arguments.path)
+    if not sessions:
+        print("No test sessions found, exiting")
+        sys.exit(1)
+
+    for test_session in sessions:
+        hdr = "Test session: {}".format(test_session.title)
+        print(hdr)
+        print('-' * len(hdr))
+        for action in test_session.actions:
+            try:
+                test_session.run(action)
+            except ExpectError as failure:
+                print("[FAILURE] {}: {}".format(action.title, failure))
+            except Exception as error:
+                print("[ERROR] {}: {}".format(action.title, error))
+        print("")
+        print("")
