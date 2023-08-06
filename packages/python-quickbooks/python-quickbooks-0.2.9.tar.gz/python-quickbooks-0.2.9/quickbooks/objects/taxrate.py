4aa75cf1fda051832264f79ed0e2c6a5504b67e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:44 +0000

    Add package python-quickbooks/0.2.9/python-quickbooks-0.2.9.tar.gz

---
+++ packages/python-quickbooks/python-quickbooks-0.2.9.tar.gz/python-quickbooks-0.2.9/quickbooks/objects/taxrate.py
@@ -0,0 +1,31 @@
+from base import QuickbooksManagedObject, QuickbooksTransactionEntity, Ref
+
+
+class TaxRate(QuickbooksManagedObject, QuickbooksTransactionEntity):
+    """
+    QBO definition: A TaxRate object represents rate applied to calculate tax liability. Use the TaxService
+    entity to create a taxrate.
+    """
+    class_dict = {
+        "AgencyRef": Ref
+    }
+
+    qbo_object_name = "TaxRate"
+
+    def __init__(self):
+        super(TaxRate, self).__init__()
+        self.Name = ""
+        self.Description = ""
+        self.RateValue = 0
+        self.SpecialTaxType = ""
+        self.Active = True
+        self.TaxReturnLineRef = ""
+        self.DisplayType = ""
+        self.EffectiveTaxRate = ""
+
+        self.AgencyRef = None
+
+    def __unicode__(self):
+        return self.Name
+
+
