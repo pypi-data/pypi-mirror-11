d62102d135e4b0ac7f29e40cbbfd7b63f76bdbce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:03 +0000

    Add package pyqg/0.1.2/pyqg-0.1.2.tar.gz

---
+++ packages/pyqg/pyqg-0.1.2.tar.gz/pyqg-0.1.2/pyqg/__init__.py
@@ -0,0 +1,6 @@
+__version__='0.1.2'
+from model import Model
+from qg_model import QGModel
+from bt_model import BTModel
+from sqg_model import SQGModel
+from particles import LagrangianParticleArray2D, GriddedLagrangianParticleArray2D
