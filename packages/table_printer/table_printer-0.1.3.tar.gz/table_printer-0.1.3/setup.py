7446e3a69a06716e2d6691a9fe464abab01cdb91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:21 +0000

    Add package table_printer/0.1.3/table_printer-0.1.3.tar.gz

---
+++ packages/table_printer/table_printer-0.1.3.tar.gz/table_printer-0.1.3/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='table_printer',
+      version='0.1.3',
+      description='Python Table Printer',
+      author='Abtin Gramian',
+      author_email='abtin.gramian@gmail.com',
+      url='https://github.com/agramian/table-printer',
+      packages=['table_printer'],
+      download_url = 'https://github.com/agramian/table-printer/tarball/v0.1.3',
+      keywords = ['table', 'display', 'print', 'tabular', 'data'],
+      classifiers = [],
+     )
