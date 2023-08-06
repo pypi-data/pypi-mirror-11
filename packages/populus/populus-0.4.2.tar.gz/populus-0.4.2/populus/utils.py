fdfda8559f88fcc4dec342be9b509569280782da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:34 +0000

    Add package populus/0.4.2/populus-0.4.2.tar.gz

---
+++ packages/populus/populus-0.4.2.tar.gz/populus-0.4.2/populus/utils.py
@@ -0,0 +1,109 @@
+import os
+import shutil
+import json
+import socket
+import time
+
+
+CONTRACTS_DIR = "./contracts/"
+
+
+def get_contracts_dir(project_dir):
+    contracts_dir = os.path.join(project_dir, CONTRACTS_DIR)
+    return os.path.abspath(contracts_dir)
+
+
+BUILD_DIR = "./build/"
+
+
+def ensure_path_exists(dir_path):
+    """
+    Make sure that a path exists
+    """
+    if not os.path.exists(dir_path):
+        os.mkdir(dir_path)
+        return True
+    return False
+
+
+def remove_file_if_exists(path):
+    if os.path.isfile(path):
+        os.remove(path)
+        return True
+    return False
+
+
+def remove_dir_if_exists(path):
+    if os.path.isdir(path):
+        shutil.rmtree(path)
+        return True
+    return False
+
+
+def get_build_dir(project_dir):
+    build_dir_path = os.path.join(project_dir, BUILD_DIR)
+    ensure_path_exists(build_dir_path)
+    return build_dir_path
+
+
+def load_contracts(project_dir):
+    compiled_contracts_path = os.path.join(project_dir, BUILD_DIR, 'contracts.json')
+    if not os.path.exists(compiled_contracts_path):
+        raise ValueError("No compiled contracts found")
+
+    with open(compiled_contracts_path) as contracts_file:
+        contracts = json.loads(contracts_file.read())
+
+    return contracts
+
+
+def deploy_contracts(client, contracts):
+    _from = client.get_coinbase()
+    deployed_contracts = {}
+
+    for name, contract in contracts.items():
+        txn_hash = client.send_transaction(_from=_from, data=contract['code'])
+        receipt = client.get_transaction_receipt(txn_hash)
+        if receipt is None:
+            contract_addr = None
+        else:
+            contract_addr = receipt['contractAddress']
+        deployed_contracts[name] = {
+            'txn': txn_hash,
+            'addr': contract_addr,
+        }
+
+    return deployed_contracts
+
+
+def is_executable_available(program):
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath = os.path.dirname(program)
+    if fpath:
+        if is_exe(program):
+            return True
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return True
+
+    return False
+
+
+def get_open_port():
+    sock = socket.socket()
+    sock.bind(('', 0))
+    port = sock.getsockname()[1]
+    sock.close()
+    return str(port)
+
+
+def wait_for_popen(proc, max_wait=5):
+    wait_till = time.time() + 5
+    while time.time() < wait_till:
+        if proc.poll() is not None:
+            break
