086fa32c495f1730fec38e610287218ca1dc98a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:14 +0000

    Add package bayespy/0.3.7/bayespy-0.3.7.tar.gz

---
+++ packages/bayespy/bayespy-0.3.7.tar.gz/bayespy-0.3.7/bayespy/__init__.py
@@ -0,0 +1,11 @@
+################################################################################
+# Copyright (C) 2011-2013 Jaakko Luttinen
+#
+# This file is licensed under the MIT License.
+################################################################################
+
+
+from . import utils
+from . import inference
+from . import nodes
+from . import plot
