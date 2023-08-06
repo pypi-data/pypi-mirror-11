e0a259ea2efd57cf34bb940bf28ce1429933d05e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:19 +0000

    Add package clfparser/0.1/clfparser-0.1.tar.gz

---
+++ packages/clfparser/clfparser-0.1.tar.gz/clfparser-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='clfparser',
+      version='0.1',
+      description='Apache Common/Combined Log Format Parser',
+      url='http://github.com/nnon/commonlogformat.git',
+      author='Rob Mackinnon',
+      author_email='remackinnon@gmail.com',
+      license='MIT',
+      packages=['clfparser'],
+      zip_safe=False)
