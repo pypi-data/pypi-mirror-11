e33c9f3450d3bfb8bb06a42ab61f590e02a2bbfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:18 +0000

    Add package infi.jira_cli/0.3.6/infi.jira_cli-0.3.6-py2.7.egg

---
+++ packages/infi.jira_cli/infi.jira_cli-0.3.6-py2.7.egg/infi/jira_cli/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.3.6"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(HEAD detached at v0.3.6)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'd92c0990f656a862072168610cbe7ce438cf4a1a'
+__git_head_subject__ = 'Finished release v0.3.6.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
