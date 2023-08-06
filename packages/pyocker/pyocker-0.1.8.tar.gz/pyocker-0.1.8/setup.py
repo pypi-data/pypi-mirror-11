6cc5a219f2bf80a60e1955ae69cc84040d5ed1cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:17 +0000

    Add package pyocker/0.1.8/pyocker-0.1.8.tar.gz

---
+++ packages/pyocker/pyocker-0.1.8.tar.gz/pyocker-0.1.8/setup.py
@@ -0,0 +1,49 @@
+import os
+from setuptools import setup, find_packages
+
+version = '0.1.8'
+
+description = "Docker Utils with Python"
+cur_dir = os.path.dirname(__file__)
+try:
+    long_description = open(os.path.join(cur_dir, 'README.rst')).read()
+except:
+    long_description = description
+
+try:
+    import pypandoc
+    long_description = lambda f: pypandoc.convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    long_description = lambda f: open(f, 'r').read()
+
+setup(
+    name = "pyocker",
+    version = version,
+    url = 'https://github.com/AkihikoITOH/docker_tools/tree/master/pyocker',
+    license = 'MIT',
+    description = description,
+    long_description = long_description('README.md'),
+    author = 'ITOH Akihiko',
+    # author_email = 'jhmsmits@gmail.com',
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    scripts = ['src/pyocker/pyocker_modules/build.py','src/pyocker/pyocker_modules/parse.py','src/pyocker/pyocker_modules/push.py','src/pyocker/pyocker_modules/validate.py'],
+    # package_data={ 'src': ['*.py']},
+    install_requires = ['setuptools', 'pypandoc'],
+    entry_points="""
+    [console_scripts]
+    pyocker = pyocker.pyocker:main
+    """,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        # 'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        # 'Topic :: Software Development :: Bug Tracking',
+    ],
+    # test_suite = 'nose.collector',
+)
---
+++ packages/pyocker/pyocker-0.1.8.tar.gz/pyocker-0.1.8/src/pyocker/pyocker_modules/validate.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+def validate_image_directive(image):
+    valid = True
+    if 'name' not in image:
+        sys.stderr.write(' * Cannot find `name` for image.\n')
+        valid = False
+
+    return valid
+
+def validate_config(cwd, config, verbose=True):
+    valid = True
+    if 'images' not in config:
+        sys.stderr.write(' * Cannot find `images` directive.\n')
+        valid = False
+    else:
+        for image in config['images']:
+            if not validate_image_directive(image):
+                valid = False
+
+    if not is_files_exist(cwd, config['images']):
+        valid = False
+
+    if not valid:
+        sys.stderr.write("Validation failed.\n")
+        exit()
+    else:
+        sys.stdout.write("Validation succeeded.\n")
+        sys.stdout.write('---\n')
+
+    return
+
+def is_files_exist(cwd, images):
+    valid = True
+    for image in images:
+        if 'build' in image and image['build']:
+            if not os.path.exists(os.path.join(cwd, image['name'], 'Dockerfile')):
+                sys.stderr.write('* Cannot find Dockerfile for %s\n' % image['name'])
+                valid = False
+            if 'dependencies' in image:
+                if not is_files_exist(cwd, image['dependencies']):
+                    valid = False
+    return valid
