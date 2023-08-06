f660bd56d2a47155e4f9f8e13ef9f49ceced79cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:14 +0000

    Add package netdisco/0.4.1/netdisco-0.4.1-py2.py3-none-any.whl

---
+++ packages/netdisco/netdisco-0.4.1-py2.py3-none-any.whl/netdisco/const.py
@@ -0,0 +1,10 @@
+"""
+Constants of services that can be discovered.
+"""
+
+BELKIN_WEMO = "belkin_wemo"
+DLNA = "DLNA"
+GOOGLE_CAST = "google_cast"
+PHILIPS_HUE = "philips_hue"
+NETGEAR_ROUTER = "netgear_router"
+SONOS = "sonos"
