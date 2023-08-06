e85128210cd628c41dbbf4be7dbaf14c5a306fff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:13 +0000

    Add package dj-saml-idp/0.21.0/dj-saml-idp-0.21.0.tar.gz

---
+++ packages/dj-saml-idp/dj-saml-idp-0.21.0.tar.gz/dj-saml-idp-0.21.0/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+import saml2idp
+
+
+with open('README.rst') as readme:
+    description = readme.read()
+
+setup(
+    name='dj-saml-idp',
+    version=saml2idp.__version__,
+    author='Sebastian Vetter',
+    author_email='sebastian@mobify.com',
+    description='SAML 2.0 IdP for Django',
+    long_description=description,
+    install_requires=[
+        'Django>=1.4',
+        'M2Crypto>=0.20.1',
+        'BeautifulSoup>=3.2.0'],
+    license='MIT',
+    packages=['saml2idp'],
+    url='http://github.com/mobify/dj-saml-idp',
+    zip_safe=False,
+    include_package_data=True,
+)
