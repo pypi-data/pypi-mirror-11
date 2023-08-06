39ac8e885a9a47f2a5a199c6e927ce20cc886cb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:34 +0000

    Add package para/0.0.3/para-0.0.3.tar.gz

---
+++ packages/para/para-0.0.3.tar.gz/para-0.0.3/para/__init__.py
@@ -0,0 +1,12 @@
+"""
+This library implements a simple set of parallel processing utilities that
+take advantage of python's `multiprocessing` module to distribute processing
+over multiple CPUs on a single machine.  The most salient feature of this
+library is the `map()` function that can be used to distribute CPU-intensive
+processing of a collection of items over multiple cores.
+"""
+from .map import map
+
+__version__ = "0.0.3"
+
+__all__ = [map]
