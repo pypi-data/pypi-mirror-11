4692977a7e4117872f278580946b03a6f3065f57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:25 +0000

    Add package rtsprofile/4.1.0/rtsprofile-4.1.0-py2-none-any.whl

---
+++ packages/rtsprofile/rtsprofile-4.1.0-py2-none-any.whl/rtsprofile/__init__.py
@@ -0,0 +1,37 @@
+# -*- Python -*-
+# -*- coding: utf-8 -*-
+
+'''rtsprofile
+
+Copyright (C) 2009-2015
+    Geoffrey Biggs
+    RT-Synthesis Research Group
+    Intelligent Systems Research Institute,
+    National Institute of Advanced Industrial Science and Technology (AIST),
+    Japan
+    All rights reserved.
+Licensed under the GNU Lesser General Public License version 3.
+http://www.gnu.org/licenses/lgpl-3.0.en.html
+
+File: __init__.py
+
+Library for parsing the RtsProfile XML (such as that produced by the
+RTSystemEditor tool) into an easy-to-navigate Python data structure.
+
+'''
+
+__version__ = '$Revision: $'
+# $Source$
+
+
+RTSPROFILE_VERSION = '4.1.0'
+RTSPROFILE_SPEC_VERSION = '0.2'
+RTS_NS = 'http://www.openrtp.org/namespaces/rts'
+RTS_NS_S = 'rts:'
+RTS_EXT_NS = 'http://www.openrtp.org/namespaces/rts_ext'
+RTS_EXT_NS_S = 'rtsExt:'
+RTS_EXT_NS_YAML = 'rtsExt::'
+
+
+# vim: tw=79
+
