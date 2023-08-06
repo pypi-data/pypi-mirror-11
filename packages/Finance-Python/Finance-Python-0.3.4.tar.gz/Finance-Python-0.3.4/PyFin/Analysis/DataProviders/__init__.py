c924d48f6a2f23015b28c6c2fd1dd7b4821505aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:35 +0000

    Add package Finance-Python/0.3.4/Finance-Python-0.3.4.tar.gz

---
+++ packages/Finance-Python/Finance-Python-0.3.4.tar.gz/Finance-Python-0.3.4/PyFin/Analysis/DataProviders/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+u"""
+Created on 2015-8-19
+
+@author: cheng.li
+"""
+
+__all__ = ['DataProvider']
+
+from PyFin.Analysis.DataProviders.DataProviders import DataProvider
---
+++ packages/Finance-Python/Finance-Python-0.3.4.tar.gz/Finance-Python-0.3.4/PyFin/Patterns/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+u"""
+Created on 2015-8-14
+
+@author: cheng.li
+"""
