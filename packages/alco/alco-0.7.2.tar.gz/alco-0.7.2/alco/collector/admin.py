20715bd7097470476012418ca5648aac24024150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:42 +0000

    Add package alco/0.7.2/alco-0.7.2.tar.gz

---
+++ packages/alco/alco-0.7.2.tar.gz/alco-0.7.2/alco/collector/admin.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+# $Id: $
+from django.contrib import admin
+from alco.collector.models import LoggerIndex, LoggerColumn
+
+
+class LoggerIndexAdmin(admin.ModelAdmin):
+    list_display = ('name', 'queue_name', 'routing_key')
+
+
+class LoggerColumnAdmin(admin.ModelAdmin):
+    list_display = ('name', 'index_name', 'filtered', 'display', 'excluded')
+    list_filter = ('index',)
+
+    def index_name(self, obj):
+        return obj.index.name
+
+
+admin.site.register(LoggerIndex, LoggerIndexAdmin)
+admin.site.register(LoggerColumn, LoggerColumnAdmin)
+
