fb99000cab2a99f528556cb0b504dfa3d033b5f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:19 +0000

    Add package django-validate-model-attribute-assignment/1/django-validate-model-attribute-assignment-1.tar.gz

---
+++ packages/django-validate-model-attribute-assignment/django-validate-model-attribute-assignment-1.tar.gz/django-validate-model-attribute-assignment-1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='django-validate-model-attribute-assignment',
+    version=1,
+    packages=(
+        'django_validate_model_attribute_assignment',
+    )
+)
