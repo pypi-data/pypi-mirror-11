4ff3b417cabbec1bbd0e742b58a3eaf9f0df274a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:18 +0000

    Add package snakespec/0.1.1/snakespec-0.1.1.tar.gz

---
+++ packages/snakespec/snakespec-0.1.1.tar.gz/snakespec-0.1.1/test/test_snakespec.py
@@ -0,0 +1,27 @@
+import os
+from unittest import TestCase
+from nose.plugins import PluginTester
+from snakespec.snakespec import SnakeSpec
+
+
+class SnakeSpecTester(PluginTester, TestCase):
+    activate = '--snakespec'
+    plugins = [SnakeSpec()]
+
+
+class TestNestedExample(SnakeSpecTester):
+    suitepath = os.path.join(os.getcwd(), 'examples', 'test_nested_example.py')
+
+    def test_it_should_run_all_tests(self):
+        assert 'Ran 4 tests' in self.output
+        assert 'FAILED' not in self.output
+        assert 'ERROR' not in self.output
+
+
+class TestHamcrestExample(SnakeSpecTester):
+    suitepath = os.path.join(os.getcwd(), 'examples', 'test_hamcrest_example.py')
+
+    def test_test_it_should_work_with_hamcrest(self):
+        assert 'Ran 3 tests' in self.output
+        assert 'FAILED' not in self.output
+        assert 'ERROR' not in self.output
