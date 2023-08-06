607a3a60f8875b7d8612b490b4ff51f1d3e89619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:32 +0000

    Add package pydemon/0.0.1/pydemon-0.0.1.tar.gz

---
+++ packages/pydemon/pydemon-0.0.1.tar.gz/pydemon-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name='pydemon',
+	version='0.0.1',
+	py_modules=['pydemon'],
+	author ='becxer',
+	author_email='becxer87@gmail.com',
+	url = '',
+	description = 'Monitor for any changes in your Project'
+)
+
+
+	
