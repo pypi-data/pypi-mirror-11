cfafdd64e2747bc662bb69dd557f0f4889e0db4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:20 +0000

    Add package scikit-allel/0.16.2/scikit-allel-0.16.2.tar.gz

---
+++ packages/scikit-allel/scikit-allel-0.16.2.tar.gz/scikit-allel-0.16.2/allel/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+
+
+__version__ = '0.16.2'
+
+
+import allel.model as model
+from allel.model import *
+import allel.stats as stats
+import allel.plot as plot
+import allel.io as io
+import allel.constants as constants
