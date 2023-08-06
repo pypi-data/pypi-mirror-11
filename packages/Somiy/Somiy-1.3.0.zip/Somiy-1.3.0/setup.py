6b5d9e1e6715b1ac5669577c65fb869d42143396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:32 +0000

    Add package Somiy/1.3.0/Somiy-1.3.0.zip

---
+++ packages/Somiy/Somiy-1.3.0.zip/Somiy-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup 
+setup(
+		 name = 'Somiy', 
+		 version = '1.3.0', 		
+		 py_modules = ['Somiy'], 
+		 author = 'resalg', 		
+		 author_email = 'rainsky512@gmail.com', 
+		 url = 'http://yuitaku.github.io', 
+		 description = 'A simple printer of nester lists  This is Mode Exercise  '  	
+		 )
