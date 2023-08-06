fb7ac109bb98b9f081dfbacc15d3adf67cb66994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:24 +0000

    Add package felapps/1.3.0/felapps-1.3.0-py2.py3-none-any.whl

---
+++ packages/felapps/felapps-1.3.0-py2.py3-none-any.whl/felapps/__init__.py
@@ -0,0 +1,9 @@
+from .utils import felutils, funutils, resutils, scanutils
+from .physics import felcalc, felbase
+from .facilities import dcls
+from .apps.imageviewer import imageviewer
+from .apps.cornalyzer import cornalyzer
+from .apps.felformula import felformula
+
+#__all__ = [imageviewer]
+
