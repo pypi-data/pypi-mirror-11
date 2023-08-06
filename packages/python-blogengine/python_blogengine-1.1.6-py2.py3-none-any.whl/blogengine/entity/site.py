eb6c66f93d32881c2c492be32e114b8158eb9593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:32 +0000

    Add package python-blogengine/1.1.6/python_blogengine-1.1.6-py2.py3-none-any.whl

---
+++ packages/python-blogengine/python_blogengine-1.1.6-py2.py3-none-any.whl/blogengine/entity/site.py
@@ -0,0 +1,23 @@
+from . import BaseEntity
+
+class Site(BaseEntity):
+    type = 'unknown'
+
+    disqus = None
+    base_url = None
+    author = None
+    name = None
+    description = None
+    analytics = None
+    cover_photo = None
+    twitter = None
+    facebook = None
+    googleplus = None
+    linkedin = None
+    rss_url = None
+    avatar = None
+    timezone = 'Europe/Zurich'
+
+    def __init__(self, **kwargs):
+        for k in kwargs.keys():
+            setattr(self, k, kwargs[k])
