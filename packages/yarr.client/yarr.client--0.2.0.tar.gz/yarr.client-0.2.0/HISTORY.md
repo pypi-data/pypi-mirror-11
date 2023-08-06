1006ef14739bebeb67743264eb1b10748763c99f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:21 +0000

    Add package yarr.client/0.2.0/yarr.client--0.2.0.tar.gz

---
+++ packages/yarr.client/yarr.client--0.2.0.tar.gz/yarr.client-0.2.0/HISTORY.md
@@ -0,0 +1,15 @@
+# Changelog
+
+## 0.2.0
+
+- Added Docker
+- Changed from Travis CI to Circle CI
+
+## 0.1.0
+
+- Added tests
+- Added api-token support
+- Added search route
+- Added index route
+- Added config file configuration
+- Added CLI
---
+++ packages/yarr.client/yarr.client--0.2.0.tar.gz/yarr.client-0.2.0/yarr_client/defaults.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+HOST = '0.0.0.0'
+DEBUG = True
+ASSETS_DEBUG = True
+TESTING = False
+SECRET_KEY = 'PmwGsaKKA14hqgpzbvgK+txDEiw='
+MINIFY_HTML = False
+
+# Yarr! specific
+YARR_URL = 'http://localhost:8080'
+YARR_API_TOKEN = ''
