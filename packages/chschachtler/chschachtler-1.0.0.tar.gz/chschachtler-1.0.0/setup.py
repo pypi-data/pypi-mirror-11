e5518d8fdee219d10353516e4911005bc42f1787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:20 +0000

    Add package chschachtler/1.0.0/chschachtler-1.0.0.tar.gz

---
+++ packages/chschachtler/chschachtler-1.0.0.tar.gz/chschachtler-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup (
+  name = 'chschachtler',
+  version = '1.0.0',
+  py_modules = ['schachtler'],
+  author = 'choefig',
+  author_email = 'choefig@harung.de',
+  url = 'www.harung.de',
+  description = 'yet another schachtler',
+)
+  
