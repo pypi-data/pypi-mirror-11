8485f3584d3d4d64c628f9c25e84e24810f4e450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:21 +0000

    Add package django-report-builder/3.2.0rc1/django-report-builder-3.2.0rc1.tar.gz

---
+++ packages/django-report-builder/django-report-builder-3.2.0rc1.tar.gz/django-report-builder-3.2.0rc1/report_builder_demo/demo_models/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from custom_field.custom_field import CustomFieldAdmin
+from .models import Foo, Bar, Place, Waiter, Restaurant, Person, Child
+
+
+@admin.register(Foo)
+class FooAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Bar)
+class BarAdmin(CustomFieldAdmin, admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Place)
+admin.site.register(Waiter)
+admin.site.register(Restaurant)
+admin.site.register(Person)
+admin.site.register(Child)
