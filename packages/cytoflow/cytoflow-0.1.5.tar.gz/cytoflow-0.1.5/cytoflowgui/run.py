368d5a63d3d58a86f0049d84b6108a678e5fa809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:19 +0000

    Add package cytoflow/0.1.5/cytoflow-0.1.5.tar.gz

---
+++ packages/cytoflow/cytoflow-0.1.5.tar.gz/cytoflow-0.1.5/cytoflowgui/run.py
@@ -0,0 +1,56 @@
+"""
+Created on Feb 11, 2015
+
+@author: brian
+"""
+
+from traits.etsconfig.api import ETSConfig
+ETSConfig.toolkit = 'qt4'
+
+import logging
+
+from envisage.core_plugin import CorePlugin
+from envisage.ui.tasks.tasks_plugin import TasksPlugin
+
+from flow_task import FlowTaskPlugin
+from cytoflow_application import CytoflowApplication
+from op_plugins import ImportPlugin, ThresholdPlugin, HLogPlugin, RangePlugin, \
+                       Range2DPlugin, PolygonPlugin, LogiclePlugin
+from view_plugins import HistogramPlugin, HexbinPlugin, ScatterplotPlugin, \
+                         BarChartPlugin, Stats1DPlugin
+
+def run_gui(argv):
+    
+    logging.basicConfig(level=logging.DEBUG)
+    
+    debug = ("--debug" in argv)
+
+    plugins = [CorePlugin(), TasksPlugin(), FlowTaskPlugin(debug = debug),
+               ImportPlugin(), ThresholdPlugin(), HistogramPlugin(),
+               HLogPlugin(), HexbinPlugin(), ScatterplotPlugin(), RangePlugin(),
+               Range2DPlugin(), PolygonPlugin(), BarChartPlugin(), 
+               Stats1DPlugin(), LogiclePlugin()]
+    
+    app = CytoflowApplication(id = 'edu.mit.synbio.cytoflow',
+                              plugins = plugins)
+    app.run()
+    
+    logging.shutdown()
+
+if __name__ == '__main__':
+    import sys
+    from pyface.qt import qt_api
+    
+    if qt_api == "pyside":
+        print "Cytoflow uses PyQT; but it is trying to use PySide instead."
+        print " - Make sure PyQT is installed."
+        print " - If both are installed, and you don't need both, uninstall PySide."
+        print " - If you must have both installed, select PyQT by setting the"
+        print "   environment variable QT_API to \"pyqt\""
+        print "   * eg, on Linux, type on the command line:"
+        print "     QT_API=\"pyqt\" python run.py"
+        # TODO - better instructions
+        sys.exit(1)
+    
+
+    run_gui(sys.argv)
