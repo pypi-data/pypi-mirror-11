f079f223d8c3b882e1f7ec2f554e8276d06f7561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:46 +0000

    Add package flask-template-loader/0.0.2/flask-template-loader-0.0.2.tar.gz

---
+++ packages/flask-template-loader/flask-template-loader-0.0.2.tar.gz/flask-template-loader-0.0.2/flask_template_loader/__init__.py
@@ -0,0 +1,33 @@
+import flask
+from werkzeug import import_string
+from jinja2 import loaders
+
+class FlaskTemplateLoader(object):
+    def __init__(self,app=None):
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self,app):
+        self.app = app
+        old_loader = app.jinja_loader
+        if not self.app.config.get('PREPEND_OTHER_LOADERS'):
+            self.loaders = [old_loader]
+        else: 
+            self.loaders = []
+        self.add_from_settings()
+        self.add_from_blueprints()
+        if not old_loader in self.loaders:
+            self.loaders.append(old_loader)
+        self.app.jinja_loader = loaders.ChoiceLoader(self.loaders)
+
+    def add_from_settings(self):
+        if self.app.config.get('TEMPLATE_LOADERS'):
+            for loader in self.app.config.get('TEMPLATE_LOADERS'):
+                self.loaders.append(import_string(loader))
+
+    def add_from_blueprints(self):
+        if self.app.blueprints:
+            for bp in self.app.blueprints:
+                self.loaders.append(bp.jinja_loader)
+
+
---
+++ packages/flask-template-loader/flask-template-loader-0.0.2.tar.gz/flask-template-loader-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='flask-template-loader',
+    author='Kyle Roux',
+    author_email='kyle@level2designs.com',
+    packages=['flask_template_loader'],
+    install_requires=['flask'],
+    url='git@github.com:jstacoder/flask-template-loader.git',
+    version='0.0.2',
+)
