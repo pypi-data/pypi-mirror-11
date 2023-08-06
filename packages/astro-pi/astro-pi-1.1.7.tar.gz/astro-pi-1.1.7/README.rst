2a4d5dee2b21eedf0a47309d8d054886b65ee756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:02 +0000

    Add package astro-pi/1.1.7/astro-pi-1.1.7.tar.gz

---
+++ packages/astro-pi/astro-pi-1.1.7.tar.gz/astro-pi-1.1.7/README.rst
@@ -0,0 +1,7 @@
+========
+Astro Pi
+========
+
+The Astro Pi software is now called Sense HAT. For information on installing the new software and for comprehensive documentation see `pythonhosted.org/astro-pi`_
+
+.. _pythonhosted.org/sense-hat: http://pythonhosted.org/sense-hat/
---
+++ packages/astro-pi/astro-pi-1.1.7.tar.gz/astro-pi-1.1.7/setup.py
@@ -0,0 +1,44 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="astro-pi",
+    version="1.1.7",
+    author="Dave Honess",
+    author_email="dave@raspberrypi.org",
+    description="Python API for Astro Pi (Sense HAT) for the Raspberry Pi",
+    long_description=read('README.rst'),
+    license="BSD",
+    keywords=[
+        "raspberrypi",
+        "astro pi",
+        "astro pi hat",
+        "sense hat",
+    ],
+    url="https://github.com/astro-pi/astro-pi-hat",
+    packages=find_packages(),
+    package_data={
+        "txt": ['astro_pi_text.txt'],
+        "png": ['astro_pi_text.png']
+    },
+    include_package_data=True,
+    install_requires=[
+        "pillow",
+        "numpy"
+    ],
+    classifiers=[
+        "Development Status :: 7 - Inactive",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Astronomy",
+        "Topic :: Scientific/Engineering :: Atmospheric Science",
+        "Topic :: Education",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
