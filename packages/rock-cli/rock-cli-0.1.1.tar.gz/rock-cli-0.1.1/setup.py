61e6b78c1305c12324968ae5c88e0dc04697f4a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:39 +0000

    Add package rock-cli/0.1.1/rock-cli-0.1.1.tar.gz

---
+++ packages/rock-cli/rock-cli-0.1.1.tar.gz/rock-cli-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='rock-cli',
+    version='0.1.1',
+    author='Ale',
+    author_email='ale@songbee.net',
+    description='A Rocketbank CLI',
+    url='https://github.com/iamale/rock',
+    packages = ['rock_cli'],
+    install_requires=[
+        'Click',
+        'requests',
+        'yamlcfg',
+    ],
+    entry_points='''
+        [console_scripts]
+        rock=rock_cli.cli:cli
+    ''',
+)
