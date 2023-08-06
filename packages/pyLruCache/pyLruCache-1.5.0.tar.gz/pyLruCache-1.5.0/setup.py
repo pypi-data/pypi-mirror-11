ed11a8ea7948b09beafc3b04ef302bcf96679fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:13 +0000

    Add package pyLruCache/1.5.0/pyLruCache-1.5.0.tar.gz

---
+++ packages/pyLruCache/pyLruCache-1.5.0.tar.gz/pyLruCache-1.5.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pyLruCache',
+      version='1.5.0',
+      description='Use Lru for dict cache.',
+      url='http://github.com/rfyiamcool',
+      author='ruifengyun',
+      author_email='rfyiamcool@163.com',
+      long_description=open('README.md').read(),
+      license='MIT',
+      packages=['pyLruCache'],
+      zip_safe=False)
