07419918cb21beab40819366c1d39a3e65f60c05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:12 +0000

    Add package pyhocon/0.3.7/pyhocon-0.3.7.tar.gz

---
+++ packages/pyhocon/pyhocon-0.3.7.tar.gz/pyhocon-0.3.7/pyhocon/tool.py
@@ -0,0 +1,192 @@
+import argparse
+import logging
+import sys
+from pyhocon import ConfigFactory
+from pyhocon.config_tree import ConfigTree
+
+
+LOG_FORMAT = '%(asctime)s %(levelname)s: %(message)s'
+
+
+class HOCONConverter(object):
+    @staticmethod
+    def to_json(config, level=0):
+        """Convert HOCON input into a JSON output
+
+        :return: JSON string representation
+        :type return: basestring
+        """
+        lines = ""
+        if isinstance(config, ConfigTree):
+            if len(config) == 0:
+                lines += '{}'
+            else:
+                lines += '{\n'
+                bet_lines = []
+                for key, item in config.items():
+                    bet_lines.append('{indent}"{key}": {value}'.format(
+                        indent=''.rjust((level + 1) * 2, ' '),
+                        key=key.strip('"'),  # for dotted keys enclosed with "" to not be interpreted as nested key
+                        value=HOCONConverter.to_json(item, level + 1))
+                    )
+                lines += ',\n'.join(bet_lines)
+                lines += '\n{indent}}}'.format(indent=''.rjust(level * 2, ' '))
+        elif isinstance(config, list):
+            if len(config) == 0:
+                lines += '[]'
+            else:
+                lines += '[\n'
+                bet_lines = []
+                for item in config:
+                    bet_lines.append('{indent}{value}'.format(indent=''.rjust((level + 1) * 2, ' '), value=HOCONConverter.to_json(item, level + 1)))
+                lines += ',\n'.join(bet_lines)
+                lines += '\n{indent}]'.format(indent=''.rjust(level * 2, ' '))
+        elif isinstance(config, str):
+            lines = '"{value}"'.format(value=config.replace('\n', '\\n').replace('"', '\\"'))
+        elif config is None:
+            lines = 'null'
+        elif config is True:
+            lines = 'true'
+        elif config is False:
+            lines = 'false'
+        else:
+            lines = str(config)
+        return lines
+
+    @staticmethod
+    def to_yaml(config, level=0):
+        """Convert HOCON input into a YAML output
+
+        :return: YAML string representation
+        :type return: basestring
+        """
+        lines = ""
+        if isinstance(config, ConfigTree):
+            if len(config) > 0:
+                if level > 0:
+                    lines += '\n'
+                bet_lines = []
+                for key, item in config.items():
+                    bet_lines.append('{indent}{key}: {value}'.format(
+                        indent=''.rjust(level * 2, ' '),
+                        key=key.strip('"'),  # for dotted keys enclosed with "" to not be interpreted as nested key,
+                        value=HOCONConverter.to_yaml(item, level + 1))
+                    )
+                lines += '\n'.join(bet_lines)
+        elif isinstance(config, list):
+            config_list = [line for line in config if line is not None]
+            if len(config_list) == 0:
+                lines += '[]'
+            else:
+                lines += '\n'
+                bet_lines = []
+                for item in config_list:
+                    bet_lines.append('{indent}- {value}'.format(indent=''.rjust(level * 2, ' '), value=HOCONConverter.to_yaml(item, level + 1)))
+                lines += '\n'.join(bet_lines)
+        elif isinstance(config, str):
+            # if it contains a \n then it's multiline
+            lines = config.split('\n')
+            if len(lines) == 1:
+                lines = config
+            else:
+                lines = '|\n' + '\n'.join([line.rjust(level * 2, ' ') for line in lines])
+        elif config is True:
+            lines = 'true'
+        elif config is False:
+            lines = 'false'
+        else:
+            lines = str(config)
+        return lines
+
+    @staticmethod
+    def to_properties(config, key_stack=[]):
+        """Convert HOCON input into a .properties output
+
+        :return: .properties string representation
+        :type return: basestring
+        :return:
+        """
+        def escape_value(value):
+            return value.replace('=', '\\=').replace('!', '\\!').replace('#', '\\#').replace('\n', '\\\n')
+
+        stripped_key_stack = [key.strip('"') for key in key_stack]
+        lines = []
+        if isinstance(config, ConfigTree):
+            for key, item in config.items():
+                if item is not None:
+                    lines.append(HOCONConverter.to_properties(item, stripped_key_stack + [key]))
+        elif isinstance(config, list):
+            for index, item in enumerate(config):
+                if item is not None:
+                    lines.append(HOCONConverter.to_properties(item, stripped_key_stack + [str(index)]))
+        elif isinstance(config, str):
+            lines.append('.'.join(stripped_key_stack) + ' = ' + escape_value(config))
+        elif config is True:
+            lines.append('.'.join(stripped_key_stack) + ' = true')
+        elif config is False:
+            lines.append('.'.join(stripped_key_stack) + ' = false')
+        else:
+            lines.append('.'.join(stripped_key_stack) + ' = ' + str(config))
+        return '\n'.join([line for line in lines if len(line) > 0])
+
+    @staticmethod
+    def convert(input_file=None, output_file=None, format='json'):
+        """Convert to json, properties or yaml
+
+        :param format: json, properties or yaml
+        :type format: basestring
+        :return: json, properties or yaml string representation
+        """
+
+        if input_file is None:
+            content = sys.stdin.read()
+            config = ConfigFactory.parse_string(content)
+        else:
+            config = ConfigFactory.parse_file(input_file)
+
+        if format.lower() == 'json':
+            res = HOCONConverter.to_json(config)
+        elif format.lower() == 'properties':
+            res = HOCONConverter.to_properties(config)
+        elif format.lower() == 'yaml':
+            res = HOCONConverter.to_yaml(config)
+        else:
+            raise Exception("Format must be 'json', 'properties' or 'yaml'")
+
+        if output_file is None:
+            print(res)
+        else:
+            with open(output_file, "w") as fd:
+                fd.write(res)
+
+
+def main():  # pragma: no cover
+    parser = argparse.ArgumentParser(description='pyhocon tool')
+    parser.add_argument('-i', '--input', help='input file')
+    parser.add_argument('-o', '--output', help='output file')
+    parser.add_argument('-f', '--format', help='output format: json, properties or yaml', default='json')
+    parser.add_argument('-v', '--verbosity', action='count', default=0, help='Increase output verbosity')
+    args = parser.parse_args()
+
+    if args.format.lower() not in ['json', 'properties', 'yaml']:
+        raise Exception("Format must be 'json', 'properties' or 'yaml'")
+
+    # Python 2.6 support
+    def null_handler():
+        return logging.NullHandler() if hasattr(logging, 'NullHandler') else logging.FileHandler('/dev/null')
+    logger = logging.getLogger()
+    log_handler = logging.StreamHandler() if args.verbosity > 0 else null_handler()
+    log_handler.setFormatter(logging.Formatter(LOG_FORMAT))
+    logger.addHandler(log_handler)
+    if args.verbosity == 1:
+        logger.setLevel(logging.ERROR)
+    elif args.verbosity == 2:
+        logger.setLevel(logging.INFO)
+    elif args.verbosity >= 3:
+        logger.setLevel(logging.DEBUG)
+
+    HOCONConverter.convert(args.input, args.output, args.format)
+
+
+if __name__ == '__main__':  # pragma: no cover
+    main()
