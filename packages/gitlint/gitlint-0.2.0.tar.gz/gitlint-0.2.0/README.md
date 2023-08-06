ac3a4fdfd85674058c5c0c11c1ff1f5769e75cb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:29 +0000

    Add package gitlint/0.2.0/gitlint-0.2.0.tar.gz

---
+++ packages/gitlint/gitlint-0.2.0.tar.gz/gitlint-0.2.0/README.md
@@ -0,0 +1,132 @@
+# gitlint
+
+[![Build Status](https://travis-ci.org/jorisroovers/gitlint.svg?branch=master)]
+(https://travis-ci.org/jorisroovers/gitlint)
+[![PyPi Package](https://img.shields.io/pypi/v/gitlint.png)]
+(https://pypi.python.org/pypi/gitlint)
+
+Git linter written in python. Checks your git log for style.
+
+**NOTE: gitlint is still under active development**
+
+## Getting Started ##
+```bash
+# Install gitlint
+pip install gitlint
+# Check the last commit message
+gitlint
+# Alternatively, pipe a commit message to gitlint:
+cat examples/commit-message-1 | gitlint
+```
+
+Output example:
+```bash
+$ cat examples/commit-message-2 | gitlint
+1: T1 Title exceeds max length (134>80): "This is the title of a commit message that 	is over 80 characters and contains hard tabs and trailing whitespace and the word wiping  "
+1: T2 Title has trailing whitespace: "This is the title of a commit message that 	is over 80 characters and contains hard tabs and trailing whitespace and the word wiping  "
+1: T4 Title contains hard tab characters (\t): "This is the title of a commit message that 	is over 80 characters and contains hard tabs and trailing whitespace and the word wiping  "
+2: B4 Second line is not empty: "This line should not contain text"
+3: B1 Line exceeds max length (125>80): "Lines typically need to have 	a max length, meaning that they can't exceed a preset number of characters, usually 80 or 120. "
+3: B2 Line has trailing whitespace: "Lines typically need to have 	a max length, meaning that they can't exceed a preset number of characters, usually 80 or 120. "
+3: B3 Line contains hard tab characters (\t): "Lines typically need to have 	a max length, meaning that they can't exceed a preset number of characters, usually 80 or 120. "
+```
+NOTE: The returned exit code equals the number of errors found.
+
+You can modify verbosity using the ```-v``` flag, like so:
+```bash
+$ cat examples/commit-message-2 | gitlint -v
+1: T1
+1: T2
+[removed output]
+$ cat examples/commit-message-2 | gitlint -vv
+1: T1 Title exceeds max length (134>80)
+1: T2 Title has trailing whitespace
+1: T4 Title contains hard tab characters (\t)
+[removed output]
+$ cat examples/commit-message-2 | gitlint -vvv
+1: T1 Title exceeds max length (134>80): "This is the title of a commit message that 	is over 80 characters and contains hard tabs and trailing whitespace and the word wiping  "
+1: T2 Title has trailing whitespace: "This is the title of a commit message that 	is over 80 characters and contains hard tabs and trailing whitespace and the word wiping  "
+[removed output]
+```
+The default verbosity is ```-vvv```.
+
+Other commands and variations:
+
+```bash
+Usage: gitlint [OPTIONS]
+
+  Git lint tool, checks your git commit messages for styling issues
+
+Options:
+  --config PATH  Config file location (default: .gitlint).
+  --ignore TEXT  Ignore rules (comma-separated by id or name).
+  -v, --verbose  Verbosity, more v's for more verbose output (e.g.: -v, -vv,
+                 -vvv). Default: -vvv
+  -s, --silent   Silent mode (no output).
+  --version      Show the version and exit.
+  --help         Show this message and exit.
+```
+
+
+## Config file ##
+
+You can modify gitlint's behavior by specifying a config file like so: 
+```bash
+gitlint --config myconfigfile 
+```
+By default, gitlint will look for an **optional** ```.gitlint``` file for configuration.
+
+```
+[general] 
+ignore=title-trailing-punctuation, T3
+# verbosity should be a value between 1 and 3, the commandline -v flags take precedence over this
+verbosity = 2
+
+[title-max-length]
+line-length=20
+
+[B1]
+# B1 = body-max-line-length
+line-length=30
+```
+
+## Supported Rules ##
+
+ID    | Name                        | Description
+------|-----------------------------|----------------------------------------------------
+T1    | title-max-length            | Title length must be &lt; 80 chars.
+T2    | title-trailing-whitespace   | Title cannot have trailing whitespace (space or tab)
+T3    | title-trailing-punctuation  | Title cannot have trailing punctuation (?:!.,;)
+T4    | title-hard-tab              | Title cannot contain hard tab characters (\t)
+T5    | title-must-not-contain-word | Title cannot contain certain words (default: "WIP")
+B1    | body-max-line-length        | Lines in the body must be &lt; 80 chars.            
+B2    | body-trailing-whitespace    | Body cannot have trailing whitespace (space or tab)
+B3    | body-hard-tab               | Body cannot contain hard tab characters (\t)
+
+## Development ##
+
+To run tests:
+```bash
+./run_tests.sh                       # run unit tests and print test coverage
+./run_tests.sh --no-coverage         # run unit tests without test coverage
+./run_tests.sh --pep8                # pep8 checks
+./run_tests.sh --stats               # print some code stats
+```
+
+There is a Vagrantfile in this repository that can be used for development.
+```bash
+vagrant up
+vagrant ssh
+```
+
+## Wishlist ##
+- More rules: 
+    - title-regex: Title must match a given regex
+    - changed-file-mentioned: If a specific file is changed, it needs to be explicitly mentioned in the commit message
+    - ...
+- Check the entire git log
+- Rule specific configuration
+- Commandline arguments to set config, e.g.: -c general.ignore T2,B3
+- More Unit tests :D
+- Check a specific commit or range of commits, similar to how git log works, eg.: ```git log -1 -3```
+- Checks on different attributes of the the commit message: author, date, etc
---
+++ packages/gitlint/gitlint-0.2.0.tar.gz/gitlint-0.2.0/gitlint/rules.py
@@ -0,0 +1,178 @@
+from abc import abstractmethod, ABCMeta
+from gitlint.options import IntOption, ListOption
+
+import copy
+import re
+
+
+class Rule(object):
+    """ Class representing gitlint rules. """
+    options_spec = []
+    id = []
+    name = ""
+    __metaclass__ = ABCMeta
+
+    def __init__(self, opts={}):
+        self.options = {}
+        for op_spec in self.options_spec:
+            self.options[op_spec.name] = copy.deepcopy(op_spec)
+            actual_option = opts.get(op_spec.name)
+            if actual_option:
+                self.options[op_spec.name].set(actual_option)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.name == other.name
+
+    @abstractmethod
+    def validate(self):
+        pass
+
+
+class MultiLineRule(Rule):
+    """ Class representing rules that act on multiple lines at once """
+    pass
+
+
+class LineRule(Rule):
+    """ Class representing rules that act on a line by line basis """
+    pass
+
+
+class CommitMessageTitleRule(LineRule):
+    """ 'Tagging' class representing rules that apply to a commit message title """
+    pass
+
+
+class CommitMessageBodyRule(Rule):
+    """ 'Tagging' class representing rules that apply to a commit message body """
+    pass
+
+
+class RuleViolation(object):
+    def __init__(self, rule_id, message, content=None, line_nr=None):
+        self.rule_id = rule_id
+        self.line_nr = line_nr
+        self.message = message
+        self.content = content
+
+    def __eq__(self, other):
+        equal = self.rule_id == other.rule_id and self.message == other.message
+        equal = equal and self.content == other.content and self.line_nr == other.line_nr
+        return equal
+
+    def __str__(self):
+        return "{}: {} {}: \"{}\"".format(self.line_nr, self.rule_id, self.message, self.content)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class MaxLineLength(LineRule):
+    name = "max-line-length"
+    id = "R1"
+    options_spec = [IntOption('line-length', 80, "Max line length")]
+    violation_message = "Line exceeds max length ({0}>{1})"
+
+    def validate(self, line):
+        max_length = self.options['line-length'].value
+        if len(line) > max_length:
+            return RuleViolation(self.id, self.violation_message.format(len(line), max_length), line)
+
+
+class TrailingWhiteSpace(LineRule):
+    name = "trailing-whitespace"
+    id = "R2"
+    violation_message = "Line has trailing whitespace"
+
+    def validate(self, line):
+        pattern = re.compile(r"\s$")
+        if pattern.search(line):
+            return RuleViolation(self.id, self.violation_message, line)
+
+
+class HardTab(LineRule):
+    name = "hard-tab"
+    id = "R3"
+    violation_message = "Line contains hard tab characters (\\t)"
+
+    def validate(self, line):
+        if "\t" in line:
+            return RuleViolation(self.id, self.violation_message, line)
+
+
+class LineMustNotContainWord(LineRule):
+    """ Violation if a line contains one of a list of words (NOTE: using a word in the list inside another word is not a
+    violation, e.g: WIPING is not a violation if 'WIP' is a word that is not allowed.) """
+    name = "line-must-not-contain"
+    id = "R5"
+    options_spec = [ListOption('strings', [], "Must not contain strings")]
+    violation_message = "Line contains {0}"
+
+    def validate(self, line):
+        strings = self.options['strings'].value
+        for string in strings:
+            regex = re.compile(r"\b%s\b" % string.lower(), re.I)
+            match = regex.search(line.lower())
+            if match:
+                return RuleViolation(self.id, self.violation_message.format(string), line)
+
+
+class TitleMaxLength(MaxLineLength, CommitMessageTitleRule):
+    name = "title-max-length"
+    id = "T1"
+    violation_message = "Title exceeds max length ({0}>{1})"
+
+
+class TitleTrailingWhitespace(TrailingWhiteSpace, CommitMessageTitleRule):
+    name = "title-trailing-whitespace"
+    id = "T2"
+    violation_message = "Title has trailing whitespace"
+
+
+class TitleTrailingPunctuation(CommitMessageTitleRule):
+    name = "title-trailing-punctuation"
+    id = "T3"
+
+    def validate(self, line):
+        punctuation_marks = '?:!.,;'
+        for punctuation_mark in punctuation_marks:
+            if line.endswith(punctuation_mark):
+                return RuleViolation(self.id, "Title has trailing punctuation ({0})".format(punctuation_mark), line)
+
+
+class TitleHardTab(HardTab, CommitMessageTitleRule):
+    name = "title-hard-tab"
+    id = "T4"
+    violation_message = "Title contains hard tab characters (\\t)"
+
+
+class TitleMustNotContainWord(LineMustNotContainWord, CommitMessageTitleRule):
+    name = "title-must-not-contain-word"
+    id = "T5"
+    options_spec = [ListOption('strings', ['WIP'], "Must not contain word")]
+    violation_message = "Title contains the word '{0}' (case-insensitive)"
+
+
+class BodyMaxLength(MaxLineLength, CommitMessageBodyRule):
+    name = "body-max-line-length"
+    id = "B1"
+
+
+class BodyTrailingWhitespace(TrailingWhiteSpace, CommitMessageBodyRule):
+    name = "body-trailing-whitespace"
+    id = "B2"
+
+
+class BodyHardTab(HardTab, CommitMessageBodyRule):
+    name = "body-hard-tab"
+    id = "B3"
+
+
+class BodyFirstLineEmpty(MultiLineRule, CommitMessageBodyRule):
+    name = "body-first-line-empty"
+    id = "B4"
+
+    def validate(self, lines):
+        if len(lines) >= 1:
+            if lines[0] != "":
+                return [RuleViolation(self.id, "Second line is not empty", lines[0], 2)]
