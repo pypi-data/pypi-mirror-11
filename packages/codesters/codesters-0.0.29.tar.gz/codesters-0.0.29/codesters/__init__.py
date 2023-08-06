f0739514578025fdc174d6db45b46bb6d8f945de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:23 +0000

    Add package codesters/0.0.29/codesters-0.0.29.tar.gz

---
+++ packages/codesters/codesters-0.0.29.tar.gz/codesters-0.0.29/codesters/__init__.py
@@ -0,0 +1,8 @@
+from environment import *
+from shapes import *
+from sprite import *
+from manager import *
+import sys, subprocess
+
+if sys.argv[0]:
+    subprocess.call("codesters " + sys.argv[0])
