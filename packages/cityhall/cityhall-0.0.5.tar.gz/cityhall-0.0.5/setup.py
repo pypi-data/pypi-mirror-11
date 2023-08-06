c49715faad7f008fa881ddc36e800fc458687f7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:55 +0000

    Add package cityhall/0.0.5/cityhall-0.0.5.tar.gz

---
+++ packages/cityhall/cityhall-0.0.5.tar.gz/cityhall-0.0.5/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'cityhall',
+  packages = ['cityhall'], # this must be the same as the name above
+  version = '0.0.5',
+  description = 'A library for accessing City Hall Setting Server',
+  author = 'Alex Popa',
+  author_email = 'alexpopa@digitalborderlands.com',
+  url = 'https://github.com/f00f-nyc/cityhall-python',
+  download_url = 'https://codeload.github.com/f00f-nyc/cityhall-python/legacy.tar.gz/v0.0.5',
+  keywords = ['cityhall', 'enterprise', 'cityhall', 'City Hall'],
+  classifiers = [],
+)
