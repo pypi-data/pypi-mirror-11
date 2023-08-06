95e9cd0f6f6d9df7dd5a1700f7c72771b4b779e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:11 +0000

    Add package swiftsc/0.7.0/swiftsc-0.7.0.tar.gz

---
+++ packages/swiftsc/swiftsc-0.7.0.tar.gz/swiftsc-0.7.0/swiftsc/exception.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""swiftsc exception module."""
+
+
+class Error(Exception):
+
+    """Base error class.
+
+    Child classes should define an status code, title, and message_format.
+    """
+
+    def __init__(self, message=None):
+        """constructor of Error."""
+        super(Error, self).__init__(message)
+
+
+class ValidationError(Error):
+
+    """Not found key."""
+
+    pass
