5547f31f709d0c0b2ab5d083a8fc515b58ab42eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:50 +0000

    Add package abcyui/1.0.0/abcyui-1.0.0.zip

---
+++ packages/abcyui/abcyui-1.0.0.zip/abcyui-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+        name='abcyui',
+        version='1.0.0',
+        py_modules=['abcyui'],
+        author='keyor',
+        url='123.com',
+        author_email='123@163.com',
+        description='Sorry ,This is practice!'
+
+
+    )
